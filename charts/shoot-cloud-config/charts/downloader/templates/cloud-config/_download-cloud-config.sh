5b9b3c2a154355c89cd5544b0d91af058c20968f
Rafael Franzke <rafael.franzke@sap.com>
2018-03-14 12:51:59 +0100

    Do not restart Docker on cloud config updates

--- charts/shoot-cloud-config/charts/downloader/templates/cloud-config/_download-cloud-config.sh
+++ charts/shoot-cloud-config/charts/downloader/templates/cloud-config/_download-cloud-config.sh
@@ -3,15 +3,14 @@
 
 SECRET_NAME="{{ required "secretName is required" .Values.secretName }}"
 
-DIR_CLOUDCONFIG="/var/run/coreos"
 DIR_CLOUDCONFIG_DOWNLOADER="/var/lib/cloud-config-downloader"
+DIR_CLOUDCONFIG="/var/run/coreos"
 DIR_KUBELET="/var/lib/kubelet"
 
 PATH_YAML2JSON="$DIR_CLOUDCONFIG_DOWNLOADER/yaml2json"
 PATH_KUBECONFIG="$DIR_CLOUDCONFIG_DOWNLOADER/kubeconfig"
-PATH_RESOURCEVERSION_CURRENT="$DIR_CLOUDCONFIG_DOWNLOADER/current_resourceversion"
-PATH_RESOURCE_LAST_APPLIED="$DIR_CLOUDCONFIG_DOWNLOADER/last_applied_resourceversion"
 PATH_CLOUDCONFIG="$DIR_CLOUDCONFIG/cloud_config.yml"
+PATH_CLOUDCONFIG_OLD="$DIR_CLOUDCONFIG/cloud_config.old.yml"
 
 mkdir -p "$DIR_CLOUDCONFIG" "$DIR_KUBELET"
 
@@ -25,15 +24,14 @@ if ! CLOUD_CONFIG_SECRET="$(/bin/docker run \
   --net host \
   -v "$DIR_CLOUDCONFIG"/:"$DIR_CLOUDCONFIG" \
   -v "$DIR_CLOUDCONFIG_DOWNLOADER"/:"$DIR_CLOUDCONFIG_DOWNLOADER" \
-  k8s.gcr.io/hyperkube:v1.9.2\
-  kubectl --kubeconfig="$PATH_KUBECONFIG" --namespace=kube-system get secret "$SECRET_NAME" -o jsonpath='{.metadata.resourceVersion}{"\t"}{.data.cloudconfig}{"\t"}{.data.bootstrapToken}')"; then
+  k8s.gcr.io/hyperkube:v1.9.3\
+  kubectl --kubeconfig="$PATH_KUBECONFIG" --namespace=kube-system get secret "$SECRET_NAME" -o jsonpath='{.data.cloudconfig}{"\t"}{.data.bootstrapToken}')"; then
   echo "Could not retrieve the cloud config secret with name $SECRET_NAME"
   exit 1
 fi
 
-echo $CLOUD_CONFIG_SECRET | awk '{print $1}' > "$PATH_RESOURCEVERSION_CURRENT"
-echo $CLOUD_CONFIG_SECRET | awk '{print $2}' | base64 -d > "$PATH_CLOUDCONFIG"
-BOOTSTRAP_TOKEN="$(echo $CLOUD_CONFIG_SECRET | awk '{print $3}' | base64 -d)"
+echo $CLOUD_CONFIG_SECRET | awk '{print $1}' | base64 -d > "$PATH_CLOUDCONFIG"
+BOOTSTRAP_TOKEN="$(echo $CLOUD_CONFIG_SECRET | awk '{print $2}' | base64 -d)"
 
 if [ ! -f "$PATH_CLOUDCONFIG" ]; then
   echo "No cloud config file found at location $PATH_CLOUDCONFIG"
@@ -71,26 +69,22 @@ else
   rm -f "$DIR_KUBELET/kubeconfig-bootstrap"
 fi
 
-RESOURCEVERSION_CURRENT=0
-RESOURCEVERSION_LAST_APPLIED=0
-
-if [ -f "$PATH_RESOURCEVERSION_CURRENT" ]; then
-  RESOURCEVERSION_CURRENT="$(cat $PATH_RESOURCEVERSION_CURRENT)"
-fi
-if [ -f "$PATH_RESOURCE_LAST_APPLIED" ]; then
-  RESOURCEVERSION_LAST_APPLIED="$(cat $PATH_RESOURCE_LAST_APPLIED)"
+if [ ! -f "$PATH_CLOUDCONFIG_OLD" ]; then
+  touch "$PATH_CLOUDCONFIG_OLD"
 fi
 
-if [ "$RESOURCEVERSION_CURRENT" -gt "$RESOURCEVERSION_LAST_APPLIED" ]; then
-  echo "Seen cloud config version $RESOURCEVERSION_CURRENT, applying it"
+if ! diff "$PATH_CLOUDCONFIG" "$PATH_CLOUDCONFIG_OLD" >/dev/null; then
+  echo "Seen newer cloud config version"
   if /usr/bin/coreos-cloudinit -from-file="$PATH_CLOUDCONFIG"; then
-    echo "Successfully applied cloud config with resource version $RESOURCEVERSION_CURRENT"
-    if [ "$RESOURCEVERSION_LAST_APPLIED" -ne "0" ]; then
+    echo "Successfully applied new cloud config version"
+    if [ "$(stat -c%s "$PATH_CLOUDCONFIG_OLD")" -ne "0" ]; then
       systemctl daemon-reload
-      "$PATH_YAML2JSON" < "$PATH_CLOUDCONFIG" | jq -r '.coreos.units[] | .name' | xargs systemctl restart
-      echo "Successfully restarted all our units."
+      "$PATH_YAML2JSON" < "$PATH_CLOUDCONFIG" | jq -r '.coreos.units[] | select(.name != "docker.service") | .name' | xargs systemctl restart
+      echo "Successfully restarted all units referenced in the cloud config."
     fi
-    echo $RESOURCEVERSION_CURRENT > $PATH_RESOURCE_LAST_APPLIED
+    cp "$PATH_CLOUDCONFIG" "$PATH_CLOUDCONFIG_OLD"
   fi
 fi
+
+rm "$PATH_CLOUDCONFIG"
 {{- end -}}
