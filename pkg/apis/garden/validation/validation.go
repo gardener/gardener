15598f3320b162403107d93bba5751de1ed4b79a
Rafael Franzke <rafael.franzke@sap.com>
2018-04-20 08:54:43 +0200

    Validate DNS1123 subdomain compliance for worker pool names
    cc @PadmaB

--- pkg/apis/garden/validation/validation.go
+++ pkg/apis/garden/validation/validation.go
@@ -1219,9 +1219,7 @@ func validateMaintenance(maintenance *garden.Maintenance, fldPath *field.Path) f
 func validateWorker(worker garden.Worker, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	if len(worker.Name) == 0 {
-		allErrs = append(allErrs, field.Required(fldPath.Child("name"), "must specify a name"))
-	}
+	allErrs = append(allErrs, validateDNS1123Subdomain(worker.Name, fldPath.Child("name"))...)
 	maxWorkerNameLength := 15
 	if len(worker.Name) > maxWorkerNameLength {
 		allErrs = append(allErrs, field.TooLong(fldPath.Child("name"), worker.Name, maxWorkerNameLength))
@@ -1288,8 +1286,10 @@ func ValidateShootStatusUpdate(newShoot, oldShoot *garden.Shoot) field.ErrorList
 // validateDNS1123Subdomain validates that a name is a proper DNS subdomain.
 func validateDNS1123Subdomain(value string, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
+
 	for _, msg := range validation.IsDNS1123Subdomain(value) {
 		allErrs = append(allErrs, field.Invalid(fldPath, value, msg))
 	}
+
 	return allErrs
 }
