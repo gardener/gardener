436e772b019171c99b2cb25af6ba7deb580fe406
Rafael Franzke <rafael.franzke@sap.com>
2018-08-14 19:02:49 +0200

    Define admission plugins per Kubernetes version

---
+++ pkg/client/kubernetes/admissionplugins.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubernetes
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver"
+	gardenv1beta1 "github.com/gardener/gardener/pkg/apis/garden/v1beta1"
+)
+
+var (
+	defaultPlugins = []gardenv1beta1.AdmissionPlugin{
+		{Name: "Priority"},
+		{Name: "NamespaceLifecycle"},
+		{Name: "LimitRanger"},
+		{Name: "ServiceAccount"},
+		{Name: "NodeRestriction"},
+		{Name: "DefaultStorageClass"},
+		{Name: "PersistentVolumeLabel"},
+		{Name: "DefaultTolerationSeconds"},
+	}
+
+	lowestSupportedKubernetesVersionMajorMinor = "1.8"
+	lowestSupportedKubernetesVersion, _        = semver.NewVersion(lowestSupportedKubernetesVersionMajorMinor)
+
+	admissionPlugins = map[string][]gardenv1beta1.AdmissionPlugin{
+		"1.8":  append(defaultPlugins, gardenv1beta1.AdmissionPlugin{Name: "ResourceQuota"}),
+		"1.9":  append(defaultPlugins, gardenv1beta1.AdmissionPlugin{Name: "MutatingAdmissionWebhook"}, gardenv1beta1.AdmissionPlugin{Name: "ValidatingAdmissionWebhook"}, gardenv1beta1.AdmissionPlugin{Name: "ResourceQuota"}),
+		"1.10": append(defaultPlugins, gardenv1beta1.AdmissionPlugin{Name: "ResourceQuota"}, gardenv1beta1.AdmissionPlugin{Name: "StorageObjectInUseProtection"}, gardenv1beta1.AdmissionPlugin{Name: "MutatingAdmissionWebhook"}, gardenv1beta1.AdmissionPlugin{Name: "ValidatingAdmissionWebhook"}),
+	}
+)
+
+// GetAdmissionPluginsForVersion returns the set of default admission plugins for the given Kubernetes version.
+// If the given Kubernetes version does not explicitly define admission plugins the set of names for the next
+// available version will be returned (e.g., for version X not defined the set of version X-1 will be returned).
+func GetAdmissionPluginsForVersion(v string) []gardenv1beta1.AdmissionPlugin {
+	version, err := semver.NewVersion(v)
+	if err != nil {
+		return admissionPlugins[lowestSupportedKubernetesVersionMajorMinor]
+	}
+
+	if version.LessThan(lowestSupportedKubernetesVersion) {
+		return admissionPlugins[lowestSupportedKubernetesVersionMajorMinor]
+	}
+
+	majorMinor := formatMajorMinor(version.Major(), version.Minor())
+	if pluginsForVersion, ok := admissionPlugins[majorMinor]; ok {
+		return pluginsForVersion
+	}
+
+	// We do not handle decrementing the major part of the version. The reason for this is that we would have to set
+	// the minor part to some higher value which we don't know (assume we go from 2.2->2.1->2.0->1.?). We decided not
+	// to handle decrementing the major part at all, as if Gardener supports Kubernetes 2.X (independent of the fact
+	// that it's anyway unclear when/whether that will come) many parts have to be adapted anyway.
+	return GetAdmissionPluginsForVersion(formatMajorMinor(version.Major(), version.Minor()-1))
+}
+
+func formatMajorMinor(major, minor int64) string {
+	return fmt.Sprintf("%d.%d", major, minor)
+}
---
+++ pkg/client/kubernetes/admissionplugins_test.go
@@ -0,0 +1,87 @@
+// Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubernetes_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	gardenv1beta1 "github.com/gardener/gardener/pkg/apis/garden/v1beta1"
+	. "github.com/gardener/gardener/pkg/client/kubernetes"
+)
+
+var _ = Describe("kubernetes", func() {
+	Describe("#GetAdmissionPluginsForVersion", func() {
+		It("should return the list for 1.8 (non-parseable version)", func() {
+			expected := []string{"Priority", "NamespaceLifecycle", "LimitRanger", "ServiceAccount", "NodeRestriction", "DefaultStorageClass", "PersistentVolumeLabel", "DefaultTolerationSeconds", "ResourceQuota"}
+
+			plugins := GetAdmissionPluginsForVersion("not-1-a-semver-version")
+
+			for _, plugin := range expected {
+				Expect(plugins).To(ContainElement(gardenv1beta1.AdmissionPlugin{Name: plugin}))
+			}
+		})
+
+		It("should return the list for 1.8 (lowest supported version)", func() {
+			expected := []string{"Priority", "NamespaceLifecycle", "LimitRanger", "ServiceAccount", "NodeRestriction", "DefaultStorageClass", "PersistentVolumeLabel", "DefaultTolerationSeconds", "ResourceQuota"}
+
+			plugins := GetAdmissionPluginsForVersion("1.7.4")
+
+			for _, plugin := range expected {
+				Expect(plugins).To(ContainElement(gardenv1beta1.AdmissionPlugin{Name: plugin}))
+			}
+		})
+
+		It("should return the list for 1.8", func() {
+			expected := []string{"Priority", "NamespaceLifecycle", "LimitRanger", "ServiceAccount", "NodeRestriction", "DefaultStorageClass", "PersistentVolumeLabel", "DefaultTolerationSeconds", "ResourceQuota"}
+
+			plugins := GetAdmissionPluginsForVersion("1.8.4")
+
+			for _, plugin := range expected {
+				Expect(plugins).To(ContainElement(gardenv1beta1.AdmissionPlugin{Name: plugin}))
+			}
+		})
+
+		It("should return the list for 1.9", func() {
+			expected := []string{"Priority", "NamespaceLifecycle", "LimitRanger", "ServiceAccount", "NodeRestriction", "DefaultStorageClass", "PersistentVolumeLabel", "DefaultTolerationSeconds", "ResourceQuota", "MutatingAdmissionWebhook", "ValidatingAdmissionWebhook"}
+
+			plugins := GetAdmissionPluginsForVersion("1.9.0")
+
+			for _, plugin := range expected {
+				Expect(plugins).To(ContainElement(gardenv1beta1.AdmissionPlugin{Name: plugin}))
+			}
+		})
+
+		It("should return the list for 1.10", func() {
+			expected := []string{"Priority", "NamespaceLifecycle", "LimitRanger", "ServiceAccount", "NodeRestriction", "DefaultStorageClass", "PersistentVolumeLabel", "DefaultTolerationSeconds", "ResourceQuota", "StorageObjectInUseProtection", "MutatingAdmissionWebhook", "ValidatingAdmissionWebhook"}
+
+			plugins := GetAdmissionPluginsForVersion("1.10.99")
+
+			for _, plugin := range expected {
+				Expect(plugins).To(ContainElement(gardenv1beta1.AdmissionPlugin{Name: plugin}))
+			}
+		})
+
+		It("should return the list for 1.11", func() {
+			expected := []string{"Priority", "NamespaceLifecycle", "LimitRanger", "ServiceAccount", "NodeRestriction", "DefaultStorageClass", "PersistentVolumeLabel", "DefaultTolerationSeconds", "ResourceQuota", "StorageObjectInUseProtection", "MutatingAdmissionWebhook", "ValidatingAdmissionWebhook"}
+
+			plugins := GetAdmissionPluginsForVersion("1.11.23")
+
+			for _, plugin := range expected {
+				Expect(plugins).To(ContainElement(gardenv1beta1.AdmissionPlugin{Name: plugin}))
+			}
+		})
+	})
+})
