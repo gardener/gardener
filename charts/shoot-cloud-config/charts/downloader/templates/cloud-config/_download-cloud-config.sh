539db79faf254821540b4b5ca3f33b49fd3b8d9f
Rafael Franzke <rafael.franzke@sap.com>
2018-09-10 15:54:08 +0200

    Remove dependency of yaml2json in cloud-config-downloader script
    ```noteworthy operator
    :warning: Gardener was depending on yaml2json which was downloaded on every worker machine in order to read the kubeconfig and the cloud-config. This dependency has now been removed (instead, we are now working inside Docker containers to not rely on external tools but rather on stable images). Gardener MUST be updated as the yaml2json binary is no longer available at its download URL which results in the fact that no new machines can join the cluster (as the cloud-config cannot be read/extracted). This will result in a rolling update of all machines for all clusters.
    ```
    
    /cc @Adracus @swapnilgm
    
    This will result in a rolling update of all machines.

--- charts/shoot-cloud-config/charts/downloader/templates/cloud-config/_download-cloud-config.sh
+++ charts/shoot-cloud-config/charts/downloader/templates/cloud-config/_download-cloud-config.sh
@@ -7,41 +7,47 @@ DIR_CLOUDCONFIG_DOWNLOADER="/var/lib/cloud-config-downloader"
 DIR_CLOUDCONFIG="/var/run/coreos"
 DIR_KUBELET="/var/lib/kubelet"
 
-PATH_YAML2JSON="$DIR_CLOUDCONFIG_DOWNLOADER/yaml2json"
 PATH_KUBECONFIG="$DIR_CLOUDCONFIG_DOWNLOADER/kubeconfig"
 PATH_CLOUDCONFIG="$DIR_CLOUDCONFIG/cloud_config.yml"
 PATH_CLOUDCONFIG_OLD="$DIR_CLOUDCONFIG/cloud_config.old.yml"
 
 mkdir -p "$DIR_CLOUDCONFIG" "$DIR_KUBELET"
 
-if [ ! -f "$PATH_YAML2JSON" ]; then
-  curl -L "https://github.com/bronze1man/yaml2json/raw/master/builds/linux_amd64/yaml2json" -o "$PATH_YAML2JSON"
-  chmod +x "$PATH_YAML2JSON"
-fi
+function kubectl() {
+  /bin/docker run \
+    --rm \
+    --net host \
+    -v "$DIR_CLOUDCONFIG"/:"$DIR_CLOUDCONFIG" \
+    -v "$DIR_CLOUDCONFIG_DOWNLOADER"/:"$DIR_CLOUDCONFIG_DOWNLOADER" \
+    -e "KUBECONFIG=$PATH_KUBECONFIG" \
+    k8s.gcr.io/hyperkube:v1.9.3 \
+    kubectl "$@"
+}
 
-if ! CLOUD_CONFIG_SECRET="$(/bin/docker run \
-  --rm \
-  --net host \
-  -v "$DIR_CLOUDCONFIG"/:"$DIR_CLOUDCONFIG" \
-  -v "$DIR_CLOUDCONFIG_DOWNLOADER"/:"$DIR_CLOUDCONFIG_DOWNLOADER" \
-  k8s.gcr.io/hyperkube:v1.9.3\
-  kubectl --kubeconfig="$PATH_KUBECONFIG" --namespace=kube-system get secret "$SECRET_NAME" -o jsonpath='{.data.cloudconfig}{"\t"}{.data.bootstrapToken}')"; then
-  echo "Could not retrieve the cloud config secret with name $SECRET_NAME"
+if ! CLOUD_CONFIG="$(kubectl --namespace=kube-system get secret "$SECRET_NAME" -o jsonpath='{.data.cloudconfig}')"; then
+  echo "Could not retrieve the cloud config in secret with name $SECRET_NAME"
+  exit 1
+fi
+if ! BOOTSTRAP_TOKEN="$(kubectl --namespace=kube-system get secret "$SECRET_NAME" -o jsonpath='{.data.bootstrapToken}')"; then
+  echo "Could not retrieve the bootstrap in secret with name $SECRET_NAME"
   exit 1
 fi
 
-echo $CLOUD_CONFIG_SECRET | awk '{print $1}' | base64 -d > "$PATH_CLOUDCONFIG"
-BOOTSTRAP_TOKEN="$(echo $CLOUD_CONFIG_SECRET | awk '{print $2}' | base64 -d)"
-
+base64 -d <<< $CLOUD_CONFIG > "$PATH_CLOUDCONFIG"
 if [ ! -f "$PATH_CLOUDCONFIG" ]; then
   echo "No cloud config file found at location $PATH_CLOUDCONFIG"
   exit 1
 fi
 
 if [[ ! -f "$DIR_KUBELET/kubeconfig-real" ]]; then
-  CLUSTER_INFO="$("$PATH_YAML2JSON" < "$PATH_KUBECONFIG" | jq -r '.clusters[0].cluster')"
-  CA_CRT="$(echo $CLUSTER_INFO | jq -r '."certificate-authority-data"')"
-  SERVER="$(echo $CLUSTER_INFO | jq -r '.server')"
+  if ! SERVER="$(kubectl config view -o go-template='{{ "{{" }}index .clusters 0 "cluster" "server"{{ "}}" }}' --raw)"; then
+    echo "Could not retrieve the kube-apiserver address."
+    exit 1
+  fi
+  if ! CA_CRT="$(kubectl config view -o go-template='{{ "{{" }}index .clusters 0 "cluster" "certificate-authority-data"{{ "}}" }}' --raw)"; then
+    echo "Could not retrieve the CA certificate."
+    exit 1
+  fi
 
   cat <<EOF > "$DIR_KUBELET/kubeconfig-bootstrap"
 ---
@@ -62,7 +68,7 @@ users:
 - name: kubelet-bootstrap
   user:
     as-user-extra: {}
-    token: $BOOTSTRAP_TOKEN
+    token: $(echo $BOOTSTRAP_TOKEN | base64 -d)
 EOF
 
 else
@@ -79,7 +85,7 @@ if ! diff "$PATH_CLOUDCONFIG" "$PATH_CLOUDCONFIG_OLD" >/dev/null; then
     echo "Successfully applied new cloud config version"
     if [ "$(stat -c%s "$PATH_CLOUDCONFIG_OLD")" -ne "0" ]; then
       systemctl daemon-reload
-      "$PATH_YAML2JSON" < "$PATH_CLOUDCONFIG" | jq -r '.coreos.units[] | select(.name != "docker.service") | .name' | xargs systemctl restart
+      cat "$PATH_CLOUDCONFIG" | docker run -i {{ index .Values.images "ruby" }} ruby -e "require 'yaml'; puts YAML::load(STDIN.read)['coreos']['units'].map{|v| v['name'] if v['name'] != 'docker.service'}.compact" | xargs systemctl restart
       echo "Successfully restarted all units referenced in the cloud config."
     fi
     cp "$PATH_CLOUDCONFIG" "$PATH_CLOUDCONFIG_OLD"
