c6378d677b4c1d103e11c7236274699174ee65e8
Tim Usner <tim.usner@sap.com>
2018-09-25 15:31:24 +0200

    Remove label initializers for cloud independent persistent volumes
    Co-authored-by: Axel Christ <axel.christ@sap.com>

--- pkg/operation/common/utils.go
+++ pkg/operation/common/utils.go
@@ -348,3 +348,30 @@ func MergeOwnerReferences(references []metav1.OwnerReference, newReferences ...m
 
 	return references
 }
+
+// HasInitializer checks whether the passed name is part of the pending initializers
+func HasInitializer(initializers *metav1.Initializers, name string) bool {
+	if initializers == nil {
+		return false
+	}
+	for _, initializer := range initializers.Pending {
+		if initializer.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+// RemoveInitializer removes the passed initializer name from the pending initializers
+func RemoveInitializer(initializers *metav1.Initializers, name string) *metav1.Initializers {
+	if initializers == nil {
+		return nil
+	}
+	var updatedInitializers []metav1.Initializer
+	for _, initializer := range initializers.Pending {
+		if initializer.Name != name {
+			updatedInitializers = append(updatedInitializers, initializer)
+		}
+	}
+	return &metav1.Initializers{Pending: updatedInitializers, Result: initializers.Result}
+}
