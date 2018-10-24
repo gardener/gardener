8c7cf3a6ba1911b1a27438ad4a3f8102d981f86d
Rafael Franzke <rafael.franzke@sap.com>
2018-10-24 20:13:38 +0200

    Add migration for legacy role bindings to project members
    Co-Authored-By: Axel Christ <axel.christ@sap.com>

--- pkg/controller/project/project.go
+++ pkg/controller/project/project.go
@@ -16,19 +16,31 @@ package project
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
+	gardenv1beta1 "github.com/gardener/gardener/pkg/apis/garden/v1beta1"
 	gardeninformers "github.com/gardener/gardener/pkg/client/garden/informers/externalversions"
 	gardenlisters "github.com/gardener/gardener/pkg/client/garden/listers/garden/v1beta1"
 	"github.com/gardener/gardener/pkg/client/kubernetes"
 	controllerutils "github.com/gardener/gardener/pkg/controller/utils"
 	"github.com/gardener/gardener/pkg/logger"
+	"github.com/gardener/gardener/pkg/operation/common"
+	"github.com/gardener/gardener/pkg/utils"
+	kutils "github.com/gardener/gardener/pkg/utils/kubernetes"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	kubeinformers "k8s.io/client-go/informers"
 	kubecorev1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/retry"
 	"k8s.io/client-go/util/workqueue"
+
+	multierror "github.com/hashicorp/go-multierror"
 )
 
 // Controller controls Projects.
@@ -116,6 +128,17 @@ func (c *Controller) Run(ctx context.Context, workers int) {
 
 	logger.Logger.Info("Project controller initialized.")
 
+	// Migration of legacy rolebinding to new .spec.members field in project resource
+	go func() {
+		utils.Retry(10*time.Second, 10*time.Minute, func() (ok, severe bool, err error) {
+			if err := c.migrateRoleBindingToProjectMembers(); err != nil {
+				logger.Logger.Errorf("[ROLEBINDING MIGRATION] Error migrating old rolebindings to project members: %+v", err)
+				return false, false, err
+			}
+			return true, false, nil
+		})
+	}()
+
 	for i := 0; i < workers; i++ {
 		controllerutils.CreateWorker(ctx, c.projectQueue, "Project", c.reconcileProjectKey, &waitGroup, c.workerCh)
 	}
@@ -139,4 +162,50 @@ func (c *Controller) Run(ctx context.Context, workers int) {
 // RunningWorkers returns the number of running workers.
 func (c *Controller) RunningWorkers() int {
 	return c.numberOfRunningWorkers
+}
+
+// This function has been introduced to migrate the legacy project member rolebindings created by older
+// versions of the Gardener dashboard (prior 1.23) to the new .spec.members field of the Project resource.
+// It can be removed in future versions of Gardener.
+func (c *Controller) migrateRoleBindingToProjectMembers() error {
+	projectList, err := c.projectLister.List(labels.Everything())
+	if err != nil {
+		return fmt.Errorf("error fetching project list: %+v", err)
+	}
+
+	roleBindingList, err := c.k8sGardenClient.ListRoleBindings(metav1.NamespaceAll, metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(map[string]string{common.GardenRole: common.GardenRoleMembers}).String(),
+	})
+	if err != nil {
+		return fmt.Errorf("error fetching rolebinding list: %+v", err)
+	}
+
+	var (
+		namespaceToProject = make(map[string]string, len(projectList))
+		result             error
+	)
+
+	for _, project := range projectList {
+		if namespace := project.Spec.Namespace; namespace != nil {
+			namespaceToProject[*namespace] = project.Name
+		}
+	}
+
+	for _, roleBinding := range roleBindingList.Items {
+		if projectName, ok := namespaceToProject[roleBinding.Namespace]; ok {
+			if _, err := kutils.TryUpdateProject(c.k8sGardenClient.GardenClientset(), retry.DefaultBackoff, metav1.ObjectMeta{Name: projectName}, func(project *gardenv1beta1.Project) (*gardenv1beta1.Project, error) {
+				project.Spec.Members = roleBinding.Subjects
+				return project, nil
+			}); err != nil {
+				result = multierror.Append(result, err)
+				continue
+			}
+
+			if err := c.k8sGardenClient.Clientset().RbacV1().RoleBindings(roleBinding.Namespace).Delete(roleBinding.Name, &metav1.DeleteOptions{}); err != nil && !apierrors.IsNotFound(err) {
+				result = multierror.Append(result, err)
+			}
+		}
+	}
+
+	return result
 }
