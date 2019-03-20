361588e6df7df031b2e8f787e304789f466c9e1e
Rafael Franzke <rafael.franzke@sap.com>
2019-03-20 14:36:04 +0100

    Don't delete new kube-node-lease namespace during cluster cleanup
    See kubernetes/kubernetes#72096

--- pkg/operation/botanist/cleanup.go
+++ pkg/operation/botanist/cleanup.go
@@ -26,6 +26,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/runtime"
 
 	"github.com/hashicorp/go-multierror"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -59,9 +60,10 @@ var (
 			fmt.Sprintf("%s/csi-snapshotter", metav1.NamespaceSystem): true,
 		},
 		kubernetes.Namespaces: {
-			metav1.NamespacePublic:  true,
-			metav1.NamespaceSystem:  true,
-			metav1.NamespaceDefault: true,
+			metav1.NamespacePublic:    true,
+			metav1.NamespaceSystem:    true,
+			metav1.NamespaceDefault:   true,
+			corev1.NamespaceNodeLease: true,
 		},
 		kubernetes.Services: {
 			fmt.Sprintf("%s/kubernetes", metav1.NamespaceDefault): true,
