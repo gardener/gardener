d71fd471c9068371b56d67cd0ed81dcec9fd106b
Jerry Jia <jerry.jia@sap.com>
2019-07-11 09:49:41 +0800

    Enable encryption for default storage class of shoot cluster

--- pkg/operation/cloudbotanist/alicloudbotanist/addons.go
+++ pkg/operation/cloudbotanist/alicloudbotanist/addons.go
@@ -56,6 +56,7 @@ func (b *AlicloudBotanist) GenerateStorageClassesConfig() (map[string]interface{
 					"csi.storage.k8s.io/fstype": "ext4",
 					"type":                      "cloud_ssd",
 					"readOnly":                  "false",
+					"encrypted":                 "true",
 				},
 			},
 		},
