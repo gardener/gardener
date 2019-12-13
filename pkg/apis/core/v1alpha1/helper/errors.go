db28bdc737453baea38abae7b0de698762109a95
Daniel Pacrami <daniel.pacrami@sap.com>
2019-12-13 14:33:52 +0100

    Add RegEx for new infra dependency
    Some cloud providers respond with `not available in the
    current hardware cluster` when they do not have
    sufficient machines in this region
    
    ```improvement operator
    Classify as infrastructure dependency error if a cloud provider does not have sufficient machine types in a region
    ```

--- pkg/apis/core/v1alpha1/helper/errors.go
+++ pkg/apis/core/v1alpha1/helper/errors.go
@@ -50,7 +50,7 @@ var (
 	unauthorizedRegexp           = regexp.MustCompile(`(?i)(Unauthorized|InvalidClientTokenId|SignatureDoesNotMatch|Authentication failed|AuthFailure|AuthorizationFailed|invalid character|invalid_grant|invalid_client|Authorization Profile was not found|cannot fetch token|no active subscriptions|InvalidAccessKeyId|InvalidSecretAccessKey)`)
 	quotaExceededRegexp          = regexp.MustCompile(`(?i)(LimitExceeded|Quota)`)
 	insufficientPrivilegesRegexp = regexp.MustCompile(`(?i)(AccessDenied|Forbidden|deny|denied)`)
-	dependenciesRegexp           = regexp.MustCompile(`(?i)(PendingVerification|Access Not Configured|accessNotConfigured|DependencyViolation|OptInRequired|DeleteConflict|Conflict|inactive billing state|ReadOnlyDisabledSubscription|is already being used)`)
+	dependenciesRegexp           = regexp.MustCompile(`(?i)(PendingVerification|Access Not Configured|accessNotConfigured|DependencyViolation|OptInRequired|DeleteConflict|Conflict|inactive billing state|ReadOnlyDisabledSubscription|is already being used|not available in the current hardware cluster)`)
 )
 
 // DetermineError determines the Garden error code for the given error message.
