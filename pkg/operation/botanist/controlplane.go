612952083d6d0552e7f2b53e0a0fc0cb7f35219b
Rafael Franzke <rafael.franzke@sap.com>
2020-06-26 10:03:38 +0200

    Fix replica computation for kube-controller-manager

--- pkg/operation/botanist/controlplane.go
+++ pkg/operation/botanist/controlplane.go
@@ -21,7 +21,6 @@ import (
 	"path/filepath"
 	"time"
 
-	druidv1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	gardencorev1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
@@ -38,8 +37,9 @@ import (
 	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
 	"github.com/gardener/gardener/pkg/utils/retry"
 	"github.com/gardener/gardener/pkg/utils/version"
-	hvpav1alpha1 "github.com/gardener/hvpa-controller/api/v1alpha1"
 
+	druidv1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
+	hvpav1alpha1 "github.com/gardener/hvpa-controller/api/v1alpha1"
 	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -1133,14 +1133,28 @@ func (b *Botanist) DeployKubeControllerManager(ctx context.Context) error {
 		},
 	}
 
-	if b.Shoot.HibernationEnabled == b.Shoot.Info.Status.IsHibernated {
-		// Keep the replica count same if the shoot is not transitioning from/to hibernation state
-		// otherwise this may interfere with dependency-watchdog
-		replicaCount, err := common.CurrentReplicaCount(b.K8sSeedClient.Client(), b.Shoot.SeedNamespace, v1beta1constants.DeploymentNameKubeControllerManager)
-		if err != nil {
-			return err
+	if b.Shoot.Info.Status.LastOperation != nil && b.Shoot.Info.Status.LastOperation.Type == gardencorev1beta1.LastOperationTypeCreate {
+		if b.Shoot.HibernationEnabled {
+			// shoot is being created with .spec.hibernation.enabled=true, don't deploy KCM at all
+			defaultValues["replicas"] = 0
+		} else {
+			// shoot is being created with .spec.hibernation.enabled=false, deploy KCM
+			defaultValues["replicas"] = 1
 		}
-		defaultValues["replicas"] = replicaCount
+	} else {
+		if b.Shoot.HibernationEnabled == b.Shoot.Info.Status.IsHibernated {
+			// shoot is being reconciled with .spec.hibernation.enabled=.status.isHibernated, so keep the replicas which
+			// are controlled by the dependency-watchdog
+			replicaCount, err := common.CurrentReplicaCount(ctx, b.K8sSeedClient.Client(), b.Shoot.SeedNamespace, v1beta1constants.DeploymentNameKubeControllerManager)
+			if err != nil {
+				return err
+			}
+			defaultValues["replicas"] = replicaCount
+		} else {
+			// shoot is being reconciled with .spec.hibernation.enabled!=.status.isHibernated, so deploy KCM. in case the
+			// shoot is being hibernated then it will be scaled down to zero later after all machines are gone
+			defaultValues["replicas"] = 1
+		}
 	}
 
 	controllerManagerConfig := b.Shoot.Info.Spec.Kubernetes.KubeControllerManager
