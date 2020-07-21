53a6d1e75d0c57b61e35951ebff5529733a56b56
Tim Ebert <tim.ebert@sap.com>
2020-07-21 11:57:56 +0200

    Remove v-prefix from version.Major

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -23,29 +23,35 @@ import (
 )
 
 var (
-	gitVersion   = "0.0.0-dev"
+	gitVersion   = "v0.0.0-dev"
 	gitCommit    string
 	gitTreeState string
 	buildDate    = "1970-01-01T00:00:00Z"
+
+	version *apimachineryversion.Info
 )
 
 // Get returns the overall codebase version. It's for detecting
 // what code a binary was built from.
 // These variables typically come from -ldflags settings and in
-// their absence fallback to the settings in pkg/version/base.go
+// their absence fallback to the settings in pkg/version/version.go
 func Get() apimachineryversion.Info {
+	return *version
+}
+
+func init() {
 	var (
-		version  = strings.Split(gitVersion, ".")
-		gitMajor string
-		gitMinor string
+		versionParts = strings.Split(gitVersion, ".")
+		gitMajor     string
+		gitMinor     string
 	)
 
-	if len(version) >= 2 {
-		gitMajor = version[0]
-		gitMinor = version[1]
+	if len(versionParts) >= 2 {
+		gitMajor = strings.TrimPrefix(versionParts[0], "v")
+		gitMinor = versionParts[1]
 	}
 
-	return apimachineryversion.Info{
+	version = &apimachineryversion.Info{
 		Major:        gitMajor,
 		Minor:        gitMinor,
 		GitVersion:   gitVersion,
