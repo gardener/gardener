8574804d47d68370b82d59a7944d09199f0acc73
Tim Ebert <tim.ebert@sap.com>
2020-08-10 13:41:51 +0200

    Don't execute shoot health checks simultaneously
    ```improvement operator
    The gardenlet now spreads Shoot health checks to avoid running into rate limits directly after startup.
    ```

--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -15,8 +15,12 @@
 package utils
 
 import (
-	"crypto/rand"
+	cryptorand "crypto/rand"
 	"math/big"
+	mathrand "math/rand"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // GenerateRandomString uses crypto/rand to generate a random string of the specified length <n>.
@@ -33,11 +37,29 @@ func GenerateRandomStringFromCharset(n int, allowedCharacters string) (string, e
 	output := make([]byte, n)
 	max := new(big.Int).SetInt64(int64(len(allowedCharacters)))
 	for i := range output {
-		randomCharacter, err := rand.Int(rand.Reader, max)
+		randomCharacter, err := cryptorand.Int(cryptorand.Reader, max)
 		if err != nil {
 			return "", err
 		}
 		output[i] = allowedCharacters[randomCharacter.Int64()]
 	}
 	return string(output), nil
+}
+
+// RandomDuration takes a time.Duration and computes a non-negative pseudo-random duration in [0,max).
+// It returns 0ns if max is <= 0ns.
+func RandomDuration(max time.Duration) time.Duration {
+	if max.Nanoseconds() <= 0 {
+		return time.Duration(0)
+	}
+	return time.Duration(mathrand.Int63n(max.Nanoseconds()))
+}
+
+// RandomDurationWithMetaDuration takes a *metav1.Duration and computes a non-negative pseudo-random duration in [0,max).
+// It returns 0ns if max is nil or <= 0ns.
+func RandomDurationWithMetaDuration(max *metav1.Duration) time.Duration {
+	if max == nil {
+		return time.Duration(0)
+	}
+	return RandomDuration(max.Duration)
 }
