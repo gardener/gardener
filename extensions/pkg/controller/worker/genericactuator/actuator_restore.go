2eb7fb5db97d7ff63d0aaf6f221de80c2d0d87f1
Tim Ebert <tim.ebert@sap.com>
2020-09-01 14:28:03 +0200

    Improve worker controller/actuator logs

--- extensions/pkg/controller/worker/genericactuator/actuator_restore.go
+++ extensions/pkg/controller/worker/genericactuator/actuator_restore.go
@@ -17,31 +17,36 @@ package genericactuator
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"time"
 
-	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
-	workercontroller "github.com/gardener/gardener/extensions/pkg/controller/worker"
-	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	gardeneretry "github.com/gardener/gardener/pkg/utils/retry"
 	machinev1alpha1 "github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
+	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
+	workercontroller "github.com/gardener/gardener/extensions/pkg/controller/worker"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
+	gardeneretry "github.com/gardener/gardener/pkg/utils/retry"
 )
 
 // Restore uses the Worker's spec to figure out the wanted MachineDeployments. Then it parses the Worker's state.
 // If there is a record in the state corresponding to a wanted deployment then the Restore function
 // deploys that MachineDeployment with all related MachineSet and Machines.
 func (a *genericActuator) Restore(ctx context.Context, worker *extensionsv1alpha1.Worker, cluster *extensionscontroller.Cluster) error {
+	logger := a.logger.WithValues("worker", kutil.KeyFromObject(worker), "operation", "restore")
+
 	workerDelegate, err := a.delegateFactory.WorkerDelegate(ctx, worker, cluster)
 	if err != nil {
 		return errors.Wrap(err, "could not instantiate actuator context")
 	}
 
 	// Generate the desired machine deployments.
+	logger.Info("Generating machine deployments")
 	wantedMachineDeployments, err := workerDelegate.GenerateMachineDeployments(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to generate the machine deployments")
@@ -55,40 +60,36 @@ func (a *genericActuator) Restore(ctx context.Context, worker *extensionsv1alpha
 
 	// Parse the worker state to a separate machineDeployment states and attach them to
 	// the corresponding machineDeployments which are to be deployed later
-	a.logger.Info("Extracting the worker status.state", "worker", fmt.Sprintf("%s/%s", worker.Namespace, worker.Name))
-	if err := a.addStateToMachineDeployment(ctx, worker, wantedMachineDeployments); err != nil {
+	logger.Info("Extracting state from worker status")
+	if err := a.addStateToMachineDeployment(worker, wantedMachineDeployments); err != nil {
 		return err
 	}
 
 	wantedMachineDeployments = removeWantedDeploymentWithoutState(wantedMachineDeployments)
 
 	// Delete the machine-controller-manager. During restoration MCM must not exist
-	a.logger.Info("Deleting machine-controller-manager for a Worker restore operation during control-plane migration", "worker", fmt.Sprintf("%s/%s", worker.Namespace, worker.Name))
-	if err := a.deleteMachineControllerManager(ctx, worker); err != nil {
+	if err := a.deleteMachineControllerManager(ctx, logger, worker); err != nil {
 		return errors.Wrap(err, "failed deleting machine-controller-manager")
 	}
 
-	a.logger.Info("Wait until machine-controller-manager is deleted", "worker", fmt.Sprintf("%s/%s", worker.Namespace, worker.Name))
-	if err := a.waitUntilMachineControllerManagerIsDeleted(ctx, worker.Namespace); err != nil {
+	if err := a.waitUntilMachineControllerManagerIsDeleted(ctx, logger, worker.Namespace); err != nil {
 		return errors.Wrap(err, "failed deleting machine-controller-manager")
 	}
 
 	// Do the actual restoration
-	a.logger.Info("Deploying the Machines and MachineSets", "worker", fmt.Sprintf("%s/%s", worker.Namespace, worker.Name))
-	if err := a.deployMachineSetsAndMachines(ctx, worker, wantedMachineDeployments); err != nil {
+	if err := a.deployMachineSetsAndMachines(ctx, logger, wantedMachineDeployments); err != nil {
 		return errors.Wrap(err, "failed restoration of the machineSet and the machines")
 	}
 
 	// Generate machine deployment configuration based on previously computed list of deployments and deploy them.
-	a.logger.Info("Deploying the MachineDeployments in Worker.Status.State", "worker", fmt.Sprintf("%s/%s", worker.Namespace, worker.Name))
-	if err := a.deployMachineDeployments(ctx, cluster, worker, existingMachineDeployments, wantedMachineDeployments, workerDelegate.MachineClassKind(), true); err != nil {
+	if err := a.deployMachineDeployments(ctx, logger, cluster, worker, existingMachineDeployments, wantedMachineDeployments, workerDelegate.MachineClassKind(), true); err != nil {
 		return errors.Wrap(err, "failed to restore the machine deployment config")
 	}
 
 	return nil
 }
 
-func (a *genericActuator) addStateToMachineDeployment(ctx context.Context, worker *extensionsv1alpha1.Worker, wantedMachineDeployments workercontroller.MachineDeployments) error {
+func (a *genericActuator) addStateToMachineDeployment(worker *extensionsv1alpha1.Worker, wantedMachineDeployments workercontroller.MachineDeployments) error {
 	if worker.Status.State == nil || len(worker.Status.State.Raw) <= 0 {
 		return nil
 	}
@@ -110,7 +111,8 @@ func (a *genericActuator) addStateToMachineDeployment(ctx context.Context, worke
 	return nil
 }
 
-func (a *genericActuator) deployMachineSetsAndMachines(ctx context.Context, worker *extensionsv1alpha1.Worker, wantedMachineDeployments workercontroller.MachineDeployments) error {
+func (a *genericActuator) deployMachineSetsAndMachines(ctx context.Context, logger logr.Logger, wantedMachineDeployments workercontroller.MachineDeployments) error {
+	logger.Info("Deploying Machines and MachineSets")
 	for _, wantedMachineDeployment := range wantedMachineDeployments {
 		machineSets := wantedMachineDeployment.State.MachineSets
 
