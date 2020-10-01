480a3178c8ff98700fd7e336201a2abbe4630d75
Rafael Franzke <rafael.franzke@sap.com>
2020-10-01 16:29:34 +0200

    Add unit tests for flow progress reporters

---
+++ pkg/utils/flow/progress_reporter_delaying_test.go
@@ -0,0 +1,65 @@
+// Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package flow
+
+import (
+	"context"
+	"sync/atomic"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ProgressReporterDelaying", func() {
+	It("should behave correctly", func() {
+		var (
+			ctx           = context.TODO()
+			period        = 50 * time.Millisecond
+			reportedStats atomic.Value
+			reporterFn    = func(_ context.Context, stats *Stats) { reportedStats.Store(stats) }
+			p             = NewDelayingProgressReporter(reporterFn, period)
+		)
+
+		Expect(p.Start(ctx)).To(Succeed())
+		Expect(reportedStats.Load()).To(BeNil())
+
+		stats1 := &Stats{FlowName: "1"}
+		p.Report(ctx, stats1)
+		Expect(reportedStats.Load()).To(Equal(stats1))
+
+		stats2 := &Stats{FlowName: "2"}
+		p.Report(ctx, stats2)
+		Consistently(reportedStats.Load, period/2, time.Millisecond).Should(Equal(stats1))
+		Eventually(reportedStats.Load, period, period/5).Should(Equal(stats2))
+
+		stats3 := &Stats{FlowName: "3"}
+		p.Report(ctx, stats3)
+		Consistently(reportedStats.Load, period/2, time.Millisecond).Should(Equal(stats2))
+		Eventually(reportedStats.Load, period, period/5).Should(Equal(stats3))
+
+		stats4 := &Stats{FlowName: "4"}
+		p.Report(ctx, stats4)
+		stats5 := &Stats{FlowName: "5"}
+		p.Report(ctx, stats5)
+		Consistently(reportedStats.Load, period/2, time.Millisecond).Should(Equal(stats3))
+		Eventually(reportedStats.Load, period, period/5).Should(Equal(stats5))
+
+		stats6 := &Stats{FlowName: "6"}
+		p.Report(ctx, stats6)
+		p.Stop()
+		Expect(reportedStats.Load()).To(Equal(stats6))
+	})
+})
---
+++ pkg/utils/flow/progress_reporter_immediate_test.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package flow_test
+
+import (
+	"context"
+
+	. "github.com/gardener/gardener/pkg/utils/flow"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ProgressReporterImmediate", func() {
+	var ctx = context.TODO()
+
+	Describe("#Start", func() {
+		It("should do nothing", func() {
+			pr := NewImmediateProgressReporter(nil)
+			Expect(pr.Start(ctx)).To(Succeed())
+		})
+	})
+
+	Describe("#Report", func() {
+		It("should call the reporter function", func() {
+			var (
+				resultContext context.Context
+				resultStats   *Stats
+
+				stats = &Stats{FlowName: "foo"}
+				pr    = NewImmediateProgressReporter(func(ctx context.Context, stats *Stats) {
+					resultContext = ctx
+					resultStats = stats
+				})
+			)
+
+			pr.Report(ctx, stats)
+
+			Expect(resultContext).To(Equal(ctx))
+			Expect(resultStats).To(Equal(stats))
+		})
+	})
+})
