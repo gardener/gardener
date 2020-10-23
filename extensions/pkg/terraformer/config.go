a72f1408803731a85fb6911ad017331effab8577
Tim Ebert <tim.ebert@sap.com>
2020-10-23 15:10:13 +0200

    Cleanup terraformer library

--- extensions/pkg/terraformer/config.go
+++ extensions/pkg/terraformer/config.go
@@ -21,12 +21,13 @@ import (
 	"strings"
 	"time"
 
-	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
 )
 
 const (
