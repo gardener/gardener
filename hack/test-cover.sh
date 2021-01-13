2e66ff2d575a046122eafba406252c5fff579892
Daniel Poelzleithner <poelzleithner@23technologies.cloud>
2021-01-13 11:32:22 +0100

    Use portable bash path
    /bin/bash is not available everywhere, using the proper /usr/bin/env
    lookup method fixes problems on platforms like NixOS/freebsd/...

--- hack/test-cover.sh
+++ hack/test-cover.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
 #
