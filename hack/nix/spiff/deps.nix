f0ac53de7eceddd9e0101bd97375e98d22cbf51e
Daniel Poelzleithner <poelzleithner@23technologies.cloud>
2021-01-25 18:46:23 +0100

    Add spiff++ to nix development environment

---
+++ hack/nix/spiff/deps.nix
@@ -0,0 +1,246 @@
+# file generated from Gopkg.lock using dep2nix (https://github.com/nixcloud/dep2nix)
+[
+  {
+    goPackagePath  = "github.com/cloudfoundry-incubator/candiedyaml";
+    fetch = {
+      type = "git";
+      url = "https://github.com/cloudfoundry-incubator/candiedyaml";
+      rev =  "a41693b7b7afb422c7ecb1028458ab27da047bbb";
+      sha256 = "0b8wnrr38mdkr5r717rp5wb5bv4h7r7pkpzrmxycc89qazfw4lmz";
+    };
+  }
+  {
+    goPackagePath  = "github.com/fsnotify/fsnotify";
+    fetch = {
+      type = "git";
+      url = "https://github.com/fsnotify/fsnotify";
+      rev =  "45d7d09e39ef4ac08d493309fa031790c15bfe8a";
+      sha256 = "1i1r72knpbfwwql9frn9bqc3nhfc2ai5m6qllcyr6wban62lr40x";
+    };
+  }
+  {
+    goPackagePath  = "github.com/hashicorp/hcl";
+    fetch = {
+      type = "git";
+      url = "https://github.com/hashicorp/hcl";
+      rev =  "8cb6e5b959231cc1119e43259c4a608f9c51a241";
+      sha256 = "0q6ml0qqs0yil76mpn4mdx4lp94id8vbv575qm60jzl1ijcl5i66";
+    };
+  }
+  {
+    goPackagePath  = "github.com/inconshreveable/mousetrap";
+    fetch = {
+      type = "git";
+      url = "https://github.com/inconshreveable/mousetrap";
+      rev =  "76626ae9c91c4f2a10f34cad8ce83ea42c93bb75";
+      sha256 = "1mn0kg48xkd74brf48qf5hzp0bc6g8cf5a77w895rl3qnlpfw152";
+    };
+  }
+  {
+    goPackagePath  = "github.com/magiconair/properties";
+    fetch = {
+      type = "git";
+      url = "https://github.com/magiconair/properties";
+      rev =  "e55ec40311b90df900426eae806bb80a40154583";
+      sha256 = "1lgaybkf6fnn4bmgics79kgrfmc959jyf1rbsw5bb823igrrx063";
+    };
+  }
+  {
+    goPackagePath  = "github.com/mandelsoft/filepath";
+    fetch = {
+      type = "git";
+      url = "https://github.com/mandelsoft/filepath";
+      rev =  "3df73d378d552504d3df40fd7b6d8b00c12b4427";
+      sha256 = "1i18xvhhxxw4p0x5l76rh7yqagg275241ah51akp7kfghp5nig6a";
+    };
+  }
+  {
+    goPackagePath  = "github.com/mandelsoft/vfs";
+    fetch = {
+      type = "git";
+      url = "https://github.com/mandelsoft/vfs";
+      rev =  "d03d33d5889a612bddefccdd689094cc517b374a";
+      sha256 = "054vphqffv6a09s4rp08b9mln0p2f4f295ial1hj39bqb57r10jz";
+    };
+  }
+  {
+    goPackagePath  = "github.com/mitchellh/mapstructure";
+    fetch = {
+      type = "git";
+      url = "https://github.com/mitchellh/mapstructure";
+      rev =  "9e1e4717f8567d7ead72d070d064ad17d444a67e";
+      sha256 = "1pzyvzbpd9vs4qvj2hd141hk6ppw9lxygw7sy5770raa9rml1ynl";
+    };
+  }
+  {
+    goPackagePath  = "github.com/nxadm/tail";
+    fetch = {
+      type = "git";
+      url = "https://github.com/nxadm/tail";
+      rev =  "327c577245448d8192115e77a76ea3d6aee88202";
+      sha256 = "1czmwqii0fc8v3ar5bwqpfanprkxg19waly405zlvcflzvj53wwc";
+    };
+  }
+  {
+    goPackagePath  = "github.com/onsi/ginkgo";
+    fetch = {
+      type = "git";
+      url = "https://github.com/onsi/ginkgo";
+      rev =  "6d83527acb3f6cda405ce47a475d480117098381";
+      sha256 = "01nn33r1rg210zv0qmck0b16545gzr057w1kz8ca86l64qrwbcxx";
+    };
+  }
+  {
+    goPackagePath  = "github.com/onsi/gomega";
+    fetch = {
+      type = "git";
+      url = "https://github.com/onsi/gomega";
+      rev =  "d7eb503b14592ef8cefacd3c9589398c7decaf23";
+      sha256 = "1iabsrgcw51v58ymhig4jgavpdb83v0igsjnhmd65qcafnal95vj";
+    };
+  }
+  {
+    goPackagePath  = "github.com/pelletier/go-toml";
+    fetch = {
+      type = "git";
+      url = "https://github.com/pelletier/go-toml";
+      rev =  "65ca8064882c8c308e5c804c5d5443d409e0738c";
+      sha256 = "1pi1r9ds0vxjza4qrbk52y98wxrzh1ghwzc9c2v1w6i02pdwdcz9";
+    };
+  }
+  {
+    goPackagePath  = "github.com/spf13/afero";
+    fetch = {
+      type = "git";
+      url = "https://github.com/spf13/afero";
+      rev =  "a4ea980f2d023f22c7eede4c142190115449de5b";
+      sha256 = "13iskc76qrg3zx4ww4rssv4ccgb75lzpvsn23iq2dc5sdks6khw5";
+    };
+  }
+  {
+    goPackagePath  = "github.com/spf13/cast";
+    fetch = {
+      type = "git";
+      url = "https://github.com/spf13/cast";
+      rev =  "1ffadf551085444af981432dd0f6d1160c11ec64";
+      sha256 = "0lb84788glr0qzrq2ifi36rgvp96qrgywvxrr3ggq5hrbr38hgn1";
+    };
+  }
+  {
+    goPackagePath  = "github.com/spf13/cobra";
+    fetch = {
+      type = "git";
+      url = "https://github.com/spf13/cobra";
+      rev =  "6607e6b8603f56adb027298ee6695e06ffb3a819";
+      sha256 = "0qkipxlhhs84ydpyich9x01ibhaa19zpkf1ywqlwk5ny16fqk97h";
+    };
+  }
+  {
+    goPackagePath  = "github.com/spf13/jwalterweatherman";
+    fetch = {
+      type = "git";
+      url = "https://github.com/spf13/jwalterweatherman";
+      rev =  "94f6ae3ed3bceceafa716478c5fbf8d29ca601a1";
+      sha256 = "1ywmkwci5zyd88ijym6f30fj5c0k2yayxarkmnazf5ybljv50q7b";
+    };
+  }
+  {
+    goPackagePath  = "github.com/spf13/pflag";
+    fetch = {
+      type = "git";
+      url = "https://github.com/spf13/pflag";
+      rev =  "2e9d26c8c37aae03e3f9d4e90b7116f5accb7cab";
+      sha256 = "0gpmacngd0gpslnbkzi263f5ishigzgh6pbdv9hp092rnjl4nd31";
+    };
+  }
+  {
+    goPackagePath  = "github.com/spf13/viper";
+    fetch = {
+      type = "git";
+      url = "https://github.com/spf13/viper";
+      rev =  "3826be313591f83193f048520482a7b3cf17d506";
+      sha256 = "1bci9wcqj8ghczwnj7wdlqzzbkm24fxpcl580vnrfc67yr5ppi01";
+    };
+  }
+  {
+    goPackagePath  = "github.com/subosito/gotenv";
+    fetch = {
+      type = "git";
+      url = "https://github.com/subosito/gotenv";
+      rev =  "2ef7124db659d49edac6aa459693a15ae36c671a";
+      sha256 = "0mav91j7r4arjkpq5zcf9j74f6pww8ic53x43wy7kg3ibw31yjs5";
+    };
+  }
+  {
+    goPackagePath  = "golang.org/x/crypto";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/crypto";
+      rev =  "5c72a883971a4325f8c62bf07b6d38c20ea47a6a";
+      sha256 = "1cimmqpajys001x6yq8ycklc4w34y7iwrksv7ayv7m7fgzhcjn3d";
+    };
+  }
+  {
+    goPackagePath  = "golang.org/x/net";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/net";
+      rev =  "62affa334b73ec65ed44a326519ac12c421905e3";
+      sha256 = "0v88xr36220wawwck914f90n9zvvc6lcx33ak3iplkwq0xkgw5fr";
+    };
+  }
+  {
+    goPackagePath  = "golang.org/x/sys";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/sys";
+      rev =  "288bc346aa3906399979ee2fc63bacf4c43e04c9";
+      sha256 = "1h8gl4gin9q99c5by3rz2i2hb865h1fqnx8cwkq9mb2wdk0mjv4b";
+    };
+  }
+  {
+    goPackagePath  = "golang.org/x/text";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/text";
+      rev =  "23ae387dee1f90d29a23c0e87ee0b46038fbed0e";
+      sha256 = "19pihqm3phyndmiw6i42pdv6z1rbvlqlsnhsyqf9gsnn0qnmqqlh";
+    };
+  }
+  {
+    goPackagePath  = "golang.org/x/xerrors";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/xerrors";
+      rev =  "5ec99f83aff198f5fbd629d6c8d8eb38a04218ca";
+      sha256 = "1dbzc3gmf2haazpv7cgmv97rq40g2xzwbglc17vas8dwhgwgwrzb";
+    };
+  }
+  {
+    goPackagePath  = "gopkg.in/ini.v1";
+    fetch = {
+      type = "git";
+      url = "https://github.com/go-ini/ini";
+      rev =  "fcd0515f91612282aba5f5231a7dd71487e6dd8f";
+      sha256 = "0lapmfijgjig8aral9g77agmx1vjqx5sa10mcdq5q9j4rirdl1va";
+    };
+  }
+  {
+    goPackagePath  = "gopkg.in/tomb.v1";
+    fetch = {
+      type = "git";
+      url = "https://github.com/go-tomb/tomb";
+      rev =  "dd632973f1e7218eb1089048e0798ec9ae7dceb8";
+      sha256 = "1lqmq1ag7s4b3gc3ddvr792c5xb5k6sfn0cchr3i2s7f1c231zjv";
+    };
+  }
+  {
+    goPackagePath  = "gopkg.in/yaml.v2";
+    fetch = {
+      type = "git";
+      url = "https://github.com/go-yaml/yaml";
+      rev =  "0b1645d91e851e735d3e23330303ce81f70adbe3";
+      sha256 = "1md0hlyd9s6myv3663i9l59y74n4xjazifmmyxn43g86fgkc5lzj";
+    };
+  }
+]
--- shell.nix
+++ shell.nix
@@ -1,21 +1,48 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    go
-    protobuf
-    docker
-    screen
-    git
-    kubernetes-helm
-    openvpn
-    coreutils
-    gnused
-    kubectl
-    iproute
-    minikube
-    yaml2json
-    gnumake
-    kops
-    awscli
-  ];
+let
+  inherit (pkgs) lib buildGoPackage fetchFromGitHub;
+
+  spiff = buildGoPackage rec {
+    pname = "minio-exporter";
+    version = "1.6.0-beta-1";
+    rev = "v${version}";
+
+    goPackagePath = "github.com/mandelsoft/spiff";
+    src = fetchFromGitHub {
+      inherit rev;
+      owner = "mandelsoft";
+      repo = "spiff";
+      sha256 = "1jrdjcywlqfracr4w26yz1hxqjbdpqawalpaf2jallsmdbbcsqi3";
+    };
+
+    goDeps = ./hack/nix/spiff/deps.nix;
+
+    meta = with lib; {
+      description = "In-domain YAML templating engine spiff++";
+      homepage = "https://github.com/mandelsoft/spiffr";
+      license = licenses.asl20;
+      platforms = platforms.unix;
+    };
+  };
+
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs;
+    [
+      go
+      protobuf
+      docker
+      screen
+      git
+      kubernetes-helm
+      openvpn
+      coreutils
+      gnused
+      kubectl
+      iproute
+      minikube
+      yaml2json
+      gnumake
+      kops
+      awscli
+    ] ++ [ spiff ];
 }
