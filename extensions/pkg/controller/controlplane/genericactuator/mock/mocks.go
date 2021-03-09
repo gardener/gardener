e23f221891e9ef45d09e57f553a02e7191f210ed
Ismail Alidzhikov <i.alidjikov@gmail.com>
2021-03-09 14:09:11 +0200

    Use separate ManagedResource for ControlPlane CRDs that are installed in the Shoot cluster (#3562)
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>

--- extensions/pkg/controller/controlplane/genericactuator/mock/mocks.go
+++ extensions/pkg/controller/controlplane/genericactuator/mock/mocks.go
@@ -81,6 +81,21 @@ func (mr *MockValuesProviderMockRecorder) GetControlPlaneExposureChartValues(arg
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetControlPlaneExposureChartValues", reflect.TypeOf((*MockValuesProvider)(nil).GetControlPlaneExposureChartValues), arg0, arg1, arg2, arg3)
 }
 
+// GetControlPlaneShootCRDsChartValues mocks base method.
+func (m *MockValuesProvider) GetControlPlaneShootCRDsChartValues(arg0 context.Context, arg1 *v1alpha1.ControlPlane, arg2 *extensions.Cluster) (map[string]interface{}, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetControlPlaneShootCRDsChartValues", arg0, arg1, arg2)
+	ret0, _ := ret[0].(map[string]interface{})
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetControlPlaneShootCRDsChartValues indicates an expected call of GetControlPlaneShootCRDsChartValues.
+func (mr *MockValuesProviderMockRecorder) GetControlPlaneShootCRDsChartValues(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetControlPlaneShootCRDsChartValues", reflect.TypeOf((*MockValuesProvider)(nil).GetControlPlaneShootCRDsChartValues), arg0, arg1, arg2)
+}
+
 // GetControlPlaneShootChartValues mocks base method.
 func (m *MockValuesProvider) GetControlPlaneShootChartValues(arg0 context.Context, arg1 *v1alpha1.ControlPlane, arg2 *extensions.Cluster, arg3 map[string]string) (map[string]interface{}, error) {
 	m.ctrl.T.Helper()
