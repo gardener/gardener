efcc426c3f6aaf21a5b5ba8467d3242325a56e64
Rafael Franzke <rafael.franzke@sap.com>
2021-03-30 08:35:04 +0200

    Update goimports if necessary

--- hack/install-requirements.sh
+++ hack/install-requirements.sh
@@ -18,7 +18,7 @@ set -e
 
 echo "> Installing requirements"
 
-GO111MODULE=off go get golang.org/x/tools/cmd/goimports
+GO111MODULE=off go get -u golang.org/x/tools/cmd/goimports
 
 export GO111MODULE=on
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
