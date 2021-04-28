fd52b16e57e919ee21c3f8c5c3c2797b085e1d2b
Rafael Franzke <rafael.franzke@sap.com>
2021-04-28 15:46:50 +0200

    Translate 'kube-apiserver-sni/templates/*-virtualservice.yaml' into Golang code

--- pkg/operation/botanist/component/kubeapiserverexposure/kube_apiserver_sni.go
+++ pkg/operation/botanist/component/kubeapiserverexposure/kube_apiserver_sni.go
@@ -16,95 +16,124 @@ package kubeapiserverexposure
 
 import (
 	"context"
-	"path/filepath"
+	"fmt"
 
-	"github.com/gardener/gardener/pkg/client/kubernetes"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/operation/botanist/component"
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
 
+	istioapinetworkingv1beta1 "istio.io/api/networking/v1beta1"
+	istionetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+const (
+	gatewayName = v1beta1constants.DeploymentNameKubeAPIServer
 )
 
 // SNIValues configure the kube-apiserver service SNI.
 type SNIValues struct {
-	Hosts                    []string            `json:"hosts,omitempty"`
-	Name                     string              `json:"name,omitempty"`
-	NamespaceUID             types.UID           `json:"namespaceUID,omitempty"`
-	ApiserverClusterIP       string              `json:"apiserverClusterIP,omitempty"`
-	IstioIngressGateway      IstioIngressGateway `json:"istioIngressGateway,omitempty"`
-	InternalDNSNameApiserver string              `json:"internalDNSNameApiserver,omitempty"`
-	ReversedVPN              ReversedVPN         `json:"reversedVPN,omitempty"`
+	Hosts                    []string
+	NamespaceUID             types.UID
+	ApiserverClusterIP       string
+	IstioIngressGateway      IstioIngressGateway
+	InternalDNSNameApiserver string
+	ReversedVPN              ReversedVPN
 }
 
 // IstioIngressGateway contains the values for istio ingress gateway configuration.
 type IstioIngressGateway struct {
-	Namespace string            `json:"namespace,omitempty"`
-	Labels    map[string]string `json:"labels,omitempty"`
+	Namespace string
+	Labels    map[string]string
 }
 
 // ReversedVPN contains whether the reversed vpn is enabled or not.
 type ReversedVPN struct {
-	Enabled bool `json:"enabled,omitempty"`
+	Enabled bool
 }
 
 // NewSNI creates a new instance of DeployWaiter which deploys Istio resources for
 // kube-apiserver SNI access.
 func NewSNI(
-	values *SNIValues,
+	client client.Client,
 	namespace string,
-	applier kubernetes.ChartApplier,
-	chartsRootPath string,
+	values *SNIValues,
 ) component.DeployWaiter {
 	if values == nil {
 		values = &SNIValues{}
 	}
 
 	return &sni{
-		ChartApplier: applier,
-		chartPath:    filepath.Join(chartsRootPath, "seed-controlplane", "charts", "kube-apiserver-sni"),
-		values:       values,
-		namespace:    namespace,
+		client:    client,
+		namespace: namespace,
+		values:    values,
 	}
 }
 
 type sni struct {
-	values    *SNIValues
+	client    client.Client
 	namespace string
-	kubernetes.ChartApplier
-	chartPath string
+	values    *SNIValues
 }
 
 func (s *sni) Deploy(ctx context.Context) error {
-	return s.Apply(
-		ctx,
-		s.chartPath,
-		s.namespace,
-		s.values.Name,
-		kubernetes.Values(s.values),
+	var (
+		virtualService = s.emptyVirtualService()
 	)
+
+	if _, err := controllerutil.CreateOrUpdate(ctx, s.client, virtualService, func() error {
+		virtualService.Labels = getLabels()
+		virtualService.Spec = istioapinetworkingv1beta1.VirtualService{
+			ExportTo: []string{"*"},
+			Hosts:    s.values.Hosts,
+			Gateways: []string{gatewayName},
+			Tls: []*istioapinetworkingv1beta1.TLSRoute{{
+				Match: []*istioapinetworkingv1beta1.TLSMatchAttributes{{
+					Port:     443,
+					SniHosts: s.values.Hosts,
+				}},
+				Route: []*istioapinetworkingv1beta1.RouteDestination{{
+					Destination: &istioapinetworkingv1beta1.Destination{
+						Host: fmt.Sprintf("%s.%s.svc.%s", v1beta1constants.DeploymentNameKubeAPIServer, s.namespace, gardencorev1beta1.DefaultDomain),
+						Port: &istioapinetworkingv1beta1.PortSelector{Number: 443},
+					},
+				}},
+			}},
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *sni) Destroy(ctx context.Context) error {
-	return s.Delete(
+	return kutil.DeleteObjects(
 		ctx,
-		s.chartPath,
-		s.namespace,
-		s.values.Name,
-		kubernetes.Values(s.values),
-		kubernetes.TolerateErrorFunc(meta.IsNoMatchError),
+		s.client,
+		s.emptyVirtualService(),
 	)
 }
 
-func (s *sni) Wait(ctx context.Context) error        { return nil }
-func (s *sni) WaitCleanup(ctx context.Context) error { return nil }
+func (s *sni) Wait(_ context.Context) error        { return nil }
+func (s *sni) WaitCleanup(_ context.Context) error { return nil }
+
+func (s *sni) emptyVirtualService() *istionetworkingv1beta1.VirtualService {
+	return &istionetworkingv1beta1.VirtualService{ObjectMeta: metav1.ObjectMeta{Name: v1beta1constants.DeploymentNameKubeAPIServer, Namespace: s.namespace}}
+}
 
 // AnyDeployedSNI returns true if any SNI is deployed in the cluster.
 func AnyDeployedSNI(ctx context.Context, c client.Client) (bool, error) {
 	l := &unstructured.UnstructuredList{
 		Object: map[string]interface{}{
-			"apiVersion": "networking.istio.io/v1beta1",
+			"apiVersion": istionetworkingv1beta1.SchemeGroupVersion.String(),
 			"kind":       "VirtualServiceList",
 		},
 	}
