ce5f98d66681d338af16d5ba2085cf9fcf5b2bcf
Rafael Franzke <rafael.franzke@sap.com>
2021-04-30 12:51:45 +0200

    Adapt unit tests

--- pkg/operation/botanist/component/kubeapiserverexposure/kube_apiserver_sni_test.go
+++ pkg/operation/botanist/component/kubeapiserverexposure/kube_apiserver_sni_test.go
@@ -16,189 +16,308 @@ package kubeapiserverexposure_test
 
 import (
 	"context"
-	"fmt"
+	"time"
 
-	cr "github.com/gardener/gardener/pkg/chartrenderer"
 	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/client/kubernetes/test"
 	mockclient "github.com/gardener/gardener/pkg/mock/controller-runtime/client"
 	"github.com/gardener/gardener/pkg/operation/botanist/component"
 	. "github.com/gardener/gardener/pkg/operation/botanist/component/kubeapiserverexposure"
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
+	. "github.com/gardener/gardener/pkg/utils/test/matchers"
 
+	protobuftypes "github.com/gogo/protobuf/types"
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	istioapinetworkingv1beta1 "istio.io/api/networking/v1beta1"
+	istionetworkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	istionetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/version"
+	fakediscovery "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/testing"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 var _ = Describe("#SNI", func() {
-	const (
-		deployNS   = "test-chart-namespace"
-		deployName = "test-deploy"
-	)
 	var (
-		ca               kubernetes.ChartApplier
-		ctx              context.Context
-		c                client.Client
+		ctx     context.Context
+		c       client.Client
+		applier kubernetes.Applier
+
 		defaultDepWaiter component.DeployWaiter
+		namespace        = "test-namespace"
+		namespaceUID     = types.UID("123456")
+		istioLabels      = map[string]string{"foo": "bar"}
+		istioNamespace   = "istio-foo"
+		hosts            = []string{"foo.bar"}
+		hostName         = "kube-apiserver." + namespace + ".svc.cluster.local"
+
+		expectedDestinationRule       *istionetworkingv1beta1.DestinationRule
+		expectedGateway               *istionetworkingv1beta1.Gateway
+		expectedVirtualService        *istionetworkingv1beta1.VirtualService
+		expectedEnvoyFilterObjectMeta metav1.ObjectMeta
 	)
 
 	BeforeEach(func() {
 		ctx = context.TODO()
+
 		s := runtime.NewScheme()
-		c = fake.NewFakeClientWithScheme(s)
+		Expect(istionetworkingv1beta1.AddToScheme(s)).To(Succeed())
+		Expect(istionetworkingv1alpha3.AddToScheme(s)).To(Succeed())
+		c = fake.NewClientBuilder().WithScheme(s).Build()
+
+		var err error
+		applier, err = test.NewTestApplier(c, &fakediscovery.FakeDiscovery{
+			Fake:               &testing.Fake{},
+			FakedServerVersion: &version.Info{GitVersion: "v1.21.0"},
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		expectedDestinationRule = &istionetworkingv1beta1.DestinationRule{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: istionetworkingv1beta1.SchemeGroupVersion.String(),
+				Kind:       "DestinationRule",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "kube-apiserver",
+				Namespace: namespace,
+				Labels: map[string]string{
+					"app":  "kubernetes",
+					"role": "apiserver",
+				},
+				ResourceVersion: "1",
+			},
+			Spec: istioapinetworkingv1beta1.DestinationRule{
+				ExportTo: []string{"*"},
+				Host:     hostName,
+				TrafficPolicy: &istioapinetworkingv1beta1.TrafficPolicy{
+					ConnectionPool: &istioapinetworkingv1beta1.ConnectionPoolSettings{
+						Tcp: &istioapinetworkingv1beta1.ConnectionPoolSettings_TCPSettings{
+							MaxConnections: 5000,
+							TcpKeepalive: &istioapinetworkingv1beta1.ConnectionPoolSettings_TCPSettings_TcpKeepalive{
+								Time:     protobuftypes.DurationProto(7200 * time.Second),
+								Interval: protobuftypes.DurationProto(75 * time.Second),
+							},
+						},
+					},
+					Tls: &istioapinetworkingv1beta1.ClientTLSSettings{
+						Mode: istioapinetworkingv1beta1.ClientTLSSettings_DISABLE,
+					},
+				},
+			},
+		}
+		expectedEnvoyFilterObjectMeta = metav1.ObjectMeta{
+			Name:      namespace,
+			Namespace: istioNamespace,
+			OwnerReferences: []metav1.OwnerReference{{
+				APIVersion:         "v1",
+				Kind:               "Namespace",
+				Name:               namespace,
+				UID:                namespaceUID,
+				BlockOwnerDeletion: pointer.BoolPtr(false),
+				Controller:         pointer.BoolPtr(false),
+			}},
+			ResourceVersion: "1",
+		}
+		expectedGateway = &istionetworkingv1beta1.Gateway{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: istionetworkingv1beta1.SchemeGroupVersion.String(),
+				Kind:       "Gateway",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "kube-apiserver",
+				Namespace: namespace,
+				Labels: map[string]string{
+					"app":  "kubernetes",
+					"role": "apiserver",
+				},
+				ResourceVersion: "1",
+			},
+			Spec: istioapinetworkingv1beta1.Gateway{
+				Selector: istioLabels,
+				Servers: []*istioapinetworkingv1beta1.Server{{
+					Hosts: hosts,
+					Port: &istioapinetworkingv1beta1.Port{
+						Number:   443,
+						Name:     "tls",
+						Protocol: "TLS",
+					},
+					Tls: &istioapinetworkingv1beta1.ServerTLSSettings{
+						Mode: istioapinetworkingv1beta1.ServerTLSSettings_PASSTHROUGH,
+					},
+				}},
+			},
+		}
+		expectedVirtualService = &istionetworkingv1beta1.VirtualService{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: istionetworkingv1beta1.SchemeGroupVersion.String(),
+				Kind:       "VirtualService",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "kube-apiserver",
+				Namespace: namespace,
+				Labels: map[string]string{
+					"app":  "kubernetes",
+					"role": "apiserver",
+				},
+				ResourceVersion: "1",
+			},
+			Spec: istioapinetworkingv1beta1.VirtualService{
+				ExportTo: []string{"*"},
+				Hosts:    hosts,
+				Gateways: []string{expectedGateway.Name},
+				Tls: []*istioapinetworkingv1beta1.TLSRoute{{
+					Match: []*istioapinetworkingv1beta1.TLSMatchAttributes{{
+						Port:     443,
+						SniHosts: hosts,
+					}},
+					Route: []*istioapinetworkingv1beta1.RouteDestination{{
+						Destination: &istioapinetworkingv1beta1.Destination{
+							Host: hostName,
+							Port: &istioapinetworkingv1beta1.PortSelector{Number: 443},
+						},
+					}},
+				}},
+			},
+		}
 	})
 
 	JustBeforeEach(func() {
-		ca = kubernetes.NewChartApplier(
-			cr.NewWithServerVersion(&version.Info{}),
-			kubernetes.NewApplier(c, meta.NewDefaultRESTMapper([]schema.GroupVersion{})),
-		)
-
-		defaultDepWaiter = NewSNI(&SNIValues{
-			Hosts:              []string{"foo.bar"},
+		defaultDepWaiter = NewSNI(c, applier, namespace, &SNIValues{
+			Hosts:              hosts,
 			APIServerClusterIP: "1.1.1.1",
 			IstioIngressGateway: IstioIngressGateway{
-				Namespace: "istio-foo",
-				Labels:    map[string]string{"foo": "bar"},
+				Namespace: istioNamespace,
+				Labels:    istioLabels,
 			},
-			Name:         deployName,
-			NamespaceUID: "123456",
-		}, deployNS, ca, chartsRoot())
+			NamespaceUID: namespaceUID,
+		})
 	})
 
-	It("deploys succeeds", func() {
-		Expect(defaultDepWaiter.Deploy(ctx)).ToNot(HaveOccurred())
+	Describe("#Deploy", func() {
+		It("succeeds", func() {
+			Expect(defaultDepWaiter.Deploy(ctx)).To(Succeed())
+
+			actualDestinationRule := &istionetworkingv1beta1.DestinationRule{}
+			Expect(c.Get(ctx, kutil.Key(expectedDestinationRule.Namespace, expectedDestinationRule.Name), actualDestinationRule)).To(Succeed())
+			Expect(actualDestinationRule).To(DeepEqual(expectedDestinationRule))
+
+			actualGateway := &istionetworkingv1beta1.Gateway{}
+			Expect(c.Get(ctx, kutil.Key(expectedGateway.Namespace, expectedGateway.Name), actualGateway)).To(Succeed())
+			Expect(actualGateway).To(DeepEqual(expectedGateway))
+
+			actualVirtualService := &istionetworkingv1beta1.VirtualService{}
+			Expect(c.Get(ctx, kutil.Key(expectedVirtualService.Namespace, expectedVirtualService.Name), actualVirtualService)).To(Succeed())
+			Expect(actualVirtualService).To(DeepEqual(expectedVirtualService))
+
+			actualEnvoyFilter := &istionetworkingv1alpha3.EnvoyFilter{}
+			Expect(c.Get(ctx, kutil.Key(expectedEnvoyFilterObjectMeta.Namespace, expectedEnvoyFilterObjectMeta.Name), actualEnvoyFilter)).To(Succeed())
+			// cannot validate the Spec as there is meaningful way to unmarshal the data into the Golang structure
+			Expect(actualEnvoyFilter.ObjectMeta).To(DeepEqual(expectedEnvoyFilterObjectMeta))
+		})
 	})
 
 	It("destroy succeeds", func() {
-		Expect(defaultDepWaiter.Deploy(ctx)).ToNot(HaveOccurred())
-		Expect(defaultDepWaiter.Destroy(ctx)).ToNot(HaveOccurred())
+		Expect(defaultDepWaiter.Deploy(ctx)).To(Succeed())
+
+		Expect(c.Get(ctx, kutil.Key(expectedDestinationRule.Namespace, expectedDestinationRule.Name), &istionetworkingv1beta1.DestinationRule{})).To(Succeed())
+		Expect(c.Get(ctx, kutil.Key(expectedGateway.Namespace, expectedGateway.Name), &istionetworkingv1beta1.Gateway{})).To(Succeed())
+		Expect(c.Get(ctx, kutil.Key(expectedVirtualService.Namespace, expectedVirtualService.Name), &istionetworkingv1beta1.VirtualService{})).To(Succeed())
+		Expect(c.Get(ctx, kutil.Key(expectedEnvoyFilterObjectMeta.Namespace, expectedEnvoyFilterObjectMeta.Name), &istionetworkingv1alpha3.EnvoyFilter{})).To(Succeed())
+
+		Expect(defaultDepWaiter.Destroy(ctx)).To(Succeed())
+
+		Expect(c.Get(ctx, kutil.Key(expectedDestinationRule.Namespace, expectedDestinationRule.Name), &istionetworkingv1beta1.DestinationRule{})).To(BeNotFoundError())
+		Expect(c.Get(ctx, kutil.Key(expectedGateway.Namespace, expectedGateway.Name), &istionetworkingv1beta1.Gateway{})).To(BeNotFoundError())
+		Expect(c.Get(ctx, kutil.Key(expectedVirtualService.Namespace, expectedVirtualService.Name), &istionetworkingv1beta1.VirtualService{})).To(BeNotFoundError())
+		Expect(c.Get(ctx, kutil.Key(expectedEnvoyFilterObjectMeta.Namespace, expectedEnvoyFilterObjectMeta.Name), &istionetworkingv1alpha3.EnvoyFilter{})).To(BeNotFoundError())
 	})
 
-	It("wait succeeds", func() {
-		Expect(defaultDepWaiter.Deploy(ctx)).ToNot(HaveOccurred())
-		Expect(defaultDepWaiter.Wait(ctx)).ToNot(HaveOccurred())
+	Describe("#Wait", func() {
+		It("should succeed because it's not implemented", func() {
+			Expect(defaultDepWaiter.Wait(ctx)).To(Succeed())
+		})
 	})
 
-	Context("destroy", func() {
-		Context("applier returns an error", func() {
+	Describe("#WaitCleanup", func() {
+		It("should succeed because it's not implemented", func() {
+			Expect(defaultDepWaiter.WaitCleanup(ctx)).To(Succeed())
+		})
+	})
+
+	Describe("#AnyDeployedSNI", func() {
+		var (
+			c        client.Client
+			createVS = func(name string, namespace string) *unstructured.Unstructured {
+				return &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "networking.istio.io/v1beta1",
+						"kind":       "VirtualService",
+						"metadata": map[string]interface{}{
+							"name":      name,
+							"namespace": namespace,
+						},
+					},
+				}
+			}
+		)
+
+		Context("CRD available", func() {
+			BeforeEach(func() {
+				s := runtime.NewScheme()
+				// TODO(mvladev): can't directly import the istio apis due to dependency issues.
+				s.AddKnownTypeWithName(schema.FromAPIVersionAndKind("networking.istio.io/v1beta1", "VirtualServiceList"), &unstructured.UnstructuredList{})
+				s.AddKnownTypeWithName(schema.FromAPIVersionAndKind("networking.istio.io/v1beta1", "VirtualService"), &unstructured.Unstructured{})
+				c = fake.NewClientBuilder().WithScheme(s).Build()
+			})
+
+			It("returns true when exists", func() {
+				Expect(c.Create(ctx, createVS("kube-apiserver", "test"))).NotTo(HaveOccurred())
+				any, err := AnyDeployedSNI(ctx, c)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(any).To(BeTrue())
+			})
+
+			It("returns false when does not exists", func() {
+				any, err := AnyDeployedSNI(ctx, c)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(any).To(BeFalse())
+			})
+		})
+
+		Context("CRD not available", func() {
 			var (
-				ctrl *gomock.Controller
-				mc   *mockclient.MockClient
+				ctrl   *gomock.Controller
+				client *mockclient.MockClient
 			)
 
 			BeforeEach(func() {
 				ctrl = gomock.NewController(GinkgoT())
-				mc = mockclient.NewMockClient(ctrl)
-				c = mc
+				client = mockclient.NewMockClient(ctrl)
 			})
 
 			AfterEach(func() {
 				ctrl.Finish()
 			})
 
-			It("destroy succeeds when returning no match error", func() {
-				mc.EXPECT().Delete(gomock.Any(), gomock.Any()).AnyTimes().Return(
-					&meta.NoResourceMatchError{PartialResource: schema.GroupVersionResource{}},
-				)
-				Expect(defaultDepWaiter.Destroy(ctx)).ToNot(HaveOccurred())
+			It("returns false", func() {
+				client.EXPECT().List(ctx, gomock.AssignableToTypeOf(&unstructured.UnstructuredList{}), gomock.Any()).Return(&meta.NoKindMatchError{})
+				any, err := AnyDeployedSNI(ctx, client)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(any).To(BeFalse())
 			})
-
-			It("destroy succeeds when returning not found error", func() {
-				mc.EXPECT().Delete(gomock.Any(), gomock.Any()).AnyTimes().Return(
-					apierrors.NewNotFound(schema.GroupResource{}, "foo"),
-				)
-				Expect(defaultDepWaiter.Destroy(ctx)).ToNot(HaveOccurred())
-			})
-
-			It("destroy fails when returning internal server error", func() {
-				mc.EXPECT().Delete(gomock.Any(), gomock.Any()).AnyTimes().Return(
-					apierrors.NewInternalError(fmt.Errorf("bad")),
-				)
-				Expect(defaultDepWaiter.Destroy(ctx)).To(HaveOccurred())
-			})
-		})
-
-		It("destroy succeeds", func() {
-			Expect(defaultDepWaiter.Deploy(ctx)).ToNot(HaveOccurred())
-			Expect(defaultDepWaiter.Destroy(ctx)).ToNot(HaveOccurred())
-			Expect(defaultDepWaiter.WaitCleanup(ctx)).ToNot(HaveOccurred())
-		})
-	})
-})
-
-var _ = Describe("#AnyDeployedSNI", func() {
-	var (
-		ctx      context.Context
-		c        client.Client
-		createVS = func(name string, namespace string) *unstructured.Unstructured {
-			return &unstructured.Unstructured{
-				Object: map[string]interface{}{
-					"apiVersion": "networking.istio.io/v1beta1",
-					"kind":       "VirtualService",
-					"metadata": map[string]interface{}{
-						"name":      name,
-						"namespace": namespace,
-					},
-				},
-			}
-		}
-	)
-
-	Context("CRD available", func() {
-		BeforeEach(func() {
-			ctx = context.TODO()
-			s := runtime.NewScheme()
-			// TODO(mvladev): can't directly import the istio apis due to dependency issues.
-			s.AddKnownTypeWithName(schema.FromAPIVersionAndKind("networking.istio.io/v1beta1", "VirtualServiceList"), &unstructured.UnstructuredList{})
-			s.AddKnownTypeWithName(schema.FromAPIVersionAndKind("networking.istio.io/v1beta1", "VirtualService"), &unstructured.Unstructured{})
-			c = fake.NewFakeClientWithScheme(s)
-		})
-
-		It("returns true when exists", func() {
-			Expect(c.Create(ctx, createVS("kube-apiserver", "test"))).NotTo(HaveOccurred())
-			any, err := AnyDeployedSNI(ctx, c)
-
-			Expect(err).NotTo(HaveOccurred())
-			Expect(any).To(BeTrue())
-		})
-
-		It("returns false when does not exists", func() {
-			any, err := AnyDeployedSNI(ctx, c)
-
-			Expect(err).NotTo(HaveOccurred())
-			Expect(any).To(BeFalse())
-		})
-	})
-
-	Context("CRD not available", func() {
-		var (
-			ctrl   *gomock.Controller
-			client *mockclient.MockClient
-		)
-
-		BeforeEach(func() {
-			ctrl = gomock.NewController(GinkgoT())
-			client = mockclient.NewMockClient(ctrl)
-		})
-
-		AfterEach(func() {
-			ctrl.Finish()
-		})
-
-		It("returns false", func() {
-			client.EXPECT().List(ctx, gomock.AssignableToTypeOf(&unstructured.UnstructuredList{}), gomock.Any()).Return(&meta.NoKindMatchError{})
-			any, err := AnyDeployedSNI(ctx, client)
-
-			Expect(err).NotTo(HaveOccurred())
-			Expect(any).To(BeFalse())
 		})
 	})
 })
