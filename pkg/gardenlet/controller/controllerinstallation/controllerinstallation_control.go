54c4009b1e21565a1e3cf8e613a4a81dabddd9aa
Rafael Franzke <rafael.franzke@sap.com>
2021-05-11 11:20:04 +0200

    Reintroduce `gardener.garden.identity` to Helm chart values for ControllerInstallations (#4021)
    * Revert "Remove deprecated gardenNamespace UID from ControllerInstallation values"
    
    This reverts commit 8df592702e7ffcc6153205020ea1e1f81bb60b63.
    
    * Enhance SeedAuthorizer to allow gardenlet reading garden NS

--- pkg/gardenlet/controller/controllerinstallation/controllerinstallation_control.go
+++ pkg/gardenlet/controller/controllerinstallation/controllerinstallation_control.go
@@ -115,8 +115,8 @@ type ControlInterface interface {
 // NewDefaultControllerInstallationControl returns a new instance of the default implementation ControlInterface that
 // implements the documented semantics for ControllerInstallations. You should use an instance returned from
 // NewDefaultControllerInstallationControl() for any scenario other than testing.
-func NewDefaultControllerInstallationControl(clientMap clientmap.ClientMap, k8sGardenCoreInformers gardencoreinformers.SharedInformerFactory, recorder record.EventRecorder, config *config.GardenletConfiguration, seedLister gardencorelisters.SeedLister, controllerRegistrationLister gardencorelisters.ControllerRegistrationLister, controllerInstallationLister gardencorelisters.ControllerInstallationLister, gardenClusterIdentity string) ControlInterface {
-	return &defaultControllerInstallationControl{clientMap, k8sGardenCoreInformers, recorder, config, seedLister, controllerRegistrationLister, controllerInstallationLister, gardenClusterIdentity}
+func NewDefaultControllerInstallationControl(clientMap clientmap.ClientMap, k8sGardenCoreInformers gardencoreinformers.SharedInformerFactory, recorder record.EventRecorder, config *config.GardenletConfiguration, seedLister gardencorelisters.SeedLister, controllerRegistrationLister gardencorelisters.ControllerRegistrationLister, controllerInstallationLister gardencorelisters.ControllerInstallationLister, gardenNamespace *corev1.Namespace, gardenClusterIdentity string) ControlInterface {
+	return &defaultControllerInstallationControl{clientMap, k8sGardenCoreInformers, recorder, config, seedLister, controllerRegistrationLister, controllerInstallationLister, gardenNamespace, gardenClusterIdentity}
 }
 
 type defaultControllerInstallationControl struct {
@@ -127,6 +127,7 @@ type defaultControllerInstallationControl struct {
 	seedLister                   gardencorelisters.SeedLister
 	controllerRegistrationLister gardencorelisters.ControllerRegistrationLister
 	controllerInstallationLister gardencorelisters.ControllerInstallationLister
+	gardenNamespace              *corev1.Namespace
 	gardenClusterIdentity        string
 }
 
@@ -247,6 +248,7 @@ func (c *defaultControllerInstallationControl) reconcile(ctx context.Context, ga
 	gardenerValues := map[string]interface{}{
 		"gardener": map[string]interface{}{
 			"garden": map[string]interface{}{
+				"identity":        c.gardenNamespace.UID, // 'identity' value is deprecated to be replaced by 'clusterIdentity'. Should be removed in a future version.
 				"clusterIdentity": c.gardenClusterIdentity,
 			},
 			"seed": map[string]interface{}{
