9cabff70f516d830bdca6fecfc5162bec18d57cc
Tim Ebert <tim.ebert@sap.com>
2021-05-31 08:38:56 +0200

    Remove *PatchOrCreate funcs
    As described in #4057, *PatchOrCreate is easy to use incorrectly.
    Remove *PatchOrCreate funcs in order to minimize the risk of misuse.
    There might still be good use cases for this mechanism, though they
    are limited. If there is such a good use case, we can use it also
    without a helper func.

--- pkg/controllerutils/patch.go
+++ pkg/controllerutils/patch.go
@@ -22,54 +22,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
-// MergePatchOrCreate patches (using a merge patch) or creates the given object in the Kubernetes cluster.
-// The object's desired state is only reconciled with the existing state inside the passed in callback MutateFn,
-// however, the object is not read from the client. This means the object should already be filled with the
-// last-known state if operating on more complex structures (e.g. if the patch is supposed to remove an optional field
-// or section). If you don't have the current state of an object, use GetAndCreateOrMergePatch instead.
-//
-// The MutateFn is called regardless of creating or patching an object.
-//
-// It returns the executed operation and an error.
-func MergePatchOrCreate(ctx context.Context, c client.Writer, obj client.Object, f controllerutil.MutateFn) (controllerutil.OperationResult, error) {
-	return patchOrCreate(ctx, c, obj, func(obj client.Object) client.Patch { return client.MergeFrom(obj) }, f)
-}
-
-// StrategicMergePatchOrCreate patches (using a strategic merge patch) or creates the given object in the Kubernetes cluster.
-// The object's desired state is only reconciled with the existing state inside the passed in callback MutateFn,
-// however, the object is not read from the client. This means the object should already be filled with the
-// last-known state if operating on more complex structures (e.g. if the patch is supposed to remove an optional field
-// or section). If you don't have the current state of an object, use GetAndCreateOrStrategicMergePatch instead.
-//
-// The MutateFn is called regardless of creating or patching an object.
-//
-// It returns the executed operation and an error.
-func StrategicMergePatchOrCreate(ctx context.Context, c client.Writer, obj client.Object, f controllerutil.MutateFn) (controllerutil.OperationResult, error) {
-	return patchOrCreate(ctx, c, obj, func(obj client.Object) client.Patch { return client.StrategicMergeFrom(obj) }, f)
-}
-
-func patchOrCreate(ctx context.Context, c client.Writer, obj client.Object, patchFunc func(client.Object) client.Patch, f controllerutil.MutateFn) (controllerutil.OperationResult, error) {
-	patch := patchFunc(obj.DeepCopyObject().(client.Object))
-
-	if err := f(); err != nil {
-		return controllerutil.OperationResultNone, err
-	}
-
-	if err := c.Patch(ctx, obj, patch); err != nil {
-		if !apierrors.IsNotFound(err) {
-			return controllerutil.OperationResultNone, err
-		}
-
-		if err2 := c.Create(ctx, obj); err2 != nil {
-			return controllerutil.OperationResultNone, err2
-		}
-
-		return controllerutil.OperationResultCreated, nil
-	}
-
-	return controllerutil.OperationResultUpdated, nil
-}
-
 // GetAndCreateOrMergePatch is similar to controllerutil.CreateOrPatch, but does not care about the object's status section.
 // It reads the object from the client, reconciles the desired state with the existing state using the given MutateFn
 // and creates or patches the object (using a merge patch) accordingly.
