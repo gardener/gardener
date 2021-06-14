78a4a0e9fdc1449b1569a0b3ede1a8eee0f6c193
Tim Ebert <tim.ebert@sap.com>
2021-06-14 11:56:37 +0200

    Adapt to new setup-envtest tool
    Makes use of the new setup-envtest tool (kubernetes-sigs/controller-runtime#1488)
    in hack/setup-envtest.sh instead of vendoring hack/setup-envtest.sh and fetching
    binaries with that.

--- hack/setup-envtest.sh
+++ hack/setup-envtest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#
 #  Copyright 2020 The Kubernetes Authors.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,80 +17,17 @@
 set -o errexit
 set -o pipefail
 
-# Turn colors in this script off by setting the NO_COLOR variable in your
-# environment to any value:
-#
-# $ NO_COLOR=1 test.sh
-NO_COLOR=${NO_COLOR:-""}
-if [ -z "$NO_COLOR" ]; then
-  header=$'\e[1;33m'
-  reset=$'\e[0m'
+ENVTEST_K8S_VERSION=${ENVTEST_K8S_VERSION:-"1.20"}
+
+echo "> Installing envtest tools@${ENVTEST_K8S_VERSION} with setup-envtest if necessary"
+if ! command -v setup-envtest &> /dev/null ; then
+  # Some repos that vendor g/g and reuse this hack script might not need the envtest tools.
+  # Thus, just skip installing anything if setup-envtest is not installed.
+  # If envtest tools are needed, users will notice when their tests fail.
+  echo "setup-envtest not available, skip installing envtest tools"
 else
-  header=''
-  reset=''
+  # --use-env allows overwriting the envtest tools path via the KUBEBUILDER_ASSETS env var just like it was before
+  setup-envtest use --use-env -p env ${ENVTEST_K8S_VERSION}
+  source <(setup-envtest use --use-env -p env ${ENVTEST_K8S_VERSION})
+  echo "using envtest tools installed at '$KUBEBUILDER_ASSETS'"
 fi
-
-function header_text {
-  echo "$header$*$reset"
-}
-
-function setup_envtest_env {
-  header_text "setting up env vars"
-
-  # Setup env vars
-  KUBEBUILDER_ASSETS=${KUBEBUILDER_ASSETS:-""}
-  if [[ -z "${KUBEBUILDER_ASSETS}" ]]; then
-    export KUBEBUILDER_ASSETS=$1/bin
-  fi
-}
-
-# fetch k8s API gen tools and make it available under envtest_root_dir/bin.
-#
-# Skip fetching and untaring the tools by setting the SKIP_FETCH_TOOLS variable
-# in your environment to any value:
-#
-# $ SKIP_FETCH_TOOLS=1 ./check-everything.sh
-#
-# If you skip fetching tools, this script will use the tools already on your
-# machine.
-function fetch_envtest_tools {
-  SKIP_FETCH_TOOLS=${SKIP_FETCH_TOOLS:-""}
-  if [ -n "$SKIP_FETCH_TOOLS" ]; then
-    return 0
-  fi
-
-  tmp_root=/tmp
-  envtest_root_dir=$tmp_root/envtest
-
-  k8s_version="${ENVTEST_K8S_VERSION:-1.19.2}"
-  goarch="$(go env GOARCH)"
-  goos="$(go env GOOS)"
-
-  if [[ "$goos" != "linux" && "$goos" != "darwin" ]]; then
-    echo "OS '$goos' not supported. Aborting." >&2
-    return 1
-  fi
-
-  local dest_dir="${1}"
-
-  # use the pre-existing version in the temporary folder if it matches our k8s version
-  if [[ -x "${dest_dir}/bin/kube-apiserver" ]]; then
-    version=$("${dest_dir}"/bin/kube-apiserver --version)
-    if [[ $version == *"${k8s_version}"* ]]; then
-      header_text "Using cached envtest tools from ${dest_dir}"
-      return 0
-    fi
-  fi
-
-  header_text "fetching envtest tools@${k8s_version} (into '${dest_dir}')"
-  envtest_tools_archive_name="kubebuilder-tools-$k8s_version-$goos-$goarch.tar.gz"
-  envtest_tools_download_url="https://storage.googleapis.com/kubebuilder-tools/$envtest_tools_archive_name"
-
-  envtest_tools_archive_path="$tmp_root/$envtest_tools_archive_name"
-  if [ ! -f $envtest_tools_archive_path ]; then
-    curl -sL ${envtest_tools_download_url} -o "$envtest_tools_archive_path"
-  fi
-
-  mkdir -p "${dest_dir}"
-  tar -C "${dest_dir}" --strip-components=1 -zvxf "$envtest_tools_archive_path"
-}
