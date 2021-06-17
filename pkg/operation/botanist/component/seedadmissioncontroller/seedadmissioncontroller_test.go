d0f06c4a8ccf18e6f991b206445773105fe65e18
Tim Ebert <tim.ebert@sap.com>
2021-06-17 09:16:59 +0200

    Improve node list in seedadmission component

--- pkg/operation/botanist/component/seedadmissioncontroller/seedadmissioncontroller_test.go
+++ pkg/operation/botanist/component/seedadmissioncontroller/seedadmissioncontroller_test.go
@@ -373,7 +373,7 @@ status: {}
 	Describe("#Deploy", func() {
 		It("should fail because the managed resource secret cannot be updated", func() {
 			gomock.InOrder(
-				c.EXPECT().List(ctx, gomock.Any()).DoAndReturn(
+				c.EXPECT().List(ctx, gomock.Any(), client.Limit(3)).DoAndReturn(
 					func(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
 						Expect(list).To(BeAssignableToTypeOf(&metav1.PartialObjectMetadataList{}))
 						list.(*metav1.PartialObjectMetadataList).Items = make([]metav1.PartialObjectMetadata, 3)
@@ -388,7 +388,7 @@ status: {}
 
 		It("should fail because the managed resource cannot be updated", func() {
 			gomock.InOrder(
-				c.EXPECT().List(ctx, gomock.Any()).DoAndReturn(
+				c.EXPECT().List(ctx, gomock.Any(), client.Limit(3)).DoAndReturn(
 					func(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
 						Expect(list).To(BeAssignableToTypeOf(&metav1.PartialObjectMetadataList{}))
 						list.(*metav1.PartialObjectMetadataList).Items = make([]metav1.PartialObjectMetadata, 3)
@@ -405,7 +405,7 @@ status: {}
 
 		It("should successfully deploy all resources", func() {
 			gomock.InOrder(
-				c.EXPECT().List(ctx, gomock.Any()).DoAndReturn(
+				c.EXPECT().List(ctx, gomock.Any(), client.Limit(3)).DoAndReturn(
 					func(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
 						Expect(list).To(BeAssignableToTypeOf(&metav1.PartialObjectMetadataList{}))
 						list.(*metav1.PartialObjectMetadataList).Items = make([]metav1.PartialObjectMetadata, 3)
@@ -428,7 +428,7 @@ status: {}
 			managedResourceSecret.Data["deployment__shoot--foo--bar__gardener-seed-admission-controller.yaml"] = []byte(strings.Replace(deploymentYAML, "replicas: 3", "replicas: 1", -1))
 
 			gomock.InOrder(
-				c.EXPECT().List(ctx, gomock.Any()).DoAndReturn(
+				c.EXPECT().List(ctx, gomock.Any(), client.Limit(3)).DoAndReturn(
 					func(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
 						Expect(list).To(BeAssignableToTypeOf(&metav1.PartialObjectMetadataList{}))
 						list.(*metav1.PartialObjectMetadataList).Items = make([]metav1.PartialObjectMetadata, 1)
