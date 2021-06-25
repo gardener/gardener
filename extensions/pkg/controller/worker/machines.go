278e9b4e584404e14ec1b0d214987a19f4352eb0
Marco Voelz <voelzmo@users.noreply.github.com>
2021-06-25 16:48:05 +0200

    Enh/stable worker hash (#4237)
    * Exclude cri.name `docker` from worker hash calculation
    
    Worker pools with `CRI==nil` use `docker` as their container runtime. Changing to an explicit configuration of `docker` should not trigger a node rollout. Therefore, we need to keep the Worker hash stable by excluding the `cri.name` property from hash calculation in this case.
    
    Co-authored-by: Max Becker <max.becker@sap.com>
    
    * Merge two if-blocks into single 'and' condition
    
    Co-authored-by: Max Becker <max.becker@sap.com>

--- extensions/pkg/controller/worker/machines.go
+++ extensions/pkg/controller/worker/machines.go
@@ -22,6 +22,7 @@ import (
 
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	"github.com/gardener/gardener/extensions/pkg/util"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/utils"
 
@@ -145,7 +146,7 @@ func WorkerPoolHash(pool extensionsv1alpha1.WorkerPool, cluster *extensionscontr
 
 	for _, w := range cluster.Shoot.Spec.Provider.Workers {
 		if pool.Name == w.Name {
-			if w.CRI != nil {
+			if w.CRI != nil && w.CRI.Name != gardencorev1beta1.CRINameDocker {
 				data = append(data, string(w.CRI.Name))
 			}
 		}
