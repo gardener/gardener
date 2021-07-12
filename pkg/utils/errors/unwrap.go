9ca2279727491ae6bca45be57c43983d9bbc668f
Ivan V. Dimitrov <ivan.dimitrov02@sap.com>
2021-07-12 09:13:54 +0300

    Refactor `unwrap` method to be more simpler

--- pkg/utils/errors/unwrap.go
+++ pkg/utils/errors/unwrap.go
@@ -16,54 +16,31 @@ package errors
 
 import "errors"
 
-// Unwraps and returns the root error. Multiple wrappings either via `fmt.Errorf` or via `causer` implementations are properly taken into account.
-func Unwrap(err error) error {
-	var (
-		cdone      bool
-		res, udone = unwrapErr(err)
-	)
-
-	for !(udone && cdone) {
-		res, cdone = unwrapCauser(res)
-		res, udone = unwrapErr(res)
-	}
-
-	return res
-}
-
-func unwrapErr(err error) (error, bool) {
-	if err == nil || errors.Unwrap(err) == nil {
-		// this most likely is the root error
-		return err, true
-	}
-
-	for err != nil {
-		var e = errors.Unwrap(err)
-		if e == nil {
-			break
-		}
-		err = e
-	}
-	return err, false
-}
-
 type causer interface {
 	Cause() error
 }
 
-func unwrapCauser(err error) (error, bool) {
-	if _, ok := err.(causer); !ok {
-		// this most likely is the root cause error
-		return err, true
-	}
+// Unwraps and returns the root error. Multiple wrappings either via `fmt.Errorf` or via `causer` implementations are properly taken into account.
+func Unwrap(err error) error {
+	var cdone, udone bool
 
-	for err != nil {
-		cause, ok := err.(causer)
-		if !ok {
-			break
+	for !(udone && cdone) {
+		if err == nil || errors.Unwrap(err) == nil {
+			// this most likely is the root error
+			udone = true
+		} else {
+			err = errors.Unwrap(err)
+			udone = false
 		}
-		err = cause.Cause()
+
+		if cause, ok := err.(causer); !ok {
+			// this most likely is the root cause error
+			cdone = true
+		} else {
+			err = cause.Cause()
+			cdone = false
+		}
 	}
 
-	return err, false
+	return err
 }
