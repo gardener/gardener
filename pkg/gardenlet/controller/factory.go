7b1ff48f862cfe930f84271851eba52d4f2b9e64
Tim Ebert <tim.ebert@sap.com>
2021-08-12 15:17:56 +0200

    Drop unused informers in factory.go

--- pkg/gardenlet/controller/factory.go
+++ pkg/gardenlet/controller/factory.go
@@ -100,10 +100,8 @@ func NewGardenletControllerFactory(
 // Run starts all the controllers for the Garden API group. It also performs bootstrapping tasks.
 func (f *GardenletControllerFactory) Run(ctx context.Context) error {
 	var (
-		controllerRegistrationInformer = f.k8sGardenCoreInformers.Core().V1beta1().ControllerRegistrations().Informer()
-		controllerInstallationInformer = f.k8sGardenCoreInformers.Core().V1beta1().ControllerInstallations().Informer()
-		seedInformer                   = f.k8sGardenCoreInformers.Core().V1beta1().Seeds().Informer()
-		shootInformer                  = f.k8sGardenCoreInformers.Core().V1beta1().Shoots().Informer()
+		seedInformer  = f.k8sGardenCoreInformers.Core().V1beta1().Seeds().Informer()
+		shootInformer = f.k8sGardenCoreInformers.Core().V1beta1().Shoots().Informer()
 	)
 
 	k8sGardenClient, err := f.clientMap.GetClient(ctx, keys.ForGarden())
@@ -120,7 +118,7 @@ func (f *GardenletControllerFactory) Run(ctx context.Context) error {
 	}
 
 	f.k8sGardenCoreInformers.Start(ctx.Done())
-	if !cache.WaitForCacheSync(ctx.Done(), controllerRegistrationInformer.HasSynced, controllerInstallationInformer.HasSynced, seedInformer.HasSynced, shootInformer.HasSynced) {
+	if !cache.WaitForCacheSync(ctx.Done(), seedInformer.HasSynced, shootInformer.HasSynced) {
 		return fmt.Errorf("timed out waiting for Garden core caches to sync")
 	}
 
