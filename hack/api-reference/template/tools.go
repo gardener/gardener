f06a60305f6090548e58786c0d40d902e8cfc3d1
Ismail Alidzhikov <i.alidjikov@gmail.com>
2021-10-15 18:32:15 +0300

    Update to go1.17 (#4822)
    * Update the go version in the Dockerfile
    
    * Update the go version in the TestDefinitions
    
    * make generate
    
    * Format the ./hack dir

--- hack/api-reference/template/tools.go
+++ hack/api-reference/template/tools.go
@@ -1,3 +1,4 @@
+//go:build tools
 // +build tools
 
 // Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
