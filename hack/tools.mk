2f3dec7ae43b81b0fc909b6c00b862cd7add921f
Tim Ebert <tim.ebert@sap.com>
2021-10-26 17:39:42 +0200

    Make sure to reinstall tool if desired version changed

--- hack/tools.mk
+++ hack/tools.mk
@@ -16,7 +16,7 @@
 # It can be reused by repos vendoring g/g to have some common make recipes for building and installing development
 # tools as needed.
 # Recipes in the top-level make file should declare dependencies on the respective tool recipes (e.g. $(CONTROLLER_GEN))
-# as needed. If not built/installed yet, make will then make sure to build/install the required tools.
+# as needed. If the required tool (version) is not built/installed yet, make will make sure to build/install it.
 
 HACK_PKG_PATH              := $(shell go list -tags tools -f '{{ .Dir }}' github.com/gardener/gardener/hack)
 TOOLS_BIN_DIR              := $(TOOLS_DIR)/bin
@@ -37,6 +37,34 @@ YQ                         := $(TOOLS_BIN_DIR)/yq
 export TOOLS_BIN_DIR := $(TOOLS_BIN_DIR)
 export PATH := $(abspath $(TOOLS_BIN_DIR)):$(PATH)
 
+#########################################
+# Common                                #
+#########################################
+
+# Tool targets should declare go.mod as a prerequisite, if the tool's version is managed via go modules. This causes
+# make to rebuild the tool in the desired version, when go.mod is changed.
+# For tools where the version is not managed via go.mod, we use a file per tool and version as an indicator for make
+# whether we need to install the tool or a different version of the tool (make doesn't rerun the rule if the rule is
+# changed).
+
+# Use this "function" to add the version file as a prerequisite for the tool target: e.g.
+#   $(HELM): $(call tool_version_file,$(HELM),$(HELM_VERSION))
+tool_version_file = $(TOOLS_BIN_DIR)/.version_$(subst $(TOOLS_BIN_DIR)/,,$(1))_$(2)
+
+# This target cleans up any previous version files for the given tool and creates the given version file.
+# This way, we can generically determine, which version was installed without calling each and every binary explicitly.
+$(TOOLS_BIN_DIR)/.version_%:
+	@version_file=$@; rm -f $${version_file%_*}*
+	@touch $@
+
+.PHONY: clean-tools-bin
+clean-tools-bin:
+	rm -rf $(TOOLS_BIN_DIR)/*
+
+#########################################
+# Tools                                 #
+#########################################
+
 $(CONTROLLER_GEN): go.mod
 	go build -o $(CONTROLLER_GEN) sigs.k8s.io/controller-tools/cmd/controller-gen
 
@@ -46,14 +74,16 @@ $(GEN_CRD_API_REFERENCE_DOCS): go.mod
 $(GOIMPORTS): go.mod
 	go build -o $(GOIMPORTS) golang.org/x/tools/cmd/goimports
 
-$(GOLANGCI_LINT):
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(TOOLS_BIN_DIR) v1.42.1
+GOLANGCI_LINT_VERSION := v1.42.1
+$(GOLANGCI_LINT): $(call tool_version_file,$(GOLANGCI_LINT),$(GOLANGCI_LINT_VERSION))
+	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(TOOLS_BIN_DIR) $(GOLANGCI_LINT_VERSION)
 
 $(GO_TO_PROTOBUF): go.mod
 	go build -o $(GO_TO_PROTOBUF) k8s.io/code-generator/cmd/go-to-protobuf
 
-$(HELM):
-	curl -sSfL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | HELM_INSTALL_DIR=$(TOOLS_BIN_DIR) USE_SUDO=false bash -s -- --version 'v3.5.4'
+HELM_VERSION := v3.5.4
+$(HELM): $(call tool_version_file,$(HELM),$(HELM_VERSION))
+	curl -sSfL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | HELM_INSTALL_DIR=$(TOOLS_BIN_DIR) USE_SUDO=false bash -s -- --version $(HELM_VERSION)
 
 $(MOCKGEN): go.mod
 	go build -o $(MOCKGEN) github.com/golang/mock/mockgen
@@ -73,10 +103,7 @@ $(PROTOC_GEN_GOGO): go.mod
 $(YAML2JSON): go.mod
 	go build -o $(YAML2JSON) github.com/bronze1man/yaml2json
 
-$(YQ):
-	curl -L -o "$(YQ)" https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_$(shell uname -s | tr '[:upper:]' '[:lower:]')_$(shell uname -m | sed 's/x86_64/amd64/')
-	chmod +x "$(YQ)"
-
-.PHONY: clean-tools-bin
-clean-tools-bin:
-	rm -rf $(TOOLS_BIN_DIR)/*
+YQ_VERSION := v4.9.6
+$(YQ): $(call tool_version_file,$(YQ),$(YQ_VERSION))
+	curl -L -o $(YQ) https://github.com/mikefarah/yq/releases/download/$(YQ_VERSION)/yq_$(shell uname -s | tr '[:upper:]' '[:lower:]')_$(shell uname -m | sed 's/x86_64/amd64/')
+	chmod +x $(YQ)
