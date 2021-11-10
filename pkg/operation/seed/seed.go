8ac0ce059c1f7ca66b1f8e960ef580920e30d8b8
Johannes Scheerer <johannes.scheerer@sap.com>
2021-11-10 14:40:38 +0100

    Allow seed bootstrap to succeed even with feature gate managed istio is disabled.
    The external authorization server for the authorization of reversed vpn connections
    was always deployed regardless if managed istio was enabled. This caused issues
    when the custom resource definitions of istio were missing due to managed istio
    being disabled.
    
    Co-authored-by: Sebastian Stauch <sebastian.stauch@sap.com>

--- pkg/operation/seed/seed.go
+++ pkg/operation/seed/seed.go
@@ -1144,7 +1144,7 @@ func runCreateSeedFlow(
 		})
 		_ = g.Add(flow.Task{
 			Name:         "Deploying external authz server",
-			Fn:           extAuthzServer.Deploy,
+			Fn:           flow.TaskFn(extAuthzServer.Deploy).DoIf(gardenletfeatures.FeatureGate.Enabled(features.ManagedIstio)),
 			Dependencies: flow.NewTaskIDs(deployResourceManager),
 		})
 	)
