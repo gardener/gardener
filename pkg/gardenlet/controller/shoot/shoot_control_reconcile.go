62ebe6143541742687928061a9de54876e5a7b8d
Marco Voelz <voelzmo@users.noreply.github.com>
2021-11-11 09:55:45 +0100

    Update pkg/gardenlet/controller/shoot/shoot_control_reconcile.go
    Co-authored-by: Stoyan Rachev <stoyanr@gmail.com>

--- pkg/gardenlet/controller/shoot/shoot_control_reconcile.go
+++ pkg/gardenlet/controller/shoot/shoot_control_reconcile.go
@@ -459,8 +459,8 @@ func (r *shootReconciler) runReconcileShootFlow(ctx context.Context, o *operatio
 		_ = g.Add(flow.Task{
 			Name:         "Cleaning up orphaned DNSRecord secrets",
 			Fn:           flow.TaskFn(botanist.CleanupOrphanedDNSRecordSecrets),
-			Dependencies: flow.NewTaskIDs(deployInternalDomainDNSRecord, deployExternalDomainDNSRecord, deployOwnerDomainDNSRecord, deployIngressDomainDNSRecord)})
-
+			Dependencies: flow.NewTaskIDs(deployInternalDomainDNSRecord, deployExternalDomainDNSRecord, deployOwnerDomainDNSRecord, deployIngressDomainDNSRecord)
+		})
 		_ = g.Add(flow.Task{
 			Name:         "Deploying additional DNS providers",
 			Fn:           flow.TaskFn(botanist.DeployAdditionalDNSProviders).DoIf(!o.Shoot.HibernationEnabled),
