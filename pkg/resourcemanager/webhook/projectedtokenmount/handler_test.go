gardener/gardener

commit ca8e6a4d1bfa5da7b5a31debe04da14e32f0ef38

Address PR review feedback

pkg/resourcemanager/webhook/projectedtokenmount/handler.go
@@ -75,7 +75,8 @@ func (h *handler) Handle(ctx context.Context, req admission.Request) admission.R
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 
-	if serviceAccount.AutomountServiceAccountToken == nil || *serviceAccount.AutomountServiceAccountToken {
+	if serviceAccount.AutomountServiceAccountToken == nil || *serviceAccount.AutomountServiceAccountToken ||
+		pod.Spec.AutomountServiceAccountToken == nil || *pod.Spec.AutomountServiceAccountToken {
 		return admission.Allowed("auto-mounting service account token is not disabled")
 	}
 

pkg/resourcemanager/webhook/projectedtokenmount/handler_test.go
@@ -77,8 +77,9 @@ var _ = Describe("Handler", func() {
 				Namespace: namespace,
 			},
 			Spec: corev1.PodSpec{
-				ServiceAccountName: serviceAccountName,
-				Containers:         []corev1.Container{{}, {}},
+				AutomountServiceAccountToken: pointer.Bool(false),
+				ServiceAccountName:           serviceAccountName,
+				Containers:                   []corev1.Container{{}, {}},
 			},
 		}
 		serviceAccount = &corev1.ServiceAccount{
@@ -179,8 +180,8 @@ var _ = Describe("Handler", func() {
 		})
 
 		DescribeTable("should not mutate because service account's preconditions are not met",
-			func(mutateServiceAccount func()) {
-				mutateServiceAccount()
+			func(mutate func()) {
+				mutate()
 
 				Expect(fakeClient.Create(ctx, serviceAccount)).To(Succeed())
 
@@ -199,8 +200,20 @@ var _ = Describe("Handler", func() {
 				}))
 			},
 
-			Entry("automountServiceAccountToken=nil", func() { serviceAccount.AutomountServiceAccountToken = nil }),
-			Entry("automountServiceAccountToken=true", func() { serviceAccount.AutomountServiceAccountToken = pointer.Bool(true) }),
+			Entry("ServiceAccount's automountServiceAccountToken=nil", func() {
+				serviceAccount.AutomountServiceAccountToken = nil
+			}),
+			Entry("ServiceAccount's automountServiceAccountToken=true", func() {
+				serviceAccount.AutomountServiceAccountToken = pointer.Bool(true)
+			}),
+			Entry("Pod's automountServiceAccountToken=nil", func() {
+				serviceAccount.AutomountServiceAccountToken = pointer.Bool(false)
+				pod.Spec.AutomountServiceAccountToken = nil
+			}),
+			Entry("Pod's automountServiceAccountToken=true", func() {
+				serviceAccount.AutomountServiceAccountToken = pointer.Bool(false)
+				pod.Spec.AutomountServiceAccountToken = pointer.Bool(true)
+			}),
 		)
 
 		It("should not mutate because pod already has a projected token volume", func() {

commit b7738d253f5f3efe28ceb8464a64b79787bd675d

ProjectedTokenMount handler injects volume if Pod's `.spec.automountServiceAccountToken={nil,false}`
This is a bug that was introduced with https://github.com/gardener/gardener/pull/4873/commits/ca8e6a4d1bfa5da7b5a31debe04da14e32f0ef38 due to https://github.com/gardener/gardener/pull/4873#discussion_r739777448.

The desired behaviour is: If the `Pod`'s `.spec.serviceAccountName` is set and `ServiceAccount`s '.automountServiceAccounToken=false` and ...
- ... `Pod`s `.spec.automountServiceAccountToken=nil` -> inject
- ... `Pod`s `.spec.automountServiceAccountToken=true` -> inject
- ... `Pod`s `.spec.automountServiceAccountToken=false` -> do not inject

pkg/resourcemanager/webhook/projectedtokenmount/handler.go
@@ -77,9 +77,12 @@ func (h *handler) Handle(ctx context.Context, req admission.Request) admission.R
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 
-	if serviceAccount.AutomountServiceAccountToken == nil || *serviceAccount.AutomountServiceAccountToken ||
-		pod.Spec.AutomountServiceAccountToken == nil || *pod.Spec.AutomountServiceAccountToken {
-		return admission.Allowed("auto-mounting service account token is not disabled")
+	if serviceAccount.AutomountServiceAccountToken == nil || *serviceAccount.AutomountServiceAccountToken {
+		return admission.Allowed("auto-mounting service account token is not disabled on ServiceAccount level")
+	}
+
+	if pod.Spec.AutomountServiceAccountToken != nil && !*pod.Spec.AutomountServiceAccountToken {
+		return admission.Allowed("Pod explicitly disables a service account token mount")
 	}
 
 	for _, volume := range pod.Spec.Volumes {

pkg/resourcemanager/webhook/projectedtokenmount/handler_test.go
@@ -75,9 +75,8 @@ var _ = Describe("Handler", func() {
 		}
 		pod = &corev1.Pod{
 			Spec: corev1.PodSpec{
-				AutomountServiceAccountToken: pointer.Bool(false),
-				ServiceAccountName:           serviceAccountName,
-				Containers:                   []corev1.Container{{}, {}},
+				ServiceAccountName: serviceAccountName,
+				Containers:         []corev1.Container{{}, {}},
 			},
 		}
 		serviceAccount = &corev1.ServiceAccount{
@@ -191,7 +190,7 @@ var _ = Describe("Handler", func() {
 					AdmissionResponse: admissionv1.AdmissionResponse{
 						Allowed: true,
 						Result: &metav1.Status{
-							Reason: "auto-mounting service account token is not disabled",
+							Reason: "auto-mounting service account token is not disabled on ServiceAccount level",
 							Code:   http.StatusOK,
 						},
 					},
@@ -204,15 +203,27 @@ var _ = Describe("Handler", func() {
 			Entry("ServiceAccount's automountServiceAccountToken=true", func() {
 				serviceAccount.AutomountServiceAccountToken = pointer.Bool(true)
 			}),
-			Entry("Pod's automountServiceAccountToken=nil", func() {
-				serviceAccount.AutomountServiceAccountToken = pointer.Bool(false)
-				pod.Spec.AutomountServiceAccountToken = nil
-			}),
-			Entry("Pod's automountServiceAccountToken=true", func() {
-				serviceAccount.AutomountServiceAccountToken = pointer.Bool(false)
-				pod.Spec.AutomountServiceAccountToken = pointer.Bool(true)
-			}),
 		)
+
+		It("should not mutate because pod explicitly disables the service account mount", func() {
+			pod.Spec.AutomountServiceAccountToken = pointer.Bool(false)
+
+			Expect(fakeClient.Create(ctx, serviceAccount)).To(Succeed())
+
+			objData, err := runtime.Encode(encoder, pod)
+			Expect(err).NotTo(HaveOccurred())
+			request.Object.Raw = objData
+
+			Expect(handler.Handle(ctx, request)).To(Equal(admission.Response{
+				AdmissionResponse: admissionv1.AdmissionResponse{
+					Allowed: true,
+					Result: &metav1.Status{
+						Reason: "Pod explicitly disables a service account token mount",
+						Code:   http.StatusOK,
+					},
+				},
+			}))
+		})
 
 		It("should not mutate because pod already has a projected token volume", func() {
 			Expect(fakeClient.Create(ctx, serviceAccount)).To(Succeed())