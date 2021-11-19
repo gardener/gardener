82711eb26a52732658e0e37fce203c51123dc000
Rafael Franzke <rafael.franzke@sap.com>
2021-11-19 13:10:24 +0100

    Introduce annotation to allow skipping the infrastructure cleanup sleep step

--- pkg/apis/core/v1beta1/constants/types_constants.go
+++ pkg/apis/core/v1beta1/constants/types_constants.go
@@ -388,6 +388,11 @@ const (
 	// namespaces' step. Concretely, after the specified seconds, all the finalizers of the affected resources are
 	// forcefully removed.
 	AnnotationShootCleanupNamespaceResourcesFinalizeGracePeriodSeconds = "shoot.gardener.cloud/cleanup-namespaces-finalize-grace-period-seconds"
+	// AnnotationShootCleanupInfrastructureResourceGracePeriodSeconds is a key for an annotation on a Shoot
+	// resource that declares the grace period in seconds for waiting for infrastructure resources cleanup. Concretely,
+	// Gardener will wait for the specified time after the Infrastructure extension object has been deleted to allow
+	// controllers to gracefully cleanup everything (default behaviour is 300s).
+	AnnotationShootCleanupInfrastructureResourceGracePeriodSeconds = "shoot.gardener.cloud/cleanup-infrastructure-resources-grace-period-seconds"
 	// AnnotationReversedVPN moves the vpn-server to the seed.
 	AnnotationReversedVPN = "alpha.featuregates.shoot.gardener.cloud/reversed-vpn"
 	// AnnotationNodeLocalDNS enables a per node dns cache on the shoot cluster.
