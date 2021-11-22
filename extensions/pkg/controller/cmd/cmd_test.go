eb8409ed005a7699e364c3d8ab01d866ceeee591
Tim Ebert <tim.ebert@sap.com>
2021-11-22 14:49:38 +0100

    Drop logr mock
    Using gbytes.NewBuffer and gbytes.Say is nicer than mocks

--- extensions/pkg/controller/cmd/cmd_test.go
+++ extensions/pkg/controller/cmd/cmd_test.go
@@ -17,13 +17,14 @@ package cmd
 import (
 	"errors"
 
-	mocklogr "github.com/gardener/gardener/pkg/mock/go-logr/logr"
+	"github.com/onsi/gomega/gbytes"
+	logzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
+
 	"github.com/gardener/gardener/pkg/utils/test"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var _ = Describe("Cmd", func() {
@@ -41,24 +42,17 @@ var _ = Describe("Cmd", func() {
 	Describe("#LogErrAndExit", func() {
 		It("should log the error and exit", func() {
 			called := false
-
 			defer test.WithVar(&Exit, func(code int) {
 				called = true
 				Expect(code).To(Equal(1))
 			})()
 
-			defer test.WithVar(&Log, log.NewDelegatingLogger(log.NullLogger{}))()
+			logBuffer := gbytes.NewBuffer()
+			defer test.WithVar(&Log, logzap.New(logzap.JSONEncoder(), logzap.WriteTo(logBuffer)))()
 
-			logger := mocklogr.NewMockLogger(ctrl)
-			err := errors.New("error")
-			msg := "msg"
-
-			logger.EXPECT().Error(err, msg, []interface{}{})
-			Log.Fulfill(logger)
-
-			LogErrAndExit(err, msg)
-
+			LogErrAndExit(errors.New("error"), "msg")
 			Expect(called).To(BeTrue())
+			Eventually(logBuffer).Should(gbytes.Say(`"msg":"msg","error":"error"`))
 		})
 	})
 })
