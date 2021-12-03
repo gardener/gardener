9fa13aca5dccde3048ff731cd15a866a39dbd13b
Rafael Franzke <rafael.franzke@sap.com>
2021-12-03 15:42:19 +0100

    provider-local: Add `ControlPlane` controller
    From `docs/extensions/provider-local.md`:
    
    > This controller is not deploying anything since we haven't invested yet into a `cloud-controller-manager` or CSI solution.
    > For the latter, we could probably use the [local-path-provisioner](https://github.com/rancher/local-path-provisioner).
    
    Co-Authored-By: Tim Ebert <tim.ebert@sap.com>

---
+++ pkg/provider-local/controller/controlplane/add.go
@@ -0,0 +1,64 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controlplane
+
+import (
+	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
+	"github.com/gardener/gardener/extensions/pkg/controller/controlplane"
+	"github.com/gardener/gardener/extensions/pkg/controller/controlplane/genericactuator"
+	"github.com/gardener/gardener/extensions/pkg/util"
+	"github.com/gardener/gardener/pkg/provider-local/imagevector"
+	"github.com/gardener/gardener/pkg/provider-local/local"
+
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var (
+	logger = log.Log.WithName("local-controlplane-controller")
+
+	// DefaultAddOptions are the default AddOptions for AddToManager.
+	DefaultAddOptions = AddOptions{}
+)
+
+// AddOptions are options to apply when adding the local controlplane controller to the manager.
+type AddOptions struct {
+	// Controller are the controller.Options.
+	Controller controller.Options
+	// IgnoreOperationAnnotation specifies whether to ignore the operation annotation or not.
+	IgnoreOperationAnnotation bool
+	// ShootWebhooks specifies the list of desired Shoot MutatingWebhooks.
+	ShootWebhooks []admissionregistrationv1.MutatingWebhook
+}
+
+// AddToManagerWithOptions adds a controller with the given Options to the given manager.
+// The opts.Reconciler is being set with a newly instantiated actuator.
+func AddToManagerWithOptions(mgr manager.Manager, opts AddOptions) error {
+	return controlplane.Add(mgr, controlplane.AddArgs{
+		Actuator: genericactuator.NewActuator(local.Name, nil, nil, nil, nil, nil,
+			nil, nil, nil, NewValuesProvider(), extensionscontroller.ChartRendererFactoryFunc(util.NewChartRendererForShoot),
+			imagevector.ImageVector(), "", opts.ShootWebhooks, mgr.GetWebhookServer().Port, logger),
+		ControllerOptions: opts.Controller,
+		Predicates:        controlplane.DefaultPredicates(opts.IgnoreOperationAnnotation),
+		Type:              local.Type,
+	})
+}
+
+// AddToManager adds a controller with the default Options.
+func AddToManager(mgr manager.Manager) error {
+	return AddToManagerWithOptions(mgr, DefaultAddOptions)
+}
