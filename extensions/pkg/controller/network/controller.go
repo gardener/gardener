dd0849f2fcad095194f98eb0e5ae7aee8aca233f
Stoyan Rachev <s.rachev@sap.com>
2021-12-08 14:00:11 +0200

    Fix usage of IsDeleting predicate for extension resources

--- extensions/pkg/controller/network/controller.go
+++ extensions/pkg/controller/network/controller.go
@@ -65,7 +65,10 @@ func DefaultPredicates(ignoreOperationAnnotation bool) []predicate.Predicate {
 		predicate.Or(
 			predicateutils.HasOperationAnnotation(),
 			extensionspredicate.LastOperationNotSuccessful(),
-			extensionspredicate.IsDeleting(),
+			predicate.And(
+				predicate.GenerationChangedPredicate{},
+				extensionspredicate.IsDeleting(),
+			),
 		),
 		extensionspredicate.ShootNotFailed(),
 	}
