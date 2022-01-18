f80631ff87c4ca21e4d1700e91450193c10733c4
Tim Usner <tim.usner@sap.com>
2022-01-18 11:13:48 +0100

    Revert "Adapt `dependency-watchdog` to use shoot access kubeconfig (#5011)"
    This reverts commit ca8d89cd7119e90aea049ea7efc71ce4da418742.

--- pkg/operation/botanist/botanist.go
+++ pkg/operation/botanist/botanist.go
@@ -163,7 +163,6 @@ func New(ctx context.Context, o *operation.Operation) (*Botanist, error) {
 	// other components
 	o.Shoot.Components.SourceBackupEntry = b.SourceBackupEntry()
 	o.Shoot.Components.BackupEntry = b.DefaultCoreBackupEntry()
-	o.Shoot.Components.DependencyWatchdogAccess = b.DefaultDependencyWatchdogAccess()
 	o.Shoot.Components.GardenerAccess = b.DefaultGardenerAccess()
 	o.Shoot.Components.NetworkPolicies, err = b.DefaultNetworkPolicies(sniPhase)
 	if err != nil {
