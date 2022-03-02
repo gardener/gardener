dcc36229bd2eed9e0d169c96b3d1521af153b200
Rafael Franzke <rafael.franzke@sap.com>
2022-03-02 14:21:57 +0100

    Implement `GetOrGenerate`

---
+++ pkg/utils/secrets/manager/get_or_generate.go
@@ -0,0 +1,281 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package manager
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
+	secretutils "github.com/gardener/gardener/pkg/utils/secrets"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (m *manager) GetOrGenerate(ctx context.Context, config secretutils.ConfigInterface, opts ...GetOrGenerateOption) (*corev1.Secret, error) {
+	options := &GetOrGenerateOptions{}
+	if err := options.ApplyOptions(m, config, opts); err != nil {
+		return nil, err
+	}
+
+	var bundleFor *string
+	if options.isBundleSecret {
+		bundleFor = pointer.String(strings.TrimSuffix(config.GetName(), nameSuffixBundle))
+	}
+
+	objectMeta, err := ObjectMeta(m.namespace, config, m.lastRotationStartTimes[config.GetName()], options.signingCAChecksum, &options.Persist, bundleFor)
+	if err != nil {
+		return nil, err
+	}
+
+	secret := &corev1.Secret{}
+	if err := m.client.Get(ctx, kutil.Key(objectMeta.Namespace, objectMeta.Name), secret); err != nil {
+		if !apierrors.IsNotFound(err) {
+			return nil, err
+		}
+
+		secret, err = m.generateAndCreate(ctx, config, objectMeta)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if !options.isBundleSecret {
+		if err := m.addToStore(config.GetName(), secret, current); err != nil {
+			return nil, err
+		}
+
+		if !options.IgnoreOldSecrets && options.RotationStrategy == KeepOld {
+			if err := m.storeOldSecrets(ctx, config.GetName(), secret.Name); err != nil {
+				return nil, err
+			}
+		}
+
+		if err := m.getOrGenerateBundleSecret(ctx, config); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := m.reconcileSecret(ctx, secret, objectMeta.Labels); err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+}
+
+func (m *manager) generateAndCreate(ctx context.Context, config secretutils.ConfigInterface, objectMeta metav1.ObjectMeta) (*corev1.Secret, error) {
+	data, err := config.Generate()
+	if err != nil {
+		return nil, err
+	}
+
+	secret := Secret(objectMeta, data.SecretData())
+	if err := m.client.Create(ctx, secret); kutil.IgnoreAlreadyExists(err) != nil {
+		return nil, err
+	}
+
+	m.logger.Infof("Generated new secret for %q with name %q", config.GetName(), secret.Name)
+	return secret, nil
+}
+
+func (m *manager) storeOldSecrets(ctx context.Context, name, currentSecretName string) error {
+	secretList := &corev1.SecretList{}
+	if err := m.client.List(ctx, secretList, client.InNamespace(m.namespace), client.MatchingLabels{
+		LabelKeyName:      name,
+		LabelKeyManagedBy: LabelValueSecretsManager,
+	}); err != nil {
+		return err
+	}
+
+	for _, secret := range secretList.Items {
+		if secret.Name == currentSecretName {
+			continue
+		}
+
+		if err := m.addToStore(secret.Labels[LabelKeyName], secret.DeepCopy(), old); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *manager) getOrGenerateBundleSecret(ctx context.Context, config secretutils.ConfigInterface) error {
+	var bundleConfig secretutils.ConfigInterface
+
+	secrets, found := m.getFromStore(config.GetName())
+	if !found {
+		return fmt.Errorf("secrets for name %q not found in internal store", config.GetName())
+	}
+
+	switch c := config.(type) {
+	case *secretutils.CertificateSecretConfig:
+		if c.SigningCA == nil {
+			certs := [][]byte{secrets.current.obj.Data[secretutils.DataKeyCertificateCA]}
+			if secrets.old != nil {
+				certs = append(certs, secrets.old.obj.Data[secretutils.DataKeyCertificateCA])
+			}
+
+			bundleConfig = &secretutils.CertificateBundleSecretConfig{
+				Name:            config.GetName() + nameSuffixBundle,
+				CertificatePEMs: certs,
+			}
+		}
+	}
+
+	if bundleConfig == nil {
+		return nil
+	}
+
+	secret, err := m.GetOrGenerate(ctx, bundleConfig, isBundleSecret())
+	if err != nil {
+		return err
+	}
+
+	return m.addToStore(config.GetName(), secret, bundle)
+}
+
+func (m *manager) reconcileSecret(ctx context.Context, secret *corev1.Secret, labels map[string]string) error {
+	patch := client.MergeFrom(secret.DeepCopy())
+
+	var mustPatch bool
+
+	if secret.Immutable == nil || !*secret.Immutable {
+		secret.Immutable = pointer.Bool(true)
+		mustPatch = true
+	}
+
+	for k, v := range labels {
+		if secret.Labels[k] != v {
+			metav1.SetMetaDataLabel(&secret.ObjectMeta, k, v)
+			mustPatch = true
+		}
+	}
+
+	if !mustPatch {
+		return nil
+	}
+
+	return m.client.Patch(ctx, secret, patch)
+}
+
+// GetOrGenerateOption is some configuration that modifies options for a GetOrGenerate request.
+type GetOrGenerateOption func(Interface, secretutils.ConfigInterface, *GetOrGenerateOptions) error
+
+// GetOrGenerateOptions are options for GetOrGenerate calls.
+type GetOrGenerateOptions struct {
+	// Persist specifies whether the 'persist=true' label should be added to the secret resources.
+	Persist bool
+	// RotationStrategy specifies how the secret should be rotated in case it needs to get rotated.
+	RotationStrategy rotationStrategy
+	// IgnoreOldSecrets specifies whether old secrets should be loaded to the internal store.
+	IgnoreOldSecrets bool
+
+	signingCAChecksum *string
+	isBundleSecret    bool
+}
+
+type rotationStrategy string
+
+const (
+	// InPlace is a constant for a rotation strategy regenerating a secret and NOT keeping the old one in the system.
+	InPlace rotationStrategy = "inplace"
+	// KeepOld is a constant for a rotation strategy regenerating a secret and keeping the old one in the system.
+	KeepOld rotationStrategy = "keepold"
+)
+
+// ApplyOptions applies the given update options on these options, and then returns itself (for convenient chaining).
+func (o *GetOrGenerateOptions) ApplyOptions(manager Interface, configInterface secretutils.ConfigInterface, opts []GetOrGenerateOption) error {
+	for _, opt := range opts {
+		if err := opt(manager, configInterface, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SignedByCA returns a function which sets the 'SigningCA' field in case the ConfigInterface provided to the
+// GetOrGenerate request is a CertificateSecretConfig. Additionally, in such case it stores a checksum of the signing
+// CA in the options.
+func SignedByCA(name string) GetOrGenerateOption {
+	return func(m Interface, config secretutils.ConfigInterface, options *GetOrGenerateOptions) error {
+		mgr, ok := m.(*manager)
+		if !ok {
+			return nil
+		}
+
+		certificateConfig, ok := config.(*secretutils.CertificateSecretConfig)
+		if !ok {
+			return fmt.Errorf("could not apply option to %T, expected *secrets.CertificateSecretConfig", config)
+		}
+
+		secrets, found := mgr.getFromStore(name)
+		if !found {
+			return fmt.Errorf("secrets for name %q not found in internal store", name)
+		}
+
+		// Client certificates are always renewed immediately (hence, signed with the current CA), while server
+		// certificates are signed with the old CA until they don't exist anymore in the internal store.
+		secret := secrets.current
+		if certificateConfig.CertType == secretutils.ServerCert && secrets.old != nil {
+			secret = *secrets.old
+		}
+
+		ca, err := secretutils.LoadCertificate(name, secret.obj.Data[secretutils.DataKeyPrivateKeyCA], secret.obj.Data[secretutils.DataKeyCertificateCA])
+		if err != nil {
+			return err
+		}
+
+		certificateConfig.SigningCA = ca
+		options.signingCAChecksum = pointer.String(kutil.TruncateLabelValue(secret.dataChecksum))
+		return nil
+	}
+}
+
+// Persist returns a function which sets the 'Persist' field to true.
+func Persist() GetOrGenerateOption {
+	return func(_ Interface, _ secretutils.ConfigInterface, options *GetOrGenerateOptions) error {
+		options.Persist = true
+		return nil
+	}
+}
+
+// Rotate returns a function which sets the 'RotationStrategy' field to the specified value.
+func Rotate(strategy rotationStrategy) GetOrGenerateOption {
+	return func(_ Interface, _ secretutils.ConfigInterface, options *GetOrGenerateOptions) error {
+		options.RotationStrategy = strategy
+		return nil
+	}
+}
+
+// IgnoreOldSecrets returns a function which sets the 'IgnoreOldSecrets' field to true.
+func IgnoreOldSecrets() GetOrGenerateOption {
+	return func(_ Interface, _ secretutils.ConfigInterface, options *GetOrGenerateOptions) error {
+		options.IgnoreOldSecrets = true
+		return nil
+	}
+}
+
+func isBundleSecret() GetOrGenerateOption {
+	return func(_ Interface, _ secretutils.ConfigInterface, options *GetOrGenerateOptions) error {
+		options.isBundleSecret = true
+		return nil
+	}
+}
--- pkg/utils/secrets/manager/manager.go
+++ pkg/utils/secrets/manager/manager.go
@@ -20,15 +20,42 @@ import (
 	"time"
 
 	"github.com/gardener/gardener/pkg/utils"
+	secretutils "github.com/gardener/gardener/pkg/utils/secrets"
+
+	"github.com/mitchellh/hashstructure/v2"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
+	// LabelKeyName is a constant for a key of a label on a Secret describing the name.
+	LabelKeyName = "name"
+	// LabelKeyManagedBy is a constant for a key of a label on a Secret describing whose managing it.
+	LabelKeyManagedBy = "managed-by"
+	// LabelKeyChecksumConfig is a constant for a key of a label on a Secret describing the checksum of the
+	// configuration used to create the data.
+	LabelKeyChecksumConfig = "checksum-of-config"
+	// LabelKeyChecksumSigningCA is a constant for a key of a label on a Secret describing the checksum of the
+	// certificate authority which has signed the client or server certificate in the data.
+	LabelKeyChecksumSigningCA = "checksum-of-signing-ca"
+	// LabelKeyBundleFor is a constant for a key of a label on a Secret describing that it is a bundle secret for
+	// another secret.
+	LabelKeyBundleFor = "bundle-for"
+	// LabelKeyPersist is a constant for a key of a label on a Secret describing that it should get persisted.
+	LabelKeyPersist = "persist"
 	// LabelKeyLastRotationStartedTime is a constant for a key of a value on a Secret describing the unix timestamps
 	// of when the secret was last rotated.
 	LabelKeyLastRotationStartedTime = "last-rotation-started-time"
+
+	// LabelValueTrue is a constant for a value of a label on a Secret describing the value 'true'.
+	LabelValueTrue = "true"
+	// LabelValueSecretsManager is a constant for a value of a label on a Secret describing the value 'secret-manager'.
+	LabelValueSecretsManager = "secrets-manager"
+
+	nameSuffixBundle = "-bundle"
 )
 
 type (
@@ -136,4 +163,85 @@ func computeSecretInfo(obj *corev1.Secret) (secretInfo, error) {
 		dataChecksum:            utils.ComputeSecretChecksum(obj.Data),
 		lastRotationStartedTime: lastRotationStartTime,
 	}, nil
+}
+
+// ObjectMeta returns the object meta based on the given settings.
+func ObjectMeta(
+	namespace string,
+	config secretutils.ConfigInterface,
+	lastRotationStartedTime string,
+	signingCAChecksum *string,
+	persist *bool,
+	bundleFor *string,
+) (
+	metav1.ObjectMeta,
+	error,
+) {
+	configHash, err := hashstructure.Hash(config, hashstructure.FormatV2, &hashstructure.HashOptions{IgnoreZeroValue: true})
+	if err != nil {
+		return metav1.ObjectMeta{}, err
+	}
+
+	labels := map[string]string{
+		LabelKeyName:                    config.GetName(),
+		LabelKeyManagedBy:               LabelValueSecretsManager,
+		LabelKeyChecksumConfig:          strconv.FormatUint(configHash, 10),
+		LabelKeyLastRotationStartedTime: lastRotationStartedTime,
+	}
+
+	if signingCAChecksum != nil {
+		labels[LabelKeyChecksumSigningCA] = *signingCAChecksum
+	}
+
+	if persist != nil && *persist {
+		labels[LabelKeyPersist] = LabelValueTrue
+	}
+
+	if bundleFor != nil {
+		labels[LabelKeyBundleFor] = *bundleFor
+	}
+
+	return metav1.ObjectMeta{
+		Name:      computeSecretName(config, labels),
+		Namespace: namespace,
+		Labels:    labels,
+	}, nil
+}
+
+func computeSecretName(config secretutils.ConfigInterface, labels map[string]string) string {
+	name := config.GetName()
+
+	// For backwards-compatibility we need to keep the static names of the CA secrets so that components relying on them
+	// don't break.
+	// TODO(rfranzke): The outer constraint can be removed in the future once we adapted all components relying on the
+	//  constant CA secret names, i.e., in this case we can always use 'GenerateName'.
+	if cfg, ok := config.(*secretutils.CertificateSecretConfig); !ok || cfg.SigningCA != nil {
+		if infix := labels[LabelKeyChecksumConfig] + labels[LabelKeyChecksumSigningCA]; len(infix) > 0 {
+			name += "-" + utils.ComputeSHA256Hex([]byte(infix))[:8]
+		}
+	}
+
+	if suffix := labels[LabelKeyLastRotationStartedTime]; len(suffix) > 0 {
+		name += "-" + utils.ComputeSHA256Hex([]byte(suffix))[:5]
+	}
+
+	return name
+}
+
+// Secret constructs a *corev1.Secret for the given metadata and data.
+func Secret(objectMeta metav1.ObjectMeta, data map[string][]byte) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: objectMeta,
+		Data:       data,
+		Type:       secretTypeForData(data),
+		Immutable:  pointer.Bool(true),
+	}
+}
+
+func secretTypeForData(data map[string][]byte) corev1.SecretType {
+	secretType := corev1.SecretTypeOpaque
+	if data[secretutils.DataKeyCertificate] != nil && data[secretutils.DataKeyPrivateKey] != nil {
+		secretType = corev1.SecretTypeTLS
+	}
+	return secretType
 }
