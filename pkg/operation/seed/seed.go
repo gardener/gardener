b796851ab0122c6caf3b0245a85bdbc333a40c6c
Rafael Franzke <rafael.franzke@sap.com>
2022-03-17 14:02:54 +0100

    Adapt observability ingress secret creation in seed controller

--- pkg/operation/seed/seed.go
+++ pkg/operation/seed/seed.go
@@ -359,23 +359,28 @@ func RunReconcileSeedFlow(
 		return err
 	}
 
-	if monitoringSecrets := common.GetSecretKeysWithPrefix(v1beta1constants.GardenRoleGlobalMonitoring, secrets); len(monitoringSecrets) > 0 {
-		for _, key := range monitoringSecrets {
-			secret := secrets[key]
-			secretObj := &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      fmt.Sprintf("%s-%s", "seed", secret.Name),
-					Namespace: "garden",
-				},
+	// replicate global monitoring secret (read from garden cluster) to the seed cluster's garden namespace
+	var globalMonitoringSecretSeed *corev1.Secret
+	if globalMonitoringSecretGarden, ok := secrets[v1beta1constants.GardenRoleGlobalMonitoring]; ok {
+		globalMonitoringSecretSeed = &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "seed-" + globalMonitoringSecretGarden.Name,
+				Namespace: v1beta1constants.GardenNamespace,
+			},
+		}
+
+		if _, err := controllerutils.GetAndCreateOrMergePatch(ctx, seedClient, globalMonitoringSecretSeed, func() error {
+			globalMonitoringSecretSeed.Type = globalMonitoringSecretGarden.Type
+			globalMonitoringSecretSeed.Data = globalMonitoringSecretGarden.Data
+			globalMonitoringSecretSeed.Immutable = globalMonitoringSecretGarden.Immutable
+
+			if _, ok := globalMonitoringSecretSeed.Data[secretutils.DataKeySHA1Auth]; !ok {
+				globalMonitoringSecretSeed.Data[secretutils.DataKeySHA1Auth] = utils.CreateSHA1Secret(globalMonitoringSecretGarden.Data[secretutils.DataKeyUserName], globalMonitoringSecretGarden.Data[secretutils.DataKeyPassword])
 			}
 
-			if _, err := controllerutils.GetAndCreateOrMergePatch(ctx, seedClient, secretObj, func() error {
-				secretObj.Type = corev1.SecretTypeOpaque
-				secretObj.Data = secret.Data
-				return nil
-			}); err != nil {
-				return err
-			}
+			return nil
+		}); err != nil {
+			return err
 		}
 	}
 
@@ -754,17 +759,12 @@ func RunReconcileSeedFlow(
 			// Apply status from old Object to retain status information
 			new.Object["status"] = old.Object["status"]
 		}
-		hvpaGK                = schema.GroupKind{Group: "autoscaling.k8s.io", Kind: "Hvpa"}
-		issuerGK              = schema.GroupKind{Group: "certmanager.k8s.io", Kind: "ClusterIssuer"}
-		grafanaHost           = seed.GetIngressFQDN(grafanaPrefix)
-		prometheusHost        = seed.GetIngressFQDN(prometheusPrefix)
-		monitoringCredentials = existingSecretsMap["seed-monitoring-ingress-credentials"]
-		monitoringBasicAuth   string
+		hvpaGK         = schema.GroupKind{Group: "autoscaling.k8s.io", Kind: "Hvpa"}
+		issuerGK       = schema.GroupKind{Group: "certmanager.k8s.io", Kind: "ClusterIssuer"}
+		grafanaHost    = seed.GetIngressFQDN(grafanaPrefix)
+		prometheusHost = seed.GetIngressFQDN(prometheusPrefix)
 	)
 
-	if monitoringCredentials != nil {
-		monitoringBasicAuth = utils.CreateSHA1Secret(monitoringCredentials.Data[secretutils.DataKeyUserName], monitoringCredentials.Data[secretutils.DataKeyPassword])
-	}
 	applierOptions[vpaGK] = retainStatusInformation
 	applierOptions[hvpaGK] = retainStatusInformation
 	applierOptions[issuerGK] = retainStatusInformation
@@ -1016,7 +1016,7 @@ func RunReconcileSeedFlow(
 			"enabled": gardenletfeatures.FeatureGate.Enabled(features.ManagedIstio),
 		},
 		"ingress": map[string]interface{}{
-			"basicAuthSecret": monitoringBasicAuth,
+			"authSecretName": globalMonitoringSecretSeed.Name,
 		},
 	})
 
@@ -1026,13 +1026,9 @@ func RunReconcileSeedFlow(
 
 	// TODO(rfranzke): Remove in a future release.
 	if err := kutil.DeleteObjects(ctx, seedClient,
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "fluent-bit-config"}},
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "loki-config"}},
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "telegraf-config"}},
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "nginx-ingress-controller"}},
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "grafana-dashboard-providers"}},
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "grafana-datasources"}},
-		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: v1beta1constants.GardenNamespace, Name: "grafana-dashboards"}},
+		&corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: "seed-monitoring-ingress-credentials", Namespace: v1beta1constants.GardenNamespace}},
+		&corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: "grafana-basic-auth", Namespace: v1beta1constants.GardenNamespace}},
+		&corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: "aggregate-prometheus-basic-auth", Namespace: v1beta1constants.GardenNamespace}},
 	); err != nil {
 		return err
 	}
