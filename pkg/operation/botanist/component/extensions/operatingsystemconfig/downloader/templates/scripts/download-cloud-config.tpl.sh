57f89fcaa33847c511e1e96decfcf491768c1afa
Rafael Franzke <rafael.franzke@sap.com>
2022-04-07 14:05:57 +0200

    Only download full cloud config if checksum annotation changed

--- pkg/operation/botanist/component/extensions/operatingsystemconfig/downloader/templates/scripts/download-cloud-config.tpl.sh
+++ pkg/operation/botanist/component/extensions/operatingsystemconfig/downloader/templates/scripts/download-cloud-config.tpl.sh
@@ -10,27 +10,46 @@ TOKEN_SECRET_NAME="{{ .tokenSecretName }}"
 
 PATH_CLOUDCONFIG_DOWNLOADER_CLIENT_CERT="{{ .pathCredentialsClientCert }}"
 PATH_CLOUDCONFIG_DOWNLOADER_CLIENT_KEY="{{ .pathCredentialsClientKey }}"
-PATH_BOOTSTRAP_TOKEN="{{ .pathBootstrapToken }}"
 PATH_CLOUDCONFIG_DOWNLOADER_TOKEN="{{ .pathCredentialsToken }}"
+PATH_BOOTSTRAP_TOKEN="{{ .pathBootstrapToken }}"
+PATH_EXECUTOR_SCRIPT="{{ .pathDownloadedExecutorScript }}"
+PATH_EXECUTOR_SCRIPT_CHECKSUM="{{ .pathDownloadedChecksum }}"
+
+mkdir -p "{{ .pathDownloadsDirectory }}"
 
 function readSecret() {
   wget \
     -qO- \
-    --header         "Accept: application/yaml" \
     --ca-certificate "{{ .pathCredentialsCACert }}" \
     "${@:2}" "$(cat "{{ .pathCredentialsServer }}")/api/v1/namespaces/kube-system/secrets/$1"
 }
 
+function readSecretFull() {
+  readSecret "$1" "--header=Accept: application/yaml" "${@:2}"
+}
+
+function readSecretMeta() {
+  readSecret "$1" "--header=Accept: application/yaml;as=PartialObjectMetadata;g=meta.k8s.io;v=v1,application/yaml;as=PartialObjectMetadata;g=meta.k8s.io;v=v1" "${@:2}"
+}
+
+function readSecretMetaWithToken() {
+  readSecretMeta "$1" "--header=Authorization: Bearer $2"
+}
+
 function readSecretWithToken() {
-  readSecret "$1" "--header=Authorization: Bearer $2"
+  readSecretFull "$1" "--header=Authorization: Bearer $2"
 }
 
 function readSecretWithClientCertificate() {
-  readSecret "$1" "--certificate=$PATH_CLOUDCONFIG_DOWNLOADER_CLIENT_CERT" "--private-key=$PATH_CLOUDCONFIG_DOWNLOADER_CLIENT_KEY"
+  readSecretFull "$1" "--certificate=$PATH_CLOUDCONFIG_DOWNLOADER_CLIENT_CERT" "--private-key=$PATH_CLOUDCONFIG_DOWNLOADER_CLIENT_KEY"
 }
 
 function extractDataKeyFromSecret() {
   echo "$1" | sed -rn "s/  $2: (.*)/\1/p" | base64 -d
+}
+
+function extractChecksumFromSecret() {
+  echo "$1" | sed -rn 's/    {{ .annotationChecksum | replace "/" "\\/" }}: (.*)/\1/p' | sed -e 's/^"//' -e 's/"$//'
 }
 
 function writeToDiskSafely() {
@@ -69,20 +88,41 @@ if [[ -z "$TOKEN" ]]; then
   echo "Token in shoot access secret $TOKEN_SECRET_NAME is empty"
   exit 1
 fi
-
 writeToDiskSafely "$TOKEN" "$PATH_CLOUDCONFIG_DOWNLOADER_TOKEN"
 
 # download and run the cloud config execution script
-if ! SECRET="$(readSecretWithToken "$SECRET_NAME" "$TOKEN")"; then
-  echo "Could not retrieve the cloud config script in secret with name $SECRET_NAME"
+if ! SECRET_META="$(readSecretMetaWithToken "$SECRET_NAME" "$TOKEN")"; then
+  echo "Could not retrieve the metadata in secret with name $SECRET_NAME"
   exit 1
 fi
+NEW_CHECKSUM="$(extractChecksumFromSecret "$SECRET_META")"
 
-CHECKSUM="$(echo "$SECRET" | sed -rn 's/    {{ .annotationChecksum | replace "/" "\\/" }}: (.*)/\1/p' | sed -e 's/^"//' -e 's/"$//')"
-echo "$CHECKSUM" > "{{ .pathDownloadedChecksum }}"
+OLD_CHECKSUM="<none>"
+if [[ -f "$PATH_EXECUTOR_SCRIPT_CHECKSUM" ]]; then
+  OLD_CHECKSUM="$(cat "$PATH_EXECUTOR_SCRIPT_CHECKSUM")"
+fi
 
-SCRIPT="$(extractDataKeyFromSecret "$SECRET" "{{ .dataKeyScript }}")"
-echo "$SCRIPT" | bash
+if [[ "$NEW_CHECKSUM" != "$OLD_CHECKSUM" ]]; then
+  echo "Checksum of cloud config script has changed compared to what I had downloaded earlier (new: $NEW_CHECKSUM, old: $OLD_CHECKSUM). Fetching new script..."
 
+  if ! SECRET="$(readSecretWithToken "$SECRET_NAME" "$TOKEN")"; then
+    echo "Could not retrieve the cloud config script in secret with name $SECRET_NAME"
+    exit 1
+  fi
+
+  SCRIPT="$(extractDataKeyFromSecret "$SECRET" "{{ .dataKeyScript }}")"
+  if [[ -z "$SCRIPT" ]]; then
+    echo "Script in cloud config secret $SECRET is empty"
+    exit 1
+  fi
+
+  writeToDiskSafely "$SCRIPT" "$PATH_EXECUTOR_SCRIPT" && chmod +x "$PATH_EXECUTOR_SCRIPT"
+  writeToDiskSafely "$(extractChecksumFromSecret "$SECRET")" "$PATH_EXECUTOR_SCRIPT_CHECKSUM"
+fi
+
+# TODO(rfranzke): Delete in future release.
+rm -f "{{ .pathDownloadsDirectory }}/downloaded_checksum"
+
+"$PATH_EXECUTOR_SCRIPT"
 exit $?
 }
