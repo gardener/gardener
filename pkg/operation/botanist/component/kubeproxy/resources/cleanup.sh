972acc55eccf4d9f5e86ffc659fbea73e2579f42
Gardener Prow Robot <95282030+gardener-ci-robot@users.noreply.github.com>
2022-04-08 08:29:41 +0200

    Add workaround for switching from kube-proxy mode IPVS to IPTables. (#5770)
    The cleanup of kube-proxy seems to be broken since v1.21 when started in IPVS mode.
    This workaround flushes the iptables chain KUBE-NODE-PORT in the filter table so that
    the cleanup does not fail hard, but continues as desired.
    
    Co-authored-by: Johannes Scheerer <johannes.scheerer@sap.com>

--- pkg/operation/botanist/component/kubeproxy/resources/cleanup.sh
+++ pkg/operation/botanist/component/kubeproxy/resources/cleanup.sh
@@ -6,5 +6,11 @@ if [ -z "${OLD_KUBE_PROXY_MODE}" ] || [ "${OLD_KUBE_PROXY_MODE}" = "${KUBE_PROXY
   exit 0
 fi
 
+# Workaround kube-proxy bug when switching from ipvs to iptables mode
+if iptables -t filter -L KUBE-NODE-PORT; then
+  echo "KUBE-NODE-PORT chain exists, flushing it..."
+  iptables -t filter -F KUBE-NODE-PORT
+fi
+
 /usr/local/bin/kube-proxy --v=2 --cleanup --config=/var/lib/kube-proxy-config/config.yaml --proxy-mode="${OLD_KUBE_PROXY_MODE}"
 echo "${KUBE_PROXY_MODE}" >"$1"
