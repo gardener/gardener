4a452dbc728977642934b982f1ebbd499adc912f
Mirko-Weiler <mirko.weiler@async.ac>
2022-04-11 16:00:58 +0200

    #5630 provide acl values in apiserverexposure

--- pkg/operation/botanist/kubeapiserverexposure.go
+++ pkg/operation/botanist/kubeapiserverexposure.go
@@ -86,6 +86,7 @@ func (b *Botanist) DefaultKubeAPIServerSNI() component.DeployWaiter {
 		&kubeapiserverexposure.SNIValues{
 			IstioIngressGateway:      b.getIngressGatewayConfig(),
 			APIServerInternalDNSName: b.outOfClusterAPIServerFQDN(),
+			AccessControl:            *b.Shoot.GetInfo().Spec.Kubernetes.KubeAPIServer.AccessControl,
 		},
 	))
 }
@@ -160,6 +161,7 @@ func (b *Botanist) setAPIServerServiceClusterIP(clusterIP string) {
 			},
 			IstioIngressGateway:      b.getIngressGatewayConfig(),
 			APIServerInternalDNSName: b.outOfClusterAPIServerFQDN(),
+			AccessControl:            *b.Shoot.GetInfo().Spec.Kubernetes.KubeAPIServer.AccessControl,
 		},
 	)
 }
