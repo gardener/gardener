f74b427915216972c2595d4271db0520e01b9522
Ismail Alidzhikov <i.alidjikov@gmail.com>
2022-05-03 14:24:31 +0300

    Revert "Apply resources in sorted order in managedresource controller (#5822)" (#5898)
    This reverts commit 052739856f9f90110d81c97791ee0bf4d9fcf434.

--- pkg/resourcemanager/controller/managedresource/sort_test.go
+++ pkg/resourcemanager/controller/managedresource/sort_test.go
@@ -22,132 +22,86 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-var _ = Describe("Sorter", func() {
-	Describe("Reference sorter", func() {
-		var refs, refsBase []resourcesv1alpha1.ObjectReference
+var _ = Describe("Reference sorter", func() {
+	var refs, refsBase []resourcesv1alpha1.ObjectReference
 
-		BeforeEach(func() {
-			refsBase = []resourcesv1alpha1.ObjectReference{
-				{
-					ObjectReference: corev1.ObjectReference{
-						APIVersion: "v1",
-						Kind:       "ConfigMap",
-						Name:       "foo",
-						Namespace:  "bar",
-					},
+	BeforeEach(func() {
+		refsBase = []resourcesv1alpha1.ObjectReference{
+			{
+				ObjectReference: corev1.ObjectReference{
+					APIVersion: "v1",
+					Kind:       "ConfigMap",
+					Name:       "foo",
+					Namespace:  "bar",
 				},
-				{
-					ObjectReference: corev1.ObjectReference{
-						APIVersion: "apps/v1",
-						Kind:       "Deployment",
-						Name:       "nginx",
-						Namespace:  "web",
-					},
+			},
+			{
+				ObjectReference: corev1.ObjectReference{
+					APIVersion: "apps/v1",
+					Kind:       "Deployment",
+					Name:       "nginx",
+					Namespace:  "web",
 				},
-			}
+			},
+		}
 
-			// copy refs for assertions, as referenceSorter is sorting in-place
-			refs = append(refsBase[:0:0], refsBase...)
+		// copy refs for assertions, as referenceSorter is sorting in-place
+		refs = append(refsBase[:0:0], refsBase...)
+	})
+
+	Describe("#sortObjectReferences", func() {
+		It("should correctly sort refs", func() {
+			sortObjectReferences(refs)
+			Expect(refs).To(Equal(refsBase))
 		})
-
-		Describe("#sortObjectReferences", func() {
-			It("should correctly sort refs", func() {
-				sortObjectReferences(refs)
-				Expect(refs).To(Equal(refsBase))
-			})
-			It("should correctly sort refs (inverted order)", func() {
-				refs[0], refs[1] = refs[1], refs[0]
-				sortObjectReferences(refs)
-				Expect(refs).To(Equal(refsBase))
-			})
-		})
-
-		Describe("#newReferenceSorter", func() {
-			var sorter referenceSorter
-
-			BeforeEach(func() {
-				sorter = newReferenceSorter(refs).(referenceSorter)
-			})
-
-			It("should return the correct length", func() {
-				Expect(sorter.Len()).To(BeEquivalentTo(len(refsBase)))
-			})
-
-			It("should return the correct length (nil slice)", func() {
-				sorter = newReferenceSorter(nil).(referenceSorter)
-				Expect(sorter.Len()).To(BeEquivalentTo(0))
-			})
-
-			It("should calculate the correct keys for refs", func() {
-				Expect(refs).To(Equal([]resourcesv1alpha1.ObjectReference{
-					refsBase[0],
-					refsBase[1],
-				}))
-				Expect(sorter.keys).To(Equal([]string{
-					"/ConfigMap/bar/foo",
-					"apps/Deployment/web/nginx",
-				}))
-			})
-
-			It("should correctly compare refs", func() {
-				Expect(sorter.Less(0, 1)).To(BeTrue())
-			})
-
-			It("should correctly swap refs and keys", func() {
-				sorter.Swap(0, 1)
-				Expect(refs).To(Equal([]resourcesv1alpha1.ObjectReference{
-					refsBase[1],
-					refsBase[0],
-				}))
-				Expect(sorter.keys).To(Equal([]string{
-					"apps/Deployment/web/nginx",
-					"/ConfigMap/bar/foo",
-				}))
-			})
+		It("should correctly sort refs (inverted order)", func() {
+			refs[0], refs[1] = refs[1], refs[0]
+			sortObjectReferences(refs)
+			Expect(refs).To(Equal(refsBase))
 		})
 	})
 
-	Describe("Kind sorter", func() {
-		var obj, objBase []object
+	Describe("#newReferenceSorter", func() {
+		var sorter referenceSorter
 
 		BeforeEach(func() {
-			objBase = []object{
-				{
-					oldInformation: resourcesv1alpha1.ObjectReference{
-						ObjectReference: corev1.ObjectReference{
-							APIVersion: "v1",
-							Kind:       "ConfigMap",
-							Name:       "foo",
-							Namespace:  "bar",
-						},
-					},
-				},
-				{
-					oldInformation: resourcesv1alpha1.ObjectReference{
-						ObjectReference: corev1.ObjectReference{
-							APIVersion: "apps/v1",
-							Kind:       "Deployment",
-							Name:       "nginx",
-							Namespace:  "web",
-						},
-					},
-				},
-			}
-
-			// copy refs for assertions, as kindSorter is sorting in-place
-			obj = append(obj[:0:0], objBase...)
+			sorter = newReferenceSorter(refs).(referenceSorter)
 		})
 
-		Describe("#sortObjectReferences", func() {
-			It("should correctly sort refs", func() {
-				sortByKind(obj)
-				Expect(obj).To(Equal(objBase))
-			})
-			It("should correctly sort refs (inverted order)", func() {
-				obj[0], obj[1] = obj[1], obj[0]
-				sortByKind(obj)
-				Expect(obj).To(Equal(objBase))
-			})
+		It("should return the correct length", func() {
+			Expect(sorter.Len()).To(BeEquivalentTo(len(refsBase)))
+		})
+
+		It("should return the correct length (nil slice)", func() {
+			sorter = newReferenceSorter(nil).(referenceSorter)
+			Expect(sorter.Len()).To(BeEquivalentTo(0))
+		})
+
+		It("should calculate the correct keys for refs", func() {
+			Expect(refs).To(Equal([]resourcesv1alpha1.ObjectReference{
+				refsBase[0],
+				refsBase[1],
+			}))
+			Expect(sorter.keys).To(Equal([]string{
+				"/ConfigMap/bar/foo",
+				"apps/Deployment/web/nginx",
+			}))
+		})
+
+		It("should correctly compare refs", func() {
+			Expect(sorter.Less(0, 1)).To(BeTrue())
+		})
+
+		It("should correctly swap refs and keys", func() {
+			sorter.Swap(0, 1)
+			Expect(refs).To(Equal([]resourcesv1alpha1.ObjectReference{
+				refsBase[1],
+				refsBase[0],
+			}))
+			Expect(sorter.keys).To(Equal([]string{
+				"apps/Deployment/web/nginx",
+				"/ConfigMap/bar/foo",
+			}))
 		})
 	})
 })
