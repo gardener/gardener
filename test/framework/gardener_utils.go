124c2ad9f1feec3f7ae28d65004299a707d751de
Gardener Prow Robot <95282030+gardener-ci-robot@users.noreply.github.com>
2022-05-04 08:00:32 +0200

    [release-v1.45] Fix contorlplane migration integration test (#5911)
    * Fix CP migration test
    
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>
    
    * Unify formatting
    
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>
    
    Co-authored-by: ialidzhikov <i.alidjikov@gmail.com>

--- test/framework/gardener_utils.go
+++ test/framework/gardener_utils.go
@@ -60,10 +60,8 @@ func (f *GardenerFramework) GetSeed(ctx context.Context, seedName string) (*gard
 		return seed, nil, nil
 	}
 
-	seedClient, err := kubernetes.NewClientFromSecret(ctx, f.GardenClient.Client(), seedSecretRef.Namespace, seedSecretRef.Name, kubernetes.WithClientOptions(
-		client.Options{
-			Scheme: kubernetes.SeedScheme,
-		}),
+	seedClient, err := kubernetes.NewClientFromSecret(ctx, f.GardenClient.Client(), seedSecretRef.Namespace, seedSecretRef.Name,
+		kubernetes.WithClientOptions(client.Options{Scheme: kubernetes.SeedScheme}),
 		kubernetes.WithDisabledCachedClient(),
 	)
 	if err != nil {
