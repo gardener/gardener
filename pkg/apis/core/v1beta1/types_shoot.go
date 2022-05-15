64c494bb6a0fcc6b341e9204cd8e551d2c6c8876
Mirko Weiler <80420765+Mirko-Weiler@users.noreply.github.com>
2022-05-15 22:00:06 +0200

    Update pkg/apis/core/v1beta1/types_shoot.go
    Co-authored-by: Rafael Franzke <rafael.franzke@sap.com>

--- pkg/apis/core/v1beta1/types_shoot.go
+++ pkg/apis/core/v1beta1/types_shoot.go
@@ -715,7 +715,7 @@ type Source struct {
 	// A list of IP blocks (Ipv4 & Ipv6), populated from X-Forwarded-For header or proxy protocol.
 	// Single IP (e.g. "1.2.3.4") and CIDR (e.g. "1.2.3.0/24") are supported.
 	// +optional
-	RemoteIPBlocks []string `json:"remoteIpBlocks,omitempty" protobuf:"bytes,2,opt,name=remoteIpBlocks"`
+	RemoteIPBlocks []string `json:"remoteIPBlocks,omitempty" protobuf:"bytes,2,opt,name=remoteIPBlocks"`
 }
 
 // KubeAPIServerRequests contains configuration for request-specific settings for the kube-apiserver.
