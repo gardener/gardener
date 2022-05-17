59139efcc041d914fe3a688689086c77a4213317
Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>
2022-05-17 15:45:23 +0530

    Add unit tests

--- extensions/pkg/predicate/default_test.go
+++ extensions/pkg/predicate/default_test.go
@@ -15,6 +15,8 @@
 package predicate_test
 
 import (
+	"time"
+
 	. "github.com/gardener/gardener/extensions/pkg/predicate"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
@@ -32,6 +34,7 @@ var _ = Describe("Default", func() {
 			pred      predicate.Predicate
 			obj       *extensionsv1alpha1.Infrastructure
 			namespace = "shoot--foo--bar"
+			now       = time.Now()
 		)
 
 		BeforeEach(func() {
@@ -109,6 +112,20 @@ var _ = Describe("Default", func() {
 					obj.SetDeletionTimestamp(&metav1.Time{})
 					oldObj := obj.DeepCopy()
 					obj.Status.ObservedGeneration = 3
+					Expect(pred.Update(event.UpdateEvent{ObjectNew: obj, ObjectOld: oldObj})).To(BeFalse())
+				})
+
+				It("should return true when last operation has not succeeded and the timestamp changed", func() {
+					obj.Status.LastOperation = &gardencorev1beta1.LastOperation{State: gardencorev1beta1.LastOperationStateError}
+					oldObj := obj.DeepCopy()
+					obj.SetAnnotations(map[string]string{"gardener.cloud/timestamp": now.UTC().Add(time.Second).String()})
+					Expect(pred.Update(event.UpdateEvent{ObjectNew: obj, ObjectOld: oldObj})).To(BeTrue())
+				})
+
+				It("should return false when last operation has succeeded and the timestamp changed", func() {
+					obj.Status.LastOperation = &gardencorev1beta1.LastOperation{State: gardencorev1beta1.LastOperationStateSucceeded}
+					oldObj := obj.DeepCopy()
+					obj.SetAnnotations(map[string]string{"gardener.cloud/timestamp": now.UTC().Add(time.Second).String()})
 					Expect(pred.Update(event.UpdateEvent{ObjectNew: obj, ObjectOld: oldObj})).To(BeFalse())
 				})
 			})
