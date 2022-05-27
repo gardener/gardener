17273bfbd0a11123f1f76a2b6452c1b7e87edf5b
Kristiyan Gostev <kristiyan.gostev@sap.com>
2022-05-27 16:23:53 +0300

    Addressing comments

--- pkg/controllermanager/apis/config/v1alpha1/types.go
+++ pkg/controllermanager/apis/config/v1alpha1/types.go
@@ -334,7 +334,8 @@ type ShootRetryControllerConfiguration struct {
 	// RetryJitterPeriod is a jitter duration for the reconciler retry that can be used to distribute the retries randomly.
 	// If its value is greater than 0 then the shoot will not be retried with the configured retry period but a random
 	// duration between 0 and the configured value will be added. It is defaulted to 5m.
-	RetryJitterPeriod *metav1.Duration
+	// +optional
+	RetryJitterPeriod *metav1.Duration `json:"retryJitterPeriod,omitempty"`
 }
 
 // ShootConditionsControllerConfiguration defines the configuration of the
