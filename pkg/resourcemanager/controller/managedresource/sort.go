f73089d48ab17b91f9cac820f18b51fcd3b1fc84
acumino <sksgkpvks@gmail.com>
2022-06-02 11:41:39 +0530

    Apply resources in sorted order in managedresource controller

--- pkg/resourcemanager/controller/managedresource/sort.go
+++ pkg/resourcemanager/controller/managedresource/sort.go
@@ -18,6 +18,7 @@ import (
 	"sort"
 
 	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
+	chartrenderer "github.com/gardener/gardener/pkg/chartrenderer"
 )
 
 var _ = sort.Interface(referenceSorter{})
@@ -56,4 +57,61 @@ func (r referenceSorter) Less(i, j int) bool {
 func (r referenceSorter) Swap(i, j int) {
 	r.keys[i], r.keys[j] = r.keys[j], r.keys[i]
 	r.refs[i], r.refs[j] = r.refs[j], r.refs[i]
+}
+
+type kindSorter struct {
+	ordering map[string]int
+	objects  []object
+}
+
+func newKindSorter(obj []object, s chartrenderer.SortOrder) *kindSorter {
+	o := make(map[string]int, len(s))
+	for v, k := range s {
+		o[k] = v
+	}
+
+	return &kindSorter{
+		objects:  obj,
+		ordering: o,
+	}
+}
+
+func (k *kindSorter) Len() int { return len(k.objects) }
+
+func (k *kindSorter) Swap(i, j int) { k.objects[i], k.objects[j] = k.objects[j], k.objects[i] }
+
+func (k *kindSorter) Less(i, j int) bool {
+	a := k.objects[i]
+	b := k.objects[j]
+	first, aok := k.ordering[a.obj.GetKind()]
+	second, bok := k.ordering[b.obj.GetKind()]
+
+	if !aok && !bok {
+		// if both are unknown then sort alphabetically by kind and name
+		if a.obj.GetKind() != b.obj.GetKind() {
+			return a.obj.GetKind() < b.obj.GetKind()
+		}
+		return a.obj.GetName() < b.obj.GetName()
+	}
+
+	// unknown kind is last
+	if !aok {
+		return false
+	}
+	if !bok {
+		return true
+	}
+
+	// if same kind sub sort alphanumeric
+	if first == second {
+		return a.obj.GetName() < b.obj.GetName()
+	}
+	// sort different kinds
+	return first < second
+}
+func sortByKind(resourceObject []object) []object {
+	ordering := chartrenderer.InstallOrder
+	ks := newKindSorter(resourceObject, ordering)
+	sort.Sort(ks)
+	return ks.objects
 }
