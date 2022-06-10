becf9626c320725fa08fca21f3bc557827a8f3f6
Mirko Weiler <80420765+Mirko-Weiler@users.noreply.github.com>
2022-06-10 10:19:57 +0200

    Update pkg/apis/core/types_shoot.go
    Co-authored-by: Rafael Franzke <rafael.franzke@sap.com>

--- pkg/apis/core/types_shoot.go
+++ pkg/apis/core/types_shoot.go
@@ -559,7 +559,7 @@ const (
 	AuthorizationAction_DENY AuthorizationAction = "DENY"
 )
 
-// Source holds the origin of request to which access control is applied to.
+// Source contains the origin of request to which access control is applied to.
 type Source struct {
 	// A list of IP blocks, populated from the source address of the IP packet.
 	// Single IP (e.g. "1.2.3.4") and CIDR (e.g. "1.2.3.0/24") are supported.
