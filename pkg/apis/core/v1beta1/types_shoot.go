3d4a971d2ffb5e140b43c673f0928069421cf26b
Mirko Weiler <80420765+Mirko-Weiler@users.noreply.github.com>
2022-06-10 10:19:29 +0200

    Update pkg/apis/core/v1beta1/types_shoot.go
    Co-authored-by: Rafael Franzke <rafael.franzke@sap.com>

--- pkg/apis/core/v1beta1/types_shoot.go
+++ pkg/apis/core/v1beta1/types_shoot.go
@@ -708,8 +708,7 @@ const (
 type Source struct {
 	// A list of IP blocks (Ipv4 & Ipv6), populated from the source address of the IP packet.
 	// Single IP (e.g. "1.2.3.4") and CIDR (e.g. "1.2.3.0/24") are supported.
-	// +optional
-	IPBlocks []string `json:"ipBlocks,omitempty" protobuf:"bytes,1,opt,name=ipBlocks"`
+	IPBlocks []string `json:"ipBlocks" protobuf:"bytes,1,opt,name=ipBlocks"`
 }
 
 // KubeAPIServerRequests contains configuration for request-specific settings for the kube-apiserver.
