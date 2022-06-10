94e1a5542f5ea7e79e714da0f664e4ac2ec527a8
Mirko Weiler <80420765+Mirko-Weiler@users.noreply.github.com>
2022-06-10 10:19:07 +0200

    Update pkg/operation/botanist/kubeapiserverexposure.go
    Co-authored-by: Rafael Franzke <rafael.franzke@sap.com>

--- pkg/operation/botanist/kubeapiserverexposure.go
+++ pkg/operation/botanist/kubeapiserverexposure.go
@@ -17,7 +17,7 @@ package botanist
 import (
 	"context"
 
-	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/features"
 	gardenletfeatures "github.com/gardener/gardener/pkg/gardenlet/features"
