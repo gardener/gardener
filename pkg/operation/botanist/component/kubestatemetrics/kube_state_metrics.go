907840453413bd81c2e2bda190c6f214d25a5638
Rafael Franzke <rafael.franzke@sap.com>
2022-06-23 11:35:55 +0200

    Add basic template of new `kubestatemetrics` component

---
+++ pkg/operation/botanist/component/kubestatemetrics/kube_state_metrics.go
@@ -0,0 +1,109 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubestatemetrics
+
+import (
+	"context"
+	"time"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/operation/botanist/component"
+	"github.com/gardener/gardener/pkg/utils/managedresources"
+	secretsmanager "github.com/gardener/gardener/pkg/utils/secrets/manager"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	// ManagedResourceName is the name of the managed resource for seeds.
+	ManagedResourceName      = "kube-state-metrics"
+	shootManagedResourceName = "shoot-core-" + ManagedResourceName
+)
+
+// New creates a new instance of DeployWaiter for the kube-state-metrics.
+func New(
+	client client.Client,
+	namespace string,
+	secretsManager secretsmanager.Interface,
+	values Values,
+) component.DeployWaiter {
+	k := &kubeStateMetrics{
+		client:         client,
+		secretsManager: secretsManager,
+		namespace:      namespace,
+		values:         values,
+	}
+
+	if values.ClusterType == component.ClusterTypeSeed {
+		k.registry = managedresources.NewRegistry(kubernetes.SeedScheme, kubernetes.SeedCodec, kubernetes.SeedSerializer)
+	} else {
+		k.registry = managedresources.NewRegistry(kubernetes.ShootScheme, kubernetes.ShootCodec, kubernetes.ShootSerializer)
+	}
+
+	return k
+}
+
+type kubeStateMetrics struct {
+	client         client.Client
+	secretsManager secretsmanager.Interface
+	namespace      string
+	values         Values
+	registry       *managedresources.Registry
+}
+
+// Values is a set of configuration values for the kube-state-metrics.
+type Values struct {
+	// ClusterType specifies the type of the cluster to which VPA is being deployed.
+	// For seeds, all resources are being deployed as part of a ManagedResource.
+	// For shoots, the kube-state-metrics runs in the shoot namespace in the seed as part of the control plane. Hence,
+	// only the runtime resources (like Deployment, Service, etc.) are being deployed directly (with the client). All
+	// other application-related resources (like RBAC roles, CRD, etc.) are deployed as part of a ManagedResource.
+	ClusterType component.ClusterType
+	// Image is the container image.
+	Image string
+}
+
+func (k *kubeStateMetrics) Deploy(ctx context.Context) error {
+	return component.DeployResourceConfigs(ctx, k.client, k.namespace, k.values.ClusterType, k.managedResourceName(), k.registry, k.getResourceConfigs())
+}
+
+func (k *kubeStateMetrics) Destroy(ctx context.Context) error {
+	return component.DestroyResourceConfigs(ctx, k.client, k.namespace, k.values.ClusterType, k.managedResourceName(), k.getResourceConfigs())
+}
+
+// TimeoutWaitForManagedResource is the timeout used while waiting for the ManagedResources to become healthy
+// or deleted.
+var TimeoutWaitForManagedResource = 2 * time.Minute
+
+func (k *kubeStateMetrics) Wait(ctx context.Context) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, TimeoutWaitForManagedResource)
+	defer cancel()
+
+	return managedresources.WaitUntilHealthy(timeoutCtx, k.client, k.namespace, k.managedResourceName())
+}
+
+func (k *kubeStateMetrics) WaitCleanup(ctx context.Context) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, TimeoutWaitForManagedResource)
+	defer cancel()
+
+	return managedresources.WaitUntilDeleted(timeoutCtx, k.client, k.namespace, k.managedResourceName())
+}
+
+func (k *kubeStateMetrics) managedResourceName() string {
+	if k.values.ClusterType == component.ClusterTypeSeed {
+		return ManagedResourceName
+	}
+	return shootManagedResourceName
+}
---
+++ pkg/operation/botanist/component/kubestatemetrics/kube_state_metrics_test.go
@@ -0,0 +1,356 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubestatemetrics_test
+
+import (
+	"context"
+
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/operation/botanist/component"
+	. "github.com/gardener/gardener/pkg/operation/botanist/component/kubestatemetrics"
+	"github.com/gardener/gardener/pkg/utils/retry"
+	retryfake "github.com/gardener/gardener/pkg/utils/retry/fake"
+	secretsmanager "github.com/gardener/gardener/pkg/utils/secrets/manager"
+	fakesecretsmanager "github.com/gardener/gardener/pkg/utils/secrets/manager/fake"
+	"github.com/gardener/gardener/pkg/utils/test"
+	. "github.com/gardener/gardener/pkg/utils/test/matchers"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var _ = Describe("KubeStateMetrics", func() {
+	var (
+		ctx = context.TODO()
+
+		namespace = "some-namespace"
+		values    = Values{}
+
+		c   client.Client
+		sm  secretsmanager.Interface
+		ksm component.DeployWaiter
+
+		image = "some-image:some-tag"
+
+		managedResourceName   string
+		managedResource       *resourcesv1alpha1.ManagedResource
+		managedResourceSecret *corev1.Secret
+	)
+
+	BeforeEach(func() {
+		c = fakeclient.NewClientBuilder().WithScheme(kubernetes.SeedScheme).Build()
+		sm = fakesecretsmanager.New(c, namespace)
+
+		ksm = New(c, namespace, sm, values)
+		managedResourceName = ""
+	})
+
+	JustBeforeEach(func() {
+		managedResource = &resourcesv1alpha1.ManagedResource{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      managedResourceName,
+				Namespace: namespace,
+			},
+		}
+		managedResourceSecret = &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "managedresource-" + managedResource.Name,
+				Namespace: namespace,
+			},
+		}
+	})
+
+	Describe("#Deploy", func() {
+		Context("cluster type seed", func() {
+			BeforeEach(func() {
+				ksm = New(c, namespace, nil, Values{
+					ClusterType: component.ClusterTypeSeed,
+					Image:       image,
+				})
+				managedResourceName = "kube-state-metrics"
+			})
+
+			It("should successfully deploy all resources", func() {
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(BeNotFoundError())
+
+				Expect(ksm.Deploy(ctx)).To(Succeed())
+
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(Succeed())
+				Expect(managedResource).To(DeepEqual(&resourcesv1alpha1.ManagedResource{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: resourcesv1alpha1.SchemeGroupVersion.String(),
+						Kind:       "ManagedResource",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            managedResourceName,
+						Namespace:       namespace,
+						ResourceVersion: "1",
+					},
+					Spec: resourcesv1alpha1.ManagedResourceSpec{
+						Class: pointer.String("seed"),
+						SecretRefs: []corev1.LocalObjectReference{{
+							Name: managedResourceSecret.Name,
+						}},
+						KeepObjects: pointer.Bool(false),
+					},
+				}))
+
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(Succeed())
+				Expect(managedResourceSecret.Type).To(Equal(corev1.SecretTypeOpaque))
+				Expect(managedResourceSecret.Data).To(HaveLen(0))
+			})
+		})
+
+		Context("cluster type shoot", func() {
+			BeforeEach(func() {
+				ksm = New(c, namespace, sm, Values{
+					ClusterType: component.ClusterTypeShoot,
+					Image:       image,
+				})
+				managedResourceName = "shoot-core-kube-state-metrics"
+			})
+
+			It("should successfully deploy all resources", func() {
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(BeNotFoundError())
+
+				Expect(ksm.Deploy(ctx)).To(Succeed())
+
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(Succeed())
+				Expect(managedResource).To(DeepEqual(&resourcesv1alpha1.ManagedResource{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: resourcesv1alpha1.SchemeGroupVersion.String(),
+						Kind:       "ManagedResource",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            managedResourceName,
+						Namespace:       namespace,
+						ResourceVersion: "1",
+						Labels:          map[string]string{"origin": "gardener"},
+					},
+					Spec: resourcesv1alpha1.ManagedResourceSpec{
+						InjectLabels: map[string]string{"shoot.gardener.cloud/no-cleanup": "true"},
+						SecretRefs: []corev1.LocalObjectReference{{
+							Name: managedResourceSecret.Name,
+						}},
+						KeepObjects: pointer.Bool(false),
+					},
+				}))
+
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(Succeed())
+				Expect(managedResourceSecret.Type).To(Equal(corev1.SecretTypeOpaque))
+				Expect(managedResourceSecret.Data).To(HaveLen(0))
+			})
+		})
+	})
+
+	Describe("#Destroy", func() {
+		Context("cluster type seed", func() {
+			BeforeEach(func() {
+				ksm = New(c, namespace, nil, Values{ClusterType: component.ClusterTypeSeed})
+				managedResourceName = "kube-state-metrics"
+			})
+
+			It("should successfully destroy all resources", func() {
+				Expect(c.Create(ctx, managedResource)).To(Succeed())
+				Expect(c.Create(ctx, managedResourceSecret)).To(Succeed())
+
+				Expect(ksm.Destroy(ctx)).To(Succeed())
+
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(BeNotFoundError())
+			})
+		})
+
+		Context("cluster type shoot", func() {
+			BeforeEach(func() {
+				ksm = New(c, namespace, sm, Values{ClusterType: component.ClusterTypeShoot})
+				managedResourceName = "shoot-core-kube-state-metrics"
+			})
+
+			It("should successfully destroy all resources", func() {
+				Expect(c.Create(ctx, managedResource)).To(Succeed())
+				Expect(c.Create(ctx, managedResourceSecret)).To(Succeed())
+
+				Expect(ksm.Destroy(ctx)).To(Succeed())
+
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
+				Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(BeNotFoundError())
+			})
+		})
+	})
+
+	Context("waiting functions", func() {
+		var fakeOps *retryfake.Ops
+
+		BeforeEach(func() {
+			fakeOps = &retryfake.Ops{MaxAttempts: 1}
+			DeferCleanup(test.WithVars(
+				&retry.Until, fakeOps.Until,
+				&retry.UntilTimeout, fakeOps.UntilTimeout,
+			))
+		})
+
+		Describe("#Wait", func() {
+			tests := func(managedResourceName string) {
+				It("should fail because reading the ManagedResource fails", func() {
+					Expect(ksm.Wait(ctx)).To(MatchError(ContainSubstring("not found")))
+				})
+
+				It("should fail because the ManagedResource doesn't become healthy", func() {
+					fakeOps.MaxAttempts = 2
+
+					Expect(c.Create(ctx, &resourcesv1alpha1.ManagedResource{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:       managedResourceName,
+							Namespace:  namespace,
+							Generation: 1,
+						},
+						Status: resourcesv1alpha1.ManagedResourceStatus{
+							ObservedGeneration: 1,
+							Conditions: []gardencorev1beta1.Condition{
+								{
+									Type:   resourcesv1alpha1.ResourcesApplied,
+									Status: gardencorev1beta1.ConditionFalse,
+								},
+								{
+									Type:   resourcesv1alpha1.ResourcesHealthy,
+									Status: gardencorev1beta1.ConditionFalse,
+								},
+							},
+						},
+					}))
+
+					Expect(ksm.Wait(ctx)).To(MatchError(ContainSubstring("is not healthy")))
+				})
+
+				It("should successfully wait for the managed resource to become healthy", func() {
+					fakeOps.MaxAttempts = 2
+
+					Expect(c.Create(ctx, &resourcesv1alpha1.ManagedResource{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:       managedResourceName,
+							Namespace:  namespace,
+							Generation: 1,
+						},
+						Status: resourcesv1alpha1.ManagedResourceStatus{
+							ObservedGeneration: 1,
+							Conditions: []gardencorev1beta1.Condition{
+								{
+									Type:   resourcesv1alpha1.ResourcesApplied,
+									Status: gardencorev1beta1.ConditionTrue,
+								},
+								{
+									Type:   resourcesv1alpha1.ResourcesHealthy,
+									Status: gardencorev1beta1.ConditionTrue,
+								},
+							},
+						},
+					}))
+
+					Expect(ksm.Wait(ctx)).To(Succeed())
+				})
+			}
+
+			Context("cluster type seed", func() {
+				BeforeEach(func() {
+					ksm = New(c, namespace, nil, Values{ClusterType: component.ClusterTypeSeed})
+				})
+
+				tests("kube-state-metrics")
+			})
+
+			Context("cluster type shoot", func() {
+				BeforeEach(func() {
+					ksm = New(c, namespace, sm, Values{ClusterType: component.ClusterTypeShoot})
+				})
+
+				tests("shoot-core-kube-state-metrics")
+			})
+		})
+
+		Describe("#WaitCleanup", func() {
+			tests := func(managedResourceName string) {
+				It("should fail when the wait for the managed resource deletion times out", func() {
+					fakeOps.MaxAttempts = 2
+
+					managedResource := &resourcesv1alpha1.ManagedResource{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      managedResourceName,
+							Namespace: namespace,
+						},
+					}
+
+					Expect(c.Create(ctx, managedResource)).To(Succeed())
+
+					Expect(ksm.WaitCleanup(ctx)).To(MatchError(ContainSubstring("still exists")))
+				})
+
+				It("should not return an error when it's already removed", func() {
+					Expect(ksm.WaitCleanup(ctx)).To(Succeed())
+				})
+			}
+
+			Context("cluster type seed", func() {
+				BeforeEach(func() {
+					ksm = New(c, namespace, nil, Values{ClusterType: component.ClusterTypeSeed})
+				})
+
+				tests("kube-state-metrics")
+			})
+
+			Context("cluster type shoot", func() {
+				BeforeEach(func() {
+					ksm = New(c, namespace, sm, Values{ClusterType: component.ClusterTypeShoot})
+				})
+
+				tests("shoot-core-kube-state-metrics")
+			})
+		})
+	})
+})
+
+func serialize(obj client.Object) string {
+	var (
+		scheme        = kubernetes.SeedScheme
+		groupVersions []schema.GroupVersion
+	)
+
+	for k := range scheme.AllKnownTypes() {
+		groupVersions = append(groupVersions, k.GroupVersion())
+	}
+
+	var (
+		ser   = json.NewSerializerWithOptions(json.DefaultMetaFactory, scheme, scheme, json.SerializerOptions{Yaml: true, Pretty: false, Strict: false})
+		codec = serializer.NewCodecFactory(scheme).CodecForVersions(ser, ser, schema.GroupVersions(groupVersions), schema.GroupVersions(groupVersions))
+	)
+
+	serializationYAML, err := runtime.Encode(codec, obj)
+	Expect(err).NotTo(HaveOccurred())
+
+	return string(serializationYAML)
+}
