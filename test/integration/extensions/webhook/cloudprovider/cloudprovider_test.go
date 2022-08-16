7581381c1f80fb89b4c5031089c697ce26acdf2b
Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>
2022-08-16 21:44:11 +0530

    Integration test for cloudprovider extension webhook (#6478)
    * Add integration test for cloudprovider extention webhook
    
    * Minor bug fix related to parsing IP
    
    * Address PR review feedback

---
+++ test/integration/extensions/webhook/cloudprovider/cloudprovider_test.go
@@ -0,0 +1,94 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudprovider_test
+
+import (
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("CloudProvider tests", func() {
+	var (
+		secret *corev1.Secret
+
+		originalData = map[string][]byte{
+			"clientID": []byte("test"),
+		}
+	)
+	BeforeEach(func() {
+		secret = &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      v1beta1constants.SecretNameCloudProvider,
+				Namespace: testNamespace.Name,
+			},
+			Data: originalData,
+		}
+	})
+
+	Context("secret name is not cloudprovider", func() {
+		BeforeEach(func() {
+			secret.Name = "test-secret"
+			Expect(testClient.Create(ctx, secret)).To(Succeed())
+
+			DeferCleanup(func() {
+				By("deleting Secret")
+				Expect(client.IgnoreNotFound(testClient.Delete(ctx, secret))).To(Succeed())
+			})
+		})
+
+		It("should not mutate the secret", func() {
+			Expect(secret.Data).To(Equal(originalData))
+		})
+	})
+
+	Context("secret name is cloudprovider", func() {
+		BeforeEach(func() {
+			DeferCleanup(func() {
+				By("delete Secret")
+				Expect(client.IgnoreNotFound(testClient.Delete(ctx, secret))).To(Succeed())
+			})
+		})
+
+		It("should not mutate the secret because matching labels are not present", func() {
+			By("create Secret")
+			Eventually(func(g Gomega) {
+				Expect(testClient.Create(ctx, secret)).To(Succeed())
+
+				Expect(secret.Data).To(Equal(originalData))
+			}).Should(Succeed())
+		})
+
+		It("should mutate the secret because matching labels are present", func() {
+			secret.ObjectMeta.Labels = map[string]string{
+				v1beta1constants.GardenerPurpose: v1beta1constants.SecretNameCloudProvider,
+			}
+
+			By("create Secret")
+			Eventually(func(g Gomega) {
+				Expect(testClient.Create(ctx, secret)).To(Succeed())
+
+				Expect(secret.Data).To(Equal(map[string][]byte{
+					"clientID":     []byte("foo"),
+					"clientSecret": []byte("bar"),
+				}))
+			}).Should(Succeed())
+		})
+	})
+})
---
+++ test/integration/extensions/webhook/cloudprovider/ensurer.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudprovider
+
+import (
+	"context"
+
+	"github.com/gardener/gardener/extensions/pkg/webhook/cloudprovider"
+	gcontext "github.com/gardener/gardener/extensions/pkg/webhook/context"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// NewEnsurer creates cloudprovider ensurer.
+func NewEnsurer(logger logr.Logger) cloudprovider.Ensurer {
+	return &ensurer{
+		logger: logger,
+	}
+}
+
+type ensurer struct {
+	logger logr.Logger
+}
+
+// InjectClient injects the given client into the ensurer.
+func (e *ensurer) InjectClient(_ client.Client) error {
+	return nil
+}
+
+// InjectScheme injects the given scheme into the decoder of the ensurer.
+func (e *ensurer) InjectScheme(_ *runtime.Scheme) error {
+	return nil
+}
+
+// EnsureCloudProviderSecret isimplemented on extention side which mutates the cloudprovider secret. contain
+// For testing purpose we are mutating the cloudprovider secret's data to check whether this
+// function is called in webhook.
+func (e *ensurer) EnsureCloudProviderSecret(ctx context.Context, _ gcontext.GardenContext, new, _ *corev1.Secret) error {
+	e.logger.Info("Mutate cloudprovider secret", "namespace", new.Namespace, "name", new.Name)
+	new.Data["clientID"] = []byte(`foo`)
+	new.Data["clientSecret"] = []byte(`bar`)
+
+	return nil
+}
