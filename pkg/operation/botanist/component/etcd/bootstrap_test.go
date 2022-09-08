5fea76c6affcce667b5dfbd822630d5f2f2eaed6
Madhav Bhargava <madhav.bhargava@sap.com>
2022-09-08 15:50:44 +0530

    changed crd to reflect the current crd

--- pkg/operation/botanist/component/etcd/bootstrap_test.go
+++ pkg/operation/botanist/component/etcd/bootstrap_test.go
@@ -776,7 +776,7 @@ metadata:
   annotations:
     controller-gen.kubebuilder.io/version: v0.7.0
   labels:
-    gardener.cloud/deletion-protected: "true"
+	gardener.cloud/deletion-protected: "true"
 spec:
   group: druid.gardener.cloud
   names:
@@ -2489,7 +2489,6 @@ spec:
         statusReplicasPath: .status.replicas
       status: {}
 `
-
 	etcdCopyBackupsTaskCRDYaml = `apiVersion: apiextensions.k8s.io/v1
 kind: CustomResourceDefinition
 metadata:
@@ -2498,7 +2497,7 @@ metadata:
   creationTimestamp: null
   name: etcdcopybackupstasks.druid.gardener.cloud
   labels:
-    gardener.cloud/deletion-protected: "true"
+	gardener.cloud/deletion-protected: "true"
 spec:
   group: druid.gardener.cloud
   names:
