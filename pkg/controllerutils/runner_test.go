065acda9720677153f4da1e5afc5fbdf716b6f86
Rafael Franzke <rafael.franzke@sap.com>
2022-09-14 16:37:04 +0200

    Introduce `manager.Runnable` for controlled bootstrapping
    Typically, runnables added to the a manager are all started in parallel, hence uncontrolled. In the gardenlet case, we first want to perform a few bootstrap tasks (like validating the seed config or fetching a client certificate for the garden cluster) before we start the actual runnables (controllers, etc.).
    The introduced `Runner` is capable of achieving this goal by first starting all bootstrap runnables and only adding the actual runnables once all bootstrap runnables completed successfully.

---
+++ pkg/controllerutils/runner_test.go
@@ -0,0 +1,117 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controllerutils_test
+
+import (
+	"context"
+	"errors"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	. "github.com/gardener/gardener/pkg/controllerutils"
+)
+
+var _ = Describe("Runner", func() {
+	var (
+		startedBootstrapRunnables []int
+		addedActualRunnables      []int
+
+		bootstrapRunnable1 manager.Runnable
+		bootstrapRunnable2 manager.Runnable
+		bootstrapRunnable3 manager.Runnable
+
+		actualRunnable1 manager.Runnable
+		actualRunnable2 manager.Runnable
+		actualRunnable3 manager.Runnable
+	)
+
+	BeforeEach(func() {
+		startedBootstrapRunnables = make([]int, 0, 3)
+		addedActualRunnables = make([]int, 0, 3)
+
+		bootstrapRunnable1 = newTestRunnable(&startedBootstrapRunnables, 1)
+		bootstrapRunnable2 = newTestRunnable(&startedBootstrapRunnables, 2)
+		bootstrapRunnable3 = newTestRunnable(&startedBootstrapRunnables, 3)
+
+		actualRunnable1 = newTestRunnable(&addedActualRunnables, 1)
+		actualRunnable2 = newTestRunnable(&addedActualRunnables, 2)
+		actualRunnable3 = newTestRunnable(&addedActualRunnables, 3)
+	})
+
+	Describe("ControlledRunner", func() {
+		var controlledRunner *ControlledRunner
+
+		Describe("#Start", func() {
+			BeforeEach(func() {
+				controlledRunner = &ControlledRunner{Manager: &fakeManager{}}
+			})
+
+			It("should start and add the runnables in the correct order", func() {
+				controlledRunner.BootstrapRunnables = append(controlledRunner.BootstrapRunnables,
+					bootstrapRunnable3,
+					bootstrapRunnable1,
+					bootstrapRunnable2,
+				)
+				controlledRunner.ActualRunnables = append(controlledRunner.ActualRunnables,
+					actualRunnable2,
+					actualRunnable3,
+					actualRunnable1,
+				)
+
+				Expect(controlledRunner.Start(context.TODO())).To(Succeed())
+
+				Expect(startedBootstrapRunnables).To(Equal([]int{3, 1, 2}))
+				Expect(addedActualRunnables).To(Equal([]int{2, 3, 1}))
+			})
+		})
+	})
+
+	Describe("#AddAllRunnables", func() {
+		It("should add the runnables in the correct order", func() {
+			Expect(AddAllRunnables(&fakeManager{},
+				actualRunnable1,
+				actualRunnable3,
+				actualRunnable2,
+			)).To(Succeed())
+			Expect(addedActualRunnables).To(Equal([]int{1, 3, 2}))
+		})
+
+		It("should return an error when adding fails", func() {
+			Expect(AddAllRunnables(&fakeManager{fail: true}, actualRunnable1)).To(MatchError(ContainSubstring("failed adding runnable to manager")))
+		})
+	})
+})
+
+func newTestRunnable(tracker *[]int, number int) manager.Runnable {
+	return manager.RunnableFunc(func(_ context.Context) error {
+		*tracker = append(*tracker, number)
+		return nil
+	})
+}
+
+type fakeManager struct {
+	manager.Manager
+
+	fail bool
+}
+
+func (f *fakeManager) Add(r manager.Runnable) error {
+	if f.fail {
+		return errors.New("failed")
+	}
+	return r.Start(context.TODO())
+}
