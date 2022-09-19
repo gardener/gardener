257dc4813327e5a03a20373cefbc363f47b52287
Rafael Franzke <rafael.franzke@sap.com>
2022-09-19 09:28:27 +0200

    Rework main entrypoint
    Minor cosmetics and adaptations so that it is in line with GCM, gardenlet entrypoint

---
+++ cmd/gardener-scheduler/app/app.go
@@ -0,0 +1,156 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+	goruntime "runtime"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/component-base/version"
+	"k8s.io/component-base/version/verflag"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/controllerutils/routes"
+	gardenerhealthz "github.com/gardener/gardener/pkg/healthz"
+	"github.com/gardener/gardener/pkg/logger"
+	"github.com/gardener/gardener/pkg/scheduler/apis/config"
+	"github.com/gardener/gardener/pkg/scheduler/controller"
+	schedulerfeatures "github.com/gardener/gardener/pkg/scheduler/features"
+)
+
+// Name is a const for the name of this component.
+const Name = "gardener-scheduler"
+
+// NewCommand creates a new cobra.Command for running gardener-scheduler.
+func NewCommand() *cobra.Command {
+	opts := &options{}
+
+	cmd := &cobra.Command{
+		Use:   Name,
+		Short: "Launch the " + Name,
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			verflag.PrintAndExitIfRequested()
+
+			if err := opts.complete(); err != nil {
+				return err
+			}
+			if err := opts.validate(); err != nil {
+				return err
+			}
+
+			log, err := logger.NewZapLogger(opts.config.LogLevel, opts.config.LogFormat)
+			if err != nil {
+				return fmt.Errorf("error instantiating zap logger: %w", err)
+			}
+
+			logf.SetLogger(log)
+			klog.SetLogger(log)
+
+			log.Info("Starting "+Name, "version", version.Get())
+			cmd.Flags().VisitAll(func(flag *pflag.Flag) {
+				log.Info(fmt.Sprintf("FLAG: --%s=%s", flag.Name, flag.Value)) //nolint:logcheck
+			})
+
+			// don't output usage on further errors raised during execution
+			cmd.SilenceUsage = true
+			// further errors will be logged properly, don't duplicate
+			cmd.SilenceErrors = true
+
+			return run(cmd.Context(), log, opts.config)
+		},
+	}
+
+	flags := cmd.Flags()
+	verflag.AddFlags(flags)
+	opts.addFlags(flags)
+
+	return cmd
+}
+
+func run(ctx context.Context, log logr.Logger, cfg *config.SchedulerConfiguration) error {
+	// Add feature flags
+	if err := schedulerfeatures.FeatureGate.SetFromMap(cfg.FeatureGates); err != nil {
+		return fmt.Errorf("failed to set feature gates: %w", err)
+	}
+	log.Info("Feature Gates", "featureGates", schedulerfeatures.FeatureGate.String())
+
+	log.Info("Getting rest config")
+	if kubeconfig := os.Getenv("KUBECONFIG"); kubeconfig != "" {
+		cfg.ClientConnection.Kubeconfig = kubeconfig
+	}
+
+	restCfg, err := kubernetes.RESTConfigFromClientConnectionConfiguration(&cfg.ClientConnection, nil, kubernetes.AuthTokenFile)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Setting up manager")
+	mgr, err := manager.New(restCfg, manager.Options{
+		Scheme:                  kubernetes.GardenScheme,
+		HealthProbeBindAddress:  fmt.Sprintf("%s:%d", cfg.Server.HealthProbes.BindAddress, cfg.Server.HealthProbes.Port),
+		MetricsBindAddress:      fmt.Sprintf("%s:%d", cfg.Server.Metrics.BindAddress, cfg.Server.Metrics.Port),
+		GracefulShutdownTimeout: pointer.Duration(5 * time.Second),
+		Logger:                  log,
+
+		LeaderElection:                cfg.LeaderElection.LeaderElect,
+		LeaderElectionResourceLock:    cfg.LeaderElection.ResourceLock,
+		LeaderElectionID:              cfg.LeaderElection.ResourceName,
+		LeaderElectionNamespace:       cfg.LeaderElection.ResourceNamespace,
+		LeaderElectionReleaseOnCancel: true,
+		LeaseDuration:                 &cfg.LeaderElection.LeaseDuration.Duration,
+		RenewDeadline:                 &cfg.LeaderElection.RenewDeadline.Duration,
+		RetryPeriod:                   &cfg.LeaderElection.RetryPeriod.Duration,
+	})
+	if err != nil {
+		return err
+	}
+
+	if cfg.Debugging != nil && cfg.Debugging.EnableProfiling {
+		if err := (routes.Profiling{}).AddToManager(mgr); err != nil {
+			return fmt.Errorf("failed adding profiling handlers to manager: %w", err)
+		}
+		if cfg.Debugging.EnableContentionProfiling {
+			goruntime.SetBlockProfileRate(1)
+		}
+	}
+
+	log.Info("Adding health check endpoints to manager")
+	if err := mgr.AddReadyzCheck("informer-sync", gardenerhealthz.NewCacheSyncHealthz(mgr.GetCache())); err != nil {
+		return err
+	}
+	if err := mgr.AddHealthzCheck("ping", healthz.Ping); err != nil {
+		return err
+	}
+
+	log.Info("Adding controllers to manager")
+	if err := controller.AddControllersToManager(mgr, cfg); err != nil {
+		return fmt.Errorf("failed adding controllers to manager: %w", err)
+	}
+
+	log.Info("Starting manager")
+	return mgr.Start(ctx)
+}
---
+++ pkg/scheduler/controller/add.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"fmt"
+
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	gardenversionedcoreclientset "github.com/gardener/gardener/pkg/client/core/clientset/versioned"
+	"github.com/gardener/gardener/pkg/scheduler/apis/config"
+	shoot "github.com/gardener/gardener/pkg/scheduler/controller/shoot"
+)
+
+// AddControllersToManager adds all scheduler controllers to the given manager.
+func AddControllersToManager(mgr manager.Manager, cfg *config.SchedulerConfiguration) error {
+	versionedGardenerClient, err := gardenversionedcoreclientset.NewForConfig(mgr.GetConfig())
+	if err != nil {
+		return fmt.Errorf("unable to create versioned garden API client: %w", err)
+	}
+
+	if err := (&shoot.Reconciler{
+		Config:                  cfg.Schedulers.Shoot,
+		VersionedGardenerClient: versionedGardenerClient,
+	}).AddToManager(mgr); err != nil {
+		return fmt.Errorf("failed adding Shoot controller: %w", err)
+	}
+
+	return nil
+}
