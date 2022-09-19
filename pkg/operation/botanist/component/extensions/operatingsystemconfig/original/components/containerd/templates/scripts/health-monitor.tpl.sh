9a50edebccec52e43ad6279c9e175d8f78e1094c
Daniel Foehr <daniel.foehr@sap.com>
2022-09-19 13:08:11 +0200

    prevent containerd-monitoring script from killing containerd-shims

--- pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/containerd/templates/scripts/health-monitor.tpl.sh
+++ pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/containerd/templates/scripts/health-monitor.tpl.sh
@@ -3,15 +3,30 @@ set -o nounset
 set -o pipefail
 
 function containerd_monitoring {
-  echo "ContainerD monitor has started !"
+  echo "containerd monitor has started !"
   while [ 1 ]; do
-    if ! timeout 60 ctr c list > /dev/null; then
-      echo "ContainerD daemon failed!"
-      pkill containerd
-      sleep 30
-    else
+    start_timestamp=$(date +%s)
+    until ctr c list > /dev/null; do
+      CONTAINERD_PID=$(systemctl show --property MainPID containerd --value)
+
+      if [ $CONTAINERD_PID -eq 0 ]; then
+          echo "Connection to containerd socket failed (process not started), retrying in $SLEEP_SECONDS seconds..."
+          break
+      fi
+
+      now=$(date +%s)
+      time_elapsed="$(($now-$start_timestamp))"
+
+      if [ $time_elapsed -gt 60 ]; then
+        echo "containerd daemon unreachable for more than 60s. Sending SIGTERM to PID $CONTAINERD_PID"
+        kill -n 15 $CONTAINERD_PID
+        sleep 20
+        break 2
+      fi
+      echo "Connection to containerd socket failed, retrying in $SLEEP_SECONDS seconds..."
       sleep $SLEEP_SECONDS
-    fi
+    done
+    sleep $SLEEP_SECONDS
   done
 }
 
