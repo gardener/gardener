490d3ecc7dcb4ed969a3426a77eeaf9d71ac261d
ialidzhikov <i.alidjikov@gmail.com>
2022-10-07 18:41:48 +0300

    Wait for webhook server to start in `Extension CRDs Webhook` test

--- test/integration/seedadmissioncontroller/extensioncrds/extensioncrds_suite_test.go
+++ test/integration/seedadmissioncontroller/extensioncrds/extensioncrds_suite_test.go
@@ -16,6 +16,7 @@ package extensioncrds_test
 
 import (
 	"context"
+	"net/http"
 	"strings"
 	"testing"
 
@@ -128,6 +129,12 @@ var _ = BeforeSuite(func() {
 		defer GinkgoRecover()
 		Expect(mgr.Start(mgrContext)).To(Succeed())
 	}()
+
+	// Wait for the webhook server to start
+	Eventually(func() error {
+		checker := mgr.GetWebhookServer().StartedChecker()
+		return checker(&http.Request{})
+	}).Should(BeNil())
 
 	DeferCleanup(func() {
 		By("stopping manager")
