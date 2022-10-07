017023657af9fc09853636775c15fe329fd3d4c8
Rafael Franzke <rafael.franzke@sap.com>
2022-10-07 09:07:36 +0200

    Fix nodes CIDR in local `Seed` specification (#6778)
    * Fix node CIDR of local seed
    
    The default node CIDR is `172.18.0.0/16`, however we were usign `172.19.0.0/16` which did not reflect the reality.
    The concrete value depends on the `docker networks` in the respective system, however in "fresh" systems like thw Prow execution pods it's always `172.18.0.0/16`.
    It can be retrieved via `docker network inspect kind | jq -r '.[0].IPAM.Config[0].Subnet'`
    
    * Prevent seed nodes CIDR from being excepted in `allow-to-private` network policy

---
+++ pkg/provider-local/webhook/networkpolicy/mutator.go
@@ -0,0 +1,70 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package networkpolicy
+
+import (
+	"context"
+	"fmt"
+
+	networkingv1 "k8s.io/api/networking/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
+)
+
+type mutator struct {
+	client client.Client
+}
+
+func (m *mutator) InjectClient(client client.Client) error {
+	m.client = client
+	return nil
+}
+
+func (m *mutator) Mutate(ctx context.Context, newObj, _ client.Object) error {
+	if newObj.GetName() != "allow-to-private-networks" {
+		return nil
+	}
+
+	networkPolicy, ok := newObj.(*networkingv1.NetworkPolicy)
+	if !ok {
+		return fmt.Errorf("unexpected object, got %T wanted *networkingv1.NetworkPolicy", newObj)
+	}
+
+	cluster, err := extensionscontroller.GetCluster(ctx, m.client, networkPolicy.Namespace)
+	if err != nil {
+		return err
+	}
+
+	if cluster.Seed.Spec.Networks.Nodes == nil {
+		return nil
+	}
+
+	for i, egress := range networkPolicy.Spec.Egress {
+		for j, to := range egress.To {
+			if to.IPBlock == nil {
+				continue
+			}
+
+			for k, except := range to.IPBlock.Except {
+				if except == *cluster.Seed.Spec.Networks.Nodes {
+					networkPolicy.Spec.Egress[i].To[j].IPBlock.Except = append(networkPolicy.Spec.Egress[i].To[j].IPBlock.Except[:k], networkPolicy.Spec.Egress[i].To[j].IPBlock.Except[k+1:]...)
+				}
+			}
+		}
+	}
+
+	return nil
+}
