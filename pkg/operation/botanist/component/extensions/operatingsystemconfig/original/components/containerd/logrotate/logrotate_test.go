9af9512b287ddb090e2b2b9d6f161c285e9769f8
Niki Dokovski <nickytd@gmail.com>
2022-10-11 11:02:43 +0200

    leverage kublet logrotation in containerd runtime case

--- pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/containerd/logrotate/logrotate_test.go
+++ pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/containerd/logrotate/logrotate_test.go
@@ -16,7 +16,9 @@ package logrotate_test
 
 import (
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	"github.com/gardener/gardener/pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/logrotate"
+	"github.com/gardener/gardener/pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/containerd"
+	"github.com/gardener/gardener/pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/containerd/logrotate"
+	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -25,31 +27,36 @@ import (
 
 var _ = Describe("Logrotate", func() {
 	Describe("#Config", func() {
-		It("should return the expected units and files", func() {
-			var (
-				prefix       = "foo"
-				pathConfig   = "/bar/baz"
-				pathLogFiles = "/var/log/foo/*.log"
-			)
+		Context("containerd container runtime", func() {
+			It("should return the expected units and files in", func() {
+				var (
+					prefix       = containerd.ContainerRuntime
+					pathConfig   = "/bar/baz"
+					pathLogFiles = strings.Builder{}
+				)
 
-			units, files := logrotate.Config(pathConfig, pathLogFiles, prefix)
+				pathLogFiles.WriteString("/var/log/")
+				pathLogFiles.WriteString(containerd.ContainerRuntime)
+				pathLogFiles.WriteString("/*.log")
 
-			Expect(units).To(ConsistOf(
-				extensionsv1alpha1.Unit{
-					Name:   prefix + "-logrotate.service",
-					Enable: pointer.Bool(true),
-					Content: pointer.String(`[Unit]
+				units, files := logrotate.Config(pathConfig, pathLogFiles.String(), prefix)
+
+				Expect(units).To(ConsistOf(
+					extensionsv1alpha1.Unit{
+						Name:   prefix + "-logrotate.service",
+						Enable: pointer.Bool(true),
+						Content: pointer.String(`[Unit]
 Description=Rotate and Compress System Logs
 [Service]
 ExecStart=/usr/sbin/logrotate ` + pathConfig + `
 [Install]
 WantedBy=multi-user.target`),
-				},
-				extensionsv1alpha1.Unit{
-					Name:    prefix + "-logrotate.timer",
-					Command: pointer.String("start"),
-					Enable:  pointer.Bool(true),
-					Content: pointer.String(`[Unit]
+					},
+					extensionsv1alpha1.Unit{
+						Name:    prefix + "-logrotate.timer",
+						Command: pointer.String("start"),
+						Enable:  pointer.Bool(true),
+						Content: pointer.String(`[Unit]
 Description=Log Rotation at each 10 minutes
 [Timer]
 OnCalendar=*:0/10
@@ -57,32 +64,32 @@ AccuracySec=1min
 Persistent=true
 [Install]
 WantedBy=multi-user.target`),
-				},
-			))
+					},
+				))
 
-			Expect(files).To(ConsistOf(
-				extensionsv1alpha1.File{
-					Path:        pathConfig,
-					Permissions: pointer.Int32(0644),
-					Content: extensionsv1alpha1.FileContent{
-						Inline: &extensionsv1alpha1.FileContentInline{
-							Data: pathLogFiles + ` {
+				Expect(files).To(ConsistOf(
+					extensionsv1alpha1.File{
+						Path:        pathConfig,
+						Permissions: pointer.Int32(0644),
+						Content: extensionsv1alpha1.FileContent{
+							Inline: &extensionsv1alpha1.FileContentInline{
+								Data: pathLogFiles.String() + ` {
     rotate 14
-    copytruncate
     missingok
     notifempty
     compress
-    maxsize 100M
     daily
     dateext
     dateformat -%Y%m%d-%s
     create 0644 root root
 }
 `,
+							},
 						},
 					},
-				},
-			))
+				))
+			})
 		})
 	})
+
 })
