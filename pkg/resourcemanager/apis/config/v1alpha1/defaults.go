67cc0e473b94d38ff16d813f0d69062101b5d241
Rafael Franzke <rafael.franzke@sap.com>
2022-10-21 09:47:18 +0200

    API defaulting

---
+++ pkg/resourcemanager/apis/config/v1alpha1/defaults.go
@@ -0,0 +1,207 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	componentbaseconfigv1alpha1 "k8s.io/component-base/config/v1alpha1"
+	"k8s.io/utils/pointer"
+)
+
+func addDefaultingFuncs(scheme *runtime.Scheme) error {
+	return RegisterDefaults(scheme)
+}
+
+// SetDefaults_ResourceManagerConfiguration sets defaults for the configuration of the ResourceManagerConfiguration.
+func SetDefaults_ResourceManagerConfiguration(obj *ResourceManagerConfiguration) {
+	if len(obj.LogLevel) == 0 {
+		obj.LogLevel = "info"
+	}
+	if len(obj.LogFormat) == 0 {
+		obj.LogFormat = "json"
+	}
+}
+
+// SetDefaults_SourceClientConnection sets defaults for the source client connection.
+func SetDefaults_SourceClientConnection(obj *SourceClientConnection) {
+	SetDefaults_ClientConnectionConfiguration(&obj.ClientConnectionConfiguration)
+
+	if obj.Namespace == nil {
+		obj.Namespace = pointer.String("")
+	}
+	if obj.CacheResyncPeriod == nil {
+		obj.CacheResyncPeriod = &metav1.Duration{Duration: 24 * time.Hour}
+	}
+}
+
+// SetDefaults_TargetClientConnection sets defaults for the target client connection.
+func SetDefaults_TargetClientConnection(obj *TargetClientConnection) {
+	SetDefaults_ClientConnectionConfiguration(&obj.ClientConnectionConfiguration)
+
+	if obj.Namespace == nil {
+		obj.Namespace = pointer.String("")
+	}
+	if obj.DisableCachedClient == nil {
+		obj.DisableCachedClient = pointer.Bool(false)
+	}
+	if obj.CacheResyncPeriod == nil {
+		obj.CacheResyncPeriod = &metav1.Duration{Duration: 24 * time.Hour}
+	}
+}
+
+// SetDefaults_ClientConnectionConfiguration sets defaults for the garden client connection.
+func SetDefaults_ClientConnectionConfiguration(obj *componentbaseconfigv1alpha1.ClientConnectionConfiguration) {
+	if obj.QPS == 0.0 {
+		obj.QPS = 100.0
+	}
+	if obj.Burst == 0 {
+		obj.Burst = 130
+	}
+}
+
+// SetDefaults_LeaderElectionConfiguration sets defaults for the leader election of the Gardener controller manager.
+func SetDefaults_LeaderElectionConfiguration(obj *componentbaseconfigv1alpha1.LeaderElectionConfiguration) {
+	if obj.ResourceLock == "" {
+		// Don't use a constant from the client-go resourcelock package here (resourcelock is not an API package, pulls
+		// in some other dependencies and is thereby not suitable to be used in this API package).
+		obj.ResourceLock = "leases"
+	}
+
+	componentbaseconfigv1alpha1.RecommendedDefaultLeaderElectionConfiguration(obj)
+
+	if obj.ResourceName == "" {
+		obj.ResourceName = "gardener-resource-manager"
+	}
+}
+
+// SetDefaults_ServerConfiguration sets defaults for the server configuration.
+func SetDefaults_ServerConfiguration(obj *ServerConfiguration) {
+	if len(obj.Webhooks.BindAddress) == 0 {
+		obj.Webhooks.BindAddress = "0.0.0.0"
+	}
+	if obj.Webhooks.Port == 0 {
+		obj.Webhooks.Port = 9449
+	}
+
+	if obj.HealthProbes == nil {
+		obj.HealthProbes = &Server{}
+	}
+	if obj.HealthProbes.Port == 0 {
+		obj.HealthProbes.Port = 8081
+	}
+
+	if obj.Metrics == nil {
+		obj.Metrics = &Server{}
+	}
+	if obj.Metrics.Port == 0 {
+		obj.Metrics.Port = 8080
+	}
+}
+
+// SetDefaults_ResourceManagerControllerConfiguration sets defaults for the controller configuration.
+func SetDefaults_ResourceManagerControllerConfiguration(obj *ResourceManagerControllerConfiguration) {
+	if obj.ClusterID == nil {
+		obj.ClusterID = pointer.String("")
+	}
+	if obj.ResourceClass == nil {
+		obj.ResourceClass = pointer.String(DefaultResourceClass)
+	}
+}
+
+// SetDefaults_KubeletCSRApproverControllerConfig sets defaults for the KubeletCSRApproverControllerConfig object.
+func SetDefaults_KubeletCSRApproverControllerConfig(obj *KubeletCSRApproverControllerConfig) {
+	if obj.Enabled && obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(1)
+	}
+}
+
+// SetDefaults_GarbageCollectorControllerConfig sets defaults for the GarbageCollectorControllerConfig object.
+func SetDefaults_GarbageCollectorControllerConfig(obj *GarbageCollectorControllerConfig) {
+	if obj.Enabled && obj.SyncPeriod == nil {
+		obj.SyncPeriod = &metav1.Duration{Duration: time.Hour}
+	}
+}
+
+// SetDefaults_HealthControllerConfig sets defaults for the HealthControllerConfig object.
+func SetDefaults_HealthControllerConfig(obj *HealthControllerConfig) {
+	if obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(5)
+	}
+	if obj.SyncPeriod == nil {
+		obj.SyncPeriod = &metav1.Duration{Duration: time.Minute}
+	}
+}
+
+// SetDefaults_ManagedResourceControllerConfig sets defaults for the ManagedResourceControllerConfig object.
+func SetDefaults_ManagedResourceControllerConfig(obj *ManagedResourceControllerConfig) {
+	if obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(5)
+	}
+	if obj.SyncPeriod == nil {
+		obj.SyncPeriod = &metav1.Duration{Duration: time.Minute}
+	}
+	if obj.AlwaysUpdate == nil {
+		obj.AlwaysUpdate = pointer.Bool(false)
+	}
+	if obj.ManagedByLabelValue == nil {
+		obj.ManagedByLabelValue = pointer.String("gardener")
+	}
+}
+
+// SetDefaults_RootCAPublisherControllerConfig sets defaults for the RootCAPublisherControllerConfig object.
+func SetDefaults_RootCAPublisherControllerConfig(obj *RootCAPublisherControllerConfig) {
+	if obj.Enabled && obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(5)
+	}
+}
+
+// SetDefaults_SecretControllerConfig sets defaults for the SecretControllerConfig object.
+func SetDefaults_SecretControllerConfig(obj *SecretControllerConfig) {
+	if obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(5)
+	}
+}
+
+// SetDefaults_TokenInvalidatorControllerConfig sets defaults for the TokenInvalidatorControllerConfig object.
+func SetDefaults_TokenInvalidatorControllerConfig(obj *TokenInvalidatorControllerConfig) {
+	if obj.Enabled && obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(5)
+	}
+}
+
+// SetDefaults_TokenRequestorControllerConfig sets defaults for the TokenRequestorControllerConfig object.
+func SetDefaults_TokenRequestorControllerConfig(obj *TokenRequestorControllerConfig) {
+	if obj.Enabled && obj.ConcurrentSyncs == nil {
+		obj.ConcurrentSyncs = pointer.Int(5)
+	}
+}
+
+// SetDefaults_PodSchedulerNameWebhookConfig sets defaults for the PodSchedulerNameWebhookConfig object.
+func SetDefaults_PodSchedulerNameWebhookConfig(obj *PodSchedulerNameWebhookConfig) {
+	if obj.Enabled && obj.SchedulerName == nil {
+		obj.SchedulerName = pointer.String(corev1.DefaultSchedulerName)
+	}
+}
+
+// SetDefaults_ProjectedTokenMountWebhookConfig sets defaults for the ProjectedTokenMountWebhookConfig object.
+func SetDefaults_ProjectedTokenMountWebhookConfig(obj *ProjectedTokenMountWebhookConfig) {
+	if obj.Enabled && obj.ExpirationSeconds == nil {
+		obj.ExpirationSeconds = pointer.Int64(43200)
+	}
+}
---
+++ pkg/resourcemanager/apis/config/v1alpha1/defaults_test.go
@@ -0,0 +1,505 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	componentbaseconfigv1alpha1 "k8s.io/component-base/config/v1alpha1"
+	"k8s.io/utils/pointer"
+
+	. "github.com/gardener/gardener/pkg/resourcemanager/apis/config/v1alpha1"
+)
+
+var _ = Describe("Defaults", func() {
+	Describe("#SetDefaults_ResourceManagerConfiguration", func() {
+		It("should default the object", func() {
+			obj := &ResourceManagerConfiguration{}
+
+			SetDefaults_ResourceManagerConfiguration(obj)
+
+			Expect(obj.LogLevel).To(Equal("info"))
+			Expect(obj.LogFormat).To(Equal("json"))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &ResourceManagerConfiguration{
+				LogLevel:  "foo",
+				LogFormat: "bar",
+			}
+
+			SetDefaults_ResourceManagerConfiguration(obj)
+
+			Expect(obj.LogLevel).To(Equal("foo"))
+			Expect(obj.LogFormat).To(Equal("bar"))
+		})
+	})
+
+	Describe("#SetDefaults_SourceClientConnection", func() {
+		It("should default the object", func() {
+			obj := &SourceClientConnection{}
+
+			SetDefaults_SourceClientConnection(obj)
+
+			Expect(obj.Namespace).To(PointTo(Equal("")))
+			Expect(obj.CacheResyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: 24 * time.Hour})))
+			Expect(obj.QPS).To(Equal(float32(100.0)))
+			Expect(obj.Burst).To(Equal(int32(130)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &SourceClientConnection{
+				ClientConnectionConfiguration: componentbaseconfigv1alpha1.ClientConnectionConfiguration{
+					QPS:   float32(1.2),
+					Burst: int32(34),
+				},
+				Namespace:         pointer.String("foo"),
+				CacheResyncPeriod: &metav1.Duration{Duration: time.Hour},
+			}
+
+			SetDefaults_SourceClientConnection(obj)
+
+			Expect(obj.Namespace).To(PointTo(Equal("foo")))
+			Expect(obj.CacheResyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Hour})))
+			Expect(obj.QPS).To(Equal(float32(1.2)))
+			Expect(obj.Burst).To(Equal(int32(34)))
+		})
+	})
+
+	Describe("#SetDefaults_TargetClientConnection", func() {
+		It("should default the object", func() {
+			obj := &TargetClientConnection{}
+
+			SetDefaults_TargetClientConnection(obj)
+
+			Expect(obj.Namespace).To(PointTo(Equal("")))
+			Expect(obj.DisableCachedClient).To(PointTo(BeFalse()))
+			Expect(obj.CacheResyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: 24 * time.Hour})))
+			Expect(obj.QPS).To(Equal(float32(100.0)))
+			Expect(obj.Burst).To(Equal(int32(130)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &TargetClientConnection{
+				ClientConnectionConfiguration: componentbaseconfigv1alpha1.ClientConnectionConfiguration{
+					QPS:   float32(1.2),
+					Burst: int32(34),
+				},
+				Namespace:           pointer.String("foo"),
+				DisableCachedClient: pointer.Bool(true),
+				CacheResyncPeriod:   &metav1.Duration{Duration: time.Hour},
+			}
+
+			SetDefaults_TargetClientConnection(obj)
+
+			Expect(obj.Namespace).To(PointTo(Equal("foo")))
+			Expect(obj.DisableCachedClient).To(PointTo(BeTrue()))
+			Expect(obj.CacheResyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Hour})))
+			Expect(obj.QPS).To(Equal(float32(1.2)))
+			Expect(obj.Burst).To(Equal(int32(34)))
+		})
+	})
+
+	Describe("#SetDefaults_LeaderElectionConfiguration", func() {
+		It("should default the object", func() {
+			obj := &componentbaseconfigv1alpha1.LeaderElectionConfiguration{}
+
+			SetDefaults_LeaderElectionConfiguration(obj)
+
+			Expect(obj.ResourceLock).To(Equal("leases"))
+			Expect(obj.ResourceName).To(Equal("gardener-resource-manager"))
+			Expect(obj.ResourceNamespace).To(BeEmpty())
+			Expect(obj.LeaseDuration).To(Equal(metav1.Duration{Duration: 15 * time.Second}))
+			Expect(obj.RenewDeadline).To(Equal(metav1.Duration{Duration: 10 * time.Second}))
+			Expect(obj.RetryPeriod).To(Equal(metav1.Duration{Duration: 2 * time.Second}))
+			Expect(obj.LeaderElect).To(PointTo(BeTrue()))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &componentbaseconfigv1alpha1.LeaderElectionConfiguration{
+				ResourceLock:      "foo",
+				ResourceName:      "bar",
+				ResourceNamespace: "baz",
+				LeaseDuration:     metav1.Duration{Duration: 1 * time.Second},
+				RenewDeadline:     metav1.Duration{Duration: 2 * time.Second},
+				RetryPeriod:       metav1.Duration{Duration: 3 * time.Second},
+				LeaderElect:       pointer.Bool(false),
+			}
+
+			SetDefaults_LeaderElectionConfiguration(obj)
+
+			Expect(obj.ResourceLock).To(Equal("foo"))
+			Expect(obj.ResourceName).To(Equal("bar"))
+			Expect(obj.ResourceNamespace).To(Equal("baz"))
+			Expect(obj.LeaseDuration).To(Equal(metav1.Duration{Duration: 1 * time.Second}))
+			Expect(obj.RenewDeadline).To(Equal(metav1.Duration{Duration: 2 * time.Second}))
+			Expect(obj.RetryPeriod).To(Equal(metav1.Duration{Duration: 3 * time.Second}))
+			Expect(obj.LeaderElect).To(PointTo(BeFalse()))
+		})
+	})
+
+	Describe("#SetDefaults_ServerConfiguration", func() {
+		It("should default the object", func() {
+			obj := &ServerConfiguration{}
+
+			SetDefaults_ServerConfiguration(obj)
+
+			Expect(obj.Webhooks.BindAddress).To(Equal("0.0.0.0"))
+			Expect(obj.Webhooks.Port).To(Equal(9449))
+			Expect(obj.HealthProbes.Port).To(Equal(8081))
+			Expect(obj.Metrics.Port).To(Equal(8080))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &ServerConfiguration{
+				Webhooks: HTTPSServer{
+					Server: Server{
+						BindAddress: "foo",
+						Port:        1,
+					},
+				},
+				HealthProbes: &Server{
+					Port: 2,
+				},
+				Metrics: &Server{
+					Port: 3,
+				},
+			}
+
+			SetDefaults_ServerConfiguration(obj)
+
+			Expect(obj.Webhooks.BindAddress).To(Equal("foo"))
+			Expect(obj.Webhooks.Port).To(Equal(1))
+			Expect(obj.HealthProbes.Port).To(Equal(2))
+			Expect(obj.Metrics.Port).To(Equal(3))
+		})
+	})
+
+	Describe("#SetDefaults_ResourceManagerControllerConfiguration", func() {
+		It("should default the object", func() {
+			obj := &ResourceManagerControllerConfiguration{}
+
+			SetDefaults_ResourceManagerControllerConfiguration(obj)
+
+			Expect(obj.ClusterID).To(PointTo(Equal("")))
+			Expect(obj.ResourceClass).To(PointTo(Equal("resources")))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &ResourceManagerControllerConfiguration{
+				ClusterID:     pointer.String("foo"),
+				ResourceClass: pointer.String("bar"),
+			}
+
+			SetDefaults_ResourceManagerControllerConfiguration(obj)
+
+			Expect(obj.ClusterID).To(PointTo(Equal("foo")))
+			Expect(obj.ResourceClass).To(PointTo(Equal("bar")))
+		})
+	})
+
+	Describe("#SetDefaults_KubeletCSRApproverControllerConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &KubeletCSRApproverControllerConfig{}
+
+			SetDefaults_KubeletCSRApproverControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &KubeletCSRApproverControllerConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_KubeletCSRApproverControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(1)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &KubeletCSRApproverControllerConfig{
+				Enabled:         true,
+				ConcurrentSyncs: pointer.Int(2),
+			}
+
+			SetDefaults_KubeletCSRApproverControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(2)))
+		})
+	})
+
+	Describe("#SetDefaults_GarbageCollectorControllerConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &GarbageCollectorControllerConfig{}
+
+			SetDefaults_GarbageCollectorControllerConfig(obj)
+
+			Expect(obj.SyncPeriod).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &GarbageCollectorControllerConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_GarbageCollectorControllerConfig(obj)
+
+			Expect(obj.SyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Hour})))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &GarbageCollectorControllerConfig{
+				Enabled:    true,
+				SyncPeriod: &metav1.Duration{Duration: time.Second},
+			}
+
+			SetDefaults_GarbageCollectorControllerConfig(obj)
+
+			Expect(obj.SyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Second})))
+		})
+	})
+
+	Describe("#SetDefaults_HealthControllerConfig", func() {
+		It("should not default the object", func() {
+			obj := &HealthControllerConfig{}
+
+			SetDefaults_HealthControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(5)))
+			Expect(obj.SyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Minute})))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &HealthControllerConfig{
+				ConcurrentSyncs: pointer.Int(1),
+				SyncPeriod:      &metav1.Duration{Duration: time.Second},
+			}
+
+			SetDefaults_HealthControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(1)))
+			Expect(obj.SyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Second})))
+		})
+	})
+
+	Describe("#SetDefaults_ManagedResourceControllerConfig", func() {
+		It("should not default the object", func() {
+			obj := &ManagedResourceControllerConfig{}
+
+			SetDefaults_ManagedResourceControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(5)))
+			Expect(obj.SyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Minute})))
+			Expect(obj.AlwaysUpdate).To(PointTo(BeFalse()))
+			Expect(obj.ManagedByLabelValue).To(PointTo(Equal("gardener")))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &ManagedResourceControllerConfig{
+				ConcurrentSyncs:     pointer.Int(1),
+				SyncPeriod:          &metav1.Duration{Duration: time.Second},
+				AlwaysUpdate:        pointer.Bool(true),
+				ManagedByLabelValue: pointer.String("foo"),
+			}
+
+			SetDefaults_ManagedResourceControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(1)))
+			Expect(obj.SyncPeriod).To(PointTo(Equal(metav1.Duration{Duration: time.Second})))
+			Expect(obj.AlwaysUpdate).To(PointTo(BeTrue()))
+			Expect(obj.ManagedByLabelValue).To(PointTo(Equal("foo")))
+		})
+	})
+
+	Describe("#SetDefaults_RootCAPublisherControllerConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &RootCAPublisherControllerConfig{}
+
+			SetDefaults_RootCAPublisherControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &RootCAPublisherControllerConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_RootCAPublisherControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(5)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &RootCAPublisherControllerConfig{
+				Enabled:         true,
+				ConcurrentSyncs: pointer.Int(2),
+			}
+
+			SetDefaults_RootCAPublisherControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(2)))
+		})
+	})
+
+	Describe("#SetDefaults_SecretControllerConfig", func() {
+		It("should not default the object", func() {
+			obj := &SecretControllerConfig{}
+
+			SetDefaults_SecretControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(5)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &SecretControllerConfig{
+				ConcurrentSyncs: pointer.Int(1),
+			}
+
+			SetDefaults_SecretControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(1)))
+		})
+	})
+
+	Describe("#SetDefaults_TokenInvalidatorControllerConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &TokenInvalidatorControllerConfig{}
+
+			SetDefaults_TokenInvalidatorControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &TokenInvalidatorControllerConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_TokenInvalidatorControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(5)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &TokenInvalidatorControllerConfig{
+				Enabled:         true,
+				ConcurrentSyncs: pointer.Int(2),
+			}
+
+			SetDefaults_TokenInvalidatorControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(2)))
+		})
+	})
+
+	Describe("#SetDefaults_TokenRequestorControllerConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &TokenRequestorControllerConfig{}
+
+			SetDefaults_TokenRequestorControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &TokenRequestorControllerConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_TokenRequestorControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(5)))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &TokenRequestorControllerConfig{
+				Enabled:         true,
+				ConcurrentSyncs: pointer.Int(2),
+			}
+
+			SetDefaults_TokenRequestorControllerConfig(obj)
+
+			Expect(obj.ConcurrentSyncs).To(PointTo(Equal(2)))
+		})
+	})
+
+	Describe("#SetDefaults_PodSchedulerNameWebhookConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &PodSchedulerNameWebhookConfig{}
+
+			SetDefaults_PodSchedulerNameWebhookConfig(obj)
+
+			Expect(obj.SchedulerName).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &PodSchedulerNameWebhookConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_PodSchedulerNameWebhookConfig(obj)
+
+			Expect(obj.SchedulerName).To(PointTo(Equal("default-scheduler")))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &PodSchedulerNameWebhookConfig{
+				Enabled:       true,
+				SchedulerName: pointer.String("foo"),
+			}
+
+			SetDefaults_PodSchedulerNameWebhookConfig(obj)
+
+			Expect(obj.SchedulerName).To(PointTo(Equal("foo")))
+		})
+	})
+
+	Describe("#SetDefaults_ProjectedTokenMountWebhookConfig", func() {
+		It("should not default the object because disabled", func() {
+			obj := &ProjectedTokenMountWebhookConfig{}
+
+			SetDefaults_ProjectedTokenMountWebhookConfig(obj)
+
+			Expect(obj.ExpirationSeconds).To(BeNil())
+		})
+
+		It("should default the object because enabled", func() {
+			obj := &ProjectedTokenMountWebhookConfig{
+				Enabled: true,
+			}
+
+			SetDefaults_ProjectedTokenMountWebhookConfig(obj)
+
+			Expect(obj.ExpirationSeconds).To(PointTo(Equal(int64(43200))))
+		})
+
+		It("should not overwrite existing values", func() {
+			obj := &ProjectedTokenMountWebhookConfig{
+				Enabled:           true,
+				ExpirationSeconds: pointer.Int64(1234),
+			}
+
+			SetDefaults_ProjectedTokenMountWebhookConfig(obj)
+
+			Expect(obj.ExpirationSeconds).To(PointTo(Equal(int64(1234))))
+		})
+	})
+})
