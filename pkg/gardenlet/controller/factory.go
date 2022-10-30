44b7460a3c36bfe08d1fbeeb8f1c9bb9550bdea4
Shafeeque E S <shafeeque.e.s@sap.com>
2022-10-30 13:26:46 +0530

    Drop no longer needed files

--- pkg/gardenlet/controller/factory.go
+++ pkg/gardenlet/controller/factory.go
@@ -21,17 +21,14 @@ import (
 
 	"github.com/gardener/gardener/charts"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
-	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/client/kubernetes"
 	"github.com/gardener/gardener/pkg/client/kubernetes/clientmap"
 	"github.com/gardener/gardener/pkg/gardenlet/apis/config"
-	backupentrycontroller "github.com/gardener/gardener/pkg/gardenlet/controller/backupentry"
 	managedseedcontroller "github.com/gardener/gardener/pkg/gardenlet/controller/managedseed"
 	shootcontroller "github.com/gardener/gardener/pkg/gardenlet/controller/shoot"
 	"github.com/gardener/gardener/pkg/utils/imagevector"
 
 	"github.com/go-logr/logr"
-	"k8s.io/utils/clock"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
 )
 
@@ -60,11 +57,6 @@ func (f *LegacyControllerFactory) Start(ctx context.Context) error {
 		return fmt.Errorf("failed reading image vector override: %w", err)
 	}
 
-	backupEntryController, err := backupentrycontroller.NewBackupEntryController(ctx, log, f.GardenCluster, f.SeedCluster, f.Config, clock.RealClock{}, v1beta1constants.GardenNamespace)
-	if err != nil {
-		return fmt.Errorf("failed initializing BackupEntry controller: %w", err)
-	}
-
 	managedSeedController, err := managedseedcontroller.NewManagedSeedController(ctx, log, f.GardenCluster, f.SeedCluster, f.ShootClientMap, f.Config, imageVector)
 	if err != nil {
 		return fmt.Errorf("failed initializing ManagedSeed controller: %w", err)
@@ -78,7 +70,6 @@ func (f *LegacyControllerFactory) Start(ctx context.Context) error {
 	controllerCtx, cancel := context.WithCancel(ctx)
 
 	// run controllers
-	go backupEntryController.Run(controllerCtx, *f.Config.Controllers.BackupEntry.ConcurrentSyncs, *f.Config.Controllers.BackupEntryMigration.ConcurrentSyncs)
 	go managedSeedController.Run(controllerCtx, *f.Config.Controllers.ManagedSeed.ConcurrentSyncs)
 	go shootController.Run(controllerCtx, *f.Config.Controllers.Shoot.ConcurrentSyncs, *f.Config.Controllers.ShootCare.ConcurrentSyncs, *f.Config.Controllers.ShootMigration.ConcurrentSyncs)
 
