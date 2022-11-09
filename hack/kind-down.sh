562e7a04c46ae7b09f6de7d643818de3cbd8a64b
Rafael Franzke <rafael.franzke@sap.com>
2022-11-09 14:37:12 +0100

    `hack` scripts for `kind-{up,down}`

---
+++ hack/kind-down.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CLUSTER_NAME=""
+PATH_KUBECONFIG=""
+KEEP_BACKUPBUCKETS_DIRECTORY=false
+
+parse_flags() {
+  while test $# -gt 0; do
+    case "$1" in
+    --cluster-name)
+      shift; CLUSTER_NAME="$1"
+      ;;
+    --path-kubeconfig)
+      shift; PATH_KUBECONFIG="$1"
+      ;;
+    --keep-backupbuckets-dir)
+      KEEP_BACKUPBUCKETS_DIRECTORY=false
+      ;;
+    esac
+
+    shift
+  done
+}
+
+parse_flags "$@"
+
+kind delete cluster \
+  --name "$CLUSTER_NAME"
+
+rm -f  "$PATH_KUBECONFIG"
+
+if [[ "$KEEP_BACKUPBUCKETS_DIRECTORY" == "false" ]]; then
+  rm -rf "$(dirname "$0")/../dev/local-backupbuckets"
+fi
