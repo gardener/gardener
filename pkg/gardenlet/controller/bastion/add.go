097d2033b7a84ef8c307a62d57de856e5276ddad
Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>
2022-11-10 10:29:19 +0530

    Use ExtensionStatusChanged predicate defined in predicate utils

--- pkg/gardenlet/controller/bastion/add.go
+++ pkg/gardenlet/controller/bastion/add.go
@@ -16,25 +16,21 @@ package bastion
 
 import (
 	"context"
-	"reflect"
 	"strings"
 
-	"github.com/gardener/gardener/pkg/api/extensions"
-	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
-	gardencorev1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	operationsv1alpha1 "github.com/gardener/gardener/pkg/apis/operations/v1alpha1"
 	"github.com/gardener/gardener/pkg/controllerutils/mapper"
+	predicateutils "github.com/gardener/gardener/pkg/controllerutils/predicate"
 	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
-	"github.com/go-logr/logr"
 
+	"github.com/go-logr/logr"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -81,7 +77,7 @@ func (r *Reconciler) AddToManager(mgr manager.Manager, gardenCluster, seedCluste
 	return c.Watch(
 		source.NewKindWithCache(&extensionsv1alpha1.Bastion{}, seedCluster.GetCache()),
 		mapper.EnqueueRequestsFrom(mapper.MapFunc(r.MapExtensionsBastionToOperationsBastion), mapper.UpdateWithNew, c.GetLogger()),
-		r.ExtensionsBastionPredicate(),
+		predicateutils.ExtensionStatusChanged(),
 	)
 }
 
@@ -103,87 +99,6 @@ func (r *Reconciler) MapExtensionsBastionToOperationsBastion(ctx context.Context
 	}
 
 	return []reconcile.Request{{NamespacedName: types.NamespacedName{Namespace: operationsBastion.Namespace, Name: operationsBastion.Name}}}
-}
-
-// ExtensionsBastionPredicate returns predicate for extensions Bastion.
-// It evaluates to false for `Delete` event and evaluates to true
-// * for `Create` and `Update` when `status.lastError` field is not nil
-// * for `Update` event when there is changes in status
-func (r *Reconciler) ExtensionsBastionPredicate() predicate.Predicate {
-	return predicate.Funcs{
-		CreateFunc: func(e event.CreateEvent) bool {
-			// If the object has the operation annotation, this means it's not picked up by the extension controller.
-			if gardencorev1beta1helper.HasOperationAnnotation(e.Object.GetAnnotations()) {
-				return false
-			}
-
-			// If lastOperation State is failed then we admit reconciliation.
-			// This is not possible during create but possible during a controller restart.
-			if lastOperationStateFailed(e.Object) {
-				return true
-			}
-
-			return false
-		},
-
-		UpdateFunc: func(e event.UpdateEvent) bool {
-			// If the object has the operation annotation, this means it's not picked up by the extension controller.
-			if gardencorev1beta1helper.HasOperationAnnotation(e.ObjectNew.GetAnnotations()) {
-				return false
-			}
-
-			// If lastOperation State has changed to Succeeded or Error then we admit reconciliation.
-			if lastOperationStateChanged(e.ObjectOld, e.ObjectNew) {
-				return true
-			}
-
-			return false
-		},
-
-		DeleteFunc:  func(event.DeleteEvent) bool { return false },
-		GenericFunc: func(event.GenericEvent) bool { return false },
-	}
-}
-
-func lastOperationStateFailed(obj client.Object) bool {
-	acc, err := extensions.Accessor(obj)
-	if err != nil {
-		return false
-	}
-
-	if acc.GetExtensionStatus().GetLastOperation() == nil {
-		return false
-	}
-
-	return acc.GetExtensionStatus().GetLastOperation().State == gardencorev1beta1.LastOperationStateFailed
-}
-
-func lastOperationStateChanged(oldObj, newObj client.Object) bool {
-	newAcc, err := extensions.Accessor(newObj)
-	if err != nil {
-		return false
-	}
-
-	oldAcc, err := extensions.Accessor(oldObj)
-	if err != nil {
-		return false
-	}
-
-	if newAcc.GetExtensionStatus().GetLastOperation() == nil {
-		return false
-	}
-
-	lastOperationState := newAcc.GetExtensionStatus().GetLastOperation().State
-	newLastOperationStateSucceededOrErroneous := lastOperationState == gardencorev1beta1.LastOperationStateSucceeded || lastOperationState == gardencorev1beta1.LastOperationStateError || lastOperationState == gardencorev1beta1.LastOperationStateFailed
-
-	if newLastOperationStateSucceededOrErroneous {
-		if oldAcc.GetExtensionStatus().GetLastOperation() != nil {
-			return !reflect.DeepEqual(oldAcc.GetExtensionStatus().GetLastOperation(), newAcc.GetExtensionStatus().GetLastOperation())
-		}
-		return true
-	}
-
-	return false
 }
 
 // GetProjectNameFromTechincalId returns Shoot resource name from its UID.
