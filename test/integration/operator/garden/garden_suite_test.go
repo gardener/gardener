ab160b7ec14d1daab5887432df3a9e798328cbb5
Rafael Franzke <rafael.franzke@sap.com>
2022-11-12 10:42:59 +0100

    Integration test

---
+++ test/integration/operator/garden/garden_suite_test.go
@@ -0,0 +1,157 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package garden_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/rest"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/gardener/gardener/charts"
+	operatorv1alpha1 "github.com/gardener/gardener/pkg/apis/operator/v1alpha1"
+	"github.com/gardener/gardener/pkg/logger"
+	"github.com/gardener/gardener/pkg/operator/apis/config"
+	operatorclient "github.com/gardener/gardener/pkg/operator/client"
+	"github.com/gardener/gardener/pkg/operator/controller/garden"
+	"github.com/gardener/gardener/pkg/operator/features"
+	"github.com/gardener/gardener/pkg/utils/imagevector"
+	. "github.com/gardener/gardener/pkg/utils/test/matchers"
+)
+
+func TestGarden(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Garden Controller Integration Test Suite")
+}
+
+const testID = "garden-controller-test"
+
+var (
+	ctx = context.Background()
+	log logr.Logger
+
+	restConfig *rest.Config
+	testEnv    *envtest.Environment
+	testClient client.Client
+	mgrClient  client.Client
+
+	testRunID     string
+	testNamespace *corev1.Namespace
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(logger.MustNewZapLogger(logger.DebugLevel, logger.FormatJSON, zap.WriteTo(GinkgoWriter)))
+	log = logf.Log.WithName(testID)
+
+	features.RegisterFeatureGates()
+
+	By("starting test environment")
+	testEnv = &envtest.Environment{
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			Paths: []string{filepath.Join("..", "..", "..", "..", "example", "operator", "10-crd-operator.gardener.cloud_gardens.yaml")},
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	restConfig, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(restConfig).NotTo(BeNil())
+
+	DeferCleanup(func() {
+		By("stopping test environment")
+		Expect(testEnv.Stop()).To(Succeed())
+	})
+
+	By("creating test client")
+	testClient, err = client.New(restConfig, client.Options{Scheme: operatorclient.RuntimeScheme})
+	Expect(err).NotTo(HaveOccurred())
+
+	By("creating test namespace for test")
+	testNamespace = &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "garden-",
+		},
+	}
+	Expect(testClient.Create(ctx, testNamespace)).To(Succeed())
+	log.Info("Created Namespace for test", "namespaceName", testNamespace.Name)
+	testRunID = testNamespace.Name
+
+	DeferCleanup(func() {
+		By("deleting test namespace")
+		Expect(testClient.Delete(ctx, testNamespace)).To(Or(Succeed(), BeNotFoundError()))
+	})
+
+	By("setup manager")
+	mgr, err := manager.New(restConfig, manager.Options{
+		Scheme:             operatorclient.RuntimeScheme,
+		MetricsBindAddress: "0",
+		NewCache: cache.BuilderWithOptions(cache.Options{
+			SelectorsByObject: map[client.Object]cache.ObjectSelector{
+				&operatorv1alpha1.Garden{}: {
+					Label: labels.SelectorFromSet(labels.Set{testID: testRunID}),
+				},
+			},
+		}),
+	})
+	Expect(err).NotTo(HaveOccurred())
+	mgrClient = mgr.GetClient()
+
+	By("registering controller")
+	chartsPath := filepath.Join("..", "..", "..", "..", charts.Path)
+	imageVector, err := imagevector.ReadGlobalImageVectorWithEnvOverride(filepath.Join(chartsPath, "images.yaml"))
+	Expect(err).NotTo(HaveOccurred())
+
+	Expect((&garden.Reconciler{
+		Config: config.OperatorConfiguration{
+			Controllers: config.ControllerConfiguration{
+				Garden: config.GardenControllerConfig{
+					ConcurrentSyncs: pointer.Int(5),
+					SyncPeriod:      &metav1.Duration{Duration: time.Minute},
+				},
+			},
+		},
+		ImageVector:     imageVector,
+		GardenNamespace: testNamespace.Name,
+	}).AddToManager(mgr)).To(Succeed())
+
+	By("starting manager")
+	mgrContext, mgrCancel := context.WithCancel(ctx)
+
+	go func() {
+		defer GinkgoRecover()
+		Expect(mgr.Start(mgrContext)).To(Succeed())
+	}()
+
+	DeferCleanup(func() {
+		By("stopping manager")
+		mgrCancel()
+	})
+})
---
+++ test/integration/operator/garden/garden_test.go
@@ -0,0 +1,169 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package garden_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	operatorv1alpha1 "github.com/gardener/gardener/pkg/apis/operator/v1alpha1"
+	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
+	"github.com/gardener/gardener/pkg/controllerutils"
+	secretutils "github.com/gardener/gardener/pkg/utils/secrets"
+	"github.com/gardener/gardener/pkg/utils/test"
+	. "github.com/gardener/gardener/pkg/utils/test/matchers"
+)
+
+var _ = Describe("Garden controller tests", func() {
+	var garden *operatorv1alpha1.Garden
+
+	BeforeEach(func() {
+		DeferCleanup(test.WithVar(&secretutils.GenerateKey, secretutils.FakeGenerateKey))
+
+		garden = &operatorv1alpha1.Garden{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   "garden-" + testRunID,
+				Labels: map[string]string{testID: testRunID},
+			},
+			Spec: operatorv1alpha1.GardenSpec{
+				RuntimeCluster: operatorv1alpha1.RuntimeCluster{
+					Provider: operatorv1alpha1.Provider{
+						Zones: []string{"a", "b", "c"},
+					},
+					Settings: &operatorv1alpha1.Settings{
+						VerticalPodAutoscaler: &operatorv1alpha1.SettingVerticalPodAutoscaler{
+							Enabled: pointer.Bool(true),
+						},
+					},
+				},
+			},
+		}
+
+		By("Create Garden")
+		Expect(testClient.Create(ctx, garden)).To(Succeed())
+		log.Info("Created Garden for test", "garden", garden.Name)
+
+		DeferCleanup(func() {
+			By("Delete Garden")
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, garden))).To(Succeed())
+
+			By("Forcefully remove finalizers")
+			Expect(client.IgnoreNotFound(controllerutils.RemoveAllFinalizers(ctx, testClient, garden))).To(Succeed())
+
+			By("Ensure Garden is gone")
+			Eventually(func() error {
+				return mgrClient.Get(ctx, client.ObjectKeyFromObject(garden), garden)
+			}).Should(BeNotFoundError())
+		})
+	})
+
+	It("should properly maintain the Reconciled condition", func() {
+		By("Wait for Garden to have finalizer")
+		Eventually(func(g Gomega) []string {
+			g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(garden), garden)).To(Succeed())
+			return garden.Finalizers
+		}).Should(ConsistOf("gardener.cloud/operator"))
+
+		By("Wait for Reconciled condition to be set to Progressing")
+		Eventually(func(g Gomega) []gardencorev1beta1.Condition {
+			g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(garden), garden)).To(Succeed())
+			return garden.Status.Conditions
+		}).Should(ContainCondition(
+			OfType(operatorv1alpha1.GardenReconciled),
+			WithStatus(gardencorev1beta1.ConditionProgressing),
+		))
+
+		By("Verify that CA secret was generated")
+		Eventually(func(g Gomega) []corev1.Secret {
+			secretList := &corev1.SecretList{}
+			g.Expect(testClient.List(ctx, secretList, client.InNamespace(testNamespace.Name), client.MatchingLabels{"name": "ca-garden-runtime", "managed-by": "secrets-manager", "manager-identity": "gardener-operator"})).To(Succeed())
+			return secretList.Items
+		}).Should(HaveLen(1))
+
+		By("Verify that garden namespace was labeled and annotated appropriately")
+		Eventually(func(g Gomega) {
+			g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(testNamespace), testNamespace)).To(Succeed())
+			g.Expect(testNamespace.Labels).To(HaveKeyWithValue("high-availability-config.resources.gardener.cloud/consider", "true"))
+			g.Expect(testNamespace.Annotations).To(HaveKeyWithValue("high-availability-config.resources.gardener.cloud/zones", "a,b,c"))
+		}).Should(Succeed())
+
+		// The garden controller waits for the gardener-resource-manager Deployment to be healthy, so let's fake this here.
+		By("Patch gardener-resource-manager deployment to report healthiness")
+		Eventually(func(g Gomega) {
+			deployment := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "gardener-resource-manager", Namespace: testNamespace.Name}}
+			g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(deployment), deployment)).To(Succeed())
+
+			patch := client.MergeFrom(deployment.DeepCopy())
+			deployment.Status.ObservedGeneration = deployment.Generation
+			deployment.Status.Conditions = []appsv1.DeploymentCondition{{Type: appsv1.DeploymentAvailable, Status: corev1.ConditionTrue}}
+			g.Expect(testClient.Status().Patch(ctx, deployment, patch)).To(Succeed())
+		}).Should(Succeed())
+
+		// The gardener-resource-manager is not really running in this test scenario, hence there is nothing to serve
+		// the webhook endpoint. However, the envtest kube-apiserver would try to reach it, so let's better delete it
+		// here for the sake of this test.
+		By("Delete gardener-resource-manager webhook")
+		Eventually(func(g Gomega) {
+			mutatingWebhookConfiguration := &admissionregistrationv1.MutatingWebhookConfiguration{ObjectMeta: metav1.ObjectMeta{Name: "gardener-resource-manager"}}
+			g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(mutatingWebhookConfiguration), mutatingWebhookConfiguration)).To(Succeed())
+			g.Expect(testClient.Delete(ctx, mutatingWebhookConfiguration)).To(Succeed())
+		}).Should(Succeed())
+
+		By("Verify that the garden system components have been deployed")
+		Eventually(func(g Gomega) []resourcesv1alpha1.ManagedResource {
+			managedResourceList := &resourcesv1alpha1.ManagedResourceList{}
+			g.Expect(testClient.List(ctx, managedResourceList, client.InNamespace(testNamespace.Name))).To(Succeed())
+			return managedResourceList.Items
+		}).Should(ConsistOf(
+			MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("garden-system")})}),
+			MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("vpa")})}),
+		))
+
+		By("Wait for Reconciled condition to be set to True")
+		Eventually(func(g Gomega) []gardencorev1beta1.Condition {
+			g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(garden), garden)).To(Succeed())
+			return garden.Status.Conditions
+		}).Should(ContainCondition(OfType(operatorv1alpha1.GardenReconciled), WithStatus(gardencorev1beta1.ConditionTrue)))
+
+		By("Delete Garden")
+		Expect(testClient.Delete(ctx, garden)).To(Succeed())
+
+		By("Verify that the garden system components have been deleted")
+		// When the controller succeeds then it deletes the `ManagedResource` CRD, so we only need to ensure here that
+		// the `ManagedResource` API is no longer available.
+		Eventually(func(g Gomega) error {
+			return testClient.List(ctx, &resourcesv1alpha1.ManagedResourceList{}, client.InNamespace(testNamespace.Name))
+		}).Should(BeNotFoundError())
+
+		By("Verify that gardener-resource-manager has been deleted")
+		Eventually(func(g Gomega) error {
+			deployment := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "gardener-resource-manager", Namespace: testNamespace.Name}}
+			return testClient.Get(ctx, client.ObjectKeyFromObject(deployment), deployment)
+		}).Should(BeNotFoundError())
+
+		By("Ensure Garden is gone")
+		Eventually(func() error {
+			return testClient.Get(ctx, client.ObjectKeyFromObject(garden), garden)
+		}).Should(BeNotFoundError())
+	})
+})
