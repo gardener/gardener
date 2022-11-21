dfab6a7590b33c735f9cf690c79fa812a51f1fce
Shafeeque E S <shafeeque.e.s@sap.com>
2022-11-21 15:06:20 +0530

    Deflake `Project` controller integration tests (#7043)
    * Deflake Project activity controller integration tests
    
    * Deflake Project controller integration tests
    
    * Address PR review feedback
    
    * Address PR review feedback

--- test/integration/controllermanager/project/activity/activity_suite_test.go
+++ test/integration/controllermanager/project/activity/activity_suite_test.go
@@ -59,6 +59,7 @@ var (
 	restConfig *rest.Config
 	testEnv    *gardenerenvtest.GardenerTestEnvironment
 	testClient client.Client
+	mgrClient  client.Client
 
 	testNamespace *corev1.Namespace
 	testRunID     string
@@ -120,6 +121,7 @@ var _ = BeforeSuite(func() {
 		}),
 	})
 	Expect(err).NotTo(HaveOccurred())
+	mgrClient = mgr.GetClient()
 
 	By("setting up field indexes")
 	Expect(indexer.AddProjectNamespace(ctx, mgr.GetFieldIndexer())).To(Succeed())
--- test/integration/controllermanager/project/activity/activity_test.go
+++ test/integration/controllermanager/project/activity/activity_test.go
@@ -30,7 +30,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ = Describe("Project Activity controller tests", Ordered, func() {
+var _ = Describe("Project Activity controller tests", func() {
 	var project *gardencorev1beta1.Project
 
 	BeforeEach(func() {
@@ -85,6 +85,11 @@ var _ = Describe("Project Activity controller tests", Ordered, func() {
 			Expect(testClient.Create(ctx, obj)).To(Succeed())
 			log.Info("Created object", "kind", kind, "object", client.ObjectKeyFromObject(obj))
 
+			By("Wait until manager has observed " + kind + " creation")
+			Eventually(func() error {
+				return mgrClient.Get(ctx, client.ObjectKeyFromObject(obj), obj)
+			}).Should(Succeed())
+
 			By("Ensure lastActivityTimestamp was updated after object creation")
 			lastActivityTimestamp = assertLastActivityTimestampUpdated(ctx, project, lastActivityTimestamp)
 
@@ -95,6 +100,13 @@ var _ = Describe("Project Activity controller tests", Ordered, func() {
 			Expect(testClient.Update(ctx, obj)).To(Succeed())
 			log.Info("Updated object", "kind", kind, "object", client.ObjectKeyFromObject(obj))
 
+			By("Wait until manager has observed updated" + kind)
+			objResourceVersion := obj.GetResourceVersion()
+			Eventually(func(g Gomega) string {
+				g.Expect(mgrClient.Get(ctx, client.ObjectKeyFromObject(obj), obj)).To(Succeed())
+				return obj.GetResourceVersion()
+			}).Should(Equal(objResourceVersion))
+
 			By("Ensure lastActivityTimestamp was updated after object update")
 			lastActivityTimestamp = assertLastActivityTimestampUpdated(ctx, project, lastActivityTimestamp)
 
@@ -103,6 +115,11 @@ var _ = Describe("Project Activity controller tests", Ordered, func() {
 			By("Delete " + kind)
 			Expect(testClient.Delete(ctx, obj)).To(Succeed())
 			log.Info("Deleted object", "kind", kind, "object", client.ObjectKeyFromObject(obj))
+
+			By("Wait until manager has observed " + kind + " deletion")
+			Eventually(func() error {
+				return mgrClient.Get(ctx, client.ObjectKeyFromObject(obj), obj)
+			}).Should(BeNotFoundError())
 
 			By("Ensure lastActivityTimestamp was updated after object deletion")
 			_ = assertLastActivityTimestampUpdated(ctx, project, lastActivityTimestamp)
@@ -116,6 +133,11 @@ var _ = Describe("Project Activity controller tests", Ordered, func() {
 			By("Create " + kind)
 			Expect(testClient.Create(ctx, obj)).To(Succeed())
 			log.Info("Created object", "kind", kind, "object", client.ObjectKeyFromObject(obj))
+
+			By("Wait until manager has observed " + kind)
+			Eventually(func() error {
+				return mgrClient.Get(ctx, client.ObjectKeyFromObject(obj), obj)
+			}).Should(Succeed())
 
 			By("Ensure lastActivityTimestamp was not updated")
 			assertLastActivityTimestampUnchanged(ctx, project)
