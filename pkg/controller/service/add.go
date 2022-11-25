acbe9f2056777ef10223bad6a7c2b9653205ae2f
Rafael Franzke <rafael.franzke@sap.com>
2022-11-25 15:59:26 +0100

    Expose virtual garden locally
    In the local setup case, the `provider-local`'s `Service` controller is added to the `gardener-operator`'s controller-runtime manager.
    This way, the code for reconciling the `Service` resources can be shared/reused.

---
+++ pkg/controller/service/add.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+// ControllerName is the name of this controller.
+const ControllerName = "service"
+
+// AddToManager adds Reconciler to the given manager.
+func (r *Reconciler) AddToManager(mgr manager.Manager, predicates ...predicate.Predicate) error {
+	if r.Client == nil {
+		r.Client = mgr.GetClient()
+	}
+	if r.HostIP == "" {
+		r.HostIP = "127.0.0.1"
+	}
+
+	return builder.
+		ControllerManagedBy(mgr).
+		Named(ControllerName).
+		For(&corev1.Service{}, builder.WithPredicates(predicates...)).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: 5,
+			RecoverPanic:            true,
+		}).
+		Complete(r)
+}
