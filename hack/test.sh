e68aeddaee41c223b868034d573f6afd239e2d17
Tim Usner <tim.usner@sap.com>
2022-11-29 14:45:04 +0100

    Use Ginkgo timeout in Prow

--- hack/test.sh
+++ hack/test.sh
@@ -21,7 +21,7 @@ set -o pipefail
 echo "> Test"
 
 test_flags=
-# If running in prow, we want to generate a machine-readable output file under the location specified via $ARTIFACTS.
+# If running in Prow, we want to generate a machine-readable output file under the location specified via $ARTIFACTS.
 # This will add a JUnit view above the build log that shows an overview over successful and failed test cases.
 if [ -n "${CI:-}" -a -n "${ARTIFACTS:-}" ] ; then
   if which report-collector &>/dev/null; then
@@ -31,6 +31,11 @@ if [ -n "${CI:-}" -a -n "${ARTIFACTS:-}" ] ; then
   else
     echo "report-collector not found in PATH, not generating machine-readable test report"
   fi
+  # Use Ginkgo timeout in Prow to print everything that is buffered in GinkgoWriter.
+  test_flags+=" --ginkgo.timeout=2m"
+else
+  # We don't want Ginkgo's timeout flag locally because it causes skipping the test cache.
+  timeout_flag=-timeout=2m
 fi
 
-GO111MODULE=on go test -race -timeout=2m -mod=vendor $@ $test_flags | grep -v 'no test files'
+GO111MODULE=on go test -race ${timeout_flag:-} -mod=vendor $@ $test_flags | grep -v 'no test files'
