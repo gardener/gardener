10043da33c3b4318c4b627747b5ee81795abee5b
Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>
2022-11-30 11:28:10 +0530

    Move EnqueueWithJitterDelay function to controllerutils
    - enqueue with jitter delay logic from managed seed controller is moved to controllerutils.
    - in the later commit jitter delay logic is removed from controller and instead EnqueueWithJitterDelay function is used.

--- pkg/controllerutils/eventhandler.go
+++ pkg/controllerutils/eventhandler.go
@@ -17,6 +17,7 @@ package controllerutils
 import (
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/utils/clock"
@@ -26,7 +27,17 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	seedmanagementv1alpha1 "github.com/gardener/gardener/pkg/apis/seedmanagement/v1alpha1"
+	"github.com/gardener/gardener/pkg/gardenlet/apis/config"
+	"github.com/gardener/gardener/pkg/utils"
 )
+
+var reconcileRequest = func(obj client.Object) reconcile.Request {
+	return reconcile.Request{NamespacedName: types.NamespacedName{
+		Name:      obj.GetName(),
+		Namespace: obj.GetNamespace(),
+	}}
+}
 
 // EnqueueCreateEventsOncePer24hDuration returns handler.Funcs which enqueues the object for Create events only once per 24h.
 // All other events are normally enqueued.
@@ -36,38 +47,96 @@ func EnqueueCreateEventsOncePer24hDuration(clock clock.Clock) handler.Funcs {
 			if evt.Object == nil {
 				return
 			}
-			q.AddAfter(reconcile.Request{NamespacedName: types.NamespacedName{
-				Name:      evt.Object.GetName(),
-				Namespace: evt.Object.GetNamespace(),
-			}}, getDuration(evt.Object, clock))
+			q.AddAfter(reconcileRequest(evt.Object), getDuration(evt.Object, clock, nil))
 		},
 		UpdateFunc: func(evt event.UpdateEvent, q workqueue.RateLimitingInterface) {
 			if evt.ObjectNew == nil {
 				return
 			}
-			q.Add(reconcile.Request{NamespacedName: types.NamespacedName{
-				Name:      evt.ObjectNew.GetName(),
-				Namespace: evt.ObjectNew.GetNamespace(),
-			}})
+			q.Add(reconcileRequest(evt.ObjectNew))
 		},
 		DeleteFunc: func(evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
 			if evt.Object == nil {
 				return
 			}
-			q.Add(reconcile.Request{NamespacedName: types.NamespacedName{
-				Name:      evt.Object.GetName(),
-				Namespace: evt.Object.GetNamespace(),
-			}})
+			q.Add(reconcileRequest(evt.Object))
 		},
 	}
 }
 
-func getDuration(obj client.Object, clock clock.Clock) time.Duration {
+// EnqueueWithJitterDelay returns handler.Funcs which enqueues the object with a random Jitter duration when the JitterUpdate
+// is enabled in ManagedSeed controller configuration.
+// All other events are normally enqueued.
+func EnqueueWithJitterDelay(cfg config.ManagedSeedControllerConfiguration) handler.Funcs {
+	return handler.Funcs{
+		CreateFunc: func(evt event.CreateEvent, q workqueue.RateLimitingInterface) {
+			managedSeed, ok := evt.Object.(*seedmanagementv1alpha1.ManagedSeed)
+			if !ok {
+				return
+			}
+
+			generationChanged := managedSeed.Generation != managedSeed.Status.ObservedGeneration
+
+			// Managed seed with deletion timestamp and newly created managed seed will be enqueued immediately.
+			// Generation is 1 for newly created objects.
+			if managedSeed.DeletionTimestamp != nil || managedSeed.Generation == 1 {
+				q.Add(reconcileRequest(evt.Object))
+				return
+			}
+
+			if generationChanged {
+				if *cfg.JitterUpdates {
+					q.AddAfter(reconcileRequest(evt.Object), getDuration(evt.Object, nil, cfg.SyncJitterPeriod))
+				} else {
+					q.Add(reconcileRequest(evt.Object))
+				}
+			} else {
+				// Spread reconciliation of managed seeds (including gardenlet updates/rollouts) across the configured sync jitter
+				// period to avoid overloading the gardener-apiserver if all gardenlets in all managed seeds are (re)starting
+				// roughly at the same time
+				q.AddAfter(reconcileRequest(evt.Object), getDuration(evt.Object, nil, cfg.SyncJitterPeriod))
+			}
+		},
+		UpdateFunc: func(evt event.UpdateEvent, q workqueue.RateLimitingInterface) {
+			managedSeed, ok := evt.ObjectNew.(*seedmanagementv1alpha1.ManagedSeed)
+			if !ok {
+				return
+			}
+
+			if managedSeed.Generation == managedSeed.Status.ObservedGeneration {
+				return
+			}
+
+			// Managed seed with deletion timestamp and newly created managed seed will be enqueued immediately.
+			// Generation is 1 for newly created objects.
+			if managedSeed.DeletionTimestamp != nil || managedSeed.Generation == 1 {
+				q.Add(reconcileRequest(evt.ObjectNew))
+				return
+			}
+
+			if *cfg.JitterUpdates {
+				q.AddAfter(reconcileRequest(evt.ObjectNew), getDuration(evt.ObjectNew, nil, cfg.SyncJitterPeriod))
+			} else {
+				q.Add(reconcileRequest(evt.ObjectNew))
+			}
+		},
+		DeleteFunc: func(evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
+			if evt.Object == nil {
+				return
+			}
+			q.Add(reconcileRequest(evt.Object))
+		},
+	}
+}
+
+func getDuration(obj client.Object, clock clock.Clock, syncJitterPeriod *metav1.Duration) time.Duration {
 	switch obj := obj.(type) {
 	case *gardencorev1beta1.BackupBucket:
 		return ReconcileOncePer24hDuration(clock, obj.ObjectMeta, obj.Status.ObservedGeneration, obj.Status.LastOperation)
 	case *gardencorev1beta1.BackupEntry:
 		return ReconcileOncePer24hDuration(clock, obj.ObjectMeta, obj.Status.ObservedGeneration, obj.Status.LastOperation)
+	case *seedmanagementv1alpha1.ManagedSeed:
+		return utils.RandomDurationWithMetaDuration(syncJitterPeriod)
 	}
 	return 0
 }
