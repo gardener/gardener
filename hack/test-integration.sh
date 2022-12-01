2568c9ad5286b3398ba285b0c540ef068e9210e7
Tim Usner <tim.usner@sap.com>
2022-12-01 19:14:37 +0100

    Use Ginkgo timeout in Prow (#7101)

--- hack/test-integration.sh
+++ hack/test-integration.sh
@@ -34,12 +34,17 @@ echo "> Integration Tests"
 source "$(dirname "$0")/test-integration.env"
 
 test_flags=
-# If running in prow, we want to generate a machine-readable output file under the location specified via $ARTIFACTS.
+# If running in Prow, we want to generate a machine-readable output file under the location specified via $ARTIFACTS.
 # This will add a JUnit view above the build log that shows an overview over successful and failed test cases.
 if [ -n "${CI:-}" -a -n "${ARTIFACTS:-}" ] ; then
   mkdir -p "$ARTIFACTS"
   trap "report-collector \"$ARTIFACTS/junit.xml\"" EXIT
   test_flags="--ginkgo.junit-report=junit.xml"
+  # Use Ginkgo timeout in Prow to print everything that is buffered in GinkgoWriter.
+  test_flags+=" --ginkgo.timeout=5m"
+else
+  # We don't want Ginkgo's timeout flag locally because it causes skipping the test cache.
+  timeout_flag=-timeout=5m
 fi
 
-GO111MODULE=on go test -timeout=5m -mod=vendor $@ $test_flags | grep -v 'no test files'
+GO111MODULE=on go test ${timeout_flag:-} -mod=vendor $@ $test_flags | grep -v 'no test files'
