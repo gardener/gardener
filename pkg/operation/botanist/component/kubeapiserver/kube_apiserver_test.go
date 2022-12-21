313aa33368e69a60dc105c4a24992212afec8001
Plamen Kokanov <plamen.kokanov@sap.com>
2022-12-21 10:42:05 +0200

    Adds --shutdown-send-retry-after=true for kube-apiserver v1.24

--- pkg/operation/botanist/component/kubeapiserver/kube_apiserver_test.go
+++ pkg/operation/botanist/component/kubeapiserver/kube_apiserver_test.go
@@ -2723,6 +2723,16 @@ rules:
 
 					Expect(deployment.Spec.Template.Spec.Containers[0].Lifecycle).To(BeNil())
 				})
+
+				It("should set the --shutdown-send-retry-after=true flag if the kubernetes version is 1.24", func() {
+					version = semver.MustParse("1.24.7")
+					kapi = New(kubernetesInterface, namespace, sm, Values{Images: images, RuntimeVersion: runtimeVersion, Version: version})
+					deployAndRead()
+
+					Expect(deployment.Spec.Template.Spec.Containers[0].Command).To(ContainElements(
+						"--shutdown-send-retry-after=true",
+					))
+				})
 			})
 		})
 	})
