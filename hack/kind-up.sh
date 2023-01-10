f2e57be5bb3e558ce8ce04081bf9c6c346f0e449
Seshachalam Yerasala Venkata <104052572+seshachalam-yv@users.noreply.github.com>
2023-01-10 16:36:58 +0530

    Raise error when `ip` command not found instead of skipping if `OS` is Mac. (#7269)
    * Raise error when 'ip' command not found instead of skipping.
    
    * Addressed reveiw feedback from shafeeqes
    
    * Fixed typo

--- hack/kind-up.sh
+++ hack/kind-up.sh
@@ -39,7 +39,11 @@ parse_flags() {
 }
 
 setup_loopback_device() {
-  if ! command -v ip &> /dev/null; then
+  if ! command -v ip &>/dev/null; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      echo "'ip' command not found. Please install 'ip' command, refer https://github.com/gardener/gardener/blob/master/docs/development/local_setup.md#installing-iproute2" 1>&2
+      exit 1
+    fi
     echo "Skipping loopback device setup because 'ip' command is not available..."
     return
   fi
