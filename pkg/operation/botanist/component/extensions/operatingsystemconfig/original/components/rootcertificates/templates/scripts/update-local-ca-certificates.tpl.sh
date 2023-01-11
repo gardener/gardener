85c478c530e52b6238b8a942e0298733f1034f6a
Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>
2023-01-11 12:15:31 +0100

    Add Gardener CAs to CA bundle on Debian based OS (#7253)
    * Add Gardener CAs to CA bundle on Debian based OS
    
    * Address PR review feedback

---
+++ pkg/operation/botanist/component/extensions/operatingsystemconfig/original/components/rootcertificates/templates/scripts/update-local-ca-certificates.tpl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ -f "/etc/debian_version" ]]; then
+    # Copy certificates from default "localcertsdir" because /usr is mounted read-only in Garden Linux.
+    # See https://github.com/gardenlinux/gardenlinux/issues/1490
+    mkdir -p "{{ .pathLocalSSLCerts }}"
+    if [[ -d "/usr/local/share/ca-certificates" ]]; then
+        cp -af /usr/local/share/ca-certificates/* "{{ .pathLocalSSLCerts }}"
+    fi
+    # localcertsdir is supported on Debian based OS only
+    /usr/sbin/update-ca-certificates --fresh --localcertsdir "{{ .pathLocalSSLCerts }}"
+else
+    /usr/sbin/update-ca-certificates --fresh
+fi
