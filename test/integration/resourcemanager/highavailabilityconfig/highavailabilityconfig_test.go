c4bf98889d999645c3264ede219e9acd6875f35f
Shafeeque E S <shafeeque.e.s@sap.com>
2023-01-19 17:39:35 +0530

    Adapt `highavailabilityconfig` webhook integration test
    `autoscaling/v2beta2.HorizontalPodAutoscaler` is removed in v1.26.
    Ref: https://kubernetes.io/docs/reference/using-api/deprecation-guide/#v1-26

--- test/integration/resourcemanager/highavailabilityconfig/highavailabilityconfig_test.go
+++ test/integration/resourcemanager/highavailabilityconfig/highavailabilityconfig_test.go
@@ -25,7 +25,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	autoscalingv2 "k8s.io/api/autoscaling/v2"
 	autoscalingv2beta1 "k8s.io/api/autoscaling/v2beta1"
-	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
@@ -828,32 +827,6 @@ var _ = Describe("HighAvailabilityConfig tests", func() {
 		})
 
 		Context("for HPAs", func() {
-			Context("with version v2beta2", func() {
-				var hpa *autoscalingv2beta2.HorizontalPodAutoscaler
-
-				BeforeEach(func() {
-					hpa = &autoscalingv2beta2.HorizontalPodAutoscaler{
-						ObjectMeta: objectMeta,
-						Spec: autoscalingv2beta2.HorizontalPodAutoscalerSpec{
-							ScaleTargetRef: autoscalingv2beta2.CrossVersionObjectReference{
-								Kind: "someKind",
-								Name: "someName",
-							},
-						},
-					}
-
-					scalingObject = hpa
-				})
-
-				tests(
-					func() *metav1.ObjectMeta { return &hpa.ObjectMeta },
-					func() *int32 { return hpa.Spec.MinReplicas },
-					func(n *int32) { hpa.Spec.MinReplicas = n },
-					func() int32 { return hpa.Spec.MaxReplicas },
-					func(n int32) { hpa.Spec.MaxReplicas = n },
-				)
-			})
-
 			Context("with version v2", func() {
 				var hpa *autoscalingv2.HorizontalPodAutoscaler
 
