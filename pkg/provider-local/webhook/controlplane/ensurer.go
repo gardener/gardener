d5b2279ef20f697ffa98ed7f249506cec148dcc6
Daniel Foehr <daniel.foehr@sap.com>
2023-02-01 20:06:12 +0100

    add ipv6 registry to template

--- pkg/provider-local/webhook/controlplane/ensurer.go
+++ pkg/provider-local/webhook/controlplane/ensurer.go
@@ -15,18 +15,44 @@
 package controlplane
 
 import (
+	"bytes"
 	"context"
+	_ "embed"
+	"text/template"
 
 	"github.com/Masterminds/semver"
+	"github.com/Masterminds/sprig"
 	"github.com/go-logr/logr"
 	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
 	"k8s.io/utils/pointer"
 
 	extensionscontextwebhook "github.com/gardener/gardener/extensions/pkg/webhook/context"
 	"github.com/gardener/gardener/extensions/pkg/webhook/controlplane/genericmutator"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	v1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/utils"
 )
+
+const pathContainerdConfigScript = v1beta1constants.OperatingSystemConfigFilePathBinaries + "/init-containerd-with-registry-mirrors"
+
+var (
+	tplNameInitializer = "init"
+	//go:embed templates/scripts/configure-containerd.tpl.sh
+	tplContentInitializer string
+	tplInitializer        *template.Template
+)
+
+func init() {
+	var err error
+	tplInitializer, err = template.
+		New(tplNameInitializer).
+		Funcs(sprig.TxtFuncMap()).
+		Parse(tplContentInitializer)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // NewEnsurer creates a new controlplane ensurer.
 func NewEnsurer(logger logr.Logger) genericmutator.Ensurer {
@@ -57,32 +83,49 @@ func (e *ensurer) EnsureAdditionalFiles(ctx context.Context, gc extensionscontex
 		kindClusterName = "gardener-local-ha-control-plane"
 	}
 
+	var script bytes.Buffer
+	if err := tplInitializer.Execute(&script, map[string]interface{}{
+		"kindClusterName": kindClusterName,
+	}); err != nil {
+		return err
+	}
+
 	appendUniqueFile(new, extensionsv1alpha1.File{
-		Path:        "/etc/containerd/conf.d/50-provider-local-registry.toml",
-		Permissions: pointer.Int32(0644),
+		Path:        pathContainerdConfigScript,
+		Permissions: pointer.Int32(0744),
 		Content: extensionsv1alpha1.FileContent{
 			Inline: &extensionsv1alpha1.FileContentInline{
-				Encoding: "",
-				Data: `[plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5001"]
-  endpoint = ["http://` + kindClusterName + `:5001"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
-  endpoint = ["http://` + kindClusterName + `:5002"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.ipv6.docker.com"]
-  endpoint = ["http://` + kindClusterName + `:5008"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."gcr.io"]
-  endpoint = ["http://` + kindClusterName + `:5003"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."eu.gcr.io"]
-  endpoint = ["http://` + kindClusterName + `:5004"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."ghcr.io"]
-  endpoint = ["http://` + kindClusterName + `:5005"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.k8s.io"]
-  endpoint = ["http://` + kindClusterName + `:5006"]
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."quay.io"]
-  endpoint = ["http://` + kindClusterName + `:5007"]
-`,
+				Encoding: "b64",
+				Data:     utils.EncodeBase64(script.Bytes()),
 			},
 		},
 	})
+	return nil
+}
+
+func (e *ensurer) EnsureAdditionalUnits(_ context.Context, _ extensionscontextwebhook.GardenContext, new, _ *[]extensionsv1alpha1.Unit) error {
+	const unitNameInitializer = "containerd-configuration-local-setup.service"
+	unit := extensionsv1alpha1.Unit{
+		Name:    unitNameInitializer,
+		Command: pointer.String("start"),
+		Enable:  pointer.Bool(true),
+		Content: pointer.String(`[Unit]
+Description=Containerd config configuration for local-setup
+
+[Install]
+WantedBy=multi-user.target
+
+[Unit]
+After=containerd-initializer.service
+Requires=containerd-initializer.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=` + pathContainerdConfigScript)}
+
+	appendUniqueUnit(new, unit)
+
 	return nil
 }
 
@@ -97,4 +140,17 @@ func appendUniqueFile(files *[]extensionsv1alpha1.File, file extensionsv1alpha1.
 	}
 
 	*files = append(resFiles, file)
+}
+
+// appendUniqueUnit appends a unit only if it does not exist, otherwise overwrite content of previous unit
+func appendUniqueUnit(units *[]extensionsv1alpha1.Unit, unit extensionsv1alpha1.Unit) {
+	resFiles := make([]extensionsv1alpha1.Unit, 0, len(*units))
+
+	for _, f := range *units {
+		if f.Name != unit.Name {
+			resFiles = append(resFiles, f)
+		}
+	}
+
+	*units = append(resFiles, unit)
 }
