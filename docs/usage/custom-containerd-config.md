50ecc0ffae563e7a3ae9323717263f2c491a58d7
Daniel Föhr <daniel.foehr@sap.com>
2023-02-02 11:33:05 +0100

    Local Provider: Fix containerd configuration for registry mirrors (#7316)
    * add ipv6 registry to template
    
    * update containerd custom config documentation with cautionary comment

--- docs/usage/custom-containerd-config.md
+++ docs/usage/custom-containerd-config.md
@@ -12,6 +12,7 @@ imports = ["/etc/containerd/conf.d/*.toml"]
 ```
 
 This means that all `*.toml` files in the `/etc/containerd/conf.d` directory will be imported and merged with the default configuration.
+To prevent unintended configuration overwrites, please be aware that containerd merges config sections, not individual keys (see [here](https://github.com/containerd/containerd/issues/5837#issuecomment-894840240) and [here](https://github.com/gardener/gardener/pull/7316)).
 Please consult the [upstream `containerd` documentation](https://github.com/containerd/containerd/blob/main/docs/man/containerd-config.toml.5.md#format) for more information.
 
 > ⚠️ Note that this only applies to nodes which were newly created after `gardener/gardener@v1.51` was deployed. Existing nodes are not affected. 
---
+++ pkg/provider-local/webhook/controlplane/templates/scripts/configure-containerd.tpl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cluster_name={{ .kindClusterName }}
+
+FILENAME=/etc/containerd/config.toml
+if ! grep -q plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"localhost:5001\" "$FILENAME"; then
+  cat <<EOF >> $FILENAME
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5001"]
+  endpoint = ["http://$cluster_name:5001"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
+  endpoint = ["http://$cluster_name:5002"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."gcr.io"]
+  endpoint = ["http://$cluster_name:5003"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."eu.gcr.io"]
+  endpoint = ["http://$cluster_name:5004"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.ipv6.docker.com"]
+  endpoint = ["http://$cluster_name:5004"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."ghcr.io"]
+  endpoint = ["http://$cluster_name:5005"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.k8s.io"]
+  endpoint = ["http://$cluster_name:5006"]
+[plugins."io.containerd.grpc.v1.cri".registry.mirrors."quay.io"]
+  endpoint = ["http://$cluster_name:5007"]
+EOF
+  echo "Configured containerd with registry mirrors for local-setup."
+else
+  echo "Containerd already configured with registry mirrors."
+fi
