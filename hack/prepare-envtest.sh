e1be1d6a12fa515dda01b7290b8cb4bef2ad8358
Tim Ebert <timebertt@gmail.com>
2023-02-02 08:17:48 +0100

    Prefactor: extract `hack/prepare-envtest.sh`

---
+++ hack/prepare-envtest.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ENVTEST_K8S_VERSION=${ENVTEST_K8S_VERSION:-"1.26"}
+
+echo "> Installing envtest tools@${ENVTEST_K8S_VERSION} with setup-envtest if necessary"
+if ! command -v setup-envtest &> /dev/null ; then
+  >&2 echo "setup-envtest not available"
+  exit 1
+fi
+
+export KUBEBUILDER_ASSETS="$(setup-envtest use -p path ${ENVTEST_K8S_VERSION})"
+echo "using envtest tools installed at '$KUBEBUILDER_ASSETS'"
+
+source "$(dirname "$0")/test-integration.env"
