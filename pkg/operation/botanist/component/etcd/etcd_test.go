69405db32ff4efde22e464265228cd1393c9d61d
Rafael Franzke <rafael.franzke@sap.com>
2023-02-06 09:08:37 +0100

    Use to-seed-apiserver label for ETCD (#7434)

--- pkg/operation/botanist/component/etcd/etcd_test.go
+++ pkg/operation/botanist/component/etcd/etcd_test.go
@@ -317,9 +317,9 @@ var _ = Describe("Etcd", func() {
 						"role":                             testRole,
 						"app":                              "etcd-statefulset",
 						"networking.gardener.cloud/to-dns": "allowed",
-						"networking.gardener.cloud/to-public-networks":   "allowed",
-						"networking.gardener.cloud/to-private-networks":  "allowed",
-						"networking.gardener.cloud/to-runtime-apiserver": "allowed",
+						"networking.gardener.cloud/to-public-networks":  "allowed",
+						"networking.gardener.cloud/to-private-networks": "allowed",
+						"networking.gardener.cloud/to-seed-apiserver":   "allowed",
 					},
 					Selector: &metav1.LabelSelector{
 						MatchLabels: map[string]string{
