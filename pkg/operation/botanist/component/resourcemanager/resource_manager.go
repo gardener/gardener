6c1d3d77f40512b0d28ac53ae9dda081c33313d1
Rafael Franzke <rafael.franzke@sap.com>
2023-02-14 10:07:13 +0100

    Adapt `gardener-resource-manager`

--- pkg/operation/botanist/component/resourcemanager/resource_manager.go
+++ pkg/operation/botanist/component/resourcemanager/resource_manager.go
@@ -662,6 +662,12 @@ func (r *resourceManager) ensureService(ctx context.Context) error {
 	service := r.emptyService()
 	_, err := controllerutils.GetAndCreateOrMergePatch(ctx, r.client, service, func() error {
 		service.Labels = r.getLabels()
+
+		utilruntime.Must(gardenerutils.InjectNetworkPolicyAnnotationsForScrapeTargets(service, networkingv1.NetworkPolicyPort{
+			Port:     utils.IntStrPtrFromInt(metricsPort),
+			Protocol: utils.ProtocolPtr(corev1.ProtocolTCP),
+		}))
+
 		service.Spec.Selector = appLabel()
 		service.Spec.Type = corev1.ServiceTypeClusterIP
 		desiredPorts := []corev1.ServicePort{
@@ -1837,13 +1843,10 @@ func (r *resourceManager) getNetworkPolicyLabels() map[string]string {
 			v1beta1constants.LabelNetworkPolicyToShootAPIServer:   v1beta1constants.LabelNetworkPolicyAllowed,
 			v1beta1constants.LabelNetworkPolicyFromShootAPIServer: v1beta1constants.LabelNetworkPolicyAllowed,
 			v1beta1constants.LabelNetworkPolicyToRuntimeAPIServer: v1beta1constants.LabelNetworkPolicyAllowed,
-			v1beta1constants.LabelNetworkPolicyFromPrometheus:     v1beta1constants.LabelNetworkPolicyAllowed,
 		}
 	}
 
-	return map[string]string{
-		v1beta1constants.LabelNetworkPolicyFromPrometheus: v1beta1constants.LabelNetworkPolicyAllowed,
-	}
+	return nil
 }
 
 func appLabel() map[string]string {
--- pkg/operation/botanist/component/resourcemanager/resource_manager_test.go
+++ pkg/operation/botanist/component/resourcemanager/resource_manager_test.go
@@ -270,7 +270,12 @@ var _ = Describe("ResourceManager", func() {
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "gardener-resource-manager",
 				Namespace: deployNamespace,
-				Labels:    defaultLabels},
+				Labels:    defaultLabels,
+				Annotations: map[string]string{
+					"networking.resources.gardener.cloud/from-policy-pod-label-selector": "all-scrape-targets",
+					"networking.resources.gardener.cloud/from-policy-allowed-ports":      `[{"protocol":"TCP","port":8080}]`,
+				},
+			},
 			Spec: corev1.ServiceSpec{
 				Selector: map[string]string{
 					"app": "gardener-resource-manager"},
@@ -500,7 +505,6 @@ var _ = Describe("ResourceManager", func() {
 								"projected-token-mount.resources.gardener.cloud/skip": "true",
 								"networking.gardener.cloud/to-dns":                    "allowed",
 								"networking.gardener.cloud/to-runtime-apiserver":      "allowed",
-								"networking.gardener.cloud/from-prometheus":           "allowed",
 								"networking.gardener.cloud/to-shoot-apiserver":        "allowed",
 								"networking.gardener.cloud/from-shoot-apiserver":      "allowed",
 								v1beta1constants.GardenRole:                           v1beta1constants.GardenRoleControlPlane,
