6e634a7e922827fcc2a79757f2bbb2110e839114
Vladimir Nachev <vladimir.nachev@sap.com>
2023-02-16 12:28:10 +0200

    hack/format.sh now supports custom options for goimports-reviser

--- hack/format.sh
+++ hack/format.sh
@@ -23,6 +23,8 @@ goimports -l -w $@
 # Format import order only after files have been formatted by imports.
 echo "> Format Import Order"
 
+goimports_reviser_opts=${GOIMPORTS_REVISER_OPTIONS:-""}
+
 for p in "$@" ; do
-  goimports-reviser -recursive $p
+  goimports-reviser $goimports_reviser_opts -recursive $p
 done
