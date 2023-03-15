88d7a353cdb4e915751bcbd003292a5bad5a231d
Kristian-ZH <k.zhelyazkov@sap.com>
2023-03-15 14:31:44 +0200

    Add fluent-operator custom resources for the kubernetes-dashboard component

--- pkg/operation/botanist/component/kubernetesdashboard/logging.go
+++ pkg/operation/botanist/component/kubernetesdashboard/logging.go
@@ -15,28 +15,65 @@
 package kubernetesdashboard
 
 import (
+	"fmt"
+
+	fluentbitv1alpha2 "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2"
+	fluentbitv1alpha2filter "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/filter"
+	fluentbitv1alpha2parser "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/parser"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/operation/botanist/component"
 )
 
-const (
-	loggingParserName = "kubernetesDashboardParser"
-	loggingParser     = `[PARSER]
-    Name        ` + loggingParserName + `
-    Format      regex
-    Regex       ^(?<time>\d{4}\/\d{2}\/\d{2}\s+[^\s]*)\s+(?<log>.*)
-    Time_Key    time
-    Time_Format %Y/%m/%d %H:%M:%S
-`
-	loggingFilter = `[FILTER]
-    Name                parser
-    Match               kubernetes.*addons-kubernetes-dashboard*kubernetes-dashboard*
-    Key_Name            log
-    Parser              ` + loggingParserName + `
-    Reserve_Data        True
-`
+var (
+	// ComponentName is the name of addons-kubernetes-dashboard component.
+	ComponentName = "addons-" + name
 )
 
 // CentralLoggingConfiguration returns a fluent-bit parser and filter for the kubernetesDashboard logs.
 func CentralLoggingConfiguration() (component.CentralLoggingConfig, error) {
-	return component.CentralLoggingConfig{Filters: loggingFilter, Parsers: loggingParser}, nil
+	return component.CentralLoggingConfig{Filters: generateClusterFilters(), Parsers: generateClusterParsers()}, nil
+}
+
+func generateClusterFilters() []*fluentbitv1alpha2.ClusterFilter {
+	return []*fluentbitv1alpha2.ClusterFilter{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   ComponentName,
+				Labels: map[string]string{v1beta1constants.LabelKeyCustomLoggingResource: v1beta1constants.LabelValueCustomLoggingResource},
+			},
+			Spec: fluentbitv1alpha2.FilterSpec{
+				Match: fmt.Sprintf("kubernetes.*%s*%s*", ComponentName, name),
+				FilterItems: []fluentbitv1alpha2.FilterItem{
+					{
+						Parser: &fluentbitv1alpha2filter.Parser{
+							KeyName:     "log",
+							Parser:      name + "-parser",
+							ReserveData: pointer.Bool(true),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func generateClusterParsers() []*fluentbitv1alpha2.ClusterParser {
+	return []*fluentbitv1alpha2.ClusterParser{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   name + "-parser",
+				Labels: map[string]string{v1beta1constants.LabelKeyCustomLoggingResource: v1beta1constants.LabelValueCustomLoggingResource},
+			},
+			Spec: fluentbitv1alpha2.ParserSpec{
+				Regex: &fluentbitv1alpha2parser.Regex{
+					Regex:      "^(?<time>\\d{4}\\/\\d{2}\\/\\d{2}\\s+[^\\s]*)\\s+(?<log>.*)",
+					TimeKey:    "time",
+					TimeFormat: "%Y/%m/%d %H:%M:%S",
+				},
+			},
+		},
+	}
 }
--- pkg/operation/botanist/component/kubernetesdashboard/logging_test.go
+++ pkg/operation/botanist/component/kubernetesdashboard/logging_test.go
@@ -15,8 +15,13 @@
 package kubernetesdashboard_test
 
 import (
+	fluentbitv1alpha2 "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2"
+	fluentbitv1alpha2filter "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/filter"
+	fluentbitv1alpha2parser "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/parser"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	. "github.com/gardener/gardener/pkg/operation/botanist/component/kubernetesdashboard"
 )
@@ -27,21 +32,44 @@ var _ = Describe("Logging", func() {
 			loggingConfig, err := CentralLoggingConfiguration()
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(loggingConfig.Parsers).To(Equal(`[PARSER]
-    Name        kubernetesDashboardParser
-    Format      regex
-    Regex       ^(?<time>\d{4}\/\d{2}\/\d{2}\s+[^\s]*)\s+(?<log>.*)
-    Time_Key    time
-    Time_Format %Y/%m/%d %H:%M:%S
-`))
-
-			Expect(loggingConfig.Filters).To(Equal(`[FILTER]
-    Name                parser
-    Match               kubernetes.*addons-kubernetes-dashboard*kubernetes-dashboard*
-    Key_Name            log
-    Parser              kubernetesDashboardParser
-    Reserve_Data        True
-`))
+			Expect(loggingConfig.Filters).To(Equal(
+				[]*fluentbitv1alpha2.ClusterFilter{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "addons-kubernetes-dashboard",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.FilterSpec{
+							Match: "kubernetes.*addons-kubernetes-dashboard*kubernetes-dashboard*",
+							FilterItems: []fluentbitv1alpha2.FilterItem{
+								{
+									Parser: &fluentbitv1alpha2filter.Parser{
+										KeyName:     "log",
+										Parser:      "kubernetes-dashboard-parser",
+										ReserveData: pointer.Bool(true),
+									},
+								},
+							},
+						},
+					},
+				}))
+			Expect(loggingConfig.Parsers).To(Equal(
+				[]*fluentbitv1alpha2.ClusterParser{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "kubernetes-dashboard-parser",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.ParserSpec{
+							Regex: &fluentbitv1alpha2parser.Regex{
+								Regex:      "^(?<time>\\d{4}\\/\\d{2}\\/\\d{2}\\s+[^\\s]*)\\s+(?<log>.*)",
+								TimeKey:    "time",
+								TimeFormat: "%Y/%m/%d %H:%M:%S",
+							},
+						},
+					},
+				}))
+			Expect(loggingConfig.Inputs).To(BeNil())
 		})
 	})
 })
