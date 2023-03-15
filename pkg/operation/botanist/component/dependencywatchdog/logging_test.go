b10db69e7de8032023da5becb7938e902a83737a
Kristian-ZH <k.zhelyazkov@sap.com>
2023-03-15 14:29:16 +0200

    Add fluent-operator custom resources for the dependency-watchdog component

--- pkg/operation/botanist/component/dependencywatchdog/logging_test.go
+++ pkg/operation/botanist/component/dependencywatchdog/logging_test.go
@@ -15,8 +15,13 @@
 package dependencywatchdog_test
 
 import (
+	fluentbitv1alpha2 "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2"
+	fluentbitv1alpha2filter "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/filter"
+	fluentbitv1alpha2parser "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/parser"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	. "github.com/gardener/gardener/pkg/operation/botanist/component/dependencywatchdog"
 )
@@ -27,21 +32,44 @@ var _ = Describe("Logging", func() {
 			loggingConfig, err := CentralLoggingConfiguration()
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(loggingConfig.Parsers).To(Equal(`[PARSER]
-    Name        dependencyWatchdogParser
-    Format      regex
-    Regex       ^(?<severity>\w)(?<time>\d{4} [^\s]*)\s+(?<pid>\d+)\s+(?<source>[^ \]]+)\] (?<log>.*)$
-    Time_Key    time
-    Time_Format %m%d %H:%M:%S.%L
-`))
-
-			Expect(loggingConfig.Filters).To(Equal(`[FILTER]
-    Name                parser
-    Match               kubernetes.*dependency-watchdog*dependency-watchdog*
-    Key_Name            log
-    Parser              dependencyWatchdogParser
-    Reserve_Data        True
-`))
+			Expect(loggingConfig.Filters).To(Equal(
+				[]*fluentbitv1alpha2.ClusterFilter{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "dependency-watchdog",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.FilterSpec{
+							Match: "kubernetes.*dependency-watchdog*dependency-watchdog*",
+							FilterItems: []fluentbitv1alpha2.FilterItem{
+								{
+									Parser: &fluentbitv1alpha2filter.Parser{
+										KeyName:     "log",
+										Parser:      "dependency-watchdog-parser",
+										ReserveData: pointer.Bool(true),
+									},
+								},
+							},
+						},
+					},
+				}))
+			Expect(loggingConfig.Parsers).To(Equal(
+				[]*fluentbitv1alpha2.ClusterParser{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "dependency-watchdog-parser",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.ParserSpec{
+							Regex: &fluentbitv1alpha2parser.Regex{
+								Regex:      "^(?<severity>\\w)(?<time>\\d{4} [^\\s]*)\\s+(?<pid>\\d+)\\s+(?<source>[^ \\]]+)\\] (?<log>.*)$",
+								TimeKey:    "time",
+								TimeFormat: "%m%d %H:%M:%S.%L",
+							},
+						},
+					},
+				}))
+			Expect(loggingConfig.Inputs).To(BeNil())
 		})
 	})
 })
