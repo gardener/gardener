842dddf37aa94b80b763d8185bcbe28761871a05
Niki Dokovski <nickytd@gmail.com>
2023-03-15 10:32:42 +0100

    Create a unified logging service definition in the logging stack  (#7623)
    * Logging service duplicates the current loki service with the purpose of
    providing the same target in fluent-bit outputs.
    Once the service is made available on all clusters (clusters are reconciled on a daily basis) the
    fluent-bits output will be switched.
    After the switch the fluent-bit will deliver logs to the same target logging solving the problem
    loki to vali migration. Since the target in fluent-bit is the same, fluent-bit will deliver logs to both
    migrated and non migrated shoots.
    
    * Add cleaning up of the newly introduced logging service. It shall be deleted when the logging stack is removed.

--- pkg/operation/common/utils_test.go
+++ pkg/operation/common/utils_test.go
@@ -213,6 +213,8 @@ var _ = Describe("common", func() {
 			&hvpav1alpha1.Hvpa{ObjectMeta: metav1.ObjectMeta{Name: "loki", Namespace: v1beta1constants.GardenNamespace}},
 			&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "loki-config", Namespace: v1beta1constants.GardenNamespace}},
 			&corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: "loki", Namespace: v1beta1constants.GardenNamespace}},
+			&corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: "logging",
+				Namespace: v1beta1constants.GardenNamespace}},
 			&appsv1.StatefulSet{ObjectMeta: metav1.ObjectMeta{Name: "loki", Namespace: v1beta1constants.GardenNamespace}},
 			&corev1.PersistentVolumeClaim{ObjectMeta: metav1.ObjectMeta{Name: "loki-loki-0", Namespace: v1beta1constants.GardenNamespace}},
 		}
