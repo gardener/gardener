f13433a923bb416deaa6ddeb528158f27d5de763
Kristian-ZH <k.zhelyazkov@sap.com>
2023-03-15 14:28:33 +0200

    Add fluent-operator custom resources for the coredns component

--- pkg/operation/botanist/component/coredns/logging_test.go
+++ pkg/operation/botanist/component/coredns/logging_test.go
@@ -15,8 +15,13 @@
 package coredns_test
 
 import (
+	fluentbitv1alpha2 "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2"
+	fluentbitv1alpha2filter "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/filter"
+	fluentbitv1alpha2parser "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/parser"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	. "github.com/gardener/gardener/pkg/operation/botanist/component/coredns"
 )
@@ -27,29 +32,58 @@ var _ = Describe("Logging", func() {
 			loggingConfig, err := CentralLoggingConfiguration()
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(loggingConfig.Parsers).To(Equal(`[PARSER]
-    Name        coreDNSParser1
-    Format      regex
-    Regex       ^(?<time>\d{4}-\d{2}-\d{2}[Tt].*[zZ])\s+\[(?<severity>\w*[^\]])\]\s+(?<log>.*)
-    Time_Key    time
-    Time_Format  %Y-%m-%dT%H:%M:%S.%L
+			Expect(loggingConfig.Filters).To(Equal(
+				[]*fluentbitv1alpha2.ClusterFilter{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "coredns",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.FilterSpec{
+							Match: "kubernetes.*coredns*coredns*",
+							FilterItems: []fluentbitv1alpha2.FilterItem{
+								{
+									Parser: &fluentbitv1alpha2filter.Parser{
+										KeyName:     "log",
+										Parser:      "coredns-parser1,coredns-parser2",
+										ReserveData: pointer.Bool(true),
+									},
+								},
+							},
+						},
+					},
+				}))
 
-[PARSER]
-    Name        coreDNSParser2
-    Format      regex
-    Regex       ^(?<severity>\w)(?<time>\d{4} [^\s]*)\s+(?<pid>\d+)\s+(?<source>[^ \]]+)\] (?<log>.*)$
-    Time_Key    time
-    Time_Format %m%d %H:%M:%S.%L
-`))
-
-			Expect(loggingConfig.Filters).To(Equal(`[FILTER]
-    Name                parser
-    Match               kubernetes.*coredns*coredns*
-    Key_Name            log
-    Parser              coreDNSParser1
-    Parser              coreDNSParser2
-    Reserve_Data        True
-`))
+			Expect(loggingConfig.Parsers).To(Equal(
+				[]*fluentbitv1alpha2.ClusterParser{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "coredns-parser1",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.ParserSpec{
+							Regex: &fluentbitv1alpha2parser.Regex{
+								Regex:      "^(?<time>\\d{4}-\\d{2}-\\d{2}[Tt].*[zZ])\\s+\\[(?<severity>\\w*[^\\]])\\]\\s+(?<log>.*)",
+								TimeKey:    "time",
+								TimeFormat: "%Y-%m-%dT%H:%M:%S.%L",
+							},
+						},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:   "coredns-parser2",
+							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+						},
+						Spec: fluentbitv1alpha2.ParserSpec{
+							Regex: &fluentbitv1alpha2parser.Regex{
+								Regex:      "^(?<severity>\\w)(?<time>\\d{4} [^\\s]*)\\s+(?<pid>\\d+)\\s+(?<source>[^ \\]]+)\\] (?<log>.*)$",
+								TimeKey:    "time",
+								TimeFormat: "%m%d %H:%M:%S.%L",
+							},
+						},
+					},
+				}))
+			Expect(loggingConfig.Inputs).To(BeNil())
 		})
 	})
 })
