c33c003f8ab9e4119fd2f625e3205f92ceba733f
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-03-20 18:54:17 +0100

    [release-v1.65] Drop stale condition from extension status (#7677)
    * Allow extensions to delete conditions from status
    
    Co-Authored-By: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Drop not required health check from provider-local
    
    Co-Authored-By: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Address review
    
    ---------
    
    Co-authored-by: acumino <sksgkpvks@gmail.com>
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>

--- pkg/provider-local/controller/healthcheck/add.go
+++ pkg/provider-local/controller/healthcheck/add.go
@@ -18,18 +18,15 @@ import (
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	extensionsconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
-	genericcontrolplaneactuator "github.com/gardener/gardener/extensions/pkg/controller/controlplane/genericactuator"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/general"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/worker"
-	genericworkeractuator "github.com/gardener/gardener/extensions/pkg/controller/worker/genericactuator"
-	extensionspredicate "github.com/gardener/gardener/extensions/pkg/predicate"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/provider-local/local"
@@ -53,24 +50,6 @@ var (
 // RegisterHealthChecks registers health checks for each extension resource
 // HealthChecks are grouped by extension (e.g worker), extension.type (e.g local) and  Health Check Type (e.g SystemComponentsHealthy)
 func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) error {
-	if err := healthcheck.DefaultRegistration(
-		local.Type,
-		extensionsv1alpha1.SchemeGroupVersion.WithKind(extensionsv1alpha1.ControlPlaneResource),
-		func() client.ObjectList { return &extensionsv1alpha1.ControlPlaneList{} },
-		func() extensionsv1alpha1.Object { return &extensionsv1alpha1.ControlPlane{} },
-		mgr,
-		opts,
-		[]predicate.Predicate{extensionspredicate.HasPurpose(extensionsv1alpha1.Normal)},
-		[]healthcheck.ConditionTypeToHealthCheck{
-			{
-				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
-				HealthCheck:   general.CheckManagedResource(genericcontrolplaneactuator.ShootWebhooksResourceName),
-			},
-		},
-	); err != nil {
-		return err
-	}
-
 	return healthcheck.DefaultRegistration(
 		local.Type,
 		extensionsv1alpha1.SchemeGroupVersion.WithKind(extensionsv1alpha1.WorkerResource),
@@ -81,10 +60,6 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 		nil,
 		[]healthcheck.ConditionTypeToHealthCheck{
 			{
-				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
-				HealthCheck:   general.CheckManagedResource(genericworkeractuator.McmShootResourceName),
-			},
-			{
 				ConditionType: string(gardencorev1beta1.ShootControlPlaneHealthy),
 				HealthCheck:   general.NewSeedDeploymentHealthChecker(local.MachineControllerManagerName),
 			},
@@ -93,6 +68,7 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 				HealthCheck:   worker.NewNodesChecker(),
 			},
 		},
+		sets.Set[gardencorev1beta1.ConditionType]{},
 	)
 }
 
