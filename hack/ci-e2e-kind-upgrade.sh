572ebe2fa6cc07d71ea3e4900653e6b59b0c58ff
Seshachalam Yerasala Venkata <seshachalam.yerasala.venkata@sap.com>
2023-04-14 17:40:28 +0530

    Revert commented code for debugging

--- hack/ci-e2e-kind-upgrade.sh
+++ hack/ci-e2e-kind-upgrade.sh
@@ -205,11 +205,11 @@ export GARDENER_PREVIOUS_VERSION="$(cat $GARDENER_RELEASE_DOWNLOAD_PATH/gardener
 kind_up
 
 # export all container logs and events after test execution
-# trap '{
-#   rm -rf "$GARDENER_RELEASE_DOWNLOAD_PATH/gardener-releases"
-#   export_artifacts "$CLUSTER_NAME"
-#   kind_down
-# }' EXIT
+trap '{
+  rm -rf "$GARDENER_RELEASE_DOWNLOAD_PATH/gardener-releases"
+  export_artifacts "$CLUSTER_NAME"
+  kind_down
+}' EXIT
 
 echo "Installing gardener version '$GARDENER_PREVIOUS_RELEASE'"
 install_previous_release
@@ -236,4 +236,4 @@ sleep 60
 echo "Running gardener post-upgrade tests"
 run_post_upgrade_test
 
-# gardener_down
+gardener_down
