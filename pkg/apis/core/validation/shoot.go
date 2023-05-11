8ae1b2bb421f2e8fcc037d7585627921ca287d02
Aaron Francis Fernandes <79958509+aaronfern@users.noreply.github.com>
2023-05-11 13:05:47 +0530

    Update pkg/apis/core/validation/shoot.go
    Co-authored-by: Sonu Kumar Singh <sonu.kumar.singh02@sap.com>

--- pkg/apis/core/validation/shoot.go
+++ pkg/apis/core/validation/shoot.go
@@ -2078,7 +2078,7 @@ func isShootReadyForRotationStart(lastOperation *core.LastOperation) bool {
 }
 
 func isShootInHibernation(shoot *core.Shoot) bool {
-	if shoot.Spec.Hibernation != nil {
+	if shoot.Spec.Hibernation != nil  && shoot.Spec.Hibernation.Enabled != nil {
 		return *shoot.Spec.Hibernation.Enabled || shoot.Status.IsHibernated
 	}
 
