4b3bfa6a3bb4ac2f86bbb1d0dec0b9a6cfb9c63d
Rafael Franzke <rafael.franzke@sap.com>
2023-05-24 21:25:12 +0200

    [provider-local] Delete stale `Machine`s after CPM
    Co-Authored-By: Jens Schneider <schneider@23technologies.cloud>
    Co-Authored-By: mreiger <michael@rauschpfeife.net>

--- pkg/provider-local/controller/worker/actuator.go
+++ pkg/provider-local/controller/worker/actuator.go
@@ -17,11 +17,17 @@ package worker
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	machinev1alpha1 "github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 
+	extensionsconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	"github.com/gardener/gardener/extensions/pkg/controller/common"
 	"github.com/gardener/gardener/extensions/pkg/controller/worker"
@@ -41,21 +47,23 @@ type delegateFactory struct {
 
 type actuator struct {
 	worker.Actuator
+	workerDelegate *delegateFactory
 }
 
 // NewActuator creates a new Actuator that updates the status of the handled WorkerPoolConfigs.
 func NewActuator() worker.Actuator {
-	delegateFactory := &delegateFactory{}
+	workerDelegate := &delegateFactory{}
 
 	return &actuator{
-		genericactuator.NewActuator(
-			delegateFactory,
+		Actuator: genericactuator.NewActuator(
+			workerDelegate,
 			local.MachineControllerManagerName,
 			mcmChart,
 			mcmShootChart,
 			imagevector.ImageVector(),
 			extensionscontroller.ChartRendererFactoryFunc(util.NewChartRendererForShoot),
 		),
+		workerDelegate: workerDelegate,
 	}
 }
 
@@ -68,7 +76,62 @@ func (a *actuator) Restore(ctx context.Context, log logr.Logger, worker *extensi
 		return fmt.Errorf("failed restoring the worker state: %w", err)
 	}
 
+	// At this point, the generic actuator has restored all Machine objects into the shoot namespace of the new
+	// destination seed. However, in the local scenario, the shoot worker nodes are not really external machines but
+	// "internal" pods running next to the control plane in the seed.
+	// Since the pods cannot be migrated from the source seed to the destination seed, the shoot worker node pods cannot
+	// be restored. Instead, they have to be recreated.
+	// In order to trigger this recreation, we are deleting all (restored) machines which are no longer backed by any
+	// pods now. We also delete the corresponding Node objects in the shoot. The MCM's MachineSet controller will
+	// automatically recreate new Machines now, which in fact will result in new pods and nodes.
+	// In summary, we are still not simulating the very same CPM scenario as for real clouds (here, the nodes/VMs are
+	// external and remain during the migration), but this is as good as we can get for the local scenario.
+	if err := a.deleteNoLongerMachines(ctx, log, worker.Namespace); err != nil {
+		return fmt.Errorf("failed deleting no longer existing machines after restoration: %w", err)
+	}
+
 	return a.Actuator.Reconcile(ctx, log, worker, cluster)
+}
+
+func (a *actuator) deleteNoLongerMachines(ctx context.Context, log logr.Logger, namespace string) error {
+	_, shootClient, err := util.NewClientForShoot(ctx, a.workerDelegate.Client(), namespace, client.Options{}, extensionsconfig.RESTOptions{})
+	if err != nil {
+		return fmt.Errorf("failed creating client for shoot cluster: %w", err)
+	}
+
+	machineList := &machinev1alpha1.MachineList{}
+	if err := a.workerDelegate.Client().List(ctx, machineList, client.InNamespace(namespace)); err != nil {
+		return fmt.Errorf("failed listing machines: %w", err)
+	}
+
+	podList := &corev1.PodList{}
+	if err := a.workerDelegate.Client().List(ctx, podList, client.InNamespace(namespace), client.MatchingLabels{"app": "machine"}); err != nil {
+		return fmt.Errorf("failed listing pods: %w", err)
+	}
+
+	machineNameToPodName := make(map[string]string)
+	for _, pod := range podList.Items {
+		machineNameToPodName[strings.TrimPrefix(pod.Name, "machine-")] = pod.Name
+	}
+
+	for _, machine := range machineList.Items {
+		if _, ok := machineNameToPodName[machine.Name]; ok {
+			continue
+		}
+
+		log.Info("Deleting machine since it is not backed by any pod", "machine", client.ObjectKeyFromObject(machine.DeepCopy()))
+
+		nodeName := "machine-" + machine.Name
+		if err := shootClient.Delete(ctx, &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: nodeName}}); client.IgnoreNotFound(err) != nil {
+			return fmt.Errorf("failed deleting node %q for machine %q: %w", nodeName, machine.Name, err)
+		}
+
+		if err := a.workerDelegate.Client().Delete(ctx, machine.DeepCopy()); err != nil {
+			return fmt.Errorf("failed deleting machine %q: %w", machine.Name, err)
+		}
+	}
+
+	return nil
 }
 
 func (d *delegateFactory) WorkerDelegate(_ context.Context, worker *extensionsv1alpha1.Worker, cluster *extensionscontroller.Cluster) (genericactuator.WorkerDelegate, error) {
