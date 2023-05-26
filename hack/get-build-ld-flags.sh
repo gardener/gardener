0ce7c25dde355099ff2f5e8880546eda1477146c
Brian Topping <brian.topping@sap.com>
2023-05-26 00:36:40 -0600

    Cleanup build dates in Skaffold (#7956)
    Signed-off-by: Brian Topping <brian.topping@sap.com>

--- hack/get-build-ld-flags.sh
+++ hack/get-build-ld-flags.sh
@@ -19,6 +19,7 @@ set -e
 PACKAGE_PATH="${1:-k8s.io/component-base}"
 VERSION_PATH="${2:-$(dirname $0)/../VERSION}"
 PROGRAM_NAME="${3:-Gardener}"
+BUILD_DATE="${4:-$(date '+%Y-%m-%dT%H:%M:%S%z' | sed 's/\([0-9][0-9]\)$/:\1/g')}"
 VERSION_VERSIONFILE="$(cat "$VERSION_PATH")"
 VERSION="${EFFECTIVE_VERSION:-$VERSION_VERSIONFILE}"
 
@@ -46,5 +47,5 @@ echo "-X $PACKAGE_PATH/version.gitMajor=$MAJOR_VERSION
       -X $PACKAGE_PATH/version.gitVersion=$VERSION
       -X $PACKAGE_PATH/version.gitTreeState=$TREE_STATE
       -X $PACKAGE_PATH/version.gitCommit=$(git rev-parse --verify HEAD)
-      -X $PACKAGE_PATH/version.buildDate=$(date '+%Y-%m-%dT%H:%M:%S%z' | sed 's/\([0-9][0-9]\)$/:\1/g')
+      -X $PACKAGE_PATH/version.buildDate=$BUILD_DATE
       -X $PACKAGE_PATH/version/verflag.programName=$PROGRAM_NAME"
