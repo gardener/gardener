c92bcc742ebff9b48cee408673a073b99d3b97ee
Shafeeque E S <shafeeque.e.s@sap.com>
2023-05-29 23:35:09 +0530

    Deflake shoot care integration test

--- test/integration/gardenlet/shoot/care/care_suite_test.go
+++ test/integration/gardenlet/shoot/care/care_suite_test.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"k8s.io/client-go/rest"
 	testclock "k8s.io/utils/clock/testing"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -77,8 +78,10 @@ var (
 	shootClientMap clientmap.ClientMap
 	mgrClient      client.Client
 
-	testRunID     string
+	project       *gardencorev1beta1.Project
+	seed          *gardencorev1beta1.Seed
 	testNamespace *corev1.Namespace
+	testRunID     string
 	projectName   string
 	seedName      string
 	shootName     string
@@ -160,6 +163,68 @@ var _ = BeforeSuite(func() {
 	DeferCleanup(func() {
 		By("Delete test Namespace")
 		Expect(testClient.Delete(ctx, testNamespace)).To(Or(Succeed(), BeNotFoundError()))
+	})
+
+	project = &gardencorev1beta1.Project{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   projectName,
+			Labels: map[string]string{testID: testRunID},
+		},
+		Spec: gardencorev1beta1.ProjectSpec{
+			Namespace: &testNamespace.Name,
+		},
+	}
+
+	By("Create Project")
+	Expect(testClient.Create(ctx, project)).To(Succeed())
+	log.Info("Created Project for test", "project", project.Name)
+
+	DeferCleanup(func() {
+		By("Delete Project")
+		Expect(client.IgnoreNotFound(testClient.Delete(ctx, project))).To(Succeed())
+	})
+
+	seed = &gardencorev1beta1.Seed{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   seedName,
+			Labels: map[string]string{testID: testRunID},
+		},
+		Spec: gardencorev1beta1.SeedSpec{
+			Provider: gardencorev1beta1.SeedProvider{
+				Region: "region",
+				Type:   "providerType",
+				Zones:  []string{"a", "b", "c"},
+			},
+			Ingress: &gardencorev1beta1.Ingress{
+				Domain: "seed.example.com",
+				Controller: gardencorev1beta1.IngressController{
+					Kind: "nginx",
+				},
+			},
+			DNS: gardencorev1beta1.SeedDNS{
+				Provider: &gardencorev1beta1.SeedDNSProvider{
+					Type: "providerType",
+					SecretRef: corev1.SecretReference{
+						Name:      "some-secret",
+						Namespace: "some-namespace",
+					},
+				},
+			},
+			Networks: gardencorev1beta1.SeedNetworks{
+				Pods:     "10.0.0.0/16",
+				Services: "10.1.0.0/16",
+				Nodes:    pointer.String("10.2.0.0/16"),
+			},
+		},
+	}
+
+	By("Create Seed")
+	Expect(testClient.Create(ctx, seed)).To(Succeed())
+	log.Info("Created Seed for test", "seed", seed.Name)
+
+	DeferCleanup(func() {
+		By("Delete Project")
+		Expect(client.IgnoreNotFound(testClient.Delete(ctx, seed))).To(Succeed())
 	})
 
 	By("Setup manager")
