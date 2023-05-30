90eb84e474f3c2119e3993df3c2f1d28cabda7af
acumino <sonu.kumar.singh02@sap.com>
2023-05-31 01:22:19 +0530

    Introduce separate deployer for etcd CRDs

---
+++ pkg/component/etcd/crd.go
@@ -0,0 +1,116 @@
+// Copyright 2023 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package etcd
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+
+	druidv1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/component"
+	"github.com/gardener/gardener/pkg/utils/flow"
+	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
+)
+
+var (
+	//go:embed crds/templates/crd-druid.gardener.cloud_etcds.yaml
+	// CRD holds the etcd custom resource definition template
+	CRD string
+	//go:embed crds/templates/crd-druid.gardener.cloud_etcdcopybackupstasks.yaml
+	etcdCopyBackupsTaskCRD string
+
+	etcdCRDName                = "etcds.druid.gardener.cloud"
+	etcdCopyBackupsTaskCRDName = "etcdcopybackupstasks.druid.gardener.cloud"
+	crdResources               []string
+)
+
+type etcdCRD struct {
+	client  client.Client
+	applier kubernetes.Applier
+}
+
+func init() {
+	crdResources = append(crdResources, CRD, etcdCopyBackupsTaskCRD)
+}
+
+// NewCRD can be used to deploy the CRD definitions for Etcd and EtcdCopyBackupsTask.
+func NewCRD(c client.Client, applier kubernetes.Applier) component.Deployer {
+	return &etcdCRD{
+		client:  c,
+		applier: applier,
+	}
+}
+
+// Deploy creates and updates the CRD definitions for Etcd and EtcdCopyBackupsTask.
+func (e *etcdCRD) Deploy(ctx context.Context) error {
+	var fns []flow.TaskFn
+
+	for _, resource := range crdResources {
+		r := resource
+		fns = append(fns, func(ctx context.Context) error {
+			return e.applier.ApplyManifest(ctx, kubernetes.NewManifestReader([]byte(r)), kubernetes.DefaultMergeFuncs)
+		})
+	}
+
+	return flow.Parallel(fns...)(ctx)
+}
+
+func (e *etcdCRD) Destroy(ctx context.Context) error {
+	etcdList := &druidv1alpha1.EtcdList{}
+	// Need to check for both error types. The DynamicRestMapper can hold a stale cache returning a path to a non-existing api-resource leading to a NotFound error.
+	if err := e.client.List(ctx, etcdList); err != nil && !meta.IsNoMatchError(err) && !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	if len(etcdList.Items) > 0 {
+		return fmt.Errorf("cannot delete etcd CRDs because there are still druidv1alpha1.Etcd resources left in the cluster")
+	}
+
+	if err := gardenerutils.ConfirmDeletion(ctx, e.client, &apiextensionsv1.CustomResourceDefinition{ObjectMeta: metav1.ObjectMeta{Name: etcdCRDName}}); client.IgnoreNotFound(err) != nil {
+		return err
+	}
+
+	etcdCopyBackupsTaskList := &druidv1alpha1.EtcdCopyBackupsTaskList{}
+	if err := e.client.List(ctx, etcdCopyBackupsTaskList); err != nil && !meta.IsNoMatchError(err) && !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	if len(etcdCopyBackupsTaskList.Items) > 0 {
+		return fmt.Errorf("cannot delete etcd CRDs because there are still druidv1alpha1.EtcdCopyBackupsTask resources left in the cluster")
+	}
+
+	if err := gardenerutils.ConfirmDeletion(ctx, e.client, &apiextensionsv1.CustomResourceDefinition{ObjectMeta: metav1.ObjectMeta{Name: etcdCopyBackupsTaskCRDName}}); client.IgnoreNotFound(err) != nil {
+		return err
+	}
+
+	var fns []flow.TaskFn
+
+	for _, resource := range crdResources {
+		r := resource
+		fns = append(fns, func(ctx context.Context) error {
+			return client.IgnoreNotFound(e.applier.DeleteManifest(ctx, kubernetes.NewManifestReader([]byte(r))))
+		})
+	}
+
+	return flow.Parallel(fns...)(ctx)
+}
