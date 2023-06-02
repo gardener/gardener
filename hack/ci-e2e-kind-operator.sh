d9cde17b395aa945da50eb404a231b5067fb9843
acumino <sonu.kumar.singh02@sap.com>
2023-06-02 17:57:55 +0530

    Export resource YAMLs of `garden` for prow jobs
    Co-Authored-By: Seshachalam <104052572+seshachalam-yv@users.noreply.github.com>

--- hack/ci-e2e-kind-operator.sh
+++ hack/ci-e2e-kind-operator.sh
@@ -27,7 +27,7 @@ make kind-operator-up
 
 # export all container logs and events after test execution
 trap "
-  ( export KUBECONFIG=$PWD/example/gardener-local/kind/operator/kubeconfig; export_artifacts 'gardener-operator-local' )
+  ( export KUBECONFIG=$PWD/example/gardener-local/kind/operator/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden)
   ( make kind-operator-down )
 " EXIT
 
