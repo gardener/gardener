77ea4ce8defd8c3ac9c73c7a8937f71ccc4c4d6a
Ishan Tyagi <42602577+ishan16696@users.noreply.github.com>
2023-06-06 14:40:56 +0530

    Updated etcd-druid crds to v0.18.1 (#8031)
    Update the github.com/gardener/etcd-druid dependency to v0.18.1

---
+++ vendor/github.com/gardener/etcd-druid/pkg/utils/lease.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/gardener/etcd-druid/pkg/common"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	"github.com/go-logr/logr"
+	coordinationv1 "k8s.io/api/coordination/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// IsPeerURLTLSEnabled checks if the TLS has been enabled for all existing members of an etcd cluster identified by etcdName and in the provided namespace.
+func IsPeerURLTLSEnabled(ctx context.Context, cli client.Client, namespace, etcdName string, logger logr.Logger) (bool, error) {
+	var tlsEnabledValues []bool
+	labels := GetMemberLeaseLabels(etcdName)
+	leaseList := &coordinationv1.LeaseList{}
+	if err := cli.List(ctx, leaseList, client.InNamespace(namespace), client.MatchingLabels(labels)); err != nil {
+		return false, err
+	}
+	for _, lease := range leaseList.Items {
+		tlsEnabled := parseAndGetTLSEnabledValue(lease, logger)
+		if tlsEnabled != nil {
+			tlsEnabledValues = append(tlsEnabledValues, *tlsEnabled)
+		}
+	}
+	tlsEnabled := true
+	for _, v := range tlsEnabledValues {
+		tlsEnabled = tlsEnabled && v
+	}
+	return tlsEnabled, nil
+}
+
+// PurposeMemberLease is a constant used as a purpose for etcd member lease objects.
+const PurposeMemberLease = "etcd-member-lease"
+
+// GetMemberLeaseLabels creates a map of default labels for member lease.
+func GetMemberLeaseLabels(etcdName string) map[string]string {
+	return map[string]string{
+		common.GardenerOwnedBy:           etcdName,
+		v1beta1constants.GardenerPurpose: PurposeMemberLease,
+	}
+}
+
+func parseAndGetTLSEnabledValue(lease coordinationv1.Lease, logger logr.Logger) *bool {
+	const peerURLTLSEnabledKey = "member.etcd.gardener.cloud/tls-enabled"
+	if lease.Annotations != nil {
+		if tlsEnabledStr, ok := lease.Annotations[peerURLTLSEnabledKey]; ok {
+			tlsEnabled, err := strconv.ParseBool(tlsEnabledStr)
+			if err != nil {
+				logger.Error(err, "tls-enabled value is not a valid boolean", "namespace", lease.Namespace, "leaseName", lease.Name)
+				return nil
+			}
+			return &tlsEnabled
+		}
+		logger.V(4).Info("tls-enabled annotation not present for lease.", "namespace", lease.Namespace, "leaseName", lease.Name)
+	}
+	return nil
+}
