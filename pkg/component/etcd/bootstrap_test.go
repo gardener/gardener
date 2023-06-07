5ef1f3636d6c79514a6ae5decbd83ecbfbb527a9
Sonu Kumar Singh <sonu.kumar.singh02@sap.com>
2023-06-07 11:22:34 +0530

    Cleanup TODO (#8022)

--- pkg/component/etcd/bootstrap_test.go
+++ pkg/component/etcd/bootstrap_test.go
@@ -16,7 +16,6 @@ package etcd_test
 
 import (
 	"context"
-	_ "embed"
 	"fmt"
 	"time"
 
@@ -43,13 +42,6 @@ import (
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
 	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
 	. "github.com/gardener/gardener/pkg/utils/test/matchers"
-)
-
-var (
-	//go:embed crds/templates/crd-druid.gardener.cloud_etcds-copy.yaml
-	etcdCRD string
-	//go:embed crds/templates/crd-druid.gardener.cloud_etcdcopybackupstasks-copy.yaml
-	etcdCopyBackupsTaskCRD1 string
 )
 
 var _ = Describe("Etcd", func() {
@@ -479,8 +471,6 @@ status:
 					"verticalpodautoscaler__" + namespace + "__etcd-druid-vpa.yaml": []byte(vpaYAML),
 					"deployment__" + namespace + "__etcd-druid.yaml":                []byte(deploymentWithoutImageVectorOverwriteYAML),
 					"poddisruptionbudget__" + namespace + "__etcd-druid.yaml":       []byte(podDisruptionYAML),
-					"crd.yaml":                    []byte(etcdCRD),
-					"crdEtcdCopyBackupsTask.yaml": []byte(etcdCopyBackupsTaskCRD1),
 				},
 			}
 			managedResource = &resourcesv1alpha1.ManagedResource{
