feac8cfc1c85279694dd605a48d8d19f5ba5d4a9
Rafael Franzke <rafael.franzke@sap.com>
2023-06-09 17:54:16 +0200

    Copy remaining helper functions to `care` package
    Copied from https://github.com/gardener/gardener/blob/master/extensions/pkg/controller/healthcheck/worker/helpers.go

--- pkg/operation/care/checker.go
+++ pkg/operation/care/checker.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/Masterminds/semver"
 	druidv1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
+	machinev1alpha1 "github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -253,6 +254,114 @@ func (b *HealthChecker) checkNodes(condition gardencorev1beta1.Condition, nodes 
 	}
 
 	return nil
+}
+
+// annotationKeyNotManagedByMCM is a constant for an annotation on the node resource that indicates that the node is not
+// handled by machine-controller-manager.
+const annotationKeyNotManagedByMCM = "node.machine.sapcloud.io/not-managed-by-mcm"
+
+func checkMachineDeploymentsHealthy(machineDeployments []machinev1alpha1.MachineDeployment) (bool, error) {
+	for _, deployment := range machineDeployments {
+		for _, failedMachine := range deployment.Status.FailedMachines {
+			return false, fmt.Errorf("machine %q failed: %s", failedMachine.Name, failedMachine.LastOperation.Description)
+		}
+
+		if err := health.CheckMachineDeployment(&deployment); err != nil {
+			return false, fmt.Errorf("machine deployment %q in namespace %q is unhealthy: %w", deployment.Name, deployment.Namespace, err)
+		}
+	}
+
+	return true, nil
+}
+
+func getDesiredMachineCount(machineDeployments []machinev1alpha1.MachineDeployment) int {
+	desiredMachines := 0
+	for _, deployment := range machineDeployments {
+		if deployment.DeletionTimestamp == nil {
+			desiredMachines += int(deployment.Spec.Replicas)
+		}
+	}
+	return desiredMachines
+}
+
+func checkNodesScalingUp(machineList *machinev1alpha1.MachineList, readyNodes, desiredMachines int) error {
+	if readyNodes == desiredMachines {
+		return nil
+	}
+
+	if machineObjects := len(machineList.Items); machineObjects < desiredMachines {
+		return fmt.Errorf("not enough machine objects created yet (%d/%d)", machineObjects, desiredMachines)
+	}
+
+	var pendingMachines, erroneousMachines int
+	for _, machine := range machineList.Items {
+		switch machine.Status.CurrentStatus.Phase {
+		case machinev1alpha1.MachineRunning, machinev1alpha1.MachineAvailable:
+			// machine is already running fine
+			continue
+		case machinev1alpha1.MachinePending, "": // https://github.com/gardener/machine-controller-manager/issues/466
+			// machine is in the process of being created
+			pendingMachines++
+		default:
+			// undesired machine phase
+			erroneousMachines++
+		}
+	}
+
+	if erroneousMachines > 0 {
+		return fmt.Errorf("%s erroneous", cosmeticMachineMessage(erroneousMachines))
+	}
+	if pendingMachines == 0 {
+		return fmt.Errorf("not enough ready worker nodes registered in the cluster (%d/%d)", readyNodes, desiredMachines)
+	}
+
+	return fmt.Errorf("%s provisioning and should join the cluster soon", cosmeticMachineMessage(pendingMachines))
+}
+
+func checkNodesScalingDown(machineList *machinev1alpha1.MachineList, nodeList *corev1.NodeList, registeredNodes, desiredMachines int) error {
+	if registeredNodes == desiredMachines {
+		return nil
+	}
+
+	// Check if all nodes that are cordoned map to machines with a deletion timestamp. This might be the case during
+	// a rolling update.
+	nodeNameToMachine := map[string]machinev1alpha1.Machine{}
+	for _, machine := range machineList.Items {
+		if machine.Labels != nil && machine.Labels["node"] != "" {
+			nodeNameToMachine[machine.Labels["node"]] = machine
+		}
+	}
+
+	var cordonedNodes int
+	for _, node := range nodeList.Items {
+		if metav1.HasAnnotation(node.ObjectMeta, annotationKeyNotManagedByMCM) && node.Annotations[annotationKeyNotManagedByMCM] == "1" {
+			continue
+		}
+		if node.Spec.Unschedulable {
+			machine, ok := nodeNameToMachine[node.Name]
+			if !ok {
+				return fmt.Errorf("machine object for cordoned node %q not found", node.Name)
+			}
+			if machine.DeletionTimestamp == nil {
+				return fmt.Errorf("cordoned node %q found but corresponding machine object does not have a deletion timestamp", node.Name)
+			}
+			cordonedNodes++
+		}
+	}
+
+	// If there are still more nodes than desired then report an error.
+	if registeredNodes-cordonedNodes != desiredMachines {
+		return fmt.Errorf("too many worker nodes are registered. Exceeding maximum desired machine count (%d/%d)", registeredNodes, desiredMachines)
+	}
+
+	return fmt.Errorf("%s waiting to be completely drained from pods. If this persists, check your pod disruption budgets and pending finalizers. Please note, that nodes that fail to be drained will be deleted automatically", cosmeticMachineMessage(cordonedNodes))
+}
+
+func cosmeticMachineMessage(numberOfMachines int) string {
+	if numberOfMachines == 1 {
+		return fmt.Sprintf("%d machine is", numberOfMachines)
+	}
+	return fmt.Sprintf("%d machines are", numberOfMachines)
 }
 
 // CheckManagedResource checks the conditions of the given managed resource and reflects the state in the returned condition.
