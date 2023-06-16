fabfb6486f8f3a770ea65b523346140668b198f1
Shafeeque E S <shafeeque.e.s@sap.com>
2023-06-16 13:57:21 +0530

    Adapt kubernetes admission plugins functions

--- pkg/utils/kubernetes/admissionplugins.go
+++ pkg/utils/kubernetes/admissionplugins.go
@@ -23,11 +23,11 @@ import (
 )
 
 var (
-	lowestSupportedKubernetesVersionMajorMinor = "1.20"
+	lowestSupportedKubernetesVersionMajorMinor = "1.22"
 	lowestSupportedKubernetesVersion, _        = semver.NewVersion(lowestSupportedKubernetesVersionMajorMinor)
 
 	admissionPlugins = map[string][]gardencorev1beta1.AdmissionPlugin{
-		"1.20": getDefaultPlugins("1.20"),
+		"1.22": getDefaultPlugins("1.22"),
 		"1.23": getDefaultPlugins("1.23"),
 		"1.25": getDefaultPlugins("1.25"),
 	}
@@ -65,7 +65,7 @@ func getAdmissionPluginsForVersionInternal(v string) []gardencorev1beta1.Admissi
 func getDefaultPlugins(version string) []gardencorev1beta1.AdmissionPlugin {
 	var admissionPlugins []gardencorev1beta1.AdmissionPlugin
 	switch version {
-	case "1.20":
+	case "1.22":
 		admissionPlugins = append(admissionPlugins, []gardencorev1beta1.AdmissionPlugin{
 			{Name: "Priority"},
 			{Name: "NamespaceLifecycle"},
