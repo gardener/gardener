fce68405db27ab0ad4e8fc5999772e09c38a30dd
ary1992 <ashish.ranjan.yadav@sap.com>
2023-07-03 08:00:15 +0530

    Adapt extensions/pkg/controller/dnsrecord

--- extensions/pkg/controller/dnsrecord/controller.go
+++ extensions/pkg/controller/dnsrecord/controller.go
@@ -68,7 +68,7 @@ func DefaultPredicates(ignoreOperationAnnotation bool) []predicate.Predicate {
 
 // Add creates a new dnsrecord controller and adds it to the given Manager.
 func Add(mgr manager.Manager, args AddArgs) error {
-	args.ControllerOptions.Reconciler = NewReconciler(args.Actuator)
+	args.ControllerOptions.Reconciler = NewReconciler(mgr, args.Actuator)
 
 	ctrl, err := controller.New(ControllerName, mgr, args.ControllerOptions)
 	if err != nil {
