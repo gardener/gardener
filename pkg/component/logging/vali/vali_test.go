264f97ef40dbff6927c5f3258ff0248f8351a079
Niki Dokovski <nickytd@gmail.com>
2023-07-20 16:29:24 +0200

    fix(logging): fix vali ingress backend definition

--- pkg/component/logging/vali/vali_test.go
+++ pkg/component/logging/vali/vali_test.go
@@ -1154,7 +1154,7 @@ func getIngress() *networkingv1.Ingress {
 								{
 									Backend: networkingv1.IngressBackend{
 										Service: &networkingv1.IngressServiceBackend{
-											Name: "vali",
+											Name: "logging",
 											Port: networkingv1.ServiceBackendPort{
 												Number: 8080,
 											},
