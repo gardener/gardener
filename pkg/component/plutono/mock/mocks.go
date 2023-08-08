95e7be3f7ca83ba4300299f1f18fe9812d29414c
acumino <sonu.kumar.singh02@sap.com>
2023-08-08 10:01:11 +0530

    Run `make generate`

--- pkg/component/plutono/mock/mocks.go
+++ pkg/component/plutono/mock/mocks.go
@@ -8,7 +8,7 @@ import (
 	context "context"
 	reflect "reflect"
 
-	gomock "go.uber.org/mock/gomock"
+	gomock "github.com/golang/mock/gomock"
 )
 
 // MockInterface is a mock of Interface interface.
