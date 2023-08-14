82cb5285bab1e55c91854088f03f07ffa9fe97f1
Jeremy Rickards <jeremy.rickards@sap.com>
2023-08-14 16:51:48 +0200

    Enhance kubeproxy scrape job config
    Restricting the service discovery for kubeproxy to the `kube-system`
    namespace helps reduce the number of potential targets Prometheus has
    to discard during relabeling.

--- pkg/component/kubeproxy/monitoring.go
+++ pkg/component/kubeproxy/monitoring.go
@@ -81,9 +81,12 @@ tls_config:
 authorization:
   type: Bearer
   credentials_file: /var/run/secrets/gardener.cloud/shoot/token/token
+follow_redirects: false
 kubernetes_sd_configs:
 - role: endpoints
   api_server: https://` + v1beta1constants.DeploymentNameKubeAPIServer + `:` + strconv.Itoa(kubeapiserverconstants.Port) + `
+  namespaces:
+    names: [ kube-system ]
   tls_config:
     ca_file: /etc/prometheus/seed/ca.crt
   authorization:
