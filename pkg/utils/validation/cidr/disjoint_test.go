f9c13788bf8456068b44429d7e8d0cdf7388b299
Johannes Scheerer <johannes.scheerer@sap.com>
2023-08-14 13:09:40 +0200

    Add network disjointness check between shoot node network and seed pod network.
    In case there is an overlap between the shoot node network and the seed pod network
    there can be strange network issues due to incorrect layer 3 routing. For the default
    VPN, the affected connections are from kube-apiserver to vpn-seed-server and
    from istio-ingressgateway to vpn-seed-server. In both cases the reply (SYN-ACK) may
    be incorrectly sent into the VPN tunnel when the source pod IP overlaps with the
    shoot node network as the shoot node network is added as a special route along with
    the shoot service and shoot pod networks.
    In the highly-available VPN, the affected connections are from any pod in the seed to
    kube-apiserver of the shoot cluster. SYN-ACK packets may be sent incorrectly into the
    VPN tunnel as kube-apiserver has similar special route for shoot nodes, pods and
    services.
    This issue is not new, but was present also with the previous VPN solution.

--- pkg/utils/validation/cidr/disjoint_test.go
+++ pkg/utils/validation/cidr/disjoint_test.go
@@ -258,7 +258,31 @@ var _ = Describe("utils", func() {
 			var (
 				podsCIDR     = "10.242.128.0/17"
 				servicesCIDR = "10.242.0.0/17"
-				nodesCIDR    = "10.241.0.0/16"
+				nodesCIDR    = "10.241.0.0/17"
+			)
+
+			errorList := ValidateNetworkDisjointedness(
+				field.NewPath(""),
+				&nodesCIDR,
+				&podsCIDR,
+				&servicesCIDR,
+				&seedNodesCIDR,
+				seedPodsCIDR,
+				seedServicesCIDR,
+				false,
+			)
+
+			Expect(errorList).To(ConsistOf(PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeInvalid),
+				"Field": Equal("[].nodes"),
+			}))))
+		})
+
+		It("should fail due to seed pod network and shoot node network overlap", func() {
+			var (
+				podsCIDR     = "10.242.128.0/17"
+				servicesCIDR = "10.242.0.0/17"
+				nodesCIDR    = seedPodsCIDR
 			)
 
 			errorList := ValidateNetworkDisjointedness(
@@ -341,7 +365,7 @@ var _ = Describe("utils", func() {
 			var (
 				podsCIDR     = seedPodsCIDRIPv6
 				servicesCIDR = seedServicesCIDRIPv6
-				nodesCIDR    = "2001:0db8:65a3::/113"
+				nodesCIDR    = "2001:0db8:55a3::/112"
 			)
 
 			errorList := ValidateNetworkDisjointedness(
@@ -369,7 +393,7 @@ var _ = Describe("utils", func() {
 			var (
 				podsCIDR     = seedNodesCIDRIPv6
 				servicesCIDR = seedNodesCIDRIPv6
-				nodesCIDR    = "2001:0db8:65a3::/113"
+				nodesCIDR    = "2001:0db8:55a3::/112"
 			)
 
 			errorList := ValidateNetworkDisjointedness(
@@ -391,6 +415,30 @@ var _ = Describe("utils", func() {
 				"Field": Equal("[].services"),
 			})),
 			))
+		})
+
+		It("should fail due to seed pod network and shoot node network overlap", func() {
+			var (
+				podsCIDR     = "2001:0db8:35a3::/113"
+				servicesCIDR = "2001:0db8:45a3::/113"
+				nodesCIDR    = seedPodsCIDRIPv6
+			)
+
+			errorList := ValidateNetworkDisjointedness(
+				field.NewPath(""),
+				&nodesCIDR,
+				&podsCIDR,
+				&servicesCIDR,
+				&seedNodesCIDRIPv6,
+				seedPodsCIDRIPv6,
+				seedServicesCIDRIPv6,
+				false,
+			)
+
+			Expect(errorList).To(ConsistOf(PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeInvalid),
+				"Field": Equal("[].nodes"),
+			}))))
 		})
 	})
 
