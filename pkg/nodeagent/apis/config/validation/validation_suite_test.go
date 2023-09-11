e638cc3762823ba44adad0b1646f647c9e1bd340
Valentin Knabel <dev@vknabel.com>
2023-09-11 15:13:41 +0200

    fix(nodeagent): validation test specs

--- pkg/nodeagent/apis/config/validation/validation_suite_test.go
+++ pkg/nodeagent/apis/config/validation/validation_suite_test.go
@@ -23,5 +23,5 @@ import (
 
 func TestValidation(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "ControllerManager APIs Config Validation Suite")
+	RunSpecs(t, "NodeAgent APIs Config Validation Suite")
 }
