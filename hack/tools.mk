abec9ae7a83e91c22b5735611603d3207cc0fba6
oliver-goetz <o.goetz@sap.com>
2023-09-21 13:21:25 +0200

    Remove unversioned binaries from `gardenertools` image.
    Additionally, removing `golangci-lint` because importing dependent modules `gomegacheck` and `logcheck` fails atm.

--- hack/tools.mk
+++ hack/tools.mk
@@ -109,7 +109,7 @@ ifeq ($(shell if [ -d $(TOOLS_BIN_SOURCE_DIR) ]; then echo "found"; fi),found)
 endif
 
 .PHONY: create-tools-bin
-create-tools-bin: $(CONTROLLER_GEN) $(DOCFORGE) $(GEN_CRD_API_REFERENCE_DOCS) $(GINKGO) $(GOIMPORTS) $(GOIMPORTSREVISER) $(GOLANGCI_LINT) $(GOMEGACHECK) $(GO_ADD_LICENSE) $(GO_APIDIFF) $(GO_VULN_CHECK) $(GO_TO_PROTOBUF) $(HELM) $(IMPORT_BOSS) $(KIND) $(KUBECTL) $(LOGCHECK) $(MOCKGEN) $(OPENAPI_GEN) $(PROMTOOL) $(PROTOC) $(PROTOC_GEN_GOGO) $(REPORT_COLLECTOR) $(SETUP_ENVTEST) $(SKAFFOLD) $(YAML2JSON) $(YQ)
+create-tools-bin: $(DOCFORGE) $(GOIMPORTSREVISER) $(GO_ADD_LICENSE) $(GO_APIDIFF) $(GO_VULN_CHECK) $(HELM) $(KIND) $(KUBECTL) $(PROTOC) $(SKAFFOLD) $(YQ)
 
 #########################################
 # Tools                                 #
@@ -147,7 +147,7 @@ $(GOMEGACHECK): go.mod
 	CGO_ENABLED=1 go build -o $(GOMEGACHECK) -buildmode=plugin github.com/gardener/gardener/hack/tools/gomegacheck/plugin
 endif
 
-$(GO_ADD_LICENSE):
+$(GO_ADD_LICENSE):  $(call tool_version_file,$(GO_ADD_LICENSE),$(GO_ADD_LICENSE_VERSION))
 	GOBIN=$(abspath $(TOOLS_BIN_DIR)) go install github.com/google/addlicense@$(GO_ADD_LICENSE_VERSION)
 
 $(GO_APIDIFF): $(call tool_version_file,$(GO_APIDIFF),$(GO_APIDIFF_VERSION))
