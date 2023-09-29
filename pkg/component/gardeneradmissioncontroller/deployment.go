80feac852da0fcdbee16167cde509f067673e766
Rafael Franzke <rafael.franzke@sap.com>
2023-09-29 12:03:14 +0200

    Set `.spec.revisionHistoryLimit=2` for Gardener control plane `Deployment`s (#8578)
    According to point 3. of https://github.com/gardener/gardener/blob/master/docs/development/component-checklist.md#observability--operations-productivity

--- pkg/component/gardeneradmissioncontroller/deployment.go
+++ pkg/component/gardeneradmissioncontroller/deployment.go
@@ -51,7 +51,8 @@ func (a *gardenerAdmissionController) deployment(secretServerCert, secretGeneric
 			}),
 		},
 		Spec: appsv1.DeploymentSpec{
-			Replicas: pointer.Int32(1),
+			Replicas:             pointer.Int32(1),
+			RevisionHistoryLimit: pointer.Int32(2),
 			Selector: &metav1.LabelSelector{
 				MatchLabels: GetLabels(),
 			},
