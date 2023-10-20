2c6b221b4098b0b56f0b89d1bc5e4cb2eabaf5e6
Abhishek Dasgupta <a.dasgupta@sap.com>
2023-10-20 06:01:40 +0530

    Improved alerts for ETCD backup failures. (#8355)
    * Formats the snapshots alerts for better readablility.
    
    * Improved alerts for ETCD backup failures.
    
    Previously, the expression checked that the last snapshot is older than
    a specified time, and a full or incremental backup is required, and
    the etcd instance is a leader. It checked this by using arithmetics and
    bool comparison expressions. In this commit,  it is performing an equivalent check
    by cartessian product of  all the values and checking if the result is
    more than 0.
    
    Example: The first part `((time() -
    etcdbr_snapshot_latest_timestamp{job="kube-etcd3-backup-restore-main",kind="Incr"}
    > bool 900)` returns a set of ETCD instances, if metrics `etcdbr_snapshot_latest_timestamp`
    with `kind="Incr"` is older than 900 seconds.
    The second part
    `etcdbr_snapshot_required{job="kube-etcd3-backup-restore-main",kind="Incr"})`
    returns a set of ETCD instances if metrics `etcdbr_snapshot_required`
    with `kind="Incr"` is set.
    The third part `on (pod, role)
    etcd_server_is_leader{job="kube-etcd3-main"}` returns the leader of ETCD instances on pod and role.
    If the cartessian product of the three parts is greater than 0 then an
    alert is raised. In plain English word, if the leader of ETCD instance
    requires an incremental snapshot to be taken  and has
    last incremental snapshot that is more than 900 seconds old, then an
    alert is raised.

--- pkg/component/etcd/monitoring.go
+++ pkg/component/etcd/monitoring.go
@@ -173,7 +173,20 @@ const (
   {{- if .backupEnabled }}
   # etcd backup failure alerts
   - alert: KubeEtcdDeltaBackupFailed
-    expr: ((time() - ` + monitoringMetricBackupRestoreSnapshotLatestTimestamp + `{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}",kind="Incr"} > bool 900) + (etcdbr_snapshot_required{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}", kind="Incr"} >= bool 1) == 2) + on(pod,role) group_left 0 * (` + monitoringMetricEtcdServerIsLeader + `{job="` + monitoringPrometheusJobEtcdNamePrefix + `-{{ .role }}"} == 1 )
+    expr:
+            (
+                (
+                    time() - ` + monitoringMetricBackupRestoreSnapshotLatestTimestamp + `{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}",kind="Incr"}
+                  > bool
+                    900
+                )
+              *
+                etcdbr_snapshot_required{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}",kind="Incr"}
+            )
+          * on (pod, role)
+            ` + monitoringMetricEtcdServerIsLeader + `{job="` + monitoringPrometheusJobEtcdNamePrefix + `-{{ .role }}"}
+        >
+          0
     for: 15m
     labels:
       service: etcd
@@ -181,10 +194,23 @@ const (
       type: seed
       visibility: operator
     annotations:
-      description: No delta snapshot for the past at least 30 minutes taken by backup-restore leader.
+      description: No delta snapshot for the past 30 minutes have been taken by backup-restore leader.
       summary: Etcd delta snapshot failure.
   - alert: KubeEtcdFullBackupFailed
-    expr: ((time() - ` + monitoringMetricBackupRestoreSnapshotLatestTimestamp + `{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}",kind="Full"} > bool 86400) + (etcdbr_snapshot_required{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}", kind="Full"} >= bool 1) == 2) + on(pod,role) group_left 0 * (` + monitoringMetricEtcdServerIsLeader + `{job="` + monitoringPrometheusJobEtcdNamePrefix + `-{{ .role }}"} == 1 )
+    expr:
+            (
+                (
+                    time() - ` + monitoringMetricBackupRestoreSnapshotLatestTimestamp + `{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}",kind="Full"}
+                  > bool
+                    86400
+                )
+              *
+                etcdbr_snapshot_required{job="` + monitoringPrometheusJobBackupRestoreNamePrefix + `-{{ .role }}",kind="Full"}
+            )
+          * on (pod, role)
+            ` + monitoringMetricEtcdServerIsLeader + `{job="` + monitoringPrometheusJobEtcdNamePrefix + `-{{ .role }}"}
+        >
+          0
     for: 15m
     labels:
       service: etcd
@@ -192,7 +218,7 @@ const (
       type: seed
       visibility: operator
     annotations:
-      description: No full snapshot taken in the past day taken by backup-restore leader.
+      description: No full snapshot for at least last 24 hours have been taken by backup-restore leader.
       summary: Etcd full snapshot failure.
 
   # etcd data restoration failure alert
