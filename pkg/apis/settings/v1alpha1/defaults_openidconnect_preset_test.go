afd216efc3fdad6877a291a822cf7806a296b7d7
Viktor <mail.victor.kostov@gmail.com>
2023-11-06 16:52:27 +0200

    [OpenIDConnectPreset] Add unit tests

--- pkg/apis/settings/v1alpha1/defaults_openidconnect_preset_test.go
+++ pkg/apis/settings/v1alpha1/defaults_openidconnect_preset_test.go
@@ -17,21 +17,70 @@ package v1alpha1_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/gardener/gardener/pkg/apis/settings/v1alpha1"
 )
 
-var _ = Describe("#SetDefaults_OpenIDConnectPreset", func() {
+var _ = Describe("OpenIDConnectPreset defaulting", func() {
+	var (
+		given    *v1alpha1.OpenIDConnectPreset
+		expected *v1alpha1.OpenIDConnectPreset
+	)
 
-	It("correct defaults are set", func() {
-		given := &v1alpha1.OpenIDConnectPreset{}
-		expected := &v1alpha1.OpenIDConnectPreset{
-			Spec: defaultSpec(),
+	BeforeEach(func() {
+		given = &v1alpha1.OpenIDConnectPreset{}
+		usernameClaim := "sub"
+		expected = &v1alpha1.OpenIDConnectPreset{
+			Spec: v1alpha1.OpenIDConnectPresetSpec{
+				Server: v1alpha1.KubeAPIServerOpenIDConnect{
+					// string literal are used to be sure that the test fails
+					// if the constant values are changed.
+					UsernameClaim: &usernameClaim,
+					SigningAlgs:   []string{"RS256"},
+				},
+				ShootSelector: &metav1.LabelSelector{},
+			},
 		}
+	})
+
+	It("should default OpenIDConnectPreset correctly", func() {
 
 		v1alpha1.SetDefaults_OpenIDConnectPreset(given)
 
 		Expect(given).To(BeEquivalentTo(expected))
+	})
+
+	It("should not default ShootSelector if it is already set", func() {
+		given.Spec.ShootSelector = &metav1.LabelSelector{MatchLabels: map[string]string{"foo": "bar"}}
+
+		v1alpha1.SetDefaults_OpenIDConnectPreset(given)
+
+		expected.Spec.ShootSelector = &metav1.LabelSelector{MatchLabels: map[string]string{"foo": "bar"}}
+		Expect(given).To(BeEquivalentTo(expected))
+
+	})
+
+	It("should not default SigningAlgs if they are already set", func() {
+		given.Spec.Server.SigningAlgs = []string{"alg1", "alg2"}
+
+		v1alpha1.SetDefaults_OpenIDConnectPreset(given)
+
+		expected.Spec.Server.SigningAlgs = []string{"alg1", "alg2"}
+		Expect(given).To(BeEquivalentTo(expected))
+
+	})
+
+	It("should not default UsernameClaim if it is already set", func() {
+		usernameClaim := "usr"
+		given.Spec.Server.UsernameClaim = &usernameClaim
+
+		v1alpha1.SetDefaults_OpenIDConnectPreset(given)
+
+		expectedUsernameClaim := "usr"
+		expected.Spec.Server.UsernameClaim = &expectedUsernameClaim
+		Expect(given).To(BeEquivalentTo(expected))
+
 	})
 
 })
