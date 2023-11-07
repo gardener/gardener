e86bd8955fc57821498839181913f297502a1f8c
acumino <sonu.kumar.singh02@sap.com>
2023-11-07 15:33:24 +0530

    pkg/extensions

--- pkg/extensions/cluster_test.go
+++ pkg/extensions/cluster_test.go
@@ -16,11 +16,13 @@ package extensions_test
 
 import (
 	"context"
+	"encoding/json"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
@@ -59,6 +61,346 @@ var _ = Describe("Cluster", func() {
 			metav1.SetMetaDataAnnotation(&cluster.ObjectMeta, "generic-token-kubeconfig.secret.gardener.cloud/name", name)
 
 			Expect(GenericTokenKubeconfigSecretNameFromCluster(cluster)).To(Equal(name))
+		})
+	})
+
+	Describe("#SyncClusterResourceToSeed", func() {
+		var (
+			expectedCloudProfile *gardencorev1beta1.CloudProfile
+			expectedSeed         *gardencorev1beta1.Seed
+			expectedShoot        *gardencorev1beta1.Shoot
+
+			clusterName string
+			cluster     *extensionsv1alpha1.Cluster
+		)
+
+		BeforeEach(func() {
+			expectedCloudProfile = &gardencorev1beta1.CloudProfile{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "CloudProfile",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			expectedSeed = &gardencorev1beta1.Seed{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "Seed",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			expectedShoot = &gardencorev1beta1.Shoot{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "Shoot",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "garden-bar",
+				},
+			}
+
+			clusterName = "shoot--" + expectedShoot.Namespace + "--" + expectedShoot.Name
+			cluster = &extensionsv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+			}
+		})
+
+		It("should not sync cloudprofile, shoot and seed to cluster if seed is not assigned to shoot", func() {
+			Expect(fakeGardenClient.Create(ctx, expectedCloudProfile)).To(Succeed())
+			Expect(fakeGardenClient.Create(ctx, expectedSeed)).To(Succeed())
+			Expect(fakeGardenClient.Create(ctx, expectedShoot)).To(Succeed())
+			Expect(fakeSeedClient.Create(ctx, cluster)).To(Succeed())
+
+			Expect(SyncClusterResourceToSeed(ctx, fakeSeedClient, cluster.Name, expectedShoot, expectedCloudProfile, expectedSeed)).To(Succeed())
+			Expect(fakeSeedClient.Get(ctx, client.ObjectKeyFromObject(cluster), cluster)).To(Succeed())
+
+			Expect(cluster.Spec.CloudProfile.Object).To(BeNil())
+			Expect(cluster.Spec.Seed.Object).To(BeNil())
+			Expect(cluster.Spec.Shoot.Object).To(BeNil())
+		})
+
+		It("should sync cloudprofile, shoot and seed to cluster", func() {
+			expectedShoot.Spec.SeedName = pointer.String(expectedSeed.Name)
+
+			Expect(fakeGardenClient.Create(ctx, expectedCloudProfile)).To(Succeed())
+			Expect(fakeGardenClient.Create(ctx, expectedSeed)).To(Succeed())
+			Expect(fakeGardenClient.Create(ctx, expectedShoot)).To(Succeed())
+			Expect(fakeSeedClient.Create(ctx, cluster)).To(Succeed())
+
+			Expect(SyncClusterResourceToSeed(ctx, fakeSeedClient, cluster.Name, expectedShoot, expectedCloudProfile, expectedSeed)).To(Succeed())
+			Expect(fakeSeedClient.Get(ctx, client.ObjectKeyFromObject(cluster), cluster)).To(Succeed())
+
+			Expect(cluster.Spec.CloudProfile.Raw).To(Not(BeNil()))
+			Expect(cluster.Spec.Seed.Raw).To(Not(BeNil()))
+			Expect(cluster.Spec.Shoot.Raw).To(Not(BeNil()))
+		})
+	})
+
+	Describe("#GetCluster", func() {
+		var (
+			expectedCloudProfile *gardencorev1beta1.CloudProfile
+			expectedSeed         *gardencorev1beta1.Seed
+			expectedShoot        *gardencorev1beta1.Shoot
+
+			clusterName     string
+			cluster         *extensionsv1alpha1.Cluster
+			expectedCluster *Cluster
+		)
+
+		BeforeEach(func() {
+			expectedCloudProfile = &gardencorev1beta1.CloudProfile{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "CloudProfile",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			expectedSeed = &gardencorev1beta1.Seed{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "Seed",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			expectedShoot = &gardencorev1beta1.Shoot{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "Shoot",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "garden-bar",
+				},
+			}
+
+			clusterName = "shoot--" + expectedShoot.Namespace + "--" + expectedShoot.Name
+			cluster = &extensionsv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+				Spec: extensionsv1alpha1.ClusterSpec{
+					CloudProfile: runtime.RawExtension{
+						Object: expectedCloudProfile,
+					},
+					Seed: runtime.RawExtension{
+						Object: expectedSeed,
+					},
+					Shoot: runtime.RawExtension{
+						Object: expectedShoot,
+					},
+				},
+			}
+
+			expectedCluster = &Cluster{
+				ObjectMeta:   cluster.ObjectMeta,
+				CloudProfile: expectedCloudProfile,
+				Seed:         expectedSeed,
+				Shoot:        expectedShoot,
+			}
+		})
+
+		It("should return error if cluster not found", func() {
+			cluster, err := GetCluster(ctx, fakeSeedClient, "foo")
+			Expect(err).To(MatchError(ContainSubstring("clusters.extensions.gardener.cloud \"foo\" not found")))
+			Expect(cluster).To(BeNil())
+		})
+
+		It("should get the cluster", func() {
+			Expect(fakeGardenClient.Create(ctx, expectedCloudProfile)).To(Succeed())
+			Expect(fakeGardenClient.Create(ctx, expectedSeed)).To(Succeed())
+			Expect(fakeGardenClient.Create(ctx, expectedShoot)).To(Succeed())
+			Expect(fakeSeedClient.Create(ctx, cluster)).To(Succeed())
+
+			cluster, err := GetCluster(ctx, fakeSeedClient, cluster.Name)
+			Expect(err).NotTo(HaveOccurred())
+			expectedCluster.ObjectMeta.ResourceVersion = "1"
+			Expect(cluster).To(Equal(expectedCluster))
+		})
+	})
+
+	Describe("#CloudProfileFromCluster", func() {
+		var (
+			expectedCloudProfile *gardencorev1beta1.CloudProfile
+
+			clusterName string
+			cluster     *extensionsv1alpha1.Cluster
+		)
+
+		BeforeEach(func() {
+			expectedCloudProfile = &gardencorev1beta1.CloudProfile{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "CloudProfile",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			clusterName = "foo"
+			cluster = &extensionsv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+				Spec: extensionsv1alpha1.ClusterSpec{
+					CloudProfile: runtime.RawExtension{
+						Raw: encode(expectedCloudProfile),
+					},
+				},
+			}
+		})
+
+		It("should retrieve cloudprofile from cluster", func() {
+			cloudProfile, err := CloudProfileFromCluster(cluster)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cloudProfile).To(Equal(expectedCloudProfile))
+		})
+
+		It("should return an error because the cloudprofile cannot be decoded the cluster", func() {
+			cluster.Spec.CloudProfile.Raw = []byte(`{`)
+			Expect(fakeSeedClient.Create(ctx, cluster)).To(Succeed())
+
+			cloudProfile, err := CloudProfileFromCluster(cluster)
+			Expect(err).To(MatchError(ContainSubstring("unexpected end of JSON input")))
+			Expect(cloudProfile).To(BeNil())
+		})
+
+		It("should return an nil because the cloudprofile is not in raw format the cluster", func() {
+			cluster.Spec.CloudProfile.Raw = nil
+
+			cloudProfile, err := CloudProfileFromCluster(cluster)
+			Expect(err).To(BeNil())
+			Expect(cloudProfile).To(BeNil())
+		})
+	})
+
+	Describe("#SeedFromCluster", func() {
+		var (
+			expectedSeed *gardencorev1beta1.Seed
+
+			clusterName string
+			cluster     *extensionsv1alpha1.Cluster
+		)
+
+		BeforeEach(func() {
+			expectedSeed = &gardencorev1beta1.Seed{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "Seed",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			clusterName = "foo"
+			cluster = &extensionsv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+				Spec: extensionsv1alpha1.ClusterSpec{
+					Seed: runtime.RawExtension{
+						Raw: encode(expectedSeed),
+					},
+				},
+			}
+		})
+
+		It("should retrieve seed from cluster", func() {
+			seed, err := SeedFromCluster(cluster)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(seed).To(Equal(expectedSeed))
+		})
+
+		It("should return an error because the seed cannot be decoded the cluster", func() {
+			cluster.Spec.Seed.Raw = []byte(`{`)
+			Expect(fakeSeedClient.Create(ctx, cluster)).To(Succeed())
+
+			seed, err := SeedFromCluster(cluster)
+			Expect(err).To(MatchError(ContainSubstring("unexpected end of JSON input")))
+			Expect(seed).To(BeNil())
+		})
+
+		It("should return an nil because the seed is not in raw format the cluster", func() {
+			cluster.Spec.Seed.Raw = nil
+
+			seed, err := SeedFromCluster(cluster)
+			Expect(err).To(BeNil())
+			Expect(seed).To(BeNil())
+		})
+	})
+
+	Describe("#ShootFromCluster", func() {
+		var (
+			expectedShoot *gardencorev1beta1.Shoot
+
+			clusterName string
+			cluster     *extensionsv1alpha1.Cluster
+		)
+
+		BeforeEach(func() {
+			expectedShoot = &gardencorev1beta1.Shoot{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "core.gardener.cloud/v1beta1",
+					Kind:       "Shoot",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo",
+				},
+			}
+
+			clusterName = "foo"
+			cluster = &extensionsv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+				Spec: extensionsv1alpha1.ClusterSpec{
+					Shoot: runtime.RawExtension{
+						Raw: encode(expectedShoot),
+					},
+				},
+			}
+		})
+
+		It("should retrieve shoot from cluster", func() {
+			shoot, err := ShootFromCluster(cluster)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(shoot).To(Equal(expectedShoot))
+		})
+
+		It("should return an error because the shoot cannot be decoded the cluster", func() {
+			cluster.Spec.Shoot.Raw = []byte(`{`)
+			Expect(fakeSeedClient.Create(ctx, cluster)).To(Succeed())
+
+			shoot, err := ShootFromCluster(cluster)
+			Expect(err).To(MatchError(ContainSubstring("unexpected end of JSON input")))
+			Expect(shoot).To(BeNil())
+		})
+
+		It("should return an nil because the shoot is not in raw format the cluster", func() {
+			cluster.Spec.Shoot.Raw = nil
+
+			shoot, err := ShootFromCluster(cluster)
+			Expect(err).To(BeNil())
+			Expect(shoot).To(BeNil())
 		})
 	})
 
@@ -156,3 +498,9 @@ var _ = Describe("Cluster", func() {
 		})
 	})
 })
+
+func encode(obj runtime.Object) []byte {
+	bytes, err := json.Marshal(obj)
+	Expect(err).NotTo(HaveOccurred())
+	return bytes
+}
