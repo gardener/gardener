2ebfb04e363722dfba35cdcc374f997f9920842e
Tim Ebert <timebertt@gmail.com>
2023-11-13 15:17:15 +0100

    Drop unwanted annotations from referenced resources (#8803)

--- pkg/operation/botanist/resources.go
+++ pkg/operation/botanist/resources.go
@@ -51,6 +51,15 @@ func (b *Botanist) DeployReferencedResources(ctx context.Context) error {
 		unstructuredObj := &unstructured.Unstructured{Object: obj}
 		unstructuredObj.SetNamespace(b.Shoot.SeedNamespace)
 		unstructuredObj.SetName(v1beta1constants.ReferencedResourcesPrefix + unstructuredObj.GetName())
+
+		// Drop unwanted annotations before copying the resource to the seed.
+		// All annotations contained in the ManagedResource secret will end up in `ManagedResource.status.resources[].annotations`.
+		// We don't want this to happen for the last applied annotation of secrets, which includes the secret data in plain
+		// text. This would put sensitive secret data into the ManagedResource object which is probably unencrypted in etcd.
+		annotations := unstructuredObj.GetAnnotations()
+		delete(annotations, "kubectl.kubernetes.io/last-applied-configuration")
+		unstructuredObj.SetAnnotations(annotations)
+
 		unstructuredObjs = append(unstructuredObjs, unstructuredObj)
 	}
 
