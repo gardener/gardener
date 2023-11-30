886c7b0597906e12ff7083c6ff9cbf08b66d10d0
Rafael Franzke <rafael.franzke@sap.com>
2023-11-30 15:53:09 +0100

    Address PR review feedback

--- pkg/apis/authentication/v1alpha1/types_viewerkubeconfigrequest.go
+++ pkg/apis/authentication/v1alpha1/types_viewerkubeconfigrequest.go
@@ -39,9 +39,9 @@ type ViewerKubeconfigRequest struct {
 // the kubeconfig and expiration of the credential.
 type ViewerKubeconfigRequestStatus struct {
 	// Kubeconfig contains the kubeconfig with viewer privileges (excluding Secrets) for the shoot cluster.
-	Kubeconfig []byte `json:"kubeconfig" protobuf:"bytes,1,name=kubeconfig"`
+	Kubeconfig []byte `json:"kubeconfig" protobuf:"bytes,1,opt,name=kubeconfig"`
 	// ExpirationTimestamp is the expiration timestamp of the returned credential.
-	ExpirationTimestamp metav1.Time `json:"expirationTimestamp" protobuf:"bytes,2,name=expirationTimestamp"`
+	ExpirationTimestamp metav1.Time `json:"expirationTimestamp" protobuf:"bytes,2,opt,name=expirationTimestamp"`
 }
 
 // ViewerKubeconfigRequestSpec contains the expiration time of the kubeconfig.
