d632bdd1178bc8c3f01daa16dc3a5610360946bb
Kumm-Kai <70690427+Kumm-Kai@users.noreply.github.com>
2023-12-04 09:17:25 +0000

    Local-Setup: add node internal IP addresses to istio-ingressgateway LB service (#8792)
    * service controller (local-setup): add node internal IP addresses to istio-ingressgateway loadbalancer service in local setup
    
    * fix missing rbac for operator
    
    * Fix loadbalancer ingress IP order

--- pkg/controller/service/reconciler.go
+++ pkg/controller/service/reconciler.go
@@ -107,7 +107,27 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 	}
 
 	patch = client.MergeFrom(service.DeepCopy())
-	service.Status.LoadBalancer.Ingress = []corev1.LoadBalancerIngress{{IP: ip}}
+	service.Status.LoadBalancer.Ingress = []corev1.LoadBalancerIngress{}
+
+	if key != keyNginxIngress {
+		nodes := &corev1.NodeList{}
+		if err := r.Client.List(ctx, nodes); err != nil {
+			return reconcile.Result{}, err
+		}
+
+		for _, node := range nodes.Items {
+			for _, address := range node.Status.Addresses {
+				if address.Type == corev1.NodeInternalIP {
+					service.Status.LoadBalancer.Ingress = append(service.Status.LoadBalancer.Ingress, corev1.LoadBalancerIngress{IP: address.Address})
+				}
+			}
+		}
+	}
+
+	// We append this IP behind the nodeIPs because the gardenlet puts the last ingress IP into the DNSRecords
+	// and we need a change in the DNSRecords if the control-plane moved from one zone to another to update the coredns-custom configMap.
+	service.Status.LoadBalancer.Ingress = append(service.Status.LoadBalancer.Ingress, corev1.LoadBalancerIngress{IP: ip})
+
 	return reconcile.Result{}, r.Client.Status().Patch(ctx, service, patch)
 }
 
