d415d526343f19f6c5c3322f7b83e08da8951c0c
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-12-12 14:59:56 +0100

    [release-v1.84] Add support for optional `REPO_ROOT` environment var in `vgopath` enabled hack scripts (#8944)
    * Add support for optional SCRIPT_ROOT environment variable
    
    This commit introduces the ability to use an optional environment variable, SCRIPT_ROOT,
    in the vgopath enabled generation scripts script. When SCRIPT_ROOT is set, the script will
    use this directory instead of the default $SCRIPT_DIR/.. for setting up the virtual go environment.
    This enhancement provides flexibility for different runtime environments where the script's working
    directory might need to be dynamically specified.
    
    * Factor out vgopath setup into sourcable part
    
    * Update hack/vgopath-setup.sh
    
    Co-authored-by: Martin Weindel <martin.weindel@sap.com>
    
    ---------
    
    Co-authored-by: Andreas Fritzler <andreas.fritzler@sap.com>
    Co-authored-by: Andreas Fritzler <andreas.fritzler@gmail.com>
    Co-authored-by: Martin Weindel <martin.weindel@sap.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -42,23 +42,8 @@ AVAILABLE_CODEGEN_OPTIONS=(
   "nodeagent_groups"
 )
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-VGOPATH="$VGOPATH"
-
-# Ensure that if GOPATH is set, the GOPATH/{bin,pkg} directory exists. This seems to be not always
-# the case in certain environments like Prow. As we will create a symlink against the bin folder we
-# need to make sure that the bin directory is present in the GOPATH.
-if [ -n "$GOPATH" ] && [ ! -d "$GOPATH/bin" ]; then mkdir -p "$GOPATH/bin"; fi
-if [ -n "$GOPATH" ] && [ ! -d "$GOPATH/pkg" ]; then mkdir -p "$GOPATH/pkg"; fi
-
-VIRTUAL_GOPATH="$(mktemp -d)"
-trap 'rm -rf "$VIRTUAL_GOPATH"' EXIT
-
-# Setup virtual GOPATH so the codegen tools work as expected.
-(cd "$SCRIPT_DIR/.."; go mod download && "$VGOPATH" -o "$VIRTUAL_GOPATH")
-
-export GOROOT="${GOROOT:-"$(go env GOROOT)"}"
-export GOPATH="$VIRTUAL_GOPATH"
+# setup virtual GOPATH
+source $(dirname $0)/vgopath-setup.sh
 
 # We need to explicitly pass GO111MODULE=off to k8s.io/code-generator as it is significantly slower otherwise,
 # see https://github.com/kubernetes/code-generator/issues/100.
