f2bd22a31dc4f39fd20da9110805685041f2fe47
Andreas Fritzler <andreas.fritzler@sap.com>
2023-12-19 16:55:13 +0100

    Change license header in hack

--- hack/download_gardener_source_code.sh
+++ hack/download_gardener_source_code.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
-# Copyright 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Gardener contributors
+# SPDX-License-Identifier: Apache-2.0
 
 set -o errexit
 set -o nounset
@@ -38,6 +27,6 @@ parse_flags() {
 
 parse_flags "$@"
 
-curl -sL "https://codeload.github.com/gardener/gardener/legacy.tar.gz/${GARDENER_VERSION}" -o /tmp/source_code.tar.gz && 
+curl -sL "https://codeload.github.com/gardener/gardener/legacy.tar.gz/${GARDENER_VERSION}" -o /tmp/source_code.tar.gz &&
   mkdir -p "${GARDENER_RELEASE_DOWNLOAD_PATH}/${GARDENER_VERSION}" &&
   tar -C "${GARDENER_RELEASE_DOWNLOAD_PATH}/${GARDENER_VERSION}" -xzf /tmp/source_code.tar.gz --strip-components=1 && rm /tmp/source_code.tar.gz
