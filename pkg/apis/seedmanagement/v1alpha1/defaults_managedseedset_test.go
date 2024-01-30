0f900068f0b189d8474fb89a891b051d9e6fd551
Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>
2024-01-30 19:35:29 +0530

    [SeedManagement] Clean up technical debt in defaulting code (#9033)
    * [ManagedSeed] Unit test overall defaulting function
    
    * [ManagedSeed] Refactor defaulting to SetDefaults_* functions
    
    * [ManagedSeed] Refactor defaulting and add missing unit tests
    
    * [ManagedSeedSet] Unit test overall defaulting function
    
    * [ManagedSeedSet] Refactor and add missing unit tests
    
    * Address PR review feedback

--- pkg/apis/seedmanagement/v1alpha1/defaults_managedseedset_test.go
+++ pkg/apis/seedmanagement/v1alpha1/defaults_managedseedset_test.go
@@ -23,54 +23,78 @@ import (
 )
 
 var _ = Describe("Defaults", func() {
-	Describe("#SetDefaults_ManagedSeedSet", func() {
-		var obj *ManagedSeedSet
+	var obj *ManagedSeedSet
 
-		BeforeEach(func() {
-			obj = &ManagedSeedSet{}
+	BeforeEach(func() {
+		obj = &ManagedSeedSet{}
+	})
+
+	Describe("ManagedSeedSet defaulting", func() {
+		It("should default replicas to 1 and revisionHistoryLimit to 10", func() {
+			SetObjectDefaults_ManagedSeedSet(obj)
+
+			Expect(obj.Spec.Replicas).To(Equal(pointer.Int32(1)))
+			Expect(obj.Spec.UpdateStrategy).NotTo(BeNil())
+			Expect(obj.Spec.RevisionHistoryLimit).To(Equal(pointer.Int32(10)))
 		})
 
-		It("should default replicas to 1 and revisionHistoryLimit to 10", func() {
-			SetDefaults_ManagedSeedSet(obj)
+		It("should not overwrite the already set values for ManagedSeedSet spec", func() {
+			obj.Spec = ManagedSeedSetSpec{
+				Replicas:             pointer.Int32(5),
+				RevisionHistoryLimit: pointer.Int32(15),
+			}
+			SetObjectDefaults_ManagedSeedSet(obj)
 
-			Expect(obj).To(Equal(&ManagedSeedSet{
-				Spec: ManagedSeedSetSpec{
-					Replicas:             pointer.Int32(1),
-					UpdateStrategy:       &UpdateStrategy{},
-					RevisionHistoryLimit: pointer.Int32(10),
-				},
-			}))
+			Expect(obj.Spec.Replicas).To(Equal(pointer.Int32(5)))
+			Expect(obj.Spec.UpdateStrategy).NotTo(BeNil())
+			Expect(obj.Spec.RevisionHistoryLimit).To(Equal(pointer.Int32(15)))
 		})
 	})
 
-	Describe("#SetDefaults_UpdateStrategy", func() {
-		var obj *UpdateStrategy
-
-		BeforeEach(func() {
-			obj = &UpdateStrategy{}
-		})
-
+	Describe("UpdateStrategy defaulting", func() {
 		It("should default type to RollingUpdate", func() {
-			SetDefaults_UpdateStrategy(obj)
+			obj.Spec.UpdateStrategy = &UpdateStrategy{}
+			SetObjectDefaults_ManagedSeedSet(obj)
 
-			Expect(obj).To(Equal(&UpdateStrategy{
+			Expect(obj.Spec.UpdateStrategy).To(Equal(&UpdateStrategy{
 				Type: updateStrategyTypePtr(RollingUpdateStrategyType),
 			}))
 		})
+
+		It("should not overwrite already set values for UpdateStrategy", func() {
+			obj.Spec.UpdateStrategy = &UpdateStrategy{
+				Type: updateStrategyTypePtr(UpdateStrategyType("foo")),
+			}
+			SetObjectDefaults_ManagedSeedSet(obj)
+
+			Expect(obj.Spec.UpdateStrategy).To(Equal(&UpdateStrategy{
+				Type: updateStrategyTypePtr(UpdateStrategyType("foo")),
+			}))
+		})
 	})
 
-	Describe("#SetDefaults_RollingUpdateStrategy", func() {
-		var obj *RollingUpdateStrategy
+	Describe("RollingUpdateStrategy defaulting", func() {
+		It("should default partition to 0", func() {
+			obj.Spec.UpdateStrategy = &UpdateStrategy{
+				RollingUpdate: &RollingUpdateStrategy{},
+			}
+			SetObjectDefaults_ManagedSeedSet(obj)
 
-		BeforeEach(func() {
-			obj = &RollingUpdateStrategy{}
+			Expect(obj.Spec.UpdateStrategy.RollingUpdate).To(Equal(&RollingUpdateStrategy{
+				Partition: pointer.Int32(0),
+			}))
 		})
 
-		It("should default partition to 0", func() {
-			SetDefaults_RollingUpdateStrategy(obj)
+		It("should not overwrote the already set values for RollingUpdateStrategy", func() {
+			obj.Spec.UpdateStrategy = &UpdateStrategy{
+				RollingUpdate: &RollingUpdateStrategy{
+					Partition: pointer.Int32(1),
+				},
+			}
+			SetObjectDefaults_ManagedSeedSet(obj)
 
-			Expect(obj).To(Equal(&RollingUpdateStrategy{
-				Partition: pointer.Int32(0),
+			Expect(obj.Spec.UpdateStrategy.RollingUpdate).To(Equal(&RollingUpdateStrategy{
+				Partition: pointer.Int32(1),
 			}))
 		})
 	})
