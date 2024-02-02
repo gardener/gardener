240250ae50641898ae1e00d00727143c366257a7
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-02-02 19:16:12 +0100

    Log `hostname` in `gardener-node-agent` (#9115)
    Co-authored-by: oliver-goetz <o.goetz@sap.com>

--- cmd/gardener-node-agent/app/app.go
+++ cmd/gardener-node-agent/app/app.go
@@ -156,6 +156,7 @@ func run(ctx context.Context, cancel context.CancelFunc, log logr.Logger, cfg *c
 		return fmt.Errorf("failed fetching hostname: %w", err)
 	}
 	hostName = strings.ToLower(hostName)
+	log.Info("Fetched hostname", "hostname", hostName)
 
 	log.Info("Setting up manager")
 	mgr, err := manager.New(restConfig, manager.Options{
