85c95c412d39525180e1fec9ee9a51d9a2a7d266
Rafael Franzke <rafael.franzke@sap.com>
2024-02-06 15:19:35 +0100

    Introduce `alertmanager` component boilerplate

---
+++ pkg/component/monitoring/alertmanager/component.go
@@ -0,0 +1,84 @@
+// Copyright 2024 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alertmanager
+
+import (
+	"context"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/component"
+	"github.com/gardener/gardener/pkg/utils/managedresources"
+)
+
+// Values contains configuration values for the AlertManager resources.
+type Values struct {
+	// Name is the name of the AlertManager. It will be used for the resource names of AlertManager and ManagedResource.
+	Name string
+}
+
+// New creates a new instance of DeployWaiter for the AlertManager.
+func New(client client.Client, namespace string, values Values) component.DeployWaiter {
+	return &alertManager{
+		client:    client,
+		namespace: namespace,
+		values:    values,
+	}
+}
+
+type alertManager struct {
+	client    client.Client
+	namespace string
+	values    Values
+}
+
+func (a *alertManager) Deploy(ctx context.Context) error {
+	registry := managedresources.NewRegistry(kubernetes.SeedScheme, kubernetes.SeedCodec, kubernetes.SeedSerializer)
+
+	resources, err := registry.AddAllAndSerialize()
+	if err != nil {
+		return err
+	}
+
+	return managedresources.CreateForSeed(ctx, a.client, a.namespace, a.name(), false, resources)
+}
+
+func (a *alertManager) Destroy(ctx context.Context) error {
+	return managedresources.DeleteForSeed(ctx, a.client, a.namespace, a.name())
+}
+
+// TimeoutWaitForManagedResource is the timeout used while waiting for the ManagedResources to become healthy or
+// deleted.
+var TimeoutWaitForManagedResource = 5 * time.Minute
+
+func (a *alertManager) Wait(ctx context.Context) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, TimeoutWaitForManagedResource)
+	defer cancel()
+
+	return managedresources.WaitUntilHealthy(timeoutCtx, a.client, a.namespace, a.name())
+}
+
+func (a *alertManager) WaitCleanup(ctx context.Context) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, TimeoutWaitForManagedResource)
+	defer cancel()
+
+	return managedresources.WaitUntilDeleted(timeoutCtx, a.client, a.namespace, a.name())
+}
+
+func (a *alertManager) name() string {
+	return "alertmanager-" + a.values.Name
+}
