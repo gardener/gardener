0439e63d0f196c474b9cdaa44db6163aaf42e7ff
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-02-08 21:37:40 +0200

    tools: drop yaml2json dependency in favour of yq

--- hack/generate-imagename-constants.sh
+++ hack/generate-imagename-constants.sh
@@ -34,7 +34,7 @@ package $package_name
 
 const ("
 
-for image_name in $(yaml2json < "$images_yaml" | jq -r '[.images[].name] | unique | .[]'); do
+for image_name in $(cat "$images_yaml" | yq -r '[.images[].name] | unique | .[]'); do
   variable_name="$(camelCase "$image_name")"
 
   out="
