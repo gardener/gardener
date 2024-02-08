ebc92c234b067f303d983fb5d794cab3b0d00b2c
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-02-08 20:17:12 +0100

    [release-v1.88] Annotate `kube-apiserver` deployment after labeling the resources for encryption (#9147)
    * Annotate the kapi deployment marking the completion of adding labels
    
    * Execute the tasks for a certain GVK in parallel but the tasks for different GVKs in sequence
    
    * Remove empty line
    
    ---------
    
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>

--- pkg/utils/gardener/secretsrotation/constants.go
+++ pkg/utils/gardener/secretsrotation/constants.go
@@ -18,6 +18,8 @@ const (
 	// AnnotationKeyNewEncryptionKeyPopulated is an annotation indicating that the new ETCD encryption key was populated
 	AnnotationKeyNewEncryptionKeyPopulated = "credentials.gardener.cloud/new-encryption-key-populated"
 
+	// AnnotationKeyResourcesLabeled is an annotation indicating the completion of labeling the resources with the credentials.gardener.cloud/key-name label
+	AnnotationKeyResourcesLabeled = "credentials.gardener.cloud/resources-labeled"
 	// AnnotationKeyEtcdSnapshotted is an annotation indicating that ETCD snapshot was completed
 	AnnotationKeyEtcdSnapshotted = "credentials.gardener.cloud/etcd-snapshotted"
 
