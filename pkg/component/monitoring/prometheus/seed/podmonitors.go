ee96f89496807c79f7d862c1fa969e0988743967
Rafael Franzke <rafael.franzke@sap.com>
2024-02-13 15:39:29 +0100

    Translate `extensions` scrape config into `PodMonitor`

--- pkg/component/monitoring/prometheus/seed/podmonitors.go
+++ pkg/component/monitoring/prometheus/seed/podmonitors.go
@@ -26,6 +26,55 @@ func CentralPodMonitors() []*monitoringv1.PodMonitor {
 	return []*monitoringv1.PodMonitor{
 		{
 			ObjectMeta: metav1.ObjectMeta{
+				Name: "extensions",
+			},
+			Spec: monitoringv1.PodMonitorSpec{
+				// Selector: metav1.LabelSelector{MatchExpressions: []metav1.LabelSelectorRequirement{
+				// 	{Key: "prometheus.io/scrape", Values: []string{"true"}, Operator: metav1.LabelSelectorOpIn},
+				// 	{Key: "prometheus.io/port", Operator: metav1.LabelSelectorOpExists},
+				// }},
+				NamespaceSelector: monitoringv1.NamespaceSelector{Any: true},
+				PodMetricsEndpoints: []monitoringv1.PodMetricsEndpoint{{
+					RelabelConfigs: []*monitoringv1.RelabelConfig{
+						// TODO: These annotations should actually be labels so that PodMonitorSpec.Selector can be used
+						//  instead of manually crafting this relabel config.
+						{
+							SourceLabels: []monitoringv1.LabelName{
+								"__meta_kubernetes_namespace",
+								"__meta_kubernetes_pod_annotation_prometheus_io_scrape",
+								"__meta_kubernetes_pod_annotation_prometheus_io_port",
+							},
+							Regex:  `extension-(.+);true;(.+)`,
+							Action: "keep",
+						},
+						{
+							SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_namespace"},
+							Regex:        `extension-(.+)`,
+							Action:       "keep",
+						},
+						{
+							SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_pod_annotation_prometheus_io_name"},
+							Regex:        `(.+)`,
+							Action:       "replace",
+							TargetLabel:  "job",
+						},
+						{
+							SourceLabels: []monitoringv1.LabelName{"__address__", "__meta_kubernetes_pod_annotation_prometheus_io_port"},
+							Regex:        `([^:]+)(?::\d+)?;(\d+)`,
+							Action:       "replace",
+							Replacement:  `$1:$2`,
+							TargetLabel:  "__address__",
+						},
+						{
+							Action: "labelmap",
+							Regex:  `__meta_kubernetes_pod_label_(.+)`,
+						},
+					},
+				}},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
 				Name: "garden",
 			},
 			Spec: monitoringv1.PodMonitorSpec{
--- pkg/component/monitoring/prometheus/seed/podmonitors_test.go
+++ pkg/component/monitoring/prometheus/seed/podmonitors_test.go
@@ -29,6 +29,49 @@ var _ = Describe("PodMonitors", func() {
 			Expect(seed.CentralPodMonitors()).To(HaveExactElements(
 				&monitoringv1.PodMonitor{
 					ObjectMeta: metav1.ObjectMeta{
+						Name: "extensions",
+					},
+					Spec: monitoringv1.PodMonitorSpec{
+						NamespaceSelector: monitoringv1.NamespaceSelector{Any: true},
+						PodMetricsEndpoints: []monitoringv1.PodMetricsEndpoint{{
+							RelabelConfigs: []*monitoringv1.RelabelConfig{
+								{
+									SourceLabels: []monitoringv1.LabelName{
+										"__meta_kubernetes_namespace",
+										"__meta_kubernetes_pod_annotation_prometheus_io_scrape",
+										"__meta_kubernetes_pod_annotation_prometheus_io_port",
+									},
+									Regex:  `extension-(.+);true;(.+)`,
+									Action: "keep",
+								},
+								{
+									SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_namespace"},
+									Regex:        `extension-(.+)`,
+									Action:       "keep",
+								},
+								{
+									SourceLabels: []monitoringv1.LabelName{"__meta_kubernetes_pod_annotation_prometheus_io_name"},
+									Regex:        `(.+)`,
+									Action:       "replace",
+									TargetLabel:  "job",
+								},
+								{
+									SourceLabels: []monitoringv1.LabelName{"__address__", "__meta_kubernetes_pod_annotation_prometheus_io_port"},
+									Regex:        `([^:]+)(?::\d+)?;(\d+)`,
+									Action:       "replace",
+									Replacement:  `$1:$2`,
+									TargetLabel:  "__address__",
+								},
+								{
+									Action: "labelmap",
+									Regex:  `__meta_kubernetes_pod_label_(.+)`,
+								},
+							},
+						}},
+					},
+				},
+				&monitoringv1.PodMonitor{
+					ObjectMeta: metav1.ObjectMeta{
 						Name: "garden",
 					},
 					Spec: monitoringv1.PodMonitorSpec{
