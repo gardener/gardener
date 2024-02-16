b3279ba5ad4334f09b4a02e7ea36255301e37c0f
Shafeeque E S <shafeeque.e.s@sap.com>
2024-02-16 10:46:58 +0530

    Remove deprecated field

--- pkg/apis/core/v1beta1/types_shoot.go
+++ pkg/apis/core/v1beta1/types_shoot.go
@@ -524,12 +524,9 @@ type HibernationSchedule struct {
 
 // Kubernetes contains the version and configuration variables for the Shoot control plane.
 type Kubernetes struct {
-	// AllowPrivilegedContainers indicates whether privileged containers are allowed in the Shoot.
-	// Defaults to true for Kubernetes versions below v1.25. Unusable for Kubernetes versions v1.25 and higher.
-	// +optional
-	//
-	// Deprecated: This field is deprecated and will be removed in a future version.
+	// AllowPrivilegedContainers is tombstoned to show why 1 is reserved protobuf tag.
 	AllowPrivilegedContainers *bool `json:"allowPrivilegedContainers,omitempty" protobuf:"varint,1,opt,name=allowPrivilegedContainers"`
+
 	// ClusterAutoscaler contains the configuration flags for the Kubernetes cluster autoscaler.
 	// +optional
 	ClusterAutoscaler *ClusterAutoscaler `json:"clusterAutoscaler,omitempty" protobuf:"bytes,2,opt,name=clusterAutoscaler"`
