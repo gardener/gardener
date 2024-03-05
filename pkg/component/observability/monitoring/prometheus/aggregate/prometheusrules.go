a37dc7a94e9d412353b6c1dc612bb4cb6a3fceae
Andreea Denitiu <119592402+adenitiu@users.noreply.github.com>
2024-03-05 12:08:21 +0200

    Remove node seed label description

--- pkg/component/observability/monitoring/prometheus/aggregate/prometheusrules.go
+++ pkg/component/observability/monitoring/prometheus/aggregate/prometheusrules.go
@@ -66,14 +66,14 @@ func CentralPrometheusRules() []*monitoringv1.PrometheusRule {
 						{
 							Alert: "NodeNotHealthy",
 							Expr:  intstr.FromString(`count_over_time((sum by (node) (kube_node_spec_taint{effect="NoSchedule", key!="ToBeDeletedByClusterAutoscaler", key!="` + v1beta1constants.LabelNodeCriticalComponent + `"}))[30m:]) > 5`),
-							For:   ptr.To(monitoringv1.Duration("10m")),
+							For:   ptr.To(monitoringv1.Duration("0m")),
 							Labels: map[string]string{
 								"severity":   "warning",
 								"type":       "seed",
 								"visibility": "operator",
 							},
 							Annotations: map[string]string{
-								"description": "Node {{$labels.node}} in seed {{$labels.name}} in landscape {{$externalLabels.landscape}} is not healthy for over 10 min.",
+								"description": "Node {{$labels.node}} in landscape {{$externalLabels.landscape}} was not healthy for five scrapes in the past 30 mins.",
 								"summary":     "A node is not healthy.",
 							},
 						},
