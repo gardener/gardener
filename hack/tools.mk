c20da1eaaed4abe345974bcfd0abfe87aa66c6da
Dimitar Mirchev <dimitar.mirchev@sap.com>
2024-03-06 15:40:06 +0200

    Downgrade skaffold to v2.9.0

--- hack/tools.mk
+++ hack/tools.mk
@@ -76,7 +76,7 @@ PROMTOOL_VERSION ?= 2.50.1
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf
 PROTOC_VERSION ?= 25.3
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
-SKAFFOLD_VERSION ?= v2.10.1
+SKAFFOLD_VERSION ?= v2.9.0
 # renovate: datasource=github-releases depName=mikefarah/yq
 YQ_VERSION ?= v4.41.1
 # renovate: datasource=github-releases depName=ironcore-dev/vgopath
