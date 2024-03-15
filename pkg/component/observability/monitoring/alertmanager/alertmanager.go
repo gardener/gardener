5221389db97cdef00772c6a9c973c01fec9e0af3
Niki Dokovski <nickytd@gmail.com>
2024-03-15 09:24:57 +0100

    Use alertmanager name
    Co-authored-by: Rafael Franzke <rafael.franzke@sap.com>

--- pkg/component/observability/monitoring/alertmanager/alertmanager.go
+++ pkg/component/observability/monitoring/alertmanager/alertmanager.go
@@ -39,7 +39,7 @@ func (a *alertManager) alertManager(takeOverOldPV bool) *monitoringv1.Alertmanag
 					v1beta1constants.LabelNetworkPolicyToDNS:             v1beta1constants.LabelNetworkPolicyAllowed,
 					v1beta1constants.LabelNetworkPolicyToPublicNetworks:  v1beta1constants.LabelNetworkPolicyAllowed,
 					v1beta1constants.LabelNetworkPolicyToPrivateNetworks: v1beta1constants.LabelNetworkPolicyAllowed,
-					v1beta1constants.LabelObservabilityApplication:       a.values.Name,
+					v1beta1constants.LabelObservabilityApplication:       a.name(),
 				}),
 			},
 			PriorityClassName: a.values.PriorityClassName,
