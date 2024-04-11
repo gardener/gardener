688e2081210da3d9f6144a549ae2bb942aa10267
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-04-11 12:13:51 +0200

    chore(deps): update dependency helm/helm to v3.14.4 (#9566)

--- hack/tools.mk
+++ hack/tools.mk
@@ -69,7 +69,7 @@ GO_ADD_LICENSE_VERSION ?= v1.1.1
 GOIMPORTSREVISER_VERSION ?= v3.6.4
 GO_VULN_CHECK_VERSION ?= latest
 # renovate: datasource=github-releases depName=helm/helm
-HELM_VERSION ?= v3.14.3
+HELM_VERSION ?= v3.14.4
 # renovate: datasource=github-releases depName=kubernetes-sigs/kind
 KIND_VERSION ?= v0.22.0
 # renovate: datasource=github-releases depName=kubernetes/kubernetes
