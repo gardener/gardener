fcff2b85222e0d533fdb712c996ab560a9eb3594
vpnachev <vladimir.nachev@sap.com>
2024-04-15 16:13:48 +0300

    Define CredentialsBinding API

---
+++ pkg/apis/authentication/types_credentialsbinding.go
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package authentication
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CredentialsBinding represents a binding to credentials in the same or another namespace.
+type CredentialsBinding struct {
+	metav1.TypeMeta
+	// Standard object metadata.
+	metav1.ObjectMeta
+	// Provider defines the provider type of the CredentialsBinding.
+	// This field is immutable.
+	Provider CredentialsBindingProvider
+	// CredentialsRef specify reference to credentials.
+	CredentialsRef Credentials
+	// Quotas is a list of references to Quota objects in the same or another namespace.
+	// This field is immutable.
+	Quotas []corev1.ObjectReference
+}
+
+// Credentials holds reference to credentials implementation.
+type Credentials struct {
+	// Secret is a reference to a secret object in the same or another namespace.
+	Secret *corev1.SecretReference
+	// WorkloadIdentity is a reference to a workloadidentity object in the same or another namespace.
+	WorkloadIdentity *WorkloadIdentityReference
+}
+
+// GetProviderType gets the type of the provider.
+func (sb *CredentialsBinding) GetProviderType() string {
+	return sb.Provider.Type
+}
+
+// CredentialsBindingProvider defines the provider type of the CredentialsBinding.
+type CredentialsBindingProvider struct {
+	// Type is the type of the provider.
+	Type string
+}
+
+// WorkloadIdentityReference represents a WorkloadIdentity Reference. It has enough information to retrieve
+// workloadidentity in any namespace
+type WorkloadIdentityReference struct {
+	// Name is unique within a namespace to reference a workloadidentity resource.
+	Name string
+	// Namespace defines the space within which the workloadidentity name must be unique.
+	Namespace string
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CredentialsBindingList is a collection of CredentialsBindings.
+type CredentialsBindingList struct {
+	metav1.TypeMeta
+	// Standard list object metadata.
+	metav1.ListMeta
+	// Items is the list of CredentialsBindings.
+	Items []CredentialsBinding
+}
---
+++ pkg/apis/authentication/v1alpha1/types_credentialsbinding.go
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CredentialsBinding represents a binding to credentials in the same or another namespace.
+type CredentialsBinding struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object metadata.
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// Provider defines the provider type of the CredentialsBinding.
+	// This field is immutable.
+	Provider CredentialsBindingProvider `json:"provider" protobuf:"bytes,2,opt,name=provider"`
+	// CredentialsRef specify reference to credentials.
+	CredentialsRef Credentials `json:"credentialsRef" protobuf:"bytes,3,name=credentialsRef"`
+	// Quotas is a list of references to Quota objects in the same or another namespace.
+	// This field is immutable.
+	// +optional
+	Quotas []corev1.ObjectReference `json:"quotas,omitempty" protobuf:"bytes,4,rep,name=quotas"`
+}
+
+// Credentials holds reference to credentials implementation.
+type Credentials struct {
+	// Secret is a reference to a secret object in the same or another namespace.
+	// +optional
+	Secret *corev1.SecretReference `json:"secret,omitempty" protobuf:"bytes,1,opt,name=secret"`
+	// WorkloadIdentity is a reference to a workloadidentity object in the same or another namespace.
+	// +optional
+	WorkloadIdentity *WorkloadIdentityReference `json:"workloadIdentity,omitempty" protobuf:"bytes,2,opt,name=workloadIdentity"`
+}
+
+// GetProviderType gets the type of the provider.
+func (sb *CredentialsBinding) GetProviderType() string {
+	return sb.Provider.Type
+}
+
+// CredentialsBindingProvider defines the provider type of the CredentialsBinding.
+type CredentialsBindingProvider struct {
+	// Type is the type of the provider.
+	Type string `json:"type" protobuf:"bytes,1,opt,name=type"`
+}
+
+// WorkloadIdentityReference represents a WorkloadIdentity Reference. It has enough information to retrieve
+// workloadidentity in any namespace
+type WorkloadIdentityReference struct {
+	// Name is unique within a namespace to reference a workloadidentity resource.
+	// +optional
+	Name string `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
+	// Namespace defines the space within which the workloadidentity name must be unique.
+	// +optional
+	Namespace string `json:"namespace,omitempty" protobuf:"bytes,2,opt,name=namespace"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CredentialsBindingList is a collection of CredentialsBindings.
+type CredentialsBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard list object metadata.
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// Items is the list of CredentialsBindings.
+	Items []CredentialsBinding `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
