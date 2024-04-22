fd14e6176eb601fb05fcc19f7494050ebc33c326
vpnachev <vladimir.nachev@sap.com>
2024-04-22 18:23:49 +0300

    Generate client, informer and lister for authentication.gardener.cloud
    Exclude (Admin|Viewer)Kubeconfig from client generation

--- pkg/apis/authentication/v1alpha1/types_adminkubeconfigrequest.go
+++ pkg/apis/authentication/v1alpha1/types_adminkubeconfigrequest.go
@@ -8,7 +8,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // AdminKubeconfigRequest can be used to request a kubeconfig with admin credentials
