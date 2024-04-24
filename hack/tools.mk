d9a3196b1619b1fae5a9d6fecb44af22e781f4fc
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-04-24 17:08:14 +0200

    chore(deps): update dependency ironcore-dev/vgopath to v0.1.5 (#9655)

--- hack/tools.mk
+++ hack/tools.mk
@@ -75,7 +75,7 @@ SKAFFOLD_VERSION ?= v2.11.1
 # renovate: datasource=github-releases depName=mikefarah/yq
 YQ_VERSION ?= v4.43.1
 # renovate: datasource=github-releases depName=ironcore-dev/vgopath
-VGOPATH_VERSION ?= v0.1.4
+VGOPATH_VERSION ?= v0.1.5
 
 # tool versions from go.mod
 CONTROLLER_GEN_VERSION ?= $(call version_gomod,sigs.k8s.io/controller-tools)
