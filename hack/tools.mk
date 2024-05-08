7423157948f685f55380cdf082b4b149a213a36b
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-05-08 19:44:21 +0000

    chore(deps): update dependency golangci/golangci-lint to v1.58.1

--- hack/tools.mk
+++ hack/tools.mk
@@ -50,7 +50,7 @@ VGOPATH                    := $(TOOLS_BIN_DIR)/vgopath
 
 # default tool versions
 # renovate: datasource=github-releases depName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION ?= v1.58.0
+GOLANGCI_LINT_VERSION ?= v1.58.1
 # renovate: datasource=github-releases depName=joelanford/go-apidiff
 GO_APIDIFF_VERSION ?= v0.8.2
 # renovate: datasource=github-releases depName=google/addlicense
