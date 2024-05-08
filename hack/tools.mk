8f941604d32c5c2b8fb784d02b7d87293c3d73dd
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-05-08 22:48:15 +0000

    chore(deps): update dependency prometheus/prometheus to v2.52.0

--- hack/tools.mk
+++ hack/tools.mk
@@ -67,7 +67,7 @@ KUBECTL_VERSION ?= v1.30.0
 # renovate: datasource=github-releases depName=kubernetes-sigs/kustomize
 KUSTOMIZE_VERSION ?= v5.3.0
 # renovate: datasource=github-releases depName=prometheus/prometheus
-PROMTOOL_VERSION ?= 2.51.2
+PROMTOOL_VERSION ?= 2.52.0
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf
 PROTOC_VERSION ?= 26.1
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
