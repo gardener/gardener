b53916ce11b470cc03a93ff4e8424fff9ef796b9
Tim Ebert <timebertt@gmail.com>
2024-05-13 18:33:21 +0200

    Add API reference docs for `core.gardener.cloud/v1`

--- pkg/apis/core/v1/doc.go
+++ pkg/apis/core/v1/doc.go
@@ -9,6 +9,8 @@
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:protobuf-gen=package
 
+//go:generate gen-crd-api-reference-docs -api-dir . -config ../../../../hack/api-reference/core-config.json -template-dir ../../../../hack/api-reference/template -out-file ../../../../docs/api-reference/core-v1.md
+
 // Package v1 is a version of the API.
 // +groupName=core.gardener.cloud
 package v1
