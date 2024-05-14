cc7b8d0c57fbc8bc6c4a474657287d59355f1a0a
Tim Ebert <timebertt@gmail.com>
2024-05-14 10:14:27 +0200

    Add validation for new `ControllerDeployment` fields

--- pkg/apis/core/validation/controllerdeployment.go
+++ pkg/apis/core/validation/controllerdeployment.go
@@ -5,6 +5,8 @@
 package validation
 
 import (
+	"fmt"
+
 	apivalidation "k8s.io/apimachinery/pkg/api/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
@@ -17,9 +19,33 @@ func ValidateControllerDeployment(controllerDeployment *core.ControllerDeploymen
 
 	allErrs = append(allErrs, apivalidation.ValidateObjectMeta(&controllerDeployment.ObjectMeta, false, apivalidation.NameIsDNSLabel, field.NewPath("metadata"))...)
 
-	if len(controllerDeployment.Type) == 0 {
-		allErrs = append(allErrs, field.Required(field.NewPath("type"), "must provide a type"))
+	var (
+		isBuiltInType  = false
+		deploymentType = controllerDeployment.Type
+	)
+
+	switch {
+	case controllerDeployment.Helm != nil:
+		isBuiltInType = true
+		deploymentType = "helm"
+
+		allErrs = append(allErrs, validateHelmControllerDeployment(controllerDeployment.Helm, field.NewPath("helm"))...)
 	}
+
+	if isBuiltInType {
+		// a built-in type is configured: type and providerConfig must be empty
+		if len(controllerDeployment.Type) > 0 {
+			allErrs = append(allErrs, field.Forbidden(field.NewPath("type"), fmt.Sprintf("must not provide type if a built-in deployment type (%s) is used", deploymentType)))
+		}
+		if controllerDeployment.ProviderConfig != nil {
+			allErrs = append(allErrs, field.Forbidden(field.NewPath("providerConfig"), fmt.Sprintf("must not provide providerConfig if a built-in deployment type (%s) is used", deploymentType)))
+		}
+	} else if len(controllerDeployment.Type) == 0 {
+		allErrs = append(allErrs, field.Forbidden(field.NewPath(""), "must use either helm or a custom deployment configuration"))
+	}
+	// If a custom type is configured, only type and providerConfig can be set, and other fields must be empty.
+	// We don't need to validate this case, as it is covered by the built-in type case. In other words, configuring a
+	// built-in type takes precedence over configuring a custom type in the validation.
 
 	return allErrs
 }
@@ -27,4 +53,15 @@ func ValidateControllerDeployment(controllerDeployment *core.ControllerDeploymen
 // ValidateControllerDeploymentUpdate validates a ControllerDeployment object before an update.
 func ValidateControllerDeploymentUpdate(new, _ *core.ControllerDeployment) field.ErrorList {
 	return ValidateControllerDeployment(new)
+}
+
+func validateHelmControllerDeployment(helmControllerDeployment *core.HelmControllerDeployment, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	// rawChart is required as long as there is no other source (OCI sources will be added later on)
+	if len(helmControllerDeployment.RawChart) == 0 {
+		allErrs = append(allErrs, field.Required(fldPath.Child("rawChart"), ""))
+	}
+
+	return allErrs
 }
