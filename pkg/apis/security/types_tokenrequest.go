fddd069a35bcdd0afb3fedf90cdf64e6ac1bc6a6
vpnachev <vladimir.nachev@sap.com>
2024-05-17 18:17:33 +0300

    Define TokenRequest API

---
+++ pkg/apis/security/types_tokenrequest.go
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package security
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TokenRequest is resource that can be used to request WorkloadIdentity tokens.
+type TokenRequest struct {
+	metav1.TypeMeta
+	// Standard object metadata.
+	metav1.ObjectMeta
+	// Spec holds configuration settings for the requested token.
+	Spec TokenRequestSpec
+	// Status is bears the issued token with additional information back to the client.
+	Status TokenRequestStatus
+}
+
+// TokenRequestSpec holds configuration settings for the requested token.
+type TokenRequestSpec struct {
+	// ContextObject identify the object the token is requested for.
+	ContextObject *ContextObject
+	// Duration specifies for how long the requested token to be valid.
+	Duration *metav1.Duration
+}
+
+// ContextObject identify the object the token is requested for.
+type ContextObject struct {
+	// Kind of the object the token is requested for. Valid kinds are 'Shoot', 'Seed', etc.
+	Kind string
+	// API version of the object the token is requested for.
+	APIVersion string
+	// Name of the object the token is requested for.
+	Name string
+	// Namespace of the object the token is requested for.
+	Namespace string
+	// UID of the object the token is requested for.
+	UID types.UID
+}
+
+// TokenRequestStatus bears the issued token with additional information back to the client.
+type TokenRequestStatus struct {
+	// Token is the issued token.
+	Token string
+	// ExpirationTimeStamp is the time of expiration of the returned token.
+	ExpirationTimeStamp metav1.Time
+}
---
+++ pkg/apis/security/v1alpha1/types_tokenrequest.go
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TokenRequest is resource that can be used to request WorkloadIdentity tokens.
+type TokenRequest struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object metadata.
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// Spec holds configuration settings for the requested token.
+	Spec TokenRequestSpec `json:"spec" protobuf:"bytes,2,opt,name=spec"`
+	// Status is bears the issued token with additional information back to the client.
+	Status TokenRequestStatus `json:"status" protobuf:"bytes,3,opt,name=status"`
+}
+
+// TokenRequestSpec holds configuration settings for the requested token.
+type TokenRequestSpec struct {
+	// ContextObject identify the object the token is requested for.
+	// +optional
+	ContextObject *ContextObject `json:"contextObject,omitempty" protobuf:"bytes,1,opt,name=contextObject"`
+	// Duration specifies for how long the requested token to be valid.
+	// +optional
+	Duration *metav1.Duration `json:"duration,omitempty" protobuf:"bytes,2,opt,name=duration"`
+}
+
+// ContextObject identify the object the token is requested for.
+type ContextObject struct {
+	// Kind of the object the token is requested for. Valid kinds are 'Shoot', 'Seed', etc.
+	Kind string `json:"kind" protobuf:"bytes,1,opt,name=kind"`
+	// API version of the object the token is requested for.
+	APIVersion string `json:"apiVersion" protobuf:"bytes,2,opt,name=apiVersion"`
+	// Name of the object the token is requested for.
+	Name string `json:"name" protobuf:"bytes,3,opt,name=name"`
+	// Namespace of the object the token is requested for.
+	Namespace string `json:"namespace" protobuf:"bytes,4,opt,name=namespace"`
+	// UID of the object the token is requested for.
+	UID types.UID `json:"uid" protobuf:"bytes,5,opt,name=uid,casttype=k8s.io/apimachinery/pkg/types.UID"`
+}
+
+// TokenRequestStatus bears the issued token with additional information back to the client.
+type TokenRequestStatus struct {
+	// Token is the issued token.
+	Token string `json:"token" protobuf:"bytes,1,opt,name=token"`
+	// ExpirationTimeStamp is the time of expiration of the returned token.
+	ExpirationTimeStamp metav1.Time `json:"expirationTimestamp" protobuf:"bytes,2,opt,name=expirationTimestamp"`
+}
