313b2f3d72a15d73a2931a206717903d20193b3d
Rafael Franzke <rafael.franzke@sap.com>
2024-05-21 14:15:45 +0200

    Address PR review feedback

--- pkg/component/observability/plutono/plutono.go
+++ pkg/component/observability/plutono/plutono.go
@@ -174,7 +174,6 @@ func (p *plutono) Deploy(ctx context.Context) error {
 				metav1.SetMetaDataLabel(&configMap.ObjectMeta, k, v)
 			}
 
-			configMap.Immutable = dashboardConfigMap.Immutable
 			configMap.Data = dashboardConfigMap.Data
 			configMap.BinaryData = dashboardConfigMap.BinaryData
 			return nil
