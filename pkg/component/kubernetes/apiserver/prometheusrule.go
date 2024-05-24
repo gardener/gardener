182b4b44d91ed6fed1abd6a44c148ff5872522f1
Rafael Franzke <rafael.franzke@sap.com>
2024-05-24 13:17:24 +0200

    Adapt `kube-apiserver`

---
+++ pkg/component/kubernetes/apiserver/prometheusrule.go
@@ -0,0 +1,213 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package apiserver
+
+import (
+	"context"
+
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
+
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	monitoringutils "github.com/gardener/gardener/pkg/component/observability/monitoring/utils"
+	"github.com/gardener/gardener/pkg/controllerutils"
+)
+
+func (k *kubeAPIServer) emptyPrometheusRule() *monitoringv1.PrometheusRule {
+	return &monitoringv1.PrometheusRule{ObjectMeta: monitoringutils.ConfigObjectMeta(k.values.NamePrefix+v1beta1constants.DeploymentNameKubeAPIServer, k.namespace, k.prometheusLabel())}
+}
+
+func (k *kubeAPIServer) reconcilePrometheusRule(ctx context.Context, prometheusRule *monitoringv1.PrometheusRule) error {
+	_, err := controllerutils.GetAndCreateOrMergePatch(ctx, k.client.Client(), prometheusRule, func() error {
+		prometheusRule.Spec = monitoringv1.PrometheusRuleSpec{
+			Groups: []monitoringv1.RuleGroup{{
+				Name: "kube-apiserver.rules",
+				Rules: []monitoringv1.Rule{
+					{
+						Alert: "ApiServerNotReachable",
+						Expr:  intstr.FromString(`probe_success{job="blackbox-apiserver"} == 0`),
+						For:   ptr.To(monitoringv1.Duration("5m")),
+						Labels: map[string]string{
+							"service":    v1beta1constants.DeploymentNameKubeAPIServer,
+							"severity":   "blocker",
+							"type":       "seed",
+							"visibility": "all",
+						},
+						Annotations: map[string]string{
+							"summary":     "API server not reachable (externally).",
+							"description": "API server not reachable via external endpoint: {{ $labels.instance }}.",
+						},
+					},
+					{
+						Alert: "KubeApiserverDown",
+						Expr:  intstr.FromString(`absent(up{job="kube-apiserver"} == 1)`),
+						For:   ptr.To(monitoringv1.Duration("5m")),
+						Labels: map[string]string{
+							"service":    v1beta1constants.DeploymentNameKubeAPIServer,
+							"severity":   "blocker",
+							"type":       "seed",
+							"visibility": "operator",
+						},
+						Annotations: map[string]string{
+							"summary":     "API server unreachable.",
+							"description": "All API server replicas are down/unreachable, or all API server could not be found.",
+						},
+					},
+					{
+						Alert: "KubeApiServerTooManyOpenFileDescriptors",
+						Expr:  intstr.FromString(`100 * process_open_fds{job="kube-apiserver"} / process_max_fds > 50`),
+						For:   ptr.To(monitoringv1.Duration("30m")),
+						Labels: map[string]string{
+							"service":    v1beta1constants.DeploymentNameKubeAPIServer,
+							"severity":   "warning",
+							"type":       "seed",
+							"visibility": "owner",
+						},
+						Annotations: map[string]string{
+							"summary":     "The API server has too many open file descriptors",
+							"description": "The API server ({{ $labels.instance }}) is using {{ $value }}% of the available file/socket descriptors.",
+						},
+					},
+					{
+						Alert: "KubeApiServerTooManyOpenFileDescriptors",
+						Expr:  intstr.FromString(`100 * process_open_fds{job="kube-apiserver"} / process_max_fds{job="kube-apiserver"} > 80`),
+						For:   ptr.To(monitoringv1.Duration("30m")),
+						Labels: map[string]string{
+							"service":    v1beta1constants.DeploymentNameKubeAPIServer,
+							"severity":   "critical",
+							"type":       "seed",
+							"visibility": "owner",
+						},
+						Annotations: map[string]string{
+							"summary":     "The API server has too many open file descriptors",
+							"description": "The API server ({{ $labels.instance }}) is using {{ $value }}% of the available file/socket descriptors.",
+						},
+					},
+					{
+						Alert: "KubeApiServerLatency",
+						// Some verbs excluded because they are expected to be long-lasting:
+						// - WATCHLIST is long-poll
+						// - CONNECT is "kubectl exec"
+						Expr: intstr.FromString(`histogram_quantile(0.99, sum without (instance,resource,subresource) (rate(apiserver_request_duration_seconds_bucket{subresource!~"log|portforward|exec|proxy|attach",verb!~"CONNECT|WATCHLIST|WATCH|PROXY proxy"}[5m]))) > 3`),
+						For:  ptr.To(monitoringv1.Duration("30m")),
+						Labels: map[string]string{
+							"service":    v1beta1constants.DeploymentNameKubeAPIServer,
+							"severity":   "warning",
+							"type":       "seed",
+							"visibility": "owner",
+						},
+						Annotations: map[string]string{
+							"summary":     "Kubernetes API server latency is high",
+							"description": "Kube API server latency for verb {{ $labels.verb }} is high. This could be because the shoot workers and the control plane are in different regions. 99th percentile of request latency is greater than 3 seconds.",
+						},
+					},
+					{
+						Record: "shoot:apiserver_watch_duration:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.2, sum(rate(apiserver_request_duration_seconds_bucket{verb="WATCH",resource=~"configmaps|deployments|secrets|daemonsets|services|nodes|pods|namespaces|endpoints|statefulsets|clusterroles|roles"}[5m])) by (le,scope,resource))`),
+						Labels: map[string]string{"quantile": "0.2"},
+					},
+					{
+						Record: "shoot:apiserver_watch_duration:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.5, sum(rate(apiserver_request_duration_seconds_bucket{verb="WATCH",resource=~"configmaps|deployments|secrets|daemonsets|services|nodes|pods|namespaces|endpoints|statefulsets|clusterroles|roles"}[5m])) by (le,scope,resource))`),
+						Labels: map[string]string{"quantile": "0.5"},
+					},
+					{
+						Record: "shoot:apiserver_watch_duration:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.9, sum(rate(apiserver_request_duration_seconds_bucket{verb="WATCH",resource=~"configmaps|deployments|secrets|daemonsets|services|nodes|pods|namespaces|endpoints|statefulsets|clusterroles|roles"}[5m])) by (le,scope,resource))`),
+						Labels: map[string]string{"quantile": "0.9"},
+					},
+					{
+						Record: "shoot:apiserver_watch_duration:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.2, sum(rate(apiserver_request_duration_seconds_bucket{verb="WATCH",group=~".+garden.+"}[5m])) by (le,scope,resource))`),
+						Labels: map[string]string{"quantile": "0.2"},
+					},
+					{
+						Record: "shoot:apiserver_watch_duration:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.5, sum(rate(apiserver_request_duration_seconds_bucket{verb="WATCH",group=~".+garden.+"}[5m])) by (le,scope,resource))`),
+						Labels: map[string]string{"quantile": "0.5"},
+					},
+					{
+						Record: "shoot:apiserver_watch_duration:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.9, sum(rate(apiserver_request_duration_seconds_bucket{verb="WATCH",group=~".+garden.+"}[5m])) by (le,scope,resource))`),
+						Labels: map[string]string{"quantile": "0.9"},
+					},
+
+					// API Auditlog
+					{
+						Alert: "KubeApiServerTooManyAuditlogFailures",
+						Expr:  intstr.FromString(`sum(rate (apiserver_audit_error_total{plugin!="log",job="kube-apiserver"}[5m])) / sum(rate(apiserver_audit_event_total{job="kube-apiserver"}[5m])) > bool 0.02 == 1`),
+						For:   ptr.To(monitoringv1.Duration("15m")),
+						Labels: map[string]string{
+							"service":    "auditlog",
+							"severity":   "warning",
+							"type":       "seed",
+							"visibility": "operator",
+						},
+						Annotations: map[string]string{
+							"summary":     "The kubernetes API server has too many failed attempts to log audit events",
+							"description": "The API servers cumulative failure rate in logging audit events is greater than 2%.",
+						},
+					},
+					{
+						Record: "shoot:apiserver_audit_event_total:sum",
+						Expr:   intstr.FromString(`sum(rate(apiserver_audit_event_total{job="kube-apiserver"}[5m]))`),
+					},
+					{
+						Record: "shoot:apiserver_audit_error_total:sum",
+						Expr:   intstr.FromString(`sum(rate(apiserver_audit_error_total{plugin!="log",job="kube-apiserver"}[5m]))`),
+					},
+
+					// API latency
+					{
+						Record: "shoot:apiserver_latency_seconds:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.99, sum without (instance, pod) (rate(apiserver_request_duration_seconds_bucket[5m])))`),
+						Labels: map[string]string{"quantile": "0.99"},
+					},
+					{
+						Record: "shoot:apiserver_latency_seconds:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.9, sum without (instance, pod) (rate(apiserver_request_duration_seconds_bucket[5m])))`),
+						Labels: map[string]string{"quantile": "0.9"},
+					},
+					{
+						Record: "shoot:apiserver_latency_seconds:quantile",
+						Expr:   intstr.FromString(`histogram_quantile(0.5, sum without (instance, pod) (rate(apiserver_request_duration_seconds_bucket[5m])))`),
+						Labels: map[string]string{"quantile": "0.5"},
+					},
+
+					// API server request duration greater than 1s percentage
+					{
+						Record: "shoot:apiserver_latency:percentage",
+						Expr:   intstr.FromString(`1 - sum(rate(apiserver_request_duration_seconds_bucket{le="1",subresource!~"log|portforward|exec|proxy|attach",verb!~"CONNECT|LIST|WATCH"}[1h])) / sum(rate(apiserver_request_duration_seconds_count{subresource!~"log|portforward|exec|proxy|attach",verb!~"CONNECT|LIST|WATCH"}[1h]))`),
+					},
+
+					{
+						Record: "shoot:kube_apiserver:sum_by_pod",
+						Expr:   intstr.FromString(`sum(up{job="kube-apiserver"}) by (pod)`),
+					},
+
+					// API failure rate
+					{
+						Alert: "ApiserverRequestsFailureRate",
+						Expr:  intstr.FromString(`max(sum by(instance,resource,verb) (rate(apiserver_request_total{code=~"5.."}[10m])) / sum by(instance,resource,verb) (rate(apiserver_request_total[10m]))) * 100 > 10`),
+						For:   ptr.To(monitoringv1.Duration("30m")),
+						Labels: map[string]string{
+							"service":    v1beta1constants.DeploymentNameKubeAPIServer,
+							"severity":   "warning",
+							"type":       "seed",
+							"visibility": "operator",
+						},
+						Annotations: map[string]string{
+							"summary":     "Kubernetes API server failure rate is high",
+							"description": "The API Server requests failure rate exceeds 10%.",
+						},
+					},
+				},
+			}},
+		}
+		return nil
+	})
+	return err
+}
--- pkg/component/kubernetes/apiserver/servicemonitor.go
+++ pkg/component/kubernetes/apiserver/servicemonitor.go
@@ -16,18 +16,17 @@ import (
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
 	kubeapiserverconstants "github.com/gardener/gardener/pkg/component/kubernetes/apiserver/constants"
-	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheus/garden"
 	monitoringutils "github.com/gardener/gardener/pkg/component/observability/monitoring/utils"
 	"github.com/gardener/gardener/pkg/controllerutils"
 )
 
 func (k *kubeAPIServer) emptyServiceMonitor() *monitoringv1.ServiceMonitor {
-	return &monitoringv1.ServiceMonitor{ObjectMeta: monitoringutils.ConfigObjectMeta(k.values.NamePrefix+v1beta1constants.DeploymentNameKubeAPIServer, k.namespace, garden.Label)}
+	return &monitoringv1.ServiceMonitor{ObjectMeta: monitoringutils.ConfigObjectMeta(k.values.NamePrefix+v1beta1constants.DeploymentNameKubeAPIServer, k.namespace, k.prometheusLabel())}
 }
 
 func (k *kubeAPIServer) reconcileServiceMonitor(ctx context.Context, serviceMonitor *monitoringv1.ServiceMonitor) error {
 	_, err := controllerutils.GetAndCreateOrMergePatch(ctx, k.client.Client(), serviceMonitor, func() error {
-		serviceMonitor.Labels = monitoringutils.Labels(garden.Label)
+		serviceMonitor.Labels = monitoringutils.Labels(k.prometheusLabel())
 		serviceMonitor.Spec = monitoringv1.ServiceMonitorSpec{
 			Selector: metav1.LabelSelector{MatchLabels: getLabels()},
 			Endpoints: []monitoringv1.Endpoint{{
@@ -35,12 +34,14 @@ func (k *kubeAPIServer) reconcileServiceMonitor(ctx context.Context, serviceMoni
 				Scheme:     "https",
 				TLSConfig:  &monitoringv1.TLSConfig{SafeTLSConfig: monitoringv1.SafeTLSConfig{InsecureSkipVerify: ptr.To(true)}},
 				Authorization: &monitoringv1.SafeAuthorization{Credentials: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{Name: garden.AccessSecretName},
+					LocalObjectReference: corev1.LocalObjectReference{Name: k.prometheusAccessSecretName()},
 					Key:                  resourcesv1alpha1.DataKeyToken,
 				}},
+				RelabelConfigs: []monitoringv1.RelabelConfig{{
+					Action: "labelmap",
+					Regex:  `__meta_kubernetes_service_label_(.+)`,
+				}},
 				MetricRelabelConfigs: monitoringutils.StandardMetricRelabelConfig(
-					"authentication_attempts",
-					"authenticated_user_requests",
 					"apiserver_admission_controller_admission_duration_seconds_.+",
 					"apiserver_admission_webhook_admission_duration_seconds_.+",
 					"apiserver_admission_step_admission_duration_seconds_.+",
@@ -48,26 +49,50 @@ func (k *kubeAPIServer) reconcileServiceMonitor(ctx context.Context, serviceMoni
 					"apiserver_audit_event_total",
 					"apiserver_audit_error_total",
 					"apiserver_audit_requests_rejected_total",
-					"apiserver_request_total",
-					"apiserver_latency_seconds",
+					"apiserver_cache_list_.+",
+					"apiserver_crd_webhook_conversion_duration_seconds_.+",
 					"apiserver_current_inflight_requests",
 					"apiserver_current_inqueue_requests",
-					"apiserver_response_sizes_.+",
+					"apiserver_init_events_total",
+					"apiserver_latency",
+					"apiserver_latency_seconds",
+					"apiserver_longrunning_requests",
 					"apiserver_request_duration_seconds_.+",
+					"apiserver_request_duration_seconds_bucket",
+					"apiserver_request_duration_seconds_count",
 					"apiserver_request_terminations_total",
+					"apiserver_response_sizes_.+",
+					// TODO: The apiserver_storage_db_total_size_in_bytes metric is replaced by
+					//  apiserver_storage_size_bytes in K8s 1.28 (https://github.com/kubernetes/kubernetes/pull/118812).
+					//  Remove this metric when we remove support for K8s 1.28.
+					"apiserver_storage_db_total_size_in_bytes",
+					"apiserver_storage_list_.+",
 					"apiserver_storage_objects",
 					"apiserver_storage_transformation_duration_seconds_.+",
 					"apiserver_storage_transformation_operations_total",
+					"apiserver_storage_size_bytes",
 					"apiserver_registered_watchers",
-					"apiserver_init_events_total",
+					"apiserver_request_count",
+					"apiserver_request_total",
+					"apiserver_watch_duration",
 					"apiserver_watch_events_sizes_.+",
 					"apiserver_watch_events_total",
+					// TODO: The etcd_db_total_size_in_bytes metric is renamed to
+					//  apiserver_storage_db_total_size_in_bytes in K8s 1.26
+					//  (https://github.com/kubernetes/kubernetes/pull/113310). Remove this metric when we remove
+					//  support for K8s 1.26.
 					"etcd_db_total_size_in_bytes",
+					// TODO: The etcd_object_counts metric is removed in K8s 1.25
+					//  (https://github.com/kubernetes/kubernetes/pull/110337). Remove this metric when we remove
+					//  support for K8s 1.25.
+					"etcd_object_counts",
 					"etcd_request_duration_seconds_.+",
+					"go_.+",
+					"process_max_fds",
+					"process_open_fds",
 					"watch_cache_capacity_increase_total",
 					"watch_cache_capacity_decrease_total",
 					"watch_cache_capacity",
-					"go_.+",
 				),
 			}},
 		}
