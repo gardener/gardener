6f4d3c67162a0b18d0a570185aa581c1a58563c5
oliver-goetz <o.goetz@sap.com>
2024-05-25 10:52:54 +0200

    Introduce `NodeAgentAuthorizer` webhook in `resource-manager`

---
+++ pkg/resourcemanager/webhook/nodeagentauthorizer/identity.go
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package nodeagentauthorizer
+
+import (
+	"strings"
+
+	"k8s.io/apiserver/pkg/authentication/user"
+
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+)
+
+// GetNodeAgentIdentity returns the node name and a boolean which indicates whether the user is a gardener-node-agent
+func GetNodeAgentIdentity(u user.Info) (string, bool) {
+	if u == nil {
+		return "", false
+	}
+
+	if !strings.HasPrefix(u.GetName(), v1beta1constants.NodeAgentUserNamePrefix) {
+		return "", false
+	}
+
+	return strings.TrimPrefix(u.GetName(), v1beta1constants.NodeAgentUserNamePrefix), true
+}
