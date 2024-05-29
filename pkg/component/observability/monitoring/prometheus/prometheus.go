56dbf5e7e39d278857ba4ff3ec16e858b429829b
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2024-05-29 14:32:42 +0200

    Support multiple Alertmanagers in the prometheus component
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
    Co-authored-by: Victor Herrero Otal <victor.herrero.otal@sap.com>

--- pkg/component/observability/monitoring/prometheus/prometheus.go
+++ pkg/component/observability/monitoring/prometheus/prometheus.go
@@ -104,12 +104,20 @@ func (p *prometheus) prometheus(takeOverOldPV bool, cortexConfigMap *corev1.Conf
 	}
 
 	if p.values.Alerting != nil {
-		obj.Spec.Alerting = &monitoringv1.AlertingSpec{
-			Alertmanagers: []monitoringv1.AlertmanagerEndpoints{{
-				Namespace: p.namespace,
-				Name:      p.values.Alerting.AlertmanagerName,
-				Port:      intstr.FromString(alertmanager.PortNameMetrics),
-			}},
+		if len(p.values.Alerting.Alertmanagers) > 0 {
+			obj.Spec.Alerting = &monitoringv1.AlertingSpec{}
+		}
+		for _, alertmanagerItem := range p.values.Alerting.Alertmanagers {
+			namespace := p.namespace
+			if alertmanagerItem.Namespace != nil {
+				namespace = *alertmanagerItem.Namespace
+			}
+			obj.Spec.Alerting.Alertmanagers = append(obj.Spec.Alerting.Alertmanagers,
+				monitoringv1.AlertmanagerEndpoints{
+					Namespace: namespace,
+					Name:      alertmanagerItem.Name,
+					Port:      intstr.FromString(alertmanager.PortNameMetrics),
+				})
 		}
 		obj.Spec.AdditionalAlertRelabelConfigs = &corev1.SecretKeySelector{
 			LocalObjectReference: corev1.LocalObjectReference{Name: p.name() + secretNameSuffixAdditionalAlertRelabelConfigs},
