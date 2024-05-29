4fccffe29dd030b2b17e5a3f43d57d7357d73da7
vpnachev <vladimir.nachev@sap.com>
2024-05-29 18:00:15 +0300

    Fix docstrings of manual conversion funcs

--- pkg/apis/security/v1alpha1/conversions.go
+++ pkg/apis/security/v1alpha1/conversions.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gardener/gardener/pkg/apis/security"
 )
 
-// Convert_security_TargetSystem_To_v1alpha1_TargetSystem is ...
+// Convert_security_TargetSystem_To_v1alpha1_TargetSystem is a manual conversion function.
 func Convert_security_TargetSystem_To_v1alpha1_TargetSystem(in *security.TargetSystem, out *TargetSystem, s conversion.Scope) error {
 	if err := autoConvert_security_TargetSystem_To_v1alpha1_TargetSystem(in, out, s); err != nil {
 		return err
@@ -26,7 +26,7 @@ func Convert_security_TargetSystem_To_v1alpha1_TargetSystem(in *security.TargetS
 	return runtime.Convert_runtime_Object_To_runtime_RawExtension(&in.ProviderConfig, out.ProviderConfig, s)
 }
 
-// Convert_v1alpha1_TargetSystem_To_security_TargetSystem is ...
+// Convert_v1alpha1_TargetSystem_To_security_TargetSystem is a manual conversion function.
 func Convert_v1alpha1_TargetSystem_To_security_TargetSystem(in *TargetSystem, out *security.TargetSystem, s conversion.Scope) error {
 	if err := autoConvert_v1alpha1_TargetSystem_To_security_TargetSystem(in, out, s); err != nil {
 		return err
