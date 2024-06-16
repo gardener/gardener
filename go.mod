122b376d668bceb2f56ae555023a94f1c4406857
vpnachev <vladimir.nachev@sap.com>
2024-06-16 17:43:03 +0300

    Issue signed JWT

--- go.mod
+++ go.mod
@@ -55,23 +55,24 @@ require (
 	gomodules.xyz/jsonpatch/v2 v2.4.0
 	gonum.org/v1/gonum v0.15.0
 	google.golang.org/protobuf v1.34.0
+	gopkg.in/square/go-jose.v2 v2.6.0
 	gopkg.in/yaml.v3 v3.0.1
 	helm.sh/helm/v3 v3.14.4
 	istio.io/api v1.22.2
 	istio.io/client-go v1.22.0
-	k8s.io/api v0.29.6
+	k8s.io/api v0.30.2
 	k8s.io/apiextensions-apiserver v0.29.6
-	k8s.io/apimachinery v0.29.6
+	k8s.io/apimachinery v0.30.2
 	k8s.io/apiserver v0.29.6
 	k8s.io/autoscaler/vertical-pod-autoscaler v1.1.2
-	k8s.io/client-go v0.29.6
+	k8s.io/client-go v0.30.2
 	k8s.io/cluster-bootstrap v0.29.6
 	k8s.io/code-generator v0.29.6
 	k8s.io/component-base v0.29.6
 	k8s.io/component-helpers v0.29.6
 	k8s.io/klog/v2 v2.120.1
 	k8s.io/kube-aggregator v0.29.6
-	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00
+	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340
 	k8s.io/kube-proxy v0.29.6
 	k8s.io/kubelet v0.29.6
 	k8s.io/metrics v0.29.6
@@ -83,6 +84,8 @@ require (
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1
 	sigs.k8s.io/yaml v1.4.0
 )
+
+require k8s.io/kubernetes v1.30.2
 
 require (
 	github.com/AdaLogics/go-fuzz-headers v0.0.0-20230811130428-ced1acdcaa24 // indirect
@@ -145,7 +148,7 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/cel-go v0.17.7 // indirect
+	github.com/google/cel-go v0.17.8 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20240424215950-a892ee059fd6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -249,9 +252,10 @@ require (
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/gengo v0.0.0-20230829151522-9cce18d56c01 // indirect
+	k8s.io/gengo/v2 v2.0.0-20240228010128-51d4e06bde70 // indirect
 	k8s.io/klog v1.0.0 // indirect
 	k8s.io/kms v0.29.6 // indirect
 	oras.land/oras-go v1.2.4 // indirect
-	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.28.0 // indirect
+	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.29.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 )
--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -14,6 +14,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/util/keyutil"
 
 	corerest "github.com/gardener/gardener/pkg/apiserver/registry/core/rest"
 	operationsrest "github.com/gardener/gardener/pkg/apiserver/registry/operations/rest"
@@ -32,6 +33,8 @@ type ExtraConfig struct {
 	WorkloadIdentityTokenIssuer        string
 	WorkloadIdentityTokenMinExpiration time.Duration
 	WorkloadIdentityTokenMaxExpiration time.Duration
+	WorkloadIdentitySigningKey         any
+	ClusterIdentity                    string
 }
 
 // Config contains Gardener API server configuration.
@@ -95,6 +98,9 @@ func (c completedConfig) New() (*GardenerServer, error) {
 			WorkloadIdentityTokenIssuer:        c.ExtraConfig.WorkloadIdentityTokenIssuer,
 			WorkloadIdentityTokenMinExpiration: c.ExtraConfig.WorkloadIdentityTokenMinExpiration,
 			WorkloadIdentityTokenMaxExpiration: c.ExtraConfig.WorkloadIdentityTokenMaxExpiration,
+			WorkloadIdentitySigningKey:         c.ExtraConfig.WorkloadIdentitySigningKey,
+			CoreInformerFactory:                c.coreInformerFactory,
+			ClusterIdentity:                    c.ExtraConfig.ClusterIdentity,
 		}).NewRESTStorage(c.GenericConfig.RESTOptionsGetter)
 	)
 
@@ -114,6 +120,7 @@ type ExtraOptions struct {
 	WorkloadIdentityTokenIssuer        string
 	WorkloadIdentityTokenMinExpiration time.Duration
 	WorkloadIdentityTokenMaxExpiration time.Duration
+	WorkloadIdentitySigningKeyFile     string
 
 	LogLevel  string
 	LogFormat string
@@ -156,6 +163,12 @@ func (o *ExtraOptions) Validate() []error {
 		allErrors = append(allErrors, errors.New("--workload-identity-token-max-expiration must be between 10 minutes and 2^32 seconds"))
 	}
 
+	if len(o.WorkloadIdentitySigningKeyFile) != 0 {
+		if _, err := keyutil.PrivateKeyFromFile(o.WorkloadIdentitySigningKeyFile); err != nil {
+			allErrors = append(allErrors, fmt.Errorf("--workload-identity-signing-key-file does not contain valid key, err: %w", err))
+		}
+	}
+
 	if !sets.New(logger.AllLogLevels...).Has(o.LogLevel) {
 		allErrors = append(allErrors, fmt.Errorf("invalid --log-level: %s", o.LogLevel))
 	}
@@ -176,6 +189,7 @@ func (o *ExtraOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&o.WorkloadIdentityTokenIssuer, "workload-identity-token-issuer", o.WorkloadIdentityTokenIssuer, "The issuer identifier of the workload identity tokens set in the 'iss' claim. If set, it must be a valid URL")
 	fs.DurationVar(&o.WorkloadIdentityTokenMinExpiration, "workload-identity-token-min-expiration", time.Hour, "The minimum validity duration of a workload identity token. If an otherwise valid TokenRequest with a validity duration less than this value is requested, a token will be issued with a validity duration of this value.")
 	fs.DurationVar(&o.WorkloadIdentityTokenMaxExpiration, "workload-identity-token-max-expiration", time.Hour*48, "The maximum validity duration of a workload identity token. If an otherwise valid TokenRequest with a validity duration greater than this value is requested, a token will be issued with a validity duration of this value.")
+	fs.StringVar(&o.WorkloadIdentitySigningKeyFile, "workload-identity-signing-key-file", o.WorkloadIdentitySigningKeyFile, "Path to the file that contains the current private key of the workload identity token issuer. The issuer will sign issued ID tokens with this private key.")
 
 	fs.StringVar(&o.LogLevel, "log-level", "info", "The level/severity for the logs. Must be one of [info,debug,error]")
 	fs.StringVar(&o.LogFormat, "log-format", "json", "The format for the logs. Must be one of [json,text]")
@@ -189,5 +203,13 @@ func (o *ExtraOptions) ApplyTo(c *Config) error {
 	c.ExtraConfig.WorkloadIdentityTokenIssuer = o.WorkloadIdentityTokenIssuer
 	c.ExtraConfig.WorkloadIdentityTokenMinExpiration = o.WorkloadIdentityTokenMinExpiration
 	c.ExtraConfig.WorkloadIdentityTokenMaxExpiration = o.WorkloadIdentityTokenMaxExpiration
+	c.ExtraConfig.ClusterIdentity = o.ClusterIdentity
+
+	signingKey, err := keyutil.PrivateKeyFromFile(o.WorkloadIdentitySigningKeyFile)
+	if err != nil {
+		return err
+	}
+	c.ExtraConfig.WorkloadIdentitySigningKey = signingKey
+
 	return nil
 }
