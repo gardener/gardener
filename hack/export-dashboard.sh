89778f7b5d65c3c554f5030f604c1bcbfcbe2100
Victor Herrero Otal <victor.herrero.otal@sap.com>
2024-07-02 18:17:11 +0200

    Rename CoreDNS dashboard to DNS
    Also rename some references to it.
    
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>

--- hack/export-dashboard.sh
+++ hack/export-dashboard.sh
@@ -50,7 +50,7 @@
 # export it via this script and create meaningful git commits.
 #
 # ```bash
-# hack/export-dashboard.sh QSl_u1wIz core-dns pkg/component/observability/plutono/dashboards/shoot/owners/worker/coredns-dashboard.json
+# hack/export-dashboard.sh QSl_u1wIz dns pkg/component/observability/plutono/dashboards/shoot/owners/worker/dns-dashboard.json
 # ```
 
 cd "$(dirname "$(realpath "$0")")/.." || exit 1
