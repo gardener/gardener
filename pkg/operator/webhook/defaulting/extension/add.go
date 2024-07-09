0d9095bb32a388160901605b1554735d50134ee9
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2024-07-09 04:37:46 +0200

    fix copyright template

--- pkg/operator/webhook/defaulting/extension/add.go
+++ pkg/operator/webhook/defaulting/extension/add.go
@@ -1,6 +1,6 @@
-//  SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
 //
-//  SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 
 package extension
 
