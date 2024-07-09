5f30847f55cefc70d3cee760a9a7b952c7c29132
Viktor <v.kostov@sap.com>
2024-07-09 12:31:56 +0300

    Use forkedyaml and add test to guarantee deterministic yaml serialization

---
+++ pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler_export_test.go
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controllerinstallation
+
+var MutateObjects = mutateObjects
