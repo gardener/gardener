3e695674769a75a6fe1f3241c15d560faf003ba3
Aleksandar Savchev <aleksandar.savchev@sap.com>
2024-07-15 15:37:03 +0300

    Use mask flag --now to stop sshd service

--- pkg/component/extensions/operatingsystemconfig/original/components/sshdensurer/component_test.go
+++ pkg/component/extensions/operatingsystemconfig/original/components/sshdensurer/component_test.go
@@ -128,12 +128,7 @@ set -e
 # Disable and mask sshd service if not masked
 if [ "$(systemctl is-enabled sshd.service)" != "masked" ] ; then
     systemctl disable sshd.service || true
-    systemctl mask sshd.service
-fi
-
-# Stop sshd service if active
-if systemctl is-active --quiet sshd.service ; then
-    systemctl stop sshd.service
+    systemctl mask --now sshd.service
 fi
 
 # Stopping the sshd service does not terminate already established connections
