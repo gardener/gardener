5e4891e20c190298257f795d72879ef3dfe2a069
Victor Herrero Otal <victor.herrero.otal@sap.com>
2024-07-17 16:32:17 +0200

    Wait for previous managed resource to be healthy
    The cluster role and cluster role binding were originally managed by the
    shoot-core-kube-state-metrics managed resource but are now managed by
    the shoot-core-kube-state-metrics-target managed resource.
    
    In order to migrate those two resources to the latter managed resource,
    we need to wait until the former stops managing them and deletes them.
    Otherwise, the previous managed resource wants to delete them while the
    new one keeps creating them.
    
    As for the tests, a second goroutine is introduced to watch to the
    shoot-core-kube-state-metrics managed resource and set its generation
    and status conditions to healthy.

--- pkg/component/observability/monitoring/kubestatemetrics/kubestatemetrics_test.go
+++ pkg/component/observability/monitoring/kubestatemetrics/kubestatemetrics_test.go
@@ -1147,6 +1147,39 @@ var _ = Describe("KubeStateMetrics", func() {
 					Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
 					Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceTarget), managedResourceTarget)).To(BeNotFoundError())
 
+					// TODO(vicwicker): Remove after KSM upgrade to 2.13.
+					go func() {
+						defer GinkgoRecover()
+						mr := &resourcesv1alpha1.ManagedResource{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      managedResourceName,
+								Namespace: namespace,
+							},
+						}
+						Eventually(func() error {
+							return c.Get(ctx, client.ObjectKeyFromObject(mr), mr)
+						}).Should(Succeed())
+						mr.ObjectMeta.Generation = 1
+						mr.Status = resourcesv1alpha1.ManagedResourceStatus{
+							ObservedGeneration: 1,
+							Conditions: []gardencorev1beta1.Condition{
+								{
+									Type:   resourcesv1alpha1.ResourcesApplied,
+									Status: gardencorev1beta1.ConditionTrue,
+								},
+								{
+									Type:   resourcesv1alpha1.ResourcesHealthy,
+									Status: gardencorev1beta1.ConditionTrue,
+								},
+								{
+									Type:   resourcesv1alpha1.ResourcesProgressing,
+									Status: gardencorev1beta1.ConditionFalse,
+								},
+							},
+						}
+						Expect(c.Update(ctx, mr)).To(Succeed())
+					}()
+
 					Expect(ksm.Deploy(ctx)).To(Succeed())
 
 					Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(Succeed())
@@ -1195,6 +1228,28 @@ var _ = Describe("KubeStateMetrics", func() {
 							KeepObjects: ptr.To(false),
 						},
 					}
+
+					// TODO(vicwicker): Remove after KSM upgrade to 2.13.
+					expectedMr.ObjectMeta.ResourceVersion = "2"
+					expectedMr.ObjectMeta.Generation = 1
+					expectedMr.Status = resourcesv1alpha1.ManagedResourceStatus{
+						ObservedGeneration: 1,
+						Conditions: []gardencorev1beta1.Condition{
+							{
+								Type:   resourcesv1alpha1.ResourcesApplied,
+								Status: gardencorev1beta1.ConditionTrue,
+							},
+							{
+								Type:   resourcesv1alpha1.ResourcesHealthy,
+								Status: gardencorev1beta1.ConditionTrue,
+							},
+							{
+								Type:   resourcesv1alpha1.ResourcesProgressing,
+								Status: gardencorev1beta1.ConditionFalse,
+							},
+						},
+					}
+
 					utilruntime.Must(references.InjectAnnotations(expectedMr))
 					Expect(managedResource).To(DeepEqual(expectedMr))
 					prometheusRule := prometheusRuleShoot(values.IsWorkerless)
@@ -1232,6 +1287,39 @@ var _ = Describe("KubeStateMetrics", func() {
 					Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
 					Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResourceTarget), managedResourceTarget)).To(BeNotFoundError())
 
+					// TODO(vicwicker): Remove after KSM upgrade to 2.13.
+					go func() {
+						defer GinkgoRecover()
+						mr := &resourcesv1alpha1.ManagedResource{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      managedResourceName,
+								Namespace: namespace,
+							},
+						}
+						Eventually(func() error {
+							return c.Get(ctx, client.ObjectKeyFromObject(mr), mr)
+						}).Should(Succeed())
+						mr.ObjectMeta.Generation = 1
+						mr.Status = resourcesv1alpha1.ManagedResourceStatus{
+							ObservedGeneration: 1,
+							Conditions: []gardencorev1beta1.Condition{
+								{
+									Type:   resourcesv1alpha1.ResourcesApplied,
+									Status: gardencorev1beta1.ConditionTrue,
+								},
+								{
+									Type:   resourcesv1alpha1.ResourcesHealthy,
+									Status: gardencorev1beta1.ConditionTrue,
+								},
+								{
+									Type:   resourcesv1alpha1.ResourcesProgressing,
+									Status: gardencorev1beta1.ConditionFalse,
+								},
+							},
+						}
+						Expect(c.Update(ctx, mr)).To(Succeed())
+					}()
+
 					Expect(ksm.Deploy(ctx)).To(Succeed())
 
 					Expect(c.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(Succeed())
@@ -1280,6 +1368,28 @@ var _ = Describe("KubeStateMetrics", func() {
 							KeepObjects: ptr.To(false),
 						},
 					}
+
+					// TODO(vicwicker): Remove after KSM upgrade to 2.13.
+					expectedMr.ObjectMeta.ResourceVersion = "2"
+					expectedMr.ObjectMeta.Generation = 1
+					expectedMr.Status = resourcesv1alpha1.ManagedResourceStatus{
+						ObservedGeneration: 1,
+						Conditions: []gardencorev1beta1.Condition{
+							{
+								Type:   resourcesv1alpha1.ResourcesApplied,
+								Status: gardencorev1beta1.ConditionTrue,
+							},
+							{
+								Type:   resourcesv1alpha1.ResourcesHealthy,
+								Status: gardencorev1beta1.ConditionTrue,
+							},
+							{
+								Type:   resourcesv1alpha1.ResourcesProgressing,
+								Status: gardencorev1beta1.ConditionFalse,
+							},
+						},
+					}
+
 					utilruntime.Must(references.InjectAnnotations(expectedMr))
 					Expect(managedResource).To(DeepEqual(expectedMr))
 					prometheusRule := prometheusRuleShoot(values.IsWorkerless)
