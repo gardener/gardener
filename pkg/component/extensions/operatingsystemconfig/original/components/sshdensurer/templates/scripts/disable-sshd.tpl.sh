c9209ce3dd6161e6a1e12e5997302fab2f6e74b3
Aleksandar Savchev <aleksandar.savchev@sap.com>
2024-07-18 17:43:21 +0300

    Improve disable-sshd script comments

--- pkg/component/extensions/operatingsystemconfig/original/components/sshdensurer/templates/scripts/disable-sshd.tpl.sh
+++ pkg/component/extensions/operatingsystemconfig/original/components/sshdensurer/templates/scripts/disable-sshd.tpl.sh
@@ -4,9 +4,11 @@ set -e
 # Disable and mask sshd service if not masked
 if [ "$(systemctl is-enabled sshd.service)" != "masked" ] ; then
     systemctl disable sshd.service || true
+    # Using the --now flag stops the selected service
     systemctl mask --now sshd.service
 fi
 
-# Stopping the sshd service does not terminate already established connections
+# Stopping the sshd service with the --now flag does
+# not terminate already established connections
 # Kill all currently established orphaned ssh connections on the host
 pkill -P 1 -x sshd || true
