37c4dc800f02714061a4b4fbf105265d0411ba24
Martin Weindel <martin.weindel@sap.com>
2024-07-26 12:56:40 +0200

    address review feedback

--- pkg/utils/secrets/manager/generate.go
+++ pkg/utils/secrets/manager/generate.go
@@ -254,7 +254,7 @@ func (m *manager) isStillValid(secret *corev1.Secret) (bool, error) {
 	}
 	validUntilUnix, err := strconv.ParseInt(value, 10, 64)
 	if err != nil {
-		return false, fmt.Errorf("parsing label %q of secret %s/%s failed: %w", LabelKeyValidUntilTime, secret.Namespace, secret.Name, err)
+		return false, fmt.Errorf("parsing label %q of secret %s failed: %w", LabelKeyValidUntilTime, client.ObjectKeyFromObject(secret), err)
 	}
 	return m.clock.Now().UTC().Unix() < validUntilUnix, nil
 }
