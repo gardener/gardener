gardener/gardener

commit b48c204dd19180ad3ce14d886f72764033c4db04

Rework seed alerts to consider all possible conditions (#9750)
* Revisit topology for current alerts

- Topology `shoot` to refer a seed is incorrect.
- Change topology from `garden` to `seed` for
  those alerts checking seed conditions.

* Remove unnecessary labels `is_compliant` and `has_user_errors`

We do not want to suppress alerts on seeds that are managed by the
Gardener team, even if the seeds are not compliant or the errors are
user errors.

* Make sure `max_over_time` is used in the alerts in case of flapping

Without it, flapping conditions would probably go undetected because
the alerts would be reset each time.

For example, when the seed is reconciling its artifacts, some of its
managed resources can enter the progressing state for a brief period,
and to avoid alerts from being restarted, the `max_over_time` function
is now introduced to smooth out this behaviour. This is combined with
the subquery syntax [1] to query over the last minutes only.

This change is also reducing the time before firing an alert in the `for`
entry from 30m to 10m for some alerts to alert more aggressively if any of
those conditions fail. An exception is the alert `SeedAPIServerUnavailable`,
which was already firing after 2m, but now changed to 3m to preserve the
same behaviour after the change introducing `max_over_time` to smooth
out possible flappings.

[1] https://prometheus.io/docs/prometheus/latest/querying/basics/#subquery

* Use `last_over_time` instead of `max_over_time`

The usage of a `<aggregate>_over_time` function is only required to
check if there is data within the queried time window that triggers
the alert. If the query return data, then the value is always true.
Otherwise, the query returns nothing.

Therefore, it is meaningless to calculate the maximum over a set
of "trues". Instead, using `last_over_time` is a better approach
to check if the query returned data.

* Make sure all seed alerts are not muted on the weekends

pkg/component/observability/monitoring/prometheus/garden/assets/prometheusrules/seed.yaml
@@ -15,7 +15,7 @@ spec:
         topology: garden
       annotations:
         summary: Some container is restarting frequently
-        description: >
+        description: >-
           Some container in namespace {{$labels.namespace}} in seed
           {{$labels.seed}} in landscape {{$externalLabels.landscape}} was
           restarted more than 5 times within the last hour.
@@ -28,57 +28,12 @@ spec:
         topology: garden
       annotations:
         summary: There is a persistent volume with a size mismatch
-        description: |
+        description: |-
           Landscape: {{$externalLabels.landscape}}
           Seed: {{$labels.seed}}
           Namespace:  {{$labels.namespace}}
           PersistentVolumeClaim:  {{$labels.persistentvolumeclaim}}
           Size difference: {{$value}}%
-
-    - alert: GardenletDown
-      expr: |
-        min_over_time(garden_seed_condition{condition = "GardenletReady"}[5m])
-        == 0
-      for: 10m
-      labels:
-        severity: critical
-        topology: seed
-        service: gardenlet
-      annotations:
-        summary: Gardenlet Down in landscape {{$externalLabels.landscape}}
-        description: The Gardenlet in seed {{$labels.name}} is down.
-
-    - alert: GardenletUnknown
-      expr: |
-        min_over_time(garden_seed_condition{condition = "GardenletReady"}[5m])
-        == -1
-      for: 10m
-      labels:
-        severity: critical
-        topology: seed
-        service: gardenlet
-      annotations:
-        summary: >-
-          Gardenlet Unknown Status in landscape {{$externalLabels.landscape}}
-        description: >
-          The Gardenlet in seed {{$labels.name}} has been in an in unknown state
-          for over 10 minutes.
-
-    - alert: SeedAPIServerUnavailable
-      expr: |
-        garden_shoot_condition{condition = "APIServerAvailable",
-                              operation = "Reconcile",
-                              is_seed   = "true"}
-        < 1
-      for: 2m
-      labels:
-        severity: critical
-        topology: shoot
-      annotations:
-        summary: API Server Unavailable
-        description: >
-          The seed cluster: {{$labels.project}}/{{$labels.name}} has the
-          condition APIServerAvailable < 1.
 
     - alert: ApproachingSeedCapacity
       expr: |
@@ -132,7 +87,7 @@ spec:
         summary: >-
           {{$externalLabels.landscape}}: {{$labels.region}} in {{$labels.iaas}} is
           approaching capacity
-        description: >
+        description: >-
           {{$labels.region}} in {{$labels.iaas}} has crossed the 98% capacity threshold.
           The alert will stop firing when the seed usage drops below 90%.
 
@@ -234,7 +189,7 @@ spec:
         summary: >-
           {{$externalLabels.landscape}}: {{$labels.region}} in {{$labels.iaas}} is
           approaching capacity
-        description: >
+        description: >-
           {{$labels.region}} in {{$labels.iaas}} has crossed the 90% capacity threshold.
           The alert will stop firing when the seed usage drops below 85%.
 
@@ -292,7 +247,7 @@ spec:
         topology: garden
       annotations:
         summary: Seed federation failure
-        description: |
+        description: |-
           Federating from the prometheus-aggregate instance {{$labels.instance}} failed.
           Please check the error message in the `prometheus-garden` in the garden runtime cluster.
           Port-forward to `prometheus-garden-0` in the `garden` namespace and open:
@@ -307,7 +262,7 @@ spec:
         topology: garden
       annotations:
         summary: A pod is stuck in Pending state for more than 10 minutes.
-        description: >
+        description: >-
           The pod {{$labels.pod}} in namespace {{$labels.namespace}} in seed
           {{$labels.seed}} in landscape {{$externalLabels.landscape}} is
           stuck in Pending state for more than 10 minutes.
@@ -321,79 +276,25 @@ spec:
         topology: garden
       annotations:
         summary:  A node was reported not healthy for 5 scrapes in the past 30 minutes.
-        description: >
+        description: >-
           Node {{$labels.node}} in landscape
-          {{$externalLabels.landscape}} was not healthy for five scrapes in the past 30 minutes.
 
-    - alert: SeedControlPlaneUnhealthy
+    - alert: SeedConditionFailing
       expr: |
-        max_over_time(
-          garden_shoot_condition{condition       = "ControlPlaneHealthy",
-                                 operation       = "Reconcile",
-                                 is_seed         = "true",
-                                 is_compliant    = "True",
-                                 has_user_errors = "false"}[2m])
-        == 0
-      for: 30m
+        max by (name, condition) (
+          last_over_time((
+            garden_seed_condition <= 0
+            or
+            garden_shoot_condition{is_seed   = "true",
+                                   operation = "Reconcile"} <= 0)[5m:]))
+      for: 10m
       labels:
         severity: critical
-        topology: garden
-      annotations:
-        summary: Seed ControlPlane Components Unhealthy
-        description: >
-          The seed cluster: {{$labels.name}} has
-          condition ControlPlaneHealthy == 0 for more than 30 minutes. Some Control Plane
-          components of the seed may have an issue.
-
-          {{with printf `ALERTS{alertstate       = 'firing',
-                                project          = '%s',
-                                shoot_name       = '%s',
-                                severity         =~'critical|blocker',
-                                shoot_alertname != ''}` $labels.project $labels.name
-                 | query
-                 | sortByLabel "shoot_alertname"}}
-          {{if .}}Currently firing alerts:
-
-          {{end -}}
-          {{range . -}}
-          - {{.Labels.shoot_alertname}}
-
-          {{end -}}
-          {{end -}}
-
-    - alert: SeedSystemComponentsUnhealthy
-      expr: |
-        max_over_time(
-          garden_shoot_condition{condition       = "SystemComponentsHealthy",
-                                 operation       = "Reconcile",
-                                 is_seed         = "true",
-                                 is_compliant    = "True",
-                                 has_user_errors = "false"}[2m])
-        == 0
-      for: 30m
-      labels:
-        severity: critical
-        topology: garden
+        topology: seed
         mute_on_weekends: "true"
       annotations:
-        summary: Seed System Components Unhealthy
-        description: >
-          The seed cluster: {{$labels.name}} has
-          condition SystemComponentsHealthy == 0 for more than 30 minutes. Some
-          components required by the seed may have an issue.
-
-          {{with printf `ALERTS{alertstate       = 'firing',
-                                project          = '%s',
-                                shoot_name       = '%s',
-                                severity         =~'critical|blocker',
-                                shoot_alertname != ''}` $labels.project $labels.name
-                 | query
-                 | sortByLabel "shoot_alertname"}}
-          {{if .}}Currently firing alerts:
-
-          {{end -}}
-          {{range . -}}
-          - {{.Labels.shoot_alertname}}
-
-          {{end -}}
-          {{end -}}
+        summary: >-
+          Seed Condition Failing
+        description: >-
+          The seed cluster {{$labels.name}} in {{$externalLabels.landscape}}
+          has a failing condition: {{$labels.condition}}.

pkg/component/observability/monitoring/prometheus/garden/prometheusrules_test.go
@@ -47,6 +47,7 @@ var _ = ginkgo.Describe("PrometheusRules", func() {
 			))
 
 			test.PrometheusRule(metering, "testdata/metering-meta.prometheusrule.test.yaml")
+			test.PrometheusRule(seed, "testdata/seed.prometheusrule.test.yaml")
 		})
 	})
 })

pkg/component/observability/monitoring/prometheus/garden/testdata/seed.prometheusrule.test.yaml
@@ -1,0 +1,78 @@
+# Condition states are mapped to numbers by the gardener-metrics-exporter:
+#
+# 2: Progressing
+# 1: True
+# 0: False
+# -1: Unknown
+
+rule_files:
+- seed.prometheusrule.yaml
+
+
+tests:
+
+  - name: SeedConditionFailing:SmoothOutFlapping
+    interval: 1m
+    input_series:
+      - series: garden_seed_condition{condition = "ExtensionsReady",
+                                      name      = "seed-flapping"}
+        values: 0 0 2 -1 2 0
+      - series: garden_seed_condition{condition = "ExtensionsReady",
+                                      name      = "seed-progressing-for-long"}
+        values: 2 2 0 0 2 2 0 0 0 0 0
+    external_labels:
+      landscape: landscape-unit-tests
+    alert_rule_test:
+      - alertname: SeedConditionFailing
+        eval_time: 10m
+        exp_alerts:
+          - exp_labels:
+              severity: critical
+              topology: seed
+              condition: ExtensionsReady
+              name: seed-flapping
+              mute_on_weekends: "true"
+            exp_annotations:
+              summary: >-
+                Seed Condition Failing
+              description: >-
+                The seed cluster seed-flapping in landscape-unit-tests
+                has a failing condition: ExtensionsReady.
+
+  - name: SeedConditionFailing:MultipleSeeds
+    interval: 1m
+    input_series:
+      - series: garden_shoot_condition{is_seed="true", operation="Reconcile", condition="SystemComponentsHealthy", name="seed-one"}
+        values: "0x10"
+      - series: garden_seed_condition{condition="SeedSystemComponentsHealthy", name="seed-two"}
+        values: "0x10"
+    external_labels:
+      landscape: landscape-unit-tests
+    alert_rule_test:
+      - alertname: SeedConditionFailing
+        eval_time: 10m
+        exp_alerts:
+          - exp_labels:
+              severity: critical
+              topology: seed
+              condition: SystemComponentsHealthy
+              name: seed-one
+              mute_on_weekends: "true"
+            exp_annotations:
+              summary: >-
+                Seed Condition Failing
+              description: >-
+                The seed cluster seed-one in landscape-unit-tests
+                has a failing condition: SystemComponentsHealthy.
+          - exp_labels:
+              severity: critical
+              topology: seed
+              condition: SeedSystemComponentsHealthy
+              name: seed-two
+              mute_on_weekends: "true"
+            exp_annotations:
+              summary: >-
+                Seed Condition Failing
+              description: >-
+                The seed cluster seed-two in landscape-unit-tests
+                has a failing condition: SeedSystemComponentsHealthy.

commit e185f910c2670cdb1164105e014e7484f8a42bdf

Fix and improve alert descriptions (#10195)
* Remove empty lines from alert descriptions

Alert descriptions and summaries should not end with empty lines because
they might break the final alert format generated by the alert template.

* Fix SeedNodeNotHealthy alert description

Commit b48c204 accidentally removed one line too much and broke the
SeedNodeNotHealthy alert description.

* Remove use of externalLabels.landscape in alert from aggregate Prometheus

This label is only available in the garden Prometheus. The seed label
can be used instead despite it does not contain any information about
the landscape in the aggregate Prometheus.

However, this alert is federated into the garden Prometheus and
reimplemented there again to also be sent using the alert channels from
the garden Prometheus.

This commit adapts this alert description in the garden Prometheus to
add information about the seed and the landscape. This way, the alert
still does not contain information about the landscape when sent by the
seed Alertmanager, but it does now when sent by the garden Alertmanager.
Other alerts follow the same approach when federated into the garden
Prometheus.

* Generalize SeedNodeNotHealthy alert message

This alert is federated from another alert in the aggregate Prometheus
in the seed. This was changed from five to ten scrapes but we missed to
adapt it in the garden Prometheus as well.

It is better to have a more generic message that matches possible future
changes.

pkg/component/observability/monitoring/prometheus/aggregate/prometheusrules.go
@@ -63,7 +63,7 @@ func CentralPrometheusRules() []*monitoringv1.PrometheusRule {
 								"visibility": "operator",
 							},
 							Annotations: map[string]string{
-								"description": "Node {{$labels.node}} in landscape {{$externalLabels.landscape}} was not healthy for ten scrapes in the past 30 mins.",
+								"description": "Node {{$labels.node}} in seed {{$externalLabels.seed}} was not healthy for ten scrapes in the past 30 mins.",
 								"summary":     "A node is not healthy.",
 							},
 						},

pkg/component/observability/monitoring/prometheus/garden/assets/prometheusrules/auditlog.yaml
@@ -28,6 +28,6 @@ spec:
         summary: >-
           The {{$labels.job}} server in landscape {{$externalLabels.landscape}}
           has too many failed attempts to log audit events.
-        description: >
+        description: >-
           The API server's cumulative failure rate in logging audit events is
           greater than 2%.

pkg/component/observability/monitoring/prometheus/garden/assets/prometheusrules/etcd.yaml
@@ -73,7 +73,7 @@ spec:
         summary: >-
           Virtual garden etcd main has no leader in landscape
           {{$externalLabels.landscape}}.
-        description: >
+        description: >-
           Virtual garden etcd main has no leader. Possible network partition in the etcd cluster.
 
     - alert: EtcdEventsNoLeader

pkg/component/observability/monitoring/prometheus/garden/assets/prometheusrules/seed.yaml
@@ -275,9 +275,10 @@ spec:
         severity: warning
         topology: garden
       annotations:
-        summary:  A node was reported not healthy for 5 scrapes in the past 30 minutes.
+        summary:  A node was reported not healthy for several scrapes in the past 30 minutes.
         description: >-
-          Node {{$labels.node}} in landscape
+          Node {{$labels.node}} in seed {{$labels.seed}} in landscape
+          {{$externalLabels.landscape}} was not healthy for several scrapes in the past 30 minutes.
 
     - alert: SeedConditionFailing
       expr: |

pkg/component/observability/monitoring/prometheus/garden/assets/prometheusrules/shoot.yaml
@@ -31,7 +31,7 @@ spec:
         summary: >-
           Over 10% of productive shoots are unavailable on the 
           {{$externalLabels.landscape}} landscape.
-        description: >
+        description: >-
           Over 10% of the shoots have the condition APIServerAvailable false.
           (
           {{`count(
@@ -75,7 +75,7 @@ spec:
         summary: >-
           Over 10% of productive {{$labels.iaas}} shoots are unavailable on the 
           {{$externalLabels.landscape}} landscape.
-        description: >
+        description: >-
           Over 10% of {{$labels.iaas}} shoots have the condition
           APIServerAvailable == 0.
           (
@@ -108,6 +108,6 @@ spec:
         topology: seed
       annotations:
         summary: Node filesystem has less than 5% inodes left.
-        description: >
+        description: >-
           The seed cluster: {{$labels.project}}/{{$labels.name}} has a filesystem mounted at {{ $labels.mountpoint }}
           on node {{ $labels.node }} that has only {{ printf "%.2f" $value }}% available inodes left.

pkg/component/observability/monitoring/prometheus/aggregate/prometheusrules_test.go
@@ -55,7 +55,7 @@ var _ = Describe("PrometheusRules", func() {
 										"visibility": "operator",
 									},
 									Annotations: map[string]string{
-										"description": "Node {{$labels.node}} in landscape {{$externalLabels.landscape}} was not healthy for ten scrapes in the past 30 mins.",
+										"description": "Node {{$labels.node}} in seed {{$externalLabels.seed}} was not healthy for ten scrapes in the past 30 mins.",
 										"summary":     "A node is not healthy.",
 									},
 								},