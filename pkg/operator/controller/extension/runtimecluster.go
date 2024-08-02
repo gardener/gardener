2a24204d353051456bf72921f4fb276677070bb4
oliver-goetz <o.goetz@sap.com>
2024-08-02 13:59:33 +0200

    Deploy admission controller of extensions into virtual and runtime-cluster

---
+++ pkg/operator/controller/extension/runtimecluster.go
@@ -0,0 +1,99 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package extension
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	operatorv1alpha1 "github.com/gardener/gardener/pkg/apis/operator/v1alpha1"
+	"github.com/gardener/gardener/pkg/utils"
+	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
+	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
+	"github.com/gardener/gardener/pkg/utils/managedresources"
+)
+
+func runtimeClusterAdmissionManagedResourceName(extension *operatorv1alpha1.Extension) string {
+	return fmt.Sprintf("extension-admission-runtime-%s", extension.Name)
+}
+
+func (r *Reconciler) reconcileAdmissionRuntimeClusterResources(ctx context.Context, log logr.Logger, genericTokenKubeconfigSecretName string, extension *operatorv1alpha1.Extension) error {
+	// return early if we do not have to make a deployment
+	if extension.Spec.Deployment == nil ||
+		extension.Spec.Deployment.AdmissionDeployment == nil ||
+		extension.Spec.Deployment.AdmissionDeployment.RuntimeCluster == nil ||
+		extension.Spec.Deployment.AdmissionDeployment.RuntimeCluster.Helm == nil {
+		return r.deleteAdmissionRuntimeClusterResources(ctx, log, extension)
+	}
+
+	archive, err := r.HelmRegistry.Pull(ctx, extension.Spec.Deployment.AdmissionDeployment.RuntimeCluster.Helm.OCIRepository)
+	if err != nil {
+		return fmt.Errorf("failed pulling Helm chart from OCI repository: %w", err)
+	}
+
+	accessSecret := r.getVirtualClusterAccessSecret(extension)
+	if err := accessSecret.Reconcile(ctx, r.RuntimeClientSet.Client()); err != nil {
+		return fmt.Errorf("failed reconciling access secret: %w", err)
+	}
+
+	gardenerValues := map[string]any{
+		"global": map[string]any{
+			"projectedKubeconfig": map[string]any{
+				"baseMountPath":               gardenerutils.VolumeMountPathGenericKubeconfig,
+				"genericKubeconfigSecretName": genericTokenKubeconfigSecretName,
+				"tokenSecretName":             accessSecret.Secret.Name,
+			},
+			"virtualGarden": map[string]any{
+				"enabled": true,
+				"user": map[string]any{
+					"name": fmt.Sprintf("system:serviceaccount:kube-system:%s", accessSecret.ServiceAccountName),
+				},
+			},
+		},
+	}
+
+	var helmValues map[string]any
+	if extension.Spec.Deployment.AdmissionDeployment.Values != nil {
+		if err := json.Unmarshal(extension.Spec.Deployment.AdmissionDeployment.Values.Raw, &helmValues); err != nil {
+			return err
+		}
+	}
+
+	renderedChart, err := r.RuntimeClientSet.ChartRenderer().RenderArchive(archive, extension.Name, v1beta1constants.GardenNamespace, utils.MergeMaps(helmValues, gardenerValues))
+	if err != nil {
+		return fmt.Errorf("failed rendering Helm chart: %w", err)
+	}
+
+	if err := managedresources.CreateForSeed(ctx, r.RuntimeClientSet.Client(), r.GardenNamespace, runtimeClusterAdmissionManagedResourceName(extension), false, renderedChart.AsSecretData()); err != nil {
+		return fmt.Errorf("failed creating ManagedResource: %w", err)
+	}
+
+	if err := managedresources.WaitUntilHealthy(ctx, r.RuntimeClientSet.Client(), r.GardenNamespace, runtimeClusterAdmissionManagedResourceName(extension)); err != nil {
+		return fmt.Errorf("failed waiting for ManagedResource to be healthy: %w", err)
+	}
+
+	r.Recorder.Event(extension, corev1.EventTypeNormal, "Reconciliation", "Admission Helm chart applied successfully to runtime cluster")
+
+	return nil
+}
+
+func (r *Reconciler) deleteAdmissionRuntimeClusterResources(ctx context.Context, log logr.Logger, extension *operatorv1alpha1.Extension) error {
+	log.Info("Deleting admission ManagedResource for runtime cluster")
+	if err := managedresources.DeleteForSeed(ctx, r.RuntimeClientSet.Client(), r.GardenNamespace, runtimeClusterAdmissionManagedResourceName(extension)); err != nil {
+		return fmt.Errorf("failed deleting ManagedResource: %w", err)
+	}
+
+	if err := managedresources.WaitUntilDeleted(ctx, r.RuntimeClientSet.Client(), r.GardenNamespace, runtimeClusterAdmissionManagedResourceName(extension)); err != nil {
+		return fmt.Errorf("failed waiting for ManagedResource to be deleted: %w", err)
+	}
+
+	log.Info("Deleting admission access secret for virtual cluster")
+	return kubernetesutils.DeleteObjects(ctx, r.RuntimeClientSet.Client(), r.getVirtualClusterAccessSecret(extension).Secret)
+}
