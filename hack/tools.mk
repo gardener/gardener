2eae3daddd7a54dff82f37eae664186d1d296d6b
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-08-05 15:29:25 +0200

    chore(deps): update dependency mikefarah/yq to v4.44.3 (#10259)

--- hack/tools.mk
+++ hack/tools.mk
@@ -76,7 +76,7 @@ PROTOC_VERSION ?= v27.3
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
 SKAFFOLD_VERSION ?= v2.13.1
 # renovate: datasource=github-releases depName=mikefarah/yq
-YQ_VERSION ?= v4.44.2
+YQ_VERSION ?= v4.44.3
 # renovate: datasource=github-releases depName=ironcore-dev/vgopath
 VGOPATH_VERSION ?= v0.1.5
 
