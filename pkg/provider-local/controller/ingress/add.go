b1c1096893b57ab3f5355f95df9a5ffbf2580c3b
rfranzke <rafael.franzke@sap.com>
2024-08-13 13:40:49 +0200

    Restrict `provider-local`'s `Ingress` controller to shoot namespaces
    Otherwise, it creates `DNSRecord`s even in the `garden` namespace. When
    a seed is the garden cluster at the same time, this leads to issues on
    seed deletion because some `DNSRecord`s are still left (those managed by
    `gardener-operator` belonging to the garden cluster).
    
    As the purpose of this controller was to just make the domains
    resolvable in the shoot machine pods, we now restrict it to the shoot
    namespaces.

--- pkg/provider-local/controller/ingress/add.go
+++ pkg/provider-local/controller/ingress/add.go
@@ -7,11 +7,16 @@ package ingress
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 )
 
 // ControllerName is the name of the controller.
@@ -28,7 +33,7 @@ type AddOptions struct {
 
 // AddToManagerWithOptions adds a controller with the given Options to the given manager.
 // The opts.Reconciler is being set with a newly instantiated actuator.
-func AddToManagerWithOptions(mgr manager.Manager, opts AddOptions) error {
+func AddToManagerWithOptions(ctx context.Context, mgr manager.Manager, opts AddOptions) error {
 	opts.Controller.Reconciler = &reconciler{client: mgr.GetClient()}
 
 	ctrl, err := controller.New(ControllerName, mgr, opts.Controller)
@@ -36,10 +41,16 @@ func AddToManagerWithOptions(mgr manager.Manager, opts AddOptions) error {
 		return err
 	}
 
-	return ctrl.Watch(source.Kind(mgr.GetCache(), &networkingv1.Ingress{}), &handler.EnqueueRequestForObject{})
+	return ctrl.Watch(source.Kind(mgr.GetCache(), &networkingv1.Ingress{}), &handler.EnqueueRequestForObject{}, predicate.NewPredicateFuncs(func(object client.Object) bool {
+		namespace := &corev1.Namespace{}
+		if err := mgr.GetClient().Get(ctx, client.ObjectKey{Name: object.GetNamespace()}, namespace); err != nil {
+			ctrl.GetLogger().Error(err, "Unable to fetch namespace")
+		}
+		return namespace.Labels[v1beta1constants.GardenRole] == v1beta1constants.GardenRoleShoot
+	}))
 }
 
 // AddToManager adds a controller with the default Options.
-func AddToManager(_ context.Context, mgr manager.Manager) error {
-	return AddToManagerWithOptions(mgr, DefaultAddOptions)
+func AddToManager(ctx context.Context, mgr manager.Manager) error {
+	return AddToManagerWithOptions(ctx, mgr, DefaultAddOptions)
 }
