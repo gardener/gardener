1d46b6f49d0917a5f2bafff1b3f1bc191516b42a
Rafael Franzke <rafael.franzke@sap.com>
2024-08-13 14:37:16 +0200

    Fix flaky operator seed e2e tests (#10315)
    * Do not use unhandled `RequeueAfterError`
    
    When using this, the reconciler must handle it using `pkg/controllerutils/reconciler.ReconcileErr` (https://github.com/gardener/gardener/blob/41a694b024144465bd4f02f39d6d1c3493549c2f/pkg/controllerutils/reconciler/reconcile.go#L13-L18).
    Otherwise, this will just return a regular error for the `Reconcile`
    function, which leads to exponential backoff.
    
    Also, the log statement improves the output:
    
    ```
    "msg":"Reconciler error","controller":"extension-virtual-cluster","controllerGroup":"operator.gardener.cloud","controllerKind":"Extension","Extension":{"name":"provider-local"},"namespace":"","name":"provider-local","reconcileID":"0acc37e6-3a5a-4c09-82ec-7dfc9442561f","error":"requeue in 10s"
    ```
    
    is not really understandable
    
    * Wait for `Seed` readiness after deploying `Gardenlet`
    
    We should wait for the `Seed` object to be ready after we deployed the
    `gardenlet` (via the `seedmanagement.gardener.cloud/v1alpha1.Gardenlet`
    resource).
    
    This way, we make sure that the deployment worked before running e2e
    tests. In addition, this makes the scenarios consistent (we also wait
    for `Seed` readiness when deploying the `gardenlet` Helm chart
    directly).
    
    * Also consider runtime component readiness
    
    For the sake of completeness, we should also care about all runtime
    components to be ready.
    
    Both `VirtualGardenAPIServerAvailable` and `VirtualComponentsHealthy`
    could already be `True` even though a `Garden` reconciliation is still
    ongoing. This may confuse e2e tests and let them start too early.
    
    Examples:
    - https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/10300/pull-gardener-e2e-kind-operator-seed/1821912673458065408
    - https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/10312/pull-gardener-e2e-kind-operator-seed/1822977761006850048
    
    * `wait-for.sh` script ensures last operation state is `Succeeded`
    
    This is to make sure the condition check passes while a reconciliation
    is still ongoing.
    
    * Requeue instead of error when objects referencing `Seed` are still left
    
    Returning an error leads to exponential backoff, unnecessarily delaying
    the seed deletion.
    Let's better requeue and retry as it is an expected/valid scenario if
    there are still objects that have to be get deleted first.
    
    * Destroy CRDs in seed at the end of the flow
    
    Meanwhile, all relevant components are deployed as part of
    `ManagedResource`s, hence, we do no longer need to explicitly delete all
    `Ingress` resources.
    
    Also, this allows to drop the explicit dependency of the `Etcd` CRD
    deletion on the fact that all `ControllerInstallation`s are gone.
    
    * Increase requeue frequency in `ControllerInstallation` controller when `ManagedResource` still exists
    
    We can expect the deletion to be rather fast, so there is no need to
    requeue after `30s` only. `5s` should lead to faster reponses.
    
    This should helpw ith occurrences like this:
    
    - https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/10312/pull-gardener-e2e-kind-operator-seed/1822962980573155328
    
    * KSM `ScrapeConfig` for cache Prometheus should only be part of seed's KSM
    
    Previously, it was part of both garden and seed KSMs
    
    * Restrict `provider-local`'s `Ingress` controller to shoot namespaces
    
    Otherwise, it creates `DNSRecord`s even in the `garden` namespace. When
    a seed is the garden cluster at the same time, this leads to issues on
    seed deletion because some `DNSRecord`s are still left (those managed by
    `gardener-operator` belonging to the garden cluster).
    
    As the purpose of this controller was to just make the domains
    resolvable in the shoot machine pods, we now restrict it to the shoot
    namespaces.

--- pkg/gardenlet/controller/seed/seed/reconciler_delete.go
+++ pkg/gardenlet/controller/seed/seed/reconciler_delete.go
@@ -6,17 +6,17 @@ package seed
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"time"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
@@ -34,11 +34,14 @@ func (r *Reconciler) delete(
 	seedObj *seedpkg.Seed,
 	seedIsGarden bool,
 	isManagedSeed bool,
-) error {
+) (
+	reconcile.Result,
+	error,
+) {
 	seed := seedObj.GetInfo()
 
 	if !sets.New(seed.Finalizers...).Has(gardencorev1beta1.GardenerName) {
-		return nil
+		return reconcile.Result{}, nil
 	}
 
 	// Before deletion, it has to be ensured that no Shoots nor BackupBuckets depend on the Seed anymore.
@@ -47,51 +50,48 @@ func (r *Reconciler) delete(
 
 	associatedShoots, err := controllerutils.DetermineShootsAssociatedTo(ctx, r.GardenClient, seed)
 	if err != nil {
-		return err
+		return reconcile.Result{}, fmt.Errorf("failed to determine shoots associated to this seed: %w", err)
 	}
 
 	if len(associatedShoots) > 0 {
 		log.Info("Cannot delete Seed because the following Shoots are still referencing it", "shoots", associatedShoots)
 		r.Recorder.Event(seed, corev1.EventTypeNormal, v1beta1constants.EventResourceReferenced, fmt.Sprintf("%s Shoots=%v", parentLogMessage, associatedShoots))
-
-		return errors.New("seed still has references")
+		return reconcile.Result{RequeueAfter: time.Minute}, nil
 	}
 
 	if seed.Spec.Backup != nil {
 		backupBucket := &gardencorev1beta1.BackupBucket{ObjectMeta: metav1.ObjectMeta{Name: string(seed.UID)}}
 
 		if err := r.GardenClient.Delete(ctx, backupBucket); client.IgnoreNotFound(err) != nil {
-			return err
+			return reconcile.Result{}, fmt.Errorf("failed deleting backup bucket %s: %w", client.ObjectKeyFromObject(backupBucket), err)
 		}
 	}
 
 	associatedBackupBuckets, err := controllerutils.DetermineBackupBucketAssociations(ctx, r.GardenClient, seed.Name)
 	if err != nil {
-		return err
+		return reconcile.Result{}, fmt.Errorf("failed to determine backup buckets associated to this seed: %w", err)
 	}
 
 	if len(associatedBackupBuckets) > 0 {
 		log.Info("Cannot delete Seed because the following BackupBuckets are still referencing it", "backupBuckets", associatedBackupBuckets)
 		r.Recorder.Event(seed, corev1.EventTypeNormal, v1beta1constants.EventResourceReferenced, fmt.Sprintf("%s BackupBuckets=%v", parentLogMessage, associatedBackupBuckets))
-
-		return errors.New("seed still has references")
+		return reconcile.Result{RequeueAfter: 5 * time.Second}, nil
 	}
 
 	log.Info("No Shoots or BackupBuckets are referencing the Seed, deletion accepted")
-
 	if err := r.runDeleteSeedFlow(ctx, log, seedObj, seedIsGarden, isManagedSeed); err != nil {
-		return err
+		return reconcile.Result{}, err
 	}
 
 	// Remove finalizer from Seed
 	if controllerutil.ContainsFinalizer(seed, gardencorev1beta1.GardenerName) {
 		log.Info("Removing finalizer")
 		if err := controllerutils.RemoveFinalizers(ctx, r.GardenClient, seed, gardencorev1beta1.GardenerName); err != nil {
-			return fmt.Errorf("failed to remove finalizer: %w", err)
+			return reconcile.Result{}, fmt.Errorf("failed to remove finalizer: %w", err)
 		}
 	}
 
-	return nil
+	return reconcile.Result{}, nil
 }
 
 func (r *Reconciler) runDeleteSeedFlow(
@@ -115,15 +115,6 @@ func (r *Reconciler) runDeleteSeedFlow(
 	var (
 		g = flow.NewGraph("Seed deletion")
 
-		// Delete all ingress objects in garden namespace which are not created as part of ManagedResources. This can be
-		// removed once all seed system components are deployed as part of ManagedResources.
-		// See https://github.com/gardener/gardener/issues/6062 for details.
-		_ = g.Add(flow.Task{
-			Name: "Destroying all networkingv1.Ingress resources in the garden namespace",
-			Fn: func(ctx context.Context) error {
-				return r.SeedClientSet.Client().DeleteAllOf(ctx, &networkingv1.Ingress{}, client.InNamespace(r.GardenNamespace))
-			},
-		})
 		// Use the managed resource for cluster-identity only if there is no cluster-identity config map in kube-system namespace from a different origin than seed.
 		// This prevents gardenlet from deleting the config map accidentally on seed deletion when it was created by a different party (gardener-apiserver or shoot).
 		destroyClusterIdentity = g.Add(flow.Task{
@@ -135,11 +126,6 @@ func (r *Reconciler) runDeleteSeedFlow(
 		destroyDNSRecord = g.Add(flow.Task{
 			Name: "Destroying managed ingress DNS record (if existing)",
 			Fn:   component.OpDestroyAndWait(c.ingressDNSRecord).Destroy,
-		})
-		noControllerInstallations = g.Add(flow.Task{
-			Name:         "Ensuring no ControllerInstallations are left",
-			Fn:           ensureNoControllerInstallations(r.GardenClient, seed.GetInfo().Name),
-			Dependencies: flow.NewTaskIDs(destroyDNSRecord),
 		})
 		destroyCachePrometheus = g.Add(flow.Task{
 			Name: "Destroying cache Prometheus",
@@ -194,16 +180,6 @@ func (r *Reconciler) runDeleteSeedFlow(
 			Name: "Destroy Istio",
 			Fn:   component.OpDestroyAndWait(c.istio).Destroy,
 		})
-		destroyIstioCRDs = g.Add(flow.Task{
-			Name:         "Destroy Istio CRDs",
-			Fn:           component.OpDestroyAndWait(c.istioCRD).Destroy,
-			SkipIf:       seedIsGarden,
-			Dependencies: flow.NewTaskIDs(destroyIstio),
-		})
-		destroyMachineControllerManagerCRDs = g.Add(flow.Task{
-			Name: "Destroy machine-controller-manager CRDs",
-			Fn:   component.OpDestroyAndWait(c.machineCRD).Destroy,
-		})
 		destroyFluentOperatorResources = g.Add(flow.Task{
 			Name: "Destroy Fluent Operator Custom Resources",
 			Fn:   component.OpDestroyAndWait(c.fluentOperatorCustomResources).Destroy,
@@ -216,13 +192,9 @@ func (r *Reconciler) runDeleteSeedFlow(
 			SkipIf: seedIsGarden,
 		})
 		destroyEtcdDruid = g.Add(flow.Task{
-			Name: "Destroying etcd druid",
-			Fn:   component.OpDestroyAndWait(c.etcdDruid).Destroy,
-			// only destroy Etcd CRD once all extension controllers are gone, otherwise they might not be able to start
-			// up again (e.g. after being evicted by VPA)
-			// see https://github.com/gardener/gardener/issues/6487#issuecomment-1220597217
-			Dependencies: flow.NewTaskIDs(noControllerInstallations),
-			SkipIf:       seedIsGarden,
+			Name:   "Destroying etcd druid",
+			Fn:     component.OpDestroyAndWait(c.etcdDruid).Destroy,
+			SkipIf: seedIsGarden,
 		})
 		destroyVPA = g.Add(flow.Task{
 			Name:   "Destroy Kubernetes vertical pod autoscaler",
@@ -260,20 +232,9 @@ func (r *Reconciler) runDeleteSeedFlow(
 			Dependencies: flow.NewTaskIDs(destroyFluentOperatorResources),
 			SkipIf:       seedIsGarden,
 		})
-		destroyEtcdCRD = g.Add(flow.Task{
-			Name:         "Destroy ETCD-related custom resource definitions",
-			Fn:           component.OpDestroyAndWait(c.etcdCRD).Destroy,
-			Dependencies: flow.NewTaskIDs(destroyEtcdDruid),
-			SkipIf:       seedIsGarden,
-		})
-		destroyFluentOperatorCRDs = g.Add(flow.Task{
-			Name:         "Destroy Fluent Operator CRDs",
-			Fn:           component.OpDestroyAndWait(c.fluentCRD).Destroy,
-			Dependencies: flow.NewTaskIDs(destroyFluentOperatorResources, noControllerInstallations),
-			SkipIf:       seedIsGarden,
-		})
 
 		syncPointCleanedUp = flow.NewTaskIDs(
+			destroyDNSRecord,
 			destroyClusterIdentity,
 			destroyCachePrometheus,
 			destroySeedPrometheus,
@@ -288,10 +249,7 @@ func (r *Reconciler) runDeleteSeedFlow(
 			destroyKubeAPIServerService,
 			destroyVPNAuthzServer,
 			destroyIstio,
-			destroyIstioCRDs,
-			destroyMachineControllerManagerCRDs,
 			destroyFluentOperatorResources,
-			noControllerInstallations,
 			destroyPrometheusOperator,
 			destroyPlutono,
 			destroyKubeStateMetrics,
@@ -301,6 +259,41 @@ func (r *Reconciler) runDeleteSeedFlow(
 			destroyFluentBit,
 			destroyFluentOperator,
 			destroyVali,
+		)
+
+		ensureNoControllerInstallationsExist = g.Add(flow.Task{
+			Name:         "Ensuring all ControllerInstallations are gone",
+			Fn:           ensureNoControllerInstallations(r.GardenClient, seed.GetInfo().Name),
+			Dependencies: flow.NewTaskIDs(syncPointCleanedUp),
+		})
+
+		destroyIstioCRDs = g.Add(flow.Task{
+			Name:         "Destroy Istio CRDs",
+			Fn:           component.OpDestroyAndWait(c.istioCRD).Destroy,
+			SkipIf:       seedIsGarden,
+			Dependencies: flow.NewTaskIDs(ensureNoControllerInstallationsExist),
+		})
+		destroyMachineControllerManagerCRDs = g.Add(flow.Task{
+			Name:         "Destroy machine-controller-manager CRDs",
+			Fn:           component.OpDestroyAndWait(c.machineCRD).Destroy,
+			Dependencies: flow.NewTaskIDs(ensureNoControllerInstallationsExist),
+		})
+		destroyEtcdCRD = g.Add(flow.Task{
+			Name:         "Destroy ETCD-related custom resource definitions",
+			Fn:           component.OpDestroyAndWait(c.etcdCRD).Destroy,
+			Dependencies: flow.NewTaskIDs(ensureNoControllerInstallationsExist),
+			SkipIf:       seedIsGarden,
+		})
+		destroyFluentOperatorCRDs = g.Add(flow.Task{
+			Name:         "Destroy Fluent Operator CRDs",
+			Fn:           component.OpDestroyAndWait(c.fluentCRD).Destroy,
+			Dependencies: flow.NewTaskIDs(ensureNoControllerInstallationsExist),
+			SkipIf:       seedIsGarden,
+		})
+
+		destroyCRDs = flow.NewTaskIDs(
+			destroyIstioCRDs,
+			destroyMachineControllerManagerCRDs,
 			destroyEtcdCRD,
 			destroyFluentOperatorCRDs,
 		)
@@ -308,7 +301,7 @@ func (r *Reconciler) runDeleteSeedFlow(
 		destroySystemResources = g.Add(flow.Task{
 			Name:         "Destroy system resources",
 			Fn:           component.OpDestroyAndWait(c.system).Destroy,
-			Dependencies: flow.NewTaskIDs(syncPointCleanedUp),
+			Dependencies: flow.NewTaskIDs(destroyCRDs),
 		})
 		ensureNoManagedResourcesExist = g.Add(flow.Task{
 			Name:         "Ensuring all ManagedResources are gone",
@@ -341,7 +334,7 @@ func ensureNoControllerInstallations(c client.Client, seedName string) func(ctx 
 			return err
 		}
 
-		if associatedControllerInstallations != nil {
+		if len(associatedControllerInstallations) > 0 {
 			return fmt.Errorf("can't continue with Seed deletion, because the following objects are still referencing it: ControllerInstallations=%v", associatedControllerInstallations)
 		}
 
