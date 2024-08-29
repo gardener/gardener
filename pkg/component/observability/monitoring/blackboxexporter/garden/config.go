72e01b64064874c3958af2b023c3f1fccab2ba2a
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2024-08-29 14:46:08 +0300

    Adding a http module that can be used for admission webhooks

--- pkg/component/observability/monitoring/blackboxexporter/garden/config.go
+++ pkg/component/observability/monitoring/blackboxexporter/garden/config.go
@@ -21,61 +21,9 @@ const (
 	httpKubeAPIServerRootCAsModuleName    = "http_kube_apiserver_root_cas"
 	httpGardenerDashboardModuleName       = "http_gardener_dashboard"
 	httpGardenerDiscoveryServerModuleName = "http_gardener_discovery_server"
-	httpWebhookServerModuleName           = "http_webhooks"
-	// As we want to only check tls certificates for webhooks and don't care
-	// for the rest of the response, all of the following should be treated as OK.
-	// 5XX are excluded as they indicate that the returned result is not reliable
-)
 
-var (
-	WebhooksOKStatusCodes = []int{
-		http.StatusOK,                           // 200
-		http.StatusCreated,                      // 201
-		http.StatusAccepted,                     // 202
-		http.StatusNonAuthoritativeInfo,         // 203
-		http.StatusNoContent,                    // 204
-		http.StatusResetContent,                 // 205
-		http.StatusPartialContent,               // 206
-		http.StatusMultiStatus,                  // 207
-		http.StatusAlreadyReported,              // 208
-		http.StatusIMUsed,                       // 226
-		http.StatusMultipleChoices,              // 300
-		http.StatusMovedPermanently,             // 301
-		http.StatusFound,                        // 302
-		http.StatusSeeOther,                     // 303
-		http.StatusNotModified,                  // 304
-		http.StatusUseProxy,                     // 305
-		http.StatusTemporaryRedirect,            // 307
-		http.StatusPermanentRedirect,            // 308
-		http.StatusBadRequest,                   // 400
-		http.StatusUnauthorized,                 // 401
-		http.StatusPaymentRequired,              // 402
-		http.StatusForbidden,                    // 403
-		http.StatusNotFound,                     // 404
-		http.StatusMethodNotAllowed,             // 405
-		http.StatusNotAcceptable,                // 406
-		http.StatusProxyAuthRequired,            // 407
-		http.StatusRequestTimeout,               // 408
-		http.StatusConflict,                     // 409
-		http.StatusGone,                         // 410
-		http.StatusLengthRequired,               // 411
-		http.StatusPreconditionFailed,           // 412
-		http.StatusRequestEntityTooLarge,        // 413
-		http.StatusRequestURITooLong,            // 414
-		http.StatusUnsupportedMediaType,         // 415
-		http.StatusRequestedRangeNotSatisfiable, // 416
-		http.StatusExpectationFailed,            // 417
-		http.StatusTeapot,                       // 418
-		http.StatusMisdirectedRequest,           // 421
-		http.StatusUnprocessableEntity,          // 422
-		http.StatusLocked,                       // 423
-		http.StatusFailedDependency,             // 424
-		http.StatusUpgradeRequired,              // 426
-		http.StatusPreconditionRequired,         // 428
-		http.StatusTooManyRequests,              // 429
-		http.StatusRequestHeaderFieldsTooLarge,  // 431
-		http.StatusUnavailableForLegalReasons,   // 451
-	}
+	// Name of the module to be used for scraping of webhooks
+	HttpWebhookServerModuleName = "http_webhooks"
 )
 
 // Config returns the blackbox-exporter config for the garden use-case.
@@ -93,6 +41,58 @@ func Config(isDashboardCertificateIssuedByGardener, isGardenerDiscoveryServerEna
 					IPProtocol: "ipv4",
 				},
 			}
+		}
+
+		// As we want to only check tls certificates for webhooks and don't care
+		// for the rest of the response, all of the following should be treated as OK.
+		// 5XX are excluded as they indicate that the returned result is not reliable
+		webhookOKStatusCodes = []int{
+			http.StatusOK,                           // 200
+			http.StatusCreated,                      // 201
+			http.StatusAccepted,                     // 202
+			http.StatusNonAuthoritativeInfo,         // 203
+			http.StatusNoContent,                    // 204
+			http.StatusResetContent,                 // 205
+			http.StatusPartialContent,               // 206
+			http.StatusMultiStatus,                  // 207
+			http.StatusAlreadyReported,              // 208
+			http.StatusIMUsed,                       // 226
+			http.StatusMultipleChoices,              // 300
+			http.StatusMovedPermanently,             // 301
+			http.StatusFound,                        // 302
+			http.StatusSeeOther,                     // 303
+			http.StatusNotModified,                  // 304
+			http.StatusUseProxy,                     // 305
+			http.StatusTemporaryRedirect,            // 307
+			http.StatusPermanentRedirect,            // 308
+			http.StatusBadRequest,                   // 400
+			http.StatusUnauthorized,                 // 401
+			http.StatusPaymentRequired,              // 402
+			http.StatusForbidden,                    // 403
+			http.StatusNotFound,                     // 404
+			http.StatusMethodNotAllowed,             // 405
+			http.StatusNotAcceptable,                // 406
+			http.StatusProxyAuthRequired,            // 407
+			http.StatusRequestTimeout,               // 408
+			http.StatusConflict,                     // 409
+			http.StatusGone,                         // 410
+			http.StatusLengthRequired,               // 411
+			http.StatusPreconditionFailed,           // 412
+			http.StatusRequestEntityTooLarge,        // 413
+			http.StatusRequestURITooLong,            // 414
+			http.StatusUnsupportedMediaType,         // 415
+			http.StatusRequestedRangeNotSatisfiable, // 416
+			http.StatusExpectationFailed,            // 417
+			http.StatusTeapot,                       // 418
+			http.StatusMisdirectedRequest,           // 421
+			http.StatusUnprocessableEntity,          // 422
+			http.StatusLocked,                       // 423
+			http.StatusFailedDependency,             // 424
+			http.StatusUpgradeRequired,              // 426
+			http.StatusPreconditionRequired,         // 428
+			http.StatusTooManyRequests,              // 429
+			http.StatusRequestHeaderFieldsTooLarge,  // 431
+			http.StatusUnavailableForLegalReasons,   // 451
 		}
 
 		httpGardenerAPIServerModule       = defaultModuleConfig()
@@ -113,7 +113,7 @@ func Config(isDashboardCertificateIssuedByGardener, isGardenerDiscoveryServerEna
 	httpKubeAPIServerRootCAsModule.HTTP.HTTPClientConfig.BearerTokenFile = pathToken
 	// Webhooks are using this certificate as CA
 	httpWebhookServerModule.HTTP.HTTPClientConfig.TLSConfig.CAFile = pathGardenerAPIServerCABundle
-	httpWebhookServerModule.HTTP.ValidStatusCodes = WebhooksOKStatusCodes
+	httpWebhookServerModule.HTTP.ValidStatusCodes = webhookOKStatusCodes
 
 	if isDashboardCertificateIssuedByGardener {
 		httpGardenerDashboardModule.HTTP.HTTPClientConfig.TLSConfig.CAFile = pathGardenerAPIServerCABundle
@@ -124,7 +124,7 @@ func Config(isDashboardCertificateIssuedByGardener, isGardenerDiscoveryServerEna
 		httpKubeAPIServerModuleName:        httpKubeAPIServerModule,
 		httpKubeAPIServerRootCAsModuleName: httpKubeAPIServerRootCAsModule,
 		httpGardenerDashboardModuleName:    httpGardenerDashboardModule,
-		httpWebhookServerModuleName:        httpWebhookServerModule,
+		HttpWebhookServerModuleName:        httpWebhookServerModule,
 	}}
 
 	if isGardenerDiscoveryServerEnabled {
