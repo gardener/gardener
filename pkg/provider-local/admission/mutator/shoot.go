db4c8b9f17a8c422ecfa3b143408b985889bc6d0
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-09-10 15:50:56 +0200

    Fix workerless shoot in local setup (#10503)
    Co-authored-by: Tim Usner <tim.usner@sap.com>

--- pkg/provider-local/admission/mutator/shoot.go
+++ pkg/provider-local/admission/mutator/shoot.go
@@ -7,18 +7,15 @@ package mutator
 import (
 	"context"
 	"fmt"
-	"reflect"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
-	localv1alpha1 "github.com/gardener/gardener/pkg/provider-local/apis/local/v1alpha1"
 )
 
 // NewShootMutator returns a new instance of a shoot mutator.
@@ -33,38 +30,21 @@ type shoot struct {
 }
 
 // Mutate mutates the given shoot object.
-func (s *shoot) Mutate(_ context.Context, newObj, oldObj client.Object) error {
+func (s *shoot) Mutate(_ context.Context, newObj, _ client.Object) error {
 	shoot, ok := newObj.(*gardencorev1beta1.Shoot)
 	if !ok {
 		return fmt.Errorf("wrong object type %T", newObj)
 	}
 
-	var oldShoot *gardencorev1beta1.Shoot
-	if oldObj != nil {
-		oldShoot, ok = oldObj.(*gardencorev1beta1.Shoot)
-		if !ok {
-			return fmt.Errorf("wrong object type %T", oldObj)
-		}
-	}
-
-	// Skip if specs are matching
-	if oldShoot != nil && reflect.DeepEqual(shoot.Spec, oldShoot.Spec) {
-		return nil
-	}
+	processedByKey := "extensions.gardener.cloud/processed-by"
 
 	// Skip if shoot is in deletion phase
-	if shoot.DeletionTimestamp != nil || oldShoot != nil && oldShoot.DeletionTimestamp != nil {
+	if shoot.DeletionTimestamp != nil {
 		return nil
 	}
 
-	shoot.Spec.Provider.ControlPlaneConfig = &runtime.RawExtension{
-		Object: &localv1alpha1.ControlPlaneConfig{
-			TypeMeta: metav1.TypeMeta{
-				APIVersion: localv1alpha1.SchemeGroupVersion.String(),
-				Kind:       "ControlPlaneConfig",
-			},
-			TestValue: ptr.To("filled-by-admission-webhook"),
-		},
+	if !metav1.HasAnnotation(shoot.ObjectMeta, processedByKey) {
+		metav1.SetMetaDataAnnotation(&shoot.ObjectMeta, processedByKey, "admission-webhook-local-provider")
 	}
 
 	return nil
