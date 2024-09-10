668ea72a9e276aa21788eaa44891c2843bd22741
Dimitar Mirchev <dimitar.mirchev@sap.com>
2024-09-10 11:22:43 +0300

    Add Security Object resource

---
+++ pkg/apis/security/types.go
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package security
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Object is a security object resource.
+type Object interface {
+	metav1.Object
+	// GetProviderType gets the type of the provider.
+	GetProviderType() string
+}
