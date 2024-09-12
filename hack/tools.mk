21599ec1db579f350aa75ba594153d74357b150f
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-09-12 04:50:45 +0200

    chore(deps): update dependency kubernetes/kubernetes to v1.31.1 (#10513)

--- hack/tools.mk
+++ hack/tools.mk
@@ -66,7 +66,7 @@ HELM_VERSION ?= v3.15.4
 # renovate: datasource=github-releases depName=kubernetes-sigs/kind
 KIND_VERSION ?= v0.24.0
 # renovate: datasource=github-releases depName=kubernetes/kubernetes
-KUBECTL_VERSION ?= v1.31.0
+KUBECTL_VERSION ?= v1.31.1
 # renovate: datasource=github-releases depName=kubernetes-sigs/kustomize
 KUSTOMIZE_VERSION ?= v5.3.0
 # renovate: datasource=github-releases depName=prometheus/prometheus
