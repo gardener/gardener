156012e8efb3f7a438ae019224708606010cf71e
Alexander Hebel <a.hebelsun@gmail.com>
2024-09-17 15:19:26 +0200

    Adress comments

--- extensions/pkg/bastion/vmdetails.go
+++ extensions/pkg/bastion/vmdetails.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package bastion
 
 import (
@@ -20,27 +24,27 @@ type VMDetails struct {
 }
 
 // DetermineVmDetails determines the bastion vm details based on information in the cloud profile
-func DetermineVmDetails(spec core.CloudProfileSpec) (vm VmDetails, err error) {
+func DetermineVmDetails(spec gardencorev1beta1.CloudProfileSpec) (vm VMDetails, err error) {
 	imageArchs, err := getArchitectures(spec.Bastion, spec.MachineImages)
 	if err != nil {
-		return VmDetails{}, err
+		return VMDetails{}, err
 	}
 	vm.MachineName, vm.Architecture, err = getMachine(spec.Bastion, spec.MachineTypes, imageArchs)
 	if err != nil {
-		return VmDetails{}, err
+		return VMDetails{}, err
 	}
 	vm.ImageBaseName, err = getImageName(spec.Bastion, spec.MachineImages, vm.Architecture)
 	if err != nil {
-		return VmDetails{}, err
+		return VMDetails{}, err
 	}
 	vm.ImageVersion, err = getImageVersion(vm.ImageBaseName, vm.Architecture, spec.Bastion, spec.MachineImages)
 	return vm, err
 }
 
 // getMachine retrieves the bastion machine name and arch
-func getMachine(bastion *core.Bastion, machineTypes []core.MachineType, supportedArchs []string) (machineName string, machineArch string, err error) {
+func getMachine(bastion *gardencorev1beta1.Bastion, machineTypes []gardencorev1beta1.MachineType, supportedArchs []string) (machineName string, machineArch string, err error) {
 	if bastion != nil && bastion.MachineType != nil {
-		machineIndex := slices.IndexFunc(machineTypes, func(machine core.MachineType) bool {
+		machineIndex := slices.IndexFunc(machineTypes, func(machine gardencorev1beta1.MachineType) bool {
 			return machine.Name == bastion.MachineType.Name
 		})
 
@@ -79,10 +83,10 @@ func getMachine(bastion *core.Bastion, machineTypes []core.MachineType, supporte
 
 // getArchitectures finds the supported architectures of the cloudProfiles images
 // returning an empty array means all architectures are allowed
-func getArchitectures(bastion *core.Bastion, images []core.MachineImage) ([]string, error) {
+func getArchitectures(bastion *gardencorev1beta1.Bastion, images []gardencorev1beta1.MachineImage) ([]string, error) {
 	archs := make(map[string]bool)
 
-	findSupportedArchs := func(versions []core.MachineImageVersion, bastionVersion *string) {
+	findSupportedArchs := func(versions []gardencorev1beta1.MachineImageVersion, bastionVersion *string) {
 		for _, version := range versions {
 			if bastionVersion != nil && version.Version == *bastionVersion {
 				archs = make(map[string]bool)
@@ -92,7 +96,7 @@ func getArchitectures(bastion *core.Bastion, images []core.MachineImage) ([]stri
 				return
 			}
 
-			if version.Classification != nil && *version.Classification == core.ClassificationSupported {
+			if version.Classification != nil && *version.Classification == gardencorev1beta1.ClassificationSupported {
 				for _, arch := range version.Architectures {
 					archs[arch] = true
 				}
@@ -124,7 +128,7 @@ func getArchitectures(bastion *core.Bastion, images []core.MachineImage) ([]stri
 }
 
 // getImageName returns the image name for the bastion.
-func getImageName(bastion *core.Bastion, images []core.MachineImage, arch string) (string, error) {
+func getImageName(bastion *gardencorev1beta1.Bastion, images []gardencorev1beta1.MachineImage, arch string) (string, error) {
 	// check if image name exists is also done in gardener cloudProfile validation
 	if bastion != nil && bastion.MachineImage != nil {
 		image, err := findImageByName(images, bastion.MachineImage.Name)
@@ -137,7 +141,7 @@ func getImageName(bastion *core.Bastion, images []core.MachineImage, arch string
 	// take the first image from cloud profile that is supported and arch compatible
 	for _, image := range images {
 		for _, version := range image.Versions {
-			if version.Classification == nil || *version.Classification != core.ClassificationSupported {
+			if version.Classification == nil || *version.Classification != gardencorev1beta1.ClassificationSupported {
 				continue
 			}
 			if !slices.Contains(version.Architectures, arch) {
@@ -150,7 +154,7 @@ func getImageName(bastion *core.Bastion, images []core.MachineImage, arch string
 }
 
 // getImageVersion returns the image version for the bastion.
-func getImageVersion(imageName, machineArch string, bastion *core.Bastion, images []core.MachineImage) (string, error) {
+func getImageVersion(imageName, machineArch string, bastion *gardencorev1beta1.Bastion, images []gardencorev1beta1.MachineImage) (string, error) {
 	image, err := findImageByName(images, imageName)
 	if err != nil {
 		return "", err
@@ -158,7 +162,7 @@ func getImageVersion(imageName, machineArch string, bastion *core.Bastion, image
 
 	// check if image version exists is also done in gardener cloudProfile validation
 	if bastion != nil && bastion.MachineImage != nil && bastion.MachineImage.Version != nil {
-		versionIndex := slices.IndexFunc(image.Versions, func(version core.MachineImageVersion) bool {
+		versionIndex := slices.IndexFunc(image.Versions, func(version gardencorev1beta1.MachineImageVersion) bool {
 			return version.Version == *bastion.MachineImage.Version
 		})
 
@@ -166,7 +170,7 @@ func getImageVersion(imageName, machineArch string, bastion *core.Bastion, image
 			return "", fmt.Errorf("image version %s not found not found in cloudProfile", *bastion.MachineImage.Version)
 		}
 
-		if image.Versions[versionIndex].Classification != nil && *image.Versions[versionIndex].Classification != core.ClassificationSupported {
+		if image.Versions[versionIndex].Classification != nil && *image.Versions[versionIndex].Classification != gardencorev1beta1.ClassificationSupported {
 			return "", fmt.Errorf("specified image %s in version %s is not classified supported", imageName, *bastion.MachineImage.Version)
 		}
 
@@ -175,7 +179,7 @@ func getImageVersion(imageName, machineArch string, bastion *core.Bastion, image
 
 	var greatest *semver.Version
 	for _, version := range image.Versions {
-		if version.Classification == nil || *version.Classification != core.ClassificationSupported {
+		if version.Classification == nil || *version.Classification != gardencorev1beta1.ClassificationSupported {
 			continue
 		}
 
@@ -200,13 +204,13 @@ func getImageVersion(imageName, machineArch string, bastion *core.Bastion, image
 }
 
 // findImageByName returns image object found by name in the cloudProfile
-func findImageByName(images []core.MachineImage, name string) (core.MachineImage, error) {
-	imageIndex := slices.IndexFunc(images, func(image core.MachineImage) bool {
+func findImageByName(images []gardencorev1beta1.MachineImage, name string) (gardencorev1beta1.MachineImage, error) {
+	imageIndex := slices.IndexFunc(images, func(image gardencorev1beta1.MachineImage) bool {
 		return image.Name == name
 	})
 
 	if imageIndex == -1 {
-		return core.MachineImage{}, fmt.Errorf("bastion image %s not found in cloudProfile", name)
+		return gardencorev1beta1.MachineImage{}, fmt.Errorf("bastion image %s not found in cloudProfile", name)
 	}
 
 	return images[imageIndex], nil
--- extensions/pkg/bastion/vmdetails_test.go
+++ extensions/pkg/bastion/vmdetails_test.go
@@ -1,50 +1,54 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package bastion_test
 
 import (
 	"slices"
 
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/utils/ptr"
 
-	"github.com/gardener/gardener/extensions/pkg/bastion"
-	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	. "github.com/gardener/gardener/extensions/pkg/bastion"
 	. "github.com/gardener/gardener/pkg/utils/test/matchers"
 )
 
 var _ = Describe("Bastion VM Details", func() {
-	var desired bastion.VmDetails
-	var spec core.CloudProfileSpec
+	var desired VMDetails
+	var spec gardencorev1beta1.CloudProfileSpec
 
 	BeforeEach(func() {
-		desired = bastion.VmDetails{
+		desired = VMDetails{
 			MachineName:   "small_machine",
 			Architecture:  "amd64",
 			ImageBaseName: "gardenlinux",
 			ImageVersion:  "1.2.3",
 		}
-		spec = core.CloudProfileSpec{
-			Bastion: &core.Bastion{
-				MachineImage: &core.BastionMachineImage{
+		spec = gardencorev1beta1.CloudProfileSpec{
+			Bastion: &gardencorev1beta1.Bastion{
+				MachineImage: &gardencorev1beta1.BastionMachineImage{
 					Name: desired.ImageBaseName,
 				},
-				MachineType: &core.BastionMachineType{
+				MachineType: &gardencorev1beta1.BastionMachineType{
 					Name: desired.MachineName,
 				},
 			},
-			MachineTypes: []core.MachineType{{
+			MachineTypes: []gardencorev1beta1.MachineType{{
 				CPU:          resource.MustParse("4"),
 				Name:         desired.MachineName,
 				Architecture: ptr.To(desired.Architecture),
 			}},
-			MachineImages: []core.MachineImage{{
+			MachineImages: []gardencorev1beta1.MachineImage{{
 				Name: desired.ImageBaseName,
-				Versions: []core.MachineImageVersion{
+				Versions: []gardencorev1beta1.MachineImageVersion{
 					{
-						ExpirableVersion: core.ExpirableVersion{
+						ExpirableVersion: gardencorev1beta1.ExpirableVersion{
 							Version:        desired.ImageVersion,
-							Classification: ptr.To(core.ClassificationSupported),
+							Classification: ptr.To(gardencorev1beta1.ClassificationSupported),
 						},
 						Architectures: []string{desired.Architecture, "arm64"},
 					}},
@@ -52,13 +56,13 @@ var _ = Describe("Bastion VM Details", func() {
 		}
 	})
 
-	addImageToCloudProfile := func(imageName, version string, classification core.VersionClassification, archs []string) {
-		machineIndex := slices.IndexFunc(spec.MachineImages, func(image core.MachineImage) bool {
+	addImageToCloudProfile := func(imageName, version string, classification gardencorev1beta1.VersionClassification, archs []string) {
+		machineIndex := slices.IndexFunc(spec.MachineImages, func(image gardencorev1beta1.MachineImage) bool {
 			return image.Name == imageName
 		})
 
-		newVersion := core.MachineImageVersion{
-			ExpirableVersion: core.ExpirableVersion{
+		newVersion := gardencorev1beta1.MachineImageVersion{
+			ExpirableVersion: gardencorev1beta1.ExpirableVersion{
 				Version:        version,
 				Classification: ptr.To(classification),
 			},
@@ -67,9 +71,9 @@ var _ = Describe("Bastion VM Details", func() {
 
 		// append new machine image
 		if machineIndex == -1 {
-			spec.MachineImages = append(spec.MachineImages, core.MachineImage{
+			spec.MachineImages = append(spec.MachineImages, gardencorev1beta1.MachineImage{
 				Name:     imageName,
-				Versions: []core.MachineImageVersion{newVersion},
+				Versions: []gardencorev1beta1.MachineImageVersion{newVersion},
 			})
 		}
 
@@ -79,110 +83,110 @@ var _ = Describe("Bastion VM Details", func() {
 
 	Describe("#DetermineVmDetails", func() {
 		It("should succeed without setting bastion image version", func() {
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should succeed with empty bastion section", func() {
-			spec.Bastion = &core.Bastion{}
-			details, err := bastion.DetermineVmDetails(spec)
+			spec.Bastion = &gardencorev1beta1.Bastion{}
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should succeed without setting bastion section", func() {
 			spec.Bastion = nil
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should succeed without setting bastion image", func() {
 			spec.Bastion.MachineImage = nil
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should succeed without setting machineType", func() {
 			spec.Bastion.MachineType = nil
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("forbid unknown image name", func() {
 			spec.Bastion.MachineImage.Name = "unknown_image"
-			_, err := bastion.DetermineVmDetails(spec)
+			_, err := DetermineVmDetails(spec)
 			Expect(err).To(HaveOccurred())
 		})
 
 		It("forbid unknown image version", func() {
 			spec.Bastion.MachineImage.Version = ptr.To("6.6.6")
-			_, err := bastion.DetermineVmDetails(spec)
+			_, err := DetermineVmDetails(spec)
 			Expect(err).To(HaveOccurred())
 		})
 
 		It("forbid unknown machineType", func() {
 			spec.Bastion.MachineType.Name = "unknown_machine"
-			_, err := bastion.DetermineVmDetails(spec)
+			_, err := DetermineVmDetails(spec)
 			Expect(err).To(HaveOccurred())
 		})
 
 		It("should find greatest supported version", func() {
-			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", core.ClassificationSupported, []string{"amd64"})
+			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", gardencorev1beta1.ClassificationSupported, []string{"amd64"})
 			desired.ImageVersion = "1.2.4"
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should find smallest machine", func() {
 			spec.Bastion.MachineType = nil
-			spec.MachineTypes = append(spec.MachineTypes, core.MachineType{
+			spec.MachineTypes = append(spec.MachineTypes, gardencorev1beta1.MachineType{
 				CPU:          resource.MustParse("1"),
 				GPU:          resource.MustParse("1"),
 				Name:         "smallerMachine",
 				Architecture: ptr.To(desired.Architecture),
 			})
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details.MachineName).To(DeepEqual("smallerMachine"))
 		})
 
 		It("should only use supported version", func() {
-			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", core.ClassificationPreview, []string{"amd64"})
-			details, err := bastion.DetermineVmDetails(spec)
+			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", gardencorev1beta1.ClassificationPreview, []string{"amd64"})
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should use version which has been specified", func() {
-			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", core.ClassificationSupported, []string{"amd64"})
+			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", gardencorev1beta1.ClassificationSupported, []string{"amd64"})
 			spec.Bastion.MachineImage.Version = ptr.To("1.2.3")
-			details, err := bastion.DetermineVmDetails(spec)
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("should not allow preview image even if version is specified", func() {
-			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", core.ClassificationPreview, []string{"amd64"})
+			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", gardencorev1beta1.ClassificationPreview, []string{"amd64"})
 			spec.Bastion.MachineImage.Version = ptr.To("1.2.4")
-			_, err := bastion.DetermineVmDetails(spec)
+			_, err := DetermineVmDetails(spec)
 			Expect(err).To(HaveOccurred())
 		})
 
 		It("only use images for matching machineType architecture", func() {
-			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", core.ClassificationSupported, []string{"x86"})
-			details, err := bastion.DetermineVmDetails(spec)
+			addImageToCloudProfile(desired.ImageBaseName, "1.2.4", gardencorev1beta1.ClassificationSupported, []string{"x86"})
+			details, err := DetermineVmDetails(spec)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(details).To(DeepEqual(desired))
 		})
 
 		It("fail if no image with matching machineType architecture can be found", func() {
 			spec.MachineImages[0].Versions[0].Architectures = []string{"x86"}
-			_, err := bastion.DetermineVmDetails(spec)
+			_, err := DetermineVmDetails(spec)
 			Expect(err).To(HaveOccurred())
 		})
 	})
