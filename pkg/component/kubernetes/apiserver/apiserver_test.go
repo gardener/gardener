1fd5f0bcf24668889d25fefcbe83ede32dcfc969
Shafeeque E S <shafeeque.e.s@sap.com>
2024-10-07 11:24:25 +0530

    Adapt usages of dropped versions in tests

--- pkg/component/kubernetes/apiserver/apiserver_test.go
+++ pkg/component/kubernetes/apiserver/apiserver_test.go
@@ -4005,7 +4005,7 @@ kind: AuthenticationConfiguration
 							RuntimeVersion: runtimeVersion,
 						},
 						Images:  images,
-						Version: semver.MustParse("1.26.9"),
+						Version: semver.MustParse("1.31.1"),
 					})
 					deployAndRead()
 
