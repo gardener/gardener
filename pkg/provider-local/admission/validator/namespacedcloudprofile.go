f2a733fc6f78e2032d93b62209babd853b298a9c
Luca Bernstein <luca.bernstein@sap.com>
2024-10-21 17:06:33 +0200

    Refactor `ImagesContext`.

--- pkg/provider-local/admission/validator/namespacedcloudprofile.go
+++ pkg/provider-local/admission/validator/namespacedcloudprofile.go
@@ -14,6 +14,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
+	"github.com/gardener/gardener/extensions/pkg/util"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	"github.com/gardener/gardener/pkg/apis/core"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
@@ -65,17 +66,18 @@ func (p *namespacedCloudProfile) Validate(ctx context.Context, new, _ client.Obj
 	return p.ValidateNamespacedCloudProfileProviderConfig(cloudProfileConfig, profile.Spec.MachineImages, parentProfile).ToAggregate()
 }
 
+// ValidateNamespacedCloudProfileProviderConfig validates the providerConfig of the NamespacedCloudProfile.
 func (p *namespacedCloudProfile) ValidateNamespacedCloudProfileProviderConfig(providerConfig *api.CloudProfileConfig, machineImages []core.MachineImage, parentProfile *gardencorev1beta1.CloudProfile) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	profileImages := newProfileImagesContext(machineImages)
-	parentImages := newParentImagesContext(parentProfile.Spec.MachineImages)
+	profileImages := util.NewCoreImagesContext(machineImages)
+	parentImages := util.NewV1beta1ImagesContext(parentProfile.Spec.MachineImages)
 	providerImages := newProviderImagesContext(providerConfig.MachineImages)
 
-	for _, machineImage := range profileImages.images {
+	for _, machineImage := range profileImages.Images {
 		// Check that for each new image version defined in the NamespacedCloudProfile, the image is also defined in the providerConfig.
-		_, existsInParent := parentImages.getImage(machineImage.Name)
-		if _, existsInProvider := providerImages.getImage(machineImage.Name); !existsInParent && !existsInProvider {
+		_, existsInParent := parentImages.GetImage(machineImage.Name)
+		if _, existsInProvider := providerImages.GetImage(machineImage.Name); !existsInParent && !existsInProvider {
 			allErrs = append(allErrs, field.Required(
 				field.NewPath("spec.providerConfig.machineImages"),
 				fmt.Sprintf("machine image %s is not defined in the NamespacedCloudProfile providerConfig", machineImage.Name),
@@ -83,8 +85,8 @@ func (p *namespacedCloudProfile) ValidateNamespacedCloudProfileProviderConfig(pr
 			continue
 		}
 		for _, version := range machineImage.Versions {
-			_, existsInParent := parentImages.getImageVersion(machineImage.Name, version.Version)
-			if _, exists := providerImages.getImageVersion(machineImage.Name, version.Version); !existsInParent && !exists {
+			_, existsInParent := parentImages.GetImageVersion(machineImage.Name, version.Version)
+			if _, exists := providerImages.GetImageVersion(machineImage.Name, version.Version); !existsInParent && !exists {
 				allErrs = append(allErrs, field.Required(
 					field.NewPath("spec.providerConfig.machineImages"),
 					fmt.Sprintf("machine image version %s@%s is not defined in the NamespacedCloudProfile providerConfig", machineImage.Name, version.Version),
@@ -94,9 +96,9 @@ func (p *namespacedCloudProfile) ValidateNamespacedCloudProfileProviderConfig(pr
 	}
 	for imageIdx, machineImage := range providerConfig.MachineImages {
 		// Check that the machine image version is not already defined in the parent CloudProfile.
-		if _, exists := parentImages.getImage(machineImage.Name); exists {
+		if _, exists := parentImages.GetImage(machineImage.Name); exists {
 			for versionIdx, version := range machineImage.Versions {
-				if _, exists := parentImages.getImageVersion(machineImage.Name, version.Version); exists {
+				if _, exists := parentImages.GetImageVersion(machineImage.Name, version.Version); exists {
 					allErrs = append(allErrs, field.Forbidden(
 						field.NewPath("spec.providerConfig.machineImages").Index(imageIdx).Child("versions").Index(versionIdx),
 						fmt.Sprintf("machine image version %s@%s is already defined in the parent CloudProfile", machineImage.Name, version.Version),
@@ -106,7 +108,7 @@ func (p *namespacedCloudProfile) ValidateNamespacedCloudProfileProviderConfig(pr
 		}
 
 		// Check that the machine image version is defined in the NamespacedCloudProfile.
-		if _, exists := profileImages.getImage(machineImage.Name); !exists {
+		if _, exists := profileImages.GetImage(machineImage.Name); !exists {
 			allErrs = append(allErrs, field.Required(
 				field.NewPath("spec.providerConfig.machineImages").Index(imageIdx),
 				fmt.Sprintf("machine image %s is not defined in the NamespacedCloudProfile .spec.machineImages", machineImage.Name),
@@ -114,7 +116,7 @@ func (p *namespacedCloudProfile) ValidateNamespacedCloudProfileProviderConfig(pr
 			continue
 		}
 		for versionIdx, version := range machineImage.Versions {
-			if _, exists := profileImages.getImageVersion(machineImage.Name, version.Version); !exists {
+			if _, exists := profileImages.GetImageVersion(machineImage.Name, version.Version); !exists {
 				allErrs = append(allErrs, field.Invalid(
 					field.NewPath("spec.providerConfig.machineImages").Index(imageIdx).Child("versions").Index(versionIdx),
 					fmt.Sprintf("%s@%s", machineImage.Name, version.Version),
@@ -127,57 +129,11 @@ func (p *namespacedCloudProfile) ValidateNamespacedCloudProfileProviderConfig(pr
 	return allErrs
 }
 
-type imagesContext[A any, B any] struct {
-	images            map[string]A
-	createVersionsMap func(A) map[string]B
-	// imageVersions will be calculated lazy on first access of each key.
-	imageVersions map[string]map[string]B
-}
-
-func (vc *imagesContext[A, B]) getImage(imageName string) (A, bool) {
-	o, exists := vc.images[imageName]
-	return o, exists
-}
-
-func (vc *imagesContext[A, B]) getImageVersion(imageName string, version string) (B, bool) {
-	o, exists := vc.getImageVersions(imageName)[version]
-	return o, exists
-}
-
-func (vc *imagesContext[A, B]) getImageVersions(imageName string) map[string]B {
-	if versions, exists := vc.imageVersions[imageName]; exists {
-		return versions
-	}
-	vc.imageVersions[imageName] = vc.createVersionsMap(vc.images[imageName])
-	return vc.imageVersions[imageName]
-}
-
-func newProfileImagesContext(profileImages []core.MachineImage) *imagesContext[core.MachineImage, core.MachineImageVersion] {
-	return &imagesContext[core.MachineImage, core.MachineImageVersion]{
-		images:        utils.CreateMapFromSlice(profileImages, func(mi core.MachineImage) string { return mi.Name }),
-		imageVersions: make(map[string]map[string]core.MachineImageVersion),
-		createVersionsMap: func(mi core.MachineImage) map[string]core.MachineImageVersion {
-			return utils.CreateMapFromSlice(mi.Versions, func(v core.MachineImageVersion) string { return v.Version })
-		},
-	}
-}
-
-func newParentImagesContext(parentImages []gardencorev1beta1.MachineImage) *imagesContext[gardencorev1beta1.MachineImage, gardencorev1beta1.MachineImageVersion] {
-	return &imagesContext[gardencorev1beta1.MachineImage, gardencorev1beta1.MachineImageVersion]{
-		images:        utils.CreateMapFromSlice(parentImages, func(mi gardencorev1beta1.MachineImage) string { return mi.Name }),
-		imageVersions: make(map[string]map[string]gardencorev1beta1.MachineImageVersion),
-		createVersionsMap: func(mi gardencorev1beta1.MachineImage) map[string]gardencorev1beta1.MachineImageVersion {
-			return utils.CreateMapFromSlice(mi.Versions, func(v gardencorev1beta1.MachineImageVersion) string { return v.Version })
-		},
-	}
-}
-
-func newProviderImagesContext(providerImages []api.MachineImages) *imagesContext[api.MachineImages, api.MachineImageVersion] {
-	return &imagesContext[api.MachineImages, api.MachineImageVersion]{
-		images:        utils.CreateMapFromSlice(providerImages, func(mi api.MachineImages) string { return mi.Name }),
-		imageVersions: make(map[string]map[string]api.MachineImageVersion),
-		createVersionsMap: func(mi api.MachineImages) map[string]api.MachineImageVersion {
+func newProviderImagesContext(providerImages []api.MachineImages) *util.ImagesContext[api.MachineImages, api.MachineImageVersion] {
+	return util.NewImagesContext(
+		utils.CreateMapFromSlice(providerImages, func(mi api.MachineImages) string { return mi.Name }),
+		func(mi api.MachineImages) map[string]api.MachineImageVersion {
 			return utils.CreateMapFromSlice(mi.Versions, func(v api.MachineImageVersion) string { return v.Version })
 		},
-	}
+	)
 }
