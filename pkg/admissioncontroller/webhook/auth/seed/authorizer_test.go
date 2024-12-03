bc4981a175b450560db8c562aeaf5b3b3a94b319
rfranzke <rafael.franzke@sap.com>
2024-12-03 22:02:57 +0100

    No longer unconditionally allow `update`, `patch` verbs for `Seed`s
    The `gardenlet` only updates its own `Seed`, so there is no need to have
    these verbs as part of the "always allowed" list.

--- pkg/admissioncontroller/webhook/auth/seed/authorizer_test.go
+++ pkg/admissioncontroller/webhook/auth/seed/authorizer_test.go
@@ -1773,9 +1773,31 @@ var _ = Describe("Seed", func() {
 					Entry("list", "list"),
 					Entry("watch", "watch"),
 					Entry("create", "create"),
-					Entry("update", "update"),
-					Entry("patch", "patch"),
 					Entry("delete", "delete"),
+				)
+
+				DescribeTable("should return correct result if path exists",
+					func(verb, subresource string) {
+						attrs.Verb = verb
+						attrs.Subresource = subresource
+
+						graph.EXPECT().HasPathFrom(graphpkg.VertexTypeSeed, "", name, graphpkg.VertexTypeSeed, "", seedName).Return(true)
+						decision, reason, err := authorizer.Authorize(ctx, attrs)
+						Expect(err).NotTo(HaveOccurred())
+						Expect(decision).To(Equal(auth.DecisionAllow))
+						Expect(reason).To(BeEmpty())
+
+						graph.EXPECT().HasPathFrom(graphpkg.VertexTypeSeed, "", name, graphpkg.VertexTypeSeed, "", seedName).Return(false)
+						decision, reason, err = authorizer.Authorize(ctx, attrs)
+						Expect(err).NotTo(HaveOccurred())
+						Expect(decision).To(Equal(auth.DecisionNoOpinion))
+						Expect(reason).To(ContainSubstring("no relationship found"))
+					},
+
+					Entry("patch w/o subresource", "patch", ""),
+					Entry("patch w/ subresource", "patch", "status"),
+					Entry("update w/o subresource", "update", ""),
+					Entry("update w/ subresource", "update", "status"),
 				)
 
 				It("should have no opinion because no allowed verb", func() {
@@ -1784,7 +1806,7 @@ var _ = Describe("Seed", func() {
 					decision, reason, err := authorizer.Authorize(ctx, attrs)
 					Expect(err).NotTo(HaveOccurred())
 					Expect(decision).To(Equal(auth.DecisionNoOpinion))
-					Expect(reason).To(ContainSubstring("only the following verbs are allowed for this resource type: [create update patch delete get list watch]"))
+					Expect(reason).To(ContainSubstring("only the following verbs are allowed for this resource type: [create delete get list watch update patch]"))
 				})
 
 				It("should have no opinion because no allowed subresource", func() {
