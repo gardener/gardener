9caf7eef5720cfbc03c769d16b97617573649528
oliver-goetz <o.goetz@sap.com>
2024-12-03 18:06:33 +0100

    Create `IstioTLSTermination` feature gate
    Co-authored-by: Michael Eischer <michael.eischer@inovex.de>

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -85,6 +85,12 @@ const (
 	// owner: @Wieneo @timebertt
 	// alpha: v1.113.0
 	RemoveAPIServerProxyLegacyPort featuregate.Feature = "RemoveAPIServerProxyLegacyPort"
+
+	// IstioTLSTermination enables TLS termination for the Istio Ingress Gateway instead of TLS termination at the kube-apiserver.
+	// It allows load-balancing of requests to the kube-apiserver on request level instead of connection level.
+	// owner: @oliver-goetz
+	// alpha: v1.114.0
+	IstioTLSTermination featuregate.Feature = "IstioTLSTermination"
 )
 
 // DefaultFeatureGate is the central feature gate map used by all gardener components.
@@ -122,6 +128,7 @@ var AllFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	CredentialsRotationWithoutWorkersRollout: {Default: false, PreRelease: featuregate.Alpha},
 	InPlaceNodeUpdates:                       {Default: false, PreRelease: featuregate.Alpha},
 	RemoveAPIServerProxyLegacyPort:           {Default: false, PreRelease: featuregate.Alpha},
+	IstioTLSTermination:                      {Default: false, PreRelease: featuregate.Alpha},
 }
 
 // GetFeatures returns a feature gate map with the respective specifications. Non-existing feature gates are ignored.
