4ec852f4db33dfe845c32997fabb8cefccde4dea
rfranzke <rafael.franzke@sap.com>
2024-12-04 15:30:34 +0100

    Scale up machine pods and add dummy test for `gardenadm join`
    Co-authored-by: Tim Ebert <timebertt@gmail.com>

--- test/e2e/gardenadm/common.go
+++ test/e2e/gardenadm/common.go
@@ -5,16 +5,12 @@
 package gardenadm
 
 import (
-	"context"
 	"os"
-	"time"
+	"strconv"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	componentbaseconfig "k8s.io/component-base/config"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -23,20 +19,14 @@ import (
 )
 
 const (
-	namespace     = "gardenadm"
-	containerName = "node"
+	statefulSetName = "machine"
+	namespace       = "gardenadm"
+	containerName   = "node"
 )
 
-var (
-	runtimeClient kubernetes.Interface
-
-	machinePod *corev1.Pod
-)
+var runtimeClient kubernetes.Interface
 
 var _ = BeforeSuite(func() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
 	logf.SetLogger(logger.MustNewZapLogger(logger.InfoLevel, logger.FormatJSON, zap.WriteTo(GinkgoWriter)))
 
 	restConfig, err := kubernetes.RESTConfigFromClientConnectionConfiguration(&componentbaseconfig.ClientConnectionConfiguration{Kubeconfig: os.Getenv("KUBECONFIG")}, nil, kubernetes.AuthTokenFile)
@@ -44,7 +34,8 @@ var _ = BeforeSuite(func() {
 
 	runtimeClient, err = kubernetes.NewWithConfig(kubernetes.WithRESTConfig(restConfig), kubernetes.WithDisabledCachedClient())
 	Expect(err).NotTo(HaveOccurred())
-
-	machinePod = &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "machine-0", Namespace: namespace}}
-	Expect(runtimeClient.Client().Get(ctx, client.ObjectKeyFromObject(machinePod), machinePod)).To(Succeed())
 })
+
+func machinePodName(ordinal int) string {
+	return statefulSetName + "-" + strconv.Itoa(ordinal)
+}
