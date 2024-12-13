eab2c3c8689e8c603ed6827e6dad0b8a3433b8e4
oliver-goetz <o.goetz@sap.com>
2024-12-13 13:55:26 +0100

    Sync kube-apiserver TLS and CA secrets into istio ingress-gateway namespace
    Co-authored-by: Michael Eischer <michael.eischer@inovex.de>

--- pkg/component/kubernetes/apiserverexposure/apiserverexposure_suite_test.go
+++ pkg/component/kubernetes/apiserverexposure/apiserverexposure_suite_test.go
@@ -9,9 +9,12 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"github.com/gardener/gardener/pkg/gardenlet/features"
 )
 
 func TestAPIServerExposure(t *testing.T) {
+	features.RegisterFeatureGates()
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Component Kubernetes APIServerExposure Suite")
 }
