bb90b02ebd687964daebdfa9c688ee0c958e6343
oliver-goetz <o.goetz@sap.com>
2024-12-13 14:04:07 +0100

    Include istio TLS termination into garden controller
    Co-authored-by: Michael Eischer <michael.eischer@inovex.de>

--- pkg/operator/controller/garden/garden/components.go
+++ pkg/operator/controller/garden/garden/components.go
@@ -499,6 +499,16 @@ func (r *Reconciler) newKubeAPIServerService(log logr.Logger, garden *operatorv1
 		return nil, fmt.Errorf("exactly one Istio Ingress Gateway is required for the SNI config")
 	}
 
+	deployer := []component.Deployer{r.newKubeAPIServerServiceWithSuffix(log, garden, ingressGatewayValues, "")}
+
+	if features.DefaultFeatureGate.Enabled(features.IstioTLSTermination) {
+		deployer = append(deployer, r.newKubeAPIServerServiceWithSuffix(log, garden, ingressGatewayValues, kubeapiserverexposure.MutualTLSServiceNameSuffix))
+	}
+
+	return component.OpWait(deployer...), nil
+}
+
+func (r *Reconciler) newKubeAPIServerServiceWithSuffix(log logr.Logger, garden *operatorv1alpha1.Garden, ingressGatewayValues []istio.IngressGatewayValues, suffix string) component.DeployWaiter {
 	var annotations map[string]string
 	if settings := garden.Spec.RuntimeCluster.Settings; settings != nil && settings.LoadBalancerServices != nil {
 		annotations = settings.LoadBalancerServices.Annotations
@@ -511,6 +521,7 @@ func (r *Reconciler) newKubeAPIServerService(log logr.Logger, garden *operatorv1
 		&kubeapiserverexposure.ServiceValues{
 			AnnotationsFunc:             func() map[string]string { return annotations },
 			NamePrefix:                  namePrefix,
+			NameSuffix:                  suffix,
 			TopologyAwareRoutingEnabled: helper.TopologyAwareRoutingEnabled(garden.Spec.RuntimeCluster.Settings),
 			RuntimeKubernetesVersion:    r.RuntimeVersion,
 		},
@@ -520,7 +531,7 @@ func (r *Reconciler) newKubeAPIServerService(log logr.Logger, garden *operatorv1
 		nil,
 		nil,
 		nil,
-	), nil
+	)
 }
 
 func (r *Reconciler) newKubeAPIServer(
