249777661500cee79ead555590f02e67658ff107
Marc Vornetran <marc.vornetran@sap.com>
2024-12-16 13:01:12 +0100

    feat: Add version constraints for Kubernetes v1.32

--- pkg/utils/version/version_test.go
+++ pkg/utils/version/version_test.go
@@ -67,6 +67,16 @@ var _ = Describe("Version", func() {
 		Entry("ConstraintK8sGreaterEqual131, failure", ConstraintK8sGreaterEqual131, semver.MustParse("1.30.0"), BeFalse()),
 		Entry("ConstraintK8sGreaterEqual131, success w/ suffix", ConstraintK8sGreaterEqual131, semver.MustParse("v1.31.0-foo.12"), BeTrue()),
 		Entry("ConstraintK8sGreaterEqual131, failure w/ suffix", ConstraintK8sGreaterEqual131, semver.MustParse("v1.30.0-foo.12"), BeFalse()),
+
+		Entry("ConstraintK8sLess132, success", ConstraintK8sLess132, semver.MustParse("1.31.1"), BeTrue()),
+		Entry("ConstraintK8sLess132, failure", ConstraintK8sLess132, semver.MustParse("1.32.0"), BeFalse()),
+		Entry("ConstraintK8sLess132, success w/ suffix", ConstraintK8sLess132, semver.MustParse("v1.31.1-foo.12"), BeTrue()),
+		Entry("ConstraintK8sLess132, failure w/ suffix", ConstraintK8sLess132, semver.MustParse("v1.32.0-foo.12"), BeFalse()),
+
+		Entry("ConstraintK8sGreaterEqual132, success", ConstraintK8sGreaterEqual132, semver.MustParse("1.32.0"), BeTrue()),
+		Entry("ConstraintK8sGreaterEqual132, failure", ConstraintK8sGreaterEqual132, semver.MustParse("1.31.0"), BeFalse()),
+		Entry("ConstraintK8sGreaterEqual132, success w/ suffix", ConstraintK8sGreaterEqual132, semver.MustParse("v1.32.0-foo.12"), BeTrue()),
+		Entry("ConstraintK8sGreaterEqual132, failure w/ suffix", ConstraintK8sGreaterEqual132, semver.MustParse("v1.31.0-foo.12"), BeFalse()),
 	)
 
 	DescribeTable("#CompareVersions",
