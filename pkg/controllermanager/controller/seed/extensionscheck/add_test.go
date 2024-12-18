a4809a7b22b64337bdbfbea3443d73749c3299f3
rfranzke <rafael.franzke@sap.com>
2024-12-18 14:29:57 +0100

    controller-manager

--- pkg/controllermanager/controller/seed/extensionscheck/add_test.go
+++ pkg/controllermanager/controller/seed/extensionscheck/add_test.go
@@ -7,17 +7,13 @@ package extensionscheck_test
 import (
 	"context"
 
-	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
-	"github.com/gardener/gardener/pkg/client/kubernetes"
 	. "github.com/gardener/gardener/pkg/controllermanager/controller/seed/extensionscheck"
 )
 
@@ -39,23 +35,14 @@ var _ = Describe("Add", func() {
 	})
 
 	Describe("#MapControllerInstallationToSeed", func() {
-		var (
-			ctx        = context.TODO()
-			log        logr.Logger
-			fakeClient client.Client
-		)
-
-		BeforeEach(func() {
-			log = logr.Discard()
-			fakeClient = fakeclient.NewClientBuilder().WithScheme(kubernetes.GardenScheme).Build()
-		})
+		ctx := context.TODO()
 
 		It("should do nothing if the object is no ControllerInstallation", func() {
-			Expect(reconciler.MapControllerInstallationToSeed(ctx, log, fakeClient, &corev1.Secret{})).To(BeEmpty())
+			Expect(reconciler.MapControllerInstallationToSeed(ctx, &corev1.Secret{})).To(BeEmpty())
 		})
 
 		It("should map the ControllerInstallation to the Seed", func() {
-			Expect(reconciler.MapControllerInstallationToSeed(ctx, log, fakeClient, controllerInstallation)).To(ConsistOf(
+			Expect(reconciler.MapControllerInstallationToSeed(ctx, controllerInstallation)).To(ConsistOf(
 				reconcile.Request{NamespacedName: types.NamespacedName{Name: controllerInstallation.Spec.SeedRef.Name}},
 			))
 		})
