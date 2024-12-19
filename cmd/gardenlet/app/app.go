c70f7dede481a9b954d01b94ceea08e0448c737a
rfranzke <rafael.franzke@sap.com>
2024-12-19 13:04:55 +0100

    Revert "[DROP] Enable label selector for Seeds"
    This reverts commit d85f2170133ca47992a87be2a725109daad3a142.
    
    All e2e tests passed.

--- cmd/gardenlet/app/app.go
+++ cmd/gardenlet/app/app.go
@@ -243,9 +243,9 @@ func (g *garden) Start(ctx context.Context) error {
 					Field: fields.SelectorFromSet(fields.Set{gardencore.SeedRefName: g.config.SeedConfig.SeedTemplate.Name}),
 				},
 				// TODO(rfranzke): Enable the label selector for Seeds after Gardener v1.113 has been released.
-				&gardencorev1beta1.Seed{}: {
-					Label: labels.SelectorFromSet(labels.Set{v1beta1constants.LabelPrefixSeedName + g.config.SeedConfig.SeedTemplate.Name: "true"}),
-				},
+				// &gardencorev1beta1.Seed{}: {
+				// 	Label: labels.SelectorFromSet(labels.Set{v1beta1constants.LabelPrefixSeedName + g.config.SeedConfig.SeedTemplate.Name: "true"}),
+				// },
 				&gardencorev1beta1.Shoot{}: {
 					Label: labels.SelectorFromSet(labels.Set{v1beta1constants.LabelPrefixSeedName + g.config.SeedConfig.SeedTemplate.Name: "true"}),
 				},
