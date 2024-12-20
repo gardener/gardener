cebe8bc2b0626a81a2140ece6dd4df9da33cdd79
RadaBDimitrova <rada09@abv.bg>
2024-12-20 16:56:42 +0200

    /seedmanagement/v1alpha1

---
+++ pkg/apis/seedmanagement/v1alpha1/helper/helper_managedseed.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package helper
+
+import (
+	seedmanagementv1alpha1 "github.com/gardener/gardener/pkg/apis/seedmanagement/v1alpha1"
+)
+
+// GetBootstrap returns the value of the given Bootstrap, or None if nil.
+func GetBootstrap(bootstrap *seedmanagementv1alpha1.Bootstrap) seedmanagementv1alpha1.Bootstrap {
+	if bootstrap != nil {
+		return *bootstrap
+	}
+	return seedmanagementv1alpha1.BootstrapNone
+}
