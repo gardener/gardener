c03e11a65636112df84d10dde23ab0c7e31c3a03
Luca Bernstein <luca.bernstein@sap.com>
2024-12-27 14:21:36 +0100

    Directly register flow metrics collection in gardenlet cmd

--- cmd/gardenlet/main.go
+++ cmd/gardenlet/main.go
@@ -6,15 +6,19 @@ package main
 
 import (
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	runtimemetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
 
 	"github.com/gardener/gardener/cmd/gardenlet/app"
 	"github.com/gardener/gardener/cmd/utils"
 	"github.com/gardener/gardener/pkg/gardenlet/features"
+	"github.com/gardener/gardener/pkg/utils/flow"
 )
 
 func main() {
 	utils.DeduplicateWarnings()
 	features.RegisterFeatureGates()
+
+	flow.RegisterMetrics(runtimemetrics.Registry)
 
 	if err := app.NewCommand().ExecuteContext(signals.SetupSignalHandler()); err != nil {
 		panic(err)
