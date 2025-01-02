746e99f212d996bf07be6f56d1bb6a8ee52d0725
Tim Ebert <timebertt@gmail.com>
2025-01-02 11:44:56 +0100

    Drop internal API version

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -224,12 +224,10 @@ controllermanager_groups() {
   
   kube::codegen::gen_helpers \
     --boilerplate "${PROJECT_ROOT}/hack/LICENSE_BOILERPLATE.txt" \
-    --extra-peer-dir github.com/gardener/gardener/pkg/controllermanager/apis/config \
     --extra-peer-dir github.com/gardener/gardener/pkg/controllermanager/apis/config/v1alpha1 \
     --extra-peer-dir k8s.io/apimachinery/pkg/apis/meta/v1 \
     --extra-peer-dir k8s.io/apimachinery/pkg/conversion \
     --extra-peer-dir k8s.io/apimachinery/pkg/runtime \
-    --extra-peer-dir k8s.io/component-base/config \
     --extra-peer-dir k8s.io/component-base/config/v1alpha1 \
     "${PROJECT_ROOT}/pkg/controllermanager/apis/config"
 }
