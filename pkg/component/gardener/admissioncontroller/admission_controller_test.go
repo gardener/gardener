be1bbde884093acf966e1858a9c40e7168d24e1a
Dimitar Mirchev <dimitar.mirchev@sap.com>
2025-01-06 10:46:14 +0200

    Configure the update restriction handler

--- pkg/component/gardener/admissioncontroller/admission_controller_test.go
+++ pkg/component/gardener/admissioncontroller/admission_controller_test.go
@@ -1144,6 +1144,36 @@ func validatingWebhookConfiguration(namespace string, caBundle []byte, testValue
 				},
 				SideEffects: &sideEffectsNone,
 			},
+			{
+				Name:                    "update-restriction.gardener.cloud",
+				AdmissionReviewVersions: []string{"v1", "v1beta1"},
+				TimeoutSeconds:          ptr.To[int32](10),
+				Rules: []admissionregistrationv1.RuleWithOperations{
+					{
+						Operations: []admissionregistrationv1.OperationType{
+							admissionregistrationv1.Create,
+							admissionregistrationv1.Update,
+							admissionregistrationv1.Delete,
+						},
+						Rule: admissionregistrationv1.Rule{
+							APIGroups:   []string{corev1.GroupName},
+							APIVersions: []string{"v1"},
+							Resources:   []string{"secrets", "configmaps"},
+						},
+					},
+				},
+				FailurePolicy: &failurePolicyFail,
+				ObjectSelector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"gardener.cloud/update-restriction": "true",
+					},
+				},
+				ClientConfig: admissionregistrationv1.WebhookClientConfig{
+					URL:      ptr.To("https://gardener-admission-controller." + namespace + "/webhooks/update-restriction"),
+					CABundle: caBundle,
+				},
+				SideEffects: &sideEffectsNone,
+			},
 		},
 	}
 
