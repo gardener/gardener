e3d8bcd2f3bea5298ae31f5646ab5ceb1d6821ec
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-01-10 17:09:20 +0200

    Correcting predicate for deployment

--- pkg/component/observability/monitoring/x509certificateexporter/x509certificateexporter.go
+++ pkg/component/observability/monitoring/x509certificateexporter/x509certificateexporter.go
@@ -120,7 +120,7 @@ func (x *x509CertificateExporter) Deploy(ctx context.Context) error {
 		return errors.New("x509CertificateExporter is currently supported only on the seed and runtime clusters")
 	}
 
-	if x.values.WorkerGroups != nil && len(x.values.SecretTypes) == 0 && len(x.values.ConfigMapKeys) == 0 {
+	if x.values.WorkerGroups == nil && len(x.values.SecretTypes) == 0 && len(x.values.ConfigMapKeys) == 0 {
 		return fmt.Errorf("no secret types, configmap keys and worker groups provided, nothing to monitor")
 	}
 	var (
