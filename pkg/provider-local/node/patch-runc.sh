e6c41bbf0acc9b595d8bb6a94f6ea9291f103613
Marc Vornetran <marc.vornetran@sap.com>
2025-01-17 15:53:19 +0100

    refactor: Cleanup patch-runc.sh script

--- pkg/provider-local/node/patch-runc.sh
+++ pkg/provider-local/node/patch-runc.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
+
 # SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
 #
 # SPDX-License-Identifier: Apache-2.0
-
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-RUNC_VERSION="1.2.4"
+RUNC_VERSION="v1.2.4"
 ARCH="$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')"
 
-echo "Installing runc v${RUNC_VERSION} ..."
-curl -sSL --retry 5 --output "/tmp/runc.${ARCH}" "https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.${ARCH}"
+echo "Installing runc ${RUNC_VERSION} ..."
+curl -sSL --retry 5 --output "/tmp/runc.${ARCH}" "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.${ARCH}"
 mv "/tmp/runc.${ARCH}" /usr/local/sbin/runc
 chmod 0755 /usr/local/sbin/runc
 runc --version
