ceaced6d357299403ec32a2e5c7fc46c6b300be0
Dimitar Mirchev <dimitar.mirchev@sap.com>
2025-01-22 12:53:03 +0200

    Allow generic-garbage-collector serviceaccount to delete update restricted resources (#11216)
    * Allow generic-garbage-collector sa to delete update restricted resources
    
    * Address review feedback
    
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>

--- pkg/admissioncontroller/webhook/admission/updaterestriction/handler.go
+++ pkg/admissioncontroller/webhook/admission/updaterestriction/handler.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"slices"
 
+	admissionv1 "k8s.io/api/admission/v1"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
@@ -21,9 +22,16 @@ type Handler struct{}
 // Handle checks if the request is issued by a gardenlet
 // and rejects the request if that is not the case.
 func (h *Handler) Handle(_ context.Context, req admission.Request) admission.Response {
-	if !slices.Contains(req.UserInfo.Groups, v1beta1constants.SeedsGroup) {
-		return admission.Denied(fmt.Sprintf("user %q is not allowed to modify system %s", req.UserInfo.Username, req.Resource.Resource))
+	// Allow the garbage-collector-controller to delete resources.
+	// This is required as KCM is used to GC resources
+	// that might be considered stale as their owner object is already gone.
+	if req.UserInfo.Username == "system:serviceaccount:kube-system:generic-garbage-collector" && req.Operation == admissionv1.Delete {
+		return admission.Allowed("generic-garbage-collector is allowed to delete system resources")
 	}
 
-	return admission.Allowed("")
+	if slices.Contains(req.UserInfo.Groups, v1beta1constants.SeedsGroup) {
+		return admission.Allowed("gardenlet is allowed to modify system resources")
+	}
+
+	return admission.Denied(fmt.Sprintf("user %q is not allowed to %s system %s", req.UserInfo.Username, req.Operation, req.Resource.Resource))
 }
--- pkg/admissioncontroller/webhook/admission/updaterestriction/handler_test.go
+++ pkg/admissioncontroller/webhook/admission/updaterestriction/handler_test.go
@@ -34,6 +34,7 @@ var _ = Describe("handler", func() {
 		req.Resource = metav1.GroupVersionResource{
 			Resource: "configmaps",
 		}
+		req.Operation = admissionv1.Update
 	})
 
 	Describe("#Handle", func() {
@@ -45,7 +46,7 @@ var _ = Describe("handler", func() {
 				Result: &metav1.Status{
 					Code:    int32(200),
 					Reason:  "",
-					Message: "",
+					Message: "gardenlet is allowed to modify system resources",
 				},
 			}))
 		})
@@ -61,7 +62,41 @@ var _ = Describe("handler", func() {
 				Result: &metav1.Status{
 					Code:    int32(403),
 					Reason:  "Forbidden",
-					Message: "user \"not-gardenlet\" is not allowed to modify system configmaps",
+					Message: "user \"not-gardenlet\" is not allowed to UPDATE system configmaps",
+				},
+			}))
+		})
+
+		It("should allow the delete request as it is made by the generic-garbage-collector serviceaccount", func() {
+			req.UserInfo = authenticationv1.UserInfo{
+				Username: "system:serviceaccount:kube-system:generic-garbage-collector",
+			}
+			req.Operation = admissionv1.Delete
+			resp := handler.Handle(ctx, req)
+			Expect(resp.Allowed).To(BeTrue())
+			Expect(resp.AdmissionResponse).To(Equal(admissionv1.AdmissionResponse{
+				Allowed: true,
+				Result: &metav1.Status{
+					Code:    int32(200),
+					Reason:  "",
+					Message: "generic-garbage-collector is allowed to delete system resources",
+				},
+			}))
+		})
+
+		It("should not allow the update request even if it is made by the generic-garbage-collector serviceaccount", func() {
+			req.UserInfo = authenticationv1.UserInfo{
+				Username: "system:serviceaccount:kube-system:generic-garbage-collector",
+			}
+			req.Operation = admissionv1.Update
+			resp := handler.Handle(ctx, req)
+			Expect(resp.Allowed).To(BeFalse())
+			Expect(resp.AdmissionResponse).To(Equal(admissionv1.AdmissionResponse{
+				Allowed: false,
+				Result: &metav1.Status{
+					Code:    int32(403),
+					Reason:  "Forbidden",
+					Message: "user \"system:serviceaccount:kube-system:generic-garbage-collector\" is not allowed to UPDATE system configmaps",
 				},
 			}))
 		})
