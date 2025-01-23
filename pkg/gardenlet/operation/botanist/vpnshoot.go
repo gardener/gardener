78d4a504bd1b23052a976d8e0c1dda17128c92a4
Dominik Froehlich <dominik.froehlich@sap.com>
2025-01-23 11:24:41 +0100

    feat: Allow CIDR overlap for non-HA VPN shoots

--- pkg/gardenlet/operation/botanist/vpnshoot.go
+++ pkg/gardenlet/operation/botanist/vpnshoot.go
@@ -6,6 +6,7 @@ package botanist
 
 import (
 	"github.com/gardener/gardener/imagevector"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/gardener/gardener/pkg/component"
 	vpnseedserver "github.com/gardener/gardener/pkg/component/networking/vpn/seedserver"
 	vpnshoot "github.com/gardener/gardener/pkg/component/networking/vpn/shoot"
@@ -38,6 +39,12 @@ func (b *Botanist) DefaultVPNShoot() (component.DeployWaiter, error) {
 		HighAvailabilityNumberOfShootClients: b.Shoot.VPNHighAvailabilityNumberOfShootClients,
 		DisableNewVPN:                        !b.Shoot.UsesNewVPN,
 		SeedPodNetwork:                       b.Seed.GetInfo().Spec.Networks.Pods,
+	}
+
+	if !gardencorev1beta1.IsIPv6SingleStack(b.Shoot.GetInfo().Spec.Networking.IPFamilies) {
+		values.ShootPodNetwork = *b.Shoot.GetInfo().Spec.Networking.Pods
+		values.ShootServiceNetwork = *b.Shoot.GetInfo().Spec.Networking.Services
+		values.ShootNodeNetwork = *b.Shoot.GetInfo().Spec.Networking.Nodes
 	}
 
 	return vpnshoot.New(
