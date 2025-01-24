6f95d1a4043bcb94e8ee9af878d6f5f8702de450
oliver-goetz <o.goetz@sap.com>
2025-01-24 22:30:06 +0100

    Istio TLS termination can now be disabled by setting the shoot annotation `shoot.gardener.cloud/disable-istio-tls-termination: "true"`
    Additionally, it is active for Kubernetes=>v1.31.0 only

--- pkg/operator/controller/garden/garden/components.go
+++ pkg/operator/controller/garden/garden/components.go
@@ -501,8 +501,11 @@ func (r *Reconciler) newKubeAPIServerService(log logr.Logger, garden *operatorv1
 
 	deployer := []component.Deployer{r.newKubeAPIServerServiceWithSuffix(log, garden, ingressGatewayValues, "")}
 
-	if features.DefaultFeatureGate.Enabled(features.IstioTLSTermination) {
-		deployer = append(deployer, r.newKubeAPIServerServiceWithSuffix(log, garden, ingressGatewayValues, kubeapiserverexposure.MutualTLSServiceNameSuffix))
+	mutualTLSService := r.newKubeAPIServerServiceWithSuffix(log, garden, ingressGatewayValues, kubeapiserverexposure.MutualTLSServiceNameSuffix)
+	if isIstioTLSTerminationEnabled(garden) {
+		deployer = append(deployer, mutualTLSService)
+	} else {
+		deployer = append(deployer, component.OpDestroy(mutualTLSService))
 	}
 
 	return component.OpWait(deployer...), nil
@@ -794,6 +797,7 @@ func (r *Reconciler) newSNI(garden *operatorv1alpha1.Garden, secretsManager secr
 					Namespace: ingressGatewayValues[0].Namespace,
 					Labels:    ingressGatewayValues[0].Labels,
 				},
+				IstioTLSTermination: isIstioTLSTerminationEnabled(garden),
 			}
 		},
 	), nil
