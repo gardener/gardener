60e2041e499fec34ec666312aa89838f596eea56
Tim Usner <tim.usner@sap.com>
2025-01-31 17:05:36 +0100

    Add `generate-extension.sh` script
    This script is supposed to be used by gardener-extensions
    to create `Extension` example manifests.
    Compared to `hack/generate-controller-registration.sh`
    the input parameters are reduced to a bare minimum
    and instead Kustomize mechanics are used behind the scenes.

---
+++ example/operator/extensions/doc.go
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// This package contains assets to generate extension resources - it is to force `go mod` to see them as dependencies.
+package extensions
