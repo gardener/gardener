2363a6066f5dec1494cc0e863a3a7b063b155124
Tim Ebert <timebertt@gmail.com>
2025-02-04 14:37:30 +0100

    Move timeout context for similar test

--- test/e2e/gardener/shoot/create_hibernate_wakeup_delete.go
+++ test/e2e/gardener/shoot/create_hibernate_wakeup_delete.go
@@ -88,8 +88,6 @@ var _ = Describe("Shoot Tests", Label("Shoot", "default"), func() {
 				// We verify the node readiness feature in this specific e2e test because it uses a single-node shoot cluster.
 				// The default shoot e2e test deals with multiple nodes, deleting all of them and waiting for them to be recreated
 				// might increase the test duration undesirably.
-				ctx, cancel = context.WithTimeout(parentCtx, 15*time.Minute)
-				defer cancel()
 				node.VerifyNodeCriticalComponentsBootstrapping(ctx, f.ShootFramework)
 			}
 
