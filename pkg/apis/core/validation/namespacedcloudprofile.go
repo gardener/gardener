f71926eb3ee4c2c2657b7ca98f02ae4c20232d73
Johannes Roncossek <johannes.roncossek@sap.com>
2025-02-07 10:56:51 +0100

    Capabilities validation in CloudProfile

--- pkg/apis/core/validation/namespacedcloudprofile.go
+++ pkg/apis/core/validation/namespacedcloudprofile.go
@@ -19,10 +19,12 @@ func ValidateNamespacedCloudProfile(namespacedCloudProfile *core.NamespacedCloud
 	allErrs = append(allErrs, apivalidation.ValidateObjectMeta(&namespacedCloudProfile.ObjectMeta, true, ValidateName, field.NewPath("metadata"))...)
 	allErrs = append(allErrs, validateNamespacedCloudProfileParent(namespacedCloudProfile.Spec.Parent, field.NewPath("spec.parent"))...)
 
+	// TODO: (Roncossek) load parend cloud profile
+
 	allErrs = append(allErrs, validateNamespacedCloudProfileKubernetesVersions(namespacedCloudProfile.Spec.Kubernetes, field.NewPath("spec.kubernetes"))...)
-	allErrs = append(allErrs, ValidateMachineImages(namespacedCloudProfile.Spec.MachineImages, field.NewPath("spec.machineImages"))...)
+	allErrs = append(allErrs, ValidateMachineImages(namespacedCloudProfile.Spec.MachineImages, nil, field.NewPath("spec.machineImages"))...)
 	allErrs = append(allErrs, validateVolumeTypes(namespacedCloudProfile.Spec.VolumeTypes, field.NewPath("spec.volumeTypes"))...)
-	allErrs = append(allErrs, validateMachineTypes(namespacedCloudProfile.Spec.MachineTypes, field.NewPath("spec.machineTypes"))...)
+	allErrs = append(allErrs, validateMachineTypes(namespacedCloudProfile.Spec.MachineTypes, nil, field.NewPath("spec.machineTypes"))...)
 
 	if namespacedCloudProfile.Spec.CABundle != nil {
 		_, err := utils.DecodeCertificate([]byte(*(namespacedCloudProfile.Spec.CABundle)))
@@ -60,10 +62,10 @@ func ValidateNamespacedCloudProfileStatus(spec *core.CloudProfileSpec, fldPath *
 
 	allErrs = append(allErrs, validateCloudProfileKubernetesSettings(spec.Kubernetes, fldPath.Child("kubernetes"))...)
 	if spec.MachineImages != nil {
-		allErrs = append(allErrs, ValidateCloudProfileMachineImages(spec.MachineImages, fldPath.Child("machineImages"))...)
+		allErrs = append(allErrs, ValidateCloudProfileMachineImages(spec.MachineImages, nil, fldPath.Child("machineImages"))...)
 	}
 	if spec.MachineTypes != nil {
-		allErrs = append(allErrs, validateMachineTypes(spec.MachineTypes, fldPath.Child("machineTypes"))...)
+		allErrs = append(allErrs, validateMachineTypes(spec.MachineTypes, nil, fldPath.Child("machineTypes"))...)
 	}
 	if spec.VolumeTypes != nil {
 		allErrs = append(allErrs, validateVolumeTypes(spec.VolumeTypes, fldPath.Child("volumeTypes"))...)
--- pkg/apis/core/validation/utils_test.go
+++ pkg/apis/core/validation/utils_test.go
@@ -5,9 +5,12 @@
 package validation_test
 
 import (
+	"github.com/bsm/gomega/types"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
+
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	"github.com/gardener/gardener/pkg/apis/core"
@@ -91,4 +94,99 @@ var _ = Describe("Utils tests", func() {
 			Expect(errorList).To(BeEmpty())
 		})
 	})
+
+	Describe("#ValidateCapabilities", func() {
+		var (
+			dummyPath    *field.Path       = field.NewPath("dummy")
+			capabilities core.Capabilities = map[string]string{
+				"architecture":   "amd64,arm64",
+				"hypervisorType": "gen1,gen2,gen3",
+			}
+		)
+		It("should accept a capabilitiesDefinition with architecture as capability", func() {
+			errorList := ValidateCapabilitiesDefinition(capabilities, dummyPath)
+			Expect(errorList).To(BeEmpty())
+
+		})
+
+		DescribeTable("should reject invalid capabilitiesDefinition",
+			func(capabilities core.Capabilities, expectedError []types.GomegaMatcher) {
+				errorList := ValidateCapabilitiesDefinition(capabilities, dummyPath)
+				Expect(errorList).To(ConsistOf(expectedError))
+			},
+			Entry("empty capability values", core.Capabilities{"architecture": "amd64", "hypervisorType": ""}, []types.GomegaMatcher{
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":  Equal(field.ErrorTypeRequired),
+					"Field": Equal(dummyPath.Child("hypervisorType").String()),
+				})),
+			}),
+			Entry("missing architecture capability", core.Capabilities{"hypervisorType": "gen1,gen2,gen3"}, []types.GomegaMatcher{
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":  Equal(field.ErrorTypeRequired),
+					"Field": Equal(dummyPath.Child("architecture").String()),
+				})),
+			}),
+		)
+
+		It("should unmarshal correct CapabilitiesSet", func() {
+			var CapabilitiesSet = []v1.JSON{
+				{Raw: []byte(`{"architecture":"amd64","hypervisorType":"gen2"}`)},
+				{Raw: []byte(`{"architecture":"amd64","hypervisorType":"gen2"}`)},
+				{Raw: []byte(`{"architecture":"arm64","hypervisorType":"gen1"}`)},
+				{Raw: []byte(`{"architecture":"arm64","hypervisorType":"gen2,gen3"}`)},
+			}
+			capabilitiesSet, err := UnmarshalCapabilitiesSet(CapabilitiesSet, dummyPath)
+
+			Expect(err).To(BeNil())
+			Expect(capabilitiesSet).To(HaveLen(4))
+			Expect(capabilitiesSet[0]).To(Equal(core.Capabilities{"architecture": "amd64", "hypervisorType": "gen2"}))
+		})
+
+		It("should error on unparsable CapabilitiesSet", func() {
+			var CapabilitiesSet = []v1.JSON{
+				{Raw: []byte(`{"architecture":"amd64","hypervisorType":"gen2"}`)},
+				{Raw: []byte(`{"architecture":"amd64","hype....🆘`)},
+			}
+
+			capabilitiesSet, err := UnmarshalCapabilitiesSet(CapabilitiesSet, dummyPath)
+
+			Expect(err).To(ConsistOf([]types.GomegaMatcher{
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":  Equal(field.ErrorTypeInvalid),
+					"Field": Equal("dummy[1]"),
+				})),
+			}))
+			Expect(capabilitiesSet[1]).To(HaveLen(0))
+		})
+
+		It("should sanitize capability values on parsing", func() {
+			var capabilities2 core.Capabilities = map[string]string{
+				"architecture":   "  amd64 ,arm64 ,amd32, , 'asdas   '    , I look wierd  ",
+				"hypervisorType": `"gen1", "gen4"`,
+			}
+			parsedCapabilities := ParseCapabilityValues(capabilities2)
+			architectureSet := parsedCapabilities["architecture"]
+			hypervisorTypeSet := parsedCapabilities["hypervisorType"]
+
+			Expect(architectureSet.Contains("amd64", "arm64", "amd32", "I look wierd", "asdas")).To(BeTrue())
+			Expect(architectureSet).To(HaveLen(5))
+			Expect(hypervisorTypeSet.Contains("gen1", "gen4")).To(BeTrue())
+			Expect(hypervisorTypeSet).To(HaveLen(2))
+		})
+
+		It("should create intersection of capabilities", func() {
+			var capabilities2 core.Capabilities = map[string]string{
+				"architecture":   "amd64,arm64,amd32",
+				"hypervisorType": "gen1,gen4",
+				"notIntersect":   "value",
+			}
+			intersection := GetCapabilitiesIntersection(ParseCapabilityValues(capabilities), ParseCapabilityValues(capabilities2))
+			isArchitectureIntersection := intersection["architecture"].Contains("amd64", "arm64")
+			isHypervisorTypeIntersection := intersection["hypervisorType"].Contains("gen1")
+			Expect(isArchitectureIntersection).To(BeTrue())
+			Expect(isHypervisorTypeIntersection).To(BeTrue())
+			Expect(len(intersection)).To(Equal(2))
+		})
+	})
+
 })
