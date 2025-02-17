5b4102c47dbb16c100e181c1a02b6e82c613e880
Johann Gnaucke <johann@gnaucke.com>
2025-02-17 11:29:06 +0100

    fix failing unit test with mock interface

--- pkg/gardenlet/operation/botanist/kubeapiserver_test.go
+++ pkg/gardenlet/operation/botanist/kubeapiserver_test.go
@@ -645,6 +645,8 @@ users:
 			Expect(seedClient.Create(ctx, deployment)).To(Succeed())
 			Expect(seedClient.Get(ctx, client.ObjectKeyFromObject(deployment), deployment)).To(Succeed())
 
+			kubeAPIServer.EXPECT().SetAutoscalingReplicas(gomock.Any())
+
 			Expect(botanist.ScaleKubeAPIServerToOne(ctx)).To(Succeed())
 
 			Expect(seedClient.Get(ctx, client.ObjectKeyFromObject(deployment), deployment)).To(Succeed())
