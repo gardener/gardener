3072b6afcbbeb5ba30bc46e03b6d5c220e0cd97a
Johannes Scheerer <johannes.scheerer@sap.com>
2025-02-17 17:47:33 +0100

    Increase log output when adding to/checking /etc/hosts

--- hack/ci-common.sh
+++ hack/ci-common.sh
@@ -38,6 +38,9 @@ export_artifacts() {
       kubectl cp "$namespace/$node":/var/log "$node_dir" || true
     done < <(kubectl -n "$namespace" get po -l app=machine -oname | cut -d/ -f2)
   done < <(kubectl get ns -l gardener.cloud/role=shoot -oname | cut -d/ -f2)
+
+  echo "> Exporting /etc/hosts"
+  cp /etc/hosts $ARTIFACTS/$cluster_name/hosts
 }
 
 export_events_for_kind() {
@@ -94,7 +97,9 @@ clamp_mss_to_pmtu() {
 # If running in prow, we need to ensure that garden.local.gardener.cloud resolves to localhost
 ensure_glgc_resolves_to_localhost() {
   if [ -n "${CI:-}" -a -n "${ARTIFACTS:-}" ]; then
+    echo "> Adding garden.local.gardener.cloud to /etc/hosts..."
     printf "\n127.0.0.1 garden.local.gardener.cloud\n" >> /etc/hosts
     printf "\n::1 garden.local.gardener.cloud\n" >> /etc/hosts
+    echo "> Content of '/etc/hosts' after adding garden.local.gardener.cloud:\n$(cat /etc/hosts)"
   fi
 }
