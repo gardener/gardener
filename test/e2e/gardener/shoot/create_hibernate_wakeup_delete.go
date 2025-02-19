7e56fc4de32eabff3fc68034d3f5bd6467666844
rfranzke <rafael.franzke@sap.com>
2025-02-19 14:36:24 +0100

    Drop questionable child context for single delete call
    Co-Authored-By: Luca Bernstein <luca.bernstein@sap.com>

--- test/e2e/gardener/shoot/create_hibernate_wakeup_delete.go
+++ test/e2e/gardener/shoot/create_hibernate_wakeup_delete.go
@@ -44,9 +44,7 @@ var _ = Describe("Shoot Tests", Label("Shoot", "default"), func() {
 				Expect(f.GardenClient.Client().Create(ctx, namespacedCloudProfile)).To(Succeed())
 				DeferCleanup(func() {
 					By("Delete NamespacedCloudProfile")
-					ctx, cancel = context.WithTimeout(parentCtx, 15*time.Minute)
-					defer cancel()
-					Expect(f.GardenClient.Client().Delete(ctx, namespacedCloudProfile)).To(Or(Succeed(), BeNotFoundError()))
+					Expect(f.GardenClient.Client().Delete(parentCtx, namespacedCloudProfile)).To(Or(Succeed(), BeNotFoundError()))
 				})
 
 				By("Wait for new NamespacedCloudProfile to be reconciled")
