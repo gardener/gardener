6b67a020a1b93d14c3fd4b88bf5adb828c0a7220
oliver-goetz <o.goetz@sap.com>
2025-02-25 16:00:57 +0100

    Download runc to `dev` instead of `/tmp` directory

--- pkg/provider-local/node/get-runc.sh
+++ pkg/provider-local/node/get-runc.sh
@@ -11,8 +11,9 @@ set -o pipefail
 RUNC_VERSION="v1.2.4"
 ARCH="$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')"
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DEV_DIR="${SCRIPT_DIR}/../../../dev"
 
 echo "Getting runc ${RUNC_VERSION} for ${ARCH}..."
-curl -sSL --retry 5 --output "/tmp/runc.${ARCH}" "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.${ARCH}"
-mv "/tmp/runc.${ARCH}" "${SCRIPT_DIR}/runc"
+curl -sSL --retry 5 --output "${DEV_DIR}/runc.${ARCH}" "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.${ARCH}"
+mv "${DEV_DIR}/runc.${ARCH}" "${SCRIPT_DIR}/runc"
 chmod 0755 "${SCRIPT_DIR}/runc"
