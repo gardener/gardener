6942fc20bfd55b9d91c12e687aac1a298754c001
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-02-25 14:36:46 +0200

    chore/shootstate/finalizer: wrap reconciler errors with additional context
    This commit wraps the errors returned from the reconciler with
    additional information about the context in which the error appeared.

--- pkg/controllermanager/controller/shoot/state/finalizer/reconciler.go
+++ pkg/controllermanager/controller/shoot/state/finalizer/reconciler.go
@@ -6,6 +6,7 @@ package finalizer
 
 import (
 	"context"
+	"fmt"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/gardener/gardener/pkg/controllerutils"
@@ -23,45 +24,36 @@ type Reconciler struct {
 }
 
 func (r *Reconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
-	log := logf.FromContext(ctx).WithValues("reconcileRequest", request)
-
-	shoot := &gardencorev1beta1.Shoot{}
-	if err := r.Client.Get(ctx, request.NamespacedName, shoot); err != nil {
-		log.Info("Did not manage to retrieve Shoot for the given ShootState")
-		return reconcile.Result{}, err
-	}
-
-	shootUid := shoot.ObjectMeta.UID
-	log = log.WithValues("shootUid", shootUid)
+	log := logf.FromContext(ctx)
 
 	shootState := &gardencorev1beta1.ShootState{}
-	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(shoot), shootState); err != nil {
+	if err := r.Client.Get(ctx, request.NamespacedName, shootState); err != nil {
 		if apierrors.IsNotFound(err) {
-			log.Info("Did not manage to retrieve ShootState object")
-			return reconcile.Result{}, nil
+			log.V(1).Info("Did not manage to retrieve ShootState object")
+			return reconcile.Result{}, fmt.Errorf("error retrieving ShootState from store: %w", err)
 		}
 		return reconcile.Result{}, err
 	}
 
-	shootStateUid := shoot.ObjectMeta.UID
+	shoot := &gardencorev1beta1.Shoot{}
+	if err := r.Client.Get(ctx, request.NamespacedName, shoot); err != nil {
+		log.Info("Did not manage to retrieve Shoot for the given ShootState")
+		return reconcile.Result{}, fmt.Errorf("error retrieving Shoot from store: %w", err)
+	}
+
 	shootLastOperationType := shoot.Status.LastOperation.Type
 	shootLastOperationState := shoot.Status.LastOperation.State
-	shootLastOpProgress := shoot.Status.LastOperation.Progress
 	shootStateHasFinalizer := controllerutil.ContainsFinalizer(shootState, FinalizerName)
 
 	log = log.
-		WithValues("shootStateUid", shootStateUid).
 		WithValues("shootLastOperationType", shootLastOperationType).
-		WithValues("shootLastOperationState", shootLastOperationState).
-		WithValues("shootLastOpProgress", shootLastOpProgress).
-		WithValues("isFinalizerAdded", shootStateHasFinalizer).
-		WithValues("finalizer", FinalizerName)
+		WithValues("shootLastOperationState", shootLastOperationState)
 
 	isShootOpMigrate := shootLastOperationType == gardencorev1beta1.LastOperationTypeMigrate
 	if isShootOpMigrate && !shootStateHasFinalizer {
 		log.Info("Adding finalizer")
 		if err := controllerutils.AddFinalizers(ctx, r.Client, shootState, FinalizerName); err != nil {
-			return reconcile.Result{}, err
+			return reconcile.Result{}, fmt.Errorf("error adding finalizer to ShootState: %w", err)
 		}
 		return reconcile.Result{}, nil
 	}
@@ -71,7 +63,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request reconcile.Request) (
 	if isShootOpRestore && isShootOpSucceeded && shootStateHasFinalizer {
 		log.Info("Removing finalizer")
 		if err := controllerutils.RemoveFinalizers(ctx, r.Client, shootState, FinalizerName); err != nil {
-			return reconcile.Result{}, err
+			return reconcile.Result{}, fmt.Errorf("error removing finalizer from ShootState: %w", err)
 		}
 		return reconcile.Result{}, nil
 	}
