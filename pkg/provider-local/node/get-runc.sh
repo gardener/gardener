81c884b7087a5ce26b4d1a447a5b755be459a9c0
oliver-goetz <o.goetz@sap.com>
2025-02-25 15:31:52 +0100

    Copy for `runc` from disk into `pkg/provider-local/node` image too

--- pkg/provider-local/node/get-runc.sh
+++ pkg/provider-local/node/get-runc.sh
@@ -10,9 +10,9 @@ set -o pipefail
 
 RUNC_VERSION="v1.2.4"
 ARCH="$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo "Installing runc ${RUNC_VERSION} ..."
+echo "Getting runc ${RUNC_VERSION} for ${ARCH}..."
 curl -sSL --retry 5 --output "/tmp/runc.${ARCH}" "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.${ARCH}"
-mv "/tmp/runc.${ARCH}" /usr/local/sbin/runc
-chmod 0755 /usr/local/sbin/runc
-runc --version
+mv "/tmp/runc.${ARCH}" "${SCRIPT_DIR}/runc"
+chmod 0755 "${SCRIPT_DIR}/runc"
