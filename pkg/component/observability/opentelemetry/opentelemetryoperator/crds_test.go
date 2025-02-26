58218260f3909b78a53cf591d9c8ee415edd70d6
Radoslav Hubenov <rrhubenov@gmail.com>
2025-02-26 15:58:14 +0200

    Add opentelemetry operator tests

--- pkg/component/observability/opentelemetry/opentelemetryoperator/crds_test.go
+++ pkg/component/observability/opentelemetry/opentelemetryoperator/crds_test.go
@@ -5,79 +5,74 @@
 package opentelemetryoperator_test
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/component"
+	"github.com/gardener/gardener/pkg/component/observability/opentelemetry/opentelemetryoperator"
 	. "github.com/onsi/gomega"
 
 	. "github.com/gardener/gardener/pkg/utils/test/matchers"
 )
 
-// var _ = Describe("CRDs", func() {
-// 	var (
-// 		ctx         context.Context
-// 		c           client.Client
-// 		crdDeployer component.DeployWaiter
-// 	)
-//
-// 	BeforeEach(func() {
-// 		ctx = context.TODO()
-//
-// 		s := runtime.NewScheme()
-// 		Expect(apiextensionsv1.AddToScheme(s)).NotTo(HaveOccurred())
-//
-// 		c = fake.NewClientBuilder().WithScheme(s).Build()
-//
-// 		mapper := meta.NewDefaultRESTMapper([]schema.GroupVersion{apiextensionsv1.SchemeGroupVersion})
-// 		mapper.Add(apiextensionsv1.SchemeGroupVersion.WithKind("CustomResourceDefinition"), meta.RESTScopeRoot)
-//
-// 		applier := kubernetes.NewApplier(c, mapper)
-//
-// 		crdDeployer = fluentoperator.NewCRDs(applier)
-// 	})
-//
-// 	JustBeforeEach(func() {
-// 		Expect(crdDeployer.Deploy(ctx)).ToNot(HaveOccurred(), "fluent operator crds deploy succeeds")
-// 	})
-//
-// 	DescribeTable("CRD is deployed",
-// 		func(crdName string) {
-// 			Expect(c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})).ToNot(HaveOccurred())
-// 		},
-//
-// 		Entry("ClusterFilter", "clusterfilters.fluentbit.fluent.io"),
-// 		Entry("ClusterFluentBitConfig", "clusterfluentbitconfigs.fluentbit.fluent.io"),
-// 		Entry("ClusterInput", "clusterinputs.fluentbit.fluent.io"),
-// 		Entry("ClusterOutput", "clusteroutputs.fluentbit.fluent.io"),
-// 		Entry("ClusterParser", "clusterparsers.fluentbit.fluent.io"),
-// 		Entry("FluentBit", "fluentbits.fluentbit.fluent.io"),
-// 		Entry("Collector", "collectors.fluentbit.fluent.io"),
-// 		Entry("FluentBitConfig", "fluentbitconfigs.fluentbit.fluent.io"),
-// 		Entry("Filter", "filters.fluentbit.fluent.io"),
-// 		Entry("Parser", "parsers.fluentbit.fluent.io"),
-// 		Entry("Output", "outputs.fluentbit.fluent.io"),
-// 		Entry("ClusterMultilineParser", "clustermultilineparsers.fluentbit.fluent.io"),
-// 		Entry("MultilineParser", "multilineparsers.fluentbit.fluent.io"),
-// 	)
-//
-// 	DescribeTable("should re-create CRD if it is deleted",
-// 		func(crdName string) {
-// 			Expect(c.Delete(ctx, &apiextensionsv1.CustomResourceDefinition{ObjectMeta: metav1.ObjectMeta{Name: crdName}}, &client.DeleteOptions{})).ToNot(HaveOccurred())
-// 			Expect(c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})).To(BeNotFoundError())
-// 			Expect(crdDeployer.Deploy(ctx)).ToNot(HaveOccurred())
-// 			Expect(c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})).ToNot(HaveOccurred())
-// 		},
-//
-// 		Entry("ClusterFilter", "clusterfilters.fluentbit.fluent.io"),
-// 		Entry("ClusterFluentBitConfig", "clusterfluentbitconfigs.fluentbit.fluent.io"),
-// 		Entry("ClusterInput", "clusterinputs.fluentbit.fluent.io"),
-// 		Entry("ClusterOutput", "clusteroutputs.fluentbit.fluent.io"),
-// 		Entry("ClusterParser", "clusterparsers.fluentbit.fluent.io"),
-// 		Entry("FluentBit", "fluentbits.fluentbit.fluent.io"),
-// 		Entry("Collectors", "collectors.fluentbit.fluent.io"),
-// 		Entry("FluentBitConfig", "fluentbitconfigs.fluentbit.fluent.io"),
-// 		Entry("Filter", "filters.fluentbit.fluent.io"),
-// 		Entry("Parser", "parsers.fluentbit.fluent.io"),
-// 		Entry("Output", "outputs.fluentbit.fluent.io"),
-// 		Entry("ClusterMultilineParser", "clustermultilineparsers.fluentbit.fluent.io"),
-// 		Entry("MultilineParser", "multilineparsers.fluentbit.fluent.io"),
-// 	)
-// })
+var _ = Describe("CRDs", func() {
+	var (
+		ctx         context.Context
+		c           client.Client
+		crdDeployer component.DeployWaiter
+	)
+
+	BeforeEach(func() {
+		ctx = context.TODO()
+
+		s := runtime.NewScheme()
+		Expect(apiextensionsv1.AddToScheme(s)).NotTo(HaveOccurred())
+
+		c = fake.NewClientBuilder().WithScheme(s).Build()
+
+		mapper := meta.NewDefaultRESTMapper([]schema.GroupVersion{apiextensionsv1.SchemeGroupVersion})
+		mapper.Add(apiextensionsv1.SchemeGroupVersion.WithKind("CustomResourceDefinition"), meta.RESTScopeRoot)
+
+		applier := kubernetes.NewApplier(c, mapper)
+
+		crdDeployer = opentelemetryoperator.NewCRDs(applier)
+	})
+
+	JustBeforeEach(func() {
+		Expect(crdDeployer.Deploy(ctx)).ToNot(HaveOccurred(), "opentelemetry operator crds deploy succeeds")
+	})
+
+	DescribeTable("CRD is deployed",
+		func(crdName string) {
+			Expect(c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})).ToNot(HaveOccurred())
+		},
+
+		Entry("OpenTelemetryCollector", "opentelemetrycollectors.opentelemetry.io"),
+		Entry("Instrumentation", "instrumentations.opentelemetry.io"),
+		Entry("OpamBridge", "opampbridges.opentelemetry.io"),
+		Entry("TargetAllocator", "targetallocators.opentelemetry.io"),
+	)
+
+	DescribeTable("should re-create CRD if it is deleted",
+		func(crdName string) {
+			Expect(c.Delete(ctx, &apiextensionsv1.CustomResourceDefinition{ObjectMeta: metav1.ObjectMeta{Name: crdName}}, &client.DeleteOptions{})).ToNot(HaveOccurred())
+			Expect(c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})).To(BeNotFoundError())
+			Expect(crdDeployer.Deploy(ctx)).ToNot(HaveOccurred())
+			Expect(c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})).ToNot(HaveOccurred())
+		},
+
+		Entry("OpenTelemetryCollector", "opentelemetrycollectors.opentelemetry.io"),
+		Entry("Instrumentation", "instrumentations.opentelemetry.io"),
+		Entry("OpamBridge", "opampbridges.opentelemetry.io"),
+		Entry("TargetAllocator", "targetallocators.opentelemetry.io"),
+	)
+})
---
+++ pkg/component/observability/opentelemetry/opentelemetryoperator/export_test.go
@@ -0,0 +1,9 @@
+package opentelemetryoperator
+
+var ServiceAccount = serviceAccount
+var ClusterRole = clusterRole
+var ClusterRoleBinding = clusterRoleBinding
+var Role = role
+var RoleBinding = roleBinding
+var Deployment = deployment
+var Vpa = vpa
