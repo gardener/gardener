642b81f0256c7299f78c68176ceb1a15af8b4e9b
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-02-27 17:54:07 +0200

    feat/shootstate/integration/test: add integration test for ShootState finalizer lifecycle
    This commit features an integration test that aims to validate the newly
    controller's responsiveness to `Shoot` object updates.

---
+++ test/integration/controllermanager/shoot/state/state_suite_test.go
@@ -0,0 +1,130 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package state_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	controllerconfig "sigs.k8s.io/controller-runtime/pkg/config"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	controllermanagerconfigv1alpha1 "github.com/gardener/gardener/pkg/controllermanager/apis/config/v1alpha1"
+	"github.com/gardener/gardener/pkg/controllermanager/controller/shoot/state/finalizer"
+	"github.com/gardener/gardener/pkg/logger"
+	. "github.com/gardener/gardener/pkg/utils/test/matchers"
+	gardenerenvtest "github.com/gardener/gardener/test/envtest"
+)
+
+func TestState(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "State Suite")
+}
+
+const (
+	testID                     = "shootstate-controller-test"
+	gardenClusterApiServerArgs = `--disable-admission-plugins=DeletionConfirmation,ResourceReferenceManager,ExtensionValidator,ShootQuotaValidator,ShootValidator,ShootTolerationRestriction,ShootDNS`
+)
+
+var (
+	ctx = context.Background()
+	log logr.Logger
+
+	testEnv    *gardenerenvtest.GardenerTestEnvironment
+	testClient client.Client
+	restConfig *rest.Config
+
+	mgr        manager.Manager
+	mgrContext context.Context
+	mgrCancel  context.CancelFunc
+
+	testNamespace *corev1.Namespace
+	testRunID     string
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(logger.MustNewZapLogger(logger.DebugLevel, logger.FormatJSON, zap.WriteTo(GinkgoWriter)))
+	log = logf.Log.WithName(testID)
+
+	By("Start test environment")
+	testEnv = &gardenerenvtest.GardenerTestEnvironment{
+		GardenerAPIServer: &gardenerenvtest.GardenerAPIServer{
+			Args: []string{gardenClusterApiServerArgs},
+		},
+	}
+
+	var err error
+	restConfig, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(restConfig).NotTo(BeNil())
+
+	By("Create test client")
+	testClient, err = client.New(restConfig, client.Options{Scheme: kubernetes.GardenScheme})
+	Expect(err).NotTo(HaveOccurred())
+
+	By("Create test Namespace")
+	// create dedicated namespace for each test run,
+	// so that we can run multiple tests concurrently for stress tests
+	testNamespace = &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "garden-",
+		},
+	}
+	Expect(testClient.Create(ctx, testNamespace)).To(Succeed())
+	log.Info("Created Namespace for test", "namespaceName", testNamespace.Name)
+	testRunID = testNamespace.Name
+
+	By("Setup manager")
+	mgr, err = manager.New(restConfig, manager.Options{
+		Scheme:  kubernetes.GardenScheme,
+		Metrics: metricsserver.Options{BindAddress: "0"},
+		Cache: cache.Options{
+			DefaultNamespaces: map[string]cache.Config{testNamespace.Name: {}},
+		},
+		Controller: controllerconfig.Controller{
+			SkipNameValidation: ptr.To(true),
+		},
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	By("Setup finalizer reconciler")
+	Expect((&finalizer.Reconciler{
+		Config: controllermanagerconfigv1alpha1.ShootStateControllerConfiguration{
+			ConcurrentSyncs: ptr.To(5),
+		},
+	}).AddToManager(mgr)).To(Succeed())
+
+	mgrContext, mgrCancel = context.WithCancel(ctx)
+	go func() {
+		defer GinkgoRecover()
+		fmt.Println("STARTING THE CONTROLLER MANAGER")
+		Expect(mgr.Start(mgrContext)).To(Succeed())
+	}()
+})
+
+var _ = AfterSuite(func() {
+	By("Stop the controller manager")
+	mgrCancel()
+
+	By("Delete test Namespace")
+	Expect(testClient.Delete(ctx, testNamespace)).To(Or(Succeed(), BeNotFoundError()))
+
+	By("Stop the test environment")
+	Expect(testEnv.Stop()).To(Succeed())
+})
