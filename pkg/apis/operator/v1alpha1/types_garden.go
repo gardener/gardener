06af00aff889321eba5d31c91b6c6fd4d49c0614
rfranzke <rafael.franzke@sap.com>
2025-02-28 15:11:47 +0100

    Augment API

--- pkg/apis/operator/v1alpha1/types_garden.go
+++ pkg/apis/operator/v1alpha1/types_garden.go
@@ -488,6 +488,16 @@ type GardenerAPIServerConfig struct {
 	// EncryptionConfig contains customizable encryption configuration of the Gardener API server.
 	// +optional
 	EncryptionConfig *gardencorev1beta1.EncryptionConfig `json:"encryptionConfig,omitempty"`
+	// GoAwayChance can be used to prevent HTTP/2 clients from getting stuck on a single apiserver, randomly close a
+	// connection (GOAWAY). The client's other in-flight requests won't be affected, and the client will reconnect,
+	// likely landing on a different apiserver after going through the load balancer again. This field sets the fraction
+	// of requests that will be sent a GOAWAY. Clusters with single apiservers, or which don't use a load balancer,
+	// should NOT enable this. Min is 0 (off), Max is .02 (1/50 requests); .001 (1/1000) is a recommended starting
+	// point.
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=0.02
+	// +optional
+	GoAwayChance *float64 `json:"goAwayChance,omitempty"`
 }
 
 // GardenerAdmissionControllerConfig contains configuration settings for the gardener-admission-controller.
