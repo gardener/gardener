2ab393f84dd5755e89936abb0b092e85963eadd5
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-02-28 13:30:20 +0200

    feat/shootstate/finalizer: update license docs from 2024 to 2025
    Update the license docs of newly added files from 2024 to 2025.
    
    Also:
    - add `shootState` configuration to the controller manager example
      manifest.

--- test/integration/controllermanager/shoot/state/state_test.go
+++ test/integration/controllermanager/shoot/state/state_test.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
 //
 // SPDX-License-Identifier: Apache-2.0
 
