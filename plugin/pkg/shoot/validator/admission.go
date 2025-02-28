472d0d120987dd6208752b42ce5cf2a131ac2c95
Johannes Roncossek <johannes.roncossek@sap.com>
2025-02-28 08:17:08 +0100

    fix error message in shoot kubernetes version parsing

--- plugin/pkg/shoot/validator/admission.go
+++ plugin/pkg/shoot/validator/admission.go
@@ -1026,7 +1026,7 @@ func (c *validationContext) validateProvider(a admission.Attributes) field.Error
 
 	controlPlaneVersion, err := semver.NewVersion(c.shoot.Spec.Kubernetes.Version)
 	if err != nil {
-		return append(allErrs, field.Invalid(field.NewPath("spec", "kubernetes", "version"), c.shoot.Spec.Kubernetes.Version, fmt.Sprintf("cannot parse the kubernetes version: %w", err)))
+		return append(allErrs, field.Invalid(field.NewPath("spec", "kubernetes", "version"), c.shoot.Spec.Kubernetes.Version, fmt.Sprintf("cannot parse the kubernetes version: %v", err)))
 	}
 
 	for i, worker := range c.shoot.Spec.Provider.Workers {
