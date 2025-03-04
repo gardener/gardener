720cb56b8b95804f3cf506bce7a9ce38497615cc
Johannes Scheerer <johannes.scheerer@sap.com>
2025-03-04 14:21:08 +0100

    Addressed review feedback.
    Removed state from reconciler and reused `utilsnet.SuggestPort(...)`
    instead.

--- pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler_test.go
+++ pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler_test.go
@@ -5,11 +5,14 @@
 package controllerinstallation_test
 
 import (
+	"math"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/ptr"
 
 	. "github.com/gardener/gardener/pkg/gardenlet/controller/controllerinstallation/controllerinstallation"
@@ -74,61 +77,71 @@ var _ = Describe("Reconciler", func() {
 		It("should adapt objects if bootstrapping control plane node", func() {
 			reconciler.BootstrapControlPlaneNode = true
 
-			Expect(reconciler.BootstrapControlPlaneNodeFunc(pod)).To(Succeed())
-			Expect(pod.Spec.HostNetwork).To(BeTrue())
-			Expect(pod.Spec.Tolerations).To(Equal([]corev1.Toleration{
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
-			}))
+			checkHostNetworkAndTolerations := func(podSpec *corev1.PodSpec) {
+				Expect(podSpec.HostNetwork).To(BeTrue())
+				Expect(podSpec.Tolerations).To(Equal([]corev1.Toleration{
+					{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
+					{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
+				}))
+			}
 
-			Expect(reconciler.BootstrapControlPlaneNodeFunc(deployment)).To(Succeed())
-			Expect(deployment.Spec.Replicas).To(Equal(ptr.To(int32(1))))
-			Expect(deployment.Spec.Template.Spec.HostNetwork).To(BeTrue())
-			Expect(deployment.Spec.Template.Spec.Tolerations).To(Equal([]corev1.Toleration{
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
-			}))
-
-			Expect(reconciler.BootstrapControlPlaneNodeFunc(statefulSet)).To(Succeed())
-			Expect(statefulSet.Spec.Template.Spec.HostNetwork).To(BeTrue())
-			Expect(statefulSet.Spec.Template.Spec.Tolerations).To(Equal([]corev1.Toleration{
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
-			}))
-
-			Expect(reconciler.BootstrapControlPlaneNodeFunc(daemonSet)).To(Succeed())
-			Expect(daemonSet.Spec.Template.Spec.HostNetwork).To(BeTrue())
-			Expect(daemonSet.Spec.Template.Spec.Tolerations).To(Equal([]corev1.Toleration{
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
-			}))
-
-			Expect(reconciler.BootstrapControlPlaneNodeFunc(job)).To(Succeed())
-			Expect(job.Spec.Template.Spec.HostNetwork).To(BeTrue())
-			Expect(job.Spec.Template.Spec.Tolerations).To(Equal([]corev1.Toleration{
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
-			}))
-
-			Expect(reconciler.BootstrapControlPlaneNodeFunc(cronJob)).To(Succeed())
-			Expect(cronJob.Spec.JobTemplate.Spec.Template.Spec.HostNetwork).To(BeTrue())
-			Expect(cronJob.Spec.JobTemplate.Spec.Template.Spec.Tolerations).To(Equal([]corev1.Toleration{
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule},
-				{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
-			}))
+			for _, obj := range []struct {
+				object    runtime.Object
+				checkFunc func()
+			}{
+				{pod, func() {
+					checkHostNetworkAndTolerations(&pod.Spec)
+				}},
+				{deployment, func() {
+					Expect(deployment.Spec.Replicas).To(Equal(ptr.To(int32(1))))
+					checkHostNetworkAndTolerations(&deployment.Spec.Template.Spec)
+				}},
+				{statefulSet, func() {
+					checkHostNetworkAndTolerations(&statefulSet.Spec.Template.Spec)
+				}},
+				{daemonSet, func() {
+					checkHostNetworkAndTolerations(&daemonSet.Spec.Template.Spec)
+				}},
+				{job, func() {
+					checkHostNetworkAndTolerations(&job.Spec.Template.Spec)
+				}},
+				{cronJob, func() {
+					checkHostNetworkAndTolerations(&cronJob.Spec.JobTemplate.Spec.Template.Spec)
+				}},
+			} {
+				Expect(reconciler.BootstrapControlPlaneNodeFunc(obj.object)).To(Succeed())
+				obj.checkFunc()
+			}
 		})
 	})
 
 	Describe("#CalculateNextUsablePorts", func() {
-		It("should calculate the first usable ports range", func() {
-			ports := reconciler.CalculateNextUsablePorts()
-			Expect(ports).To(Equal([]int{10101, 10102, 10103, 10104, 10105}))
+		It("should calculate usable ports range", func() {
+			ports, err := reconciler.CalculateNextUsablePorts()
+			Expect(err).To(Succeed())
+			Expect(ports).To(HaveLen(5))
+			allocatedPorts := map[int]struct{}{}
+			for i, p := range ports {
+				ExpectWithOffset(i, p).To(BeNumerically(">", 0))
+				ExpectWithOffset(i, p).To(BeNumerically("<", math.MaxUint16+1))
+				ExpectWithOffset(i, allocatedPorts).NotTo(HaveKey(p))
+				allocatedPorts[p] = struct{}{}
+			}
 		})
 
-		It("should calculate the second usable ports range", func() {
-			_ = reconciler.CalculateNextUsablePorts()
-			ports := reconciler.CalculateNextUsablePorts()
-			Expect(ports).To(Equal([]int{10106, 10107, 10108, 10109, 10110}))
+		It("should not have any overlap between two calls", func() {
+			ports, err := reconciler.CalculateNextUsablePorts()
+			Expect(err).To(Succeed())
+			allocatedPorts := map[int]struct{}{}
+			for i, p := range ports {
+				ExpectWithOffset(i, allocatedPorts).NotTo(HaveKey(p))
+				allocatedPorts[p] = struct{}{}
+			}
+			ports, err = reconciler.CalculateNextUsablePorts()
+			Expect(err).To(Succeed())
+			for i, p := range ports {
+				ExpectWithOffset(i, allocatedPorts).NotTo(HaveKey(p))
+			}
 		})
 	})
 })
