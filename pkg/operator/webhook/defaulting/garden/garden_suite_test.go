69e11d654d98e7c95daedc385f5984f8a7365b7f
Tim Usner <tim.usner@sap.com>
2025-03-06 09:04:21 +0100

    Add defaulting handler tests

---
+++ pkg/operator/webhook/defaulting/garden/garden_suite_test.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package garden_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGarden(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Operator Webhook Defaulting Garden Suite")
+}
