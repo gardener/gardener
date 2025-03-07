9d2f3241a0b98f58c70ad9ec8cbc3f0e486de241
Johann Gnaucke <johann@gnaucke.com>
2025-03-07 11:14:23 +0100

    address PR feedback

--- test/e2e/operator/garden/create_delete.go
+++ test/e2e/operator/garden/create_delete.go
@@ -31,91 +31,86 @@ import (
 )
 
 var _ = Describe("Garden Tests", Label("Garden", "default"), func() {
-	Describe("Create, Delete Garden", Label("simple"), func() {
-		test := func(s *GardenContext) {
-			ItShouldCreateGarden(s)
-			ItShouldWaitForGardenToBeReconciledAndHealthy(s)
-			ItShouldVerifyGardenManagedResourcesAndAwaitHealthiness(s)
-			ItShouldVerifyIstioManagedResourcesAndAwaitHealthiness(s)
-			ItShouldInitializeVirtualClusterClient(s)
-
-			It("Verify Gardener APIs availability", func(ctx SpecContext) {
-				Eventually(ctx, s.VirtualClusterKomega.List(&gardencorev1beta1.ShootList{})).Should(Succeed())
-				Eventually(ctx, s.VirtualClusterKomega.List(&seedmanagementv1alpha1.ManagedSeedList{})).Should(Succeed())
-				Eventually(ctx, s.VirtualClusterKomega.List(&settingsv1alpha1.ClusterOpenIDConnectPresetList{})).Should(Succeed())
-				Eventually(ctx, s.VirtualClusterKomega.List(&operationsv1alpha1.BastionList{})).Should(Succeed())
-			}, SpecTimeout(time.Minute))
-
-			It("Verify virtual cluster extension installations", func(ctx SpecContext) {
-				controllerRegistrationList := &gardencorev1beta1.ControllerRegistrationList{}
-				Eventually(ctx, s.VirtualClusterKomega.List(controllerRegistrationList)).Should(Succeed())
-				Expect(controllerRegistrationList.Items).To(ContainElement(MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("provider-local")})})))
-
-				controllerDeploymentList := &gardencorev1.ControllerDeploymentList{}
-				Eventually(ctx, s.VirtualClusterKomega.List(controllerDeploymentList)).Should(Succeed())
-				Expect(controllerDeploymentList.Items).To(ContainElement(MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("provider-local")})})))
-			}, SpecTimeout(time.Minute))
-
-			It("Verify 'gardener-system-public' namespace and 'gardener-info' configmap exist", func(ctx SpecContext) {
-				namespace := &corev1.Namespace{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: gardencorev1beta1.GardenerSystemPublicNamespace,
-					},
-				}
-				Eventually(ctx, s.VirtualClusterKomega.Get(namespace)).Should(Succeed())
-
-				configMap := &corev1.ConfigMap{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "gardener-info",
-						Namespace: gardencorev1beta1.GardenerSystemPublicNamespace,
-					},
-				}
-				Eventually(ctx, s.VirtualClusterKomega.Get(configMap)).Should(Succeed())
-				Expect(configMap.Data).To(HaveKey("gardenerAPIServer"))
-			}, SpecTimeout(time.Minute))
-
-			ItShouldDeleteGarden(s)
-			ItShouldWaitForGardenToBeDeleted(s)
-			ItShouldCleanUp(s)
-
-			It("Verify no leftover secrets", func(ctx SpecContext) {
-				secretList := &corev1.SecretList{}
-				Eventually(ctx, s.GardenKomega.List(secretList, client.InNamespace(v1beta1constants.GardenNamespace), client.MatchingLabels{
-					secretsmanager.LabelKeyManagedBy:       secretsmanager.LabelValueSecretsManager,
-					secretsmanager.LabelKeyManagerIdentity: operatorv1alpha1.SecretManagerIdentityOperator,
-				})).Should(Succeed())
-
-				Expect(secretList.Items).To(BeEmpty())
-			}, SpecTimeout(time.Minute))
-
-			It("Verify CRD still present", func(ctx SpecContext) {
-				crdList := &apiextensionsv1.CustomResourceDefinitionList{}
-				Eventually(ctx, s.GardenKomega.List(crdList)).Should(Succeed())
-				Expect(crdList.Items).To(ContainElement(MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("gardens.operator.gardener.cloud")})})))
-			}, SpecTimeout(time.Minute))
-
-			It("Verify gardener-resource-manager was deleted", func(ctx SpecContext) {
-				deployment := &appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      v1beta1constants.DeploymentNameGardenerResourceManager,
-						Namespace: v1beta1constants.GardenNamespace,
-					},
-				}
-
-				Eventually(ctx, s.GardenKomega.Get(deployment)).Should(BeNotFoundError())
-			}, SpecTimeout(time.Minute))
-
-			ItShouldWaitForExtensionToReportDeletion(s, "provider-local")
-		}
-
-		Context("Garden", Ordered, func() {
-			var s *GardenContext
-			BeforeTestSetup(func() {
-				backupSecret := defaultBackupSecret()
-				s = NewTestContext().ForGarden(defaultGarden(backupSecret, true), backupSecret)
-			})
-
-			test(s)
+	Describe("Create, Delete Garden", Label("simple"), Ordered, func() {
+		var s *GardenContext
+		BeforeTestSetup(func() {
+			backupSecret := defaultBackupSecret()
+			s = NewTestContext().ForGarden(defaultGarden(backupSecret, true), backupSecret)
 		})
+
+		ItShouldCreateGarden(s)
+		ItShouldWaitForGardenToBeReconciledAndHealthy(s)
+		ItShouldVerifyGardenManagedResourcesAndAwaitHealthiness(s)
+		ItShouldVerifyIstioManagedResourcesAndAwaitHealthiness(s)
+		ItShouldInitializeVirtualClusterClient(s)
+
+		It("Verify Gardener APIs availability", func(ctx SpecContext) {
+			Eventually(ctx, s.VirtualClusterKomega.List(&gardencorev1beta1.ShootList{})).Should(Succeed())
+			Eventually(ctx, s.VirtualClusterKomega.List(&seedmanagementv1alpha1.ManagedSeedList{})).Should(Succeed())
+			Eventually(ctx, s.VirtualClusterKomega.List(&settingsv1alpha1.ClusterOpenIDConnectPresetList{})).Should(Succeed())
+			Eventually(ctx, s.VirtualClusterKomega.List(&operationsv1alpha1.BastionList{})).Should(Succeed())
+		}, SpecTimeout(time.Minute))
+
+		It("Verify virtual cluster extension installations", func(ctx SpecContext) {
+			controllerRegistrationList := &gardencorev1beta1.ControllerRegistrationList{}
+			Eventually(ctx, s.VirtualClusterKomega.List(controllerRegistrationList)).Should(Succeed())
+			Expect(controllerRegistrationList.Items).To(ContainElement(MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("provider-local")})})))
+
+			controllerDeploymentList := &gardencorev1.ControllerDeploymentList{}
+			Eventually(ctx, s.VirtualClusterKomega.List(controllerDeploymentList)).Should(Succeed())
+			Expect(controllerDeploymentList.Items).To(ContainElement(MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("provider-local")})})))
+		}, SpecTimeout(time.Minute))
+
+		It("Verify 'gardener-system-public' namespace and 'gardener-info' configmap exist", func(ctx SpecContext) {
+			namespace := &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: gardencorev1beta1.GardenerSystemPublicNamespace,
+				},
+			}
+			Eventually(ctx, s.VirtualClusterKomega.Get(namespace)).Should(Succeed())
+
+			configMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "gardener-info",
+					Namespace: gardencorev1beta1.GardenerSystemPublicNamespace,
+				},
+			}
+			Eventually(ctx, s.VirtualClusterKomega.Get(configMap)).Should(Succeed())
+			Expect(configMap.Data).To(HaveKey("gardenerAPIServer"))
+		}, SpecTimeout(time.Minute))
+
+		ItShouldDeleteGarden(s)
+		ItShouldWaitForGardenToBeDeleted(s)
+		ItShouldCleanUp(s)
+
+		It("Verify no leftover secrets", func(ctx SpecContext) {
+			secretList := &corev1.SecretList{}
+			Eventually(ctx, s.GardenKomega.List(secretList, client.InNamespace(v1beta1constants.GardenNamespace), client.MatchingLabels{
+				secretsmanager.LabelKeyManagedBy:       secretsmanager.LabelValueSecretsManager,
+				secretsmanager.LabelKeyManagerIdentity: operatorv1alpha1.SecretManagerIdentityOperator,
+			})).Should(Succeed())
+
+			Expect(secretList.Items).To(BeEmpty())
+		}, SpecTimeout(time.Minute))
+
+		It("Verify CRD still present", func(ctx SpecContext) {
+			crdList := &apiextensionsv1.CustomResourceDefinitionList{}
+			Eventually(ctx, s.GardenKomega.List(crdList)).Should(Succeed())
+			Expect(crdList.Items).To(ContainElement(MatchFields(IgnoreExtras, Fields{"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("gardens.operator.gardener.cloud")})})))
+		}, SpecTimeout(time.Minute))
+
+		It("Verify gardener-resource-manager was deleted", func(ctx SpecContext) {
+			deployment := &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      v1beta1constants.DeploymentNameGardenerResourceManager,
+					Namespace: v1beta1constants.GardenNamespace,
+				},
+			}
+
+			Eventually(ctx, s.GardenKomega.Get(deployment)).Should(BeNotFoundError())
+		}, SpecTimeout(time.Minute))
+
+		ItShouldWaitForExtensionToReportDeletion(s, "provider-local")
+
 	})
 })
