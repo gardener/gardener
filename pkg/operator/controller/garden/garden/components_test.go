0836f32930a34af1bb686f5f8e39b5a498deb4f2
oliver-goetz <o.goetz@sap.com>
2025-03-13 17:17:21 +0100

    Use wildcard certificate in virtual-garden if configured

---
+++ pkg/operator/controller/garden/garden/components_test.go
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package garden_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	operatorv1alpha1 "github.com/gardener/gardener/pkg/apis/operator/v1alpha1"
+	. "github.com/gardener/gardener/pkg/operator/controller/garden/garden"
+)
+
+var _ = Describe("Components", func() {
+	Describe("GetAPIServerSNIDomains", func() {
+		It("should return the correct SNI domains", func() {
+			domains := []string{"foo.bar", "bar.foo.bar", "foo.foo.bar", "foo.bar.foo.bar", "api.bar"}
+			sni := operatorv1alpha1.SNI{
+				DomainPatterns: []string{"api.bar", "*.foo.bar"},
+			}
+
+			Expect(GetAPIServerSNIDomains(domains, sni)).To(Equal([]string{"api.bar", "bar.foo.bar", "foo.foo.bar"}))
+		})
+	})
+})
