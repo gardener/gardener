b047da414f81bd4306e285faed4b443c6675595d
Luca Bernstein <luca.bernstein@sap.com>
2025-03-21 13:13:17 +0100

    Adapt CloudProfile architecture defaulting.
    Only default architecture fields if no capabilities are defined.

--- pkg/apis/core/v1beta1/defaults_cloudprofile.go
+++ pkg/apis/core/v1beta1/defaults_cloudprofile.go
@@ -10,6 +10,32 @@ import (
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 )
 
+// SetDefaults_CloudProfile sets default values for CloudProfile objects.
+func SetDefaults_CloudProfile(cloudProfile *CloudProfile) {
+	// If Capabilities are defined, no Architecture field defaults are required.
+	// The default value is instead defined by the value(s) defined in cloudProfile.Spec.Capabilities.architecture.
+	if len(cloudProfile.Spec.Capabilities) > 0 {
+		return
+	}
+
+	for imageIdx := range cloudProfile.Spec.MachineImages {
+		machineImage := &cloudProfile.Spec.MachineImages[imageIdx]
+
+		for versionIdx := range machineImage.Versions {
+			imageVersion := &machineImage.Versions[versionIdx]
+			if len(imageVersion.Architectures) == 0 {
+				imageVersion.Architectures = []string{v1beta1constants.ArchitectureAMD64}
+			}
+		}
+	}
+	for i := range cloudProfile.Spec.MachineTypes {
+		machineType := &cloudProfile.Spec.MachineTypes[i]
+		if machineType.Architecture == nil {
+			machineType.Architecture = ptr.To(v1beta1constants.ArchitectureAMD64)
+		}
+	}
+}
+
 // SetDefaults_MachineImage sets default values for MachineImage objects.
 func SetDefaults_MachineImage(obj *MachineImage) {
 	if obj.UpdateStrategy == nil {
@@ -27,18 +53,10 @@ func SetDefaults_MachineImageVersion(obj *MachineImageVersion) {
 			},
 		}
 	}
-
-	if len(obj.Architectures) == 0 {
-		obj.Architectures = []string{v1beta1constants.ArchitectureAMD64}
-	}
 }
 
 // SetDefaults_MachineType sets default values for MachineType objects.
 func SetDefaults_MachineType(obj *MachineType) {
-	if obj.Architecture == nil {
-		obj.Architecture = ptr.To(v1beta1constants.ArchitectureAMD64)
-	}
-
 	if obj.Usable == nil {
 		obj.Usable = ptr.To(true)
 	}
