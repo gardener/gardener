a962db38eeec396e7e3f426e86b0ffb8e4fed432
Dominik Froehlich <dominik.froehlich@sap.com>
2025-04-01 10:00:39 +0200

    test: add test for DeployVPNShoot

--- pkg/gardenlet/operation/botanist/vpnshoot_test.go
+++ pkg/gardenlet/operation/botanist/vpnshoot_test.go
@@ -5,6 +5,8 @@
 package botanist_test
 
 import (
+	"context"
+	"errors"
 	"net"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -14,6 +16,7 @@ import (
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	kubernetesmock "github.com/gardener/gardener/pkg/client/kubernetes/mock"
+	mockvpnshoot "github.com/gardener/gardener/pkg/component/networking/vpn/shoot/mock"
 	"github.com/gardener/gardener/pkg/gardenlet/operation"
 	. "github.com/gardener/gardener/pkg/gardenlet/operation/botanist"
 	seedpkg "github.com/gardener/gardener/pkg/gardenlet/operation/seed"
@@ -70,6 +73,47 @@ var _ = Describe("VPNShoot", func() {
 			vpnShoot, err := botanist.DefaultVPNShoot()
 			Expect(vpnShoot).NotTo(BeNil())
 			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("#DeployVPNShoot", func() {
+		var (
+			vpnShoot *mockvpnshoot.MockInterface
+			ctx      = context.TODO()
+			fakeErr  = errors.New("fake err")
+		)
+
+		BeforeEach(func() {
+			vpnShoot = mockvpnshoot.NewMockInterface(ctrl)
+
+			botanist.Shoot = &shootpkg.Shoot{
+				Components: &shootpkg.Components{
+					SystemComponents: &shootpkg.SystemComponents{
+						VPNShoot: vpnShoot,
+					},
+				},
+				Networks: &shootpkg.Networks{
+					Pods:     []net.IPNet{{IP: []byte("192.168.0.0"), Mask: []byte("16")}},
+					Services: []net.IPNet{{IP: []byte("10.0.0.0"), Mask: []byte("24")}},
+					Nodes:    []net.IPNet{{IP: []byte("10.181.0.0"), Mask: []byte("16")}},
+				},
+			}
+		})
+
+		BeforeEach(func() {
+			vpnShoot.EXPECT().SetNodeNetworkCIDRs(botanist.Shoot.Networks.Nodes)
+			vpnShoot.EXPECT().SetServiceNetworkCIDRs(botanist.Shoot.Networks.Services)
+			vpnShoot.EXPECT().SetPodNetworkCIDRs(botanist.Shoot.Networks.Pods)
+		})
+
+		It("should set the network ranges and deploy", func() {
+			vpnShoot.EXPECT().Deploy(ctx)
+			Expect(botanist.DeployVPNShoot(ctx)).To(Succeed())
+		})
+
+		It("should fail when the deploy function fails", func() {
+			vpnShoot.EXPECT().Deploy(ctx).Return(fakeErr)
+			Expect(botanist.DeployVPNShoot(ctx)).To(Equal(fakeErr))
 		})
 	})
 })
