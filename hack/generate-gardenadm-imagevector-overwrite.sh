1c663b9a6256b9eadf79f3ef871d7091d0d11c91
rfranzke <rafael.franzke@sap.com>
2025-04-01 15:45:59 +0200

    Build `gardener-resource-manager` in `skaffold-gardenadm.yaml`

---
+++ hack/generate-gardenadm-imagevector-overwrite.sh
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -o pipefail
+
+source $(dirname "${0}")/lockfile.sh
+acquire_lockfile "/tmp/generate-gardenadm-imagevector-overwrite.sh.lock"
+
+dir="$(dirname $0)/../example/gardenadm-local"
+patch_file="$dir/.imagevector-overwrite.yaml"
+image_name="$1"
+ref="$SKAFFOLD_IMAGE"
+
+if [[ ! -f "$patch_file" ]]; then
+  cat <<EOF > "$patch_file"
+images: []
+EOF
+fi
+
+images="$(yq e '.' "$patch_file" | yq -o json)"
+
+images="$(echo "$images" | jq -r \
+  --arg name "$image_name" \
+  --arg ref "$ref" \
+  '.images |= if any(.name == $name) then
+      map(if .name == $name then .ref = $ref else . end)
+    else
+      . + [{name: $name, ref: $ref}] end' |\
+ yq eval -P)"
+
+yq eval ". = \"$images\"" -i "$patch_file"
