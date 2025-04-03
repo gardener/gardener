41b5e9c99a54f57050b1c623d609d0f12c6c21f5
rfranzke <rafael.franzke@sap.com>
2025-04-03 13:51:26 +0200

    Implement dummy `Secret`/`WorkloadIdentity` validator for provider-local
    for local testing
    
    Co-Authored-By: Tim Usner <tim.usner@sap.com>

---
+++ pkg/provider-local/admission/validator/secret.go
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package validator
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
+)
+
+type secret struct{}
+
+// NewSecretValidator returns a new instance of a secret validator.
+func NewSecretValidator() extensionswebhook.Validator {
+	return &secret{}
+}
+
+// Validate checks whether the data is empty.
+func (s *secret) Validate(_ context.Context, newObj, oldObj client.Object) error {
+	secret, ok := newObj.(*corev1.Secret)
+	if !ok {
+		return fmt.Errorf("wrong object type %T", newObj)
+	}
+
+	if oldObj != nil {
+		oldSecret, ok := oldObj.(*corev1.Secret)
+		if !ok {
+			return fmt.Errorf("wrong object type %T for old object", oldObj)
+		}
+
+		if apiequality.Semantic.DeepEqual(secret.Data, oldSecret.Data) {
+			return nil
+		}
+	}
+
+	if len(secret.Data) != 0 {
+		return fmt.Errorf("secret data should be empty")
+	}
+
+	return nil
+}
--- pkg/provider-local/admission/validator/webhook.go
+++ pkg/provider-local/admission/validator/webhook.go
@@ -5,18 +5,22 @@
 package validator
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	"github.com/gardener/gardener/pkg/apis/core"
+	securityv1alpha1 "github.com/gardener/gardener/pkg/apis/security/v1alpha1"
 	"github.com/gardener/gardener/pkg/provider-local/local"
 )
 
 const (
 	// Name is a name for a validation webhook.
 	Name = "validator"
+	// SecretsValidatorName is the name of the secrets validator.
+	SecretsValidatorName = "secrets." + Name
 )
 
 var logger = log.Log.WithName("local-validator-webhook")
@@ -31,10 +35,29 @@ func New(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
 		Path:     "/webhooks/validate",
 		Validators: map[extensionswebhook.Validator][]extensionswebhook.Type{
 			NewNamespacedCloudProfileValidator(mgr): {{Obj: &core.NamespacedCloudProfile{}}},
+			NewWorkloadIdentityValidator():          {{Obj: &securityv1alpha1.WorkloadIdentity{}}},
 		},
 		Target: extensionswebhook.TargetSeed,
 		ObjectSelector: &metav1.LabelSelector{
-			MatchLabels: map[string]string{"provider.extensions.gardener.cloud/local": "true"},
+			MatchLabels: map[string]string{"provider.extensions.gardener.cloud/" + local.Type: "true"},
+		},
+	})
+}
+
+// NewSecretsWebhook creates a new validation webhook for Secrets.
+func NewSecretsWebhook(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
+	logger.Info("Setting up webhook", "name", SecretsValidatorName)
+
+	return extensionswebhook.New(mgr, extensionswebhook.Args{
+		Provider: local.Type,
+		Name:     SecretsValidatorName,
+		Path:     "/webhooks/validate/secrets",
+		Validators: map[extensionswebhook.Validator][]extensionswebhook.Type{
+			NewSecretValidator(): {{Obj: &corev1.Secret{}}},
+		},
+		Target: extensionswebhook.TargetSeed,
+		ObjectSelector: &metav1.LabelSelector{
+			MatchLabels: map[string]string{"provider.shoot.gardener.cloud/" + local.Type: "true"},
 		},
 	})
 }
