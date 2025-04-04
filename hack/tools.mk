2a9c566f89a6a16e654193945250f60755c560b9
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-04-04 11:42:18 +0200

    Update dependency securego/gosec to v2.22.3 (#11815)

--- hack/tools.mk
+++ hack/tools.mk
@@ -56,7 +56,7 @@ TYPOS                      := $(TOOLS_BIN_DIR)/typos
 # renovate: datasource=github-releases depName=golangci/golangci-lint
 GOLANGCI_LINT_VERSION ?= v2.0.2
 # renovate: datasource=github-releases depName=securego/gosec
-GOSEC_VERSION ?= v2.22.2
+GOSEC_VERSION ?= v2.22.3
 # renovate: datasource=github-releases depName=joelanford/go-apidiff
 GO_APIDIFF_VERSION ?= v0.8.2
 # renovate: datasource=github-releases depName=google/addlicense
