aea8dfc6e80db1f46b1f5709f5bf4f7a255f9812
Shafeeque E S <shafeeque.e.s@sap.com>
2025-04-08 23:50:44 +0530

    Add a new controller in gardenlet to update Shoot status manual in-place pending workers

---
+++ pkg/gardenlet/controller/shoot/status/reconciler.go
@@ -0,0 +1,163 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package status
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	v1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/controllerutils"
+	gardenletconfigv1alpha1 "github.com/gardener/gardener/pkg/gardenlet/apis/config/v1alpha1"
+	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
+	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
+)
+
+// Reconciler updates the Shoot status with Manual In-Place pending workers from the Worker extension status.
+type Reconciler struct {
+	GardenClient client.Client
+	SeedClient   client.Client
+	Config       gardenletconfigv1alpha1.ShootStatusControllerConfiguration
+	SeedName     string
+}
+
+// Reconcile updates the Shoot status with Manual In-Place pending workers from the Worker extension status.
+func (r *Reconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	log := logf.FromContext(ctx)
+
+	ctx, cancel := controllerutils.GetMainReconciliationContext(ctx, controllerutils.DefaultReconciliationTimeout)
+	defer cancel()
+
+	shoot := &gardencorev1beta1.Shoot{}
+	if err := r.GardenClient.Get(ctx, request.NamespacedName, shoot); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.V(1).Info("Object is gone, stop reconciling")
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, fmt.Errorf("error retrieving object from store: %w", err)
+	}
+
+	// if shoot got deleted or is no longer managed by this gardenlet (e.g., due to migration to another seed) then don't requeue
+	if shoot.DeletionTimestamp != nil || ptr.Deref(shoot.Spec.SeedName, "") != r.SeedName {
+		log.Info("Shoot is being deleted or is no longer managed by this gardenlet, stop reconciling", "shoot", shoot.Name)
+		return reconcile.Result{}, nil
+	}
+
+	worker := &extensionsv1alpha1.Worker{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      shoot.Name,
+			Namespace: v1beta1helper.ControlPlaneNamespaceForShoot(shoot),
+		},
+	}
+	if err := r.SeedClient.Get(ctx, client.ObjectKeyFromObject(worker), worker); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.V(1).Info("Worker is gone, nothing to update", "worker", worker.Name)
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, fmt.Errorf("error retrieving object from store: %w", err)
+	}
+
+	if worker.Status.InPlaceUpdates == nil || worker.Status.InPlaceUpdates.WorkerPoolToHashMap == nil {
+		// This means that the worker is not reconciled yet or doesn't have in-place update workers, nothing to do here
+		return reconcile.Result{}, nil
+	}
+
+	// If there are no manual in-place update workers in the shoot status, then nothing to do
+	if shoot.Status.InPlaceUpdates == nil || shoot.Status.InPlaceUpdates.PendingWorkerUpdates == nil || len(shoot.Status.InPlaceUpdates.PendingWorkerUpdates.ManualInPlaceUpdate) == 0 {
+		return reconcile.Result{}, nil
+	}
+
+	var (
+		manualInPlacePendingWorkers  = sets.New[string]()
+		inPlaceUpdatesWorkerPoolHash = worker.Status.InPlaceUpdates.WorkerPoolToHashMap
+	)
+
+	for _, pool := range shoot.Spec.Provider.Workers {
+		if !v1beta1helper.IsUpdateStrategyManualInPlace(pool.UpdateStrategy) {
+			continue
+		}
+
+		var (
+			kubernetesVersion    = shoot.Spec.Kubernetes.Version
+			kubeletConfiguration = shoot.Spec.Kubernetes.Kubelet
+		)
+
+		if pool.Kubernetes != nil {
+			if pool.Kubernetes.Version != nil {
+				kubernetesVersion = *pool.Kubernetes.Version
+			}
+
+			if pool.Kubernetes.Kubelet != nil {
+				kubeletConfiguration = pool.Kubernetes.Kubelet
+			}
+		}
+
+		shootWorkerPoolHash, err := gardenerutils.CalculateWorkerPoolHashForInPlaceUpdate(
+			pool.Name,
+			&kubernetesVersion,
+			kubeletConfiguration,
+			ptr.Deref(pool.Machine.Image.Version, ""),
+			shoot.Status.Credentials,
+		)
+		if err != nil {
+			return reconcile.Result{}, fmt.Errorf("failed to calculate worker pool %q hash: %w", pool.Name, err)
+		}
+
+		// If the pool is not at all present in the worker status or the hash is different, then add it to the manual in-place pending workers
+		if workerStatusWorkerPoolHash, ok := inPlaceUpdatesWorkerPoolHash[pool.Name]; !ok || workerStatusWorkerPoolHash != shootWorkerPoolHash {
+			manualInPlacePendingWorkers.Insert(pool.Name)
+		}
+	}
+
+	// If both the slices are equal, then nothing to do here
+	if sets.New(shoot.Status.InPlaceUpdates.PendingWorkerUpdates.ManualInPlaceUpdate...).Equal(manualInPlacePendingWorkers) {
+		log.Info("Manual in-place pending workers are already up-to-date")
+		return reconcile.Result{}, nil
+	}
+
+	patch := client.StrategicMergeFrom(shoot.DeepCopy())
+	shoot.Status.InPlaceUpdates.PendingWorkerUpdates.ManualInPlaceUpdate = slices.DeleteFunc(shoot.Status.InPlaceUpdates.PendingWorkerUpdates.ManualInPlaceUpdate, func(pool string) bool {
+		return !manualInPlacePendingWorkers.Has(pool)
+	})
+
+	if len(shoot.Status.InPlaceUpdates.PendingWorkerUpdates.ManualInPlaceUpdate) == 0 {
+		shoot.Status.InPlaceUpdates.PendingWorkerUpdates.ManualInPlaceUpdate = nil
+
+		if len(shoot.Status.InPlaceUpdates.PendingWorkerUpdates.AutoInPlaceUpdate) == 0 {
+			shoot.Status.InPlaceUpdates.PendingWorkerUpdates = nil
+		}
+	}
+
+	if shoot.Status.InPlaceUpdates.PendingWorkerUpdates == nil {
+		shoot.Status.InPlaceUpdates = nil
+	}
+
+	log.Info("Updating Shoot status with manual in-place pending workers", "shoot", shoot.Name, "manualInPlacePendingWorkers", sets.List(manualInPlacePendingWorkers))
+	if err := r.GardenClient.Status().Patch(ctx, shoot, patch); err != nil {
+		return reconcile.Result{}, fmt.Errorf("failed to patch Shoot status: %w", err)
+	}
+
+	if shoot.Status.InPlaceUpdates == nil && kubernetesutils.HasMetaDataAnnotation(shoot, v1beta1constants.GardenerOperation, v1beta1constants.ShootOperationForceInPlaceUpdate) {
+		patch := client.MergeFrom(shoot.DeepCopy())
+		delete(shoot.Annotations, v1beta1constants.GardenerOperation)
+		if err := r.GardenClient.Patch(ctx, shoot, patch); err != nil {
+			return reconcile.Result{}, fmt.Errorf("failed to patch Shoot: %w", err)
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
