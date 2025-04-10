6ab63982450c0a1fe7550285e098dde66b8a7a22
Tim Usner <tim.usner@sap.com>
2025-04-10 16:21:26 +0200

    Handle unset `provider` field in `SecretBinding`s

--- pkg/apis/core/helper/secretbinding.go
+++ pkg/apis/core/helper/secretbinding.go
@@ -12,5 +12,8 @@ import (
 
 // GetSecretBindingTypes returns the SecretBinding provider types.
 func GetSecretBindingTypes(secretBinding *core.SecretBinding) []string {
+	if secretBinding.Provider == nil {
+		return []string{}
+	}
 	return strings.Split(secretBinding.Provider.Type, ",")
 }
