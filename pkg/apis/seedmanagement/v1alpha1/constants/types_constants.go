61a408fb54c0449ff43d9942e3288d903b1a62bd
Shafeeque E S <shafeeque.e.s@sap.com>
2025-04-17 13:12:54 +0530

    Comment formatting

--- pkg/apis/seedmanagement/v1alpha1/constants/types_constants.go
+++ pkg/apis/seedmanagement/v1alpha1/constants/types_constants.go
@@ -6,6 +6,6 @@ package constants
 
 const (
 	// AnnotationProtectFromDeletion is a constant for an annotation on a replica of a ManagedSeedSet
-	//(either ManagedSeed or Shoot) to protect it from deletion..
+	// (either ManagedSeed or Shoot) to protect it from deletion..
 	AnnotationProtectFromDeletion = "seedmanagement.gardener.cloud/protect-from-deletion"
 )
