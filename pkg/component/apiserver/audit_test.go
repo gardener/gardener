48e2cebb3205b1f2c43ed18289b02a00c98906d7
vpnachev <vladimir.nachev@sap.com>
2025-04-24 17:14:25 +0300

    Gardener no longer sets by default the `--audit-log-(path|maxsize|maxbackups)` flags

--- pkg/component/apiserver/audit_test.go
+++ pkg/component/apiserver/audit_test.go
@@ -187,9 +187,6 @@ rules:
 							Containers: []corev1.Container{{
 								Args: []string{
 									"--audit-policy-file=/etc/kubernetes/audit/audit-policy.yaml",
-									"--audit-log-path=/tmp/audit/audit.log",
-									"--audit-log-maxsize=100",
-									"--audit-log-maxbackup=5",
 								},
 								VolumeMounts: []corev1.VolumeMount{{
 									Name:      "audit-policy-config",
