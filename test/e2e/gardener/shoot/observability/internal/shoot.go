dd43f56953ed702d70c12cc7da47bef70c9c52c9
Radoslav Hubenov <rrhubenov@gmail.com>
2025-04-30 15:08:02 +0300

    Ensure log count is correct

--- test/e2e/gardener/shoot/observability/internal/shoot.go
+++ test/e2e/gardener/shoot/observability/internal/shoot.go
@@ -16,9 +16,27 @@ import (
 	"github.com/gardener/gardener/test/utils/shoots/logging"
 )
 
-// TODO(Rado): Do we need this to check if the received log count is correct?
-// TODO(Rado): Current Vali query is 'query=count_over_time({<key>=\"<value>\"}[1h])' which returns the count of logs in the last hour.
-// We might want to change it.
+func ItShouldWaitForLogsCountWithLabelToBeInVali(s *ShootContext, valiLabels map[string]string, key, value string, count int) {
+	GinkgoHelper()
+
+	It("Wait for logs with label to appear in Vali", func(ctx SpecContext) {
+		Eventually(ctx, func() error {
+			searchResponse, err := logging.GetValiLogs(ctx, valiLabels, s.ControlPlaneNamespace, key, value, s.SeedClientSet)
+			if err != nil {
+				return err
+			}
+
+			logCount := logging.GetLogCountFromSearchResponse(searchResponse)
+
+			if logCount != count {
+				return fmt.Errorf("expected %d logs in Vali for %s=%s, but got %d", count, key, value, logCount)
+			}
+
+			return nil
+		}).Should(Succeed())
+	}, SpecTimeout(time.Minute))
+}
+
 func ItShouldWaitForLogsWithLabelToBeInVali(s *ShootContext, valiLabels map[string]string, key, value string) {
 	GinkgoHelper()
 
@@ -29,43 +47,37 @@ func ItShouldWaitForLogsWithLabelToBeInVali(s *ShootContext, valiLabels map[stri
 				return err
 			}
 
-			logCount, err := logging.GetLogCountFromSearchResponse(searchResponse)
-			if err != nil {
-				return err
-			}
+			logCount := logging.GetLogCountFromSearchResponse(searchResponse)
 
 			if logCount == 0 {
-				return fmt.Errorf("no logs found for %s=%s", key, value)
+				return fmt.Errorf("no logs in Vali for %s=%s", key, value)
 			}
 
 			return nil
 		}).Should(Succeed())
-	}, SpecTimeout(time.Second*30))
+	}, SpecTimeout(time.Minute))
 }
 
-func ItShouldWaitForLogsWithLabelToNotBeInVali(s *ShootContext, valiLabels map[string]string, key, value string) {
-	GinkgoHelper()
-
-	It("Ensure logs do not exist", func(ctx SpecContext) {
-		// No easy way to guarantee that a log won't eventually be in Vali except waiting.
-		time.Sleep(10 * time.Second)
-
-		Eventually(ctx, func() error {
-			searchResponse, err := logging.GetValiLogs(ctx, valiLabels, s.ControlPlaneNamespace, key, value, s.SeedClientSet)
-			if err != nil {
-				return err
-			}
-
-			logCount, err := logging.GetLogCountFromSearchResponse(searchResponse)
-			if err != nil {
-				return err
-			}
-
-			if logCount > 0 {
-				return fmt.Errorf("found logs in Vali for %s=%s when they were unexpected", key, value)
-			}
-
-			return nil
-		}).Should(Succeed())
-	}, SpecTimeout(time.Second*30))
-}
+// func ItShouldWaitForLogsWithLabelToNotBeInVali(s *ShootContext, valiLabels map[string]string, key, value string) {
+// 	GinkgoHelper()
+//
+// 	It("Ensure logs do not exist", func(ctx SpecContext) {
+// 		// No easy way to guarantee that a log won't eventually be in Vali except waiting.
+// 		time.Sleep(10 * time.Second)
+//
+// 		Eventually(ctx, func() error {
+// 			searchResponse, err := logging.GetValiLogs(ctx, valiLabels, s.ControlPlaneNamespace, key, value, s.SeedClientSet)
+// 			if err != nil {
+// 				return err
+// 			}
+//
+// 			logCount := logging.GetLogCountFromSearchResponse(searchResponse)
+//
+// 			if logCount > 0 {
+// 				return fmt.Errorf("found logs in Vali for %s=%s when they were unexpected", key, value)
+// 			}
+//
+// 			return nil
+// 		}).Should(Succeed())
+// 	}, SpecTimeout(time.Minute))
+// }
--- test/utils/shoots/logging/logging.go
+++ test/utils/shoots/logging/logging.go
@@ -1,17 +1,11 @@
 package logging
 
 import (
-	"bufio"
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"strconv"
-
-	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
-
-	"github.com/hashicorp/go-multierror"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -22,7 +16,6 @@ import (
 	"github.com/gardener/gardener/pkg/utils/retry"
 
 	"github.com/gardener/gardener/test/framework"
-	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -34,80 +27,47 @@ const (
 	valiPort    = 3100
 )
 
-func GetLogCountFromSearchResponse(search *framework.SearchResponse) (int, error) {
-	var totalLogs int
+// SearchResponse represents the response from a search query to vali
+type SearchResponse struct {
+	Data struct {
+		Result []struct {
+			Values []any `json:"values"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
+func GetLogCountFromSearchResponse(search *SearchResponse) int {
+	total := 0
+	fmt.Println("Number of results:", len(search.Data.Result))
 	for _, result := range search.Data.Result {
-		currentStr, ok := result.Value[1].(string)
-		if !ok {
-			return totalLogs, fmt.Errorf("Data.Result.Value[1] is not a string")
-		}
-		current, err := strconv.Atoi(currentStr)
-		if err != nil {
-			return totalLogs, fmt.Errorf("Data.Result.Value[1] string is not parsable to integer")
-		}
-		totalLogs += current
+		total += len(result.Values)
 	}
-	return totalLogs, nil
+
+	return total
 }
 
 // GetValiLogs gets logs from the last 1 hour for <key>, <value> from the vali instance in <valiNamespace>
-func GetValiLogs(ctx context.Context, valiLabels map[string]string, valiNamespace, key, value string, client kubernetes.Interface) (*framework.SearchResponse, error) {
+func GetValiLogs(ctx context.Context, valiLabels map[string]string, valiNamespace, key, value string, client kubernetes.Interface) (*SearchResponse, error) {
 	valiLabelsSelector := labels.SelectorFromSet(valiLabels)
 
-	query := fmt.Sprintf("query=count_over_time({%s=~\"%s\"}[1h])", key, value)
+	query := fmt.Sprintf("query={%s=~\"%s\"}", key, value)
 
 	var stdout io.Reader
 	var err error
 	stdout, _, err = framework.PodExecByLabel(ctx, client, valiNamespace, valiLabelsSelector, valiLogging,
-		"wget", "http://localhost:"+strconv.Itoa(valiPort)+"/vali/api/v1/query", "-O-", "--post-data="+query,
+		"wget", "http://localhost:"+strconv.Itoa(valiPort)+"/vali/api/v1/query_range?limit=200", "-O-", "--post-data="+query,
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	search := &framework.SearchResponse{}
+	search := &SearchResponse{}
 
 	if err := json.NewDecoder(stdout).Decode(search); err != nil {
 		return nil, err
 	}
 
 	return search, nil
-}
-
-// DumpLogsForPodsWithLabelsInNamespace prints the logs of pods in the given namespace selected by the given list options.
-func DumpLogsForPodsWithLabelsInNamespace(ctx context.Context, k8sClient kubernetes.Interface, log logr.Logger, namespace string, opts ...client.ListOption) error {
-	pods := &corev1.PodList{}
-	opts = append(opts, client.InNamespace(namespace))
-	if err := k8sClient.Client().List(ctx, pods, opts...); err != nil {
-		return err
-	}
-
-	var result error
-	for _, pod := range pods.Items {
-		if err := DumpLogsForPodInNamespace(ctx, k8sClient, log, namespace, pod.Name, &corev1.PodLogOptions{}); err != nil {
-			result = multierror.Append(result, err)
-		}
-	}
-	return result
-}
-
-// DumpLogsForPodInNamespace prints the logs of the pod with the given namespace and name.
-func DumpLogsForPodInNamespace(ctx context.Context, k8sClient kubernetes.Interface, log logr.Logger, namespace, name string, options *corev1.PodLogOptions) error {
-	log = log.WithValues("pod", client.ObjectKey{Namespace: namespace, Name: name})
-	log.Info("Dumping logs for corev1.Pod")
-
-	podIf := k8sClient.Kubernetes().CoreV1().Pods(namespace)
-	logs, err := kubernetesutils.GetPodLogs(ctx, podIf, name, options)
-	if err != nil {
-		return err
-	}
-
-	scanner := bufio.NewScanner(bytes.NewReader(logs))
-	for scanner.Scan() {
-		log.Info(scanner.Text()) //nolint:logcheck
-	}
-
-	return nil
 }
 
 // This was copied from test/testmachinery/shoots/logging/utils.go
@@ -163,98 +123,6 @@ func EnsureValiLogsCount(ctx context.Context, interval time.Duration, shootFrame
 			shootFramework.Logger.Error(dumpError, "Error dumping logs for pod")
 		}
 	}
-
-	return err
-}
-
-func EnsureNoValiLogsFrameworkLess(ctx context.Context, log logr.Logger, valiLabels map[string]string, valiNamespace, key, value string, c kubernetes.Interface) error {
-	time.Sleep(10 * time.Second)
-
-	search, err := GetValiLogs(ctx, valiLabels, valiNamespace, key, value, c)
-	if err != nil {
-		return fmt.Errorf("error when trying to fetch logs from Vali: %w", err)
-	}
-
-	count, err := GetLogCountFromSearchResponse(search)
-	if err != nil {
-		return fmt.Errorf("error when trying to get log count from Vali: %w", err)
-	}
-
-	if count > 0 {
-		return fmt.Errorf("found logs in Vali for %s=%s when they were unexpected", key, value)
-	}
-
-	log = log.WithValues("key", key, "value", value)
-	log.Info("Did not receive logs for non-gardener pod. As expected.")
-	return nil
-}
-
-func EnsureNoValiLogs(ctx context.Context, shootFramework *framework.ShootFramework, valiLabels map[string]string, valiNamespace, key, value string, c kubernetes.Interface) error {
-	time.Sleep(10 * time.Second)
-
-	search, err := shootFramework.GetValiLogs(ctx, valiLabels, valiNamespace, key, value, c)
-	if err != nil {
-		return fmt.Errorf("error when trying to fetch logs from Vali: %w", err)
-	}
-
-	count, err := GetLogCountFromSearchResponse(search)
-	if err != nil {
-		return fmt.Errorf("error when trying to get log count from Vali: %w", err)
-	}
-
-	if count > 0 {
-		return fmt.Errorf("found logs in Vali for %s=%s when they were unexpected", key, value)
-	}
-
-	log := shootFramework.Logger.WithValues("key", key, "value", value)
-	log.Info("Did not receive logs for non-gardener pod. As expected.")
-	return nil
-}
-
-func EnsureValiLogsFrameworkLess(ctx context.Context, interval time.Duration, log logr.Logger, valiLabels map[string]string, valiNamespace, key, value string, c kubernetes.Interface) error {
-	err := retry.Until(ctx, interval, func(ctx context.Context) (done bool, err error) {
-		search, err := GetValiLogs(ctx, valiLabels, valiNamespace, key, value, c)
-		if err != nil {
-			return retry.SevereError(err)
-		}
-
-		count, err := GetLogCountFromSearchResponse(search)
-		if err != nil {
-			return retry.SevereError(fmt.Errorf("error when trying to get log count from Vali: %w", err))
-		}
-
-		if count == 0 {
-			return retry.MinorError(fmt.Errorf("no logs found in Vali for %s=%s", key, value))
-		}
-
-		log = log.WithValues("key", key, "value", value)
-		log.Info("Received logs")
-		return retry.Ok()
-	})
-
-	return err
-}
-
-func EnsureValiLogs(ctx context.Context, interval time.Duration, shootFramework *framework.ShootFramework, valiLabels map[string]string, valiNamespace, key, value string, c kubernetes.Interface) error {
-	err := retry.Until(ctx, interval, func(ctx context.Context) (done bool, err error) {
-		search, err := shootFramework.GetValiLogs(ctx, valiLabels, valiNamespace, key, value, c)
-		if err != nil {
-			return retry.SevereError(err)
-		}
-
-		count, err := GetLogCountFromSearchResponse(search)
-		if err != nil {
-			return retry.SevereError(fmt.Errorf("error when trying to get log count from Vali: %w", err))
-		}
-
-		if count == 0 {
-			return retry.MinorError(fmt.Errorf("no logs found in Vali for %s=%s", key, value))
-		}
-
-		log := shootFramework.Logger.WithValues("key", key, "value", value)
-		log.Info("Received logs")
-		return retry.Ok()
-	})
 
 	return err
 }
