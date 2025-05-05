94530e4d0ecc1461f96051ab410df50487225bb6
Robert Kaussow <mail@thegeeklab.de>
2025-05-05 14:10:30 +0200

    add tests

--- pkg/component/shared/gardenerapiserver_test.go
+++ pkg/component/shared/gardenerapiserver_test.go
@@ -6,6 +6,7 @@ package shared_test
 
 import (
 	"context"
+	"time"
 
 	"github.com/Masterminds/semver/v3"
 	. "github.com/onsi/ginkgo/v2"
@@ -384,6 +385,23 @@ var _ = Describe("GardenerAPIServer", func() {
 				gardenerAPIServer, err := NewGardenerAPIServer(ctx, runtimeClient, namespace, objectMeta, runtimeVersion, sm, apiServerConfig, autoscalingConfig, auditWebhookConfig, topologyAwareRoutingEnabled, clusterIdentity, workloadIdentityTokenIssuer, &goAwayChance)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(gardenerAPIServer.GetValues().WatchCacheSizes).To(Equal(watchCacheSizes))
+			})
+		})
+
+		Describe("adminKubeconfigMaxExpiration", func() {
+			It("should set the field to nil by default", func() {
+				gardenerAPIServer, err := NewGardenerAPIServer(ctx, runtimeClient, namespace, objectMeta, runtimeVersion, sm, apiServerConfig, autoscalingConfig, auditWebhookConfig, topologyAwareRoutingEnabled, clusterIdentity, workloadIdentityTokenIssuer, &goAwayChance)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(gardenerAPIServer.GetValues().AdminKubeconfigMaxExpiration).To(BeNil())
+			})
+
+			It("should set the field to the configured values", func() {
+				adminKubeconfigMaxExpiration := ptr.To[metav1.Duration](metav1.Duration{Duration: 1 * time.Hour})
+				apiServerConfig = &operatorv1alpha1.GardenerAPIServerConfig{AdminKubeconfigMaxExpiration: adminKubeconfigMaxExpiration}
+
+				gardenerAPIServer, err := NewGardenerAPIServer(ctx, runtimeClient, namespace, objectMeta, runtimeVersion, sm, apiServerConfig, autoscalingConfig, auditWebhookConfig, topologyAwareRoutingEnabled, clusterIdentity, workloadIdentityTokenIssuer, &goAwayChance)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(gardenerAPIServer.GetValues().AdminKubeconfigMaxExpiration).To(Equal(adminKubeconfigMaxExpiration))
 			})
 		})
 
