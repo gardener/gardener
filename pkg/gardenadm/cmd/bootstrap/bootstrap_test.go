99ec6dcc8089b3a1df27cf6d2e5da5692c001355
Tim Ebert <timebertt@gmail.com>
2025-05-07 17:04:18 +0200

    Initialize client and perform safety check

--- pkg/gardenadm/cmd/bootstrap/bootstrap_test.go
+++ pkg/gardenadm/cmd/bootstrap/bootstrap_test.go
@@ -5,23 +5,37 @@
 package bootstrap_test
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	"github.com/spf13/cobra"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	fakekubernetes "github.com/gardener/gardener/pkg/client/kubernetes/fake"
 	"github.com/gardener/gardener/pkg/gardenadm/cmd"
 	. "github.com/gardener/gardener/pkg/gardenadm/cmd/bootstrap"
 	"github.com/gardener/gardener/pkg/logger"
+	"github.com/gardener/gardener/pkg/utils/test"
 	clitest "github.com/gardener/gardener/pkg/utils/test/cli"
 )
 
 var _ = Describe("Bootstrap", func() {
 	var (
+		ctx = context.Background()
+
 		globalOpts *cmd.Options
 		stdErr     *Buffer
 		command    *cobra.Command
+
+		fakeClient client.Client
+		clientSet  kubernetes.Interface
 	)
 
 	BeforeEach(func() {
@@ -29,15 +43,38 @@ var _ = Describe("Bootstrap", func() {
 		globalOpts.IOStreams, _, _, stdErr = clitest.NewTestIOStreams()
 		globalOpts.Log = logger.MustNewZapLogger(logger.DebugLevel, logger.FormatJSON, logzap.WriteTo(stdErr))
 		command = NewCommand(globalOpts)
+
+		fakeClient = fakeclient.NewClientBuilder().Build()
+		clientSet = fakekubernetes.NewClientSetBuilder().WithClient(fakeClient).Build()
+
+		DeferCleanup(test.WithVar(&NewClientSetFromFile, func(string) (kubernetes.Interface, error) { return clientSet, nil }))
 	})
 
 	Describe("#RunE", func() {
-		It("should return the expected output", func() {
+		BeforeEach(func() {
 			Expect(command.Flags().Set("kubeconfig", "some-path-to-kubeconfig")).To(Succeed())
+		})
 
+		It("should return the expected output", func() {
 			Expect(command.RunE(command, nil)).To(Succeed())
 
-			Eventually(stdErr).Should(Say("Not implemented as well"))
+			Eventually(stdErr).Should(Say("work in progress"))
+		})
+
+		Describe("safety check", func() {
+			It("should abort the execution if gardener-operator is deployed on the targeted cluster", func() {
+				Expect(fakeClient.Create(ctx, &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{Namespace: "garden", Name: "gardener-operator"},
+				})).To(Succeed())
+				Expect(command.RunE(command, nil)).To(MatchError(ContainSubstring(`deployment "garden/gardener-operator" exists on the targeted cluster`)))
+			})
+
+			It("should abort the execution if gardenlet is deployed on the targeted cluster", func() {
+				Expect(fakeClient.Create(ctx, &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{Namespace: "garden", Name: "gardenlet"},
+				})).To(Succeed())
+				Expect(command.RunE(command, nil)).To(MatchError(ContainSubstring(`deployment "garden/gardenlet" exists on the targeted cluster`)))
+			})
 		})
 	})
 })
