cfab46cf917af061974ccd9e46bfe3dc79723f1e
Johann Gnaucke <johann@gnaucke.com>
2025-05-09 09:35:35 +0200

    move operator bootstrappers to `pkg`

--- pkg/operator/bootstrappers/verify_version_test.go
+++ pkg/operator/bootstrappers/verify_version_test.go
@@ -16,9 +16,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
-	. "github.com/gardener/gardener/cmd/gardener-operator/app/bootstrappers"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	operatorv1alpha1 "github.com/gardener/gardener/pkg/apis/operator/v1alpha1"
+	. "github.com/gardener/gardener/pkg/operator/bootstrappers"
 	operatorclient "github.com/gardener/gardener/pkg/operator/client"
 	"github.com/gardener/gardener/pkg/utils/test"
 )
