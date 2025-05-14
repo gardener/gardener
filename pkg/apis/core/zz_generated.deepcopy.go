1f63d0f5bd270f4968543eb93ce7d7f3cfa52a8e
Theodora Tosheva <theodora.tosheva@sap.com>
2025-05-14 16:29:57 +0300

    Run `make generate`

--- pkg/apis/core/zz_generated.deepcopy.go
+++ pkg/apis/core/zz_generated.deepcopy.go
@@ -2711,6 +2711,11 @@ func (in *KubeletConfig) DeepCopyInto(out *KubeletConfig) {
 		*out = new(MemorySwapConfiguration)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.MaxParallelImagePulls != nil {
+		in, out := &in.MaxParallelImagePulls, &out.MaxParallelImagePulls
+		*out = new(int32)
+		**out = **in
+	}
 	return
 }
 
