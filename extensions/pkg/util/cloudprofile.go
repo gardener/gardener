d8c5042372b56baf331f16553498fde9f5f7f1c0
Johannes Roncossek <johannes.roncossek@sap.com>
2025-05-16 14:12:17 +0200

    Add extension utils for Capabilitiy validation

--- extensions/pkg/util/cloudprofile.go
+++ extensions/pkg/util/cloudprofile.go
@@ -5,8 +5,14 @@
 package util
 
 import (
+	"maps"
+	"slices"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
 	"github.com/gardener/gardener/pkg/apis/core"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/utils"
 )
 
@@ -66,4 +72,84 @@ func NewV1beta1ImagesContext(parentImages []gardencorev1beta1.MachineImage) *Ima
 			return utils.CreateMapFromSlice(mi.Versions, func(v gardencorev1beta1.MachineImageVersion) string { return v.Version })
 		},
 	)
+}
+
+// ValidateCapabilities validates the capabilities of a machine type or machine image against the capabilitiesDefinition
+// located in a cloud profile at spec.capabilities.
+// It checks if the capabilities are supported by the cloud profile and if the architecture is defined correctly.
+// It returns a list of field errors if any validation fails.
+func ValidateCapabilities(capabilities core.Capabilities, capabilitiesDefinition core.Capabilities, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	supportedCapabilityKeys := slices.Collect(maps.Keys(capabilitiesDefinition))
+
+	// Check if all capabilities are supported by the cloud profile
+	for capabilityKey, capability := range capabilities {
+		supportedValues, keyExists := capabilitiesDefinition[capabilityKey]
+		if !keyExists {
+			allErrs = append(allErrs, field.NotSupported(fldPath, capabilityKey, supportedCapabilityKeys))
+			continue
+		}
+		for i, value := range capability {
+			if !slices.Contains(supportedValues, value) {
+				allErrs = append(allErrs, field.NotSupported(fldPath.Child(capabilityKey).Index(i), value, supportedValues))
+			}
+		}
+	}
+
+	// Check additional requirements for architecture
+	// must be defined when multiple architectures are supported by the cloud profile
+	supportedArchitectures := capabilitiesDefinition[v1beta1constants.ArchitectureName]
+	architectures := capabilities[v1beta1constants.ArchitectureName]
+	if len(supportedArchitectures) > 1 && len(architectures) != 1 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child(v1beta1constants.ArchitectureName), architectures, "must define exactly one architecture when multiple architectures are supported by the cloud profile"))
+	}
+
+	return allErrs
+}
+
+// GetVersionCapabilitySets returns the CapabilitySets for a given machine image version and adds the default capabilitySet if applicable.
+func GetVersionCapabilitySets(version core.MachineImageVersion, capabilitiesDefinition core.Capabilities) []core.CapabilitySet {
+	versionCapabilitySets := version.CapabilitySets
+	if len(version.CapabilitySets) == 0 {
+		// It is allowed not to define capabilitySets in the machine image version if there is only one architecture
+		// if so the capabilityDefinition is used as default
+		if len(capabilitiesDefinition[v1beta1constants.ArchitectureName]) == 1 {
+			versionCapabilitySets = []core.CapabilitySet{{Capabilities: capabilitiesDefinition}}
+		}
+	}
+	return versionCapabilitySets
+}
+
+// AreCapabilitiesEqual checks if two capabilities are semantically equal.
+// It compares the keys and values of the capabilities maps.
+func AreCapabilitiesEqual(a, b, capabilitiesDefinition core.Capabilities) bool {
+	a = SetDefaultCapabilities(a, capabilitiesDefinition)
+	b = SetDefaultCapabilities(b, capabilitiesDefinition)
+	for key, valuesA := range a {
+		valuesB, exists := b[key]
+		if !exists || len(valuesA) != len(valuesB) {
+			return false
+		}
+		for _, value := range valuesA {
+			if !slices.Contains(valuesB, value) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// SetDefaultCapabilities sets the default capabilities based on a capabilitiesDefinition for a machine type or machine image.
+func SetDefaultCapabilities(capabilities, capabilitiesDefinition core.Capabilities) core.Capabilities {
+	if len(capabilities) == 0 {
+		capabilities = make(core.Capabilities)
+	}
+
+	for key, values := range capabilitiesDefinition {
+		if _, exists := capabilities[key]; !exists {
+			capabilities[key] = values
+		}
+	}
+
+	return capabilities
 }
