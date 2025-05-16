428d49a76bd8ad811096a6376a8dc9207c73b782
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-05-16 10:35:05 +0000

    Update dependency kubernetes/kubernetes to v1.33.1

--- hack/tools.mk
+++ hack/tools.mk
@@ -69,7 +69,7 @@ HELM_VERSION ?= v3.17.3
 # renovate: datasource=github-releases depName=kubernetes-sigs/kind
 KIND_VERSION ?= v0.27.0
 # renovate: datasource=github-releases depName=kubernetes/kubernetes
-KUBECTL_VERSION ?= v1.33.0
+KUBECTL_VERSION ?= v1.33.1
 # renovate: datasource=github-releases depName=kubernetes-sigs/kustomize
 KUSTOMIZE_VERSION ?= v5.3.0
 # renovate: datasource=github-releases depName=prometheus/prometheus
