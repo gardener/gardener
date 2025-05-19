24c7a23fa60e7ad08a541ca2bc9887e88d2951b7
Dominik Froehlich <dominik.froehlich@sap.com>
2025-05-19 11:24:47 +0200

    refactor: use one template for HA/non-HA VPN envoy

--- pkg/operator/controller/garden/garden/reconciler_reconcile.go
+++ pkg/operator/controller/garden/garden/reconciler_reconcile.go
@@ -847,6 +847,7 @@ func (r *Reconciler) deployKubeAPIServerFunc(garden *operatorv1alpha1.Garden, ku
 			nil,
 			services,
 			nil,
+			"",
 			shared.NormalizeResources(getKubernetesResourcesForEncryption(garden)),
 			utils.FilterEntriesByFilterFn(shared.NormalizeResources(garden.Status.EncryptedResources), operator.IsServedByKubeAPIServer),
 			helper.GetETCDEncryptionKeyRotationPhase(garden.Status.Credentials),
