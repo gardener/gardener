7f98f0e02074fc6165e21e2088e6c3ab2a38e35c
Radoslav Hubenov <rrhubenov@gmail.com>
2025-05-20 11:20:53 +0300

    operator crds

--- pkg/component/observability/opentelemetry/opentelemetryoperator/assets/doc.go
+++ pkg/component/observability/opentelemetry/opentelemetryoperator/assets/doc.go
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate ../../../../../../hack/generate-crds.sh -p crd- opentelemetry.io
+//go:generate ../../../../../../hack/generate-crds.sh -p crd- opentelemetry.io_v1alpha1
+//go:generate ../../../../../../hack/generate-crds.sh -p crd- opentelemetry.io_v1beta1
 
 package assets
