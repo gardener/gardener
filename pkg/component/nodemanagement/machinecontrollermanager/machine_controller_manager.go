62315f49194eb607f100f52efa382c5e805cf749
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2025-05-20 16:11:21 +0300

    machine-controller-manager: Increase cpu requests from 5m to 10m

--- pkg/component/nodemanagement/machinecontrollermanager/machine_controller_manager.go
+++ pkg/component/nodemanagement/machinecontrollermanager/machine_controller_manager.go
@@ -243,7 +243,7 @@ func (m *machineControllerManager) Deploy(ctx context.Context) error {
 					}},
 					Resources: corev1.ResourceRequirements{
 						Requests: corev1.ResourceList{
-							corev1.ResourceCPU:    resource.MustParse("5m"),
+							corev1.ResourceCPU:    resource.MustParse("10m"),
 							corev1.ResourceMemory: resource.MustParse("20M"),
 						},
 					},
