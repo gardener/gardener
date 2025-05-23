f1ad1d9d0cc066eedb5e6d2ca2ff44d06e6cd3ff
Radoslav Hubenov <rrhubenov@gmail.com>
2025-05-23 15:53:39 +0300

    Add forgottent `component.OpWait` to CRD deployment

--- pkg/gardenlet/controller/seed/seed/reconciler_reconcile.go
+++ pkg/gardenlet/controller/seed/seed/reconciler_reconcile.go
@@ -237,7 +237,7 @@ func (r *Reconciler) runReconcileSeedFlow(
 		})
 		deployOpenTelemetryCRD = g.Add(flow.Task{
 			Name:   "Deploy OpenTelemetry-related custom resource definitions",
-			Fn:     c.openTelemetryCRD.Deploy,
+			Fn:     component.OpWait(c.openTelemetryCRD).Deploy,
 			SkipIf: seedIsGarden,
 		})
 		syncPointCRDs = flow.NewTaskIDs(
