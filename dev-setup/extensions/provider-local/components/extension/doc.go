82d14d30a2d0adaf8415af3b0aca99890c0b07d1
rfranzke <rafael.franzke@sap.com>
2025-05-26 12:54:26 +0200

    Move `provider-local` `Controller{Registration,Deployment}` to `/dev-setup`
    Co-Authored-By: Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>

--- dev-setup/extensions/provider-local/components/extension/doc.go
+++ dev-setup/extensions/provider-local/components/extension/doc.go
@@ -4,4 +4,4 @@
 
 //go:generate extension-generator --name=provider-local --provider-type=local --component-category=provider-extension --component-category=operating-system --extension-oci-repository=local-skaffold/gardener-extension-provider-local/charts/extension:v0.0.0 --admission-runtime-oci-repository=local-skaffold/gardener-extension-admission-local/charts/runtime:v0.0.0 --admission-application-oci-repository=local-skaffold/gardener-extension-admission-local/charts/application:v0.0.0 --destination=$PWD/extension.yaml
 
-package operator
+package extension
