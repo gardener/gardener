ae2ea7fe5859016524c8cc2912a2a9f3b08a698e
Radoslav Hubenov <rrhubenov@gmail.com>
2025-05-27 17:09:00 +0300

    Increase timeout for garden operator integ. test

--- test/integration/operator/garden/garden/garden_test.go
+++ test/integration/operator/garden/garden/garden_test.go
@@ -568,7 +568,7 @@ spec:
 			etcdList := &druidcorev1alpha1.EtcdList{}
 			g.Expect(testClient.List(ctx, etcdList, client.InNamespace(testNamespace.Name))).To(Succeed())
 			return test.ObjectNames(etcdList)
-		}).Should(ConsistOf(
+		}).WithTimeout(20 * time.Second).Should(ConsistOf(
 			"virtual-garden-etcd-main",
 			"virtual-garden-etcd-events",
 		))
