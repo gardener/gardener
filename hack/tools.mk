eeea2298f352ba303c68b62c57290c3404591e68
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-05-30 11:17:44 +0200

    Update dependency protocolbuffers/protobuf to v31.1 (#12206)

--- hack/tools.mk
+++ hack/tools.mk
@@ -75,7 +75,7 @@ KUSTOMIZE_VERSION ?= v5.3.0
 # renovate: datasource=github-releases depName=prometheus/prometheus
 PROMTOOL_VERSION ?= 3.4.0
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf
-PROTOC_VERSION ?= v31.0
+PROTOC_VERSION ?= v31.1
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
 SKAFFOLD_VERSION ?= v2.16.0
 # renovate: datasource=github-releases depName=mikefarah/yq
