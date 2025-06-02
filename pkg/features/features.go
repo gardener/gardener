ab9580f10386040c7172dff0f4bf9da7378923a3
Dimitar Mirchev <dimitar.mirchev@sap.com>
2025-06-02 08:18:09 +0300

    Typo

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -99,7 +99,7 @@ const (
 	// Operators are responsible for providing the credentials for backup explicitly.
 	// Credentials that were already copied will be labeled with "secret.backup.gardener.cloud/status=previously-managed" and would have to be cleaned up by operators.
 	// owner: @dimityrmirchev
-	// alpha: v1.120.1
+	// alpha: v1.121.0
 	DoNotCopyBackupCredentials featuregate.Feature = "DoNotCopyBackupCredentials"
 )
 
