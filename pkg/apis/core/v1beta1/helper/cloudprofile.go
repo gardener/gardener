49b8239cd92c6b0eb9176a3baf8ce8f45f130a8b
Luca Bernstein <luca.bernstein@sap.com>
2025-06-05 09:02:16 +0200

    Adapt cloudprofile helpers
    Co-authored-by: Valentin Knabel <dev@vknabel.com>
    Co-authored-by: Gerrit <Gerrit91@users.noreply.github.com>
    Co-authored-by: Claus-Theodor Riegg <crigertg@users.noreply.github.com>
    Co-authored-by: Luca Bernstein <LucaBernstein@users.noreply.github.com>
    Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>

--- pkg/apis/core/v1beta1/helper/cloudprofile.go
+++ pkg/apis/core/v1beta1/helper/cloudprofile.go
@@ -13,25 +13,120 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/utils/ptr"
 
 	"github.com/gardener/gardener/pkg/api"
 	"github.com/gardener/gardener/pkg/apis/core"
+	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	versionutils "github.com/gardener/gardener/pkg/utils/version"
 )
 
-// CurrentLifecycleClassification returns the current lifecycle classification of the given version.
-// An empty classification is interpreted as supported. If the version is expired, it returns ClassificationExpired.
-func CurrentLifecycleClassification(version gardencorev1beta1.ExpirableVersion) gardencorev1beta1.VersionClassification {
-	var currentTime = time.Now()
+func CurrentLifecycleClassification(version v1beta1.ExpirableVersion) v1beta1.VersionClassification {
+	var (
+		currentClassification = v1beta1.ClassificationUnavailable
+		currentTime           = time.Now()
+	)
 
-	if version.ExpirationDate != nil && !currentTime.Before(version.ExpirationDate.Time) {
-		return gardencorev1beta1.ClassificationExpired
+	if version.Classification != nil || version.ExpirationDate != nil {
+		// old cloud profile definition, convert to lifecycle
+		// this can be removed as soon as we remove the old classification and expiration date fields
+
+		if version.Classification != nil {
+			version.Lifecycle = append(version.Lifecycle, v1beta1.LifecycleStage{
+				Classification: *version.Classification,
+			})
+		}
+
+		if version.ExpirationDate != nil {
+			if version.Classification == nil {
+				version.Lifecycle = append(version.Lifecycle, v1beta1.LifecycleStage{
+					Classification: v1beta1.ClassificationSupported,
+				})
+			}
+
+			version.Lifecycle = append(version.Lifecycle, v1beta1.LifecycleStage{
+				Classification: v1beta1.ClassificationExpired,
+				StartTime:      version.ExpirationDate,
+			})
+		}
 	}
 
-	return ptr.Deref(version.Classification, gardencorev1beta1.ClassificationSupported)
+	if len(version.Lifecycle) == 0 {
+		// when there is no classification lifecycle defined then default to supported
+		version.Lifecycle = append(version.Lifecycle, v1beta1.LifecycleStage{
+			Classification: v1beta1.ClassificationSupported,
+		})
+	}
+
+	for _, stage := range version.Lifecycle {
+		startTime := time.Time{}
+		if stage.StartTime != nil {
+			startTime = stage.StartTime.Time
+		}
+
+		if startTime.Before(currentTime) {
+			currentClassification = stage.Classification
+		}
+	}
+
+	return currentClassification
+}
+
+func VersionIsExpired(version v1beta1.ExpirableVersion) bool {
+	return CurrentLifecycleClassification(version) == v1beta1.ClassificationExpired
+}
+
+func VersionIsActive(version v1beta1.ExpirableVersion) bool {
+	curr := CurrentLifecycleClassification(version)
+	return curr != v1beta1.ClassificationExpired && curr != v1beta1.ClassificationUnavailable
+}
+
+func VersionIsSupported(version v1beta1.ExpirableVersion) bool {
+	return CurrentLifecycleClassification(version) == v1beta1.ClassificationSupported
+}
+
+func VersionIsPreview(version v1beta1.ExpirableVersion) bool {
+	return CurrentLifecycleClassification(version) == v1beta1.ClassificationPreview
+}
+
+func DurationUntilNextVersionLifecycleStage(cloudProfile *v1beta1.CloudProfileSpec) time.Duration {
+	var (
+		next time.Time
+		now  = time.Now()
+	)
+
+	for _, version := range cloudProfile.Kubernetes.Versions {
+		for _, stage := range version.Lifecycle {
+			if stage.StartTime == nil {
+				continue
+			}
+			time := stage.StartTime.Time
+			if now.Before(time) && next.IsZero() || next.After(time) {
+				next = time
+			}
+		}
+	}
+
+	for _, image := range cloudProfile.MachineImages {
+		for _, version := range image.Versions {
+
+			for _, stage := range version.Lifecycle {
+				if stage.StartTime == nil {
+					continue
+				}
+				time := stage.StartTime.Time
+				if now.Before(time) && next.After(time) {
+					next = time
+				}
+			}
+		}
+	}
+
+	if next.IsZero() {
+		return 0
+	}
+	return next.Sub(now)
 }
 
 // DetermineMachineImageForName finds the cloud specific machine images in the <cloudProfile> for the given <name> and
@@ -521,7 +616,7 @@ func FilterLowerVersion(currentSemVerVersion semver.Version) VersionPredicate {
 // returns true if it is expired
 func FilterExpiredVersion() func(expirableVersion gardencorev1beta1.ExpirableVersion, version *semver.Version) (bool, error) {
 	return func(expirableVersion gardencorev1beta1.ExpirableVersion, _ *semver.Version) (bool, error) {
-		return CurrentLifecycleClassification(expirableVersion) == gardencorev1beta1.ClassificationExpired, nil
+		return VersionIsExpired(expirableVersion), nil
 	}
 }
 
