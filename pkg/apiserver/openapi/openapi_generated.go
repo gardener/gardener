a1898ec38fb075cdf1c6622a9039686ed7963c08
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2025-06-10 15:07:28 +0300

    Improve the doc string of the EnableAnonymousAuthentication field (#12255)

--- pkg/apiserver/openapi/openapi_generated.go
+++ pkg/apiserver/openapi/openapi_generated.go
@@ -4651,7 +4651,7 @@ func schema_pkg_apis_core_v1beta1_KubeAPIServerConfig(ref common.ReferenceCallba
 					},
 					"enableAnonymousAuthentication": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Deprecated: This field is deprecated and will be removed in a future release. Please use anonymous authentication configuration instead. For more information see: https://kubernetes.io/docs/reference/access-authn-authz/authentication/#anonymous-authenticator-configuration EnableAnonymousAuthentication defines whether anonymous requests to the secure port of the API server should be allowed (flag `--anonymous-auth`). See: https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/",
+							Description: "EnableAnonymousAuthentication defines whether anonymous requests to the secure port of the API server should be allowed (flag `--anonymous-auth`). See: https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/\n\nDeprecated: This field is deprecated and will be removed in a future release. Please use anonymous authentication configuration instead. For more information see: https://kubernetes.io/docs/reference/access-authn-authz/authentication/#anonymous-authenticator-configuration",
 							Type:        []string{"boolean"},
 							Format:      "",
 						},
