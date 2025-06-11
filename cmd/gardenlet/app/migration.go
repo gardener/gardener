0fdcefb1b5c04f67334182d2bc0ea65331a484db
Victor Herrero Otal <victor.herrero.otal@sap.com>
2025-06-11 17:26:30 +0200

    Add annotation to avoid reiterating attempts to clean up
    This commit uses a temporal annotation to mark shoot Prometheus
    instances already cleaned up. This way, the gardenlet doesn't repeatedly
    attempt to clean up every shoot Prometheus on every restart.
    
    This temporal annotation will be removed in a second migration step.

--- cmd/gardenlet/app/migration.go
+++ cmd/gardenlet/app/migration.go
@@ -317,6 +317,10 @@ func cleanupPrometheusObsoleteFolders(ctx context.Context, log logr.Logger, seed
 				return nil
 			}
 
+			if value, ok := prometheus.Annotations[resourcesv1alpha1.PrometheusObsoleteFolderCleanedUp]; ok && value == "true" {
+				return nil
+			}
+
 			log.Info("Clean up obsolete Prometheus folders", "cluster", cluster.Name)
 
 			if needsInitContainer(prometheus) {
@@ -343,6 +347,17 @@ func cleanupPrometheusObsoleteFolders(ctx context.Context, log logr.Logger, seed
 
 			if err := waitUntilReady(ctx, cluster.Name); err != nil {
 				log.Error(err, "Prometheus statefulset did not become healthy", "cluster", cluster.Name)
+			}
+
+			prometheus, prometheusPatch, err = getPrometheusWithPatch(ctx, cluster.Name)
+			if err != nil {
+				log.Error(err, "Failed to get Prometheus resource to mark it as cleaned up", "cluster", cluster.Name)
+				return nil
+			}
+
+			prometheus.Annotations[resourcesv1alpha1.PrometheusObsoleteFolderCleanedUp] = "true"
+			if err := seedClient.Patch(ctx, prometheus, prometheusPatch); err != nil {
+				log.Error(err, "Failed to mark Prometheus resource as cleaned up", "cluster", cluster.Name)
 			}
 
 			return nil
