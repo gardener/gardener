3951454fe50f30c7c979e3d772f196a110998536
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2025-06-12 15:19:22 +0300

    Address PR review feedback

--- pkg/component/extensions/operatingsystemconfig/original/components/containerd/component.go
+++ pkg/component/extensions/operatingsystemconfig/original/components/containerd/component.go
@@ -5,8 +5,6 @@
 package containerd
 
 import (
-	_ "embed"
-
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/component/extensions/operatingsystemconfig/original/components"
