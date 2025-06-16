gardener/gardener

commit 2b37e9e6f17983db3d758f48cd3f2585da88708a

[GEP-32] Introduce helper functions for version classifications (#12268)
* Preliminary cleanup

Co-authored-by: Valentin Knabel <dev@vknabel.com>
Co-authored-by: Gerrit <Gerrit91@users.noreply.github.com>
Co-authored-by: Claus-Theodor Riegg <crigertg@users.noreply.github.com>
Co-authored-by: Luca Bernstein <LucaBernstein@users.noreply.github.com>
Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>

* Add `CurrentLifecycleClassification` helper functions and replace according usages of `version.ExpirationDate`s

Co-authored-by: Valentin Knabel <dev@vknabel.com>
Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>

* refactor(GEP-32): use more classification helpers

* Add TODOs to change former behavior later

Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>

* test(GEP-32): gardenercore tests for lifecycle classifications

---------

Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>
Co-authored-by: Valentin Knabel <dev@vknabel.com>
Co-authored-by: Gerrit <Gerrit91@users.noreply.github.com>
Co-authored-by: Claus-Theodor Riegg <crigertg@users.noreply.github.com>
Co-authored-by: Luca Bernstein <LucaBernstein@users.noreply.github.com>

pkg/apis/core/helper/cloudprofile.go
@@ -7,14 +7,28 @@ package helper
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/Masterminds/semver/v3"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/utils/ptr"
 
 	"github.com/gardener/gardener/pkg/apis/core"
 	"github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/utils"
 )
+
+// CurrentLifecycleClassification returns the current lifecycle classification of the given version.
+// An empty classification is interpreted as supported. If the version is expired, it returns ClassificationExpired.
+func CurrentLifecycleClassification(version core.ExpirableVersion) core.VersionClassification {
+	var currentTime = time.Now()
+
+	if version.ExpirationDate != nil && !currentTime.Before(version.ExpirationDate.Time) {
+		return core.ClassificationExpired
+	}
+
+	return ptr.Deref(version.Classification, core.ClassificationSupported)
+}
 
 // FindMachineImageVersion finds the machine image version in the <cloudProfile> for the given <name> and <version>.
 // In case no machine image version can be found with the given <name> or <version>, false is being returned.
@@ -92,7 +106,7 @@ func DetermineLatestExpirableVersion(versions []core.ExpirableVersion, filterPre
 			return core.ExpirableVersion{}, core.ExpirableVersion{}, fmt.Errorf("error while parsing expirable version '%s': %s", version.Version, err.Error())
 		}
 
-		if filterPreviewVersions && version.Classification != nil && *version.Classification == core.ClassificationPreview {
+		if filterPreviewVersions && CurrentLifecycleClassification(version) == core.ClassificationPreview {
 			continue
 		}
 
@@ -101,7 +115,7 @@ func DetermineLatestExpirableVersion(versions []core.ExpirableVersion, filterPre
 			latestExpirableVersion = version
 		}
 
-		if version.Classification != nil && *version.Classification != core.ClassificationDeprecated {
+		if CurrentLifecycleClassification(version) != core.ClassificationDeprecated {
 			if latestNonDeprecatedSemVerVersion == nil || v.GreaterThan(latestNonDeprecatedSemVerVersion) {
 				latestNonDeprecatedSemVerVersion = v
 				latestNonDeprecatedExpirableVersion = version
@@ -205,7 +219,7 @@ func GetMachineImageDiff(old, new []core.MachineImage) (removedMachineImages set
 func FilterVersionsWithClassification(versions []core.ExpirableVersion, classification core.VersionClassification) []core.ExpirableVersion {
 	var result []core.ExpirableVersion
 	for _, version := range versions {
-		if version.Classification == nil || *version.Classification != classification {
+		if CurrentLifecycleClassification(version) != classification {
 			continue
 		}
 

pkg/apis/core/helper/cloudprofile_test.go
@@ -6,18 +6,72 @@ package helper_test
 
 import (
 	"strings"
+	"time"
 
 	"github.com/Masterminds/semver/v3"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/utils/ptr"
 
 	"github.com/gardener/gardener/pkg/apis/core"
 	. "github.com/gardener/gardener/pkg/apis/core/helper"
 )
 
-var _ = Describe("Helper", func() {
+var _ = Describe("CloudProfile Helper", func() {
+	Describe("#CurrentLifecycleClassification", func() {
+		It("version is implicitly supported", func() {
+			classification := CurrentLifecycleClassification(core.ExpirableVersion{
+				Version: "1.28.0",
+			})
+			Expect(classification).To(Equal(core.ClassificationSupported))
+		})
+
+		It("version is explicitly supported", func() {
+			classification := CurrentLifecycleClassification(core.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(core.ClassificationSupported),
+			})
+			Expect(classification).To(Equal(core.ClassificationSupported))
+		})
+
+		It("version is in preview stage", func() {
+			classification := CurrentLifecycleClassification(core.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(core.ClassificationPreview),
+			})
+			Expect(classification).To(Equal(core.ClassificationPreview))
+		})
+
+		It("version is deprecated ", func() {
+			classification := CurrentLifecycleClassification(core.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(core.ClassificationDeprecated),
+			})
+			Expect(classification).To(Equal(core.ClassificationDeprecated))
+		})
+
+		It("supported version will expire in the future", func() {
+			classification := CurrentLifecycleClassification(core.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(core.ClassificationSupported),
+				ExpirationDate: ptr.To(metav1.NewTime(time.Now().Add(2 * time.Hour))),
+			})
+			Expect(classification).To(Equal(core.ClassificationSupported))
+		})
+
+		It("supported version has already expired", func() {
+			classification := CurrentLifecycleClassification(core.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(core.ClassificationSupported),
+				ExpirationDate: ptr.To(metav1.NewTime(time.Now().Add(-2 * time.Hour))),
+			})
+			Expect(classification).To(Equal(core.ClassificationExpired))
+		})
+	})
+
 	Describe("#FindMachineImageVersion", func() {
 		var machineImages []core.MachineImage
 

pkg/apis/core/v1beta1/helper/cloudprofile.go
@@ -12,11 +12,24 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/utils/ptr"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	versionutils "github.com/gardener/gardener/pkg/utils/version"
 )
+
+// CurrentLifecycleClassification returns the current lifecycle classification of the given version.
+// An empty classification is interpreted as supported. If the version is expired, it returns ClassificationExpired.
+func CurrentLifecycleClassification(version gardencorev1beta1.ExpirableVersion) gardencorev1beta1.VersionClassification {
+	var currentTime = time.Now()
+
+	if version.ExpirationDate != nil && !currentTime.Before(version.ExpirationDate.Time) {
+		return gardencorev1beta1.ClassificationExpired
+	}
+
+	return ptr.Deref(version.Classification, gardencorev1beta1.ClassificationSupported)
+}
 
 // DetermineMachineImageForName finds the cloud specific machine images in the <cloudProfile> for the given <name> and
 // region. In case it does not find the machine image with the <name>, it returns false. Otherwise, true and the
@@ -322,7 +335,7 @@ func GetLatestQualifyingVersion(versions []gardencorev1beta1.ExpirableVersion, p
 	)
 OUTER:
 	for _, v := range versions {
-		if v.Classification != nil && *v.Classification == gardencorev1beta1.ClassificationPreview {
+		if CurrentLifecycleClassification(v) == gardencorev1beta1.ClassificationPreview {
 			continue
 		}
 
@@ -398,7 +411,7 @@ OUTER:
 		}
 
 		// never update to preview versions
-		if v.Classification != nil && *v.Classification == gardencorev1beta1.ClassificationPreview {
+		if CurrentLifecycleClassification(v) == gardencorev1beta1.ClassificationPreview {
 			continue
 		}
 
@@ -505,7 +518,7 @@ func FilterLowerVersion(currentSemVerVersion semver.Version) VersionPredicate {
 // returns true if it is expired
 func FilterExpiredVersion() func(expirableVersion gardencorev1beta1.ExpirableVersion, version *semver.Version) (bool, error) {
 	return func(expirableVersion gardencorev1beta1.ExpirableVersion, _ *semver.Version) (bool, error) {
-		return expirableVersion.ExpirationDate != nil && (time.Now().UTC().After(expirableVersion.ExpirationDate.UTC()) || time.Now().UTC().Equal(expirableVersion.ExpirationDate.UTC())), nil
+		return CurrentLifecycleClassification(expirableVersion) == gardencorev1beta1.ClassificationExpired, nil
 	}
 }
 
@@ -513,7 +526,7 @@ func FilterExpiredVersion() func(expirableVersion gardencorev1beta1.ExpirableVer
 // returns true if it is deprecated
 func FilterDeprecatedVersion() func(expirableVersion gardencorev1beta1.ExpirableVersion, version *semver.Version) (bool, error) {
 	return func(expirableVersion gardencorev1beta1.ExpirableVersion, _ *semver.Version) (bool, error) {
-		return expirableVersion.Classification != nil && *expirableVersion.Classification == gardencorev1beta1.ClassificationDeprecated, nil
+		return CurrentLifecycleClassification(expirableVersion) == gardencorev1beta1.ClassificationDeprecated, nil
 	}
 }
 

pkg/apis/core/v1beta1/helper/cloudprofile_test.go
@@ -17,11 +17,62 @@ import (
 	. "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
 )
 
-var _ = Describe("Helper", func() {
+var _ = Describe("CloudProfile Helper", func() {
 	var (
 		trueVar                 = true
 		expirationDateInThePast = metav1.Time{Time: time.Now().AddDate(0, 0, -1)}
 	)
+
+	Describe("#CurrentLifecycleClassification", func() {
+		It("version is implicitly supported", func() {
+			classification := CurrentLifecycleClassification(gardencorev1beta1.ExpirableVersion{
+				Version: "1.28.0",
+			})
+			Expect(classification).To(Equal(gardencorev1beta1.ClassificationSupported))
+		})
+
+		It("version is explicitly supported", func() {
+			classification := CurrentLifecycleClassification(gardencorev1beta1.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(gardencorev1beta1.ClassificationSupported),
+			})
+			Expect(classification).To(Equal(gardencorev1beta1.ClassificationSupported))
+		})
+
+		It("version is in preview stage", func() {
+			classification := CurrentLifecycleClassification(gardencorev1beta1.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(gardencorev1beta1.ClassificationPreview),
+			})
+			Expect(classification).To(Equal(gardencorev1beta1.ClassificationPreview))
+		})
+
+		It("version is deprecated ", func() {
+			classification := CurrentLifecycleClassification(gardencorev1beta1.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(gardencorev1beta1.ClassificationDeprecated),
+			})
+			Expect(classification).To(Equal(gardencorev1beta1.ClassificationDeprecated))
+		})
+
+		It("supported version will expire in the future", func() {
+			classification := CurrentLifecycleClassification(gardencorev1beta1.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(gardencorev1beta1.ClassificationSupported),
+				ExpirationDate: ptr.To(metav1.NewTime(time.Now().Add(2 * time.Hour))),
+			})
+			Expect(classification).To(Equal(gardencorev1beta1.ClassificationSupported))
+		})
+
+		It("supported version has already expired", func() {
+			classification := CurrentLifecycleClassification(gardencorev1beta1.ExpirableVersion{
+				Version:        "1.28.0",
+				Classification: ptr.To(gardencorev1beta1.ClassificationSupported),
+				ExpirationDate: ptr.To(metav1.NewTime(time.Now().Add(-2 * time.Hour))),
+			})
+			Expect(classification).To(Equal(gardencorev1beta1.ClassificationExpired))
+		})
+	})
 
 	Describe("#FindMachineImageVersion", func() {
 		var machineImages []gardencorev1beta1.MachineImage

pkg/apis/core/validation/cloudprofile.go
@@ -121,7 +121,9 @@ func validateCloudProfileKubernetesSettings(kubernetes core.KubernetesSettings, 
 func validateSupportedVersionsConfiguration(version core.ExpirableVersion, allVersions []core.ExpirableVersion, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	if version.Classification != nil && *version.Classification == core.ClassificationSupported {
+	// TODO(LucaBernstein): Check whether this behavior should be corrected (i.e. changed) in a later GEP-32-PR.
+	//  The current behavior for nil classifications is treated differently across the codebase.
+	if version.Classification != nil && helper.CurrentLifecycleClassification(version) == core.ClassificationSupported {
 		currentSemVer, err := semver.NewVersion(version.Version)
 		if err != nil {
 			// check is already performed by caller, avoid duplicate error
@@ -363,7 +365,9 @@ func checkImageSupport(bastionImageName string, imageVersions []core.MachineImag
 			archSupported = true
 		}
 
-		if version.Classification != nil && *version.Classification == core.ClassificationSupported {
+		// TODO(LucaBernstein): Check whether this behavior should be corrected (i.e. changed) in a later GEP-32-PR.
+		//  The current behavior for nil classifications is treated differently across the codebase.
+		if version.Classification != nil && helper.CurrentLifecycleClassification(version.ExpirableVersion) == core.ClassificationSupported {
 			validClassification = true
 		}
 		if archSupported && validClassification {

pkg/utils/gardener/cloudprofile_test.go
@@ -404,7 +404,7 @@ var _ = Describe("CloudProfile", func() {
 			})
 		})
 
-		Describe("#BuildCloudProfileReference", func() {
+		Describe("#BuildCoreCloudProfileReference", func() {
 			It("should return nil for nil shoot", func() {
 				Expect(gardenerutils.BuildCoreCloudProfileReference(nil)).To(BeNil())
 			})

extensions/pkg/bastion/machine.go
@@ -85,7 +85,9 @@ func findSupportedArchitectures(images []gardencorev1beta1.MachineImage, machine
 				// Skip versions that are not the specified one.
 				continue
 			}
-			if version.Classification != nil && *version.Classification == gardencorev1beta1.ClassificationSupported {
+			// TODO(LucaBernstein): Check whether this behavior should be corrected (i.e. changed) in a later GEP-32-PR.
+			//  The current behavior for nil classifications is treated differently across the codebase.
+			if version.Classification != nil && v1beta1helper.CurrentLifecycleClassification(version.ExpirableVersion) == gardencorev1beta1.ClassificationSupported {
 				architectures.Insert(v1beta1helper.GetArchitecturesFromImageVersion(version)...)
 			}
 			if machineImageVersion != "" {
@@ -132,7 +134,7 @@ func getImageName(bastion *gardencorev1beta1.Bastion, images []gardencorev1beta1
 	// take the first image from cloud profile that is supported and arch compatible
 	for _, image := range images {
 		for _, version := range image.Versions {
-			if version.Classification == nil || *version.Classification != gardencorev1beta1.ClassificationSupported {
+			if v1beta1helper.CurrentLifecycleClassification(version.ExpirableVersion) != gardencorev1beta1.ClassificationSupported {
 				continue
 			}
 			if !slices.Contains(v1beta1helper.GetArchitecturesFromImageVersion(version), arch) {
@@ -161,7 +163,9 @@ func getImageVersion(bastion *gardencorev1beta1.Bastion, imageName, machineArch 
 			return "", fmt.Errorf("image version %s not found not found in cloudProfile", *bastion.MachineImage.Version)
 		}
 
-		if image.Versions[versionIndex].Classification != nil && *image.Versions[versionIndex].Classification != gardencorev1beta1.ClassificationSupported {
+		// TODO(LucaBernstein): Check whether this behavior should be corrected (i.e. changed) in a later GEP-32-PR.
+		//  The current behavior for nil classifications is treated differently across the codebase.
+		if image.Versions[versionIndex].Classification != nil && v1beta1helper.CurrentLifecycleClassification(image.Versions[versionIndex].ExpirableVersion) != gardencorev1beta1.ClassificationSupported {
 			return "", fmt.Errorf("specified image %s in version %s is not classified supported", imageName, *bastion.MachineImage.Version)
 		}
 
@@ -170,7 +174,7 @@ func getImageVersion(bastion *gardencorev1beta1.Bastion, imageName, machineArch 
 
 	var greatest *semver.Version
 	for _, version := range image.Versions {
-		if version.Classification == nil || *version.Classification != gardencorev1beta1.ClassificationSupported {
+		if v1beta1helper.CurrentLifecycleClassification(version.ExpirableVersion) != gardencorev1beta1.ClassificationSupported {
 			continue
 		}
 

pkg/apis/core/types_cloudprofile.go
@@ -144,7 +144,8 @@ type ExpirableVersion struct {
 	Version string
 	// ExpirationDate defines the time at which this version expires.
 	ExpirationDate *metav1.Time
-	// Classification defines the state of a version (preview, supported, deprecated)
+	// Classification defines the state of a version (preview, supported, deprecated).
+	// To get the currently valid classification, use CurrentLifecycleClassification().
 	Classification *VersionClassification
 }
 
@@ -264,7 +265,14 @@ const (
 // VersionClassification is the logical state of a version.
 type VersionClassification string
 
+// IsActive returns whether the version can be used.
+func (v VersionClassification) IsActive() bool {
+	return v != ClassificationExpired && v != ClassificationUnavailable
+}
+
 const (
+	// ClassificationUnavailable indicates that a version is currently not available and is planned to become available depending on the classification lifecycle.
+	ClassificationUnavailable VersionClassification = "unavailable"
 	// ClassificationPreview indicates that a version has recently been added and not promoted to "Supported" yet.
 	// ClassificationPreview versions will not be considered for automatic Kubernetes and Machine Image patch version updates.
 	ClassificationPreview VersionClassification = "preview"
@@ -275,6 +283,9 @@ const (
 	// ClassificationDeprecated indicates that a patch version should not be used anymore, should be updated to a new version
 	// and will eventually expire.
 	ClassificationDeprecated VersionClassification = "deprecated"
+	// ClassificationExpired indicates that a version has expired.
+	// New entities with that version cannot be created and existing entities are forcefully migrated to a higher version during the maintenance time.
+	ClassificationExpired VersionClassification = "expired"
 )
 
 // MachineImageUpdateStrategy is the update strategy to use for a machine image

commit 63dc6ca71168154c3e6308ed41b95d61ca7153ae

Fix `CloudProfile` validation
Fix regression introduced by 2b37e9e6f17983db3d758f48cd3f2585da88708a

pkg/apis/core/helper/cloudprofile.go
@@ -219,7 +219,9 @@ func GetMachineImageDiff(old, new []core.MachineImage) (removedMachineImages set
 func FilterVersionsWithClassification(versions []core.ExpirableVersion, classification core.VersionClassification) []core.ExpirableVersion {
 	var result []core.ExpirableVersion
 	for _, version := range versions {
-		if CurrentLifecycleClassification(version) != classification {
+		// TODO(LucaBernstein): Check whether this behavior should be corrected (i.e. changed) in a later GEP-32-PR.
+		//  The current behavior for nil classifications is treated differently across the codebase.
+		if version.Classification == nil || CurrentLifecycleClassification(version) != classification {
 			continue
 		}
 

pkg/apis/core/helper/cloudprofile_test.go
@@ -399,9 +399,9 @@ var _ = Describe("CloudProfile Helper", func() {
 	})
 
 	Describe("#FilterVersionsWithClassification", func() {
-		classification := core.ClassificationDeprecated
 		var (
-			versions = []core.ExpirableVersion{
+			classification = core.ClassificationSupported
+			versions       = []core.ExpirableVersion{
 				{
 					Version:        "1.0.2",
 					Classification: &classification,
@@ -415,6 +415,7 @@ var _ = Describe("CloudProfile Helper", func() {
 				},
 			}
 		)
+
 		It("should filter version", func() {
 			filteredVersions := FilterVersionsWithClassification(versions, classification)
 