000683e10d79c49e35e3a16af4b53907542cad03
Aleksandar Savchev <aleksandar.savchev@sap.com>
2025-06-18 18:07:31 +0300

    Format

--- pkg/component/shared/apiserver_test.go
+++ pkg/component/shared/apiserver_test.go
@@ -7,7 +7,6 @@ package shared_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
