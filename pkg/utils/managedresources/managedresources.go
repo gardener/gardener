e588e609a912fd93526d01b82a03cda5460e47a6
Tobias Schlicht <tobias.schlicht@sap.com>
2025-06-24 15:50:59 +0200

    Avoid cancelling flows when `MR` is not found
    A `apierrors.IsNotFound` error could happen due to a caching issue, leading to an unnecessary restart of the reconciliation flow

--- pkg/utils/managedresources/managedresources.go
+++ pkg/utils/managedresources/managedresources.go
@@ -317,6 +317,9 @@ func waitUntilHealthy(ctx context.Context, reader client.Reader, namespace, name
 
 	return retry.Until(ctx, IntervalWait, func(ctx context.Context) (done bool, err error) {
 		if err := reader.Get(ctx, client.ObjectKey{Namespace: namespace, Name: name}, obj); err != nil {
+			if apierrors.IsNotFound(err) {
+				return retry.MinorError(fmt.Errorf("managed resource %s/%s not found", namespace, name))
+			}
 			return retry.SevereError(err)
 		}
 
