9075ed78908fa054c45327071e89bdfd08d3baae
Luca Bernstein <luca.bernstein@sap.com>
2025-06-25 04:49:53 +0200

    Support defaulting machine image version from provided prefix on spec update (#12374)
    * Support defaulting machine image version to latest non-preview version for given prefix on Shoot spec update
    
    Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>
    
    * Add case for switched image name and only provided prefix
    
    Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>
    
    ---------
    
    Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>

--- plugin/pkg/shoot/validator/admission.go
+++ plugin/pkg/shoot/validator/admission.go
@@ -2015,12 +2015,12 @@ func ensureMachineImage(oldWorkers []core.Worker, worker core.Worker, images []g
 		if oldWorker.Machine.Image.Name == worker.Machine.Image.Name {
 			// image name was not changed -> keep version from the new worker if specified, otherwise use the old worker image version
 			if len(worker.Machine.Image.Version) != 0 {
-				return worker.Machine.Image, nil
+				return getDefaultMachineImage(images, worker.Machine.Image, worker.Machine.Architecture, helper.IsUpdateStrategyInPlace(worker.UpdateStrategy), fldPath)
 			}
 			return oldWorker.Machine.Image, nil
 		} else if len(worker.Machine.Image.Version) != 0 {
 			// image name was changed -> keep version from new worker if specified, otherwise default the image version
-			return worker.Machine.Image, nil
+			return getDefaultMachineImage(images, worker.Machine.Image, worker.Machine.Architecture, helper.IsUpdateStrategyInPlace(worker.UpdateStrategy), fldPath)
 		}
 	}
 
