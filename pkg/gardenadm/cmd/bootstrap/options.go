ad74badb82d777f017b1565147df99d18e7a9561
Tim Ebert <timebertt@gmail.com>
2025-06-27 08:49:55 +0200

    Add `--bastion-ingress-cidr` flag and auto-detection

--- pkg/gardenadm/cmd/bootstrap/options.go
+++ pkg/gardenadm/cmd/bootstrap/options.go
@@ -5,12 +5,16 @@
 package bootstrap
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"os"
+	"time"
 
 	"github.com/spf13/pflag"
 
 	"github.com/gardener/gardener/pkg/gardenadm/cmd"
+	"github.com/gardener/gardener/pkg/utils/publicip"
 )
 
 // Options contains options for this command.
@@ -19,6 +23,12 @@ type Options struct {
 	cmd.ManifestOptions
 	// Kubeconfig is the path to the kubeconfig file pointing to the KinD cluster.
 	Kubeconfig string
+	// BastionIngressCIDRs is a list of CIDRs to be allowed for accessing the created bastion host.
+	// If not given, the system's public IPs are detected using PublicIPDetector.
+	BastionIngressCIDRs []string
+
+	// exposed for testing, defaults to publicip.IpifyDetector
+	PublicIPDetector publicip.Detector
 }
 
 // ParseArgs parses the arguments to the options.
@@ -36,15 +46,47 @@ func (o *Options) Validate() error {
 		return fmt.Errorf("must provide a path to a KinD cluster kubeconfig")
 	}
 
+	for _, cidr := range o.BastionIngressCIDRs {
+		_, _, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return err
+		}
+	}
+
 	return o.ManifestOptions.Validate()
 }
 
 // Complete completes the options.
 func (o *Options) Complete() error {
+	if len(o.BastionIngressCIDRs) == 0 {
+		o.Log.Info("Auto-detecting public IP addresses. If this does not work, set --bastion-ingress-cidr instead")
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+
+		ips, err := o.PublicIPDetector.DetectPublicIPs(ctx, o.Log.WithName("public-ip-detector"))
+		if err != nil {
+			return fmt.Errorf("error detecting public IP addresses: %w", err)
+		}
+
+		for _, ip := range ips {
+			cidr := net.IPNet{IP: ip}
+			if ip.To4() != nil {
+				cidr.Mask = net.CIDRMask(32, 32) // use /32 for IPv4
+			} else {
+				cidr.Mask = net.CIDRMask(128, 128) // use /128 for IPv6
+			}
+			o.BastionIngressCIDRs = append(o.BastionIngressCIDRs, cidr.String())
+		}
+
+		o.Log.Info("Using auto-detected public IP addresses as bastion ingress CIDRs", "cidrs", o.BastionIngressCIDRs)
+	}
+
 	return o.ManifestOptions.Complete()
 }
 
 func (o *Options) addFlags(fs *pflag.FlagSet) {
 	o.ManifestOptions.AddFlags(fs)
 	fs.StringVarP(&o.Kubeconfig, "kubeconfig", "k", "", "Path to the kubeconfig file pointing to the KinD cluster")
+	fs.StringSliceVar(&o.BastionIngressCIDRs, "bastion-ingress-cidr", nil, "Restrict bastion host ingress to the given CIDRs. Defaults to your system's public IPs (IPv4 and/or IPv6) as detected using https://ipify.org/.")
 }
--- pkg/gardenadm/cmd/bootstrap/options_test.go
+++ pkg/gardenadm/cmd/bootstrap/options_test.go
@@ -5,24 +5,45 @@
 package bootstrap_test
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+
+	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	logzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"github.com/gardener/gardener/pkg/gardenadm/cmd"
 	. "github.com/gardener/gardener/pkg/gardenadm/cmd/bootstrap"
+	"github.com/gardener/gardener/pkg/logger"
 )
 
 var _ = Describe("Options", func() {
 	var (
 		options *Options
+
+		logBuffer *gbytes.Buffer
+		detector  *fakeDetector
 	)
 
 	BeforeEach(func() {
+		detector = &fakeDetector{
+			ips: []net.IP{net.ParseIP("1.2.3.4"), net.ParseIP("2001:db8::1")},
+		}
+
+		logBuffer = gbytes.NewBuffer()
 		options = &Options{
-			Kubeconfig: "some-path-to-kubeconfig",
+			Options: &cmd.Options{
+				Log: logger.MustNewZapLogger(logger.DebugLevel, logger.FormatJSON, logzap.WriteTo(io.MultiWriter(logBuffer, GinkgoWriter))),
+			},
 			ManifestOptions: cmd.ManifestOptions{
 				ConfigDir: "some-path-to-config-dir",
 			},
+			Kubeconfig:       "some-path-to-kubeconfig",
+			PublicIPDetector: detector,
 		}
 	})
 
@@ -46,11 +67,63 @@ var _ = Describe("Options", func() {
 			options.ConfigDir = ""
 			Expect(options.Validate()).To(MatchError(ContainSubstring("must provide a path to a config directory")))
 		})
+
+		Describe("bastion ingress CIDRs", func() {
+			It("should succeed without CIDRs", func() {
+				options.BastionIngressCIDRs = nil
+				Expect(options.Validate()).To(Succeed())
+			})
+
+			It("should succeed with valid CIDRs", func() {
+				options.BastionIngressCIDRs = []string{"4.3.2.1/32", "2001:db8::2/128"}
+				Expect(options.Validate()).To(Succeed())
+			})
+
+			It("should fail with invalid CIDRs", func() {
+				options.BastionIngressCIDRs = []string{"4.3.2.1/32", "1.2.3.4/128"}
+				Expect(options.Validate()).To(MatchError(ContainSubstring("invalid CIDR address: 1.2.3.4/128")))
+
+				options.BastionIngressCIDRs = []string{"4.3.2.1/32", "2001:db8::1/invalid"}
+				Expect(options.Validate()).To(MatchError(ContainSubstring("invalid CIDR address: 2001:db8::1/invalid")))
+			})
+		})
 	})
 
 	Describe("#Complete", func() {
 		It("should return nil", func() {
 			Expect(options.Complete()).To(Succeed())
 		})
+
+		Describe("bastion ingress CIDRs", func() {
+			It("should keep the configured CIDRs", func() {
+				options.BastionIngressCIDRs = []string{"4.3.2.1/32", "2001:db8::2/128"}
+				Expect(options.Complete()).To(Succeed())
+				Expect(options.BastionIngressCIDRs).To(ConsistOf("4.3.2.1/32", "2001:db8::2/128"))
+				Consistently(logBuffer).ShouldNot(gbytes.Say("Auto-detecting public IP addresses"))
+			})
+
+			It("should use auto-detected IP addresses", func() {
+				options.BastionIngressCIDRs = nil
+				Expect(options.Complete()).To(Succeed())
+				Expect(options.BastionIngressCIDRs).To(ConsistOf("1.2.3.4/32", "2001:db8::1/128"))
+				Eventually(logBuffer).Should(gbytes.Say("Using auto-detected public IP addresses"))
+			})
+
+			It("should fail if IP address detection fails", func() {
+				options.BastionIngressCIDRs = nil
+				detector.err = fmt.Errorf("foo")
+				Expect(options.Complete()).To(MatchError(ContainSubstring("foo")))
+				Eventually(logBuffer).Should(gbytes.Say("Auto-detecting public IP addresses"))
+			})
+		})
 	})
 })
+
+type fakeDetector struct {
+	ips []net.IP
+	err error
+}
+
+func (f *fakeDetector) DetectPublicIPs(context.Context, logr.Logger) ([]net.IP, error) {
+	return f.ips, f.err
+}
