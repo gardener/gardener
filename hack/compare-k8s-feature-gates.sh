cdca686d07f08d0b7afd2d2574be40439f6b16d7
Viktor <v.kostov@sap.com>
2025-07-03 14:08:28 +0300

    Address feedback and update new-kubernetes-version docu

--- hack/compare-k8s-feature-gates.sh
+++ hack/compare-k8s-feature-gates.sh
@@ -10,7 +10,9 @@ usage() {
   echo "Usage:"
   echo "> compare-k8s-feature-gates.sh [ -h | <old version> <new version> ]"
   echo
-  echo ">> For example: compare-k8s-feature-gates.sh 1.26 1.27"
+  echo ">> For example: compare-k8s-feature-gates.sh 1.32 1.33"
+  echo
+  echo ">> Note: The script only works for Kubernetes versions 1.32+"
 
   exit 0
 }
@@ -27,14 +29,17 @@ function cleanup_output {
 trap cleanup_output EXIT
 
 for version in "${versions[@]}"; do
+  if [ "$version" \< "1.32" ]; then 
+    echo "Versions less than 1.32 are not supported." 
+    exit 1 
+  fi 
   versions_dir=test/compatibility_lifecycle/reference
   
-  # TODO Drop this when Kubernetes v1.34 gets released
   if [ "$version" == "1.32" ]; then
     versions_dir="test/featuregates_linter/test_data"
   fi
 
- # Get the list of versioned and unversioned feature gates. After v1.34+ unversioned_feature_list.yaml should most likely be dropped
+  # Get the list of versioned and unversioned feature gates. After v1.34+ unversioned_feature_list.yaml should most likely be dropped
   wget -q -O - "https://raw.githubusercontent.com/kubernetes/kubernetes/release-${version}/${versions_dir}/versioned_feature_list.yaml" > "${out_dir}/versioned_featuregates_${version}.yaml"
   wget -q -O - "https://raw.githubusercontent.com/kubernetes/kubernetes/release-${version}/${versions_dir}/unversioned_feature_list.yaml" > "${out_dir}/unversioned_featuregates_${version}.yaml"
   # Extract only the names of the feature gates
@@ -50,12 +55,18 @@ echo
 echo "Feature gates removed in $2 compared to $1:"
 diff "${out_dir}/featuregates_list_${1}.yaml" "${out_dir}/featuregates_list_${2}.yaml" | grep '<' | awk '{print $2}'
 echo
-echo "Feature gates locked to default true:"
+echo "Feature gates locked to default true in $2 compared to $1:"
 # Get all feature gate names that have a version spec containing $2, are locked to default with default value of true
 yq '.[] | select(.versionedSpecs[] | select(.version == "'$2'" and .lockToDefault == true and .default == true)) | .name' "${out_dir}/versioned_featuregates_${version}.yaml"
-yq '.[] | select(.versionedSpecs[] | select(.version == "'$2'" and .lockToDefault == true and .default == true)) | .name' "${out_dir}/unversioned_featuregates_${version}.yaml"
+# get feature gates that are defaulted to true and in previous version were not locked to default but in the next version were locked to default
+comm -12 \
+  <(yq '.[] | select(.versionedSpecs[] | select(.lockToDefault == false and .default == true)) | .name' "${out_dir}/unversioned_featuregates_${versions[0]}.yaml" | sort) \
+  <(yq '.[] | select(.versionedSpecs[] | select(.lockToDefault == true and .default == true)) | .name' "${out_dir}/unversioned_featuregates_${versions[1]}.yaml" | sort)
 echo
-echo "Feature gates locked to default false:"
+echo "Feature gates locked to default false in $2 compared to $1:"
 # Get all feature gate names that have a version spec containing $2, are locked to default with default value of false
 yq '.[] | select(.versionedSpecs[] | select(.version == "'$2'" and .lockToDefault == true and .default == false)) | .name' "${out_dir}/versioned_featuregates_${version}.yaml"
-yq '.[] | select(.versionedSpecs[] | select(.version == "'$2'" and .lockToDefault == true and .default == false)) | .name' "${out_dir}/unversioned_featuregates_${version}.yaml"
+# get feature gates that are defaulted to false and in previous version were not locked to default but in the next version were locked to default
+comm -12 \
+  <(yq '.[] | select(.versionedSpecs[] | select(.lockToDefault == false and .default == false)) | .name' "${out_dir}/unversioned_featuregates_${versions[0]}.yaml" | sort) \
+  <(yq '.[] | select(.versionedSpecs[] | select(.lockToDefault == true and .default == false)) | .name' "${out_dir}/unversioned_featuregates_${versions[1]}.yaml" | sort)
