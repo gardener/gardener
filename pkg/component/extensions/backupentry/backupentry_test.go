2dd1458f0a6c82d1eae5d7009b40b45d28135462
Shafeeque E S <shafeeque.e.s@sap.com>
2025-07-10 13:47:21 +0530

    Address PR review feedback

--- pkg/component/extensions/backupentry/backupentry_test.go
+++ pkg/component/extensions/backupentry/backupentry_test.go
@@ -290,7 +290,6 @@ var _ = Describe("#BackupEntry", func() {
 			obj := expected.DeepCopy()
 			metav1.SetMetaDataAnnotation(&obj.ObjectMeta, "gardener.cloud/operation", "wait-for-state")
 			metav1.SetMetaDataAnnotation(&obj.ObjectMeta, "gardener.cloud/timestamp", fakeClock.Now().UTC().Format(time.RFC3339Nano))
-			metav1.SetMetaDataLabel(&obj.ObjectMeta, "provider.extensions.gardener.cloud/"+providerType, "true")
 
 			obj.TypeMeta = metav1.TypeMeta{}
 			mc.EXPECT().Create(ctx, test.HasObjectKeyOf(obj)).
