85b778be393201939c5cec68e5223b7fea9e2254
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2025-07-10 17:42:18 +0300

    Address PR review comment

--- pkg/component/observability/opentelemetry/operator/operator.go
+++ pkg/component/observability/opentelemetry/operator/operator.go
@@ -350,14 +350,14 @@ func (o *openTelemetryOperator) deployment() *appsv1.Deployment {
 									},
 								},
 							},
-							SecurityContext: &corev1.SecurityContext{
-								AllowPrivilegeEscalation: ptr.To(false),
-							},
 							Resources: corev1.ResourceRequirements{
 								Requests: corev1.ResourceList{
 									corev1.ResourceCPU:    resource.MustParse("10m"),
 									corev1.ResourceMemory: resource.MustParse("64Mi"),
 								},
+							},
+							SecurityContext: &corev1.SecurityContext{
+								AllowPrivilegeEscalation: ptr.To(false),
 							},
 						},
 					},
--- pkg/component/observability/opentelemetry/operator/operator_test.go
+++ pkg/component/observability/opentelemetry/operator/operator_test.go
@@ -283,14 +283,14 @@ var _ = Describe("OpenTelemetry Operator", func() {
 										},
 									},
 								},
-								SecurityContext: &corev1.SecurityContext{
-									AllowPrivilegeEscalation: ptr.To(false),
-								},
 								Resources: corev1.ResourceRequirements{
 									Requests: corev1.ResourceList{
 										corev1.ResourceCPU:    resource.MustParse("10m"),
 										corev1.ResourceMemory: resource.MustParse("64Mi"),
 									},
+								},
+								SecurityContext: &corev1.SecurityContext{
+									AllowPrivilegeEscalation: ptr.To(false),
 								},
 							},
 						},
