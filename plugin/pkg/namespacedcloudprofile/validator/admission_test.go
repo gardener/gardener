071b3844f75d395b65527c9f626ecbcfc874652e
Sonu Kumar Singh <sksgkpvks@gmail.com>
2025-07-10 18:13:33 +0530

    Forbid inPlaceUpdates in extended machine images

--- plugin/pkg/namespacedcloudprofile/validator/admission_test.go
+++ plugin/pkg/namespacedcloudprofile/validator/admission_test.go
@@ -514,6 +514,58 @@ var _ = Describe("Admission", func() {
 				Expect(admissionHandler.Validate(ctx, attrs, nil)).To(MatchError(ContainSubstring("expiration date for version \"1.0.0\" must be set")))
 			})
 
+			It("should fail for creating a NamespacedCloudProfile that overrides an existing MachineImage version and specifies classification/cri/arch/capabilitySets/kubeletVersionConstraint/inPlaceUpdates", func() {
+				parentCloudProfile.Spec.MachineImages = []gardencorev1beta1.MachineImage{
+					{Name: "test-image", Versions: []gardencorev1beta1.MachineImageVersion{
+						{ExpirableVersion: gardencorev1beta1.ExpirableVersion{Version: "1.1.0"}, CRI: []gardencorev1beta1.CRI{{Name: "containerd"}}},
+						{ExpirableVersion: gardencorev1beta1.ExpirableVersion{Version: "1.2.0"}, CRI: []gardencorev1beta1.CRI{{Name: "containerd"}}},
+					}},
+				}
+				gardencorev1beta1.SetObjectDefaults_CloudProfile(parentCloudProfile)
+				Expect(coreInformerFactory.Core().V1beta1().CloudProfiles().Informer().GetStore().Add(parentCloudProfile)).To(Succeed())
+
+				namespacedCloudProfile.Spec.MachineImages = []gardencore.MachineImage{
+					{Name: "test-image", Versions: []gardencore.MachineImageVersion{
+						{
+							ExpirableVersion:         gardencore.ExpirableVersion{Version: "1.1.0", ExpirationDate: expiredExpirationDate, Classification: ptr.To(gardencore.ClassificationSupported)},
+							CRI:                      []gardencore.CRI{{Name: "containerd"}},
+							Architectures:            []string{"amd64"},
+							CapabilitySets:           []gardencore.CapabilitySet{{Capabilities: gardencore.Capabilities{"architecture": []string{"amd64"}}}},
+							KubeletVersionConstraint: ptr.To(">=1.29.0"),
+							InPlaceUpdates:           &gardencore.InPlaceUpdates{Supported: true},
+						},
+					}},
+				}
+
+				attrs := admission.NewAttributesRecord(namespacedCloudProfile, nil, gardencorev1beta1.Kind("NamespacedCloudProfile").WithVersion("version"), "", namespacedCloudProfile.Name, gardencorev1beta1.Resource("namespacedcloudprofile").WithVersion("version"), "", admission.Create, &metav1.CreateOptions{}, false, nil)
+
+				Expect(admissionHandler.Validate(ctx, attrs, nil)).To(ConsistOf(PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeForbidden),
+					"Field":  Equal("spec.machineImages[0].versions[0].cri"),
+					"Detail": ContainSubstring("must not provide a cri to an extended machine image in NamespacedCloudProfile"),
+				})), PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeForbidden),
+					"Field":  Equal("spec.machineImages[0].versions[0].classification"),
+					"Detail": ContainSubstring("must not provide a classification to an extended machine image in NamespacedCloudProfile"),
+				})), PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeForbidden),
+					"Field":  Equal("spec.machineImages[0].versions[0].architectures"),
+					"Detail": ContainSubstring("must not provide an architecture to an extended machine image in NamespacedCloudProfile"),
+				})), PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeForbidden),
+					"Field":  Equal("spec.machineImages[0].versions[0].capabilitySets"),
+					"Detail": ContainSubstring("must not provide capabilities to an extended machine image in NamespacedCloudProfile"),
+				})), PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeForbidden),
+					"Field":  Equal("spec.machineImages[0].versions[0].kubeletVersionConstraint"),
+					"Detail": ContainSubstring("must not provide a kubelet version constraint to an extended machine image in NamespacedCloudProfile"),
+				})), PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeForbidden),
+					"Field":  Equal("spec.machineImages[0].versions[0].inPlaceUpdates"),
+					"Detail": ContainSubstring("must not provide inPlaceUpdates to an extended machine image in NamespacedCloudProfile"),
+				}))))
+			})
+
 			It("should fail for updating a NamespacedCloudProfile that specifies an already expired MachineImage version", func() {
 				parentCloudProfile.Spec.MachineImages = []gardencorev1beta1.MachineImage{
 					{Name: "test-image", Versions: []gardencorev1beta1.MachineImageVersion{
