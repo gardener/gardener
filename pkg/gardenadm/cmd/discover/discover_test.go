1c4b1a87bdbedf09215c6686aae722f7f8033af9
rfranzke <rafael.franzke@sap.com>
2025-07-18 15:24:50 +0200

    WIP: Unit tests

--- pkg/gardenadm/cmd/discover/discover_test.go
+++ pkg/gardenadm/cmd/discover/discover_test.go
@@ -8,12 +8,19 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	fakekubernetes "github.com/gardener/gardener/pkg/client/kubernetes/fake"
 	"github.com/gardener/gardener/pkg/gardenadm/cmd"
 	. "github.com/gardener/gardener/pkg/gardenadm/cmd/discover"
 	"github.com/gardener/gardener/pkg/logger"
+	"github.com/gardener/gardener/pkg/utils/test"
 	clitest "github.com/gardener/gardener/pkg/utils/test/cli"
 )
 
@@ -22,6 +29,10 @@ var _ = Describe("Discover", func() {
 		globalOpts *cmd.Options
 		stdErr     *Buffer
 		command    *cobra.Command
+
+		fs         afero.Afero
+		fakeClient client.Client
+		clientSet  kubernetes.Interface
 	)
 
 	BeforeEach(func() {
@@ -29,13 +40,28 @@ var _ = Describe("Discover", func() {
 		globalOpts.IOStreams, _, _, stdErr = clitest.NewTestIOStreams()
 		globalOpts.Log = logger.MustNewZapLogger(logger.DebugLevel, logger.FormatJSON, logzap.WriteTo(stdErr))
 		command = NewCommand(globalOpts)
+
+		fakeClient = fakeclient.NewClientBuilder().Build()
+		clientSet = fakekubernetes.NewClientSetBuilder().WithClient(fakeClient).Build()
+		fs = afero.Afero{Fs: afero.NewMemMapFs()}
+
+		DeferCleanup(test.WithVars(
+			&NewClientSetFromFile, func(string, *runtime.Scheme) (kubernetes.Interface, error) { return clientSet, nil },
+			&NewAferoFs, func() afero.Afero { return fs },
+		))
 	})
 
 	Describe("#RunE", func() {
-		It("should return the expected output", func() {
-			Expect(command.Flags().Set("kubeconfig", "some-path-to-kubeconfig")).To(Succeed())
+		var (
+			shootManifestPath = "some-path-to-shoot-manifest-file"
+		)
 
-			Expect(command.RunE(command, []string{"some-path-to-shoot-manifest-file"})).To(Succeed())
+		It("should return the expected output", func() {
+			Expect(fs.WriteFile(shootManifestPath, []byte(`apiVersion: core.gardener.cloud/v1beta1
+kind: Shoot`), 0600)).To(Succeed())
+
+			Expect(command.Flags().Set("kubeconfig", "some-path-to-kubeconfig")).To(Succeed())
+			Expect(command.RunE(command, []string{shootManifestPath})).To(Succeed())
 
 			Eventually(stdErr).Should(Say("Not implemented"))
 		})
