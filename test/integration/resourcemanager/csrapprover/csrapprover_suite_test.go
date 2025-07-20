45df067460f2ebb3a88ab75597c9002b616483f5
oliver-goetz <o.goetz@sap.com>
2025-07-20 19:08:28 +0200

    Remove `gardener-node-agent` service account related logic from `csr-approver`

--- test/integration/resourcemanager/csrapprover/csrapprover_suite_test.go
+++ test/integration/resourcemanager/csrapprover/csrapprover_suite_test.go
@@ -49,23 +49,21 @@ var (
 	ctx = context.Background()
 	log logr.Logger
 
-	restConfig            *rest.Config
-	testEnv               *envtest.Environment
-	testClientKubelet     client.Client
-	testClientNodeAgent   client.Client
-	testClientBootstrap   client.Client
-	testClientNodeAgentSA client.Client
-	mgrClient             client.Client
+	restConfig          *rest.Config
+	testEnv             *envtest.Environment
+	testClientKubelet   client.Client
+	testClientNodeAgent client.Client
+	testClientBootstrap client.Client
+	mgrClient           client.Client
 
 	testNamespace *corev1.Namespace
 	testRunID     string
 
-	machineName         string
-	nodeName            string
-	userNameKubelet     string
-	userNameNodeAgent   string
-	userNameBootstrap   string
-	userNameNodeAgentSA string
+	machineName       string
+	nodeName          string
+	userNameKubelet   string
+	userNameNodeAgent string
+	userNameBootstrap string
 )
 
 var _ = BeforeSuite(func() {
@@ -99,7 +97,6 @@ var _ = BeforeSuite(func() {
 	userNameKubelet = "system:node:" + nodeName
 	userNameNodeAgent = "gardener.cloud:node-agent:machine:" + machineName
 	userNameBootstrap = "system:bootstrap:" + testRunID
-	userNameNodeAgentSA = "system:serviceaccount:kube-system:gardener-node-agent"
 
 	createClient := func(userName string) client.Client {
 		// We have to "fake" that our test client is the kubelet or gardener-node-agent user because the .spec.username
@@ -119,7 +116,6 @@ var _ = BeforeSuite(func() {
 	testClientKubelet = createClient(userNameKubelet)
 	testClientNodeAgent = createClient(userNameNodeAgent)
 	testClientBootstrap = createClient(userNameBootstrap)
-	testClientNodeAgentSA = createClient(userNameNodeAgentSA)
 
 	By("Create test Namespace")
 	testNamespace = &corev1.Namespace{
