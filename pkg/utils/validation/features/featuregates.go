08919f0d3d74184e9d63c75acfe414e24362b574
Marc Vornetran <marc1404@users.noreply.github.com>
2025-07-31 16:51:00 +0200

    fix: Maintain feature gate ValidatingAdmissionPolicy as removed with Kubernetes v1.32 (#12643)
    Co-authored-by: Luca Bernstein <luca.bernstein@sap.com>

--- pkg/utils/validation/features/featuregates.go
+++ pkg/utils/validation/features/featuregates.go
@@ -299,7 +299,7 @@ var featureGateVersionRanges = map[string]*FeatureGateVersionRange{
 	"UserNamespacesPodSecurityStandards":               {VersionRange: versionutils.VersionRange{AddedInVersion: "1.29"}},
 	"UserNamespacesStatelessPodsSupport":               {VersionRange: versionutils.VersionRange{RemovedInVersion: "1.28"}},
 	"UserNamespacesSupport":                            {VersionRange: versionutils.VersionRange{AddedInVersion: "1.28"}},
-	"ValidatingAdmissionPolicy":                        {LockedValue: true, LockedToDefaultInVersion: "1.30"},
+	"ValidatingAdmissionPolicy":                        {LockedValue: true, LockedToDefaultInVersion: "1.30", VersionRange: versionutils.VersionRange{RemovedInVersion: "1.32"}},
 	"VolumeAttributesClass":                            {VersionRange: versionutils.VersionRange{AddedInVersion: "1.29"}},
 	"VolumeCapacityPriority":                           {VersionRange: versionutils.VersionRange{RemovedInVersion: "1.33"}},
 	"WatchBookmark":                                    {LockedValue: true, LockedToDefaultInVersion: "1.17", VersionRange: versionutils.VersionRange{RemovedInVersion: "1.33"}},
