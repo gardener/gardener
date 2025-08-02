85c5c80f6a4da088862495ec2766cef9b0b9da41
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-08-02 22:19:40 +0000

    Update dependency golangci/golangci-lint to v2.3.1

--- hack/tools.mk
+++ hack/tools.mk
@@ -54,7 +54,7 @@ TYPOS                      := $(TOOLS_BIN_DIR)/typos
 
 # default tool versions
 # renovate: datasource=github-releases depName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION ?= v2.3.0
+GOLANGCI_LINT_VERSION ?= v2.3.1
 # renovate: datasource=github-releases depName=securego/gosec
 GOSEC_VERSION ?= v2.22.7
 # renovate: datasource=github-releases depName=joelanford/go-apidiff
