f680de66d1f7b8bfb04cef44b532026728d60d16
Shafeeque E S <shafeeque.e.s@sap.com>
2025-08-05 14:09:55 +0530

    Drop migration for deprecated topology labels

--- cmd/gardenlet/app/migration.go
+++ cmd/gardenlet/app/migration.go
@@ -29,11 +29,6 @@ import (
 )
 
 func (g *garden) runMigrations(ctx context.Context, log logr.Logger) error {
-	log.Info("Migrating deprecated failure-domain.beta.kubernetes.io labels to topology.kubernetes.io")
-	if err := migrateDeprecatedTopologyLabels(ctx, log, g.mgr.GetClient()); err != nil {
-		return err
-	}
-
 	log.Info("Removing Prometheus cleaned up obsolete folder annotations")
 	if err := removePrometheusFolderCleanedupAnnotation(ctx, log, g.mgr.GetClient()); err != nil {
 		return fmt.Errorf("failed removing Prometheus cleaned up obsolete folder annotations: %w", err)
@@ -45,65 +40,6 @@ func (g *garden) runMigrations(ctx context.Context, log logr.Logger) error {
 	}
 
 	return nil
-}
-
-// TODO: Remove this function when Kubernetes 1.27 support gets dropped.
-func migrateDeprecatedTopologyLabels(ctx context.Context, log logr.Logger, seedClient client.Client) error {
-	persistentVolumeList := &corev1.PersistentVolumeList{}
-	if err := seedClient.List(ctx, persistentVolumeList); err != nil {
-		return fmt.Errorf("failed listing persistent volumes for migrating deprecated topology labels: %w", err)
-	}
-
-	var taskFns []flow.TaskFn
-
-	for _, pv := range persistentVolumeList.Items {
-		persistentVolume := pv
-
-		taskFns = append(taskFns, func(ctx context.Context) error {
-			patch := client.MergeFrom(persistentVolume.DeepCopy())
-
-			if persistentVolume.Spec.NodeAffinity == nil {
-				// when PV is very old and has no node affinity, we just replace the topology labels
-				if v, ok := persistentVolume.Labels[corev1.LabelFailureDomainBetaRegion]; ok {
-					persistentVolume.Labels[corev1.LabelTopologyRegion] = v
-				}
-				if v, ok := persistentVolume.Labels[corev1.LabelFailureDomainBetaZone]; ok {
-					persistentVolume.Labels[corev1.LabelTopologyZone] = v
-				}
-			} else if persistentVolume.Spec.NodeAffinity.Required != nil {
-				// when PV has node affinity then we do not need the labels but just need to replace the topology keys
-				// in the node selector term match expressions
-				for i, term := range persistentVolume.Spec.NodeAffinity.Required.NodeSelectorTerms {
-					for j, expression := range term.MatchExpressions {
-						if expression.Key == corev1.LabelFailureDomainBetaRegion {
-							persistentVolume.Spec.NodeAffinity.Required.NodeSelectorTerms[i].MatchExpressions[j].Key = corev1.LabelTopologyRegion
-						}
-
-						if expression.Key == corev1.LabelFailureDomainBetaZone {
-							persistentVolume.Spec.NodeAffinity.Required.NodeSelectorTerms[i].MatchExpressions[j].Key = corev1.LabelTopologyZone
-						}
-					}
-				}
-			}
-
-			// either new topology labels were added above, or node affinity keys were adjusted
-			// in both cases, the old, deprecated topology labels are no longer needed and can be removed
-			delete(persistentVolume.Labels, corev1.LabelFailureDomainBetaRegion)
-			delete(persistentVolume.Labels, corev1.LabelFailureDomainBetaZone)
-
-			// prevent sending empty patches
-			if data, err := patch.Data(&persistentVolume); err != nil {
-				return fmt.Errorf("failed getting patch data for PV %s: %w", persistentVolume.Name, err)
-			} else if string(data) == `{}` {
-				return nil
-			}
-
-			log.Info("Migrating deprecated topology labels", "persistentVolumeName", persistentVolume.Name)
-			return seedClient.Patch(ctx, &persistentVolume, patch)
-		})
-	}
-
-	return flow.Parallel(taskFns...)(ctx)
 }
 
 // TODO(vicwicker): Remove this after v1.128 has been released.
