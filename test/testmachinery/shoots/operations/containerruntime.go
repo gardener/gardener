e04c1dba539a3729767a49f101f1aedaf338be7d
Sonu Kumar Singh <sksgkpvks@gmail.com>
2025-08-13 17:38:26 +0530

    Cleanup redundant TM tests (#12687)
    * Remove disruptive test suite definitions and label
    
    * Remove serial release test suite definition
    
    There is no tests with both release and serial labels
    
    * Remove Gardener beta test suite definitions
    
    There is no tests with these labels
    
    * Remove Gardener default test suite definitions
    
    There is no tests with these labels
    
    * Remove Gardener release serial test suite definition
    
    There is no tests with these labels
    
    * Address Review
    
    * Rework containerd test to pick any node randomly
    
    containerd is only supported cri
    
    * Address Review
    
    * Expose machine image name flag also
    
    Just exposing machine-image-version version doen't help

--- test/testmachinery/shoots/operations/containerruntime.go
+++ test/testmachinery/shoots/operations/containerruntime.go
@@ -11,76 +11,25 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	"github.com/gardener/gardener/pkg/utils"
 	"github.com/gardener/gardener/test/framework"
 )
 
 var _ = Describe("Shoot container runtime testing", func() {
 	f := framework.NewShootFramework(nil)
 
-	f.Default().Serial().CIt("should add worker pool with containerd", func(ctx context.Context) {
-		var (
-			shoot       = f.Shoot
-			worker      = shoot.Spec.Provider.Workers[0]
-			workerImage = worker.Machine.Image
-		)
+	f.Default().Serial().CIt("should verify containerd configuration and runtime setup", func(ctx context.Context) {
+		var shoot = f.Shoot
 
 		if v1beta1helper.IsWorkerless(shoot) {
 			Skip("at least one worker pool is required in the test shoot")
 		}
 
-		if !supportsContainerD(f.CloudProfile.Spec.MachineImages, workerImage) {
-			message := fmt.Sprintf("machine image '%s@%s' does not support containerd", workerImage.Name, *workerImage.Version)
-			Skip(message)
-		}
-
-		containerdWorker := worker.DeepCopy()
-
-		allowedCharacters := "0123456789abcdefghijklmnopqrstuvwxyz"
-		id, err := utils.GenerateRandomStringFromCharset(3, allowedCharacters)
+		// check the node labels to contain containerd label
+		nodeList, err := framework.GetAllNodes(ctx, f.ShootClient)
 		framework.ExpectNoError(err)
-
-		containerdWorker.Name = "test-" + id
-		containerdWorker.Maximum = 1
-		containerdWorker.Minimum = 1
-		containerdWorker.CRI = &gardencorev1beta1.CRI{
-			Name:              gardencorev1beta1.CRINameContainerD,
-			ContainerRuntimes: nil,
-		}
-
-		shoot.Spec.Provider.Workers = append(shoot.Spec.Provider.Workers, *containerdWorker)
-
-		By("Add containerd worker pool")
-
-		defer func(ctx context.Context, workerPoolName string) {
-			By("Remove containerd worker pool after test execution")
-			err := f.UpdateShoot(ctx, func(s *gardencorev1beta1.Shoot) error {
-				var workers []gardencorev1beta1.Worker
-				for _, current := range s.Spec.Provider.Workers {
-					if current.Name == workerPoolName {
-						continue
-					}
-					workers = append(workers, current)
-				}
-				s.Spec.Provider.Workers = workers
-				return nil
-			})
-			framework.ExpectNoError(err)
-		}(ctx, containerdWorker.Name)
-
-		err = f.UpdateShoot(ctx, func(s *gardencorev1beta1.Shoot) error {
-			s.Spec.Provider.Workers = shoot.Spec.Provider.Workers
-			return nil
-		})
-		framework.ExpectNoError(err)
-
-		// check the node labels of the worker pool to contain containerd label
-		nodeList, err := framework.GetAllNodesInWorkerPool(ctx, f.ShootClient, &containerdWorker.Name)
-		framework.ExpectNoError(err)
-		Expect(nodeList.Items).To(HaveLen(int(containerdWorker.Minimum)))
+		Expect(nodeList.Items).NotTo(BeEmpty(), "Expected to find at least one Node in the cluster")
 
 		for _, node := range nodeList.Items {
 			value, found := node.Labels[extensionsv1alpha1.CRINameWorkerLabel]
@@ -114,41 +63,4 @@ func executeCommand(ctx context.Context, f *framework.ShootFramework, rootPodExe
 	framework.ExpectNoError(err)
 	Expect(response).ToNot(BeNil())
 	Expect(string(response)).To(Equal(fmt.Sprintf("%s\n", expected)))
-}
-
-func supportsContainerD(cloudProfileImages []gardencorev1beta1.MachineImage, workerImage *gardencorev1beta1.ShootMachineImage) bool {
-	var (
-		cloudProfileImage *gardencorev1beta1.MachineImage
-		machineVersion    *gardencorev1beta1.MachineImageVersion
-	)
-
-	for _, c := range cloudProfileImages {
-		current := c
-		if current.Name == workerImage.Name {
-			cloudProfileImage = &current
-			break
-		}
-	}
-	if cloudProfileImage == nil {
-		return false
-	}
-
-	for _, v := range cloudProfileImage.Versions {
-		version := v
-		if version.Version == *workerImage.Version {
-			machineVersion = &version
-			break
-		}
-	}
-	if machineVersion == nil {
-		return false
-	}
-
-	for _, cri := range machineVersion.CRI {
-		if cri.Name == gardencorev1beta1.CRINameContainerD {
-			return true
-		}
-	}
-
-	return false
 }
