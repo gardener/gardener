aac172f561d4087b1ff9093f989066df825bbcd5
rfranzke <rafael.franzke@sap.com>
2025-08-13 13:58:09 +0200

    Switch gardenadm setup to `kind-single-node`
    Run it with
    
    `make kind-single-node-up gardenadm-up`
    
    --> `make kind-up gardenadm-up` does no longer work

--- hack/ci-e2e-kind-gardenadm.sh
+++ hack/ci-e2e-kind-gardenadm.sh
@@ -14,11 +14,13 @@ clamp_mss_to_pmtu
 
 ensure_glgc_resolves_to_localhost
 
-make kind-up
+make kind-single-node-up
 
+# export all container logs and events after test execution
 trap "
-  ( export_artifacts "gardener-local" )
-  ( make kind-down )
+  ( export KUBECONFIG=$PWD/example/gardener-local/kind/multi-zone/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden)
+  ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/virtual-garden/kubeconfig; export cluster_name='virtual-garden'; export_resource_yamls_for seeds shoots; export_events_for_shoots)
+  ( make kind-multi-zone-down )
 " EXIT
 
 make gardenadm-up SCENARIO=high-touch
