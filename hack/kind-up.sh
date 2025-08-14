a6378e904111e12ceedce291c54d3a90cddc9478
Theodora Tosheva <theodora.tosheva@sap.com>
2025-08-14 13:58:29 +0300

    [kind-local] Add DNS entry for discovery.ingress.runtime-garden.local.gardener.cloud

--- hack/kind-up.sh
+++ hack/kind-up.sh
@@ -368,6 +368,7 @@ kubectl -n kube-system get configmap coredns -ojson | \
       $garden_cluster_ip gardener.virtual-garden.local.gardener.cloud\n\
       $garden_cluster_ip api.virtual-garden.local.gardener.cloud\n\
       $garden_cluster_ip dashboard.ingress.runtime-garden.local.gardener.cloud\n\
+      $garden_cluster_ip discovery.ingress.runtime-garden.local.gardener.cloud\n\
       fallthrough\n\
     }\
 "'/' | \
