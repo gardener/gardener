def14fc834d7a75711a1c8d942d84708d49287c7
Marcel Boehm <marcel.boehm@inovex.de>
2025-08-18 16:41:23 +0200

    Return machine addresses

--- pkg/provider-local/machine-provider/local/get_machine_status.go
+++ pkg/provider-local/machine-provider/local/get_machine_status.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gardener/machine-controller-manager/pkg/util/provider/driver"
 	"github.com/gardener/machine-controller-manager/pkg/util/provider/machinecodes/codes"
 	"github.com/gardener/machine-controller-manager/pkg/util/provider/machinecodes/status"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,5 +38,9 @@ func (d *localDriver) GetMachineStatus(ctx context.Context, req *driver.GetMachi
 	return &driver.GetMachineStatusResponse{
 		ProviderID: pod.Name,
 		NodeName:   pod.Name,
+		Addresses: []corev1.NodeAddress{{
+			Type:    corev1.NodeInternalIP,
+			Address: pod.Status.PodIP,
+		}},
 	}, nil
 }
