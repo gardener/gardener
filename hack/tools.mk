62cdef958637b8995858df114cd1d336444aded9
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-08-27 17:52:43 +0000

    Update dependency kubernetes-sigs/kind to v0.30.0

--- hack/tools.mk
+++ hack/tools.mk
@@ -67,7 +67,7 @@ GO_VULN_CHECK_VERSION ?= latest
 # renovate: datasource=github-releases depName=helm/helm
 HELM_VERSION ?= v3.18.6
 # renovate: datasource=github-releases depName=kubernetes-sigs/kind
-KIND_VERSION ?= v0.29.0
+KIND_VERSION ?= v0.30.0
 # renovate: datasource=github-releases depName=kubernetes/kubernetes
 KUBECTL_VERSION ?= v1.33.4
 # renovate: datasource=github-releases depName=kubernetes-sigs/kustomize
