5d5cfcdcb9994f71b59ac14144b511f719040d31
Dimitar Mirchev <dimitar.mirchev@sap.com>
2025-09-01 10:42:31 +0300

    Rename InternalDNSProviderCredentialsRefEqual to DNSProviderCredentialsRefEqual

--- pkg/apis/core/v1beta1/helper/seed_test.go
+++ pkg/apis/core/v1beta1/helper/seed_test.go
@@ -279,9 +279,9 @@ var _ = Describe("Helper", func() {
 		})
 	})
 
-	DescribeTable("#InternalDNSProviderCredentialsRefEqual",
+	DescribeTable("#DNSProviderCredentialsRefEqual",
 		func(oldDNSProvider, newDNSProvider *gardencorev1beta1.SeedDNSProviderConfig, equal bool) {
-			Expect(InternalDNSProviderCredentialsRefEqual(oldDNSProvider, newDNSProvider)).To(Equal(equal))
+			Expect(DNSProviderCredentialsRefEqual(oldDNSProvider, newDNSProvider)).To(Equal(equal))
 		},
 
 		Entry("both nil", nil, nil, true),
