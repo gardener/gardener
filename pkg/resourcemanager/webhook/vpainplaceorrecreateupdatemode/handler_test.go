8ec0902cabadc5338c9872c52693f1356b0fbabb
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-09-01 14:38:39 +0300

    feat(resourcemanager/webhook): Add a webhook to modify VerticalPodAutoscaler update mode
    This commit adds a new webhook to the resource manager that aims to modify
    VerticalPodAutoscaler resources and set `updateMode` values to the newly
    introduced `InPlaceOrRecreate`.

---
+++ pkg/resourcemanager/webhook/vpainplaceorrecreateupdatemode/handler_test.go
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package vpainplaceorrecreateupdatemode
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
+	"k8s.io/utils/ptr"
+	logzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/gardener/gardener/pkg/logger"
+)
+
+var _ = Describe("Handler", func() {
+	var (
+		ctx     = context.TODO()
+		log     logr.Logger
+		handler *Handler
+
+		vpa *vpaautoscalingv1.VerticalPodAutoscaler
+	)
+
+	BeforeEach(func() {
+		ctx = admission.NewContextWithRequest(ctx, admission.Request{})
+		log = logger.MustNewZapLogger(logger.InfoLevel, logger.FormatJSON, logzap.WriteTo(GinkgoWriter))
+		handler = &Handler{Logger: log}
+
+		vpa = &vpaautoscalingv1.VerticalPodAutoscaler{}
+	})
+
+	Describe("#Default", func() {
+		It("should modify update mode Auto to InPlaceOrRecreate", func() {
+			vpa.Spec.UpdatePolicy = &vpaautoscalingv1.PodUpdatePolicy{
+				UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeAuto),
+			}
+
+			Expect(handler.Default(ctx, vpa)).To(Succeed())
+			Expect(vpa.Spec.UpdatePolicy.UpdateMode).To(Equal(ptr.To(vpaautoscalingv1.UpdateModeInPlaceOrRecreate)))
+		})
+
+		It("should modify update mode Recreate to InPlaceOrRecreate", func() {
+			vpa.Spec.UpdatePolicy = &vpaautoscalingv1.PodUpdatePolicy{
+				UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeRecreate),
+			}
+
+			Expect(handler.Default(ctx, vpa)).To(Succeed())
+			Expect(vpa.Spec.UpdatePolicy.UpdateMode).To(Equal(ptr.To(vpaautoscalingv1.UpdateModeInPlaceOrRecreate)))
+		})
+
+		It("should not modify update mode when it is already set to InPlaceOrRecreate", func() {
+			vpa.Spec.UpdatePolicy = &vpaautoscalingv1.PodUpdatePolicy{
+				UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeInPlaceOrRecreate),
+			}
+
+			Expect(handler.Default(ctx, vpa)).To(Succeed())
+			Expect(vpa.Spec.UpdatePolicy.UpdateMode).To(Equal(ptr.To(vpaautoscalingv1.UpdateModeInPlaceOrRecreate)))
+		})
+
+		It("should modify update mode when update policy is not set", func() {
+			Expect(handler.Default(ctx, vpa)).To(Succeed())
+			Expect(vpa.Spec.UpdatePolicy.UpdateMode).To(Equal(ptr.To(vpaautoscalingv1.UpdateModeInPlaceOrRecreate)))
+		})
+	})
+})
