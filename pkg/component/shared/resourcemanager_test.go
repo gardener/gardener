eb0699ba3be16424326ab6c5132d827272d98f5b
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-09-03 10:24:30 +0300

    feat(shared/resourcemanager): Add default value for `InPlaceOrRecreate` GRM webhook flag
    This commit adds a default value to the `InPlaceOrRecreate` webhook enabling
    flag for the shared GRM utilities.

--- pkg/component/shared/resourcemanager_test.go
+++ pkg/component/shared/resourcemanager_test.go
@@ -70,10 +70,11 @@ var _ = Describe("ResourceManager", func() {
 						"component": "controller",
 					}},
 				},
-				PodTopologySpreadConstraintsEnabled: false,
-				Replicas:                            ptr.To[int32](2),
-				ResourceClass:                       ptr.To("seed"),
-				ResponsibilityMode:                  resourcemanager.ForSource,
+				PodTopologySpreadConstraintsEnabled:   false,
+				VpaInPlaceOrRecreateUpdateModeEnabled: false,
+				Replicas:                              ptr.To[int32](2),
+				ResourceClass:                         ptr.To("seed"),
+				ResponsibilityMode:                    resourcemanager.ForSource,
 			}))
 		})
 
