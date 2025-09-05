5a372702d9751e4f99f8c5dd0e3457eddc5cff1b
Christoph Kleineweber <c.kleineweber@sap.com>
2025-09-05 10:07:22 +0200

    Apply reviewer suggestion

--- pkg/component/networking/istio/istiod.go
+++ pkg/component/networking/istio/istiod.go
@@ -265,7 +265,8 @@ func (i *istiod) Deploy(ctx context.Context) error {
 
 	registry := managedresources.NewRegistry(kubernetes.SeedScheme, kubernetes.SeedCodec, kubernetes.SeedSerializer)
 	for _, istioIngressGateway := range i.values.IngressGateway {
-		if "" != "TODO(istvanballok): remove this block once the issue: 'Istio metrics leak for deleted shoots' #12699 is resolved" {
+		// TODO(istvanballok): remove this block once the issue: 'Istio metrics leak for deleted shoots' #12699 is resolved
+		if true {
 			continue
 		}
 		if err := registry.Add(&monitoringv1.ServiceMonitor{
