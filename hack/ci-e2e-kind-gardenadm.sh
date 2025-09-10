cb7133624146a05cf3e25e12a18de691b6802b86
rfranzke <rafael.franzke@sap.com>
2025-09-10 09:54:13 +0200

    "Separate" medium-touch from high-touch e2e tests
    see comment in code changes and
    https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/12920/pull-gardener-e2e-kind-gardenadm/1965666811680657408

--- hack/ci-e2e-kind-gardenadm.sh
+++ hack/ci-e2e-kind-gardenadm.sh
@@ -14,8 +14,6 @@ clamp_mss_to_pmtu
 
 ensure_glgc_resolves_to_localhost
 
-make kind-single-node-up
-
 # export all container logs and events after test execution
 trap "
   ( export KUBECONFIG=$PWD/example/gardener-local/kind/multi-zone/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden)
@@ -23,12 +21,26 @@ trap "
   ( make kind-multi-zone-down )
 " EXIT
 
-make gardenadm-up SCENARIO=high-touch
+# medium-touch tests cannot run when there is a gardener-operator deployment (i.e., high-touch/connect tests must run
+# separately). Hence, let's run the medium-touch tests first, then clean them up, and then run the high-touch/connect
+# tests.
+
+# medium-touch
+make kind-single-node-up
 make gardenadm-up SCENARIO=medium-touch
+
+make test-e2e-local-gardenadm-medium-touch
+
+make gardenadm-down SCENARIO=medium-touch
+make kind-single-node-down
+
+# high touch
+make kind-single-node-up
+make gardenadm-up SCENARIO=high-touch
 make gardenadm-up SCENARIO=connect
 
-make test-e2e-local-gardenadm
+make test-e2e-local-gardenadm-high-touch
 
 make gardenadm-down SCENARIO=connect
-make gardenadm-down SCENARIO=medium-touch
 make gardenadm-down SCENARIO=high-touch
+make kind-single-node-down
