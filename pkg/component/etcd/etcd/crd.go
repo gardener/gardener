ab4c6dea1e68a12d3badb55b17fcead24a3019ce
Shafeeque E S <shafeeque.e.s@sap.com>
2025-09-17 10:33:06 +0530

    Simplify etcd CRD Getter

--- pkg/component/etcd/etcd/crd.go
+++ pkg/component/etcd/etcd/crd.go
@@ -6,11 +6,12 @@ package etcd
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 
 	"github.com/Masterminds/semver/v3"
 	druidcorecrds "github.com/gardener/etcd-druid/api/core/v1alpha1/crds"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	kubernetesscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/gardener/pkg/component"
@@ -24,40 +25,28 @@ func NewCRD(client client.Client, k8sVersion *semver.Version) (component.DeployW
 	if err != nil {
 		return nil, err
 	}
-	crds, err := crdGetter.GetAllCRDsAsStringSlice()
-	if err != nil {
-		return nil, err
-	}
-	return crddeployer.New(client, crds, true)
+
+	return crddeployer.New(client, crdGetter.GetAllCRDsAsStringSlice(), true)
 }
 
 // CRDGetter provides methods to get CRDs defined in etcd-druid.
 type CRDGetter interface {
 	// GetAllCRDs returns a map of CRD names to CRD objects.
-	GetAllCRDs() map[string]*apiextensionsv1.CustomResourceDefinition
+	GetAllCRDs() (map[string]*apiextensionsv1.CustomResourceDefinition, error)
 	// GetCRD returns the CRD with the given name.
 	// An error is returned if no CRD is found with the given name.
 	GetCRD(name string) (*apiextensionsv1.CustomResourceDefinition, error)
 	// GetAllCRDsAsStringSlice returns all CRDs as Strings.
-	GetAllCRDsAsStringSlice() ([]string, error)
+	GetAllCRDsAsStringSlice() []string
 }
 
 type crdGetterImpl struct {
-	crdResources map[string]*apiextensionsv1.CustomResourceDefinition
+	crdResources map[string]string
 	k8sVersion   *semver.Version
 }
 
-func (c *crdGetterImpl) GetAllCRDsAsStringSlice() ([]string, error) {
-	crds := c.GetAllCRDs()
-	crdStrings := make([]string, 0, len(crds))
-	for _, crd := range crds {
-		crdString, err := kubernetesutils.Serialize(crd, kubernetesscheme.Scheme)
-		if err != nil {
-			return nil, err
-		}
-		crdStrings = append(crdStrings, crdString)
-	}
-	return crdStrings, nil
+func (c *crdGetterImpl) GetAllCRDsAsStringSlice() []string {
+	return slices.Collect(maps.Values(c.crdResources))
 }
 
 var _ CRDGetter = (*crdGetterImpl)(nil)
@@ -74,25 +63,9 @@ func NewCRDGetter(k8sVersion *semver.Version) (CRDGetter, error) {
 	}, nil
 }
 
-func (c *crdGetterImpl) GetAllCRDs() map[string]*apiextensionsv1.CustomResourceDefinition {
-	return c.crdResources
-}
-
-func (c *crdGetterImpl) GetCRD(name string) (*apiextensionsv1.CustomResourceDefinition, error) {
-	crdObj, ok := c.crdResources[name]
-	if !ok {
-		return nil, fmt.Errorf("CRD %s not found", name)
-	}
-	return crdObj, nil
-}
-
-func getEtcdCRDs(k8sVersion *semver.Version) (map[string]*apiextensionsv1.CustomResourceDefinition, error) {
-	crdYAMLs, err := druidcorecrds.GetAll(k8sVersion.String())
-	if err != nil {
-		return nil, fmt.Errorf("failed to get etcd-druid CRDs for Kubernetes version %s: %w", k8sVersion, err)
-	}
-	var crdResources = make(map[string]*apiextensionsv1.CustomResourceDefinition, len(crdYAMLs))
-	for crdName, crdYAML := range crdYAMLs {
+func (c *crdGetterImpl) GetAllCRDs() (map[string]*apiextensionsv1.CustomResourceDefinition, error) {
+	var crdResources = make(map[string]*apiextensionsv1.CustomResourceDefinition)
+	for crdName, crdYAML := range c.crdResources {
 		crdObj, err := kubernetesutils.DecodeCRD(crdYAML)
 		if err != nil {
 			return nil, fmt.Errorf("failed decode etcd-druid CRD: %s: %w", crdName, err)
@@ -100,4 +73,27 @@ func getEtcdCRDs(k8sVersion *semver.Version) (map[string]*apiextensionsv1.Custom
 		crdResources[crdName] = crdObj
 	}
 	return crdResources, nil
+}
+
+func (c *crdGetterImpl) GetCRD(name string) (*apiextensionsv1.CustomResourceDefinition, error) {
+	crdResources, err := c.GetAllCRDs()
+	if err != nil {
+		return nil, err
+	}
+
+	crdObj, ok := crdResources[name]
+	if !ok {
+		return nil, fmt.Errorf("required CRD not found in map: %s", name)
+	}
+
+	return crdObj, nil
+}
+
+func getEtcdCRDs(k8sVersion *semver.Version) (map[string]string, error) {
+	crdYAMLs, err := druidcorecrds.GetAll(k8sVersion.String())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get etcd-druid CRDs for Kubernetes version %s: %w", k8sVersion, err)
+	}
+
+	return crdYAMLs, nil
 }
