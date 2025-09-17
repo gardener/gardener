9f389c1e4cc93fc27c59eb108b737ebc4de60e64
rfranzke <rafael.franzke@sap.com>
2025-09-17 10:57:14 +0200

    Prefactor: Make auth helper functions generic
    Previously, they were hard-coded to the "seed" scenario, but soon we
    will need to use them for the "shoot" scenario

--- pkg/admissioncontroller/webhook/auth/authorizer.go
+++ pkg/admissioncontroller/webhook/auth/authorizer.go
@@ -40,22 +40,22 @@ func CheckSubresource(log logr.Logger, attrs auth.Attributes, allowedSubresource
 }
 
 type authzRequest struct {
-	allowedVerbs          sets.Set[string]
-	alwaysAllowedVerbs    sets.Set[string]
-	allowedSubresources   sets.Set[string]
-	listWatchSeedSelector seedSelector
+	allowedVerbs        sets.Set[string]
+	alwaysAllowedVerbs  sets.Set[string]
+	allowedSubresources sets.Set[string]
+	listWatchSelector   selector
 }
 
 func newAuthzRequest() *authzRequest {
 	return &authzRequest{
-		allowedVerbs:          sets.Set[string]{},
-		alwaysAllowedVerbs:    sets.Set[string]{},
-		allowedSubresources:   sets.Set[string]{},
-		listWatchSeedSelector: seedSelector{fieldNames: sets.Set[string]{}, labelKeys: sets.Set[string]{}},
+		allowedVerbs:        sets.Set[string]{},
+		alwaysAllowedVerbs:  sets.Set[string]{},
+		allowedSubresources: sets.Set[string]{},
+		listWatchSelector:   selector{fieldNames: sets.Set[string]{}, labelKeys: sets.Set[string]{}},
 	}
 }
 
-type seedSelector struct {
+type selector struct {
 	fieldNames sets.Set[string]
 	labelKeys  sets.Set[string]
 }
@@ -83,55 +83,62 @@ func WithAllowedSubresources(resources ...string) configFunc {
 	}
 }
 
-// WithSeedFieldSelectorFields is a config function for setting the seed field selector fields.
-// TODO: Remove this 'nolint' annotation once the function is used.
+// WithFieldSelectorFields is a config function for setting the field selector fields. Field names are matched against
+// the name of the object the requestor is associated with. For example, if the field name is '.spec.seedName' and the
+// requestor is associated with seed 'foo', then they must send a field selector for '.spec.seedName=foo' in order to be
+// authorized for list/watch requests.
+// TODO(rfranzke): Remove this 'nolint' annotation once the function is used.
 //
 //nolint:unused
-func WithSeedFieldSelectorFields(fieldNames ...string) configFunc {
+func WithFieldSelectorFields(fieldNames ...string) configFunc {
 	return func(req *authzRequest) {
-		req.listWatchSeedSelector.fieldNames.Insert(fieldNames...)
+		req.listWatchSelector.fieldNames.Insert(fieldNames...)
 	}
 }
 
-// WithSeedLabelSelectorKeys is a config function for setting the seed label selector keys.
-// TODO: Remove this 'nolint' annotation once the function is used.
+// WithLabelSelectorKeys is a config function for setting the label selector keys. Label keys must contain the name of
+// the object the requestor is associated with. For example, if the label key is 'name.seed.gardener.cloud/foo' and the
+// requestor is associated with seed 'foo', then they must send a label selector for 'name.seed.gardener.cloud/foo=true'
+// in order to be authorized for list/watch requests.
+// TODO(rfranzke): Remove this 'nolint' annotation once the function is used.
 //
 //nolint:unused
-func WithSeedLabelSelectorKeys(labelKeys ...string) configFunc {
+func WithLabelSelectorKeys(labelKeys ...string) configFunc {
 	return func(req *authzRequest) {
-		req.listWatchSeedSelector.labelKeys.Insert(labelKeys...)
+		req.listWatchSelector.labelKeys.Insert(labelKeys...)
 	}
 }
 
-// AuthorizeRead checks if a read request (get, list, watch) is allowed based on the graph relationships and the
-// provided attributes.
-func AuthorizeRead(
-	log logr.Logger,
-	g graph.Interface,
-	authorizeWithSelectors authorizerwebhook.WithSelectorsChecker,
-	seedName string,
-	fromType graph.VertexType,
-	attrs auth.Attributes,
-) (auth.Decision, string, error) {
-	return Authorize(log, g, authorizeWithSelectors, seedName, fromType, attrs,
-		WithAllowedVerbs("get", "list", "watch"),
+// RequestAuthorizer contains common fields that can be used to authorize requests based on graph relationships.
+type RequestAuthorizer struct {
+	Log                    logr.Logger
+	Graph                  graph.Interface
+	AuthorizeWithSelectors authorizerwebhook.WithSelectorsChecker
+
+	ToType      graph.VertexType
+	ToNamespace string
+	ToName      string
+}
+
+// CheckRead checks if a read request (get, list, watch) is allowed based on the graph relationships and the provided
+// attributes.
+func (a *RequestAuthorizer) CheckRead(fromType graph.VertexType, attrs auth.Attributes) (auth.Decision, string, error) {
+	return a.Check(fromType, attrs,
+		WithAllowedVerbs("list", "watch", "get"),
 	)
 }
 
-// Authorize checks if a request is allowed based on the graph relationships and the provided attributes.
-func Authorize(
-	log logr.Logger,
-	g graph.Interface,
-	authorizeWithSelectors authorizerwebhook.WithSelectorsChecker,
-	seedName string,
-	fromType graph.VertexType,
-	attrs auth.Attributes,
-	fns ...configFunc,
-) (
-	auth.Decision,
-	string,
-	error,
-) {
+// Check checks if a request is allowed based on the graph relationships and the provided attributes.
+func (a *RequestAuthorizer) Check(fromType graph.VertexType, attrs auth.Attributes, fns ...configFunc) (auth.Decision, string, error) {
+	log := a.Log.WithValues(
+		"fromType", graph.VertexTypes[fromType].Kind,
+		"fromNamespace", attrs.GetNamespace(),
+		"fromName", attrs.GetName(),
+		"toType", graph.VertexTypes[a.ToType].Kind,
+		"toNamespace", a.ToNamespace,
+		"toName", a.ToName,
+	)
+
 	req := newAuthzRequest()
 	for _, f := range fns {
 		f(req)
@@ -142,8 +149,8 @@ func Authorize(
 	}
 
 	// When a new object is created then it doesn't yet exist in the graph, so usually such requests are always allowed
-	// as the 'create case' is typically handled in the SeedRestriction admission handler. Similarly, resources for
-	// which the gardenlet has a controller need to be listed/watched, so those verbs would also be allowed here.
+	// as the 'create case' is typically handled in the respective restriction admission handler. Similarly, resources
+	// for which the gardenlet has a controller need to be listed/watched, so those verbs would also be allowed here.
 	if req.alwaysAllowedVerbs.Has(attrs.GetVerb()) {
 		return auth.DecisionAllow, "", nil
 	}
@@ -156,29 +163,29 @@ func Authorize(
 		// A resource name is also set when a specific object is read with `.metadata.name` field selector (e.g., in the
 		// single object cache), even for the list verb.
 		// If we have a resource name then we want to consult the graph. There is an exception, though, which is when
-		// the request specifies `.metadata.name` as field name for a seed selector. This means that the client wants
-		// to list/watch the resource with the seed name as field selector. This is a valid scenario and needs to be
+		// the request specifies `.metadata.name` as field name for a selector. This means that the client wants to
+		// list/watch the resource with the object name as field selector. This is a valid scenario and needs to be
 		// handled by the below check function.
-		(attrs.GetName() == "" || req.listWatchSeedSelector.fieldNames.Has(metav1.ObjectNameField)) {
-		if canAuthorizeWithSelectors, err := authorizeWithSelectors.IsPossible(); err != nil {
+		(attrs.GetName() == "" || req.listWatchSelector.fieldNames.Has(metav1.ObjectNameField)) {
+		if canAuthorizeWithSelectors, err := a.AuthorizeWithSelectors.IsPossible(); err != nil {
 			return auth.DecisionNoOpinion, "", fmt.Errorf("failed checking if authorization with selectors is possible: %w", err)
 		} else if canAuthorizeWithSelectors {
-			if ok, reason := checkListWatchRequests(attrs, seedName, req.listWatchSeedSelector); !ok {
-				log.Info("Denying list/watch request because field/label selectors don't select seed name", "fieldNames", req.listWatchSeedSelector.fieldNames, "labelKeys", req.listWatchSeedSelector.labelKeys)
+			if ok, reason := a.checkListWatchRequests(attrs, req.listWatchSelector); !ok {
+				log.Info("Denying list/watch request because field/label selectors don't the 'to object'", "fieldNames", req.listWatchSelector.fieldNames, "labelKeys", req.listWatchSelector.labelKeys)
 				return auth.DecisionNoOpinion, reason, nil
 			} else {
 				return auth.DecisionAllow, "", nil
 			}
-		} else if len(req.listWatchSeedSelector.labelKeys) > 0 || len(req.listWatchSeedSelector.fieldNames) > 0 {
+		} else if len(req.listWatchSelector.labelKeys) > 0 || len(req.listWatchSelector.fieldNames) > 0 {
 			// TODO(rfranzke): Remove this else-if branch once the lowest supported Kubernetes version is 1.34.
 			return auth.DecisionAllow, "", nil
 		}
 	}
 
-	return hasPathFrom(log, g, seedName, fromType, attrs)
+	return a.hasPathFrom(log, fromType, attrs)
 }
 
-func checkListWatchRequests(attrs auth.Attributes, seedName string, seedSelector seedSelector) (bool, string) {
+func (a *RequestAuthorizer) checkListWatchRequests(attrs auth.Attributes, selector selector) (bool, string) {
 	// The authorization request originates from the kube-apiserver. It has already parsed the field/label selector
 	// and converted it to {fields,labels}.Requirements. Hence, it is safe to ignore the error here. Furthermore, we
 	// require at least one selector. When the parsing failed, the list of selectors would be empty, resulting in
@@ -188,24 +195,24 @@ func checkListWatchRequests(attrs auth.Attributes, seedName string, seedSelector
 
 	for _, req := range fieldSelectorRequirements {
 		if (req.Operator == selection.Equals || req.Operator == selection.DoubleEquals || req.Operator == selection.In) &&
-			req.Value == seedName &&
-			seedSelector.fieldNames.Has(req.Field) {
-			return true, "field selector provided and matches seed name"
+			req.Value == a.ToName &&
+			selector.fieldNames.Has(req.Field) {
+			return true, "field selector provided and matches name of 'to object'"
 		}
 	}
 
 	for _, req := range labelSelectorRequirements {
-		for key := range seedSelector.labelKeys {
+		for key := range selector.labelKeys {
 			if req.Matches(labels.Set{key: "true"}) {
-				return true, "label selector provided and matches seed name"
+				return true, "label selector provided and matches  name of 'to object'"
 			}
 		}
 	}
 
-	return false, "must specify field or label selector for seed name " + seedName
+	return false, fmt.Sprintf("must specify field or label selector for name %s", a.ToName)
 }
 
-func hasPathFrom(log logr.Logger, g graph.Interface, seedName string, fromType graph.VertexType, attrs auth.Attributes) (auth.Decision, string, error) {
+func (a *RequestAuthorizer) hasPathFrom(log logr.Logger, fromType graph.VertexType, attrs auth.Attributes) (auth.Decision, string, error) {
 	if len(attrs.GetName()) == 0 {
 		log.Info("Denying authorization because attributes are missing object name")
 		return auth.DecisionNoOpinion, "No Object name found", nil
@@ -220,13 +227,13 @@ func hasPathFrom(log logr.Logger, g graph.Interface, seedName string, fromType g
 
 	// If the vertex does not exist in the graph (i.e., the resource does not exist in the system) then we allow the
 	// request.
-	if attrs.GetVerb() == "delete" && !g.HasVertex(fromType, namespace, attrs.GetName()) {
+	if attrs.GetVerb() == "delete" && !a.Graph.HasVertex(fromType, namespace, attrs.GetName()) {
 		return auth.DecisionAllow, "", nil
 	}
 
-	if !g.HasPathFrom(fromType, namespace, attrs.GetName(), graph.VertexTypeSeed, "", seedName) {
-		log.Info("Denying authorization because no relationship is found between seed and object")
-		return auth.DecisionNoOpinion, fmt.Sprintf("no relationship found between seed '%s' and this object", seedName), nil
+	if !a.Graph.HasPathFrom(fromType, namespace, attrs.GetName(), a.ToType, a.ToNamespace, a.ToName) {
+		log.Info("Denying authorization because no relationship is found")
+		return auth.DecisionNoOpinion, "no relationship found", nil
 	}
 
 	return auth.DecisionAllow, "", nil
