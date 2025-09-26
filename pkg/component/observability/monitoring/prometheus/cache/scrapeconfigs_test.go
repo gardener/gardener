78929a7fbd7d0a583a1f4becf9b101fb7a2c4a60
Victor Herrero Otal <victor.herrero.otal@sap.com>
2025-09-26 11:21:43 +0200

    Add health recording rules to cache Prometheus
    Recording rules tests are moved to the component package to get access
    to cache Prometheus internals. This follows the same pattern used for
    garden and longterm Prometheus.
    
    This commit also adds a new "prometheus-cache" scrape job for Prometheus
    to scrape itself for the corresponding health check rules.
    
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>

--- pkg/component/observability/monitoring/prometheus/cache/scrapeconfigs_test.go
+++ pkg/component/observability/monitoring/prometheus/cache/scrapeconfigs_test.go
@@ -7,11 +7,37 @@ package cache_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	monitoringv1alpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 
 	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheus/cache"
 )
 
-var _ = Describe("PrometheusRules", func() {
+var _ = Describe("ScrapeConfigs", func() {
+	Describe("#CentralScrapeConfigs", func() {
+		It("should return the expected objects", func() {
+			Expect(cache.CentralScrapeConfigs()).To(HaveExactElements(
+				&monitoringv1alpha1.ScrapeConfig{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "prometheus-cache",
+					},
+					Spec: monitoringv1alpha1.ScrapeConfigSpec{
+						RelabelConfigs: []monitoringv1.RelabelConfig{{
+							Action:      "replace",
+							Replacement: ptr.To("prometheus-cache"),
+							TargetLabel: "job",
+						}},
+						StaticConfigs: []monitoringv1alpha1.StaticConfig{{
+							Targets: []monitoringv1alpha1.Target{"localhost:9090"},
+						}},
+					},
+				},
+			))
+		})
+	})
+
 	Describe("#AdditionalScrapeConfigs", func() {
 		When("seedIsShoot", func() {
 			It("should return the expected objects  (with TLS verification skipped)", func() {
