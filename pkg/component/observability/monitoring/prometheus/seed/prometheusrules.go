83b475963c37f2019eeee262489b103c3f5ed1c7
Victor Herrero Otal <victor.herrero.otal@sap.com>
2025-09-26 11:46:46 +0200

    (seed) Add health recording rules
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>

--- pkg/component/observability/monitoring/prometheus/seed/prometheusrules.go
+++ pkg/component/observability/monitoring/prometheus/seed/prometheusrules.go
@@ -8,9 +8,26 @@ import (
 	_ "embed"
 
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+
+	monitoringutils "github.com/gardener/gardener/pkg/component/observability/monitoring/utils"
 )
+
+var (
+	//go:embed assets/prometheusrules/health.yaml
+	healthYAML []byte
+	health     *monitoringv1.PrometheusRule
+)
+
+func init() {
+	health = &monitoringv1.PrometheusRule{}
+	utilruntime.Must(runtime.DecodeInto(monitoringutils.Decoder, healthYAML, health))
+}
 
 // CentralPrometheusRules returns the central PrometheusRule resources for the long-term prometheus.
 func CentralPrometheusRules() []*monitoringv1.PrometheusRule {
-	return []*monitoringv1.PrometheusRule{}
+	return []*monitoringv1.PrometheusRule{
+		health.DeepCopy(),
+	}
 }
--- pkg/component/observability/monitoring/prometheus/seed/prometheusrules_test.go
+++ pkg/component/observability/monitoring/prometheus/seed/prometheusrules_test.go
@@ -7,12 +7,22 @@ package seed
 import (
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+
+	"github.com/gardener/gardener/pkg/component/test"
 )
 
 var _ = ginkgo.Describe("PrometheusRules", func() {
 	ginkgo.Describe("#CentralPrometheusRules", func() {
 		ginkgo.It("should return the expected objects", func() {
-			Expect(CentralPrometheusRules()).To(HaveExactElements())
+			Expect(CentralPrometheusRules()).To(HaveExactElements(
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"TypeMeta":   MatchFields(IgnoreExtras, Fields{"APIVersion": Equal("monitoring.coreos.com/v1"), "Kind": Equal("PrometheusRule")}),
+					"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("health")}),
+				})),
+			))
+
+			test.PrometheusRule(health, "testdata/health.prometheusrule.test.yaml")
 		})
 	})
 })
