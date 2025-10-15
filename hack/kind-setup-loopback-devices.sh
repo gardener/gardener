c54eeffd5174a030ada531d1003588c183edc9f7
Lukas Hoehl <lukas.hoehl.316@gmail.com>
2025-10-15 15:07:10 +0200

    check loopback on host and container host (#13013)
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- hack/kind-setup-loopback-devices.sh
+++ hack/kind-setup-loopback-devices.sh
@@ -75,14 +75,39 @@ if ! command -v ip &>/dev/null; then
   return
 fi
 
-LOOPBACK_DEVICE=$(ip address | grep LOOPBACK | sed "s/^[0-9]\+: //g" | awk '{print $1}' | sed "s/:$//g")
-echo "Checking loopback device ${LOOPBACK_DEVICE}..."
-for address in "${LOOPBACK_IP_ADDRESSES[@]}"; do
-  if ip address show dev ${LOOPBACK_DEVICE} | grep -q $address/; then
-    echo "IP address $address already assigned to ${LOOPBACK_DEVICE}."
-  else
-    echo "Adding IP address $address to ${LOOPBACK_DEVICE}..."
-    ${SUDO}ip address add "$address" dev "${LOOPBACK_DEVICE}"
-  fi
+# In some scenarios a developer might run the kind cluster with a docker daemon that is running inside a VM.
+#
+# Most people are using Docker Desktop which is able to mirror the IP addresses 
+# added to the loopback device on the host into it's managed VM running the docker daemon.
+#
+# However if people are not using this approach,
+# docker might not be able to start the kind containers as the IP
+# is not visible for it since it's only attached to the loopback device on the host.
+#
+# We test the loopback devices by checking on the host directly as well as checking 
+# the loopback on the docker host using an container that is running in the host network.
+
+container_ip() {
+	docker run --rm --cap-add NET_ADMIN --network=host alpine ip $@
+}
+
+for ip_func in "ip" "container_ip"; do
+  LOOPBACK_DEVICE=$(${ip_func} address | grep LOOPBACK | sed "s/^[0-9]\+: //g" | awk '{print $1}' | sed "s/:$//g")
+  echo "Checking loopback device ${LOOPBACK_DEVICE} with ${ip_func}..."
+
+  for address in "${LOOPBACK_IP_ADDRESSES[@]}"; do
+    if ${ip_func} address show dev ${LOOPBACK_DEVICE} | grep -q $address/; then
+      echo "IP address $address already assigned to ${LOOPBACK_DEVICE}."
+    else
+      echo "Adding IP address $address to ${LOOPBACK_DEVICE}..."
+      if [[ ${ip_func} == "ip" ]]; then
+        sudo_ip_func=${SUDO}${ip_func}
+      else 
+        sudo_ip_func=${ip_func}
+      fi
+      ${sudo_ip_func} address add "$address" dev "${LOOPBACK_DEVICE}"
+    fi
+  done
+
+  echo "Setting up loopback device ${LOOPBACK_DEVICE} with ${ip_func} completed."
 done
-echo "Setting up loopback device ${LOOPBACK_DEVICE} completed."
