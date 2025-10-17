01863eebc19b3dfba8c62767b09309366606cb21
Stefan Majer <stefan.majer@f-i-ts.de>
2025-10-17 14:12:01 +0200

    Allow disable k8s version check by setting a env variable

--- pkg/utils/validation/kubernetesversion/version_test.go
+++ pkg/utils/validation/kubernetesversion/version_test.go
@@ -5,6 +5,8 @@
 package kubernetesversion_test
 
 import (
+	"os"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	gomegatypes "github.com/onsi/gomega/types"
@@ -25,5 +27,21 @@ var _ = Describe("Version", func() {
 		Entry("1.33", "1.33", Succeed()),
 		Entry("1.34", "1.34", Succeed()),
 		Entry("1.35", "1.35", MatchError(ContainSubstring("unsupported kubernetes version"))),
+	)
+	DescribeTable("#UnsupportedButCheckDisabledByEnvVariable",
+		func(env, gitVersion string, matcher gomegatypes.GomegaMatcher) {
+			os.Setenv(DISABLE_VERSION_CHECK_ENV, env)
+			Expect(CheckIfSupported(gitVersion)).To(matcher)
+		},
+
+		Entry("1.28", "", "1.28", MatchError(ContainSubstring("unsupported kubernetes version"))),
+		Entry("1.29", "", "1.29", Succeed()),
+		Entry("1.30", "", "1.30", Succeed()),
+		Entry("1.31", "", "1.31", Succeed()),
+		Entry("1.32", "", "1.32", Succeed()),
+		Entry("1.33", "", "1.33", Succeed()),
+		Entry("1.34", "", "1.34", MatchError(ContainSubstring("unsupported kubernetes version"))),
+		Entry("1.34", "true", "1.34", Succeed()),
+		Entry("1.28", "true", "1.28", Succeed()),
 	)
 })
