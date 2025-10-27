235ab4b7ff62fc2a6049497cfae0c0f063a9bfeb
Radoslav Hubenov <rrhubenov@gmail.com>
2025-10-27 16:16:50 +0200

    CRD Deployer + tests

---
+++ pkg/component/observability/logging/victoria/operator/crd.go
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package operator
+
+import (
+	_ "embed"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/gardener/pkg/component"
+	"github.com/gardener/gardener/pkg/component/crddeployer"
+)
+
+var (
+	//go:embed templates/crd-operator.victoriametrics.com_vlagents.yaml
+	crdVLAgents string
+	//go:embed templates/crd-operator.victoriametrics.com_vlclusters.yaml
+	crdVLClusters string
+	//go:embed templates/crd-operator.victoriametrics.com_vlogs.yaml
+	crdVLogs string
+	//go:embed templates/crd-operator.victoriametrics.com_vlsingles.yaml
+	crdVLSingles string
+)
+
+// NewCRDs can be used to deploy victoria-operator CRDs.
+func NewCRDs(client client.Client) (component.DeployWaiter, error) {
+	resources := []string{
+		crdVLAgents,
+		crdVLClusters,
+		crdVLogs,
+		crdVLSingles,
+	}
+	return crddeployer.New(client, resources, false)
+}
