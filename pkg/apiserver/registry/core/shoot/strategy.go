ec346e182e8edd3a8b4c0dad928419b07bf2d5c7
Aleksandar Savchev <aleksandar.savchev@sap.com>
2025-10-28 13:29:05 +0200

    Add on read decoder for Shoot status field

--- pkg/apiserver/registry/core/shoot/strategy.go
+++ pkg/apiserver/registry/core/shoot/strategy.go
@@ -81,7 +81,7 @@ func (shootStrategy) PrepareForUpdate(_ context.Context, obj, old runtime.Object
 	}
 
 	// Migrate encryptedResources status from `.status.encryptedResources` to `status.credentials.encryptionAtRest.resources`.
-	syncEncryptedResourcesStatus(newShoot)
+	SyncEncryptedResourcesStatus(newShoot)
 }
 
 func mustIncreaseGeneration(oldShoot, newShoot *core.Shoot) bool {
@@ -244,7 +244,7 @@ func (shootStatusStrategy) PrepareForUpdate(_ context.Context, obj, old runtime.
 	}
 
 	// Migrate encryptedResources status from `.status.encryptedResources` to `status.credentials.encryptionAtRest.resources`.
-	syncEncryptedResourcesStatus(newShoot)
+	SyncEncryptedResourcesStatus(newShoot)
 }
 
 func (shootStatusStrategy) ValidateUpdate(_ context.Context, obj, old runtime.Object) field.ErrorList {
@@ -356,8 +356,10 @@ func getStatusSeedName(shoot *core.Shoot) string {
 	return *shoot.Status.SeedName
 }
 
+// SyncEncryptedResourcesStatus ensures the status fields shoot.status.encryptedResources and
+// shoot.status.credentials.encryptionAtRest.resources are in sync.
 // TODO(AleksandarSavchev): Remove this function with the removal of the `.status.encryptedResources` field.
-func syncEncryptedResourcesStatus(shoot *core.Shoot) {
+func SyncEncryptedResourcesStatus(shoot *core.Shoot) {
 	if len(shoot.Status.EncryptedResources) == 0 && shoot.Status.Credentials == nil {
 		return
 	} else if shoot.Status.Credentials == nil {
