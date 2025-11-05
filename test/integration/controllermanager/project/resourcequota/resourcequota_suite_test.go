b942ada37eaef749bbf2fefc0239be000a8bac80
Tobias Schlicht <tobias.schlicht@sap.com>
2025-11-05 08:27:53 +0100

    Add integration tests

---
+++ test/integration/controllermanager/project/resourcequota/resourcequota_suite_test.go
@@ -0,0 +1,122 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package resourcequota_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"k8s.io/client-go/rest"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	controllerconfig "sigs.k8s.io/controller-runtime/pkg/config"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"github.com/gardener/gardener/pkg/api/indexer"
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	controllermanagerconfigv1alpha1 "github.com/gardener/gardener/pkg/controllermanager/apis/config/v1alpha1"
+	"github.com/gardener/gardener/pkg/controllermanager/controller/project/resourcequota"
+	"github.com/gardener/gardener/pkg/logger"
+	"github.com/gardener/gardener/pkg/utils"
+	gardenerenvtest "github.com/gardener/gardener/test/envtest"
+	"github.com/gardener/gardener/test/utils/namespacefinalizer"
+)
+
+func TestProjectResourceQuota(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test Integration ControllerManager Project ResourceQuota Suite")
+}
+
+const testID = "project-resourcequota-controller-test"
+
+var (
+	ctx = context.Background()
+	log logr.Logger
+
+	restConfig *rest.Config
+	testEnv    *gardenerenvtest.GardenerTestEnvironment
+	testClient client.Client
+	mgrClient  client.Client
+
+	testRunID string
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(logger.MustNewZapLogger(logger.DebugLevel, logger.FormatJSON, zap.WriteTo(GinkgoWriter)))
+	log = logf.Log.WithName(testID)
+
+	By("Start test environment")
+	testEnv = &gardenerenvtest.GardenerTestEnvironment{
+		GardenerAPIServer: &gardenerenvtest.GardenerAPIServer{
+			Args: []string{"--disable-admission-plugins=DeletionConfirmation,ResourceReferenceManager,ExtensionValidator,ExtensionLabels,ShootQuotaValidator,ShootValidator,ShootTolerationRestriction,ShootDNS"},
+		},
+	}
+
+	var err error
+	restConfig, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(restConfig).NotTo(BeNil())
+
+	DeferCleanup(func() {
+		By("Stop test environment")
+		Expect(testEnv.Stop()).To(Succeed())
+	})
+
+	By("Create test client")
+	testClient, err = client.New(restConfig, client.Options{Scheme: kubernetes.GardenScheme})
+	Expect(err).NotTo(HaveOccurred())
+
+	testRunID = utils.ComputeSHA256Hex([]byte(uuid.NewUUID()))[:8]
+	log.Info("Using test run ID for test", "testRunID", testRunID)
+
+	By("Setup manager")
+	mgr, err := manager.New(restConfig, manager.Options{
+		Scheme:  kubernetes.GardenScheme,
+		Metrics: metricsserver.Options{BindAddress: "0"},
+		Cache: cache.Options{
+			DefaultLabelSelector: labels.SelectorFromSet(labels.Set{testID: testRunID}),
+		},
+		Controller: controllerconfig.Controller{
+			SkipNameValidation: ptr.To(true),
+		},
+	})
+	Expect(err).NotTo(HaveOccurred())
+	mgrClient = mgr.GetClient()
+
+	By("Setup field indexes")
+	Expect(indexer.AddProjectNamespace(ctx, mgr.GetFieldIndexer())).To(Succeed())
+
+	By("Register controller")
+	Expect((&resourcequota.Reconciler{
+		Config: controllermanagerconfigv1alpha1.ProjectControllerConfiguration{
+			ConcurrentSyncs: ptr.To(5),
+		},
+	}).AddToManager(mgr)).To(Succeed())
+	// The project controller waits for namespaces to be gone, so we need to finalize them as envtest doesn't run the
+	// namespace controller.
+	Expect((&namespacefinalizer.Reconciler{}).AddToManager(mgr)).To(Succeed())
+
+	By("Start manager")
+	mgrContext, mgrCancel := context.WithCancel(ctx)
+
+	go func() {
+		defer GinkgoRecover()
+		Expect(mgr.Start(mgrContext)).To(Succeed())
+	}()
+
+	DeferCleanup(func() {
+		By("Stop manager")
+		mgrCancel()
+	})
+})
---
+++ test/integration/controllermanager/project/resourcequota/resourcequota_test.go
@@ -0,0 +1,242 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package resourcequota_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	"github.com/gardener/gardener/pkg/utils"
+	. "github.com/gardener/gardener/pkg/utils/test/matchers"
+)
+
+var _ = Describe("Project ResourceQuota controller tests", func() {
+	var project *gardencorev1beta1.Project
+	var testNamespace *corev1.Namespace
+
+	BeforeEach(func() {
+		By("Create test Namespace")
+		testNamespace = &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				// create dedicated namespace for each test run, so that we can run multiple tests concurrently for stress tests
+				GenerateName: "garden-" + testID + "-",
+				Labels:       map[string]string{testID: testRunID},
+			},
+		}
+		Expect(testClient.Create(ctx, testNamespace)).To(Succeed())
+		log.Info("Created Namespace for test", "namespaceName", testNamespace.Name)
+
+		DeferCleanup(func() {
+			By("Delete test Namespace")
+			Expect(testClient.Delete(ctx, testNamespace)).To(Or(Succeed(), BeNotFoundError()))
+		})
+
+		project = &gardencorev1beta1.Project{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   "test-" + utils.ComputeSHA256Hex([]byte(testRunID + CurrentSpecReport().LeafNodeLocation.String()))[:5],
+				Labels: map[string]string{testID: testRunID},
+			},
+			Spec: gardencorev1beta1.ProjectSpec{
+				Namespace: &testNamespace.Name,
+			},
+		}
+
+		By("Create Project")
+		Expect(testClient.Create(ctx, project)).To(Succeed())
+		log.Info("Created Project", "project", client.ObjectKeyFromObject(project))
+
+		Eventually(func() error {
+			return mgrClient.Get(ctx, client.ObjectKeyFromObject(project), &gardencorev1beta1.Project{})
+		}).Should(Succeed())
+
+		DeferCleanup(func() {
+			By("Delete Project")
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, project))).To(Succeed())
+
+			By("Wait for Project to be gone")
+			Eventually(func() error {
+				return testClient.Get(ctx, client.ObjectKeyFromObject(project), project)
+			}).Should(BeNotFoundError())
+		})
+	})
+
+	It("should adapt the ResourceQuota if it's limits contradict the specified configmap and secret limits", func() {
+		By("Create ResourceQuota that has contradicting limits")
+		resourceQuota := &corev1.ResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-resourcequota-",
+				Namespace:    testNamespace.Name,
+				Labels:       map[string]string{testID: testRunID},
+			},
+			Spec: corev1.ResourceQuotaSpec{
+				Hard: corev1.ResourceList{
+					"count/shoots.core.gardener.cloud": resource.MustParse("2"),
+					"count/secrets":                    resource.MustParse("1"),
+					"count/configmaps":                 resource.MustParse("1"),
+				},
+			},
+		}
+
+		Expect(testClient.Create(ctx, resourceQuota)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, resourceQuota))).To(Succeed())
+		})
+
+		expectedHard := corev1.ResourceList{
+			"count/shoots.core.gardener.cloud": resource.MustParse("2"),
+			"count/secrets":                    resource.MustParse("8"),
+			"count/configmaps":                 resource.MustParse("4"),
+		}
+
+		Eventually(ctx, func(g Gomega) corev1.ResourceList {
+			current := &corev1.ResourceQuota{}
+			g.Expect(mgrClient.Get(ctx, client.ObjectKeyFromObject(resourceQuota), current)).To(Succeed())
+			return current.Spec.Hard
+		}).Should(Equal(expectedHard))
+	})
+
+	It("should dynamically adapt ResourceQuota limits when no Shoot limit is present and Shoots get created", func() {
+		By("Create ResourceQuota without Shoot limit")
+		resourceQuota := &corev1.ResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-resourcequota-",
+				Namespace:    testNamespace.Name,
+				Labels:       map[string]string{testID: testRunID},
+			},
+			Spec: corev1.ResourceQuotaSpec{
+				Hard: corev1.ResourceList{
+					"count/secrets":    resource.MustParse("1"),
+					"count/configmaps": resource.MustParse("1"),
+				},
+			},
+		}
+
+		Expect(testClient.Create(ctx, resourceQuota)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, resourceQuota))).To(Succeed())
+		})
+
+		By("Confirming that ResourceQuota can at least fit one Shoot")
+		assertLimitsUpdated(ctx, resourceQuota, corev1.ResourceList{
+			"count/secrets":    resource.MustParse("4"),
+			"count/configmaps": resource.MustParse("2"),
+		})
+
+		By("Creating a Shoot in the Project namespace")
+		shoot := &gardencorev1beta1.Shoot{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-shoot-",
+				Namespace:    testNamespace.Name,
+				Labels:       map[string]string{testID: testRunID},
+			},
+			Spec: gardencorev1beta1.ShootSpec{
+				Kubernetes: gardencorev1beta1.Kubernetes{
+					Version: "1.33.0",
+				},
+				CloudProfile: &gardencorev1beta1.CloudProfileReference{
+					Name: "dummy-cloudprofile",
+				},
+				Provider: gardencorev1beta1.Provider{
+					Type: "dummy-provider",
+				},
+				Region: "dummy-region",
+			},
+		}
+
+		Expect(testClient.Create(ctx, shoot)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, shoot))).To(Succeed())
+		})
+
+		By("Confirming that ResourceQuota limits have been adapted to fit the new Shoot")
+		assertLimitsUpdated(ctx, resourceQuota, corev1.ResourceList{
+			"count/secrets":    resource.MustParse("8"),
+			"count/configmaps": resource.MustParse("4"),
+		})
+
+		By("Creating another Shoot in the Project namespace")
+		shoot2 := &gardencorev1beta1.Shoot{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-shoot-",
+				Namespace:    testNamespace.Name,
+				Labels:       map[string]string{testID: testRunID},
+			},
+			Spec: gardencorev1beta1.ShootSpec{
+				Kubernetes: gardencorev1beta1.Kubernetes{
+					Version: "1.33.0",
+				},
+				CloudProfile: &gardencorev1beta1.CloudProfileReference{
+					Name: "dummy-cloudprofile",
+				},
+				Provider: gardencorev1beta1.Provider{
+					Type: "dummy-provider",
+				},
+				Region: "dummy-region",
+			},
+		}
+
+		Expect(testClient.Create(ctx, shoot2)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, shoot2))).To(Succeed())
+		})
+
+		By("Confirming that ResourceQuota limits have been adapted to fit the new Shoot")
+		assertLimitsUpdated(ctx, resourceQuota, corev1.ResourceList{
+			"count/secrets":    resource.MustParse("12"),
+			"count/configmaps": resource.MustParse("6"),
+		})
+	})
+
+	It("should not modify ResourceQuota if it already complies with configmap and secret limits", func() {
+		resourceQuota := &corev1.ResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-resourcequota-",
+				Namespace:    testNamespace.Name,
+				Labels:       map[string]string{testID: testRunID},
+			},
+			Spec: corev1.ResourceQuotaSpec{
+				Hard: corev1.ResourceList{
+					"count/shoots.core.gardener.cloud": resource.MustParse("2"),
+					"count/secrets":                    resource.MustParse("40"),
+					"count/configmaps":                 resource.MustParse("40"),
+				},
+			},
+		}
+
+		Expect(testClient.Create(ctx, resourceQuota)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(client.IgnoreNotFound(testClient.Delete(ctx, resourceQuota))).To(Succeed())
+		})
+
+		By("Wait until the ResourceQuota is fetchable by the manager client")
+		Eventually(mgrClient.Get(ctx, client.ObjectKeyFromObject(resourceQuota), resourceQuota)).Should(Succeed())
+
+		By("Ensure that the ResourceQuota is not modified")
+		assertLimitsUnchanged(ctx, resourceQuota)
+	})
+})
+
+func assertLimitsUpdated(ctx context.Context, resourceQuota *corev1.ResourceQuota, expectedLimits corev1.ResourceList) {
+	EventuallyWithOffset(1, func(g Gomega) corev1.ResourceList {
+		current := &corev1.ResourceQuota{}
+		g.Expect(mgrClient.Get(ctx, client.ObjectKeyFromObject(resourceQuota), current)).To(Succeed())
+		return current.Spec.Hard
+	}).Should(Equal(expectedLimits))
+}
+
+func assertLimitsUnchanged(ctx context.Context, resourceQuota *corev1.ResourceQuota) {
+	Consistently(func(g Gomega) corev1.ResourceList {
+		current := &corev1.ResourceQuota{}
+		g.Expect(mgrClient.Get(ctx, client.ObjectKeyFromObject(resourceQuota), current)).To(Succeed())
+		return current.Spec.Hard
+	}, "20s", "1s").Should(Equal(resourceQuota.Spec.Hard))
+}
