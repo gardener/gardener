66c5e058c9db2efda4a96a45482c5258884d67ee
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-11-05 13:07:38 +0100

    Update dependency golangci/golangci-lint to v2.6.1 (#13343)

--- hack/tools.mk
+++ hack/tools.mk
@@ -54,7 +54,7 @@ TYPOS                      := $(TOOLS_BIN_DIR)/typos
 
 # default tool versions
 # renovate: datasource=github-releases depName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION ?= v2.6.0
+GOLANGCI_LINT_VERSION ?= v2.6.1
 # renovate: datasource=github-releases depName=securego/gosec
 GOSEC_VERSION ?= v2.22.10
 # renovate: datasource=github-releases depName=joelanford/go-apidiff
