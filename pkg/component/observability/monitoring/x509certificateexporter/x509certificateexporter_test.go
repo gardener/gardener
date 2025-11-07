03610bf5adce756eb412007a8990c1ab9d524931
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-11-07 11:45:09 +0200

    Adding tests for the `New` method.

--- pkg/component/observability/monitoring/x509certificateexporter/x509certificateexporter_test.go
+++ pkg/component/observability/monitoring/x509certificateexporter/x509certificateexporter_test.go
@@ -2,7 +2,8 @@ package x509certificateexporter_test
 
 import (
 	"context"
-	"fmt"
+	"strings"
+	// "fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -11,15 +12,17 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
-	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	// gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
 	"github.com/gardener/gardener/pkg/component"
-	"github.com/gardener/gardener/pkg/utils/retry"
-	retryfake "github.com/gardener/gardener/pkg/utils/retry/fake"
+
+	// "github.com/gardener/gardener/pkg/utils/retry"
+	// retryfake "github.com/gardener/gardener/pkg/utils/retry/fake"
 	secretsmanager "github.com/gardener/gardener/pkg/utils/secrets/manager"
 	fakesecretsmanager "github.com/gardener/gardener/pkg/utils/secrets/manager/fake"
-	"github.com/gardener/gardener/pkg/utils/test"
-	. "github.com/gardener/gardener/pkg/utils/test/matchers"
+
+	// "github.com/gardener/gardener/pkg/utils/test"
+	// . "github.com/gardener/gardener/pkg/utils/test/matchers"
 
 	x "github.com/gardener/gardener/pkg/component/observability/monitoring/x509certificateexporter"
 )
@@ -41,112 +44,6 @@ var _ = Describe("X509Certificate Exporter", func() {
 		image              = "some-image:latest"
 		priorityClassName  = "some-priority-class"
 		prometheusInstance = "some-prometheus"
-		defaultSuffix      = x.SuffixRuntime
-		configData         = []byte(`
-inCluster:
-    enabled: false
-    secretTypes: 
-        - kubernetes.io/tls
-        - Opaque
-    configMapKeys:
-        - tls.crt
-`)
-		promRuleYAML = func(_ string) string {
-			return ``
-		}
-		serviceYAML = func(name string) string {
-			return `apiVersion: v1
-kind: Service
-metadata:
-  name: ` + name + `
-  namespace: ` + namespace + `
-spec:
-  selector:
-    app.kubernetes.io/name: MyApp
-  ports:
-    - protocol: TCP
-      port: ` + fmt.Sprintf("%d", x.Port) + `
-      targetPort: ` + fmt.Sprintf("%d", x.Port) + `
-`
-		}
-		serviceMonitorYAML = func(name string) string {
-			return `apiVersion: monitoring.coreos.com/v1
-kind: ServiceMonitor
-metadata:
-  name: ` + name + `
-  namespace: default
-  labels:
-    app: gitlab-runner-gitlab-runner
-    release: prometheus
-spec:
-  selector:
-    matchLabels:
-      app: gitlab-runner-gitlab-runner
-  namespaceSelector:
-    # matchNames:
-    # - default
-    any: true
-  endpoints:
-  - port: http-metrics
-    interval: 15s
-`
-		}
-		serviceAccountYAML = func(name string) string {
-			return `apiVersion: v1
-automountServiceAccountToken: false
-kind: ServiceAccount
-metadata:
-	name: ` + name + `
-	namespace: namespace
-`
-		}
-		inClusterRoleYAML = func(name string) string {
-			return `apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  creationTimestamp: "2025-11-04T11:50:21Z"
-  labels:
-    k8s-app: metrics-server
-  name: system:metrics-server
-  resourceVersion: "736"
-  uid: 38369851-829f-401d-9893-bad2d86fa7d2
-rules:
-- apiGroups:
-  - ""
-  resources:
-  - nodes/metrics
-  verbs:
-  - get
-- apiGroups:
-  - ""
-  resources:
-  - pods
-  - nodes
-  verbs:
-  - get
-  - list
-  - watch`
-		}
-		inClusterRoleBindingYAML = func(name string) string {
-			return `apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  creationTimestamp: "2025-11-04T11:50:21Z"
-  labels:
-    k8s-app: metrics-server
-  name: system:metrics-server
-  resourceVersion: "742"
-  uid: d7e5d0d2-d002-42d9-9388-b3ea828cb89e
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: system:metrics-server
-subjects:
-- kind: ServiceAccount
-  name: metrics-server
-  namespace: kube-system
-		`
-		}
 	)
 	BeforeEach(func() {
 		fakeClient = fakeclient.NewClientBuilder().Build()
@@ -160,6 +57,7 @@ subjects:
 				Image:              image,
 				PriorityClassName:  priorityClassName,
 				PrometheusInstance: prometheusInstance,
+				ConfigData:         []byte(`{inCluster: {enabled: true, secretTypes: ["Opaque"]}}`),
 			},
 		)
 		managedResourceSecret = &corev1.Secret{
@@ -174,12 +72,15 @@ subjects:
 				Namespace: namespace,
 			},
 		}
-
+		_ = x509Exporter
+		_ = err
+		_ = managedResource
+		_ = managedResourceSecret
 	})
 	Describe("#New", func() {
 		Context("with invalid target", func() {
 			DescribeTable("should return unsupported cluster type error", func(suffix string) {
-				obj, err := x.New(nil, nil, suffix, x.Values{})
+				obj, err := x.New(nil, nil, "", x.Values{NameSuffix: suffix})
 				Expect(err).To(HaveOccurred())
 				Expect(err).To(MatchError(x.ErrUnsuportedClusterType))
 				Expect(obj).To(BeNil())
@@ -189,50 +90,124 @@ subjects:
 			)
 		})
 		Context("with invalid parameters", func() {
-			DescribeTable("should return errors when config is invalid", func(configData string, expectedErr error) {
-				obj, err := x.New(nil, nil, x.SuffixRuntime, x.Values{
+			DescribeTable("should return errors when config is invalid", func(configData string, expectedErrs ...error) {
+				obj, err := x.New(nil, nil, "", x.Values{
+					NameSuffix: x.SuffixRuntime,
 					ConfigData: []byte(configData)})
 				Expect(err).To(HaveOccurred())
 				Expect(obj).To(BeNil())
-				Expect(err).To(MatchError(expectedErr))
-
+				for _, expErr := range expectedErrs {
+					Expect(err).To(MatchError(ContainSubstring(expErr.Error())))
+				}
 			},
 				Entry("should return missing deploy errors if config is empty", "invlaid yaml", x.ErrInvalidExporterConfigFormat),
 				Entry("should return empty exporter config error if neither incluster nor workergroups enabled", "", x.ErrEmptyExporterConfig),
-				Entry("should return error on invalid alerting config", `{inCluster: {enabled: true}, alertingConfigs: {readErrorsSeverity: 'thereisnosuchseverity'}}`, x.ErrAlertingConfig),
-				Entry("should return error on invalid workergroup config", `{workerGroups: {secretTypes: ['no-such-type']}}`, x.ErrConfigValidationFailed),
+				Entry("should return error on invalid alerting config", `inCluster:
+    enabled: true
+    secretTypes: ["Opaque"]
+alertingConfig:
+    readErrorsSeverity: 'thereisnosuchseverity'
+    certificationExpirationDays: 10
+    certificateRenewalDays: 15
+`, x.ErrAlertingConfig, x.ErrConfigValidationFailed, x.ErrInvalidSeverity),
+				Entry("should return error on invalid workergroup config", `{workerGroups: [{nameSuffix: 'some-suffix'}]}`,
+					x.ErrConfigValidationFailed, x.ErrWorkerGroupsConfig, x.ErrWorkerGroupInvalid,
+				),
+				Entry("should return error when some group is missing volume mounts", `workerGroups: [{nameSuffix: 'some-suffix', selector: {matchLabels: {a: b}}}]`,
+					x.ErrConfigValidationFailed, x.ErrWorkerGroupsConfig, x.ErrWorkerGroupInvalid, x.ErrWorkerGroupMissingMount,
+				),
+				Entry("should return error for multiple nodegroups when some group is missing selector or namesuffix", `workerGroups:
+- {nameSuffix: 'some-suffix', selector: {matchLabels: {a: b}}, mounts: {m1: {path: "tmp"}, m2: {path: "/tmp"} }}
+- {mounts: {m1: {path: "tmp"}, m2: {path: "/tmp"}}}
+`,
+					x.ErrConfigValidationFailed, x.ErrWorkerGroupsConfig, x.ErrWorkerGroupInvalid,
+				),
+
+				Entry("should return error for multiple nodegroups when some group is missing selector or namesuffix", `workerGroups:
+- {nameSuffix: 'some-suffix', selector: {matchLabels: {a: b}}, mounts: {m1: {path: "tmp"}, m2: {path: "/tmp"} }}
+- {nameSuffix: 'some-suffix', selector: {matchLabels: {a: b}}}
+`,
+					x.ErrConfigValidationFailed, x.ErrWorkerGroupsConfig, x.ErrWorkerGroupInvalid, x.ErrWorkerGroupMissingMount, x.ErrMountValidation, x.ErrMountPathNotAbsolute, x.ErrNoMonitorableFiles,
+				),
+				Entry("should return error when monitoring relative paths", `workerGroups:
+- {nameSuffix: 'some-suffix', selector: {matchLabels: {a: b}}, mounts:  {m1: {path: "/tmp", watchDirs: ["abc/"]} }}
+`,
+					x.ErrConfigValidationFailed, x.ErrWorkerGroupsConfig, x.ErrWorkerGroupInvalid, x.ErrMountValidation, x.ErrMountValidationErrors, x.ErrWatchedFileNotAbsolutePath,
+				),
+				Entry("should return error when configmap key is empty", `inCluster:
+  enabled: true
+  secretTypes: ["Opaque"]
+  configMapKeys: [""]
+`,
+					x.ErrConfigValidationFailed, x.ErrEmptyConfigMapKey,
+				),
+				Entry("should return error when configmap key is illegal", `inCluster:
+  enabled: true
+  secretTypes: ["Opaque"]
+  configMapKeys: ["invalid@key"]
+`,
+					x.ErrConfigValidationFailed, x.ErrKeyIsIllegal,
+				),
+				Entry("should return error when configmap key exceeds maximum length", `inCluster:
+  enabled: true
+  secretTypes: ["Opaque"]
+  configMapKeys: ["`+strings.Repeat("a", 254)+`"]
+`,
+					x.ErrConfigValidationFailed, x.ErrConfigMapMaxKeyLenght,
+				),
+				Entry("should return error when includeLabels has invalid key", `inCluster:
+  enabled: true
+  secretTypes: ["Opaque"]
+  includeLabels:
+    "invalid@key": "value"
+`,
+					x.ErrConfigValidationFailed, x.ErrIncludeLabelsInvalid,
+				),
+				Entry("should return error when namespace is invalid", `inCluster:
+  enabled: true
+  secretTypes: ["Opaque"]
+  includeNamespaces: ["invalid..namespace"]
+`,
+					x.ErrConfigValidationFailed, x.ErrInvalidNamespace,
+				),
+				Entry("should return error when secret type is invalid", `inCluster:
+  enabled: true
+  secretTypes: ["InvalidSecretType"]
+`,
+					x.ErrConfigValidationFailed, x.ErrInvalidSecretType,
+				),
 			)
 		})
 	})
-	Describe("#Deploy", func() {
-		JustBeforeEach(func() {
-			Expect(fakeClient.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
-
-			x509Exporter, err = x.New(
-				fakeClient, secretsManager, namespace, x.Values{
-					Image:              image,
-					PriorityClassName:  priorityClassName,
-					PrometheusInstance: prometheusInstance,
-				},
-			)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(x509Exporter.Deploy(ctx)).To(Succeed())
-		})
-
-	})
-
-	Describe("#Destroy", func() {
-		It("should successfully delete all the resources", func() {
-			Expect(fakeClient.Create(ctx, managedResource)).To(Succeed())
-			Expect(fakeClient.Create(ctx, managedResourceSecret)).To(Succeed())
-
-			Expect(x509Exporter.Destroy(ctx)).To(Succeed())
-
-			Expect(fakeClient.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
-			Expect(fakeClient.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(BeNotFoundError())
-		})
-	})
-	Context("waiting functions", func() {
-
-	})
+	// Describe("#Deploy", func() {
+	// 	JustBeforeEach(func() {
+	// 		Expect(fakeClient.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
+	//
+	// 		x509Exporter, err = x.New(
+	// 			fakeClient, secretsManager, namespace, x.Values{
+	// 				Image:              image,
+	// 				PriorityClassName:  priorityClassName,
+	// 				PrometheusInstance: prometheusInstance,
+	// 			},
+	// 		)
+	// 		Expect(err).NotTo(HaveOccurred())
+	// 		Expect(x509Exporter.Deploy(ctx)).To(Succeed())
+	// 	})
+	//
+	// })
+	//
+	// Describe("#Destroy", func() {
+	// 	It("should successfully delete all the resources", func() {
+	// 		Expect(fakeClient.Create(ctx, managedResource)).To(Succeed())
+	// 		Expect(fakeClient.Create(ctx, managedResourceSecret)).To(Succeed())
+	//
+	// 		Expect(x509Exporter.Destroy(ctx)).To(Succeed())
+	//
+	// 		Expect(fakeClient.Get(ctx, client.ObjectKeyFromObject(managedResource), managedResource)).To(BeNotFoundError())
+	// 		Expect(fakeClient.Get(ctx, client.ObjectKeyFromObject(managedResourceSecret), managedResourceSecret)).To(BeNotFoundError())
+	// 	})
+	// })
+	// Context("waiting functions", func() {
+	//
+	// })
 })
