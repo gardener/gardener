efb1a47710d57a168883ed63a6eaec6588ecbf68
oliver-goetz <o.goetz@sap.com>
2025-11-10 13:25:52 +0100

    Deactivate `UnauthenticatedHTTP2DOSMitigation` feature gate for kube-apiservers where IstioTLSTermination is active

--- pkg/operator/controller/garden/garden/components.go
+++ pkg/operator/controller/garden/garden/components.go
@@ -739,6 +739,7 @@ func (r *Reconciler) newKubeAPIServer(
 		v1beta1constants.PriorityClassNameGardenSystem500,
 		true,
 		false,
+		features.DefaultFeatureGate.Enabled(features.IstioTLSTermination),
 		auditWebhookConfig,
 		authenticationWebhookConfig,
 		authorizationWebhookConfigs,
