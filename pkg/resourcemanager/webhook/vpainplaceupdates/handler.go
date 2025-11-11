6f389db5902151e0f8e00eece75c7433ad8fe6a2
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-11-11 17:18:43 +0200

    chore(resourcemanager/webhook): use `kubernetesutils` to setup structured logging

--- pkg/resourcemanager/webhook/vpainplaceupdates/handler.go
+++ pkg/resourcemanager/webhook/vpainplaceupdates/handler.go
@@ -12,6 +12,9 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
 )
 
 // Handler handles admission requests and sets the vpa.spec.updatePolicy.updateMode field in VerticalPodAutoscaler resources.
@@ -20,13 +23,18 @@ type Handler struct {
 }
 
 // Default defaults the update mode of the provided VerticalPodAutoscaler.
-func (h *Handler) Default(_ context.Context, obj runtime.Object) error {
+func (h *Handler) Default(ctx context.Context, obj runtime.Object) error {
 	vpa, ok := obj.(*vpaautoscalingv1.VerticalPodAutoscaler)
 	if !ok {
 		return fmt.Errorf("expected *vpaautoscalingv1.VerticalPodAutoscaler but got %T", obj)
 	}
 
-	log := h.Logger.WithValues("vpa", vpa.GetName(), "namespace", vpa.GetNamespace())
+	req, err := admission.RequestFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	log := h.Logger.WithValues("vpa", kubernetesutils.ObjectKeyForCreateWebhooks(vpa, req))
 
 	if vpa.Spec.UpdatePolicy == nil {
 		vpa.Spec.UpdatePolicy = &vpaautoscalingv1.PodUpdatePolicy{}
