11cf15e3e4f9203c4159a378ec622e87480545e3
Tim Ebert <timebertt@gmail.com>
2025-11-11 14:03:23 +0100

    Adapt error/description of `PreferredAddress`

--- pkg/gardenadm/botanist/machines.go
+++ pkg/gardenadm/botanist/machines.go
@@ -36,7 +36,7 @@ func (b *GardenadmBotanist) GetMachineByIndex(index int) (*machinev1alpha1.Machi
 	return &b.controlPlaneMachines[index], nil
 }
 
-// addressTypePreference when retrieving the SSH Address of a machine. Higher value means higher priority.
+// addressTypePreference when retrieving the SSH Address of a node/machine. Higher value means higher priority.
 // Unknown address types have the lowest priority (0).
 var addressTypePreference = map[corev1.NodeAddressType]int{
 	// internal names have priority, as we jump via a bastion host
@@ -48,11 +48,11 @@ var addressTypePreference = map[corev1.NodeAddressType]int{
 	corev1.NodeHostName: 1,
 }
 
-// PreferredAddress returns the preferred address of the given machine based on addressTypePreference.
-// If the machine has no addresses, an error is returned.
+// PreferredAddress returns the preferred address of the given node/machine addresses based on addressTypePreference.
+// If the node/machine has no addresses, an error is returned.
 func PreferredAddress(addresses []corev1.NodeAddress) (string, error) {
 	if len(addresses) == 0 {
-		return "", fmt.Errorf("no addresses found in status of machine")
+		return "", fmt.Errorf("no addresses found in status")
 	}
 
 	address := slices.MaxFunc(addresses, func(a, b corev1.NodeAddress) int {
