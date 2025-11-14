a26d0af7ed2dfd74a314153f815a2d7f96c4d9dc
rfranzke <rafael.franzke@sap.com>
2025-11-14 16:21:35 +0100

    Check if seed is self-hosted shoot and terminate/don't enable controller
    - This check is mainly relevant for the `gardenlet`s responsible for
      seeds running in the `garden` namespace.
    - If they detect their seed is a self-hosted shoot, then they don't
      enable the controller.
    - If they detect this only later (maybe because the `gardenlet`
      deployment in the `kube-system` namespace does not exist yet because
      the operator has not yet connected the cluster to Gardener, or maybe
      because it got temporarily deleted), the entire gardenlet is
      terminated and restarted (similar approach like for the "seed is
      garden" case).
    
    | Self-Hosted Shoot? | Is Seed? | Is Garden? | Who runs this controller?    |
    | ------------------ | -------- | ---------- | ---------------------------- |
    |         0          |    0     |     0      | nobody                       |
    |         0          |    0     |     1      | `gardener-operator`          |
    |         0          |    1     |     0      | `gardenlet` in `garden`      |
    |         0          |    1     |     1      | `gardener-operator`          |
    |         1          |    0     |     0      | `gardenlet` in `kube-system` |
    |         1          |    0     |     1      | `gardener-operator`          |
    |         1          |    1     |     0      | `gardenlet` in `kube-system` |
    |         1          |    1     |     1      | `gardener-operator`          |

--- pkg/gardenlet/controller/vpaevictionrequirements/add.go
+++ pkg/gardenlet/controller/vpaevictionrequirements/add.go
@@ -35,7 +35,20 @@ func AddToManager(
 		return fmt.Errorf("failed checking whether the seed is the garden cluster: %w", err)
 	}
 	if seedIsGarden {
-		return nil // When the seed is the garden cluster at the same time, the gardener-operator runs this controller.
+		// When the seed is the garden cluster, the gardener-operator runs this controller.
+		return nil
+	}
+
+	if !gardenletutils.IsResponsibleForSelfHostedShoot() {
+		seedIsSelfHostedShoot, err := gardenletutils.SeedIsSelfHostedShoot(ctx, seedCluster.GetAPIReader())
+		if err != nil {
+			return fmt.Errorf("failed checking whether the seed is a self-hosted shoot cluster: %w", err)
+		}
+		if seedIsSelfHostedShoot {
+			// When the seed is a self-hosted shoot cluster, the gardenlet responsible for the self-hosted shoot in the
+			// kube-system namespace runs this controller.
+			return nil
+		}
 	}
 
 	if err := (&vpaevictionrequirements.Reconciler{
@@ -44,25 +57,43 @@ func AddToManager(
 		return err
 	}
 
-	// At this point, the seed is not the garden cluster at the same time. However, this could change during the runtime
-	// of gardenlet. If so, gardener-operator will take over responsibility of the VPA eviction requirements and will run
-	// this controller. Since there is no way to stop a controller after it started, we cancel the manager context in
-	// case the seed is registered as garden during runtime. This way, gardenlet will restart and not add the controller
-	// again.
+	// At this point, the seed is not the garden cluster. However, this could change during the runtime of gardenlet.
+	// If so, gardener-operator will take over responsibility of the VPA eviction requirements and will run this
+	// controller.
+	// Similarly, the seed is not a self-hosted shoot cluster (at least not detectable right now, maybe because it was
+	// not yet connected to Gardener, i.e., a gardenlet deployment in the kube-system namespace does not exist, but this
+	// could change during the runtime of gardenlet). If so, the gardenlet in the kube-system namespace will take over
+	// responsibility of the VPA eviction requirements and will run this controller.
+	// Since there is no way to stop a controller after it started, we cancel the manager context in such cases. This
+	// way, gardenlet will restart and not add the controller again.
 	return mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
 		wait.Until(func() {
 			seedIsGarden, err = gardenletutils.SeedIsGarden(ctx, seedCluster.GetClient())
 			if err != nil {
-				mgr.GetLogger().Error(err, "Failed checking whether the seed cluster is the garden cluster at the same time")
+				mgr.GetLogger().Error(err, "Failed checking whether the seed cluster is the garden cluster")
 				return
 			}
-			if !seedIsGarden {
+			if seedIsGarden {
+				mgr.GetLogger().Info("Terminating gardenlet since seed cluster has been registered as garden cluster. " +
+					"This effectively stops the VPAEvictionRequirements controller (gardener-operator takes over now).")
+				gardenletCancel()
 				return
 			}
 
-			mgr.GetLogger().Info("Terminating gardenlet since seed cluster has been registered as garden cluster. " +
-				"This effectively stops the VPAEvictionRequirements controller (gardener-operator takes over now).")
-			gardenletCancel()
+			if !gardenletutils.IsResponsibleForSelfHostedShoot() {
+				seedIsSelfHostedShoot, err := gardenletutils.SeedIsSelfHostedShoot(ctx, seedCluster.GetAPIReader())
+				if err != nil {
+					mgr.GetLogger().Error(err, "Failed checking whether the seed cluster is a self-hosted shoot cluster")
+					return
+				}
+				if seedIsSelfHostedShoot {
+					mgr.GetLogger().Info("Terminating gardenlet since seed cluster has been detect to be a self-hosted " +
+						"shoot cluster. This effectively stops the VPAEvictionRequirements controller (gardenlet in the " +
+						"kube-system namespace takes over now).")
+					gardenletCancel()
+					return
+				}
+			}
 		}, SeedIsGardenCheckInterval, ctx.Done())
 		return nil
 	}))
