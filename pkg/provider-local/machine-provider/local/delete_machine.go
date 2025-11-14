e7229fd905e7fb35006dc77392be58539b4c3ef9
Tim Ebert <timebertt@gmail.com>
2025-11-14 15:06:20 +0100

    local machine provider: optionally use kubeconfig from cloudprovider secret

--- pkg/provider-local/machine-provider/local/delete_machine.go
+++ pkg/provider-local/machine-provider/local/delete_machine.go
@@ -17,6 +17,7 @@ import (
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/gardener/gardener/pkg/provider-local/local"
 	apiv1alpha1 "github.com/gardener/gardener/pkg/provider-local/machine-provider/api/v1alpha1"
 )
 
@@ -25,17 +26,16 @@ func (d *localDriver) DeleteMachine(ctx context.Context, req *driver.DeleteMachi
 		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("requested for Provider '%s', we only support '%s'", req.MachineClass.Provider, apiv1alpha1.Provider))
 	}
 
+	providerClient, err := local.GetProviderClient(ctx, log, d.runtimeClient, *req.MachineClass.CredentialsSecretRef)
+	if err != nil {
+		return nil, fmt.Errorf("could not create client for infrastructure resources: %w", err)
+	}
+
 	klog.V(3).Infof("Machine deletion request has been received for %q", req.Machine.Name)
 	defer klog.V(3).Infof("Machine deletion request has been processed for %q", req.Machine.Name)
 
-	userDataSecret := userDataSecretForMachine(req.Machine, req.MachineClass)
-	if err := d.client.Delete(ctx, userDataSecret); client.IgnoreNotFound(err) != nil {
-		// Unknown leads to short retry in machine controller
-		return nil, status.Error(codes.Unknown, fmt.Sprintf("error deleting user data secret: %s", err.Error()))
-	}
-
 	pod := podForMachine(req.Machine, req.MachineClass)
-	if err := d.client.Delete(ctx, pod); err != nil {
+	if err := providerClient.Delete(ctx, pod); err != nil {
 		if !apierrors.IsNotFound(err) {
 			// Unknown leads to short retry in machine controller
 			return nil, status.Error(codes.Unknown, fmt.Sprintf("error deleting pod: %s", err.Error()))
@@ -50,7 +50,7 @@ func (d *localDriver) DeleteMachine(ctx context.Context, req *driver.DeleteMachi
 	defer cancel()
 
 	if err := wait.PollUntilContextCancel(timeoutCtx, 5*time.Second, false, func(ctx context.Context) (bool, error) {
-		if err := d.client.Get(ctx, client.ObjectKeyFromObject(pod), pod); err != nil {
+		if err := providerClient.Get(ctx, client.ObjectKeyFromObject(pod), pod); err != nil {
 			if apierrors.IsNotFound(err) {
 				return true, nil
 			}
