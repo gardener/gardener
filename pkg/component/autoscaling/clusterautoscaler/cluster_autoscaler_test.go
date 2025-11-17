0dded52a46d421abccad3a5c088bece3334b40e3
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2025-11-17 23:02:58 +0200

    chore(vpa): Instrument few more VPA resources with update mode `Recreate` (#13411)
    * Instrument `etcd-{events,main}` VPAs
    
    * Instrument `etcd-druid` VPA
    
    * Instrument `cluster-autoscaler` VPA

--- pkg/component/autoscaling/clusterautoscaler/cluster_autoscaler_test.go
+++ pkg/component/autoscaling/clusterautoscaler/cluster_autoscaler_test.go
@@ -182,7 +182,7 @@ var _ = Describe("ClusterAutoscaler", func() {
 					Name:       deploymentName,
 				},
 				UpdatePolicy: &vpaautoscalingv1.PodUpdatePolicy{
-					UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeAuto),
+					UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeRecreate),
 				},
 				ResourcePolicy: &vpaautoscalingv1.PodResourcePolicy{
 					ContainerPolicies: []vpaautoscalingv1.ContainerResourcePolicy{{
