5e336a75b189401b25b477a75e4c1e2627735573
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-11-17 12:04:47 +0200

    Correcting license headers

--- pkg/component/observability/monitoring/x509certificateexporter/incluster_certificate_collection.go
+++ pkg/component/observability/monitoring/x509certificateexporter/incluster_certificate_collection.go
@@ -1,6 +1,6 @@
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
 //
-// SPDX-License-Identifier: Apache-2.0package x509certificateexporter
+// SPDX-License-Identifier: Apache-2.0
 
 package x509certificateexporter
 
