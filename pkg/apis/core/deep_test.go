764c5898736bbae9f47b9807aaec1b940b909d7f
Luca Bernstein <luca.bernstein@sap.com>
2025-11-25 15:28:10 +0100

    Replace imported test matchers from `pkg/utils` with copy
    Else, the matchers would need to be made a type of the pkg/apis and add additional dependency imports
    
    Co-authored-by: Marcel Boehm <marcel.boehm@inovex.de>

---
+++ pkg/apis/core/deep_test.go
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package core_test
+
+import (
+	"errors"
+
+	"github.com/onsi/gomega/format"
+	gomegatypes "github.com/onsi/gomega/types"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	deepMatcherNilError = `refusing to compare <nil> to <nil>.
+Be explicit and use BeNil() instead.
+This is to avoid mistakes where both sides of an assertion are erroneously uninitialized`
+)
+
+// DeepEqual returns a Gomega matcher which checks whether the expected object is deeply equal with the object it is
+// being compared against.
+func DeepEqual(expected any) gomegatypes.GomegaMatcher {
+	return newDeepEqualMatcher(expected)
+}
+
+type deepMatcher struct {
+	name      string
+	expected  any
+	compareFn func(a1, a2 any) bool
+}
+
+func newDeepDerivativeMatcher(expected any) gomegatypes.GomegaMatcher {
+	return &deepMatcher{
+		name:      "deep derivative equal",
+		expected:  expected,
+		compareFn: equality.Semantic.DeepDerivative,
+	}
+}
+
+func newDeepEqualMatcher(expected any) gomegatypes.GomegaMatcher {
+	return &deepMatcher{
+		name:      "deep equal",
+		expected:  expected,
+		compareFn: equality.Semantic.DeepEqual,
+	}
+}
+
+func (m *deepMatcher) Match(actual any) (success bool, err error) {
+	if actual == nil && m.expected == nil {
+		return false, errors.New(deepMatcherNilError)
+	}
+
+	return m.compareFn(m.expected, actual), nil
+}
+
+func (m *deepMatcher) FailureMessage(actual any) (message string) {
+	return m.failureMessage(actual, "to")
+}
+
+func (m *deepMatcher) NegatedFailureMessage(actual any) (message string) {
+	return m.failureMessage(actual, "not to")
+}
+
+func (m *deepMatcher) failureMessage(actual any, messagePrefix string) (message string) {
+	var (
+		actualYAML, actualErr     = yaml.Marshal(actual)
+		expectedYAML, expectedErr = yaml.Marshal(m.expected)
+	)
+
+	if actualErr == nil && expectedErr == nil {
+		return format.MessageWithDiff(string(actualYAML), messagePrefix+" "+m.name, string(expectedYAML))
+	}
+
+	return format.Message(actual, messagePrefix+" "+m.name, m.expected)
+}
