87a5db1e04cc113438fa8bb8ba5213e828849a88
Tim Ebert <timebertt@gmail.com>
2025-11-26 22:30:31 +0100

    Fix e2e upgrade tests

--- hack/ci-e2e-kind-upgrade.sh
+++ hack/ci-e2e-kind-upgrade.sh
@@ -35,15 +35,18 @@ function copy_kubeconfig_from_kubeconfig_env_var() {
 }
 
 function gardener_up() {
+  # TODO(timebertt): remove sed and SKAFFOLD_DEFAULT_REPO override after v1.134 has been released
+  # see https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/13551/pull-gardener-e2e-kind-upgrade/1993794625793429504
+  sed -i 's|garden.local.gardener.cloud:5001|registry.local.gardener.cloud:5000|g' skaffold*.yaml
   case "$SHOOT_FAILURE_TOLERANCE_TYPE" in
   node)
-    make operator-seed-up
+    make operator-seed-up SKAFFOLD_DEFAULT_REPO=registry.local.gardener.cloud:5000
     ;;
   zone)
-    make operator-seed-up
+    make operator-seed-up SKAFFOLD_DEFAULT_REPO=registry.local.gardener.cloud:5000
     ;;
   *)
-    make gardener-up
+    make gardener-up SKAFFOLD_DEFAULT_REPO=registry.local.gardener.cloud:5000
     ;;
   esac
 }
@@ -93,8 +96,23 @@ function kind_down() {
 function install_previous_release() {
   pushd $GARDENER_RELEASE_DOWNLOAD_PATH/gardener-releases/$GARDENER_PREVIOUS_RELEASE >/dev/null
   copy_kubeconfig_from_kubeconfig_env_var
+  configure_node_mirror_new_local_registry
   gardener_up
   popd >/dev/null
+}
+
+# TODO(timebertt): Remove this after v1.134 has been released.
+# See https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/13551/pull-gardener-e2e-kind-upgrade/1994379234188988416
+# and https://gcsweb.prow.gardener.cloud/gcs/gardener-prow/pr-logs/pull/gardener_gardener/13551/pull-gardener-e2e-kind-upgrade/1994379234188988416/artifacts/shoot--local--e2e-upgrade/machine-shoot--local--e2e-upgrade-local-68bbd-8wwvp/journal.log
+function configure_node_mirror_new_local_registry() {
+  # Get the directory of the new gardener version from the directory stack
+  # (the original directory of the job before doing pushd)
+  next_version_dir="$(dirs -l +1)"
+  # copy the new registry mirror config in the node image to the previous version's source code
+  # this is necessary because we already push the images to the new local registry (see the SKAFFOLD_DEFAULT_REPO hack above)
+  # and need to configure containerd to use HTTP for pulling from the local registry
+  cp "$next_version_dir/pkg/provider-local/node/Dockerfile" "$PWD/pkg/provider-local/node"
+  cp -r "$next_version_dir/pkg/provider-local/node/containerd/registry.local.gardener.cloud_5000" "$PWD/pkg/provider-local/node/containerd"
 }
 
 function upgrade_to_next_release() {
