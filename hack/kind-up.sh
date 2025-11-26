a5af06681bd3ba4bdd132bcd36b61faf612f2a27
Tim Ebert <timebertt@gmail.com>
2025-11-26 17:21:23 +0100

    Ensure load balancer IPs are not within kind network
    Currently, the load balancer IPs (`172.18.255.*`) overlap with the kind network (`172.18.0.0/16`).
    Hence, we needed some hacks/workaround for making the load balancer IPs accessible from within the kind cluster.
    With this commit, we ensure disjoint ranges, so that traffic to load balancer IPs from withing the kind cluster are correctly routed.
    This allows us to drop the hacks/workarounds (see the next commit).

--- hack/kind-up.sh
+++ hack/kind-up.sh
@@ -85,7 +85,7 @@ setup_kind_network() {
     network="$(docker network inspect $existing_network_id | yq '.[]')"
     network_options="$(echo "$network" | yq '.EnableIPv6 + "," + .Options["com.docker.network.bridge.enable_ip_masquerade"]')"
     network_ipam="$(echo "$network" | yq '.IPAM.Config' -o=json -I=0 | sed -E 's/"IPRange":"",//g')"
-    expected_network_ipam='[{"Subnet":"172.18.0.0/16","Gateway":"172.18.0.1"},{"Subnet":"fd00:10::/64","Gateway":"fd00:10::1"}]'
+    expected_network_ipam='[{"Subnet":"172.18.0.0/24","Gateway":"172.18.0.1"},{"Subnet":"fd00:10::/64","Gateway":"fd00:10::1"}]'
 
     if [ "$network_options" = 'true,true' ] && [ "$network_ipam" = "$expected_network_ipam" ] ; then
       # kind network is already configured correctly, nothing to do
@@ -98,7 +98,7 @@ setup_kind_network() {
 
   # (re-)create kind network with expected settings
   docker network create kind --driver=bridge \
-    --subnet 172.18.0.0/16 --gateway 172.18.0.1 \
+    --subnet 172.18.0.0/24 --gateway 172.18.0.1 \
     --ipv6 --subnet fd00:10::/64 --gateway fd00:10::1 \
     --opt com.docker.network.bridge.enable_ip_masquerade=true
 }
