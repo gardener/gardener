8db80a81b692b0efa1e269b4d14c1a808ffffe4f
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-11-27 16:41:55 +0100

    [release-v1.133] Update logging stack v0.70.0 (#13563)
    * vali: increase ingestion buffers of vali backend
    
    * fluent-bit: increase batch sizes in gardener output plugin
    
    * fluent-bit: fetch additional output and valitail metrics and update the FluentBit dashboard.
    
    * logging: bump up logging stack to v0.70.0
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    
    * fluent-bit/dashboard: address pr review feedback, adding descriptions to the dashboard panels.
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    * Update pkg/component/observability/plutono/dashboards/seed/fluent-bit-dashboard.json
    
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    * fluent-bit/dashboard: address pr review feedback, setting UTC timezone and having 1h time picker
    
    * Address PR review feedback
    
    ---------
    
    Co-authored-by: Niki Dokovski <nikolai.dokovski@sap.com>
    Co-authored-by: Niki Dokovski <nickytd@gmail.com>
    Co-authored-by: Plamen Kokanov <35485709+plkokanov@users.noreply.github.com>
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>

--- pkg/component/observability/logging/vali/vali_test.go
+++ pkg/component/observability/logging/vali/vali_test.go
@@ -52,7 +52,7 @@ const (
 	managedResourceNameTarget                   = "vali-target"
 	managedResourceSecretNameTarget             = "managedresource-vali-target"
 	valiName                                    = "vali"
-	valiConfigMapName                           = "vali-config-e78955f0"
+	valiConfigMapName                           = "vali-config-53bcb8e9"
 	telegrafConfigMapName                       = "telegraf-config-b4c38756"
 	valiImage                                   = "vali:0.0.1"
 	curatorImage                                = "curator:0.0.1"
@@ -985,8 +985,8 @@ limits_config:
   enforce_metric_name: false
   reject_old_samples: true
   reject_old_samples_max_age: 168h
-  ingestion_rate_mb: 8
-  ingestion_burst_size_mb: 12
+  ingestion_rate_mb: 12
+  ingestion_burst_size_mb: 16
 schema_config:
   configs:
   - from: 2018-04-15
