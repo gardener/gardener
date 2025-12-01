e2bcf96f0c59c8ec6a3936dce17f16a3863e4a8e
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-12-01 13:55:06 +0200

    chore(cmd/internal/migration): move `gardenlet` and `gardener-operator` migration to dedicated `internal` package
    This commit moves the empty vpa patches migration logic to a dedicated
    "cmd/internal" package so that it's logic can be reused and not duplicated.
    
    Also:
    - Use the `features` package to check if `VPAInPlaceUpdates` gate is enabled
    - Use the `metav1` package to check if `vpa` resources have correct labels
    - Use `metav1` and `v1beta1constants` for `kube-system` and `kubernetes-dashboard` namespaces

--- cmd/gardenlet/app/migration.go
+++ cmd/gardenlet/app/migration.go
@@ -6,62 +6,16 @@ package app
 
 import (
 	"context"
-	"fmt"
 
-	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
+	"github.com/gardener/gardener/cmd/internal/migration"
+	"github.com/gardener/gardener/pkg/features"
 	"github.com/go-logr/logr"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
-	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func (g *garden) runMigrations(ctx context.Context, log logr.Logger) error {
-	if enabled, ok := g.config.FeatureGates["VPAInPlaceUpdates"]; enabled && ok {
-		if err := g.migrateEmptyVPAPatch(ctx, log); err != nil {
+	if features.DefaultFeatureGate.Enabled(features.VPAInPlaceUpdates) {
+		if err := migration.MigrateEmptyVPAPatch(ctx, g.mgr, log); err != nil {
 			return err
-		}
-	}
-	return nil
-}
-
-// This migration performs an empty patch updates to VerticalPodAutoscaler resources,
-// that are elighible to adopt the InPlaceOrRecreate update mode, but are filtered out, because of
-// the Resoruce Manager's alwaysUpdate=false configuration.
-// TODO(vitanovs): Remove the migration once the VPAInPlaceUpdates feature gates is deprecated.
-func (g *garden) migrateEmptyVPAPatch(ctx context.Context, log logr.Logger) error {
-	log.Info("Migrating VerticalPodAutoscalers")
-
-	list := vpaautoscalingv1.VerticalPodAutoscalerList{}
-	if err := g.mgr.GetClient().List(ctx, &list); err != nil {
-		if meta.IsNoMatchError(err) {
-			log.Info("Resources kind not found, skipping migration", "kind", "VerticalPodAutoscaler")
-			return nil
-		}
-		return fmt.Errorf("failed listing VerticalPodAutoscaler resources: %w", err)
-	}
-
-	for _, vpa := range list.Items {
-		_, vpaHasSkipLabel := vpa.Labels[resourcesv1alpha1.VPAInPlaceUpdatesSkip]
-		_, vpaHasMutatedLabel := vpa.Labels[resourcesv1alpha1.VPAInPlaceUpdatesMutated]
-		if vpaHasSkipLabel || vpaHasMutatedLabel {
-			continue
-		}
-
-		if vpa.Namespace == "kube-system" || vpa.Namespace == "kubernetes-dashboards" {
-			continue
-		}
-
-		vpaKey := client.ObjectKeyFromObject(&vpa)
-		log.Info("Updating VerticalPodAutoscaler resource", "vpa", vpaKey)
-
-		patch := client.MergeFrom(vpa.DeepCopy())
-		if err := g.mgr.GetClient().Patch(ctx, &vpa, patch); err != nil {
-			if apierrors.IsNotFound(err) {
-				log.Info("Resource not found, skipping migration", "vpa", vpaKey)
-				continue
-			}
-			return fmt.Errorf("failed updating VerticalPodAutoscaler '%s': %w", vpaKey, err)
 		}
 	}
 	return nil
