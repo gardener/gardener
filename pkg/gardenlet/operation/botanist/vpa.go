cf37fd1c20c7f25ef73c1007c65c33fdf0a2c8d5
Marco Voelz <marco.voelz@sap.com>
2025-12-04 09:23:17 +0100

    Pass value from Shoot spec to VPA component

--- pkg/gardenlet/operation/botanist/vpa.go
+++ pkg/gardenlet/operation/botanist/vpa.go
@@ -67,6 +67,7 @@ func (b *Botanist) DefaultVerticalPodAutoscaler() (vpa.Interface, error) {
 		valuesRecommender.MemoryAggregationInterval = vpaConfig.MemoryAggregationInterval
 		valuesRecommender.MemoryAggregationIntervalCount = vpaConfig.MemoryAggregationIntervalCount
 		valuesRecommender.MaxAllowed = vpaConfig.MaxAllowed
+		valuesRecommender.UpdateWorkerCount = vpaConfig.UpdateWorkerCount
 
 		valuesUpdater.EvictAfterOOMThreshold = vpaConfig.EvictAfterOOMThreshold
 		valuesUpdater.EvictionRateBurst = vpaConfig.EvictionRateBurst
