dc41bd04a9845da446ac8a486b591c78fb370b78
Tobias Schlicht <155947440+tobschli@users.noreply.github.com>
2025-12-16 12:39:40 +0100

    `CRDDeployer` improvement (#13506)
    * Remove special handling for `Cluster`s CRD generation.
    
    * Add deletion-protection label to `CRD`s that get deployed when `deletionProtection` is set to true
    
    * Add unit-test
    
    * [make generate]
    
    * Use `metav1.SetMetaDataLabel()`
    
    * Use `Etcd` `CRD`s directly
    
    As the `CRDDeployer` now adds the deletion protection annotation during creation as well.
    
    * Drop the internal `CRDGetter` for etcd
    
    * Implement PR feedback
    
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Implement PR feedback
    
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    ---------
    
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>

--- pkg/component/etcd/etcd/crd.go
+++ pkg/component/etcd/etcd/crd.go
@@ -6,101 +6,22 @@ package etcd
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 
 	"github.com/Masterminds/semver/v3"
 	druidcorecrds "github.com/gardener/etcd-druid/api/core/v1alpha1/crds"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kubernetesscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/gardener/pkg/component"
 	"github.com/gardener/gardener/pkg/component/crddeployer"
-	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
-	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
 )
 
 // NewCRD can be used to deploy the CRD definitions for all CRDs defined by etcd-druid.
 func NewCRD(client client.Client, k8sVersion *semver.Version) (component.DeployWaiter, error) {
-	crdGetter, err := NewCRDGetter(k8sVersion)
-	if err != nil {
-		return nil, err
-	}
-	crds, err := crdGetter.GetAllCRDsAsStringSlice()
-	if err != nil {
-		return nil, err
-	}
-	return crddeployer.New(client, crds, true)
-}
-
-// CRDGetter provides methods to get CRDs defined in etcd-druid.
-type CRDGetter interface {
-	// GetAllCRDs returns a map of CRD names to CRD objects.
-	GetAllCRDs() map[string]*apiextensionsv1.CustomResourceDefinition
-	// GetCRD returns the CRD with the given name.
-	// An error is returned if no CRD is found with the given name.
-	GetCRD(name string) (*apiextensionsv1.CustomResourceDefinition, error)
-	// GetAllCRDsAsStringSlice returns all CRDs as Strings.
-	GetAllCRDsAsStringSlice() ([]string, error)
-}
-
-type crdGetterImpl struct {
-	crdResources map[string]*apiextensionsv1.CustomResourceDefinition
-	k8sVersion   *semver.Version
-}
-
-func (c *crdGetterImpl) GetAllCRDsAsStringSlice() ([]string, error) {
-	crds := c.GetAllCRDs()
-	crdStrings := make([]string, 0, len(crds))
-	for _, crd := range crds {
-		crdString, err := kubernetesutils.Serialize(crd, kubernetesscheme.Scheme)
-		if err != nil {
-			return nil, err
-		}
-		crdStrings = append(crdStrings, crdString)
-	}
-	return crdStrings, nil
-}
-
-var _ CRDGetter = (*crdGetterImpl)(nil)
-
-// NewCRDGetter creates a new CRDGetter.
-func NewCRDGetter(k8sVersion *semver.Version) (CRDGetter, error) {
-	crdResources, err := getEtcdCRDs(k8sVersion)
-	if err != nil {
-		return nil, err
-	}
-	return &crdGetterImpl{
-		crdResources: crdResources,
-		k8sVersion:   k8sVersion,
-	}, nil
-}
-
-func (c *crdGetterImpl) GetAllCRDs() map[string]*apiextensionsv1.CustomResourceDefinition {
-	return c.crdResources
-}
-
-func (c *crdGetterImpl) GetCRD(name string) (*apiextensionsv1.CustomResourceDefinition, error) {
-	crdObj, ok := c.crdResources[name]
-	if !ok {
-		return nil, fmt.Errorf("CRD %s not found", name)
-	}
-	return crdObj, nil
-}
-
-func getEtcdCRDs(k8sVersion *semver.Version) (map[string]*apiextensionsv1.CustomResourceDefinition, error) {
 	crdYAMLs, err := druidcorecrds.GetAll(k8sVersion.String())
 	if err != nil {
 		return nil, fmt.Errorf("failed to get etcd-druid CRDs for Kubernetes version %s: %w", k8sVersion, err)
 	}
-	var crdResources = make(map[string]*apiextensionsv1.CustomResourceDefinition, len(crdYAMLs))
-	for crdName, crdYAML := range crdYAMLs {
-		crdObj, err := kubernetesutils.DecodeCRD(crdYAML)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode etcd-druid CRD: %s: %w", crdName, err)
-		}
-		metav1.SetMetaDataLabel(&crdObj.ObjectMeta, gardenerutils.DeletionProtected, "true")
-		crdResources[crdName] = crdObj
-	}
-	return crdResources, nil
+	return crddeployer.New(client, slices.Collect(maps.Values(crdYAMLs)), true)
 }
