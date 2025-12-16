d80666ae63bef5a1f24e67d95ff748a4aaa40e5b
Luca Bernstein <luca.bernstein@sap.com>
2025-12-16 12:52:02 +0100

    Address review feedback: extract helmregistry patching to separate function

--- hack/ci-e2e-kind-upgrade.sh
+++ hack/ci-e2e-kind-upgrade.sh
@@ -101,6 +101,7 @@ function install_previous_release() {
   copy_kubeconfig_from_kubeconfig_env_var
   migrate_kind_network_range
   configure_node_mirror_new_local_registry
+  patch_helmregistry_registry_url_port
   gardener_up
   popd >/dev/null
 }
@@ -124,9 +125,12 @@ function configure_node_mirror_new_local_registry() {
   # and need to configure containerd to use HTTP for pulling from the local registry
   cp "$next_version_dir/pkg/provider-local/node/Dockerfile" "$PWD/pkg/provider-local/node"
   cp -r "$next_version_dir/pkg/provider-local/node/containerd/registry.local.gardener.cloud_5001" "$PWD/pkg/provider-local/node/containerd"
-  # TODO(LucaBernstein): remove this after v1.135 has been released.
-  # To avoid issues in the upgrade test with the registry already listening to port 5001 instead of 5000, but the helmregistry only disabling tls for registry on port 5000.
-  # see: https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/13661/pull-gardener-e2e-kind-upgrade/2000837721429381120
+}
+
+# TODO(LucaBernstein): remove this after v1.135 has been released.
+# To avoid issues in the upgrade test with the registry already listening to port 5001 instead of 5000, but the helmregistry only disabling tls for registry on port 5000.
+# see: https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_gardener/13661/pull-gardener-e2e-kind-upgrade/2000837721429381120
+function patch_helmregistry_registry_url_port() {
   sed -i 's|registry.local.gardener.cloud:5000|registry.local.gardener.cloud:5001|g' "$PWD/pkg/utils/oci/helmregistry.go"
 }
 
