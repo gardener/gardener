b6e0ed8cd9fce839c1730cb102427ac473335545
vpnachev <vladimir.nachev@sap.com>
2025-12-17 18:49:22 +0200

    Validate 'seed.spec.dns.provider' even when seed.spec.ingress is not set
    Add more tests for the DNS validation

--- pkg/apis/core/validation/seed.go
+++ pkg/apis/core/validation/seed.go
@@ -17,6 +17,7 @@ import (
 	"github.com/gardener/gardener/pkg/apis/core"
 	"github.com/gardener/gardener/pkg/apis/core/helper"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	securityv1alpha1 "github.com/gardener/gardener/pkg/apis/security/v1alpha1"
 	"github.com/gardener/gardener/pkg/utils"
 	cidrvalidation "github.com/gardener/gardener/pkg/utils/validation/cidr"
 	featuresvalidation "github.com/gardener/gardener/pkg/utils/validation/features"
@@ -220,25 +221,30 @@ func ValidateSeedSpec(seedSpec *core.SeedSpec, fldPath *field.Path, inTemplate b
 		if seedSpec.DNS.Provider == nil {
 			allErrs = append(allErrs, field.Required(fldPath.Child("dns", "provider"),
 				"ingress controller requires dns.provider to be set"))
-		} else {
-			dnsProviderPath := fldPath.Child("dns", "provider")
-			if len(seedSpec.DNS.Provider.Type) == 0 {
-				allErrs = append(allErrs, field.Required(dnsProviderPath.Child("type"),
-					"DNS provider type must be set"))
-			}
-			allErrs = append(allErrs, ValidateCredentialsRef(seedSpec.DNS.Provider.CredentialsRef, dnsProviderPath.Child("credentialsRef"))...)
+		}
+	}
 
-			if seedSpec.DNS.Provider.CredentialsRef.APIVersion == "v1" && seedSpec.DNS.Provider.CredentialsRef.Kind == "Secret" {
-				if seedSpec.DNS.Provider.CredentialsRef.Name != seedSpec.DNS.Provider.SecretRef.Name || seedSpec.DNS.Provider.CredentialsRef.Namespace != seedSpec.DNS.Provider.SecretRef.Namespace {
-					allErrs = append(allErrs, field.Invalid(dnsProviderPath.Child("secretRef"), seedSpec.DNS.Provider.SecretRef, "must refer to the same Secret as credentialsRef"))
-				}
-			} else if seedSpec.DNS.Provider.CredentialsRef.APIVersion == "security.gardener.cloud/v1alpha1" && seedSpec.DNS.Provider.CredentialsRef.Kind == "WorkloadIdentity" {
-				if seedSpec.DNS.Provider.SecretRef.Name != "" {
-					allErrs = append(allErrs, field.Invalid(dnsProviderPath.Child("secretRef", "name"), seedSpec.DNS.Provider.SecretRef.Name, "must be empty when credentialsRef refers to a WorkloadIdentity"))
-				}
-				if seedSpec.DNS.Provider.SecretRef.Namespace != "" {
-					allErrs = append(allErrs, field.Invalid(dnsProviderPath.Child("secretRef", "namespace"), seedSpec.DNS.Provider.SecretRef.Namespace, "must be empty when credentialsRef refers to a WorkloadIdentity"))
-				}
+	if dnsProvider := seedSpec.DNS.Provider; dnsProvider != nil {
+		dnsProviderPath := fldPath.Child("dns", "provider")
+		if len(dnsProvider.Type) == 0 {
+			allErrs = append(allErrs, field.Required(dnsProviderPath.Child("type"), "DNS provider type must be set"))
+		}
+
+		allErrs = append(allErrs, ValidateCredentialsRef(dnsProvider.CredentialsRef, dnsProviderPath.Child("credentialsRef"))...)
+
+		if dnsProvider.CredentialsRef.APIVersion == corev1.SchemeGroupVersion.String() &&
+			dnsProvider.CredentialsRef.Kind == "Secret" {
+			if dnsProvider.SecretRef.Name != dnsProvider.CredentialsRef.Name ||
+				dnsProvider.SecretRef.Namespace != dnsProvider.CredentialsRef.Namespace {
+				allErrs = append(allErrs, field.Invalid(dnsProviderPath.Child("secretRef"), dnsProvider.SecretRef, "must refer to the same Secret as credentialsRef"))
+			}
+		} else if dnsProvider.CredentialsRef.APIVersion == securityv1alpha1.SchemeGroupVersion.String() &&
+			dnsProvider.CredentialsRef.Kind == "WorkloadIdentity" {
+			if dnsProvider.SecretRef.Name != "" {
+				allErrs = append(allErrs, field.Forbidden(dnsProviderPath.Child("secretRef", "name"), "must not be set when credentialsRef refers to a WorkloadIdentity"))
+			}
+			if dnsProvider.SecretRef.Namespace != "" {
+				allErrs = append(allErrs, field.Forbidden(dnsProviderPath.Child("secretRef", "namespace"), "must not be set when credentialsRef refers to a WorkloadIdentity"))
 			}
 		}
 	}
