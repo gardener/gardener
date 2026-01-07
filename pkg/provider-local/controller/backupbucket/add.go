5d6d563705938001d066c1b4fb62a324331666eb
Tim Usner <tim.usner@sap.com>
2026-01-07 15:36:00 +0100

    Remove unused options

--- pkg/provider-local/controller/backupbucket/add.go
+++ pkg/provider-local/controller/backupbucket/add.go
@@ -9,7 +9,6 @@ import (
 	"slices"
 
 	"k8s.io/apimachinery/pkg/util/sets"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"github.com/gardener/gardener/extensions/pkg/controller/backupbucket"
@@ -28,18 +27,6 @@ var (
 	// supportedExtensionClasses are the extension classes supported by the backupbucket controller.
 	supportedExtensionClasses = sets.New(extensionsv1alpha1.ExtensionClassGarden, extensionsv1alpha1.ExtensionClassShoot)
 )
-
-// AddOptions are options to apply when adding the backupbucket controller to the manager.
-type AddOptions struct {
-	// BackupBucketLocalDir is the directory of the backupbucket.
-	BackupBucketLocalDir string
-	// Controller are the controller.Options.
-	Controller controller.Options
-	// IgnoreOperationAnnotation specifies whether to ignore the operation annotation or not.
-	IgnoreOperationAnnotation bool
-	// ExtensionClasses are the configured extension classes for this extension deployment.
-	ExtensionClasses extensionsv1alpha1.ExtensionClass
-}
 
 // AddToManagerWithOptions adds a controller with the given Options to the given manager.
 // The opts.Reconciler is being set with a newly instantiated actuator.
