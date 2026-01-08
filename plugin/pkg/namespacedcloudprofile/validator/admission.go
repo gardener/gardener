08869390da925d62d3761e0cacab1b6a113b5cc9
Luca Bernstein <luca.bernstein@sap.com>
2026-01-08 16:27:37 +0100

    Allow updating a MachineImage version expiration date in NamespacedCloudProfiles

--- plugin/pkg/namespacedcloudprofile/validator/admission.go
+++ plugin/pkg/namespacedcloudprofile/validator/admission.go
@@ -300,8 +300,16 @@ func (c *validationContext) validateMachineImageOverrides(ctx context.Context, a
 						var oldMachineImageVersion gardencore.MachineImageVersion
 						oldMachineImageVersion, imageVersionAlreadyInNamespacedCloudProfile = oldVersionsSpec.GetImageVersion(image.Name, imageVersion.Version)
 
-						if imageVersionAlreadyInNamespacedCloudProfile && !reflect.DeepEqual(oldMachineImageVersion, imageVersion) {
-							allErrs = append(allErrs, field.Forbidden(imageVersionIndexPath, fmt.Sprintf("cannot update the machine image version spec of \"%s@%s\", as this version has been added to the parent CloudProfile by now", image.Name, imageVersion.Version)))
+						oldMachineImageVersionWithoutExpiration := oldMachineImageVersion.DeepCopy()
+						imageVersionWithoutExpiration := imageVersion.DeepCopy()
+						if imageVersion.ExpirationDate != nil {
+							oldMachineImageVersionWithoutExpiration.ExpirationDate = nil
+							imageVersionWithoutExpiration.ExpirationDate = nil
+						}
+						// Compare the old and new image version without considering the expiration date.
+						// If the image versions are equal except for the expiration date, then the update is allowed.
+						if imageVersionAlreadyInNamespacedCloudProfile && !reflect.DeepEqual(oldMachineImageVersionWithoutExpiration, imageVersionWithoutExpiration) {
+							allErrs = append(allErrs, field.Forbidden(imageVersionIndexPath, fmt.Sprintf("cannot update the machine image version spec (except for the expiration date) of \"%s@%s\", as this version has been added to the parent CloudProfile by now", image.Name, imageVersion.Version)))
 						}
 					}
 
