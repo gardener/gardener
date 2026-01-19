4060fd425e333bd4226e19a1cfd2284cc61c3eeb
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-01-19 09:02:48 +0000

    Update dependency ironcore-dev/vgopath to v0.1.10

--- hack/tools.mk
+++ hack/tools.mk
@@ -81,7 +81,7 @@ SKAFFOLD_VERSION ?= v2.17.1
 # renovate: datasource=github-releases depName=mikefarah/yq
 YQ_VERSION ?= v4.50.1
 # renovate: datasource=github-releases depName=ironcore-dev/vgopath
-VGOPATH_VERSION ?= v0.1.9
+VGOPATH_VERSION ?= v0.1.10
 # renovate: datasource=github-releases depName=crate-ci/typos
 TYPOS_VERSION ?= v1.42.0
 
