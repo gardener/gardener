ffafdc7cd2e7b7d325a61e439e2a8c082483940a
Tobias Schlicht <tobias.schlicht@sap.com>
2026-01-27 17:15:02 +0900

    Always use autocompletion for ETCD key rotation e2e test

--- test/e2e/gardener/shoot/create_rotate_delete.go
+++ test/e2e/gardener/shoot/create_rotate_delete.go
@@ -354,9 +354,8 @@ var _ = Describe("Shoot Tests", Label("Shoot", "default"), func() {
 					GetETCDEncryptionKeyRotation: func() *gardencorev1beta1.ETCDEncryptionKeyRotation {
 						return s.Shoot.Status.Credentials.Rotation.ETCDEncryptionKey
 					},
-					EncryptionKey:             v1beta1constants.SecretNameETCDEncryptionKey,
-					RoleLabelValue:            v1beta1constants.SecretNamePrefixETCDEncryptionConfiguration,
-					AutoCompleteAfterPrepared: true,
+					EncryptionKey:  v1beta1constants.SecretNameETCDEncryptionKey,
+					RoleLabelValue: v1beta1constants.SecretNamePrefixETCDEncryptionConfiguration,
 				},
 				&rotationutils.ServiceAccountKeyVerifier{
 					GetServiceAccountKeySecretNamespace: func() string {
