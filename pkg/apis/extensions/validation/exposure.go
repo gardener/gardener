0803ae91e0f260e1fa2e289b82fe62ebc6c5fa74
Robin Schneider <robin.schneider@stackit.cloud>
2026-02-06 12:53:27 +0100

    Add validation for SelfHostedShootExposure types
    Signed-off-by: Robin Schneider <robin.schneider@stackit.cloud>

---
+++ pkg/apis/extensions/validation/exposure.go
@@ -0,0 +1,66 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package validation
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-test/deep"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	apivalidation "k8s.io/apimachinery/pkg/api/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+)
+
+// ValidateSelfHostedShootExposure validates a SelfHostedShootExposure object.
+func ValidateSelfHostedShootExposure(exposure *extensionsv1alpha1.SelfHostedShootExposure) field.ErrorList {
+	allErrs := field.ErrorList{}
+	allErrs = append(allErrs, apivalidation.ValidateObjectMeta(&exposure.ObjectMeta, true, apivalidation.NameIsDNSSubdomain, field.NewPath("metadata"))...)
+	allErrs = append(allErrs, ValidateSelfHostedShootExposureSpec(&exposure.Spec, field.NewPath("spec"))...)
+
+	return allErrs
+}
+
+// ValidateSelfHostedShootExposureUpdate validates a SelfHostedShootExposure object before an update.
+func ValidateSelfHostedShootExposureUpdate(new, old *extensionsv1alpha1.SelfHostedShootExposure) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	allErrs = append(allErrs, apivalidation.ValidateObjectMetaUpdate(&new.ObjectMeta, &old.ObjectMeta, field.NewPath("metadata"))...)
+	allErrs = append(allErrs, ValidateSelfHostedShootExposureSpecUpdate(&new.Spec, &old.Spec, new.DeletionTimestamp != nil, field.NewPath("spec"))...)
+	allErrs = append(allErrs, ValidateSelfHostedShootExposure(new)...)
+
+	return allErrs
+}
+
+// ValidateSelfHostedShootExposureSpec validates the specification of a SelfHostedShootExposure object.
+func ValidateSelfHostedShootExposureSpec(spec *extensionsv1alpha1.SelfHostedShootExposureSpec, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	if len(spec.Type) == 0 {
+		allErrs = append(allErrs, field.Required(fldPath.Child("type"), "field is required"))
+	}
+
+	if len(spec.Endpoints) == 0 {
+		allErrs = append(allErrs, field.Required(fldPath.Child("endpoints"), "field is required"))
+	}
+
+	return allErrs
+}
+
+// ValidateSelfHostedShootExposureSpecUpdate validates the spec of an SelfHostedShootExposure object before an update.
+func ValidateSelfHostedShootExposureSpecUpdate(new, old *extensionsv1alpha1.SelfHostedShootExposureSpec, deletionTimestampSet bool, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	if deletionTimestampSet && !apiequality.Semantic.DeepEqual(new, old) {
+		diff := deep.Equal(new, old)
+		return field.ErrorList{field.Forbidden(fldPath, fmt.Sprintf("cannot update SelfHostedShootExposure spec if deletion timestamp is set. Requested changes: %s", strings.Join(diff, ",")))}
+	}
+
+	allErrs = append(allErrs, apivalidation.ValidateImmutableField(new.Type, old.Type, fldPath.Child("type"))...)
+
+	return allErrs
+}
---
+++ pkg/apis/extensions/validation/exposure_test.go
@@ -0,0 +1,136 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package validation_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	. "github.com/gardener/gardener/pkg/apis/extensions/validation"
+)
+
+var _ = Describe("SelfHostedShootExposure validation tests", func() {
+	var exposure *extensionsv1alpha1.SelfHostedShootExposure
+
+	BeforeEach(func() {
+		exposure = &extensionsv1alpha1.SelfHostedShootExposure{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-exposure",
+				Namespace: "test-namespace",
+			},
+			Spec: extensionsv1alpha1.SelfHostedShootExposureSpec{
+				DefaultSpec: extensionsv1alpha1.DefaultSpec{
+					Type:           "provider",
+					ProviderConfig: &runtime.RawExtension{},
+				},
+				SecretRef: &corev1.SecretReference{
+					Name: "test",
+				},
+				Endpoints: []extensionsv1alpha1.ControlPlaneEndpoint{
+					{
+						NodeName: "test-node",
+						Port:     443,
+						Addresses: []corev1.NodeAddress{
+							{
+								Type:    corev1.NodeInternalIP,
+								Address: "1.2.3.4",
+							},
+						},
+					},
+				},
+			},
+		}
+	})
+
+	Describe("#ValidSelfHostedShootExposure", func() {
+		It("should forbid empty SelfHostedShootExposure resources", func() {
+			errorList := ValidateSelfHostedShootExposure(&extensionsv1alpha1.SelfHostedShootExposure{})
+
+			Expect(errorList).To(ConsistOf(PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeRequired),
+				"Field": Equal("metadata.name"),
+			})), PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeRequired),
+				"Field": Equal("metadata.namespace"),
+			})), PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeRequired),
+				"Field": Equal("spec.type"),
+			})), PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeRequired),
+				"Field": Equal("spec.endpoints"),
+			}))))
+		})
+
+		It("should allow valid infra resources", func() {
+			errorList := ValidateSelfHostedShootExposure(exposure)
+
+			Expect(errorList).To(BeEmpty())
+		})
+	})
+
+	Describe("#ValidSelfHostedShootExposureUpdate", func() {
+		It("should prevent updating anything if deletion time stamp is set", func() {
+			now := metav1.Now()
+			exposure.DeletionTimestamp = &now
+
+			newSelfHostedShootExposure := prepareSelfHostedShootExposureForUpdate(exposure)
+			newSelfHostedShootExposure.DeletionTimestamp = &now
+			newSelfHostedShootExposure.Spec.SecretRef.Name = "changed-secretref-name"
+
+			errorList := ValidateSelfHostedShootExposureUpdate(newSelfHostedShootExposure, exposure)
+
+			Expect(errorList).To(ConsistOf(PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":   Equal(field.ErrorTypeForbidden),
+				"Field":  Equal("spec"),
+				"Detail": Equal("cannot update SelfHostedShootExposure spec if deletion timestamp is set. Requested changes: SecretRef.Name: changed-secretref-name != test"),
+			}))))
+		})
+
+		It("should prevent updating the type and region", func() {
+			newSelfHostedShootExposure := prepareSelfHostedShootExposureForUpdate(exposure)
+			newSelfHostedShootExposure.Spec.Type = "changed-type"
+
+			errorList := ValidateSelfHostedShootExposureUpdate(newSelfHostedShootExposure, exposure)
+
+			Expect(errorList).To(ConsistOf(PointTo(MatchFields(IgnoreExtras, Fields{
+				"Type":  Equal(field.ErrorTypeInvalid),
+				"Field": Equal("spec.type"),
+			}))))
+		})
+
+		It("should allow updating the name of the referenced secret, the provider config, or the endpoints", func() {
+			newSelfHostedShootExposure := prepareSelfHostedShootExposureForUpdate(exposure)
+			newSelfHostedShootExposure.Spec.SecretRef.Name = "changed-secretref-name"
+			newSelfHostedShootExposure.Spec.ProviderConfig = nil
+			newSelfHostedShootExposure.Spec.Endpoints = []extensionsv1alpha1.ControlPlaneEndpoint{
+				{
+					NodeName: "update-node",
+					Port:     443,
+					Addresses: []corev1.NodeAddress{
+						{
+							Type: corev1.NodeInternalIP,
+						},
+					},
+				},
+			}
+
+			errorList := ValidateSelfHostedShootExposureUpdate(newSelfHostedShootExposure, exposure)
+
+			Expect(errorList).To(BeEmpty())
+		})
+	})
+})
+
+func prepareSelfHostedShootExposureForUpdate(obj *extensionsv1alpha1.SelfHostedShootExposure) *extensionsv1alpha1.SelfHostedShootExposure {
+	newObj := obj.DeepCopy()
+	newObj.ResourceVersion = "1"
+	return newObj
+}
