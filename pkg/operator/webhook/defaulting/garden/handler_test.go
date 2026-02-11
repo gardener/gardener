57dcd5a5ca4c697b6c567c9690926d1a1b413845
AleksandarSavchev <aleksandar.savchev@sap.com>
2026-02-11 17:57:17 +0200

    Deafult to secretbox as the more secure encryption provider

--- pkg/operator/webhook/defaulting/garden/handler_test.go
+++ pkg/operator/webhook/defaulting/garden/handler_test.go
@@ -51,7 +51,7 @@ var _ = Describe("Handler", func() {
 					},
 					EncryptionConfig: &gardencorev1beta1.EncryptionConfig{
 						Provider: gardencorev1beta1.EncryptionProvider{
-							Type: ptr.To(gardencorev1beta1.EncryptionProviderTypeAESCBC),
+							Type: ptr.To(gardencorev1beta1.EncryptionProviderTypeSecretbox),
 						},
 					},
 				},
@@ -59,7 +59,7 @@ var _ = Describe("Handler", func() {
 			defaultGardenAPIServerConfig = &operatorv1alpha1.GardenerAPIServerConfig{
 				EncryptionConfig: &gardencorev1beta1.EncryptionConfig{
 					Provider: gardencorev1beta1.EncryptionProvider{
-						Type: ptr.To(gardencorev1beta1.EncryptionProviderTypeAESCBC),
+						Type: ptr.To(gardencorev1beta1.EncryptionProviderTypeSecretbox),
 					},
 				},
 			}
@@ -67,7 +67,7 @@ var _ = Describe("Handler", func() {
 				Credentials: &operatorv1alpha1.Credentials{
 					EncryptionAtRest: &operatorv1alpha1.EncryptionAtRest{
 						Provider: operatorv1alpha1.EncryptionProviderStatus{
-							Type: gardencorev1beta1.EncryptionProviderTypeAESCBC,
+							Type: gardencorev1beta1.EncryptionProviderTypeSecretbox,
 						},
 					},
 				},
