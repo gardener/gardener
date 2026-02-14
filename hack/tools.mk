0bb9934b128f083f83f747b3286e719cc9fa224c
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-02-14 08:26:46 +0000

    Update dependency mikefarah/yq to v4.52.4

--- hack/tools.mk
+++ hack/tools.mk
@@ -79,7 +79,7 @@ PROTOC_VERSION ?= v33.5
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
 SKAFFOLD_VERSION ?= v2.17.2
 # renovate: datasource=github-releases depName=mikefarah/yq
-YQ_VERSION ?= v4.52.2
+YQ_VERSION ?= v4.52.4
 # renovate: datasource=github-releases depName=ironcore-dev/vgopath
 VGOPATH_VERSION ?= v0.1.10
 # renovate: datasource=github-releases depName=crate-ci/typos
