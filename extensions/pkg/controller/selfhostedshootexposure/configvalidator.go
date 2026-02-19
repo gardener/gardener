25a3d34a47251c18bc5e6cb2d2208ecbe7034b23
Robin Schneider <robin.schneider@stackit.cloud>
2026-02-19 15:40:05 +0100

    Add SelfHostedShootExposure controller to extension library
    Signed-off-by: Robin Schneider <robin.schneider@stackit.cloud>

---
+++ extensions/pkg/controller/selfhostedshootexposure/configvalidator.go
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package selfhostedshootexposure
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/extensions"
+)
+
+// ConfigValidator validates the provider config of selfhostedshootexposure resource with the cloud provider.
+type ConfigValidator interface {
+	// Validate validates the provider config of the given selfhostedshootexposure and cluster resources used by SelfHostedShootExposure.
+	// If the returned error list is non-empty, the reconciliation will fail with an error.
+	// This error will have the error code ERR_CONFIGURATION_PROBLEM, unless there is at least one error in the list
+	// that has its ErrorType field set to field.ErrorTypeInternal.
+	Validate(ctx context.Context, selfhostedshootexposure *extensionsv1alpha1.SelfHostedShootExposure, cluster *extensions.Cluster) field.ErrorList
+}
