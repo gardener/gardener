b3a4913c1df2dc916b13f26470378107caf4e358
RAPSNX <raphael.groemmer@stackit.cloud>
2026-02-23 11:59:46 +0100

    Add status patching for CloudProfile

--- pkg/controllermanager/controller/cloudprofile/reconciler_test.go
+++ pkg/controllermanager/controller/cloudprofile/reconciler_test.go
@@ -7,12 +7,15 @@ package cloudprofile_test
 import (
 	"context"
 	"errors"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/events"
+	testclock "k8s.io/utils/clock/testing"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/client/interceptor"
@@ -35,6 +38,7 @@ var _ = Describe("Reconciler", func() {
 
 		cloudProfileName string
 		fakeErr          error
+		fakeClock        *testclock.FakeClock
 		reconciler       reconcile.Reconciler
 		cloudProfile     *gardencorev1beta1.CloudProfile
 	)
@@ -42,16 +46,19 @@ var _ = Describe("Reconciler", func() {
 	BeforeEach(func() {
 		cloudProfileName = "test-cloudprofile"
 		fakeErr = errors.New("fake err")
+		fakeClock = testclock.NewFakeClock(time.Now())
 
 		fakeClient = fakeclient.NewClientBuilder().
 			WithScheme(kubernetes.GardenScheme).
+			WithStatusSubresource(&gardencorev1beta1.CloudProfile{}).
 			WithIndex(
 				&gardencorev1beta1.NamespacedCloudProfile{},
 				core.NamespacedCloudProfileParentRefName,
 				indexer.NamespacedCloudProfileParentRefNameIndexerFunc,
 			).
 			Build()
-		reconciler = &Reconciler{Client: fakeClient, Recorder: &events.FakeRecorder{}}
+		reconciler = &Reconciler{Client: fakeClient, Recorder: &events.FakeRecorder{}, Clock: fakeClock}
+
 		cloudProfile = &gardencorev1beta1.CloudProfile{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: cloudProfileName,
@@ -107,7 +114,7 @@ var _ = Describe("Reconciler", func() {
 			Expect(patchCalls).To(Equal(1))
 		})
 
-		It("should ensure the finalizer (no error)", func() {
+		It("should ensure the finalizer", func() {
 			result, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: cloudProfileName}})
 			Expect(result).To(Equal(reconcile.Result{}))
 			Expect(err).NotTo(HaveOccurred())
@@ -199,6 +206,121 @@ var _ = Describe("Reconciler", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(fakeClient.Get(ctx, client.ObjectKey{Name: cloudProfileName}, &gardencorev1beta1.CloudProfile{})).To(BeNotFoundError())
+		})
+	})
+
+	Context("status reconciliation", func() {
+		var (
+			now    time.Time
+			future *metav1.Time
+			past   *metav1.Time
+
+			testStatus = func(wantStatus gardencorev1beta1.CloudProfileStatus) reconcile.Result {
+				Expect(fakeClient.Create(ctx, cloudProfile.DeepCopy())).To(Succeed())
+				result, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: cloudProfileName}})
+				Expect(err).NotTo(HaveOccurred())
+
+				got := &gardencorev1beta1.CloudProfile{}
+				Expect(fakeClient.Get(ctx, client.ObjectKey{Name: cloudProfileName}, got)).To(Succeed())
+				Expect(got.Status).To(Equal(wantStatus))
+
+				return result
+			}
+		)
+
+		BeforeEach(func() {
+			now = fakeClock.Now()
+			future = &metav1.Time{Time: now.Add(24 * time.Hour)}
+			past = &metav1.Time{Time: now.Add(-24 * time.Hour)}
+		})
+
+		It("should reconcile status of lifecycle classifications and requeue due to upcoming stage", func() {
+			cloudProfile.Spec.Kubernetes.Versions = []gardencorev1beta1.ExpirableVersion{
+				{
+					Version: "1.28.2",
+					Lifecycle: []gardencorev1beta1.LifecycleStage{
+						{
+							Classification: gardencorev1beta1.ClassificationPreview,
+							StartTime:      past,
+						},
+						{
+							Classification: gardencorev1beta1.ClassificationSupported,
+							StartTime:      future,
+						},
+					},
+				},
+			}
+
+			wantStatus := gardencorev1beta1.CloudProfileStatus{
+				Kubernetes: &gardencorev1beta1.KubernetesStatus{
+					Versions: []gardencorev1beta1.ExpirableVersionStatus{
+						{
+							Version:        "1.28.2",
+							Classification: gardencorev1beta1.ClassificationPreview,
+						},
+					},
+				},
+			}
+			result := testStatus(wantStatus)
+			Expect(result.RequeueAfter).To(BeNumerically("~", future.Sub(now), time.Second))
+		})
+
+		It("should reconcile status of old classifications and requeue due to expiration date", func() {
+			cloudProfile.Spec.Kubernetes.Versions = []gardencorev1beta1.ExpirableVersion{
+				{
+					Version:        "1.28.2",
+					ExpirationDate: past,
+					Classification: ptr.To(gardencorev1beta1.ClassificationSupported),
+				},
+				{
+					Version:        "1.30.2",
+					ExpirationDate: future,
+					Classification: ptr.To(gardencorev1beta1.ClassificationSupported),
+				},
+			}
+
+			wantStatus := gardencorev1beta1.CloudProfileStatus{
+				Kubernetes: &gardencorev1beta1.KubernetesStatus{
+					Versions: []gardencorev1beta1.ExpirableVersionStatus{
+						{
+							Version:        "1.28.2",
+							Classification: gardencorev1beta1.ClassificationExpired,
+						},
+						{
+							Version:        "1.30.2",
+							Classification: gardencorev1beta1.ClassificationSupported,
+						},
+					},
+				},
+			}
+
+			result := testStatus(wantStatus)
+			Expect(result.RequeueAfter).To(BeNumerically("~", future.Sub(now), time.Second))
+		})
+
+		It("should reconcile status of lifecycle classifications but not requeue without upcoming stages", func() {
+			cloudProfile.Spec.Kubernetes.Versions = []gardencorev1beta1.ExpirableVersion{
+				{
+					Version: "1.28.2",
+					Lifecycle: []gardencorev1beta1.LifecycleStage{
+						{Classification: gardencorev1beta1.ClassificationSupported, StartTime: past},
+					},
+				},
+			}
+
+			wantStatus := gardencorev1beta1.CloudProfileStatus{
+				Kubernetes: &gardencorev1beta1.KubernetesStatus{
+					Versions: []gardencorev1beta1.ExpirableVersionStatus{
+						{
+							Version:        "1.28.2",
+							Classification: gardencorev1beta1.ClassificationSupported,
+						},
+					},
+				},
+			}
+
+			result := testStatus(wantStatus)
+			Expect(result.RequeueAfter).To(BeZero())
 		})
 	})
 })
