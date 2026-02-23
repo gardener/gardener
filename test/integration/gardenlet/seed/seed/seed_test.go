c1ad9ab3c2268eeb40850941f022dbafab442131
Radoslav Hubenov <rrhubenov@gmail.com>
2026-02-23 14:53:15 +0200

    Expect VictoriaLogs managed resource if FeatureGate is enabled

--- test/integration/gardenlet/seed/seed/seed_test.go
+++ test/integration/gardenlet/seed/seed/seed_test.go
@@ -48,6 +48,7 @@ import (
 	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheusoperator"
 	opentelemetryoperator "github.com/gardener/gardener/pkg/component/observability/opentelemetry/operator"
 	"github.com/gardener/gardener/pkg/controllerutils"
+	"github.com/gardener/gardener/pkg/features"
 	seedcontroller "github.com/gardener/gardener/pkg/gardenlet/controller/seed/seed"
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
 	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
@@ -730,6 +731,10 @@ var _ = Describe("Seed controller tests", func() {
 						"opentelemetry-operator",
 						"opentelemetry-collector",
 					)
+					// Add victoria-logs when VictoriaLogsBackend feature gate is enabled
+					if features.DefaultFeatureGate.Enabled(features.VictoriaLogsBackend) {
+						expectedManagedResources = append(expectedManagedResources, "victoria-logs")
+					}
 				} else {
 					expectedManagedResources = append(expectedManagedResources,
 						"nginx-ingress-seed",
@@ -893,6 +898,7 @@ var _ = Describe("Seed controller tests", func() {
 					test(true)
 				})
 			})
+
 		})
 	})
 })
