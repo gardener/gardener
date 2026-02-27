870984cd7612a5346c6a35d7a51cfb78d950f79f
Plamen Kokanov <plamen.kokanov@sap.com>
2026-02-27 21:20:10 +0200

    Parallelise deployment of `MachineDeployments`

--- extensions/pkg/controller/worker/genericactuator/actuator.go
+++ extensions/pkg/controller/worker/genericactuator/actuator.go
@@ -28,6 +28,9 @@ import (
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
 )
 
+// maxConcurrentMachineTasks defines the maximum number of machine-related tasks that can run concurrently.
+const maxConcurrentMachineTasks = 10
+
 type genericActuator struct {
 	delegateFactory    DelegateFactory
 	gardenReader       client.Reader
