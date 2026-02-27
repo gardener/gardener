208be35b70583a43ed0797cf9414301740dad7f4
Thomas Buchner <thomas.buchner@sap.com>
2026-02-27 10:46:36 +0100

    fix: allow debian version numbers when parsing containerd version
    Certain containerd builds (mainly on Debian based distros) can have
    internal build information and package version numbers embedded
    into the containerd binary itself. This can result in containerd
    returning "1.7.23~ds2" as its version number which is not semver
    compliant. Also, version numbers like "1.7.23-ds2_1592.12" have
    been seen in the wild, which are also not semver compliant.
    As a result, GNA will fail parsing these numbers into a semver
    version object and will fail reconciling a node.
    To fix this, after the version number is retrieved from containerd
    any occurrence of -, + or ~ will be right-trimmed from the patch
    field of the version number string before it gets parsed into a
    semver object.

---
+++ pkg/nodeagent/containerd/containerd_suite_test.go
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package containerd_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/gardener/gardener/pkg/nodeagent/features"
+)
+
+func TestOperatingSystemConfig(t *testing.T) {
+	features.RegisterFeatureGates()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Containerd Client Suite")
+}
