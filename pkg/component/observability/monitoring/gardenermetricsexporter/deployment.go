59a92a06f1132f74387a7eadf7a8d87b1dd38b2b
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2026-03-04 17:32:09 +0100

    [release-v1.136] Add startup probe to gardener-metrics-exporter (#14226)
    * Add initial delay to gardener-metrics-exporter liveness probe
    
    * Use switch to startup probe
    
    ---------
    
    Co-authored-by: Christoph Kleineweber <c.kleineweber@sap.com>

--- pkg/component/observability/monitoring/gardenermetricsexporter/deployment.go
+++ pkg/component/observability/monitoring/gardenermetricsexporter/deployment.go
@@ -84,6 +84,18 @@ func (g *gardenerMetricsExporter) deployment(secretGenericTokenKubeconfig, secre
 								},
 								PeriodSeconds: 5,
 							},
+							StartupProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path:   "/",
+										Port:   intstr.FromInt32(probePort),
+										Scheme: corev1.URISchemeHTTP,
+									},
+								},
+								// Wait for 2 minutes to allow the informer cache to sync
+								PeriodSeconds:    5,
+								FailureThreshold: 24,
+							},
 							ReadinessProbe: &corev1.Probe{
 								ProbeHandler: corev1.ProbeHandler{
 									HTTPGet: &corev1.HTTPGetAction{
