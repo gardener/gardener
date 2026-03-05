7b2e73946aff7a9eb1fa0efc3fc11a4996db6baf
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-03-05 21:10:44 +0000

    Update dependency GoogleContainerTools/skaffold to v2.17.3

--- hack/tools.mk
+++ hack/tools.mk
@@ -75,7 +75,7 @@ PROMTOOL_VERSION ?= 3.9.1
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf
 PROTOC_VERSION ?= v34.0
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
-SKAFFOLD_VERSION ?= v2.17.2
+SKAFFOLD_VERSION ?= v2.17.3
 # renovate: datasource=github-releases depName=mikefarah/yq
 YQ_VERSION ?= v4.52.4
 # renovate: datasource=github-releases depName=ironcore-dev/vgopath
