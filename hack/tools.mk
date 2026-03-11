89e798a1e1116d0ac5ccb6d434f8e098618800e0
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-03-11 09:04:35 +0000

    Update prometheus to v3.10.0

--- hack/tools.mk
+++ hack/tools.mk
@@ -71,7 +71,7 @@ KUBECTL_VERSION ?= v1.35.2
 # renovate: datasource=github-releases depName=kubernetes-sigs/kustomize
 KUSTOMIZE_VERSION ?= v5.3.0
 # renovate: datasource=github-releases depName=prometheus/prometheus
-PROMTOOL_VERSION ?= 3.9.1
+PROMTOOL_VERSION ?= 3.10.0
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf
 PROTOC_VERSION ?= v34.0
 # renovate: datasource=github-releases depName=GoogleContainerTools/skaffold
