d83de135d04b4a5dc3683954f3420f25443ad747
Shafeeque E S <shafeeque.e.s@sap.com>
2026-03-24 14:00:08 +0530

    Address PR review feedback

--- hack/ci-e2e-kind.sh
+++ hack/ci-e2e-kind.sh
@@ -18,8 +18,8 @@ if [[ -n "$IPFAMILY" ]] && [[ "$IPFAMILY" == "ipv6" ]]; then
   # export all container logs and events after test execution
   trap "
     ( export_artifacts_host_services; export_artifacts_infra )
-    ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/runtime/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden extop)
-    ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/virtual-garden/kubeconfig; export cluster_name='virtual-garden'; export_resource_yamls_for gardenlet seeds shoots; export_events_for_shoots)
+    ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/runtime/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden extop )
+    ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/virtual-garden/kubeconfig; export cluster_name='virtual-garden'; export_resource_yamls_for gardenlet seeds shoots; export_events_for_shoots )
     ( make operator-seed-down )
     ( make kind-single-node-down )
   " EXIT
@@ -37,7 +37,7 @@ make kind-up
 trap "
   ( export_artifacts_host_services; export_artifacts_infra )
   ( export_artifacts "gardener-local" )
-  ( export KUBECONFIG=$GARDENER_LOCAL_KUBECONFIG; export cluster_name='virtual-garden'; export_resource_yamls_for gardenlet seeds shoots; export_events_for_shoots)
+  ( export KUBECONFIG=$GARDENER_LOCAL_KUBECONFIG; export cluster_name='virtual-garden'; export_resource_yamls_for gardenlet seeds shoots; export_events_for_shoots )
   ( make gardener-down )
   ( make kind-down )
 " EXIT
