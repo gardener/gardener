e984bfeea4dbecc3c02872747f848d5aa22a30f3
Tobias Schlicht <tobias.schlicht@sap.com>
2026-03-27 10:41:39 +0100

    Prefactor: Make `BackupBucket` check reusable

--- pkg/api/core/v1beta1/helper/backupbucket.go
+++ pkg/api/core/v1beta1/helper/backupbucket.go
@@ -5,6 +5,11 @@
 package helper
 
 import (
+	"fmt"
+	"strings"
+
+	"k8s.io/utils/clock"
+
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 )
 
@@ -20,4 +25,29 @@ func BackupBucketIsErroneous(bb *gardencorev1beta1.BackupBucket) (bool, string) 
 		return false, ""
 	}
 	return true, lastErr.Description
+}
+
+// ComputeBackupBucketsCondition computes a BackupBucketsReady condition from the given list of BackupBuckets.
+func ComputeBackupBucketsCondition(
+	clock clock.Clock,
+	condition gardencorev1beta1.Condition,
+	backupBuckets []gardencorev1beta1.BackupBucket,
+) gardencorev1beta1.Condition {
+	var erroneousBuckets []string
+	for _, bb := range backupBuckets {
+		if occurred, msg := BackupBucketIsErroneous(&bb); occurred {
+			erroneousBuckets = append(erroneousBuckets, fmt.Sprintf("Name: %s, Error: %s", bb.Name, msg))
+		}
+	}
+
+	if len(erroneousBuckets) > 0 {
+		msg := "The following BackupBuckets have issues:\n* " + strings.Join(erroneousBuckets, "\n* ")
+		return UpdatedConditionWithClock(clock, condition, gardencorev1beta1.ConditionFalse, "BackupBucketsError", msg)
+	}
+
+	if len(backupBuckets) == 0 {
+		return UpdatedConditionWithClock(clock, condition, gardencorev1beta1.ConditionUnknown, "BackupBucketsGone", "Backup Buckets are gone.")
+	}
+
+	return UpdatedConditionWithClock(clock, condition, gardencorev1beta1.ConditionTrue, "BackupBucketsAvailable", "Backup Buckets are available.")
 }
