0255653fbea7a2d5dc61778c72b17719a23afca1
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-04-03 17:17:55 +0300

    chore: mv variables to detecatd functions in dataplanedeployment_test
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- pkg/component/observability/opentelemetry/dataplanedeployment/dataplanedeployment_test.go
+++ pkg/component/observability/opentelemetry/dataplanedeployment/dataplanedeployment_test.go
@@ -7,10 +7,13 @@ package dataplanedeployment_test
 import (
 	"context"
 
+	"github.com/goccy/go-yaml"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -64,203 +67,6 @@ var _ = Describe("DataplaneDeployment", func() {
 	})
 
 	Describe("#Deploy", func() {
-		var (
-			// TODO: remove creationTimestamp lines
-			serviceAccountYAML = `apiVersion: v1
-automountServiceAccountToken: true
-kind: ServiceAccount
-metadata:
-  creationTimestamp: null
-  labels:
-    component: otel-collector-dataplane-deployment
-  name: otel-collector-dataplane-deployment
-  namespace: kube-system
-`
-			clusterRoleYAML = `apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  creationTimestamp: null
-  labels:
-    component: otel-collector-dataplane-deployment
-  name: otel-collector-dataplane-deployment
-rules:
-- apiGroups:
-  - ""
-  resources:
-  - nodes
-  - services
-  - endpoints
-  - pods
-  verbs:
-  - get
-  - list
-  - watch
-- apiGroups:
-  - ""
-  resources:
-  - nodes/metrics
-  verbs:
-  - get
-`
-			clusterRoleBindingYAML = `apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  creationTimestamp: null
-  labels:
-    component: otel-collector-dataplane-deployment
-  name: otel-collector-dataplane-deployment
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: otel-collector-dataplane-deployment
-subjects:
-- kind: ServiceAccount
-  name: otel-collector-dataplane-deployment
-  namespace: kube-system
-`
-			serviceYAML = `apiVersion: v1
-kind: Service
-metadata:
-  creationTimestamp: null
-  labels:
-    component: otel-collector-dataplane-deployment
-  name: otel-collector-dataplane-deployment
-  namespace: kube-system
-spec:
-  ports:
-  - name: metrics
-    port: 8080
-    protocol: TCP
-  selector:
-    component: otel-collector-dataplane-deployment
-status:
-  loadBalancer: {}
-`
-			// configMapYAMLPrefix = `apiVersion: v1
-			// data:
-		// config.yaml: |
-		// receivers:
-		// prometheus:
-		// config:
-		// scrape_configs:
-		// - job_name: 'kubernetes-service-endpoints'
-		// scrape_interval: 30s
-		// kubernetes_sd_configs:
-		// - role: endpoints
-		// namespaces:
-		// names:
-		// - default
-		// - kube-system
-		// relabel_configs:
-		// - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_scrape]
-		// action: keep
-		// regex: true
-		// - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_path]
-		// action: replace
-		// target_label: __metrics_path__
-		// regex: (.+)
-		// - source_labels: [__address__, __meta_kubernetes_service_annotation_prometheus_io_port]
-		// action: replace
-		// regex: ([^:]+)(?::\d+)?;(\d+)
-		// replacement: $1:$2
-		// target_label: __address__
-
-		// processors:
-		// batch:
-		// timeout: 10s
-		// send_batch_size: 1000
-
-		// exporters:
-		// prometheus:
-		// endpoint: "0.0.0.0:8080"
-		// namespace: ""
-		// send_timestamps: true
-		// metric_expiration: 5m
-
-		// service:
-		// pipelines:
-		// metrics:
-		// receivers: [prometheus]
-		// processors: [batch]
-		// exporters: [prometheus]
-		// kind: ConfigMap
-		// metadata:
-		// creationTimestamp: null
-		// labels:
-		// component: otel-collector-dataplane-deployment
-		// name: otel-collector-dataplane-deployment
-		// namespace: kube-system
-		// `
-		// deploymentYAMLPrefix = `apiVersion: apps/v1
-		// kind: Deployment
-		// metadata:
-		// creationTimestamp: null
-		// labels:
-		// component: otel-collector-dataplane-deployment
-		// gardener.cloud/role: monitoring
-		// origin: gardener
-		// name: otel-collector-dataplane-deployment
-		// namespace: kube-system
-		// spec:
-		// replicas: 1
-		// revisionHistoryLimit: 2
-		// selector:
-		// matchLabels:
-		// component: otel-collector-dataplane-deployment
-		// strategy: {}
-		// template:
-		// metadata:
-		// creationTimestamp: null
-		// labels:
-		// component: otel-collector-dataplane-deployment
-		// gardener.cloud/role: monitoring
-		// networking.gardener.cloud/from-seed: allowed
-		// networking.gardener.cloud/to-dns: allowed
-		// origin: gardener
-		// spec:
-		// automountServiceAccountToken: true
-		// containers:
-		// - command:
-		// - /otelcol-custom
-		// - --config=/etc/otel-collector/config.yaml
-		// image: ` + image + `
-		// imagePullPolicy: IfNotPresent
-		// name: otel-collector-dataplane-deployment
-		// ports:
-		// - containerPort: 8080
-		// name: metrics
-		// protocol: TCP
-		// resources:
-		// limits:
-		// memory: 512Mi
-		// requests:
-		// cpu: 100m
-		// memory: 256Mi
-		// securityContext:
-		// allowPrivilegeEscalation: false
-		// capabilities:
-		// drop:
-		// - ALL
-		// readOnlyRootFilesystem: true
-		// volumeMounts:
-		// - mountPath: /etc/otel-collector
-		// name: config
-		// readOnly: true
-		// priorityClassName: gardener-shoot-system-700
-		// securityContext:
-		// runAsNonRoot: true
-		// runAsUser: 65534
-		// seccompProfile:
-		// type: RuntimeDefault
-		// serviceAccountName: otel-collector-dataplane-deployment
-		// volumes:
-		// - configMap:
-		// name: otel-collector-dataplane-deployment
-		// name: config
-		// status: {}
-		// `
-		)
-
 		JustBeforeEach(func() {
 			component = New(c, namespace, config)
 			// why do we initiall set managedResource but overwrite it here?
@@ -286,8 +92,6 @@ status:
 					}},
 				},
 			}
-			//TODO: annotating referenced objects, is it needed?
-			// EXPLAIN: is this needed?
 			utilruntime.Must(references.InjectAnnotations(expectedMr))
 			Expect(managedResource).To(DeepEqual(expectedMr))
 
@@ -411,3 +215,304 @@ status:
 		})
 	})
 })
+
+func getOtelCollectorDataplaneServiceAccount() *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "otel-collector-dataplane-deployment",
+			Namespace: "kube-system",
+			Labels: map[string]string{
+				"component": "otel-collector-dataplane-deployment",
+			},
+		},
+		AutomountServiceAccountToken: pointer.Bool(true),
+	}
+}
+
+func getOtelCollectorDataplaneClusterRole() *rbacv1.ClusterRole {
+	return &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "otel-collector-dataplane-deployment",
+			Labels: map[string]string{
+				"component": "otel-collector-dataplane-deployment",
+			},
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{
+					"nodes",
+					"services",
+					"endpoints",
+					"pods",
+				},
+				Verbs: []string{"get", "list", "watch"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{
+					"nodes/metrics",
+				},
+				Verbs: []string{"get"},
+			},
+		},
+	}
+}
+
+func getOtelCollectorDataplaneClusterRoleBinding() *rbacv1.ClusterRoleBinding {
+	return &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "otel-collector-dataplane-deployment",
+			Labels: map[string]string{
+				"component": "otel-collector-dataplane-deployment",
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "otel-collector-dataplane-deployment",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "otel-collector-dataplane-deployment",
+				Namespace: "kube-system",
+			},
+		},
+	}
+}
+
+func getOtelCollectorDataplaneService() *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "otel-collector-dataplane-deployment",
+			Namespace: "kube-system",
+			Labels: map[string]string{
+				"component": "otel-collector-dataplane-deployment",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"component": "otel-collector-dataplane-deployment",
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Name:     "metrics",
+					Port:     8080,
+					Protocol: corev1.ProtocolTCP,
+				},
+			},
+		},
+	}
+}
+
+func getOtelCollectorDataplaneConfigMap() (*corev1.ConfigMap, error) {
+	config := map[string]interface{}{
+		"receivers": map[string]interface{}{
+			"prometheus": map[string]interface{}{
+				"config": map[string]interface{}{
+					"scrape_configs": []interface{}{
+						map[string]interface{}{
+							"job_name":          "kube-kubelet",
+							"honor_labels":      true,
+							"scrape_interval":   "30s",
+							"scheme":            "https",
+							"bearer_token_file": "/var/run/secrets/kubernetes.io/serviceaccount/token",
+							"tls_config": map[string]interface{}{
+								"insecure_skip_verify": true,
+							},
+							"metrics_path": "/metrics",
+							"kubernetes_sd_configs": []interface{}{
+								map[string]interface{}{"role": "node"},
+							},
+							"relabel_configs": []interface{}{
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_node_name"},
+									"action":        "replace",
+									"target_label":  "instance",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_node_name"},
+									"action":        "replace",
+									"target_label":  "node",
+								},
+							},
+							"metric_relabel_configs": []interface{}{
+								map[string]interface{}{
+									"source_labels": []string{"__name__"},
+									"action":        "keep",
+									"regex":         "^(up|kubelet_running_pods|process_max_fds|process_open_fds|kubelet_volume_stats_available_bytes|kubelet_volume_stats_capacity_bytes|kubelet_volume_stats_used_bytes|kubelet_image_pull_duration_seconds_bucket|kubelet_image_pull_duration_seconds_sum|kubelet_image_pull_duration_seconds_count)$",
+								},
+							},
+						},
+						map[string]interface{}{
+							"job_name":          "cadvisor",
+							"honor_labels":      true,
+							"scrape_interval":   "30s",
+							"scheme":            "https",
+							"bearer_token_file": "/var/run/secrets/kubernetes.io/serviceaccount/token",
+							"tls_config": map[string]interface{}{
+								"insecure_skip_verify": true,
+							},
+							"metrics_path": "/metrics/cadvisor",
+							"kubernetes_sd_configs": []interface{}{
+								map[string]interface{}{"role": "node"},
+							},
+							"relabel_configs": []interface{}{
+								map[string]interface{}{
+									"action": "labelmap",
+									"regex":  "__meta_kubernetes_node_label_(.+)",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_node_name"},
+									"action":        "replace",
+									"target_label":  "instance",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_node_name"},
+									"action":        "replace",
+									"target_label":  "node",
+								},
+								map[string]interface{}{
+									"target_label": "type",
+									"replacement":  "shoot",
+								},
+							},
+							"metric_relabel_configs": []interface{}{
+								map[string]interface{}{
+									"source_labels": []string{"id"},
+									"action":        "replace",
+									"regex":         "^/system\\.slice/(.+)\\.service$",
+									"target_label":  "systemd_service_name",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"id"},
+									"action":        "replace",
+									"regex":         "^/system\\.slice/(.+)\\.service$",
+									"target_label":  "container",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__name__"},
+									"action":        "keep",
+									"regex":         "^(up|container_cpu_cfs_periods_total|container_cpu_cfs_throttled_seconds_total|container_cpu_cfs_throttled_periods_total|container_cpu_usage_seconds_total|container_fs_inodes_total|container_fs_limit_bytes|container_fs_usage_bytes|container_fs_reads_total|container_fs_writes_total|container_last_seen|container_memory_working_set_bytes|container_network_receive_bytes_total|container_network_transmit_bytes_total)$",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"namespace"},
+									"action":        "keep",
+									"regex":         "(^$|^kube-system$)",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"container", "__name__"},
+									"action":        "drop",
+									"regex":         "POD;(container_cpu_cfs_periods_total|container_cpu_cfs_throttled_seconds_total|container_cpu_cfs_throttled_periods_total|container_cpu_usage_seconds_total|container_fs_inodes_total|container_fs_limit_bytes|container_fs_usage_bytes|container_last_seen|container_memory_working_set_bytes)",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__name__", "container", "interface", "id"},
+									"action":        "keep",
+									"regex":         "container_network.+;;(eth0;/.+|(en.+|tunl0|eth0);/)|.+;.+;.*;.*",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__name__", "container", "interface"},
+									"action":        "drop",
+									"regex":         "container_network.+;POD;(.{5,}|tun0|en.+)",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__name__", "id"},
+									"regex":         "container_network.+;/",
+									"replacement":   "true",
+									"target_label":  "host_network",
+								},
+								map[string]interface{}{
+									"regex":  "^id$",
+									"action": "labeldrop",
+								},
+							},
+						},
+						map[string]interface{}{
+							"job_name":        "node-exporter",
+							"honor_labels":    true,
+							"scrape_interval": "30s",
+							"kubernetes_sd_configs": []interface{}{
+								map[string]interface{}{
+									"role": "endpoints",
+									"namespaces": map[string]interface{}{
+										"names": []string{"kube-system"},
+									},
+								},
+							},
+							"relabel_configs": []interface{}{
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_service_name"},
+									"action":        "keep",
+									"regex":         "node-exporter",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_endpoint_port_name"},
+									"action":        "keep",
+									"regex":         "metrics",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_pod_node_name"},
+									"action":        "replace",
+									"target_label":  "instance",
+								},
+								map[string]interface{}{
+									"source_labels": []string{"__meta_kubernetes_pod_node_name"},
+									"action":        "replace",
+									"target_label":  "node",
+								},
+							},
+							"metric_relabel_configs": []interface{}{
+								map[string]interface{}{
+									"source_labels": []string{"__name__"},
+									"action":        "keep",
+									"regex":         "^(up|node_boot_time_seconds|node_cpu_seconds_total|node_disk_read_bytes_total|node_disk_written_bytes_total|node_disk_io_time_weighted_seconds_total|node_disk_io_time_seconds_total|node_disk_write_time_seconds_total|node_disk_writes_completed_total|node_disk_read_time_seconds_total|node_disk_reads_completed_total|node_filesystem_avail_bytes|node_filesystem_files|node_filesystem_files_free|node_filesystem_free_bytes|node_filesystem_readonly|node_filesystem_size_bytes|node_load1|node_load15|node_load5|node_memory_.+|node_nf_conntrack_.+|node_scrape_collector_duration_seconds|node_scrape_collector_success|process_max_fds|process_open_fds)$",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"processors": map[string]interface{}{
+			"batch": map[string]interface{}{
+				"timeout":         "10s",
+				"send_batch_size": 1000,
+			},
+		},
+		"exporters": map[string]interface{}{
+			"prometheus": map[string]interface{}{
+				"endpoint":          "0.0.0.0:8080",
+				"send_timestamps":   true,
+				"metric_expiration": "5m",
+			},
+		},
+		"service": map[string]interface{}{
+			"pipelines": map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"receivers":  []string{"prometheus"},
+					"processors": []string{"batch"},
+					"exporters":  []string{"prometheus"},
+				},
+			},
+		},
+	}
+
+	data, err := yaml.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "otel-collector-dataplane-deployment",
+			Namespace: "kube-system",
+			Labels: map[string]string{
+				"component": "otel-collector-dataplane-deployment",
+			},
+		},
+		Data: map[string]string{
+			"config.yaml": string(data),
+		},
+	}, nil
+}
