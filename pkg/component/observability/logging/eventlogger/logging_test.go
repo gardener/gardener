1246c0814cf33ebd13e42c5748ed9e3c247e9bee
Ilia Petrov <72142537+iypetrov@users.noreply.github.com>
2026-04-06 19:27:41 +0300

    Fix formatting of `event-logger` logs (#14423)
    * feat: add lua script to make the proper lift
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * feat: move the lua script to event-logging
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * chore: correct the tests + setup
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * fix: ups, forgot to rm old test
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * feat: rm not needed nest field
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * fix: the event-logger version should match with the one of fluent-bit-output-plugin
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * chore: probably in other PR
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * feat: disable the nest filter only when otel-col is enabled, to reduce the blast radius
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * feat: add test cov for case OpenTelemetryCollector=true, as @ialidzhikov suggested
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    
    * Apply suggestion from @ialidzhikov
    
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>

--- pkg/component/observability/logging/eventlogger/logging_test.go
+++ pkg/component/observability/logging/eventlogger/logging_test.go
@@ -12,41 +12,83 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/gardener/gardener/pkg/component/observability/logging/eventlogger"
+	"github.com/gardener/gardener/pkg/features"
+	testutils "github.com/gardener/gardener/pkg/utils/test"
 )
 
 var _ = Describe("Logging", func() {
 	Describe("#CentralLoggingConfiguration", func() {
-		It("should return the expected logging parser and filter", func() {
-			loggingConfig, err := CentralLoggingConfiguration()
 
-			Expect(err).NotTo(HaveOccurred())
-			Expect(loggingConfig.Filters).To(Equal(
-				[]*fluentbitv1alpha2.ClusterFilter{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:   "event-logger",
-							Labels: map[string]string{"fluentbit.gardener/type": "seed"},
-						},
-						Spec: fluentbitv1alpha2.FilterSpec{
-							Match: "kubernetes.*event-logger*event-logger*",
-							FilterItems: []fluentbitv1alpha2.FilterItem{
-								{
-									Nest: &fluentbitv1alpha2filter.Nest{
-										Operation:   "lift",
-										NestedUnder: "log",
+		When("OpenTelemetryCollector=false", func() {
+			BeforeEach(func() {
+				DeferCleanup(testutils.WithFeatureGate(features.DefaultFeatureGate, features.OpenTelemetryCollector, false))
+			})
+
+			It("should return the expected logging parser and filter", func() {
+				loggingConfig, err := CentralLoggingConfiguration()
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(loggingConfig.Filters).To(Equal(
+					[]*fluentbitv1alpha2.ClusterFilter{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:   "event-logger",
+								Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+							},
+							Spec: fluentbitv1alpha2.FilterSpec{
+								Match: "kubernetes.*event-logger*event-logger*",
+								FilterItems: []fluentbitv1alpha2.FilterItem{
+									{
+										RecordModifier: &fluentbitv1alpha2filter.RecordModifier{
+											Records: []string{"job event-logging"},
+										},
 									},
-								},
-								{
-									RecordModifier: &fluentbitv1alpha2filter.RecordModifier{
-										Records: []string{"job event-logging"},
+									{
+										Nest: &fluentbitv1alpha2filter.Nest{
+											Operation:   "lift",
+											NestedUnder: "log",
+										},
 									},
 								},
 							},
 						},
-					},
-				}))
-			Expect(loggingConfig.Inputs).To(BeNil())
-			Expect(loggingConfig.Parsers).To(BeNil())
+					}))
+				Expect(loggingConfig.Inputs).To(BeNil())
+				Expect(loggingConfig.Parsers).To(BeNil())
+			})
+		})
+
+		When("OpenTelemetryCollector=true", func() {
+			BeforeEach(func() {
+				DeferCleanup(testutils.WithFeatureGate(features.DefaultFeatureGate, features.OpenTelemetryCollector, true))
+			})
+
+			It("should return the expected logging parser and filter", func() {
+				loggingConfig, err := CentralLoggingConfiguration()
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(loggingConfig.Filters).To(Equal(
+					[]*fluentbitv1alpha2.ClusterFilter{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:   "event-logger",
+								Labels: map[string]string{"fluentbit.gardener/type": "seed"},
+							},
+							Spec: fluentbitv1alpha2.FilterSpec{
+								Match: "kubernetes.*event-logger*event-logger*",
+								FilterItems: []fluentbitv1alpha2.FilterItem{
+									{
+										RecordModifier: &fluentbitv1alpha2filter.RecordModifier{
+											Records: []string{"job event-logging"},
+										},
+									},
+								},
+							},
+						},
+					}))
+				Expect(loggingConfig.Inputs).To(BeNil())
+				Expect(loggingConfig.Parsers).To(BeNil())
+			})
 		})
 	})
 })
