49ba5c0cb959321f1f9029dd686d3d3ccc81fe2d
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-04-06 13:41:35 +0300

    Use the correct kubeconfig for runtime cluster

--- hack/ci-e2e-kind.sh
+++ hack/ci-e2e-kind.sh
@@ -18,7 +18,7 @@ if [[ -n "$IPFAMILY" ]] && [[ "$IPFAMILY" == "ipv6" ]]; then
   # export all container logs and events after test execution
   trap "
     ( export_artifacts_host_services; export_artifacts_infra )
-    ( export KUBECONFIG=$PWD/example/gardener-local/kind/multi-zone/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden extop)
+    ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/runtime/kubeconfig; export_artifacts 'gardener-operator-local'; export_resource_yamls_for garden extop)
     ( export KUBECONFIG=$PWD/dev-setup/kubeconfigs/virtual-garden/kubeconfig; export cluster_name='virtual-garden'; export_resource_yamls_for gardenlet seeds shoots; export_events_for_shoots)
     ( make kind-single-node-down )
   " EXIT
