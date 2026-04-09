90529e9448e9ea99fe811313281dc4dbb9304c48
Matthias Horne <matthias.horne@sap.com>
2026-04-09 13:04:31 +0200

    delete virtual garden PVCs in garden-down

--- dev-setup/garden.sh
+++ dev-setup/garden.sh
@@ -58,6 +58,10 @@ case "$COMMAND" in
 
     # cleanup the remaining resources required for successful deletion of the garden
     kubectl delete -k "$(dirname "$0")/garden/overlays/$SCENARIO" --ignore-not-found
+
+    # cleanup all PVCs in the garden namespace
+    echo "Cleaning up PVCs in garden namespace..."
+    kubectl delete pvc --all -n garden --ignore-not-found --timeout=120s
     ;;
 
   *)
