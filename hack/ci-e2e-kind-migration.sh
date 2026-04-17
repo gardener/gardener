48bc2f49e20ac42d4e0107fe8926ed9980f45616
rfranzke <rafael.franzke@sap.com>
2026-04-17 12:22:21 +0200

    Adapt e2e test scripts
    Co-authored-by: Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>

--- hack/ci-e2e-kind-migration.sh
+++ hack/ci-e2e-kind-migration.sh
@@ -13,21 +13,22 @@ source $(dirname "${0}")/ci-common.sh
 clamp_mss_to_pmtu
 
 # test setup
-make kind-up
-make kind2-up
+make kind-single-node-up
+make kind-single-node2-up
 
 # export all container logs and events after test execution
 trap "
   ( export_artifacts_host_services; export_artifacts_infra; export_artifacts_load_balancers )
   ( export KUBECONFIG=$KUBECONFIG_RUNTIME_CLUSTER; export_artifacts "gardener-local" )
-  ( export KUBECONFIG=$KUBECONFIG_RUNTIME_CLUSTER; export cluster_name='virtual-garden'; export_resource_yamls_for seeds shoots bastions.operations.gardener.cloud etcds leases; export_events_for_shoots )
+  ( export KUBECONFIG=$KUBECONFIG_VIRTUAL_GARDEN_CLUSTER; export cluster_name='virtual-garden'; export_resource_yamls_for seeds shoots bastions.operations.gardener.cloud etcds leases; export_events_for_shoots )
   ( export KUBECONFIG=$KUBECONFIG_SEED2_CLUSTER; export_artifacts "gardener-local2" )
-  ( make gardener-down )
-  ( make gardenlet-kind2-down )
-  ( make kind-down )
-  ( make kind2-down )
+  ( make seed-down KUBECONFIG="$KUBECONFIG_SEED2_CLUSTER" )
+  ( make operator-seed-down )
+  ( make kind-single-node2-down )
+  ( make kind-single-node-down )
 " EXIT
 
-make gardener-up
-make gardenlet-kind2-up
+make operator-seed-up
+make seed-up KUBECONFIG="$KUBECONFIG_SEED2_CLUSTER"
+
 make test-e2e-local-migration
