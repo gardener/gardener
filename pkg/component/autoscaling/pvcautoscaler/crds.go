f10593d9817cd569505378850bea60b80b3014d5
Rada Dimitrova <rada.dimitrova@sap.com>
2026-04-24 17:33:07 +0300

    Add pvc-autoscaler CRDs deployer

---
+++ pkg/component/autoscaling/pvcautoscaler/crds.go
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package pvcautoscaler
+
+import (
+	_ "embed"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/gardener/pkg/component"
+	"github.com/gardener/gardener/pkg/component/crddeployer"
+)
+
+var (
+	//go:embed assets/crd-persistentvolumeclaimautoscaler.yaml
+	persistentVolumeClaimAutoscalerCRD string
+)
+
+// NewCRDs can be used to deploy PVC Autoscaler CRDs
+func NewCRDs(client client.Client) (component.DeployWaiter, error) {
+	resources := []string{
+		persistentVolumeClaimAutoscalerCRD,
+	}
+
+	return crddeployer.New(client, resources, false)
+}
