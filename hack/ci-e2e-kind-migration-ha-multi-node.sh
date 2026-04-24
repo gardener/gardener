b9410ed8453d9af4b788a0044a761dd421a825ca
rfranzke <rafael.franzke@sap.com>
2026-04-24 09:12:09 +0200

    Address PR review feedback

--- hack/ci-e2e-kind-migration-ha-multi-node.sh
+++ hack/ci-e2e-kind-migration-ha-multi-node.sh
@@ -21,14 +21,14 @@ trap "
   ( export_artifacts_host_services; export_artifacts_infra; export_artifacts_load_balancers )
   ( export KUBECONFIG=$KUBECONFIG_RUNTIME_CLUSTER; export_artifacts 'gardener-local'; export_resource_yamls_for garden )
   ( export KUBECONFIG=$KUBECONFIG_VIRTUAL_GARDEN_CLUSTER; export cluster_name='virtual-garden'; export_resource_yamls_for gardenlet seeds shoots; export_events_for_shoots )
-  ( export KUBECONFIG=$KUBECONFIG_SEED2_CLUSTER; export_artifacts "gardener-local2" )
-  ( make seed-down KUBECONFIG="$KUBECONFIG_SEED2_CLUSTER" )
+  ( export KUBECONFIG=$KUBECONFIG_SEED2_CLUSTER; export_artifacts 'gardener-local2' )
+  ( make seed2-down )
   ( make gardener-down )
   ( make kind-multi-node2-down )
   ( make kind-multi-node-down )
 " EXIT
 
 make gardener-up
-make seed-up KUBECONFIG="$KUBECONFIG_SEED2_CLUSTER"
+make seed2-up
 
 make test-e2e-local-migration-ha-multi-node
--- hack/ci-e2e-kind-migration.sh
+++ hack/ci-e2e-kind-migration.sh
@@ -19,16 +19,16 @@ make kind2-up
 # export all container logs and events after test execution
 trap "
   ( export_artifacts_host_services; export_artifacts_infra; export_artifacts_load_balancers )
-  ( export KUBECONFIG=$KUBECONFIG_RUNTIME_CLUSTER; export_artifacts "gardener-local" )
+  ( export KUBECONFIG=$KUBECONFIG_RUNTIME_CLUSTER; export_artifacts 'gardener-local'; export_resource_yamls_for garden )
   ( export KUBECONFIG=$KUBECONFIG_VIRTUAL_GARDEN_CLUSTER; export cluster_name='virtual-garden'; export_resource_yamls_for seeds shoots bastions.operations.gardener.cloud etcds leases; export_events_for_shoots )
-  ( export KUBECONFIG=$KUBECONFIG_SEED2_CLUSTER; export_artifacts "gardener-local2" )
-  ( make seed-down KUBECONFIG="$KUBECONFIG_SEED2_CLUSTER" )
+  ( export KUBECONFIG=$KUBECONFIG_SEED2_CLUSTER; export_artifacts 'gardener-local2' )
+  ( make seed2-down )
   ( make gardener-down )
   ( make kind2-down )
   ( make kind-down )
 " EXIT
 
 make gardener-up
-make seed-up KUBECONFIG="$KUBECONFIG_SEED2_CLUSTER"
+make seed2-up
 
 make test-e2e-local-migration
