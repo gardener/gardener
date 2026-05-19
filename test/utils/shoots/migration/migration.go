1e0ff5ea9ff8a249a29bba8d1729f5f6f9a9503d
Shafeeque E S <shafeeque.e.s@sap.com>
2026-05-19 11:41:51 +0530

    modernize: apply stringsbuilder analyzer

--- test/utils/shoots/migration/migration.go
+++ test/utils/shoots/migration/migration.go
@@ -58,38 +58,38 @@ func GetPersistedSecrets(ctx context.Context, seedClient client.Reader, namespac
 
 // ComparePersistedSecrets ensures that two secret maps are equal.
 func ComparePersistedSecrets(secretsBefore, secretsAfter map[string]corev1.Secret) error {
-	var errorMsg string
+	var errorMsg strings.Builder
 	for name, secret := range secretsBefore {
 		if !reflect.DeepEqual(secret.Data, secretsAfter[name].Data) {
-			errorMsg += fmt.Sprintf("Secret %s/%s did not have its data persisted.\n", secret.Namespace, secret.Name)
+			errorMsg.WriteString(fmt.Sprintf("Secret %s/%s did not have its data persisted.\n", secret.Namespace, secret.Name))
 		}
 		if !maps.Equal(secret.Labels, secretsAfter[name].Labels) {
-			errorMsg += fmt.Sprintf("Secret %s/%s did not have its labels persisted: labels before migration: %v, labels after migration: %v\n",
+			errorMsg.WriteString(fmt.Sprintf("Secret %s/%s did not have its labels persisted: labels before migration: %v, labels after migration: %v\n",
 				secret.Namespace,
 				secret.Name,
 				secret.Labels,
 				secretsAfter[name].Labels,
-			)
+			))
 		}
 		if secret.Type != secretsAfter[name].Type {
-			errorMsg += fmt.Sprintf("Secret %s/%s did not have its type persisted: type before migration: %s, type after migration: %s\n",
+			errorMsg.WriteString(fmt.Sprintf("Secret %s/%s did not have its type persisted: type before migration: %s, type after migration: %s\n",
 				secret.Namespace,
 				secret.Name,
 				secret.Type,
 				secretsAfter[name].Type,
-			)
+			))
 		}
 		if !ptr.Equal(secret.Immutable, secretsAfter[name].Immutable) {
-			errorMsg += fmt.Sprintf("Secret %s/%s did not have its immutability persisted: immutable before migration: %t, immutable after migration: %t\n",
+			errorMsg.WriteString(fmt.Sprintf("Secret %s/%s did not have its immutability persisted: immutable before migration: %t, immutable after migration: %t\n",
 				secret.Namespace,
 				secret.Name,
 				ptr.Deref(secret.Immutable, false),
 				ptr.Deref(secretsAfter[name].Immutable, false),
-			)
+			))
 		}
 	}
-	if len(errorMsg) > 0 {
-		return fmt.Errorf("control plane secrets did not have their data or labels persisted during control plane migration:\n %s", errorMsg)
+	if len(errorMsg.String()) > 0 {
+		return fmt.Errorf("control plane secrets did not have their data or labels persisted during control plane migration:\n %s", errorMsg.String())
 	}
 	return nil
 }
