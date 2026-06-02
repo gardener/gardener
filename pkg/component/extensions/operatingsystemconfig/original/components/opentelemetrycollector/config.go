b38673130828365d306c8fef376cd8973cd2a50b
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-06-02 11:36:52 +0300

    fix: healthcheck oneshot should be driven only by the timer
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- pkg/component/extensions/operatingsystemconfig/original/components/opentelemetrycollector/config.go
+++ pkg/component/extensions/operatingsystemconfig/original/components/opentelemetrycollector/config.go
@@ -125,15 +125,12 @@ ExecStart=` + v1beta1constants.OperatingSystemConfigFilePathBinaries + `/opentel
 
 func getOpenTelemetryCollectorHealthCheckUnit() extensionsv1alpha1.Unit {
 	return extensionsv1alpha1.Unit{
-		Name:    UnitNameHealthCheck,
-		Command: new(extensionsv1alpha1.CommandStart),
-		Enable:  new(true),
+		Name:   UnitNameHealthCheck,
+		Enable: new(true),
 		Content: new(`[Unit]
 Description=Health check for ` + UnitName + `
 After=` + UnitName + `
 Requisite=` + UnitName + `
-[Install]
-WantedBy=multi-user.target
 [Service]
 Type=oneshot
 ExecStopPost=/bin/sh -c '[ "$SERVICE_RESULT" = "success" ] || systemctl restart ` + UnitName + `'
