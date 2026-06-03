28c8963a8fffe4c899292844df85f75841435c2f
Rada Dimitrova <rada.dimitrova@sap.com>
2026-06-03 18:25:43 +0300

    Add component checklist items

--- pkg/component/autoscaling/pvcautoscaler/pvcautoscaler.go
+++ pkg/component/autoscaling/pvcautoscaler/pvcautoscaler.go
@@ -8,13 +8,18 @@ import (
 	"context"
 	"time"
 
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	policyv1 "k8s.io/api/policy/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -22,6 +27,8 @@ import (
 	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
 	"github.com/gardener/gardener/pkg/client/kubernetes"
 	"github.com/gardener/gardener/pkg/component"
+	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheus/cache"
+	monitoringutils "github.com/gardener/gardener/pkg/component/observability/monitoring/utils"
 	"github.com/gardener/gardener/pkg/resourcemanager/controller/garbagecollector/references"
 	"github.com/gardener/gardener/pkg/utils"
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
@@ -29,11 +36,13 @@ import (
 )
 
 const (
-	// PVCAutoscalerManagedResourceNameName is the name of the PVCAutoscaler managed resource.
+	// PVCAutoscalerManagedResourceName is the name of the PVCAutoscaler managed resource.
 	PVCAutoscalerManagedResourceName = name
 
 	name               = "pvc-autoscaler"
 	serviceAccountName = name
+	metricsPortName    = "metrics"
+	metricsPort        = int32(8080)
 )
 
 // Values keeps values for the PVCAutoscaler.
@@ -73,9 +82,16 @@ func (p *pvcautoscaler) Deploy(ctx context.Context) error {
 		roleBinding         = p.roleBinding()
 		service             = p.service()
 		deployment          = p.deployment()
+		pdb                 = p.podDisruptionBudget()
+		vpa                 = p.verticalPodAutoscaler()
+		serviceMonitor      = p.serviceMonitor()
 	)
 
 	utilruntime.Must(references.InjectAnnotations(deployment))
+	utilruntime.Must(gardenerutils.InjectNetworkPolicyAnnotationsForSeedScrapeTargets(service, networkingv1.NetworkPolicyPort{
+		Port:     ptr.To(intstr.FromInt32(metricsPort)),
+		Protocol: ptr.To(corev1.ProtocolTCP),
+	}))
 
 	resources := []client.Object{
 		serviceAccount,
@@ -83,6 +99,9 @@ func (p *pvcautoscaler) Deploy(ctx context.Context) error {
 		roleBinding,
 		service,
 		deployment,
+		pdb,
+		vpa,
+		serviceMonitor,
 	}
 
 	for _, cr := range clusterRoles {
@@ -97,7 +116,6 @@ func (p *pvcautoscaler) Deploy(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-
 	return managedresources.CreateForSeed(ctx, p.client, p.namespace, PVCAutoscalerManagedResourceName, false, serializedResources)
 }
 
@@ -365,6 +383,12 @@ func (p *pvcautoscaler) service() *corev1.Service {
 					Protocol:   corev1.ProtocolTCP,
 					TargetPort: intstr.FromString("https"),
 				},
+				{
+					Name:       metricsPortName,
+					Port:       metricsPort,
+					Protocol:   corev1.ProtocolTCP,
+					TargetPort: intstr.FromString(metricsPortName),
+				},
 			},
 			Selector: map[string]string{
 				"control-plane": "controller-manager",
@@ -416,7 +440,7 @@ func (p *pvcautoscaler) deployment() *appsv1.Deployment {
 							ImagePullPolicy: corev1.PullIfNotPresent,
 							Args: []string{
 								"--health-probe-bind-address=:8081",
-								"--metrics-bind-address=127.0.0.1:8080",
+								"--metrics-bind-address=:8080",
 								"--leader-elect",
 								"--interval=30s",
 								"--prometheus-address=http://prometheus-cache.garden.svc.cluster.local:80",
@@ -436,6 +460,40 @@ func (p *pvcautoscaler) deployment() *appsv1.Deployment {
 									},
 								},
 							},
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "health",
+									ContainerPort: 8081,
+									Protocol:      corev1.ProtocolTCP,
+								},
+								{
+									Name:          metricsPortName,
+									ContainerPort: metricsPort,
+									Protocol:      corev1.ProtocolTCP,
+								},
+							},
+							LivenessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/healthz",
+										Port: intstr.FromString("health"),
+									},
+								},
+								InitialDelaySeconds: 15,
+								PeriodSeconds:       20,
+								FailureThreshold:    3,
+							},
+							ReadinessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/readyz",
+										Port: intstr.FromString("health"),
+									},
+								},
+								InitialDelaySeconds: 5,
+								PeriodSeconds:       10,
+								FailureThreshold:    3,
+							},
 							Resources: corev1.ResourceRequirements{
 								Requests: corev1.ResourceList{
 									corev1.ResourceCPU:    resource.MustParse("10m"),
@@ -449,6 +507,85 @@ func (p *pvcautoscaler) deployment() *appsv1.Deployment {
 					},
 				},
 			},
+		},
+	}
+}
+
+func (p *pvcautoscaler) podDisruptionBudget() *policyv1.PodDisruptionBudget {
+	return &policyv1.PodDisruptionBudget{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: p.namespace,
+			Labels:    getLabels(),
+		},
+		Spec: policyv1.PodDisruptionBudgetSpec{
+			MaxUnavailable: ptr.To(intstr.FromInt32(1)),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: utils.MergeStringMaps(getLabels(), map[string]string{
+					"control-plane": "controller-manager",
+				}),
+			},
+			UnhealthyPodEvictionPolicy: ptr.To(policyv1.AlwaysAllow),
+		},
+	}
+}
+
+func (p *pvcautoscaler) verticalPodAutoscaler() *vpaautoscalingv1.VerticalPodAutoscaler {
+	return &vpaautoscalingv1.VerticalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: p.namespace,
+			Labels:    getLabels(),
+		},
+		Spec: vpaautoscalingv1.VerticalPodAutoscalerSpec{
+			TargetRef: &autoscalingv1.CrossVersionObjectReference{
+				APIVersion: appsv1.SchemeGroupVersion.String(),
+				Kind:       "Deployment",
+				Name:       v1beta1constants.DeploymentNamePVCAutoscaler,
+			},
+			UpdatePolicy: &vpaautoscalingv1.PodUpdatePolicy{
+				UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeRecreate),
+			},
+			ResourcePolicy: &vpaautoscalingv1.PodResourcePolicy{
+				ContainerPolicies: []vpaautoscalingv1.ContainerResourcePolicy{
+					{
+						ContainerName:    name,
+						ControlledValues: ptr.To(vpaautoscalingv1.ContainerControlledValuesRequestsOnly),
+						MinAllowed: corev1.ResourceList{
+							corev1.ResourceMemory: resource.MustParse("64Mi"),
+						},
+					},
+					{
+						ContainerName: vpaautoscalingv1.DefaultContainerResourcePolicy,
+						Mode:          ptr.To(vpaautoscalingv1.ContainerScalingModeOff),
+					},
+				},
+			},
+		},
+	}
+}
+
+func (p *pvcautoscaler) serviceMonitor() *monitoringv1.ServiceMonitor {
+	return &monitoringv1.ServiceMonitor{
+		ObjectMeta: monitoringutils.ConfigObjectMeta(name, p.namespace, cache.Label),
+		Spec: monitoringv1.ServiceMonitorSpec{
+			Selector: metav1.LabelSelector{MatchLabels: getLabels()},
+			Endpoints: []monitoringv1.Endpoint{{
+				Port: metricsPortName,
+				RelabelConfigs: []monitoringv1.RelabelConfig{
+					{
+						Action:      "replace",
+						Replacement: ptr.To(name),
+						TargetLabel: "job",
+					},
+				},
+				MetricRelabelConfigs: monitoringutils.StandardMetricRelabelConfig(
+					"pvc_autoscaler_resized_total",
+					"pvc_autoscaler_threshold_reached_total",
+					"pvc_autoscaler_max_capacity_reached_total",
+					"pvc_autoscaler_skipped_total",
+				),
+			}},
 		},
 	}
 }
