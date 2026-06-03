279b8d4df3568363764f9b463a91340483972c2d
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-06-03 15:01:43 +0300

    Add `CPUStartupBoost` as a supported VPA feature gate

--- pkg/utils/validation/features/vpa.go
+++ pkg/utils/validation/features/vpa.go
@@ -12,6 +12,7 @@ import (
 var (
 	// vpaFeatureGates is a set of supported Vertical Pod Autoscaler feature gates
 	vpaFeatureGates = sets.New(
+		"CPUStartupBoost",
 		"InPlace",
 		"InPlaceOrRecreate",
 		"PerVPAConfig",
