928c395d5837c0557977b0ac8b5d8eb093ada9de
Lukas Hoehl <lukas.hoehl@stackit.cloud>
2026-06-15 13:30:44 +0200

    adapt e2e tests to skip if exposure class is missing
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- test/e2e/gardener/shoot/internal/exposureclass/exposureclass.go
+++ test/e2e/gardener/shoot/internal/exposureclass/exposureclass.go
@@ -5,14 +5,18 @@
 package exposureclass
 
 import (
+	"context"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	v1beta1helper "github.com/gardener/gardener/pkg/api/core/v1beta1/helper"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	. "github.com/gardener/gardener/test/e2e/gardener"
 	"github.com/gardener/gardener/test/e2e/gardener/shoot/internal/inclusterclient"
 )
@@ -24,7 +28,19 @@ func VerifyExposureClassSwitch(s *ShootContext, waitForReconcileFunc func(s *Sho
 	GinkgoHelper()
 	defer GinkgoRecover()
 
-	Describe("switching exposure class", func() {
+	expClass := &gardencorev1beta1.ExposureClass{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "local",
+		},
+	}
+
+	Describe("switching exposure class", Ordered, func() {
+		BeforeAll(func(ctx context.Context) {
+			if err := s.GardenClient.Get(ctx, client.ObjectKeyFromObject(expClass), expClass); apierrors.IsNotFound(err) {
+				Skip("exposure class not installed")
+			}
+		})
+
 		verifyAPIServerAccess := func() {
 			It("should be able to talk to API server", func(ctx SpecContext) {
 				Eventually(ctx, s.ShootKomega.Get(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: metav1.NamespaceSystem}})).Should(Succeed())
@@ -36,7 +52,7 @@ func VerifyExposureClassSwitch(s *ShootContext, waitForReconcileFunc func(s *Sho
 
 		It("Switch exposure class", func(ctx SpecContext) {
 			Eventually(ctx, s.GardenKomega.Update(s.Shoot, func() {
-				s.Shoot.Spec.ExposureClassName = new("local")
+				s.Shoot.Spec.ExposureClassName = &expClass.Name
 			})).Should(Succeed())
 		}, SpecTimeout(time.Minute))
 		waitForReconcileFunc(s)
