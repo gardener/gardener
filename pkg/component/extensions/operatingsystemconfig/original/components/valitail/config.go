885719a9bf765468f97ff4a2ca2a00f10a257a51
Ilia Petrov <72142537+iypetrov@users.noreply.github.com>
2026-06-22 18:17:41 +0300

    Apply suggestion from @ialidzhikov
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>

--- pkg/component/extensions/operatingsystemconfig/original/components/valitail/config.go
+++ pkg/component/extensions/operatingsystemconfig/original/components/valitail/config.go
@@ -118,7 +118,7 @@ func getValitailPathAuthTokenUnit() extensionsv1alpha1.Unit {
 		Command: new(extensionsv1alpha1.CommandStart),
 		Enable:  new(true),
 		Content: new(`[Unit]
-Description=Monitor the auth-token file for required for valitail
+Description=Monitor the auth-token file required for valitail
 [Install]
 WantedBy=paths.target
 [Path]
