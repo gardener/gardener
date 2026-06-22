14617382990da0c0575fabc5ca8bdfa7b14d2325
Marcel Boehm <marcel.boehm@inovex.de>
2026-06-22 12:03:54 +0200

    test machine pods did not roll during CP migration

--- test/e2e/gardener/shoot/create_migrate_delete.go
+++ test/e2e/gardener/shoot/create_migrate_delete.go
@@ -44,6 +44,8 @@ var _ = Describe("Shoot Tests", Label("Shoot", "control-plane-migration"), func(
 			seedClientSourceCluster = s.SeedClient
 		})
 
+		machinePodNamesBeforeTest := ItShouldFindAllMachinePodsBefore(s)
+
 		It("Populate comparison elements before migration", func(ctx SpecContext) {
 			Eventually(ctx, func() error {
 				var err error
@@ -77,6 +79,8 @@ var _ = Describe("Shoot Tests", Label("Shoot", "control-plane-migration"), func(
 		It("Verify that there are no orphaned resources in the source seed", func(ctx SpecContext) {
 			Expect(shootmigration.CheckForOrphanedNonNamespacedResources(ctx, s.Shoot.Namespace, seedClientSourceCluster)).To(Succeed())
 		}, SpecTimeout(time.Minute))
+
+		ItShouldCompareMachinePodNamesAfter(s, machinePodNamesBeforeTest)
 
 		if !v1beta1helper.IsWorkerless(s.Shoot) && !v1beta1helper.HibernationIsEnabled(s.Shoot) {
 			ItShouldInitializeShootClient(s)
