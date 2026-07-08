89355cc9de65969995eb8e7f5ef513dac5cb2df0
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2026-07-08 12:01:32 +0200

    [release-v1.146] Disable `health_check` extension in otel-collector config (#15207)
    * Disable `health_check` extension in otel-collector config
    
    The default `13133` port might conflict with other otel-collector instances
    run by shoot owners on the node. Comment out the extension until we switch
    to a non-default port.
    
    * Include `TODO` to comment
    
    ---------
    
    Co-authored-by: Radoslav Hubenov <rrhubenov@gmail.com>

--- pkg/component/extensions/operatingsystemconfig/original/components/opentelemetrycollector/config_test.go
+++ pkg/component/extensions/operatingsystemconfig/original/components/opentelemetrycollector/config_test.go
@@ -93,9 +93,13 @@ extensions:
     create_directory: true
   bearertokenauth:
     filename: /var/lib/opentelemetry-collector/auth-token
-  health_check:
-    endpoint: 0.0.0.0:13133
-    path: /healthz
+  # TODO(rrhubenov): health check can be enabled when ` + "`sd_notify`" + ` integration is implemented: https://github.com/open-telemetry/opentelemetry-collector/issues/15128
+  # The 13133 port should be changed to something else in the future,
+  # since it's the default value and might conflict with other instances of
+  # otel collector from shoot owners.
+  # health_check:
+  #   endpoint: 0.0.0.0:13133
+  #   path: /healthz
 
 receivers:
   journald/journal:
@@ -219,7 +223,7 @@ service:
       level: INFO
       encoding: json
 
-  extensions: [file_storage, bearertokenauth, health_check]
+  extensions: [file_storage, bearertokenauth]
   pipelines:
     logs/journal:
       receivers: [journald/journal]
