f73b16332a1ac53a21faea608a9b40d8753eec2a
Victor Herrero Otal <victor.herrero.otal@sap.com>
2026-07-21 11:13:25 +0200

    Execute "make generate" for mock recreation

--- pkg/component/observability/monitoring/alertmanager/mock/mocks.go
+++ pkg/component/observability/monitoring/alertmanager/mock/mocks.go
@@ -69,16 +69,16 @@ func (mr *MockInterfaceMockRecorder) Destroy(ctx any) *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Destroy", reflect.TypeOf((*MockInterface)(nil).Destroy), ctx)
 }
 
-// SetIngressAuthSecret mocks base method.
-func (m *MockInterface) SetIngressAuthSecret(arg0 *v1.Secret) {
+// SetIngressAuthSecretName mocks base method.
+func (m *MockInterface) SetIngressAuthSecretName(arg0 string) {
 	m.ctrl.T.Helper()
-	m.ctrl.Call(m, "SetIngressAuthSecret", arg0)
+	m.ctrl.Call(m, "SetIngressAuthSecretName", arg0)
 }
 
-// SetIngressAuthSecret indicates an expected call of SetIngressAuthSecret.
-func (mr *MockInterfaceMockRecorder) SetIngressAuthSecret(arg0 any) *gomock.Call {
+// SetIngressAuthSecretName indicates an expected call of SetIngressAuthSecretName.
+func (mr *MockInterfaceMockRecorder) SetIngressAuthSecretName(arg0 any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetIngressAuthSecret", reflect.TypeOf((*MockInterface)(nil).SetIngressAuthSecret), arg0)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetIngressAuthSecretName", reflect.TypeOf((*MockInterface)(nil).SetIngressAuthSecretName), arg0)
 }
 
 // SetIngressWildcardCertSecret mocks base method.
