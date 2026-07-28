2b12aa2ddd197d75f24ba6ec9a84b02e72162626
Marc Vornetran <marc.vornetran@sap.com>
2026-07-28 14:44:09 +0200

    Restart logrotate on failure

--- pkg/component/extensions/operatingsystemconfig/original/components/containerd/component_test.go
+++ pkg/component/extensions/operatingsystemconfig/original/components/containerd/component_test.go
@@ -38,6 +38,9 @@ var _ = Describe("Component", func() {
 Description=Rotate and Compress System Logs
 [Service]
 ExecStart=/usr/sbin/logrotate -s /var/lib/containerd-logrotate.status /etc/systemd/containerd.conf
+Restart=on-failure
+RestartSec=5
+StartLimitBurst=0
 [Install]
 WantedBy=multi-user.target`),
 				FilePaths: []string{"/etc/systemd/containerd.conf"},
