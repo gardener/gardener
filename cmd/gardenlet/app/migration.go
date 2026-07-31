0b3e42258df186ae57a74d0cadf4bf234d8abd0f
Shafeeque E S <shafeeque.e.s@sap.com>
2026-07-31 11:48:00 +0530

    Cleanup TODO related to hash versioning secrets (#15342)

--- cmd/gardenlet/app/migration.go
+++ cmd/gardenlet/app/migration.go
@@ -9,29 +9,15 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/go-logr/logr"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/gardener/pkg/api/core/v1beta1/helper"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
-	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
-	"github.com/gardener/gardener/pkg/component/extensions/operatingsystemconfig"
 	"github.com/gardener/gardener/pkg/features"
-	"github.com/gardener/gardener/pkg/utils/flow"
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
 )
 
-func (g *garden) runMigrations(ctx context.Context, log logr.Logger, gardenClient client.Client) error {
-	seedClient := g.mgr.GetClient()
-
-	// TODO(shafeeqes): Remove this function in gardener v1.148
-	log.Info("Cleaning up OSC hash versioning secrets in shoot namespaces")
-	if err := CleanupHashVersioningSecrets(ctx, seedClient); err != nil {
-		return fmt.Errorf("failed to clean up OSC hash versioning secrets: %w", err)
-	}
-
+func (g *garden) runMigrations(ctx context.Context, gardenClient client.Client) error {
 	if features.DefaultFeatureGate.Enabled(features.RemoveHTTPProxyLegacyPort) {
 		if err := VerifyRemoveHTTPProxyLegacyPortMigration(ctx, gardenClient, g.config.SeedConfig.Name); err != nil {
 			return fmt.Errorf("failed to verify migration for RemoveHTTPProxyLegacyPort feature gate: %w", err)
@@ -72,30 +58,4 @@ func VerifyRemoveHTTPProxyLegacyPortMigration(ctx context.Context, gardenClient 
 	}
 
 	return nil
-}
-
-// CleanupHashVersioningSecrets removes the OSC hash versioning secrets in shoot namespaces.
-func CleanupHashVersioningSecrets(ctx context.Context, seedClient client.Client) error {
-	shootNamespaceList := &corev1.NamespaceList{}
-	if err := seedClient.List(ctx, shootNamespaceList, client.MatchingLabels{v1beta1constants.GardenRole: v1beta1constants.GardenRoleShoot}); err != nil {
-		return err
-	}
-
-	var taskFns []flow.TaskFn
-
-	for _, ns := range shootNamespaceList.Items {
-		secret := &corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{Namespace: ns.Name, Name: operatingsystemconfig.WorkerPoolHashesSecretName},
-		}
-
-		taskFns = append(taskFns, func(ctx context.Context) error {
-			if err := client.IgnoreNotFound(seedClient.Delete(ctx, secret)); err != nil {
-				return fmt.Errorf("failed deleting secret %s in namespace %s: %w", secret.Name, secret.Namespace, err)
-			}
-
-			return nil
-		})
-	}
-
-	return flow.Parallel(taskFns...)(ctx)
 }
