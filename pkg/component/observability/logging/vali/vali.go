39af15ebcf83544114554d34eccb724615de16c2
Viktor <v.kostov@sap.com>
2026-08-03 11:19:04 +0300

    Remove redundant check to make getAutoscalerName consistent

--- pkg/component/observability/logging/vali/vali.go
+++ pkg/component/observability/logging/vali/vali.go
@@ -38,6 +38,7 @@ import (
 	kubeapiserverconstants "github.com/gardener/gardener/pkg/component/kubernetes/apiserver/constants"
 	valiconstants "github.com/gardener/gardener/pkg/component/observability/logging/vali/constants"
 	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheus/aggregate"
+	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheus/garden"
 	"github.com/gardener/gardener/pkg/component/observability/monitoring/prometheus/shoot"
 	monitoringutils "github.com/gardener/gardener/pkg/component/observability/monitoring/utils"
 	"github.com/gardener/gardener/pkg/features"
@@ -928,7 +929,7 @@ func (v *vali) getPrometheusLabel() string {
 // denotes the default (unnamed) instance running in seeds.
 func (v *vali) getAutoscalerName() string {
 	if v.values.IsGardenCluster {
-		return "garden"
+		return garden.Label
 	}
 	return ""
 }
