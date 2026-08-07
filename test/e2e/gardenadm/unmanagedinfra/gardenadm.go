576b7ffc85a7c4a9ad120b61cc22b6d95a5a043c
Johannes Scheerer <johannes.scheerer@sap.com>
2026-08-07 16:35:26 +0200

    Extend unmanaged infrastructure end-to-end test with `gardenadm reset`

--- test/e2e/gardenadm/unmanagedinfra/gardenadm.go
+++ test/e2e/gardenadm/unmanagedinfra/gardenadm.go
@@ -198,6 +198,53 @@ var _ = Describe("gardenadm unmanaged infrastructure scenario tests", Label("gar
 			}, SpecTimeout(2*time.Minute))
 		})
 
+		Context("gardenadm reset + join", Ordered, Label("resetjoin"), func() {
+			It("should generate a token and reset the worker node", func(ctx SpecContext) {
+				stdOut, _, err := execute(ctx, 0, "gardenadm", "token", "create", "--print-reset-command")
+				Expect(err).NotTo(HaveOccurred())
+				joinCommand := strings.Split(strings.ReplaceAll(string(stdOut.Contents()), `"`, ``), " ")
+
+				stdOut, _, err = execute(ctx, 1, append(joinCommand, "--log-level=debug")...)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(ctx, stdOut).Should(gbytes.Say("Cleaning up gardener-node-agent folder"))
+			}, SpecTimeout(time.Minute))
+
+			It("should no longer see the node in the cluster", func(ctx SpecContext) {
+				Eventually(ctx, func(g Gomega) {
+					node := &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: machineContainerName(1)}}
+					g.Expect(shootClientSet.Client().Get(ctx, client.ObjectKeyFromObject(node), node)).Should(HaveOccurred())
+				}).Should(Succeed())
+			}, SpecTimeout(time.Minute))
+
+			It("should generate a bootstrap token and join the worker node", func(ctx SpecContext) {
+				stdOut, _, err := execute(ctx, 0, "gardenadm", "token", "create", "--print-join-command")
+				Expect(err).NotTo(HaveOccurred())
+				joinCommand := strings.Split(strings.ReplaceAll(string(stdOut.Contents()), `"`, ``), " ")
+
+				stdOut, _, err = execute(ctx, 1, append(joinCommand, "--log-level=debug")...)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(ctx, stdOut).Should(gbytes.Say("Your node has successfully joined the cluster as a worker!"))
+			}, SpecTimeout(time.Minute))
+
+			It("should see the joined node and observe its readiness", func(ctx SpecContext) {
+				Eventually(ctx, func(g Gomega) {
+					node := &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: machineContainerName(1)}}
+					g.Expect(shootClientSet.Client().Get(ctx, client.ObjectKeyFromObject(node), node)).To(Succeed())
+
+					g.Expect(node.Status.Conditions).To(ContainCondition(
+						MatchFields(IgnoreExtras, Fields{"Type": Equal(corev1.NodeReady)}),
+						MatchFields(IgnoreExtras, Fields{"Status": Equal(corev1.ConditionTrue)}),
+					))
+					g.Expect(node.Spec.Taints).NotTo(ContainElement(corev1.Taint{
+						Key:    v1beta1constants.TaintNodeCriticalComponentsNotReady,
+						Effect: corev1.TaintEffectNoSchedule,
+					}))
+				}).Should(Succeed())
+			}, SpecTimeout(2*time.Minute))
+		})
+
 		Context("gardenadm connect", Ordered, Label("connect"), func() {
 			var (
 				gardenKomega                         Komega
