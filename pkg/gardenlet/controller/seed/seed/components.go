7bd06ea0c1dd874b2cb2f2f4e4b2f8b2d9a48641
TeodorDichev <teodichev@gmail.com>
2026-08-20 17:24:04 +0300

    Remove unused isValiEnabled bool flag
    Signed-off-by: TeodorDichev <teodichev@gmail.com>

--- pkg/gardenlet/controller/seed/seed/components.go
+++ pkg/gardenlet/controller/seed/seed/components.go
@@ -1025,7 +1025,6 @@ func (r *Reconciler) newFluentBit() (component.DeployWaiter, error) {
 		r.SeedClientSet.Client(),
 		r.GardenNamespace,
 		gardenlethelper.IsLoggingEnabled(&r.Config),
-		gardenlethelper.IsValiEnabled(&r.Config),
 		v1beta1constants.PriorityClassNameSeedSystem600,
 	)
 }
