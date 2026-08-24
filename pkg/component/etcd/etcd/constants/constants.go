14c4ed5bf10171fe399ac7e3c2f74211d200e93b
acumino <sonu.kumar.singh02@sap.com>
2026-08-24 17:27:56 +0530

    [GEP-39] Initialize etcd peerexposure component

--- pkg/component/etcd/etcd/constants/constants.go
+++ pkg/component/etcd/etcd/constants/constants.go
@@ -18,6 +18,12 @@ const (
 	ContainerNameEtcd = "etcd"
 	// ContainerNameBackupRestore is the name of the backup-restore sidecar container in the etcd pod.
 	ContainerNameBackupRestore = "backup-restore"
+	// LabelAppValue is the value of a label whose key is 'app'.
+	LabelAppValue = "etcd-statefulset"
+	// ServicePortNameEtcdPeer is the name prefix used for etcd peer ports on the Istio ingress gateway.
+	ServicePortNameEtcdPeer = "tls-etcd-peer"
+	// ServicePortNameEtcdClient is the name used for the etcd client port on the Istio ingress gateway.
+	ServicePortNameEtcdClient = "tls-etcd-client"
 )
 
 var (
@@ -25,6 +31,12 @@ var (
 	PortEtcdClient int32 = 2379
 	// PortEtcdPeer is the port exposed by etcd for server-to-server communication.
 	PortEtcdPeer int32 = 2380
+	// PortEtcdPeerExternal is the base port on the Istio ingress gateway for etcd peer traffic.
+	// Member ordinal i is exposed on PortEtcdPeerExternal+i.
+	PortEtcdPeerExternal int32 = 12380
+	// PortEtcdClientExternal is the port on the Istio ingress gateway for etcd client traffic.
+	PortEtcdClientExternal int32 = 12379
+
 	// PortBackupRestore is the client port exposed by the backup-restore sidecar container.
 	PortBackupRestore int32 = 8080
 	// PortEtcdWrapper is the port exposed by etcd-wrapper.
@@ -47,4 +59,7 @@ var (
 	VolumeNameServerTLS = "etcd-server-tls"
 	// VolumeNamePeerTLS is the name of the volume in the ETCD pod spec used for the peer TLS.
 	VolumeNamePeerTLS = "etcd-peer-server-tls"
+
+	// HAReplicaCount is the number of replicas for a highly available etcd cluster.
+	HAReplicaCount int32 = 3
 )
