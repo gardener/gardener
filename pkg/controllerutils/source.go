cdba2b5451b4fce8adf9cbb48ced3df06600a897
Shafeeque E S <shafeeque.e.s@sap.com>
2026-08-26 22:45:26 +0530

    gardenlet/backupentry: reduce etcd load on controller restart (#15568)
    * gardenlet/backupentry: skip redundant pending status patch on restart
    
    On gardenlet restart, controller-runtime re-enqueues all BackupEntries
    immediately. For entries still within the deletion grace period, the
    reconciler was unconditionally writing a "Pending" status patch for each
    one — even when the status was already Pending with the identical message
    (which is deterministic: deletionTimestamp + gracePeriod).
    
    With a high number of BackupEntries awaiting deletion, this caused a
    burst of unnecessary writes to etcd on every gardenlet restart.
    
    Skip the status patch when the BackupEntry is already in Pending state
    with the same scheduled-deletion message.
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Make EnqueueWithJitterDelay reusable
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Adapt gardenlet config
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Use EnqueueWithJitterDelay in BB and BE reconcilers
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Address PR review feedback
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Fix unit tests and linter errors
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    * Address PR review feedback
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>
    
    ---------
    
    Signed-off-by: Shafeeque E S <shafeeque.e.s@sap.com>

--- pkg/controllerutils/source.go
+++ pkg/controllerutils/source.go
@@ -7,12 +7,17 @@ package controllerutils
 import (
 	"context"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/gardener/gardener/pkg/utils"
 )
 
 // EnqueueOnce is a source.Source that simply triggers the reconciler once by directly enqueueing an empty
@@ -38,3 +43,76 @@ func (h *HandleOnce[object, request]) Start(ctx context.Context, q workqueue.Typ
 var EnqueueAnonymously = handler.EnqueueRequestsFromMapFunc(func(_ context.Context, _ client.Object) []reconcile.Request {
 	return []reconcile.Request{{}}
 })
+
+// RandomDurationWithMetaDuration is an alias for `utils.RandomDurationWithMetaDuration`. Exposed for unit tests.
+var RandomDurationWithMetaDuration = utils.RandomDurationWithMetaDuration
+
+// EnqueueWithJitterDelay returns an EventHandler that enqueues objects with an optional jitter delay.
+// getObservedGeneration must return the object's ObservedGeneration and true, or 0 and false if the object type is unexpected.
+func EnqueueWithJitterDelay(
+	getObservedGeneration func(client.Object) (int64, bool),
+	jitterUpdates *bool,
+	syncJitterPeriod *metav1.Duration,
+) handler.EventHandler {
+	reconcileRequest := func(obj client.Object) reconcile.Request {
+		return reconcile.Request{NamespacedName: types.NamespacedName{Name: obj.GetName(), Namespace: obj.GetNamespace()}}
+	}
+
+	addWithJitter := func(obj client.Object, q workqueue.TypedRateLimitingInterface[reconcile.Request], jitter bool) {
+		if jitter {
+			q.AddAfter(reconcileRequest(obj), RandomDurationWithMetaDuration(syncJitterPeriod))
+		} else {
+			q.Add(reconcileRequest(obj))
+		}
+	}
+
+	return &handler.Funcs{
+		CreateFunc: func(_ context.Context, e event.CreateEvent, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+			observedGeneration, ok := getObservedGeneration(e.Object)
+			if !ok {
+				return
+			}
+
+			// Objects with deletion timestamp and newly created objects will be enqueued immediately.
+			// Generation is 1 for newly created objects.
+			if e.Object.GetDeletionTimestamp() != nil || e.Object.GetGeneration() == 1 {
+				q.Add(reconcileRequest(e.Object))
+				return
+			}
+
+			if e.Object.GetGeneration() != observedGeneration {
+				addWithJitter(e.Object, q, ptr.Deref(jitterUpdates, false))
+				return
+			}
+
+			// Spread reconciliation of  across the configured sync jitter
+			// period to avoid overloading the gardener-apiserver
+			addWithJitter(e.Object, q, true)
+		},
+		UpdateFunc: func(_ context.Context, e event.UpdateEvent, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+			observedGeneration, ok := getObservedGeneration(e.ObjectNew)
+			if !ok {
+				return
+			}
+
+			if e.ObjectNew.GetGeneration() == observedGeneration {
+				return
+			}
+
+			// Objects with deletion timestamp and newly created objects will be enqueued immediately.
+			// Generation is 1 for newly created objects.
+			if e.ObjectNew.GetDeletionTimestamp() != nil || e.ObjectNew.GetGeneration() == 1 {
+				q.Add(reconcileRequest(e.ObjectNew))
+				return
+			}
+
+			addWithJitter(e.ObjectNew, q, ptr.Deref(jitterUpdates, false))
+		},
+		DeleteFunc: func(_ context.Context, e event.DeleteEvent, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+			if e.Object == nil {
+				return
+			}
+			q.Add(reconcileRequest(e.Object))
+		},
+	}
+}
--- pkg/gardenlet/controller/managedseed/add.go
+++ pkg/gardenlet/controller/managedseed/add.go
@@ -9,14 +9,12 @@ import (
 	"strings"
 
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/util/workqueue"
 	"k8s.io/utils/clock"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -26,7 +24,7 @@ import (
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	seedmanagementv1alpha1 "github.com/gardener/gardener/pkg/apis/seedmanagement/v1alpha1"
-	"github.com/gardener/gardener/pkg/utils"
+	"github.com/gardener/gardener/pkg/controllerutils"
 )
 
 // ControllerName is the name of this controller.
@@ -114,77 +112,19 @@ func (r *Reconciler) MapSeedToManagedSeed(_ context.Context, obj client.Object) 
 	return []reconcile.Request{{NamespacedName: types.NamespacedName{Namespace: r.GardenNamespaceGarden, Name: obj.GetName()}}}
 }
 
-func reconcileRequest(obj client.Object) reconcile.Request {
-	return reconcile.Request{NamespacedName: types.NamespacedName{
-		Name:      obj.GetName(),
-		Namespace: obj.GetNamespace(),
-	}}
-}
-
-// RandomDurationWithMetaDuration is an alias for `utils.RandomDurationWithMetaDuration`. Exposed for unit tests.
-var RandomDurationWithMetaDuration = utils.RandomDurationWithMetaDuration
-
 // EnqueueWithJitterDelay returns handler.Funcs which enqueues the object with a random Jitter duration when the JitterUpdate
 // is enabled in ManagedSeed controller configuration.
 // All other events are normally enqueued.
 func (r *Reconciler) EnqueueWithJitterDelay() handler.EventHandler {
-	return &handler.Funcs{
-		CreateFunc: func(_ context.Context, evt event.CreateEvent, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
-			managedSeed, ok := evt.Object.(*seedmanagementv1alpha1.ManagedSeed)
+	return controllerutils.EnqueueWithJitterDelay(
+		func(obj client.Object) (int64, bool) {
+			ms, ok := obj.(*seedmanagementv1alpha1.ManagedSeed)
 			if !ok {
-				return
+				return 0, false
 			}
-
-			generationChanged := managedSeed.Generation != managedSeed.Status.ObservedGeneration
-
-			// Managed seed with deletion timestamp and newly created managed seed will be enqueued immediately.
-			// Generation is 1 for newly created objects.
-			if managedSeed.DeletionTimestamp != nil || managedSeed.Generation == 1 {
-				q.Add(reconcileRequest(evt.Object))
-				return
-			}
-
-			if generationChanged {
-				if *r.Config.Controllers.ManagedSeed.JitterUpdates {
-					q.AddAfter(reconcileRequest(evt.Object), RandomDurationWithMetaDuration(r.Config.Controllers.ManagedSeed.SyncJitterPeriod))
-				} else {
-					q.Add(reconcileRequest(evt.Object))
-				}
-				return
-			}
-			// Spread reconciliation of managed seeds (including gardenlet updates/rollouts) across the configured sync jitter
-			// period to avoid overloading the gardener-apiserver if all gardenlets in all managed seeds are (re)starting
-			// roughly at the same time.
-			q.AddAfter(reconcileRequest(evt.Object), RandomDurationWithMetaDuration(r.Config.Controllers.ManagedSeed.SyncJitterPeriod))
+			return ms.Status.ObservedGeneration, true
 		},
-		UpdateFunc: func(_ context.Context, evt event.UpdateEvent, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
-			managedSeed, ok := evt.ObjectNew.(*seedmanagementv1alpha1.ManagedSeed)
-			if !ok {
-				return
-			}
-
-			if managedSeed.Generation == managedSeed.Status.ObservedGeneration {
-				return
-			}
-
-			// Managed seed with deletion timestamp and newly created managed seed will be enqueued immediately.
-			// Generation is 1 for newly created objects.
-			if managedSeed.DeletionTimestamp != nil || managedSeed.Generation == 1 {
-				q.Add(reconcileRequest(evt.ObjectNew))
-				return
-			}
-
-			if ptr.Deref(r.Config.Controllers.ManagedSeed.JitterUpdates, false) {
-				q.AddAfter(reconcileRequest(evt.ObjectNew), RandomDurationWithMetaDuration(r.Config.Controllers.ManagedSeed.SyncJitterPeriod))
-			} else {
-				q.Add(reconcileRequest(evt.ObjectNew))
-			}
-		},
-		DeleteFunc: func(_ context.Context, evt event.DeleteEvent, q workqueue.TypedRateLimitingInterface[reconcile.Request]) {
-			if evt.Object == nil {
-				return
-			}
-			q.Add(reconcileRequest(evt.Object))
-		},
-	}
+		r.Config.Controllers.ManagedSeed.JitterUpdates,
+		r.Config.Controllers.ManagedSeed.SyncJitterPeriod,
+	)
 }
