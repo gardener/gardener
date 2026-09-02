ba931a13d03cae19a5aab435b55d1558a43dbb28
acumino <sonu.kumar.singh02@sap.com>
2026-09-02 19:32:37 +0530

    Adapt shoot validator plugin to not validate os version
    Signed-off-by: acumino <sonu.kumar.singh02@sap.com>

--- plugin/pkg/shoot/validator/admission.go
+++ plugin/pkg/shoot/validator/admission.go
@@ -1060,6 +1060,11 @@ func (c *validationContext) validateMachineCapabilities(path *field.Path, worker
 }
 
 func (c *validationContext) validateMachineImage(idxPath *field.Path, worker core.Worker, oldWorker core.Worker, isNewWorkerPool bool, a admission.Attributes) *field.Error {
+	// For unmanaged infrastructure shoots, the OS is not managed by the shoot, do not default
+	// Machine image version is not required and must not be validated against the CloudProfile.
+	if helper.IsShootSelfHosted(c.shoot.Spec.Provider.Workers) && !helper.HasManagedInfrastructure(c.shoot) {
+		return nil
+	}
 	isUpdateStrategyInPlace := helper.IsUpdateStrategyInPlace(worker.UpdateStrategy)
 	isMachineImagePresentInCloudprofile, architectureSupported, activeMachineImageVersion, inPlaceUpdateSupported, validMachineImageVersions := validateMachineImagesConstraints(a, c.cloudProfileSpec.MachineImages, isNewWorkerPool, isUpdateStrategyInPlace, worker.Machine, oldWorker.Machine, c.cloudProfileSpec.MachineCapabilities)
 	if !isMachineImagePresentInCloudprofile {
