e6a52a32be64feb1cd6e618da595f4fcf9991cee
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-09-03 20:06:32 +0300

    [GEP-28] `gardenadm restore`: Delete the prior Node and its Pods before running the init flow (#15590)
    * `gardenadm restore`: Delete the prior Node and its Pods before running the init flow
    
    Signed-off-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    * Address PR review comments from rfranzke
    
    Signed-off-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    * Address PR review comments from acumino
    
    Signed-off-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    * Update the license headers
    
    https://github.com/gardener/gardener/pull/15592 was meanwhile merged.
    Signed-off-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>

---
+++ pkg/gardenadm/botanist/restore.go
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: Contributors to the Gardener project
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package botanist
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/gardener/pkg/api/indexer"
+	"github.com/gardener/gardener/pkg/utils/flow"
+	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
+)
+
+// DeletePriorNode deletes the Node object of the prior control plane Node that was lost during the disaster.
+func (b *GardenadmBotanist) DeletePriorNode(ctx context.Context, priorNodeName string) error {
+	node := &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: priorNodeName}}
+	b.Logger.Info("Deleting prior Node", "node", client.ObjectKeyFromObject(node))
+
+	return kubernetesutils.DeleteObject(ctx, b.SeedClientSet.Client(), node)
+}
+
+// ForceDeletePriorNodePods force-deletes all Pods that were running on the prior control plane Node
+// that was lost during the disaster.
+func (b *GardenadmBotanist) ForceDeletePriorNodePods(ctx context.Context, priorNodeName string) error {
+	podList := &corev1.PodList{}
+	if err := b.SeedClientSet.Client().List(ctx, podList, client.MatchingFields{indexer.PodNodeName: priorNodeName}); err != nil {
+		return fmt.Errorf("failed listing Pods: %w", err)
+	}
+
+	var (
+		taskFns []flow.TaskFn
+
+		forceDeleteOptions = &client.DeleteOptions{
+			GracePeriodSeconds: new(int64(0)),
+			PropagationPolicy:  new(metav1.DeletePropagationBackground),
+		}
+	)
+	for _, pod := range podList.Items {
+		taskFns = append(taskFns, func(ctx context.Context) error {
+			b.Logger.Info("Force deleting Pod", "pod", client.ObjectKeyFromObject(&pod), "nodeName", pod.Spec.NodeName)
+			if err := b.SeedClientSet.Client().Delete(ctx, &pod, forceDeleteOptions); client.IgnoreNotFound(err) != nil {
+				return fmt.Errorf("failed force deleting Pod %s: %w", client.ObjectKeyFromObject(&pod), err)
+			}
+
+			return nil
+		})
+	}
+
+	return flow.ParallelN(5, taskFns...)(ctx)
+}
