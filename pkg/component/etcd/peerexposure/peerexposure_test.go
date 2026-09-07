27c1a8e30e70030404740ff1da0583bf5708cfc9
acumino <sonu.kumar.singh02@sap.com>
2026-09-07 21:16:04 +0530

    Use netpol controller for creating the requied netpol

--- pkg/component/etcd/peerexposure/peerexposure_test.go
+++ pkg/component/etcd/peerexposure/peerexposure_test.go
@@ -12,12 +12,13 @@ import (
 	. "github.com/onsi/gomega"
 	istioapinetworkingv1beta1 "istio.io/api/networking/v1beta1"
 	istionetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
-	networkingv1 "k8s.io/api/networking/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
+	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
 	"github.com/gardener/gardener/pkg/component"
 	. "github.com/gardener/gardener/pkg/component/etcd/peerexposure"
 	. "github.com/gardener/gardener/pkg/utils/test/matchers"
@@ -68,7 +69,7 @@ var _ = Describe("PeerExposure", func() {
 	BeforeEach(func() {
 		s := runtime.NewScheme()
 		Expect(istionetworkingv1beta1.AddToScheme(s)).To(Succeed())
-		Expect(networkingv1.AddToScheme(s)).To(Succeed())
+		Expect(corev1.AddToScheme(s)).To(Succeed())
 		c = fakeclient.NewClientBuilder().WithScheme(s).Build()
 
 		values = Values{
@@ -130,33 +131,19 @@ var _ = Describe("PeerExposure", func() {
 			Expect(se.Spec.Ports[0].Protocol).To(Equal("TLS"))
 		})
 
-		It("should create a network policy allowing the istio ingress gateway to reach the etcd peer port", func() {
+		It("should create Service with both peer and client ports and namespace-selectors pointing to istio-ingress", func() {
 			Expect(component_.Deploy(ctx)).To(Succeed())
 
-			networkPolicy := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-peer", Namespace: namespace}}
-			Expect(c.Get(ctx, client.ObjectKeyFromObject(networkPolicy), networkPolicy)).To(Succeed())
-			Expect(networkPolicy.Labels).To(HaveKeyWithValue("app", "etcd-peer-exposure"))
-			Expect(networkPolicy.Spec.PodSelector.MatchLabels).To(HaveKeyWithValue("app", "etcd-statefulset"))
-			Expect(networkPolicy.Spec.PodSelector.MatchLabels).To(HaveKeyWithValue("role", "main"))
-			Expect(networkPolicy.Spec.PolicyTypes).To(ConsistOf(networkingv1.PolicyTypeIngress))
-			Expect(networkPolicy.Spec.Ingress).To(HaveLen(1))
-			Expect(networkPolicy.Spec.Ingress[0].From[0].NamespaceSelector.MatchLabels).To(HaveKeyWithValue("kubernetes.io/metadata.name", istioNS))
-			Expect(networkPolicy.Spec.Ingress[0].From[0].PodSelector.MatchLabels).To(Equal(istioLabels))
-			Expect(networkPolicy.Spec.Ingress[0].Ports[0].Port.IntValue()).To(Equal(2380))
-		})
-
-		It("should create an egress network policy in the istio-ingress namespace allowing the gateway to reach etcd pods on the peer port", func() {
-			Expect(component_.Deploy(ctx)).To(Succeed())
-
-			egressNP := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-peer-" + namespace, Namespace: istioNS}}
-			Expect(c.Get(ctx, client.ObjectKeyFromObject(egressNP), egressNP)).To(Succeed())
-			Expect(egressNP.Labels).To(HaveKeyWithValue("app", "etcd-peer-exposure"))
-			Expect(egressNP.Spec.PodSelector.MatchLabels).To(Equal(istioLabels))
-			Expect(egressNP.Spec.PolicyTypes).To(ConsistOf(networkingv1.PolicyTypeEgress))
-			Expect(egressNP.Spec.Egress).To(HaveLen(1))
-			Expect(egressNP.Spec.Egress[0].To[0].NamespaceSelector.MatchLabels).To(HaveKeyWithValue("kubernetes.io/metadata.name", namespace))
-			Expect(egressNP.Spec.Egress[0].To[0].PodSelector.MatchLabels).To(HaveKeyWithValue("app", "etcd-statefulset"))
-			Expect(egressNP.Spec.Egress[0].Ports[0].Port.IntValue()).To(Equal(2380))
+			svc := &corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: "etcd-main-np", Namespace: namespace}}
+			Expect(c.Get(ctx, client.ObjectKeyFromObject(svc), svc)).To(Succeed())
+			Expect(svc.Labels).To(HaveKeyWithValue("app", "etcd-peer-exposure"))
+			Expect(svc.Spec.Selector).To(HaveKeyWithValue("app", "etcd-statefulset"))
+			Expect(svc.Spec.Selector).To(HaveKeyWithValue("role", "main"))
+			Expect(svc.Spec.Ports).To(HaveLen(2))
+			ports := []int32{svc.Spec.Ports[0].Port, svc.Spec.Ports[1].Port}
+			Expect(ports).To(ConsistOf(BeEquivalentTo(2380), BeEquivalentTo(2379)))
+			Expect(svc.Annotations).To(HaveKey(resourcesv1alpha1.NetworkingNamespaceSelectors))
+			Expect(svc.Annotations).To(HaveKeyWithValue(resourcesv1alpha1.NetworkingPodLabelSelectorNamespaceAlias, "all-shoots"))
 		})
 
 		Context("with multiple members", func() {
@@ -257,33 +244,6 @@ var _ = Describe("PeerExposure", func() {
 				Expect(clientSE.Spec.Ports[0].Name).To(Equal("tls-etcd-client"))
 				Expect(clientSE.Spec.Ports[0].Protocol).To(Equal("TLS"))
 			})
-
-			It("should create a network policy allowing the istio ingress gateway to reach the etcd client port", func() {
-				Expect(component_.Deploy(ctx)).To(Succeed())
-
-				clientNP := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-client", Namespace: namespace}}
-				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientNP), clientNP)).To(Succeed())
-				Expect(clientNP.Spec.PodSelector.MatchLabels).To(HaveKeyWithValue("app", "etcd-statefulset"))
-				Expect(clientNP.Spec.PodSelector.MatchLabels).To(HaveKeyWithValue("role", "main"))
-				Expect(clientNP.Spec.PolicyTypes).To(ConsistOf(networkingv1.PolicyTypeIngress))
-				Expect(clientNP.Spec.Ingress).To(HaveLen(1))
-				Expect(clientNP.Spec.Ingress[0].From[0].NamespaceSelector.MatchLabels).To(HaveKeyWithValue("kubernetes.io/metadata.name", istioNS))
-				Expect(clientNP.Spec.Ingress[0].From[0].PodSelector.MatchLabels).To(Equal(istioLabels))
-				Expect(clientNP.Spec.Ingress[0].Ports[0].Port.IntValue()).To(Equal(2379))
-			})
-
-			It("should create an egress network policy in the istio namespace allowing the gateway to reach etcd pods on the client port", func() {
-				Expect(component_.Deploy(ctx)).To(Succeed())
-
-				clientEgressNP := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-client-" + namespace, Namespace: istioNS}}
-				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientEgressNP), clientEgressNP)).To(Succeed())
-				Expect(clientEgressNP.Spec.PodSelector.MatchLabels).To(Equal(istioLabels))
-				Expect(clientEgressNP.Spec.PolicyTypes).To(ConsistOf(networkingv1.PolicyTypeEgress))
-				Expect(clientEgressNP.Spec.Egress).To(HaveLen(1))
-				Expect(clientEgressNP.Spec.Egress[0].To[0].NamespaceSelector.MatchLabels).To(HaveKeyWithValue("kubernetes.io/metadata.name", namespace))
-				Expect(clientEgressNP.Spec.Egress[0].To[0].PodSelector.MatchLabels).To(HaveKeyWithValue("app", "etcd-statefulset"))
-				Expect(clientEgressNP.Spec.Egress[0].Ports[0].Port.IntValue()).To(Equal(2379))
-			})
 		})
 
 		Context("when ClientHost is not set", func() {
@@ -297,17 +257,15 @@ var _ = Describe("PeerExposure", func() {
 	})
 
 	Describe("#Destroy", func() {
-		It("should delete the gateway, virtual service, service entries and network policies", func() {
+		It("should delete the gateway, virtual service, service entries and NP service", func() {
 			Expect(component_.Deploy(ctx)).To(Succeed())
 			Expect(component_.Destroy(ctx)).To(Succeed())
 
-			networkPolicy := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-peer", Namespace: namespace}}
-			egressNP := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-peer-" + namespace, Namespace: istioNS}}
+			npSvc := &corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: "etcd-main-np", Namespace: namespace}}
 			se := &istionetworkingv1beta1.ServiceEntry{ObjectMeta: metav1.ObjectMeta{Name: "etcd-main-peer-0", Namespace: namespace}}
 			Expect(c.Get(ctx, client.ObjectKeyFromObject(gateway), gateway)).To(BeNotFoundError())
 			Expect(c.Get(ctx, client.ObjectKeyFromObject(virtualService), virtualService)).To(BeNotFoundError())
-			Expect(c.Get(ctx, client.ObjectKeyFromObject(networkPolicy), networkPolicy)).To(BeNotFoundError())
-			Expect(c.Get(ctx, client.ObjectKeyFromObject(egressNP), egressNP)).To(BeNotFoundError())
+			Expect(c.Get(ctx, client.ObjectKeyFromObject(npSvc), npSvc)).To(BeNotFoundError())
 			Expect(c.Get(ctx, client.ObjectKeyFromObject(se), se)).To(BeNotFoundError())
 		})
 
@@ -324,13 +282,9 @@ var _ = Describe("PeerExposure", func() {
 				clientGateway := &istionetworkingv1beta1.Gateway{ObjectMeta: metav1.ObjectMeta{Name: "etcd-main-client", Namespace: namespace}}
 				clientVS := &istionetworkingv1beta1.VirtualService{ObjectMeta: metav1.ObjectMeta{Name: "etcd-main-client", Namespace: namespace}}
 				clientSE := &istionetworkingv1beta1.ServiceEntry{ObjectMeta: metav1.ObjectMeta{Name: "etcd-main-client", Namespace: namespace}}
-				clientNP := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-client", Namespace: namespace}}
-				clientEgressNP := &networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: "allow-istio-ingress-to-etcd-main-client-" + namespace, Namespace: istioNS}}
 				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientGateway), clientGateway)).To(BeNotFoundError())
 				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientVS), clientVS)).To(BeNotFoundError())
 				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientSE), clientSE)).To(BeNotFoundError())
-				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientNP), clientNP)).To(BeNotFoundError())
-				Expect(c.Get(ctx, client.ObjectKeyFromObject(clientEgressNP), clientEgressNP)).To(BeNotFoundError())
 			})
 		})
 	})
