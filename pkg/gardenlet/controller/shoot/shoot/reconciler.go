b873bd53353dd66f5b53781db70c1e596359a426
Jan Meis <jan.meis@sap.com>
2026-09-07 09:15:25 +0200

    Do not update the shoot status. This leads to too many problems (e.g. the finalizerremoval admission plugin checks for the succeeded status) and only solves half of the visibility problem, since failure to remove non-gardenelt finalizers would not have been reflected in the shoot status anyways.
    Signed-off-by: Jan Meis <jan.meis@sap.com>

--- pkg/gardenlet/controller/shoot/shoot/reconciler.go
+++ pkg/gardenlet/controller/shoot/shoot/reconciler.go
@@ -503,6 +503,7 @@ func (r *Reconciler) deleteClusterResourceFromSeed(ctx context.Context, shoot *g
 func (r *Reconciler) removeFinalizerFromShoot(ctx context.Context, log logr.Logger, shoot *gardencorev1beta1.Shoot) error {
 	operationType := gardencorev1beta1.LastOperationTypeDelete
 
+	// this is necessary because the finalizerremoval admission plugin checks the status
 	if err := r.patchShootStatusOperationSuccess(ctx, shoot, operationType); err != nil {
 		return err
 	}
@@ -510,9 +511,8 @@ func (r *Reconciler) removeFinalizerFromShoot(ctx context.Context, log logr.Logg
 	if controllerutil.ContainsFinalizer(shoot, gardencorev1beta1.GardenerName) {
 		log.Info("Removing finalizer")
 		if err := controllerutils.RemoveFinalizers(ctx, r.GardenClient, shoot, gardencorev1beta1.GardenerName); err != nil {
-			updateErr := r.patchShootStatusOperationError(ctx, shoot, err.Error(), operationType, false, shoot.Status.LastErrors...)
 			r.Recorder.Eventf(shoot, nil, corev1.EventTypeWarning, gardencorev1beta1.EventDeleteError, gardencorev1beta1.EventActionDelete, "failed to remove finalizer: %v", err)
-			return errorsutils.WithSuppressed(fmt.Errorf("failed to remove finalizer: %w", err), updateErr)
+			return fmt.Errorf("failed to remove finalizer: %w", err)
 		}
 	}
 
