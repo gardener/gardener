d4836d3d07bf73ab7b101725e6041cac5d1327fc
Viktor <v.kostov@sap.com>
2026-09-16 14:35:25 +0300

    Pin bootstrap GRM to control plane Node
    The bootstrap-mode (BootstrapControlPlaneNode) gardener-resource-manager runs
    with hostNetwork and reaches the kube-apiserver via localhost, which only works
    on the control plane Node. If scheduled onto a worker Node it cannot reach the
    API server. Pin it there via a nodeSelector on the
    `node-role.kubernetes.io/control-plane` label.
    
    Signed-off-by: Viktor <v.kostov@sap.com>

--- pkg/component/gardener/resourcemanager/resource_manager_test.go
+++ pkg/component/gardener/resourcemanager/resource_manager_test.go
@@ -745,6 +745,7 @@ var _ = Describe("ResourceManager", func() {
 					corev1.Toleration{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoExecute},
 				)
 				deployment.Spec.Template.Spec.Containers[0].Env = []corev1.EnvVar{{Name: "KUBERNETES_SERVICE_HOST", Value: "localhost"}}
+				deployment.Spec.Template.Spec.NodeSelector = map[string]string{"node-role.kubernetes.io/control-plane": ""}
 				deployment.Spec.Template.Spec.HostNetwork = true
 				deployment.Spec.Template.Spec.PriorityClassName = "system-cluster-critical"
 			}
