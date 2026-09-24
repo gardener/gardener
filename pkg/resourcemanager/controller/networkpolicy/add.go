f913192779a86eb918d6c26720a7eb37556404c3
Matthias Horne <matthias.horne@sap.com>
2026-09-24 16:16:33 +0200

    Add option to override label selector for NetworkPolicy (#15661)
    * Add option to override label selector for NetworkPolicy
    
    This is useful in case Cilium is ignoring the label selector used for
    the Service. The VPN seed server is one case where this is relevant.
    
    Signed-off-by: Matthias Horne <matthias.horne@sap.com>
    
    * Refactor tests
    
    Signed-off-by: Matthias Horne <matthias.horne@sap.com>
    
    * Check for empty pod selector
    
    Signed-off-by: Matthias Horne <matthias.horne@sap.com>
    
    * update docs
    
    Signed-off-by: Matthias Horne <matthias.horne@sap.com>
    
    * rename annotation to networkpolicy-pod-selector
    
    Signed-off-by: Matthias Horne <matthias.horne@sap.com>
    
    ---------
    
    Signed-off-by: Matthias Horne <matthias.horne@sap.com>

--- pkg/resourcemanager/controller/networkpolicy/add.go
+++ pkg/resourcemanager/controller/networkpolicy/add.go
@@ -178,6 +178,7 @@ func (r *Reconciler) ServicePredicate() predicate.Predicate {
 				!apiequality.Semantic.DeepEqual(service.Spec.Selector, oldService.Spec.Selector) ||
 				!apiequality.Semantic.DeepEqual(service.Spec.Ports, oldService.Spec.Ports) ||
 				oldService.Annotations[resourcesv1alpha1.NetworkingPodLabelSelectorNamespaceAlias] != service.Annotations[resourcesv1alpha1.NetworkingPodLabelSelectorNamespaceAlias] ||
+				oldService.Annotations[resourcesv1alpha1.NetworkingNetworkPolicyPodSelector] != service.Annotations[resourcesv1alpha1.NetworkingNetworkPolicyPodSelector] ||
 				oldService.Annotations[resourcesv1alpha1.NetworkingNamespaceSelectors] != service.Annotations[resourcesv1alpha1.NetworkingNamespaceSelectors] ||
 				oldService.Annotations[resourcesv1alpha1.NetworkingFromWorldToPorts] != service.Annotations[resourcesv1alpha1.NetworkingFromWorldToPorts] ||
 				fromPolicyAnnotationsChanged(oldService.Annotations, service.Annotations)
